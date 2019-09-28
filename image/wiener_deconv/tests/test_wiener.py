d76fe802704bce9fd42372beab37ffab669910fc
youwen <maoyouwengo@gmail.com>
2019-09-28 21:13:02 +1000

    added test scripts for each method

--- image/wiener_deconv/tests/test_wiener.py
+++ image/wiener_deconv/tests/test_wiener.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from os.path import abspath, dirname, join as pjoin
-import sys
+from os.path import abspath, dirname
+from sys import path
 import numpy as np
-from skimage import color, data
+import tensorflow as tf
 
-sys.path.append(dirname(dirname(abspath(__file__))))
+path.append(dirname(dirname(abspath(__file__))))
 
 import wiener
 
@@ -12,13 +12,21 @@ def test_astronaut():
     psf = np.ones((5, 5)) / 25
     deconvolved = wiener.wiener(np.load('astronaut_nosiy.npy'), psf, 1)
     np.testing.assert_allclose(deconvolved, np.load('astronaut.npy'), rtol=1e-3)
-    
-def test_astronaut_real():
-    psf = np.ones((5, 5)) / 25
     deconvolved = wiener.wiener(np.load('astronaut_nosiy.npy'), psf, 1, is_real=False)
     np.testing.assert_allclose(np.real(deconvolved), np.load('astronaut.npy'), rtol=1e-3, atol=1e-3)
 
+def test_ir2tf():
+    sess = tf.InteractiveSession()
+    np.testing.assert_allclose(np.array([[4, 0], [0, 0]]), wiener._ir2tf(np.ones((2, 2)), (2, 2), sess).eval())
+    np.testing.assert_allclose((512, 257), (tuple)(tf.shape(wiener._ir2tf(np.ones((2, 2)), (512, 512), sess)).eval()))
+    np.testing.assert_allclose((512, 512), (tuple)(tf.shape(wiener._ir2tf(np.ones((2, 2)), (512, 512), sess, is_real=False)).eval()))
+
+def test_laplacian():
+    sess = tf.InteractiveSession()
+    trans, ir = wiener._laplacian(2, (32, 32), sess)
+    np.testing.assert_allclose(np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]), ir.eval())
+    np.testing.assert_allclose(trans.eval(), wiener._ir2tf(ir, (32, 32), sess).eval())
 
 if __name__ == '__main__':
-    test_astronaut()
-    test_astronaut_real()
+    from numpy import testing
+    testing.run_module_suite()
