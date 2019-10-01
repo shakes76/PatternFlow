286597c75efd15358c27c907e4549ea8f2f246fc
antoinedelplace <34864698+antoinedelplace@users.noreply.github.com>
2019-10-01 12:19:04 +1000

    Wrap driver in main() function

--- metrics/vif/main.py
+++ metrics/vif/main.py
@@ -9,12 +9,12 @@ from tabulate import tabulate
 import vif
 
 ##Driver script
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
+def main(tab_image_filenames):
+    if len(tab_image_filenames) < 2:
         raise NameError('Missing arguments: needs one reference image and at least one image to compare')
     else:
-        name_ref = sys.argv[1]
-        name_query = sys.argv[2:]
+        name_ref = tab_image_filenames[0]
+        name_query = tab_image_filenames[1:]
 
     img_ref = imageio.imread(name_ref, as_gray=True).astype(np.float32)
     img_query = []
@@ -23,4 +23,8 @@ if __name__ == "__main__":
     
     vif_tab = vif.pbvif(img_ref, img_query)
 
-    print(tabulate([(name_query[i], vif_tab[i]) for i in range(len(name_query))], headers=['Image', 'pbvif'], tablefmt='github'))
+    return tabulate([(name_query[i], vif_tab[i]) for i in range(len(name_query))], headers=['Image', 'pbvif'], tablefmt='github')
+
+
+if __name__ == "__main__":
+    print(main(sys.argv[1:]))
