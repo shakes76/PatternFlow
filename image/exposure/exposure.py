a19b2b067d470f31e72c979f76b26bc8745b64e0
Taotao Pan <pantaotao@icloud.com>
2019-10-07 15:52:51 +1000

    ADD: func rescale_intensity

--- image/exposure/exposure.py
+++ image/exposure/exposure.py
@@ -319,15 +319,18 @@ def intensity_range(image: torch.Tensor,
         If True, clip the negative range (i.e. return 0 for min intensity)
         even if the image dtype allows negative values.
     """
-    dtype: torch.dtype
+    dtype: Union[torch.dtype, str]
     if range_values == 'dtype':
         dtype = image.dtype
+    else:
+        dtype = '_'
 
     if range_values == 'image':
         i_min = torch.min(image).item()
         i_max = torch.max(image).item()
     elif dtype in DTYPE_RANGE:
-        i_min, i_max = DTYPE_RANGE[dtype]
+        if isinstance(dtype, torch.dtype):
+            i_min, i_max = DTYPE_RANGE[dtype]
         if clip_negative:
             i_min = 0
     else:
@@ -336,3 +339,87 @@ def intensity_range(image: torch.Tensor,
         else:
             raise ValueError("range_values must be a tuple")
     return i_min, i_max
+
+
+def rescale_intensity(image: torch.Tensor,
+                      in_range: Union[Tuple[float, float], str] = 'image',
+                      out_range: Union[Tuple[float, float], str] = 'dtype') -> torch.Tensor:
+    """Return image after stretching or shrinking its intensity levels.
+
+    The desired intensity range of the input and output, `in_range` and
+    `out_range` respectively, are used to stretch or shrink the intensity range
+    of the input image. See examples below.
+
+    Parameters
+    ----------
+    image : torch.Tensor
+        Input image
+    in_range, out_range : Optional[Union[Tuple[float, float], str]], default 'image'
+        Min and max intensity values of input and output image.
+        The possible values for this parameter are enumerated below.
+        'image'
+        Use image min/max as the intensity range.
+        'dtype'
+        Use min/max of the image's dtype as the intensity range.
+        dtype-name
+        Use intensity range based on desired `dtype`. Must be valid key
+        in `DTYPE_RANGE`.
+        2-tuple
+        Use `range_values` as explicit min/max intensities.
+
+    Returns
+    -------
+    torch.Tensor
+        out: Image tensor after rescaling its intensity. This image is the same dtype
+        as the input image.
+
+    See also
+    -----
+    equalize_hist
+
+    Examples
+    --------
+    By default, the min/max intensities of the input image are stretched to
+    the limits allowed by the image's dtype, since `in_range` defaults to
+    'image' and `out_range` defaults to 'dtype':
+        >>> image = np.array([51, 102, 153], dtype=np.uint8)
+    >>> rescale_intensity(image)
+    array([  0, 127, 255], dtype=uint8)
+    It's easy to accidentally convert an image dtype from uint8 to float:
+    >>> 1.0 * image
+    array([  51.,  102.,  153.])
+    Use `rescale_intensity` to rescale to the proper range for float dtypes:
+    >>> image_float = 1.0 * image
+    >>> rescale_intensity(image_float)
+    array([ 0. ,  0.5,  1. ])
+    To maintain the low contrast of the original, use the `in_range` parameter:
+    >>> rescale_intensity(image_float, in_range=(0, 255))
+    array([ 0.2,  0.4,  0.6])
+    If the min/max value of `in_range` is more/less than the min/max image
+    intensity, then the intensity levels are clipped:
+    >>> rescale_intensity(image_float, in_range=(0, 102))
+    array([ 0.5,  1. ,  1. ])
+    If you have an image with signed integers but want to rescale the image to
+    just the positive range, use the `out_range` parameter:
+    >>> image = np.array([-10, 0, 10], dtype=np.int8)
+    >>> rescale_intensity(image, out_range=(0, 127))
+    array([  0,  63, 127], dtype=int8)
+    """
+    dtype = image.dtype
+
+    imin, imax = intensity_range(image, in_range)
+    omin, omax = intensity_range(image, out_range, clip_negative=(imin >= 0))
+
+    image = torch.clamp(image, imin, imax)
+
+    image = (image - imin) / float(imax-imin)
+    return torch.tensor(image * (omax-omin) + omin, dtype=dtype)
+
+
+if __name__ == "__main__":
+    args = ['image', 'dtype', (0.3, 0.4)]
+    expects = [(0.1, 0.2), (-1, 1), (0.3, 0.4)]
+    image = torch.tensor([0.1, 0.2], dtype=torch.double)
+    for arg, expect in zip(args, expects):
+        out = intensity_range(image, range_values=arg)
+        print(out)
