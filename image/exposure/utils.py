b2abf603c724c56cbbca7f7c81dff3a6b01af473
Taotao Pan <pantaotao@icloud.com>
2019-10-07 15:02:06 +1000

    ADD: np.interp reimplementation

--- image/exposure/utils.py
+++ image/exposure/utils.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple
 
 _integer_types = (torch.uint8, torch.int8, torch.int16,
                   torch.int32, torch.int64)
-_integer_ranges = {t: (torch.iinfo(t).min, torch.iinfo(t).max)
+_integer_ranges = {t: (torch.iinfo(t).min, torch.iinfo(t).max)  # type: ignore
                    for t in _integer_types}
 dtype_range = {
     torch.uint8: (0, 1),
@@ -13,6 +13,73 @@ dtype_range = {
 }
 
 dtype_range.update(_integer_ranges)
+
+
+def interp(x: torch.Tensor, xp: torch.Tensor, yp: torch.Tensor) -> torch.Tensor:
+    """perform one-dimentional interpolation
+
+    Return the one-dimentional piece-wise interpolation to a function with
+    given discrete data points (xp, yp), evaluated at image
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        the x coordinate at which to evaluate the interpolated values
+    xp : torch.Tensor
+        The x-coordinates of the data points.
+    yp : torch.Tensor
+        The y-coordinates of the data points, same length as `xp`.
+
+    Returns
+    -------
+    torch.Tensor
+        The interpolated values, same shape as `x`.
+
+    Notes
+    -----
+    Does not check that the x-coordinate sequence `xp` is increasing.
+    If `xp` is not increasing, the results are nonsense.
+    """
+    x = x.double()
+    xp = xp.double()
+    yp = yp.double()
+
+    # these two parameters are used to create linear functions
+    slope, b = _calc_coefficients(xp, yp)
+
+    # clamp flat_x in the range of min(xp),max(xp)
+    x = torch.clamp(x, min=torch.min(
+        xp).item(), max=torch.max(xp).item())
+
+    # find the inverse of linear function
+    inverse = torch.where(slope == 0.0, torch.zeros_like(b), (-b/slope))
+
+    x_value = x.repeat(xp.size()[0]-1, 1).t()
+    right_boundary = xp[1:].repeat(x.size()[0], 1)
+    right_boundary[:, -1] += 1e-7
+
+    left_boundary = xp[:-1].repeat(x.size()[0], 1)
+
+    less_than = x_value < right_boundary
+    greater_than_or_equal = x_value >= left_boundary
+
+    def lerp(xx: torch.Tensor) -> torch.Tensor:
+        slope = (yp[1:]-yp[:-1])/(xp[1:]-xp[:-1])
+        b = yp[:-1] - slope*xp[:-1]
+        result = slope * xx + b
+        # handle slope zero case
+        result = torch.where(slope == xx, torch.zeros_like(xx), result)
+        return result
+    result = lerp(torch.where(
+        less_than == greater_than_or_equal, x_value, inverse))
+    return torch.sum(result, 1)
+
+
+def _calc_coefficients(xp: torch.Tensor,
+                       yp: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    slope = (yp[1:]-yp[:-1])/(xp[1:]-xp[:-1])
+    b = yp[:-1] - slope*xp[:-1]
+    return slope, b
 
 
 def dtype_limits(image: torch.Tensor,
