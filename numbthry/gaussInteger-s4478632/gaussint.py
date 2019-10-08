613e72336eb26de3ff0851eb06f2152f1e1082f3
Solomon <solkwan@hotmail.com>
2019-10-08 20:48:10 +1000

    Made the GaussInteger class. Wrote init method.

---
+++ numbthry/gaussInteger-s4478632/gaussint.py
@@ -0,0 +1,15 @@
+
+import tensorflow as tf
+
+class GaussInteger():
+    def __init__(self, real, imag):
+
+        # Check input type.
+        if (type(real) is not int or type(imag) is not int):
+            raise TypeError("Inputs a and b of GaussInteger(a, b) must be"
+                            + " ints.:)
+
+        # Cast variables to create complex number.
+        self.real = tf.dtypes.cast(tf.constant([real]), tf.float32)
+        self.imag = tf.dtypes.cast(tf.constant([imag]), tf.float32)
+        self.num = tf.complex(self.real, self.imag)
