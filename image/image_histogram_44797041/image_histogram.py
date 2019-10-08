d71c1eb0bf8eaee9ac0723a0dcf4d57db2cf30a8
Peter Baldry <peter.baldry@grabba.com>
2019-10-08 19:23:02 +1000

    load image, get shape and flatten complete

--- image/image_histogram_44797041/image_histogram.py
+++ image/image_histogram_44797041/image_histogram.py
@@ -6,9 +6,16 @@ class ImageHistogram:
 	"""
 	"""
 
+	def __init__(self, image, nbins=256, source_range = 'image', normalize = False):
+		sess = tf.InteractiveSession()		
+		image = tf.constant(image)
+		shape = tf.shape(image)
+		if (tf.size(shape) == 3):
+			print("If this is a colour image, the histogram will be computed on the flattened image.\
+				You can instead apply this function to each color channel.")
 
-	def __init__():
-		pass
+		image_flatten = tf.reshape(image, [-1])
+		print(shape.eval())
 
 
 
--- image/image_histogram_44797041/main.py
+++ image/image_histogram_44797041/main.py
@@ -1,15 +1,21 @@
 import sys
 from image_histogram import ImageHistogram 
 
+# only to open the image
+from PIL import Image
+
 """
 Driver script for ImageHistogram module
 """
 
 
-def main():
+def main(args):
 	"""
 	"""
-	
+	fileName = args[0]
+	img = Image.open(fileName)
+	hist = ImageHistogram(img)
+
 	#TODO: get input variables
 	#TODO: create ImageHistogram instance	
 	#TODO: get output and plot maybe?
