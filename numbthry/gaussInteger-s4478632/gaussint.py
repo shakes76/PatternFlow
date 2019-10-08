acf4aaac8e3644991802f97f8dc8f20582cd66ac
Solomon <solkwan@hotmail.com>
2019-10-08 21:04:17 +1000

    Added some docstrings. Added a getNum method.
     The addition of a getNum method that returns the complex number as type python complex was added for convenience.

--- numbthry/gaussInteger-s4478632/gaussint.py
+++ numbthry/gaussInteger-s4478632/gaussint.py
@@ -3,7 +3,10 @@ import tensorflow as tf
 
 class GaussInteger():
     def __init__(self, real, imag):
-
+        """
+        Initialises the class. real is the real part and imag is the
+        imaginary part.
+        """
         # Check input type.
         if (type(real) is not int or type(imag) is not int):
             raise TypeError("Inputs a and b of GaussInteger(a, b) must be"
@@ -13,3 +16,10 @@ class GaussInteger():
         self.real = tf.dtypes.cast(tf.constant([real]), tf.float32)
         self.imag = tf.dtypes.cast(tf.constant([imag]), tf.float32)
         self.num = tf.complex(self.real, self.imag)
+
+    def getNum(self):
+        """
+        Returns the complex number as a python complex type.
+        """
+        with tf.Session() as sess:
+            return complex(self.num.eval()[0])
