e1607a424884accdfb3d17340cdf6a992e1c3bf5
tm70 <stealthreamz@gmail.com>
2019-10-08 19:00:55 +1000

    Added skeleton of helper method called at end of radon()

--- transform/radon/radon.py
+++ transform/radon/radon.py
@@ -1,13 +1,33 @@
 # Tony Meng, Student No: 443298999
 # ported from https://github.com/scikit-image/scikit-image/blob/v0.15.0/skimage/transform/radon_transform.py#L12
+# helper method https://github.com/scikit-image/scikit-image/blob/v0.15.0/skimage/transform/_warps_cy.pyx
 
 import tensorflow as tf
+
+def _warp_fast(image, H):
+    #output_shape = None
+    #order = 1
+    #mode = 'constant'
+    #cval = 0
+    
+    #img = image
+    #M = H
+    #mode_c = 'C'
+
+    rows = image.shape.as_list()[0]
+    columns = image.shape.as_list()[1]
+    out = tf.zeros([rows, columns], tf.float64)
+    
+    # bunch of function pointer code written in cython
+    # assign functions to variables
+    
+    pass
 
 def radon(image, theta = None, circle = True):
     if tf.rank(image) != 2:
         raise ValueError('The input image must be 2D')
     if theta is None:
-        theta = range(180)
+        theta = list(range(180))
         #theta = tf.range(0, 180, 1)
     
     if circle:
