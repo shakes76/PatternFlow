b40a0df599ce069574c5465f581abaf5c1e7f0a7
tm70 <stealthreamz@gmail.com>
2019-10-09 17:09:10 +1000

    Updated test script: angles and circle setting can now be provided as command line arguments

--- transform/radon/test.py
+++ transform/radon/test.py
@@ -6,15 +6,39 @@ from skimage.io import imread
 import skimage.transform as sk
 
 def main(arglist):
+    if (len(arglist) == 1):
+        file_name = arglist[0]
+        start_angle = 0
+        end_angle = 180
+        circle = True
+    elif (len(arglist) == 3):
+        file_name = arglist[0]
+        start_angle = int(arglist[1])
+        end_angle = int(arglist[2])
+        circle = True
+    elif (len(arglist) == 4):
+        file_name = arglist[0]
+        start_angle = int(arglist[1])
+        end_angle = int(arglist[2])
+        circle = bool(int(arglist[3]))
+    else:
+        print("Usage: test.py file_name {start_angle end_angle} {circle}")
+        return
+    
+    # tensorflow eager execution setting
     tf.compat.v1.disable_eager_execution()
-    #tf.executing_eagerly()
     sess = tf.compat.v1.Session()
-    image = imread(arglist[0], as_gray = True)
+    #tf.executing_eagerly()
+    
+    # read image and convert to tensor
+    image = imread(file_name, as_gray = True)
     tensorimage = tf.constant(image)
     
-    transformed = radon.radon(tensorimage, list(range(60)))
-    check = sk.radon(image, list(range(60)))
+    # calculate radon transforms
+    transformed = radon.radon(tensorimage, list(range(start_angle, end_angle)), circle)
+    check = sk.radon(image, list(range(start_angle, end_angle)), circle)
     
+    # plot images
     plt.subplot(1, 3, 1)
     plt.imshow(image)
     plt.subplot(1, 3, 2)
