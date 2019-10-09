fafbecdedc41b14591b2a0681fb92b95c3be0467
Lynn <s4601142@student.uq.edu.au>
2019-10-09 16:16:41 +1000

    Update sigmoid function and the text code and picture.

---
+++ image/sigmoid/sigmoid.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+def sigmoid(input_img, cutoff=0.5, gain=10, inv=False):
+    
+    # Transform the input_img to a tensor
+    input_tensor = tf.constant(input_img, tf.float32)
+
+    normalize = input_tensor / 256
+    #calculate (equation : O = 1/(1 + exp*(gain*(cutoff - I))))
+    sigmoid = 1.0 / (1.0 + tf.math.exp(gain * (cutoff - input_tensor)))
+    re_normalize = sigmoid * 256
+
+    init = tf.compat.v1.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        result = sess.run(re_normalize)
+        return result
