6e2d7fb617e9dba30a95696c48402933962d77e9
Peter Baldry <peter.baldry@grabba.com>
2019-10-10 12:35:35 +1000

    introduced variable source range, no magic numbers, nice plot complete + some commenting complete

--- image/image_histogram_44797041/main.py
+++ image/image_histogram_44797041/main.py
@@ -1,9 +1,11 @@
 import sys
 from image_histogram import ImageHistogram 
+# only to plot
 from matplotlib import pyplot as plt
 
-# only to open the image
+# only to read image
 from PIL import Image
+
 
 """
 Driver script for ImageHistogram module
@@ -12,11 +14,19 @@ Driver script for ImageHistogram module
 
 def main(args):
 	"""
+	Main function for example use of ImageHistogram (get the histogram of an image 
+	in tensorflow)
 	"""
+	#open image
 	fileName = args[0]
 	img = Image.open(fileName)
-	hist = ImageHistogram(img)
-	plt.plot(hist.hist)
+	#hist = ImageHistogram(img)
+	hist = ImageHistogram(img, nbins = 256, source_range = 'dtype')
+	fig, axs = plt.subplots(1,2, figsize = (15,7))
+	axs[0].set_title('Original Image')
+	axs[0].imshow(img)
+	axs[1].set_title('Image histogram values')
+	axs[1].plot(hist.hist)
 	plt.show()
 
 
