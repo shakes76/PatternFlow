406744e2d02cf9a8325b5cbeca256fdc350b4c22
Alexander Emmanuel K.J Lim <a.k.lim@uqconnect.edu.au>
2019-10-10 22:55:58 +1000

    added xgcd function

--- numbertheory/numbthy_tf.py
+++ numbertheory/numbthy_tf.py
@@ -25,3 +25,28 @@ def gcd(a,b):
     b = tf.math.abs(b)
     result = tf.while_loop(condition, body, [a,b])
     return result[1]
+
+def xgcd(a,b):
+    """Take two integers a and b 
+    then evaluate for the equation 
+    that satisfies g = ax + by"""
+    a1 = tf.constant(1); b1 = tf.constant(0)
+    a2 = tf.constant(0); b2 = tf.constant(1)
+    aneg = tf.constant(1); bneg = tf.constant(1)
+    if(a.eval() < 0):
+        a = -a; aneg = -aneg
+    if(b.eval() < 0):
+        b = -b; bneg = -bneg
+    while(1):
+        quot = -tf.math.floordiv(a,b)
+        a = tf.math.floormod(a,b)
+        a1 = tf.math.add(a1, tf.math.multiply(quot,a2))
+        b1 = tf.math.add(b1, tf.math.multiply(quot,b2))
+        if(a.eval() == 0):
+            return (b, tf.math.multiply(a2,aneg), tf.math.multiply(b2,bneg))
+        quot = -tf.math.floordiv(b,a)
+        b = tf.math.floormod(b,a)
+        a2 = tf.math.add(a2, tf.math.multiply(quot,a1))
+        b2 = tf.math.add(b2, tf.math.multiply(quot,b1)) 
+        if(b.eval() == 0):
+            return (a, tf.math.multiply(a1,aneg), tf.math.multiply(b1,bneg))
