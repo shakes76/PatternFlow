12b2911ae5624a9d74d58f52c294dab9cae2e928
Solomon <solkwan@hotmail.com>
2019-10-12 09:51:06 +1000

    Changed the class variable names to re and im. Overloaded addition and subtraction methods.

--- numbthry/gaussInteger-s4478632/gaussint.py
+++ numbthry/gaussInteger-s4478632/gaussint.py
@@ -2,20 +2,20 @@
 import tensorflow as tf
 
 class GaussInteger():
-    def __init__(self, real, imag):
+    def __init__(self, re, im):
         """
         Initialises the class. real is the real part and imag is the
         imaginary part.
         """
         # Check input type.
-        if (type(real) is not int or type(imag) is not int):
+        if (type(re) is not int or type(im) is not int):
             raise TypeError("Inputs a and b of GaussInteger(a, b) must be"
                             + " ints.")
 
         # Cast variables to create complex number.
-        self.real = tf.dtypes.cast(tf.constant([real]), tf.float32)
-        self.imag = tf.dtypes.cast(tf.constant([imag]), tf.float32)
-        self.num = tf.complex(self.real, self.imag)
+        self.re = tf.dtypes.cast(tf.constant([re]), tf.float32)
+        self.im = tf.dtypes.cast(tf.constant([im]), tf.float32)
+        self.num = tf.complex(self.re, self.im)
 
     def __str__(self):
         """
@@ -29,8 +29,8 @@ class GaussInteger():
         Return a representation of the instance.
         """
         with tf.Session() as sess:
-            return "GaussInteger(" + str(int(self.real.eval()[0])) + \
-                    ", " + str(int(self.imag.eval()[0])) + ")"
+            return "GaussInteger(" + str(int(self.re.eval()[0])) + \
+                    ", " + str(int(self.im.eval()[0])) + ")"
 
     def __eq__(self, other):
         """
@@ -42,8 +42,8 @@ class GaussInteger():
             return False
         else:
             with tf.Session() as sess:
-                return (self.real.eval() == other.real.eval() and \
-                        self.imag.eval() == other.imag.eval())[0]
+                return (self.re.eval() == other.re.eval() and \
+                        self.im.eval() == other.im.eval())[0]
 
     def __ne__(self, other):
         """
@@ -66,10 +66,10 @@ class GaussInteger():
 
         # Retrieve and evaluate the components.
         with tf.Session() as sess:
-            real = tf.math.real(result).eval()
-            imag = tf.math.imag(result).eval()
+            re = tf.math.re(result).eval()
+            im = tf.math.im(result).eval()
 
-        return GaussInteger(int(real), int(imag))
+        return GaussInteger(int(re), int(im))
 
     def norm(self):
         """
@@ -84,8 +84,50 @@ class GaussInteger():
         """
         Adds two instances of this class together.
         """
+        if type(other) is not GaussInteger:
+            raise TypeError("Operand must be int or GaussInteger")
+        
         with tf.Session() as sess:
-            sum_real = int((self.real + other.real).eval())
-            sum_imag = int((self.imag + other.imag).eval())
+            sum_re = int((self.re + other.re).eval())
+            sum_im = int((self.im + other.im).eval())
 
-        return GaussInteger(sum_real, sum_imag)
+        return GaussInteger(sum_re, sum_im)
+
+    def __add__(self, other):
+        """
+        Overloads the "+" binary operator.
+        """
+        if type(other) is int:
+            with tf.Session() as sess:
+                newRe = int(self.re.eval() + other)
+                newIm = int(self.im.eval())
+                return GaussInteger(newRe, newIm)
+        return self.add(other)
+
+    def __radd__(self, other):
+        """
+        Overloads the "+" binary operator.
+        """
+        return self + other
+
+    def __iadd__(self, other):
+        """
+        Overloads the "+=" unary operator
+        """
+        self = self + other;
+        return self
+
+    def __neg__(self):
+        """
+        Overloads the "-" unary operator
+        """
+        with tf.Session() as sess:
+            return GaussInteger(-int(self.re.eval()), -int(self.im.eval()))
+
+    def __sub__(self, other):
+        """
+        Overloads the "-" binary operator
+        """
+        return self + (-other)
+
+    #def __rsub__
