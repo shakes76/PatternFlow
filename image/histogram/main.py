a940228345c176460d0fcfc7abddfe11cd2c9060
Junjie Ye <junjieye@JunjiedeMacBook-Pro.local>
2019-10-12 22:23:28 +1000

    Add a driven file to run an example using the function in histogram.py.

---
+++ image/histogram/main.py
@@ -0,0 +1,15 @@
+from histogram.py import *
+
+noisy_image = img_as_ubyte(data.camera())
+tf.dtypes.cast(noisy_image, tf.int32)
+hist, hist_centers = histogram(noisy_image)
+
+fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+
+ax[0].imshow(noisy_image, interpolation='nearest', cmap=plt.cm.gray)
+ax[0].axis('off')
+
+ax[1].plot(hist_centers, hist, lw=2)
+ax[1].set_title('Histogram of grey values')
+
+plt.tight_layout()
