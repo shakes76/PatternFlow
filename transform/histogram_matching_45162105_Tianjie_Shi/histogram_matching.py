be7d891b2b490d38a4384d0338fd482651e5f3b7
Tristansss <43728738+Tristansss@users.noreply.github.com>
2019-10-13 09:56:42 +1000

    Update: "the comment of function match_histograms"

--- transform/histogram_matching_45162105_Tianjie_Shi/histogram_matching.py
+++ transform/histogram_matching_45162105_Tianjie_Shi/histogram_matching.py
@@ -111,29 +111,55 @@ def _interpolate( dx_T, dy_T, x, name='interpolate'):
 
     return result
 
+
+# Function3 -- _match_cumulative_cdf
 def _match_cumulative_cdf(source, template):
+    """
+    Return modified source array so that the cumulative density function of
+    its values matches the cumulative density function of the template.
+
+    Parameters
+    ----------
+    source: tensor
+        input a source image 
+
+    template: tensor 
+        input a reference image 
+
+
+    Returns
+    ------- 
+    result: tensor 
+       a new tensor after cumulative matching which has same shape with source image
+
+    """
+    # flatten the images
     source_flatten = tf.reshape(source,[-1])
     template_flatten = tf.reshape(template,[-1])
-    #sort the tensor
+
+    #data type convert to tf.int32
     source_flatten_sort = tf.cast(source_flatten ,dtype =tf.int32)
+    #sort the value in tensor so to get the matched count value(src_counts)
     source_flatten_sort = tf.sort(source_flatten_sort)
     template_flatten = tf.cast(template_flatten ,dtype =tf.int32)
     template_flatten = tf.sort(template_flatten)
-
+    # 
     src_values, src_unique_indices, src_counts = tf.unique_with_counts(source_flatten_sort)
-    #bianhua
+
+    #get the unique indiex 
     src_indice = unique_inverse(source_flatten)
     #
     tmpl_values,tmpl_unique_indices,tmpl_counts = tf.unique_with_counts(template_flatten)
     tmpl_values = tf.cast(tmpl_values,dtype=tf.float64)
+    # calculate normalized quantiles for each tensor
     source_size = tf.size(source_flatten)
     template_size = tf.size(template_flatten)
     src_quantiles = tf.cumsum(src_counts) / source_size
     tmpl_quantiles = tf.cumsum(tmpl_counts) / template_size
     #interpolate
+    #Create a list to save the interpolate value
     interp_a_values = []
     for i in src_quantiles.numpy():
-
         interp_a_values.append(_interpolate(tmpl_quantiles, tmpl_values, tf.constant([i])))
     interp_a_values = tf.convert_to_tensor(interp_a_values).numpy()
     guodu = interp_a_values[src_indice]
@@ -141,10 +167,33 @@ def _match_cumulative_cdf(source, template):
     guodu = tf.convert_to_tensor(guodu)
     result = tf.reshape(guodu,tf.shape(source))
     return result
-
     
 def match_histograms(image, reference, multichannel=False):
-     
+
+    """Adjust an image so that its cumulative histogram matches that of another.
+    The adjustment is applied separately for each channel.
+
+    Parameters
+    ----------
+    image : tensor
+        Input image. Can be gray-scale or in color.
+    reference : tensor
+        Image to match histogram of. Must have the same number of channels as
+        image.
+    multichannel : bool, optional
+        Apply the matching separately for each channel.
+    Returns
+    -------
+    matched : tensor
+        Transformed input image.
+    Raises
+    ------
+    ValueError
+        Thrown when the number of channels in the input image and the reference
+        differ.
+
+    """   
+    #Determine whether the number of channels in the two pictures is the same
     if tf.rank(image).numpy() != tf.rank(reference).numpy():
         raise ValueError('Image and reference must have the same number of channels.')
     if multichannel:
@@ -152,14 +201,15 @@ def match_histograms(image, reference, multichannel=False):
             raise ValueError('Number of channels in the input image and reference '
                              'image must match!')
 
-        
+        #Create a list to save the results of each channel
         matched_channel = []
-    
+        
         for channel in range(image.shape[-1]):
             matched_channel.append(_match_cumulative_cdf(image[..., channel], reference[..., channel]))
-            
+        # Combine the results of all channels to convert to the picture.
         matched = tf.stack([matched_channel[0],matched_channel[1],matched_channel[2]], axis=2)
-
+        #Because the value obtained is floating point,
+        # it is converted to floating point type [0,1] to get a new picture.
         matched = matched/255.
     
     else:
