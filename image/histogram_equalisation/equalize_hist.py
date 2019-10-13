85df3837262dc0c2f5f231a99f600846d96ef1eb
Darcy Russell <darcyjohnrussell@gmail.com>
2019-10-13 19:35:24 +1000

    Implemented algorithm for grayscale and extended readme

--- image/histogram_equalisation/equalize_hist.py
+++ image/histogram_equalisation/equalize_hist.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 def equalize_hist(image, nbins=256, mask=None):
 
@@ -25,19 +26,35 @@ def equalize_hist(image, nbins=256, mask=None):
     #
     # return im2.reshape(im.shape), cdf
 
+    dims = image.shape
+
     with tf.Session() as sess:
-        image_tf = tf.Variable(image)
-        values_range = tf.constant([0., 255.])
+        # Initialise variables
+        image_tf = tf.Variable(image, dtype=tf.float32)
+        values_range = tf.constant([0., 255.], dtype=tf.float32)
+        x_min = tf.constant(0, dtype=tf.float32)
+        x_max = tf.constant(255, dtype=tf.float32)
         sess.run(tf.global_variables_initializer())
 
-        # flatten image
+        # Flatten image
         image_tf = tf.reshape(image_tf, [-1])
 
-        # calculate cumulative distribution
+        # Calculate cumulative distribution
         histogram = tf.histogram_fixed_width(image_tf, values_range, nbins)
         cdf = tf.cumsum(histogram)
 
-        cdf = sess.run(cdf)
+
+        # Calculate equalised image
+        image_eq = tfp.math.interp_regular_1d_grid(tf.cast(image_tf, tf.float32), x_min, x_max, tf.cast(cdf, tf.float32))
+
+        # if dims[2]:
+        #     image_eq = tf.reshape(image_eq, [dims[0], dims[1], dims[2]])
+        # else:
+        #     image_eq = tf.reshape(image_eq, [dims[0], dims[1]])
+
+        image_eq = tf.reshape(image_eq, dims)
+        image_eq = sess.run(image_eq)
+
         sess.close()
 
-        return cdf
+        return image_eq
