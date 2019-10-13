f01275e565e53265b18ea058898b67113e83ddde
satrfall6 <satrfall6@gmail.com>
2019-10-13 21:09:52 +1000

    edited side function _denoise_tv_chambolle_nd_torch

--- torch_denoise_tv_chambolle.py
+++ torch_denoise_tv_chambolle.py
@@ -23,3 +23,21 @@ def _denoise_tv_chambolle_nd_torch(ttt, weight=0.1, eps=2.e-4, n_iter_max=200):
             dt = -pt.sum(0)
             slices_dt = [slice(None), ] * ndimt
             slices_pt = [slice(None), ] * (ndimt + 1)
+            for ax in range(ndim):
+                slices_dt[ax] = slice(1, None)
+                slices_pt[ax+1] = slice(0, -1)
+                slices_pt[0] = ax
+                dt[tuple(slices_d)] += pt[tuple(slices_p)]
+                slices_dt[ax] = slice(None)
+                slices_pt[ax+1] = slice(None)
+            outt = ttt + dt
+        else:
+            outt = ttt
+        Et = (dt ** 2).sum()
+        
+        slices_gt = [slice(None), ] * (ndimt + 1)
+        for ax in range(ndim):
+            slices_gt[ax+1] = slice(0, -1)
+            slices_gt[0] = ax
+            gt[tuple(slices_g)] = np.diff(out, axis=ax)
+            slices_g[ax+1] = slice(None)
