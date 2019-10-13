282ce3b14495591cd31f70bdcd2acac90e87c600
1tanwang <ytwang.ethan@gmail.com>
2019-10-13 11:51:58 +1000

    define needed functions

--- denoise/denoise_tv_bregman/denoise_tv_bregman.py
+++ denoise/denoise_tv_bregman/denoise_tv_bregman.py
@@ -9,6 +9,8 @@ __author__ = "Yi-Tang Wang"
 __email__ = "yitang.wang@uq.net.au"
 __reference__ = ["skimage.restoration.denoise_tv_bregman"]
 
+import torch
+
 
 def denoise_tv_bregman(image, weight, max_iter=100, eps=1e-3, isotropic=True):
     """Perform total-variation denoising using split-Bregman optimization.
@@ -32,4 +34,26 @@ def denoise_tv_bregman(image, weight, max_iter=100, eps=1e-3, isotropic=True):
     Returns:
         u : denoised image
     """
+    pass
+
+
+def atleast_3d(image):
+    """to ensure the image has at least 3 dimensions
+
+    if the input image already has at least 3 dimensions, just return the image
+    otherwise, extend the dimensionality of the image to 3 dimensions
+
+    Parameters:
+        image (torch.Tensor):
+            input image
+
+    Return:
+        image :
+            image that at least 3 dimensions
+    """
+    pass
+
+
+def reflect_image(image):
+    """reflect the input image and extend with paddings"""
     pass
