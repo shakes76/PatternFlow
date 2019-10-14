e179139e5167cb37ace30e460bdde43e3c9d14dc
khadekirti <khadekirti@gmail.com>
2019-10-15 01:45:08 +1000

    Changing and testing the local_mean function

--- downscale_local_mean.py
+++ downscale_local_mean.py
@@ -7,5 +7,30 @@ Created on Sun Oct 13 18:24:27 2019
 """
 
 import tensorflow as tf 
+from view_as_blocks import view_as_blocks
+ 
 
-
+def downscale_local_mean(image, factors, cval=0, clip=True):
+    # check the instance of factors 
+    if not isinstance(factors, tuple):
+        raise TypeError('factors needs to be a tuple')  
+    # Check if input is tensor 
+    if not tf.is_tensor(image):
+        raise TypeError('input needs to be a Tensorflow')  
+    # Check the shape of block_size and image 
+    if len(factors) != tf.shape(image).shape:
+        raise ValueError("`block_size` must have the same length "
+                         "as `image.shape`.")  
+    # Padding width    
+    pad_width = [] 
+    for i in range(len(factors)): 
+        if (image.shape[i] % factors[i] ) != 0:
+            after_width = factors[i] - (image.shape[i] % factors[i]) 
+        else:
+            after_width = tf.constant(0)
+        pad_width.append((0, after_width))    
+    image = tf.pad(image,paddings =pad_width ,mode='CONSTANT',constant_values=cval)
+    blocked = view_as_blocks(image, factors)  
+    re = tf.math.reduce_mean( tf.dtypes.cast(blocked, 'float64',) , axis=tuple(range(tf.shape(image).shape[0], tf.shape(blocked).shape[0])))
+    return re
+ 
