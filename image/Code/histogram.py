71d226abf632dcd165d83db8854b17217fddc7b9
Qijiang Yao <qijiang.yao@uq.net.au>
2019-10-14 14:24:10 +1000

    This is my original function of histogram without tensorflow

--- image/Code/histogram.py
+++ image/Code/histogram.py
@@ -1,26 +1,35 @@
 from skimage import data, exposure, img_as_float
 from skimage import img_as_ubyte
-import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
-
-image = img_as_ubyte(data.camera())  # Convert to 8-bit uint.
+image = img_as_ubyte(data.camera())
 
 def histogram(image, nbins=256, source_range='image', normalize=False):
-  image = tf.convert_to_tensor(image, dtype = tf.float32) # Change image to tensorflow
-  with tf.Session() as sess:
-    sh = sess.run(tf.shape(image))
+  sh = image.shape
   if len(sh) == 3 and sh[-1] < 4:
-    warn("This might be a color image. The histogram will be "
-          "computed on the flattened image. You can instead "
-          "apply this function to each color channel.")
-  with tf.Session() as sess:
-    image = tf.reshape(image,[-1,1]) # 512**2 ==> (262144,)
-    #print(sess.run(tf.shape(image)))  # Matrix ==> Vector
-  value_range = [0.0, 256.0] 
-  new_values = image
-  with tf.Session() as sess: 
-    hist = sess.run(tf.histogram_fixed_width ( new_values.eval() ,value_range ,nbins = 256 ) ) #Show number of values in each bin. 256 (0~256).
-  hist_centers = [i for i in range(256)] # list,(0~255)
-  return hist, hist_centers
+      warn("This might be a color image. The histogram will be "
+            "computed on the flattened image. You can instead "
+            "apply this function to each color channel.")
+
+  image = image.flatten() # 512**2 = (262144,)
+  print(type(image))
+  # For integer types, histogramming with bincount is more efficient.
+  if np.issubdtype(image.dtype, np.integer) == False:
+      hist, bin_centers = _bincount_histogram(image, source_range)
+  else:
+      if source_range == 'image':     ###
+          hist_range = None
+      elif source_range == 'dtype':
+          hist_range = dtype_limits(image, clip_negative=False)
+      else:
+          ValueError('Wrong value for the `source_range` argument')
+      hist, bin_edges = np.histogram(image, bins=nbins, range=hist_range)  # nbins = 256
+      bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2.         # hist: 数量。 bin_centers: 刻度。
+      #print(bin_centers)
+  if normalize:
+      hist = hist / np.sum(hist)
+  return hist, bin_centers
 
 hist, hist_centers = histogram(image, nbins=256, source_range='image', normalize=False)
+
+
