a630d2ecfcc106752a6dca4485c5f4ac456e7356
khadekirti <khadekirti@gmail.com>
2019-10-15 01:02:58 +1000

    Second Commit

--- view_as_block_testing.py
+++ view_as_block_testing.py
@@ -11,32 +11,49 @@ os.chdir('/Users/khadekirti/Desktop/Pattern Recognistion/Project')
  
 import tensorflow as tf
 import numpy as np 
+from view_as_blocks import view_as_blocks
+
+
 sess = tf.InteractiveSession()  
 
-import view_as_blocks 
-
-
-def view_as_blocks(arr_in, block_shape):
-    if not isinstance(block_shape, tuple):
-        raise TypeError('block needs to be a tuple') 
-    # Convert to tensorflow     
-    arr_in = tf.convert_to_tensor(arr_in)
-    block_shape = tf.convert_to_tensor(block_shape)
-    # check if all the block_shape elements is positive
-    if not tf.math.reduce_all(tf.math.greater( block_shape, 0)).eval() : 
-        raise ValueError("'block_shape' elements must be strictly positive")
-    # Check if the shape of block matches that of array  
-    if block_shape.shape != tf.shape(arr_in).shape:
-        raise ValueError("'block_shape' must have the same length as 'arr_in.shape'") 
-    # Check if the blockshape is compatible with the array 
-    if tf.math.reduce_sum(tf.mod(tf.shape(arr_in),block_shape)).eval() != 0:
-        raise ValueError("'block_shape' is not compatible with 'arr_in'") 
-    # Get the new shape
-    new_shape = tf.concat([tf.math.floordiv(tf.shape(arr_in),block_shape), block_shape], 0)   
-    # Get the shape of the array 0
-    arr_out = tf.reshape( arr_in,new_shape.eval())
-    return arr_out.eval()  
-
-
 A = np.arange(4*4*6).reshape(4,4,6)  
-B = view_as_blocks(A,(1, 2, 2)) 
+A = tf.convert_to_tensor(A)
+
+# Try first normal test  
+test1 = view_as_blocks(A,(1, 2, 2)) 
+
+# Second - if the block shape is negative 
+try: 
+    test2  = view_as_blocks(A,(-1, 2, 2)) 
+except (ValueError): 
+    print("ValueError")
+
+# Three - If the shape is different 
+try: 
+    test3  = view_as_blocks(A,(1,1, 2, 2)) 
+except (ValueError): 
+    print("ValueError")     
+    
+    
+
+# Forth - if the block shape not divisible
+try: 
+    test4  = view_as_blocks(A,(10, 2, 2)) 
+except (ValueError): 
+    print("ValueError") 
+    
+    
+
+# Fifth - if the block shape not tuple
+try: 
+    test4  = view_as_blocks(A,[1, 2, 2]) 
+except (TypeError): 
+    print("ValueError")      
+    
+
+# Sixth - if inpiut not tensorflow 
+try: 
+    test4  = view_as_blocks(A.eval(),[1, 2, 2]) 
+except (TypeError): 
+    print("ValueError")      
+         
--- view_as_blocks.py
+++ view_as_blocks.py
@@ -8,20 +8,26 @@ Created on Sun Oct 13 18:06:03 2019
 
 import tensorflow as tf 
  
+
 def view_as_blocks(arr_in, block_shape):
-    if not tf.is_tensor(block_shape) and tf.is_tensor(arr_in):
-        raise TypeError('Please input tensorflow') 
+    if not isinstance(block_shape, tuple):
+        raise TypeError('block needs to be a tuple') 
+    # Check if input is tensor 
+    if not tf.is_tensor(arr_in):
+        raise TypeError('input needs to be a Tensorflow') 
     # check if all the block_shape elements is positive
-    if not sess.run(tf.math.reduce_all(tf.math.greater( block_shape, 0))) : 
+    if not all(i >= 0 for i in block_shape): 
         raise ValueError("'block_shape' elements must be strictly positive")
     # Check if the shape of block matches that of array  
-    if block_shape.shape != tf.shape(arr_in).shape:
+    if len(block_shape) != tf.shape(arr_in).shape:
         raise ValueError("'block_shape' must have the same length as 'arr_in.shape'") 
     # Check if the blockshape is compatible with the array 
-    if sess.run(tf.math.reduce_sum(tf.mod(tf.shape(arr_in),block_shape))) != 0:
+    if sum([(arr_in.shape[i] % block_shape[i]) for i in range(len(block_shape))])  != 0:
         raise ValueError("'block_shape' is not compatible with 'arr_in'") 
     # Get the new shape
-    new_shape = tf.concat([tf.math.floordiv(tf.shape(arr_in),block_shape), block_shape], 0)   
+    new_shape = [(arr_in.shape[i] // block_shape[i]) for i in range(len(block_shape))]
+    new_shape.extend(list(block_shape))   
     # Get the shape of the array 0
     arr_out = tf.reshape( arr_in,new_shape)
-    return arr_out 
+    return arr_out
+ 
