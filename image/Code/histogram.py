b5088682ee59f00b1056e5454e60f6305799ad27
Qijiang Yao <qijiang.yao@uq.net.au>
2019-10-14 15:51:39 +1000

    Get the parameters hist(number of pixes) and hist_centers(intervals) of the output histogram by TF

--- image/Code/histogram.py
+++ image/Code/histogram.py
@@ -3,7 +3,7 @@ from skimage import img_as_ubyte
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-image = img_as_ubyte(data.camera())
+
 
 def histogram(image, nbins=256, source_range='image', normalize=False):
   image = tf.convert_to_tensor(image, dtype = tf.float32) # Change image to tensorflow
@@ -18,11 +18,9 @@ def histogram(image, nbins=256, source_range='image', normalize=False):
     image = tf.reshape(image,[-1,1]) # 512**2 ==> (262144,)
   value_range = [0.0, 256.0] 
   new_values = image
-  hist, bin_edges = np.histogram(image, bins=nbins, range=hist_range)  # nbins = 256
-  bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2.         # hist: 数量。 bin_centers: 刻度。
-      #print(bin_centers)
-  if normalize:
-      hist = hist / np.sum(hist)
+  with tf.Session() as sess: 
+    hist = sess.run(tf.histogram_fixed_width ( new_values.eval() ,value_range ,nbins = 256 ) ) #Show number of values in each bin. 256 (0~256).
+  hist_centers = [i for i in range(256)] # list,(0~255)
   return hist, bin_centers
 
 
