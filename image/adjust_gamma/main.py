0dc3898a8fe392ed9671dfafe42e4582102d34be
Loris Rossi <lorisrossirn@gmail.com>
2019-10-15 16:39:01 +1000

    Add visualisation of input and adjusted images

--- image/adjust_gamma/main.py
+++ image/adjust_gamma/main.py
@@ -1,6 +1,19 @@
+import matplotlib.pyplot as plt
 from skimage import data, img_as_float
 from adjust_gamma import adjust_gamma
 
 image = img_as_float(data.chelsea())
 
 adjusted = adjust_gamma(image)
+
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.imshow(image)
+plt.title('Source')
+
+plt.subplot(1, 2, 2)
+plt.imshow(adjusted)
+plt.title('Adjusted')
+
+plt.tight_layout()
+plt.show()
