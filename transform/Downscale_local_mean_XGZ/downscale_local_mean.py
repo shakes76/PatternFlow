b5d7507ec03e4e26bcc32fefe58c30f60319dfb2
NinaZhaoXG <41613728+NinaZhaoXG@users.noreply.github.com>
2019-10-15 11:57:36 +1000

    Update downscale_local_mean.py

--- transform/Downscale_local_mean_XGZ/downscale_local_mean.py
+++ transform/Downscale_local_mean_XGZ/downscale_local_mean.py
@@ -2,6 +2,16 @@ import tensorflow as tf
 print("TF Version: ", tf.__version__)
 tf.InteractiveSession()
 
+class DummyArray(object):
+    """Dummy object that just exists to hang __array_interface__ dictionaries
+    and possibly keep alive a reference to a base array.
+    """
+
+    def __init__(self, interface, base=None):
+        self.__array_interface__ = interface
+        self.base = base
+
+
 def downscale_local_mean(image, factors, cval=0, clip=True):
   
   return block_reduce(image, factors, tf.reduce_mean, cval)
