6a9602434459f94268ab89f032032867651d0465
Loris Rossi <lorisrossirn@gmail.com>
2019-10-15 16:54:57 +1000

    Implements dtype_limits function

--- image/adjust_gamma/adjust_gamma.py
+++ image/adjust_gamma/adjust_gamma.py
@@ -1,5 +1,25 @@
 import tensorflow as tf
 
+# data structures for dtype_limits function
+_integer_types = (tf.int8, tf.uint8,
+                tf.int16, tf.uint16,
+                tf.int32, tf.uint32,
+                tf.int64, tf.uint64)
+_integer_ranges = {t: (t.min, t.max)
+                for t in _integer_types}
+dtype_range = {tf.bool: (False, True),
+            tf.float16: (-1.0, 1.0),
+            tf.float32: (-1.0, 1.0),
+            tf.float64: (-1.0, 1.0)}
+dtype_range.update(_integer_ranges)
+
+def dtype_limits(image, clip_negative=False):
+  "Return intensity limits, i.e. (min, max) tuple, of the image's dtype."
+  imin, imax = dtype_range[image.dtype]
+  if clip_negative:
+      imin = 0
+  return imin, imax
+
 def adjust_gamma(image, gamma=1, gain=1):
   """
   Performs Gamma Correction on the input image.
