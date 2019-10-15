39037ac9794a6446a88eaf043f0cbbe825960e67
askar <askar@askars-MacBook-Pro.local>
2019-10-15 14:21:48 +1000

    fixed importing in main.py

--- image/rank_order/main.py
+++ image/rank_order/main.py
@@ -1,12 +1,15 @@
 # Author: Askar Jaboldinov
 
-import rank_order
+from rank_order import rank_order
 import sys
 import os
+from PIL import Image
 
 ##Driver script
 if __name__ == "__main__":
-    user_input = raw_input("Enter the path to the image file: ")
+    user_input = input("Enter the path to the image file: ")
 
     assert os.path.exists(user_input), "No such file: "+str(user_input)
     converted = rank_order(user_input)
+    converted_img = Image.fromarray(rank_order(user_input)[0], 'RGB')
+    converted_img.show()
