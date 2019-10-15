97d251fd4637ca6fd19bd48a0a1e32f6dd884091
dsuncn <49934337+dsuncn@users.noreply.github.com>
2019-10-15 16:43:09 +1000

    Create the helper function block_reduce
    Create the helper function block_reduce and write the description of the function

--- transform/downscale_local_mean/downscale_local_mean.py
+++ transform/downscale_local_mean/downscale_local_mean.py
@@ -2,28 +2,24 @@ import tensorflow as tf
 
 
 def downscale_local_mean(image, factors, cval=0):
-    """Down-sample N-dimensional image by local averaging.
-
+    """
+    Down-sample N-dimensional image by local averaging.
     The image is padded with 'cval' if it is not perfectly divisible by the integer factors.
-
     This function calculates the local mean of elements in each block of size 'factors' in the input image.
     Equivalent to skimage.transform.downscale_local_mean
 
-    Parameter
-    ---------
-    image : ndarray or tensor
+    :param image: ndarray or tensor
         N-dimensional input image.
-    factors : array-like
+    :param factors: array-like
         Array containing down-sampling integer factor along each axis.
-    cval : float, optional
+    :param cval: float, optional
         Constant padding value if image is not perfectly divisible by the integer factors.
 
-    Returns
-    -------
-    image : tensor
+    :return: tensor
         Down-sampled image in the format of tensor with same number of dimensions as input image.
 
     """
+
     session = tf.Session()
     # if the input is a tensor, convert it to an ndarray
     if tf.is_tensor(image):
@@ -32,3 +28,22 @@ def downscale_local_mean(image, factors, cval=0):
 
     return tf.convert_to_tensor(image_downscaled)
 
+
+def block_reduce(image, block_size, func=tf.reduce_sum, cval=0):
+    """
+    Down-sample image by applying function to local blocks.
+
+    :param image: ndarray
+        N-dimensional input image.
+    :param block_size: array-like
+        Array containing down-sampling integer factor along each axis.
+    :param func: callable
+        Function object which is used to calculate the return value for each local block. This function must implement
+        an axis parameter such as tf.reduce_sum or tf.reduce_min
+    :param cval: float
+        Constant padding value if image is not perfectly divisible by the block size.
+
+    :return: ndarray
+        Down-sampled image with same number of dimensions as input image.
+    """
+    return image
