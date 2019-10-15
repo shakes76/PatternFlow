9440bafc4302f669cc844c424e833d215c78369d
NinaZhaoXG <41613728+NinaZhaoXG@users.noreply.github.com>
2019-10-15 13:12:06 +1000

    update _maybe_view_as_subclass  function

--- transform/Downscale_local_mean_XGZ/downscale_local_mean.py
+++ transform/Downscale_local_mean_XGZ/downscale_local_mean.py
@@ -10,6 +10,17 @@ class DummyArray(object):
         self.__array_interface__ = interface
         self.base = base
         
+def _maybe_view_as_subclass(original_array, new_array):
+    if type(original_array) is not type(new_array):
+        # if input was an ndarray subclass and subclasses were OK,
+        # then view the result as that subclass.
+        new_array = new_array.view(type=type(original_array))
+        # Since we have done something akin to a view from original_array, we
+        # should let the subclass finalize (if it has it implemented, i.e., is
+        # not None).
+        if new_array.__array_finalize__:
+            new_array.__array_finalize__(original_array)
+    return new_array
 
 def downscale_local_mean(image, factors, cval=0, clip=True):
   
