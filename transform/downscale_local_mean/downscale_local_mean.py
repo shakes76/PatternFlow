6ce68c63fda8a30c88e3f946c72e5229c985f4c3
dsuncn <49934337+dsuncn@users.noreply.github.com>
2019-10-15 16:30:18 +1000

    Update downscale_local_mean function body
    Improve the function downscale_local_mean, make it be able to accept input of type ndarray and tensor, change the output type to tensor, add comments to downscale_local_mean function

--- transform/downscale_local_mean/downscale_local_mean.py
+++ transform/downscale_local_mean/downscale_local_mean.py
@@ -1,6 +1,34 @@
 import tensorflow as tf
 
 
-def downscale_local_mean(image, factors, cval=0, clip=True):
-    return block_reduce(image, factors, tf.reduce_mean, cval)
+def downscale_local_mean(image, factors, cval=0):
+    """Down-sample N-dimensional image by local averaging.
+
+    The image is padded with 'cval' if it is not perfectly divisible by the integer factors.
+
+    This function calculates the local mean of elements in each block of size 'factors' in the input image.
+    Equivalent to skimage.transform.downscale_local_mean
+
+    Parameter
+    ---------
+    image : ndarray or tensor
+        N-dimensional input image.
+    factors : array-like
+        Array containing down-sampling integer factor along each axis.
+    cval : float, optional
+        Constant padding value if image is not perfectly divisible by the integer factors.
+
+    Returns
+    -------
+    image : tensor
+        Down-sampled image in the format of tensor with same number of dimensions as input image.
+
+    """
+    session = tf.Session()
+    # if the input is a tensor, convert it to an ndarray
+    if tf.is_tensor(image):
+        image = session.run(image)
+    image_downscaled = block_reduce(image, factors, tf.reduce_mean, cval)
+
+    return tf.convert_to_tensor(image_downscaled)
 
