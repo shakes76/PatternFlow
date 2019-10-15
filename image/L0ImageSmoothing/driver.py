de99bac052efb0bbb9d8db02190277d0ca0235ac
JNRuan <JNRuan@users.noreply.github.com>
2019-10-15 20:21:14 +1000

    Initial commit. Created empty functions for driver and l0 smoothing.

---
+++ image/L0ImageSmoothing/driver.py
@@ -0,0 +1,14 @@
+import imageio
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+
+    :return:
+    """
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ image/L0ImageSmoothing/l0_image_smoothing.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def psf2otf(psf, shape):
+    """
+
+    :return:
+    """
+    return
+
+
+def l0_image_smoothing():
+    """
+
+    :return:
+    """
+    return
