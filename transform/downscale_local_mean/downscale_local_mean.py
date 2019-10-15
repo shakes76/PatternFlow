8ccd03223c8326acdc459112d328ba43102cf114
dsuncn <49934337+dsuncn@users.noreply.github.com>
2019-10-15 18:41:56 +1000

    Complete the function body for the helper function view_as_blocks
    Complete the function body and add comments for the helper function view_as_blocks

--- transform/downscale_local_mean/downscale_local_mean.py
+++ transform/downscale_local_mean/downscale_local_mean.py
@@ -89,4 +89,33 @@ def view_as_blocks(arr_in, block_shape):
     :return: ndarray
         Block view of the input arr_in.
     """
-    return arr_in
+    # check the type of the input block_shape
+    if not isinstance(block_shape, tuple):
+        raise TypeError('block needs to be a tuple')
+    # convert block shape to array format
+    session = tf.Session()
+    block_shape = tf.convert_to_tensor(block_shape)
+    block_shape = session.run(block_shape)
+    # raise errors if the input has problems
+    if (block_shape <= 0).any():
+        raise ValueError("'block_shape' elements must be strictly positive")
+    arr_in = session.run(arr_in)
+    if block_shape.size != arr_in.ndim:
+        raise ValueError("'block_shape' must have the same length "
+                         "as 'arr_in.shape'")
+    arr_shape = arr_in.shape
+    if (arr_shape % block_shape).sum() != 0:
+        raise ValueError("'block_shape' is not compatible with 'arr_in'")
+    if not arr_in.flags.contiguous:
+        raise ValueError("Cannot provide views on a non-contiguous input "
+                         "array without copying.")
+
+    # compute the new shape of the input arr_in
+    new_shape = tuple(arr_shape // block_shape) + tuple(block_shape)
+    # compute the new strides of the input arr_in
+    new_strides = tuple(arr_in.strides * block_shape) + arr_in.strides
+    arr_out = as_strided(arr_in, shape=new_shape, strides=new_strides)
+
+    return arr_out
+
+
