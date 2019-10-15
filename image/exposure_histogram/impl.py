ff26679079787e0f74a42500da2900b089458b62
Lewis B <lbobbermen@bigpond.com>
2019-10-15 22:09:14 +1000

    Extract limit code

--- image/exposure_histogram/impl.py
+++ image/exposure_histogram/impl.py
@@ -1,4 +1,7 @@
 import warnings
+import functools
+import operator
+from typing import Tuple, TypeVar
 
 import tensorflow as tf
 
@@ -6,18 +9,36 @@ __all__ = [
     'histogram'
 ]
 
+# Type variable
+T = TypeVar('T')
 
-def get_float_centers(image: tf.Tensor, nbins: int) -> tf.Tensor:
+ALLOWABLE_SOURCE_RANGES = ['image', 'dtype']
+DEFAULT_NBINS = 256
+
+def get_limits(image: tf.Tensor, source_range: str) -> Tuple[T, T]:
+    if source_range == 'image':
+        return (tf.reduce_min(image), tf.reduce_max(image))
+    elif source_range == 'dtype':
+        return image.dtype.limits
+        if image.dtype.is_floating:
+            return (-1, 1)
+        elif image.dtype.is_integer:
+            return image.dtype.limits
+        else:
+            raise TypeError("image must either be an integer of a floating point dtype")
+    else:
+        raise ValueError("source_range must be in: {}".format(', '.join(ALLOWABLE_SOURCE_RANGES)))
+
+def get_float_centers(limits: Tuple[T, T], nbins: int) -> tf.Tensor:
     """
-    Given an image and a number of bins, return the histogram centers.
+    Given limits and a number of bins, return the histogram centers.
     
      |  i1  |  i2  |  i3  |
     min                  max
     
     Returns [i1, i2, i3]
     """
-    lower = image.min()
-    upper = image.max()
+    lower, upper = limits
     range_ = upper - lower
     
     # Amount to increase each value by
@@ -26,9 +47,9 @@ def get_float_centers(image: tf.Tensor, nbins: int) -> tf.Tensor:
     offset_for_center = increment / 2
     return tf.range(start=lower + offset_for_center, limit=upper, delta=increment)
 
-def get_int_centers(image: tf.Tensor) -> tf.Tensor:
+def get_int_centers(limits: Tuple[T, T]) -> tf.Tensor:
     """
-    Given an integer image, return the histogram centers.
+    Given limits, return the histogram centers.
     
      i1   i2     i3   ...   iN
      |     |     |     |     |
@@ -36,18 +57,15 @@ def get_int_centers(image: tf.Tensor) -> tf.Tensor:
     
     Returns [i1, i2, i3, ..., iN]
     """
-    lower = image.min()
-    upper = image.max()
-    return tf.range(start=lower, limit=upper, delta=1)
+    lower, upper = limits
+    return tf.range(start=lower, limit=upper, delta=tf.constant(1))
 
-
-ALLOWABLE_SOURCE_RANGES = ['image', 'dtype']
-DEFAULT_NBINS = 256
+def normalize_tensor(tensor: tf.Tensor) -> tf.Tensor:
+    return tensor / tf.math.reduce_sum(tensor)
 
 def histogram(image: tf.Tensor, nbins: int=DEFAULT_NBINS, source_range: str='image', normalize: bool=False):
-    # TODO - source_range takes dtype aswell
-    # TODO - normalize
-    # h = tf.histogram_fixed_width(uimage, [0, 256], nbins=nbins, dtype=tf.dtypes.int64)
+    # TODO - normalize, normalizes the value
+    # TODO - cast to int32 and reshape
     
     # Check image
     if not isinstance(image, tf.Tensor):
@@ -70,4 +88,13 @@ def histogram(image: tf.Tensor, nbins: int=DEFAULT_NBINS, source_range: str='ima
     
     # Don't need to check normalize, as a user could want to pass in a boolean-like
     
-    raise NotImplementedError()
+    image_shape = image.shape
+    image = tf.reshape(image, (functools.reduce(operator.mul, image_shape),))
+    limits = get_limits(image, source_range)
+    if image.dtype.is_integer:
+        centers = get_int_centers(limits)
+        values = tf.histogram_fixed_width(image, limits, limits[1] - limits[0], dtype=image.dtype)
+    else:
+        centers = get_float_centers(limits, nbins)
+        values = tf.histogram_fixed_width(image, limits, nbins, dtype=image.dtype)
+    return values, centers
