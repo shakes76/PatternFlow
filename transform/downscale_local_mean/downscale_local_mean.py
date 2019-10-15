a7b1818dfec48519a326f5b1ae4befb42213ec91
dsuncn <49934337+dsuncn@users.noreply.github.com>
2019-10-15 19:01:49 +1000

    Set the needed helper function and class for as_strided function
    Set the class DummyArray and helper function _maybe_view_as_subclass to serve as_strided

--- transform/downscale_local_mean/downscale_local_mean.py
+++ transform/downscale_local_mean/downscale_local_mean.py
@@ -119,6 +119,28 @@ def view_as_blocks(arr_in, block_shape):
     return arr_out
 
 
+class DummyArray(object):
+    """
+    Dummy object that just exists to hang __array_interface__ dictionaries
+    and possibly keep alive a reference to a base array.
+    """
+
+    def __init__(self, interface, base=None):
+        self.__array_interface__ = interface
+        self.base = base
+
+
+def _maybe_view_as_subclass(original_array, new_array):
+    if type(original_array) is not type(new_array):
+        # if input was an ndarray subclass and subclasses were OK, then view the result as that subclass.
+        new_array = new_array.view(type=type(original_array))
+        # Since we have done something akin to a view from original_array, we should let the subclass finalize (if it
+        # has it implemented, i.e., is not None).
+        if new_array.__array_finalize__:
+            new_array.__array_finalize__(original_array)
+    return new_array
+
+
 def as_strided(x, shape=None, strides=None, writeable=True):
     """
     Create a view into the array with the given shape and strides.
