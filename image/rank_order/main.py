8325b071a0402ffb448511fb7cae1c9e6c55ce4a
askar <askar@askars-MacBook-Pro.local>
2019-10-15 15:00:50 +1000

    added warning message to main.py

--- image/rank_order/main.py
+++ image/rank_order/main.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
         converted_img.save("rank_ordered_image.jpeg")
         converted_img.show()
     else:
-        print("only jpeg type files are supported")
+        print("only jpeg type files are supported! exiting module.")
