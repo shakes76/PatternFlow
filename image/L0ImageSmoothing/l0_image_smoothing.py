Fixed minor bug that was introduced from refactor by error in variable name. normin_v2 was mistakenly also named normin_v1 which then caused issue with concatenation, as well as wrong values.

image/L0ImageSmoothing/l0_image_smoothing.py
@@ -210,10 +210,10 @@ def l0_image_smoother(img, _lambda=2e-2, kappa=2.0, beta_max=1e5):
         normin2 += normin_h
 
         normin2_v1 = v[N-1:N, :, :] - v[0:1, :, :]
-        normin2_v1 = v[1:] - v[:-1]
+        normin2_v2 = v[1:] - v[:-1]
         normin2_v = tf.concat([
             normin2_v1,
-            -normin2_v1
+            -normin2_v2
         ], axis=0)
         normin2 += normin2_v
 