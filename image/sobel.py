38a7fced8698e5211550433394d99f006e86db01
Sambit Tarai <sambitarai17@gmail.com>
2019-10-16 16:31:22 +0530

    sobel function is created

---
+++ image/sobel.py
@@ -0,0 +1,24 @@
+def Sobel(image):
+	"""
+	Edge Detection using Sobel transform.
+
+	Parameters:
+	-----------
+
+	image : 2-D array (Image to process)
+
+	Returns:
+	--------
+
+	resultant : 2-D array (The edge map of the image)
+
+	Notes:
+	------
+	Square root of the squares of the horizontal and vertical edges are taken
+	to get a magnitude i.e. somewhat insensitive to direction.
+
+
+	"""
+	#Horizontal Sobel kernel
+	
+	return 0
