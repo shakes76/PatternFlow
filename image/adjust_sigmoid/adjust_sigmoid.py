501d48426877f3cb66900bb5fc7209f5109dfcd8
FaydSpeare <fspea123@gmail.com>
2019-10-18 01:42:48 +1000

    added code for actual function

---
+++ image/adjust_sigmoid/adjust_sigmoid.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+def adjust_sigmoid(image, cutoff=0.5, gain=10.0, inv=False):
+    '''
+    Applies sigmoid correction (also known as contrast adjustment) to the input image.
+    '''
+
+    input_image = tf.constant(image, tf.float32)
+    normalized = tf.divide(image, 256.0)
+    
+    cut_off = tf.math.subtract(tf.constant(cutoff, tf.float32), normalized)
+    gained = tf.multiply(tf.constant(gain, tf.float32), cut_off)
+    exp = tf.math.exp(gained)
+    add_one = tf.math.add(exp, 1.0)
+    divider = tf.math.divide(1.0, add_one)
+    condition = tf.cond(tf.cast(inv, tf.bool), lambda: tf.subtract(1.0, divider), lambda: divider)
+    output = tf.multiply(condition, 256.0)
+
+    init = tf.compat.v1.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        adjusted_image = sess.run(output)
+        sess.close()
+        return adjusted_image
+
+
+
