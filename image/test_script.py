d26d74981460b05294a7c4b620222978ef476405
Sambit Tarai <sambitarai17@gmail.com>
2019-10-17 14:52:56 +0530

    Comments are updated to in the test script

--- image/test_script.py
+++ image/test_script.py
@@ -8,6 +8,7 @@ import skimage.io
 import matplotlib.pyplot as plt
 
 
+#We are considering only 1 of the channel of the input image to do edge detection, because the edges associated with all the channels will be same.
 image = skimage.io.imread('test_images/katy.jpg', as_grey=True)
 #Calling the Sobel function to do edge detection
 result = sobel.Sobel(image)
