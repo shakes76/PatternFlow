0a946e716148d61421bb9cca704e4d453eb671b0
1tanwang <ytwang.ethan@gmail.com>
2019-10-17 23:02:33 +1000

    update test script

--- denoise/denoise_tv_bregman/main.py
+++ denoise/denoise_tv_bregman/main.py
@@ -19,8 +19,13 @@ from denoise_tv_bregman import denoise_tv_bregman
 
 original = img_as_float(data.chelsea()[100:250, 50:300])
 
+# add noise to image
 sigma = 0.155
 noisy = random_noise(original, var=sigma**2)
+
+# the denoised image is returned as torch tensor
+# use .numpy() to convert into numpy array for plotting
+denoised = denoise_tv_bregman(torch.FloatTensor(noisy), weight=0.1).numpy()
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5),
                        sharex=True, sharey=True)
@@ -28,9 +33,8 @@ fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5),
 ax[0].imshow(noisy)
 ax[0].axis('off')
 ax[0].set_title('Noisy')
-ax[1].imshow(denoise_tv_bregman(torch.Tensor(noisy), weight=0.1).numpy())
+ax[1].imshow(denoised)
 ax[1].axis('off')
 ax[1].set_title('Denoised')
-
 
 plt.show()
