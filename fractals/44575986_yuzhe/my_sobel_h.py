108ba7793068257196d6aa65a83f0def3f7ef8fb
揭昱哲 <jackie2021ars@gmail.com>
2019-10-18 04:20:21 +1000

    sobel horizontal transform filter

--- fractals/44575986_yuzhe/my_sobel_h.py
+++ fractals/44575986_yuzhe/my_sobel_h.py
@@ -1,18 +1,12 @@
 import tensorflow as tf
-import numpy as np
 
-
-def tf_sobel_h(image, mask=None):
+def tf_sobel_h(image):
 
     """Find the vertical edges of an image using the Sobel transform.
     Parameters
     ----------
     image : 2-D array
-        Image to process.
-    mask : 2-D array, optional
-        An optional mask to limit the application to a certain area.
-        Note that pixels surrounding masked regions are also masked to
-        prevent masked regions from affecting the result.
+        Image to process
     Returns
     -------
     output : 2-D array
@@ -27,24 +21,18 @@ def tf_sobel_h(image, mask=None):
     image = tf.Variable(image, dtype=tf.float64)
     if len(image.get_shape().as_list()) != 2:
         raise ValueError("the image dimension is not 2")
-
+    # expand both image and weight dimension to 4d
     image = tf.expand_dims(image, 0)
     image = tf.expand_dims(image, 3)
-    HSOBEL_WEIGHTS = tf.Variable([[1, 2, 1],
+    HSOBEL_WEIGHTS = tf.Variable([[-1, -2, -1],
                                    [0, 0, 0],
-                                    [-1, -2, -1]], dtype=tf.float64)
+                                    [1, 2, 1]], dtype=tf.float64)
     HSOBEL_WEIGHTS = tf.divide(HSOBEL_WEIGHTS, 4)
     HSOBEL_WEIGHTS = tf.expand_dims(HSOBEL_WEIGHTS, 2)
     HSOBEL_WEIGHTS = tf.expand_dims(HSOBEL_WEIGHTS, 3)
-
+    # apply the convolution
     result = tf.nn.conv2d(image, HSOBEL_WEIGHTS, 1, "SAME")
-
-    if mask is None:
-        result = result[0, :].assign(0)
-        result = result[-1, :].assign(0)
-        result = result[:, 0].assign(0)
-        result = result[:, -1].assign(0)
-        return result
+    return result[0, :, :, 0]
 
 
-#hshshsh
+
