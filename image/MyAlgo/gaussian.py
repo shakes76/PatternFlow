6522a011d3bc2071b5b85af5832a5f9fad8b6c9c
Kajajs <kajajs@stud.ntnu.no>
2019-10-17 11:55:12 +1000

    Fixed creating kernel, todo: sum matrix

--- image/MyAlgo/gaussian.py
+++ image/MyAlgo/gaussian.py
@@ -15,27 +15,36 @@ def make_gaussian_kernel(mean, std):
     std = float(std)
     dist = tf.distributions.Normal(mean, std)
     
-    #testing values
+    # testing values
     start_pt = -10
     stop_pt = abs(start_pt) +1
     leng = abs(start_pt) + stop_pt
     value_range = tf.linspace(float(start_pt), float(stop_pt-1), leng, name="linspace")
     
+    # print("value_range  : ", value_range)
+    # find values by probability density function
+    values = dist.prob(value_range)
+    
+    
+    
+    # make it 2D -> 2D[i,j] = values[i]*values[j]
+    # Outer product
+    # syntax: einsum('i,j->ij', u, v)  # output[i,j] = u[i]*v[j]
+    matrix = tf.einsum('i,j -> ij', values, values )
+    
+    summm = 0
+    #normalize
+    for i in matrix:
+        if i < 3:
+            print("Inside for loop, i: ", i)
+    # Printing
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     value_range = sess.run(value_range)
-    
-    #print("value_range  : ", value_range)
-    
-    values = dist.prob(value_range)
-    
-    # make it 2D -> 2D[i,j] = values[i]*values[j]
-    # x=tf.tensordot(A_tf, B_tf,axes = [[1], [0]])
-    # x.get_shape()
-    
-    x = tf.tensordot(values, values, axes = [[1], [0]])
-    
+    values= sess.run(values)
+    matrix = sess.run(matrix)
     print(values.shape)
+    print(matrix.shape)
     return
 
 # test run
--- image/MyAlgo/testing.py
+++ image/MyAlgo/testing.py
@@ -7,16 +7,43 @@ Created on Wed Oct 16 09:39:38 2019
 """
 
 import tensorflow as tf
-import numpy as np
+# ----------------------------------------------------------------------
+# EXAMPLE USE KERAS
+#import numpy as np
+#
+#model = tf.keras.models.Sequential()
+#model.add(tf.keras.layers.Dense(1, input_dim = 1))
+#
+#model.compile(loss = 'mean_squared_error', optimizer = 'sgd')
+#x_samples = np.array([-1, 0, 1, 2, 3, 4])
+#y_samples = np.array([-3, -1, 1, 3, 5, 7])
+#
+#model.fit(x_samples, y_samples, epochs = 500)
+#to_predict = np.array([10, 11, 12, 13])
+#
+#print(model.predict(to_predict))
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(1, input_dim = 1))
 
-model.compile(loss = 'mean_squared_error', optimizer = 'sgd')
-x_samples = np.array([-1, 0, 1, 2, 3, 4])
-y_samples = np.array([-3, -1, 1, 3, 5, 7])
+# Both tf.tensordot() and tf.einsum() are syntactic sugar that wrap one 
+# or more invocations of tf.matmul() (although in some special cases
+# tf.einsum() can reduce to the simpler elementwise tf.multiply())
+# ---------------------------------------------------------------------
+#EXAMPLE EINSUM
+u = tf.ones(4, tf.int32)
+w = tf.einsum('i,j->ij', u, u)
+tf.math.reduce_sum(
+    input_tensor,
+    axis=None,
+    keepdims=False,
+    name=None
+)
 
-model.fit(x_samples, y_samples, epochs = 500)
-to_predict = np.array([10, 11, 12, 13])
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-print(model.predict(to_predict))
+u = sess.run(u)
+w = sess.run(w)
+
+print("u.shape: ", u.shape)
+print("w.shape: ", w.shape)
+
