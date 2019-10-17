7172858d8bbee4880b40a629b11f9b9160db0ca8
Sambit Tarai <sambitarai17@gmail.com>
2019-10-17 12:53:45 +0530

    sobel.py is modified and working till now

--- image/sobel.py
+++ image/sobel.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+import numpy as np
 def Sobel(image):
 	"""
 	Edge Detection using Sobel transform.
@@ -19,23 +21,34 @@ def Sobel(image):
 
 
 	"""
-	#Horizontal Sobel kernel
+
+	tf.reset_default_graph()
 
 	#Horizontal Sobel kernel
-	Sobel_h = [[1,2,1], [0,0,0], [-1,-2,-1]]
+	#Sobel_h = np.array([3,3])
+	Sobel_h = [ [1,2,1], [0,0,0], [-1,-2,-1] ]
 	#Vertical Sobel kernel
-	Sobel_v = [[1,0,-1], [2,0,-2], [1,0,-1]]
+	#Sobel_v = np.array([3,3])
+	Sobel_v = [ [1,0,-1], [2,0,-2], [1,0,-1] ]
 
-	input_placeholder = tf.input_placeholder(dtype=tf.float32, shape=(1, image.shape[0], image.shape[1], 1))
+	input_placeholder = tf.placeholder(dtype=tf.float32, shape=(1, image.shape[0], image.shape[1], 1))
 
-	kernel_h = tf.constant(Sobel_h, dtype=tf.float32, shape=(3,3,1,1))
-	kernel_v = tf.constant(Sobel_v, dtype=tf.float32, shape=(3,3,1,1))
-	output_h = tf.nn.conv2d(input=input_placeholder, filter=kernel_h, strides=[1,1,1,1], padding="SAME")
-	output_v = tf.nn.conv2d(input=input_placeholder, filter=kernel_v, strides=[1,1,1,1], padding="SAME")
-
+	with tf.name_scope('Convolution'):
+		kernel_h = tf.constant(Sobel_h, dtype=tf.float32, shape=(3,3,1,1))
+		kernel_v = tf.constant(Sobel_v, dtype=tf.float32, shape=(3,3,1,1))
+		output_h = tf.nn.conv2d(input=input_placeholder, filter=kernel_h, strides=[1,1,1,1], padding="SAME")
+		output_v = tf.nn.conv2d(input=input_placeholder, filter=kernel_v, strides=[1,1,1,1], padding="SAME")  
+    
 	with tf.Session() as sess:
 		h_edges = sess.run(output_h, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
 		v_edges = sess.run(output_v, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
 
+	#plt.imshow(h_edges[0,:,:,0], cmap="hot")
+	#plt.show()
+	#plt.imshow(v_edges[0,:,:,0], cmap="hot")
+	#plt.show()
 
-	return h_edges, v_edges
+
+	resultant = ((h_edges**2) + (v_edges**2))**0.5
+	#plt.imshow(resultant[0,:,:,0], cmap="hot")
+	return resultant
