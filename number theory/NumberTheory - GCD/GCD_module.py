2b76bf6385fd6f9fbb4891aae7a5ecb072e90038
Unknown <s175195@student.dtu.dk>
2019-10-17 03:52:00 +0200

    Implemented Euclidean algorithm as module

---
+++ number theory/NumberTheory - GCD/GCD_module.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+
+def cond(a, b):
+    # b is not used here 
+    # They are only parsed because of structure of tf.while_loop
+    
+    return tf.greater(a, tf.constant(0))
+    
+def body(a, b):
+    b = tf.mod(b, a)
+    temp = a
+    a = b
+    b = temp
+    
+    return a, b
+
+def gcd(a, b):
+    '''
+    Return the Greatest Common Divisior of two interger values (a and b)
+    '''
+    # Only whole numbers 
+    a = abs(a)
+    b = abs(b)
+    
+    ### Initialize tensors ###
+    a = tf.Variable(a)
+    b = tf.Variable(b)
+    
+    # While a
+    loop = tf.while_loop(cond, body, [a, b])  
+        
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        result = sess.run(loop)
+        gcd = result[1]
+        
+        return gcd
