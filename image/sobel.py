b94bb1e7fd99eda02451d5ab7d3e0becac5ab216
Sambit Tarai <sambitarai17@gmail.com>
2019-10-17 12:08:02 +0530

    Basic Sobel.py function is ready

--- image/sobel.py
+++ image/sobel.py
@@ -20,5 +20,22 @@ def Sobel(image):
 
 	"""
 	#Horizontal Sobel kernel
-	
-	return 0
+
+	#Horizontal Sobel kernel
+	Sobel_h = np.array([1,2,1], [0,0,0], [-1,-2,-1])
+	#Vertical Sobel kernel
+	Sobel_v = np.array([1,0,-1], [2,0,-2], [1,0,-1])
+
+	input_placeholder = tf.input_placeholder(dtype=tf.float32, shape=(1, image.shape[0], image.shape[1], 1))
+
+	kernel_h = tf.constant(Sobel_h, dtype=tf.float32, shape=(3,3,1,1))
+	kernel_v = tf.constant(Sobel_v, dtype=tf.float32, shape=(3,3,1,1))
+	output_h = tf.nn.conv2d(input=input_placeholder, filter=kernel_h, strides=[1,1,1,1], padding="SAME")
+	output_v = tf.nn.conv2d(input=input_placeholder, filter=kernel_v, strides=[1,1,1,1], padding="SAME")
+
+	with tf.Session() as sess:
+		h_edges = sess.run(output_h, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
+		v_edges = sess.run(output_v, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
+
+
+	return h_edges, v_edges
