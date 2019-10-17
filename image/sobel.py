b117c23860d24ef114abed2ed337ac7ce7da546f
Sambit Tarai <sambitarai17@gmail.com>
2019-10-17 13:56:41 +0530

    All the scripts and functions are updated

--- image/sobel.py
+++ image/sobel.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+
 def Sobel(image):
 	"""
 	Edge Detection using Sobel transform.
@@ -12,7 +13,7 @@ def Sobel(image):
 	Returns:
 	--------
 
-	resultant : 2-D array (The edge map of the image)
+	Returns a 4D array of shape (1, img_height, img_width, 1) (The edge map of the image)
 
 	Notes:
 	------
@@ -25,17 +26,18 @@ def Sobel(image):
 	tf.reset_default_graph()
 
 	#Horizontal Sobel kernel
-	#Sobel_h = np.array([3,3])
 	Sobel_h = [ [1,2,1], [0,0,0], [-1,-2,-1] ]
 	#Vertical Sobel kernel
-	#Sobel_v = np.array([3,3])
 	Sobel_v = [ [1,0,-1], [2,0,-2], [1,0,-1] ]
 
 	input_placeholder = tf.placeholder(dtype=tf.float32, shape=(1, image.shape[0], image.shape[1], 1))
 
 	with tf.name_scope('Convolution'):
+		#Creating 4D kernels for convolution
 		kernel_h = tf.constant(Sobel_h, dtype=tf.float32, shape=(3,3,1,1))
 		kernel_v = tf.constant(Sobel_v, dtype=tf.float32, shape=(3,3,1,1))
+
+		#Computes 2D convolution given 4D input image and 4D kernels
 		output_h = tf.nn.conv2d(input=input_placeholder, filter=kernel_h, strides=[1,1,1,1], padding="SAME")
 		output_v = tf.nn.conv2d(input=input_placeholder, filter=kernel_v, strides=[1,1,1,1], padding="SAME")  
     
@@ -43,12 +45,6 @@ def Sobel(image):
 		h_edges = sess.run(output_h, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
 		v_edges = sess.run(output_v, feed_dict={input_placeholder: image[np.newaxis,:,:,np.newaxis]})
 
-	#plt.imshow(h_edges[0,:,:,0], cmap="hot")
-	#plt.show()
-	#plt.imshow(v_edges[0,:,:,0], cmap="hot")
-	#plt.show()
-
-
+	#Sobel transform to find the resultant edge map of the image
 	resultant = ((h_edges**2) + (v_edges**2))**0.5
-	#plt.imshow(resultant[0,:,:,0], cmap="hot")
 	return resultant
--- image/test_script.py
+++ image/test_script.py
@@ -1,7 +1,12 @@
 #Test Script
+
+#Import Necessary Libraries
+import numpy as np
+import tensorflow as tf
 import sobel 
 import skimage.io
 import matplotlib.pyplot as plt
+
 
 image = skimage.io.imread('G:/IIT_MADRAS_DD/Semesters/7th sem (UQ)/COMP3710 (Pattern Recognition and Analysis)/Lab/Lab_3/PatternFlow/image/test_images/katy.jpg', as_grey=True)
 result = sobel.Sobel(image)
