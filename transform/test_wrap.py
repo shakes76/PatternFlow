0dc09cb006cb1c30751c597356171ee9cc0b2264
Boi Tran Van <tranvanboi321@gmail.com>
2019-10-17 22:42:57 +1000

    add debug plot for comparison

--- transform/test_wrap.py
+++ transform/test_wrap.py
@@ -27,6 +27,27 @@ def test_warp_coords_example():
     warped_image = map_coordinates(image, coords)
     assert_almost_equal(tf_warped_image , warped_image)
     
+    _debug_plot(warped_image, tf_warped_image)
     
 test_stackcopy()
 test_warp_coords_example()
+
+def _debug_plot(original, result, sinogram=None):
+    from matplotlib import pyplot as plt
+    imkwargs = dict(cmap='gray', interpolation='nearest')
+    if sinogram is None:
+        plt.figure(figsize=(15, 6))
+        sp = 130
+    else:
+        plt.figure(figsize=(11, 11))
+        sp = 221
+        plt.subplot(sp + 0)
+        plt.imshow(sinogram, aspect='auto', **imkwargs)
+    plt.subplot(sp + 1)
+    plt.imshow(original, **imkwargs)
+    plt.subplot(sp + 2)
+    plt.imshow(result, vmin=original.min(), vmax=original.max(), **imkwargs)
+    plt.subplot(sp + 3)
+    plt.imshow(result - original, **imkwargs)
+    plt.colorbar()
+    plt.show()
