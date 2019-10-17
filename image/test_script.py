bd510a33e651579e6df99346867263b594809692
Sambit Tarai <sambitarai17@gmail.com>
2019-10-17 14:06:31 +0530

    test_script is ready to use

--- image/test_script.py
+++ image/test_script.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 image = skimage.io.imread('G:/IIT_MADRAS_DD/Semesters/7th sem (UQ)/COMP3710 (Pattern Recognition and Analysis)/Lab/Lab_3/PatternFlow/image/test_images/katy.jpg', as_grey=True)
 result = sobel.Sobel(image)
 
-plt.imshow(result[0,:,:,0], cmap="hot")
+plt.imshow(result[0,:,:,0], cmap="gray")
 plt.show()
-plt.imshow(image)
+plt.imshow(image, cmap="hot")
 plt.show()
