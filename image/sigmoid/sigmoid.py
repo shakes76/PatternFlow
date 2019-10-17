756ce29f5e0e8568a3c79307dad17197da8c345f
Lynn <s4601142@student.uq.edu.au>
2019-10-17 17:57:48 +1000

    modify some details in algorithm.

--- image/sigmoid/sigmoid.py
+++ image/sigmoid/sigmoid.py
@@ -5,14 +5,14 @@ def sigmoid(input_img, cutoff=0.5, gain=10, inv=False):
     # Transform the input_img to a tensor
     input_tensor = tf.constant(input_img, tf.float32)
 
-    normalize = input_tensor / 256
+    normalize = input_tensor / 255
     #calculate (equation : O = 1/(1 + exp*(gain*(cutoff - I))))
     if inv:
       sigmoid = 1.0 - 1.0 / (1.0 + tf.math.exp(gain * (cutoff - input_tensor)))
     else:
       sigmoid = 1.0 / (1.0 + tf.math.exp(gain * (cutoff - input_tensor)))
       
-    re_normalize = sigmoid * 256
+    re_normalize = sigmoid * 255
 
     init = tf.compat.v1.global_variables_initializer()
     with tf.Session() as sess:
