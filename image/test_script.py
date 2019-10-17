2b6a7c33062945b4a96d766332a479c0b3772aba
Sambit Tarai <44292855+sambittarai@users.noreply.github.com>
2019-10-17 09:42:48 +0530

    Update test_script.py

--- image/test_script.py
+++ image/test_script.py
@@ -8,10 +8,13 @@ import skimage.io
 import matplotlib.pyplot as plt
 
 
-image = skimage.io.imread('G:/IIT_MADRAS_DD/Semesters/7th sem (UQ)/COMP3710 (Pattern Recognition and Analysis)/Lab/Lab_3/PatternFlow/image/test_images/katy.jpg', as_grey=True)
+image = skimage.io.imread('test_images/katy.jpg', as_grey=True)
+#Calling the Sobel function to do edge detection
 result = sobel.Sobel(image)
 
+#Plotting the edge map of the image
 plt.imshow(result[0,:,:,0], cmap="gray")
 plt.show()
+#Plotting the actual image
 plt.imshow(image, cmap="hot")
 plt.show()
