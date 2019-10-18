78316769cbbd322337954a4b3b31ce9fcbdb6a1a
craigdeng <craigdeng@qq.com>
2019-10-18 18:10:49 +1000

    Rewrite the situation when data type is not integer
    1. Rewrite the statement when data type is not integer
    
    2.  Source is image; source is dtype
    
    3.  Rewrite the hist_range to  [0,256] when source is image, since the picture is normally define as 256 rgb
    
    4. Source range should be image or dtype otherwise raise error
    
    5. Using dtype_limit as function (will write in next commit)

--- image/histogram/histogram.py
+++ image/histogram/histogram.py
@@ -42,19 +42,22 @@ def histogram(image, nbins=256, source_range='image', normalize=False):
         warnings.warn("This might be a color image. The histogram will be "
              "computed on the flattened image. You can instead "
              "apply this function to each color channel.")
-    image = image.flatten()
+        
+    image = image.flatten()#flattern the image into 1 dimentional vector
     
     # For integer types, histogramming with bincount is more efficient.
     if image.dtype == 'int':
         hist, bin_centers = _bincount_histogram(image, source_range)
         
     else:
+        
         if source_range == 'image':
-            hist_range = None
+            hist_range = [0.0, 256.0]
         elif source_range == 'dtype':
             hist_range = dtype_limits(image, clip_negative=False)
         else:
             ValueError('Wrong value for the `source_range` argument')
+            
         hist, bin_edges = np.histogram(image, bins=nbins, range=hist_range)
         bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2.
 
