834df17d2e18c7663375177296fce2620e21d85b
craigdeng <craigdeng@qq.com>
2019-10-18 15:40:18 +1000

    import tensorflow, change shape function
    Import the tensorflow as tf

--- image/histogram/histogram.py
+++ image/histogram/histogram.py
@@ -1,42 +1,7 @@
+import tnesorflow as tf
+
 def histogram(image, nbins=256, source_range='image', normalize=False):
-    """Return histogram of image.
-    Unlike `numpy.histogram`, this function returns the centers of bins and
-    does not rebin integer arrays. For integer arrays, each integer value has
-    its own bin, which improves speed and intensity-resolution.
-    The histogram is computed on the flattened image: for color images, the
-    function should be used separately on each channel to obtain a histogram
-    for each color channel.
-    Parameters
-    ----------
-    image : array
-        Input image.
-    nbins : int, optional
-        Number of bins used to calculate histogram. This value is ignored for
-        integer arrays.
-    source_range : string, optional
-        'image' (default) determines the range from the input image.
-        'dtype' determines the range from the expected range of the images
-        of that data type.
-    normalize : bool, optional
-        If True, normalize the histogram by the sum of its values.
-    Returns
-    -------
-    hist : array
-        The values of the histogram.
-    bin_centers : array
-        The values at the center of the bins.
-    See Also
-    --------
-    cumulative_distribution
-    Examples
-    --------
-    >>> from skimage import data, exposure, img_as_float
-    >>> image = img_as_float(data.camera())
-    >>> np.histogram(image, bins=2)
-    (array([107432, 154712]), array([ 0. ,  0.5,  1. ]))
-    >>> exposure.histogram(image, nbins=2)
-    (array([107432, 154712]), array([ 0.25,  0.75]))
-    """
+   
     sh = image.shape
     if len(sh) == 3 and sh[-1] < 4:
         warn("This might be a color image. The histogram will be "
