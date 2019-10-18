88524ad0b38595e41d5a75045b6694b532a8b051
44334969 <56623926+44334969@users.noreply.github.com>
2019-10-18 22:22:34 +1000

    Ported the Contrast_Sigmoid function to tensorflow
    Fully implemented the Contrast_Sigmoid function (functionally equivalent to the adjust_sigmoid function from Skikit-Image's exposure module) in TensorFlow.

--- image/ElaineSchenk/Contrast_Corrector_Source.py
+++ image/ElaineSchenk/Contrast_Corrector_Source.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
     
-def Contrast_Sigmoid(array,cutoff=0.5,gain=10,inv=false):
+def Contrast_Sigmoid(array,cutoff=0.5,gain=10.,inv=False):
     #convert the numpy array to tensor 
     tensor = tf.convert_to_tensor(array)
     tf.assert_non_negative(tensor) #ensure tensor values are all non negative
-    tensor_dtype = tensor_img.dtype #save type so can return same type as input
-    
-    #First -> define scale
-    scale = tf.reduce_max(tensor) - tf.reduce_min(tensor) #scale to scale image by (to 0->1)
+    tensor_dtype = tensor.dtype #save type so can return same type as input
+    tensor = tf.cast(tensor, dtype=tf.float32)
+
+    #First -> define scale, sclae is a 1x1 tensor
+    scale = tf.cast(tf.reduce_max(tensor) - tf.reduce_min(tensor), dtype=tf.float32) #scale to scale image by (to 0->1)
     #Second -> Apply sigmoid function (checking for inverse (inv) parameter)
     if inv:
-        out = (1 - 1 / (1 + np.exp(gain * (cutoff - image / scale)))) * scale
+        out = (1 - 1 / (1. + tf.exp(gain * (cutoff - tensor / scale)))) * scale
     else:
-        out = (1 / (1 + np.exp(gain * (cutoff - image / scale)))) * scale
-    #out = #fixing return type
+        print(gain, cutoff, tensor, scale.eval())
+        out = tf.divide(1, tf.cast(1 + tf.exp(gain * (cutoff - tf.divide(tensor, scale))),dtype=tf.float32 )) * scale
+    out = tf.cast(out, dtype=tensor_dtype) #fixing return type
     return out
     
    
