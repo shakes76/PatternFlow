58d75bab93af453a77f3c83bb1c9e14b19304400
tshang <stryt2@gmail.com>
2019-10-18 18:57:42 +1000

    Added iradon to validate the implementation.

--- transform/s4371869_radon_transform/main.py
+++ transform/s4371869_radon_transform/main.py
@@ -27,24 +27,24 @@ def main():
     ax1.set_title("Original")
     ax1.imshow(image, cmap=plt.cm.Greys_r)
 
-    theta = np.linspace(0., 180., max(image.shape) * 1.5, endpoint=False)
+    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
     tic = time.time()
     sinogram = radon(image, theta=theta, circle=True)
     toc = time.time()
-    ax2.set_title("Scikit Radon transform\n(t={0:.3f})".format(toc - tic))
+    ax2.set_title("Scikit Radon transform\n(t={0:.3f}s)".format(toc - tic))
     ax2.set_xlabel("Projection angle (deg)")
     ax2.set_ylabel("Projection position (pixels)")
     ax2.imshow(sinogram, cmap=plt.cm.Greys_r,
             extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
 
     pr = cProfile.Profile()
-    # pr.enable()
-    # tic = time.time()
+    pr.enable()
+    tic = time.time()
     my_sinogram = my_radon(image, theta=theta, circle=True)
     toc = time.time()
-    # pr.disable()
-    # pr.print_stats(sort='cumtime')
-    ax3.set_title("My Radon transform\n(t={0:.3f})".format(toc - tic))
+    pr.disable()
+    pr.print_stats(sort='cumtime')
+    ax3.set_title("My Radon transform\n(t={0:.3f}s)".format(toc - tic))
     ax3.set_xlabel("Projection angle (deg)")
     ax3.set_ylabel("Projection position (pixels)")
     ax3.imshow(my_sinogram, cmap=plt.cm.Greys_r,
@@ -54,16 +54,24 @@ def main():
     plt.show()
 
     reconstruction_fbp = iradon(sinogram, theta=theta, circle=True)
+    my_reconstruction_fbp = iradon(my_sinogram, theta=theta, circle=True)
+
     error = reconstruction_fbp - image
-    print(f"FBP rms reconstruction error: {np.sqrt(np.mean(error**2)):.3g}")
+    my_error = my_reconstruction_fbp - image
 
     imkwargs = dict(vmin=-0.2, vmax=0.2)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5),
+    fig, axes = plt.subplots(2, 2, figsize=(8, 4.5),
                                 sharex=True, sharey=True)
-    ax1.set_title("Reconstruction\nFiltered back projection")
-    ax1.imshow(reconstruction_fbp, cmap=plt.cm.Greys_r)
-    ax2.set_title("Reconstruction error\nFiltered back projection")
-    ax2.imshow(reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
+    axes[0, 0].set_title("Reconstruction on\nScikit sinogram")
+    axes[0, 0].imshow(reconstruction_fbp, cmap=plt.cm.Greys_r)
+    axes[0, 1].set_title(f"Reconstruction error\n(error={np.sqrt(np.mean(error**2)):.3g})")
+    axes[0, 1].imshow(reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
+
+    axes[1, 0].set_title("Reconstruction on\nmy sinogram")
+    axes[1, 0].imshow(my_reconstruction_fbp, cmap=plt.cm.Greys_r)
+    axes[1, 1].set_title(f"Reconstruction error\n(error={np.sqrt(np.mean(my_error**2)):.3g})")
+    axes[1, 1].imshow(my_reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
+
     plt.show()
 
 
