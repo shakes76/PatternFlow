13c741398f6c2d2a439d61d892fbc08078f66750
E-Hern Lee <ehern.lee@gmail.com>
2019-10-18 12:52:47 +1000

    fixing bugs

--- image/radon/radon.py
+++ image/radon/radon.py
@@ -6,30 +6,36 @@ def radon(image, theta=None, circle=True, *, preserve_range=None):
     Calculates the radon transform of an image given specified
     projection angles.
     """
+    if len(image.shape) > 2:
+        image = tf.image.rgb_to_grayscale(image)[:, :, 0]
+    
+    if theta is None:
+        theta = tf.range(180)
+    
     if circle:
-        shape_min = min(image.shape)
-        radius = shape_min // 2
+        shape_min = tf.math.reduce_min(image.shape)
+        radius = tf.math.floordiv(shape_min, 2)
         img_shape = image.shape
-        coords = tf.meshgrid(range(0, image.shape[0]), range(0, image.shape[1]))
-        dist = tf.math.reduce_sum((coords - img_shape // 2) ** 2, 0)
+        x, y = tf.meshgrid(range(0, image.shape[0]), range(0, image.shape[1]))
+        center_x, center_y = tf.math.floordiv(img_shape, 2)
+        dist = (x - center_x) ** 2 + (y - center_y) ** 2
         outside_reconstruction_circle = dist > radius ** 2
-        if tf.any(image[outside_reconstruction_circle]):
+        if tf.math.reduce_any(image[outside_reconstruction_circle] > 0):
             warn('Radon transform: image must be zero outside the '
                  'reconstruction circle')
         # Crop image to make it square
         slices = tuple(
-            slice(int(tf.math.ceil(excess / 2)),
-            (
-                int(tf.math.ceil(excess / 2) + shape_min))
-                if excess > 0 else
-                slice(None)
-            ) for excess in (img_shape - shape_min)
+            slice(int(tf.math.ceil(excess / 2).numpy()),
+                int(tf.math.ceil(excess / 2) + shape_min).numpy())
+            if excess > 0 else
+            slice(None)
+            for excess in (img_shape - shape_min)
         )
         padded_image = image[slices]
     else:
         diagonal = tf.math.sqrt(2) * max(image.shape)
         pad = [int(tf.math.ceil(diagonal - s)) for s in image.shape]
-        new_center = [(s + p) // 2 for s, p in zip(image.shape, pad)]
+        new_center = [tf.math.floordiv(s + p, 2) for s, p in zip(image.shape, pad)]
         old_center = [s // 2 for s in image.shape]
         pad_before = [nc - oc for oc, nc in zip(old_center, new_center)]
         pad_width = [(pb, p - pb) for pb, p in zip(pad_before, pad)]
@@ -38,10 +44,10 @@ def radon(image, theta=None, circle=True, *, preserve_range=None):
     # padded_image is always square
     if padded_image.shape[0] != padded_image.shape[1]:
         raise ValueError('padded_image must be a square')
-    center = padded_image.shape[0] // 2
-    radon_image = np.zeros((padded_image.shape[0], len(theta)))
+    center = tf.cast(tf.math.floordiv(padded_image.shape[0], 2), tf.float64)
+    radon_image = tf.zeros((padded_image.shape[0], len(theta)))
 
-    for i, angle in enumerate(theta * math.pi / 180):
+    for i, angle in enumerate(tf.cast(theta, tf.float64) * math.pi / 180):
         cos_a, sin_a = tf.math.cos(angle), tf.math.sin(angle)
         R = tf.convert_to_tensor([
             [[cos_a, sin_a, -center * (cos_a + sin_a - 1)],
@@ -49,6 +55,14 @@ def radon(image, theta=None, circle=True, *, preserve_range=None):
             [0, 0, 1]]
         ])
         # TODO: warp. we don't need full warp though, just this specific case
-        rotated = warp(padded_image, R, clip=False)
+        # rotated = warp(padded_image, R, clip=False)
+        rotated = tf.zeros_like(padded_image)
+        width, height = padded_image.shape
+        for x in range(width):
+            for y in range(height):
+                multiplied = tf.linalg.matmul(R, tf.convert_to_tensor([[x], [y], [1]], tf.float64))
+                x2, y2 = multiplied[0][0][0], multiplied[0][1][0]
+                print(x2, y2, x, y)
+                rotated[x2, y2] += padded_image[x, y]
         radon_image[:, i] = tf.math.reduce_sum(rotated, 0)
     return radon_image
