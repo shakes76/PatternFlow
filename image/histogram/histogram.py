9c95154240ccd0c9e1199bbf06ac85f5bf1664be
craigdeng <craigdeng@qq.com>
2019-10-18 16:02:20 +1000

    Rewrite function bitcount
    1. rewrite the if statement checking the datatype of image
    2. rewrite the bitfunction with tensorflow
    
    major changes:
    1. datatype change to tensorflow
    2. changing the numpy fucntion: arange bincount to tensorflow function: range , math.bincount

--- image/histogram/histogram.py
+++ image/histogram/histogram.py
@@ -1,17 +1,44 @@
-import tnesorflow as tf
+import tensorflow as tf
+import warnings
+
+def _bincount_histogram(image, source_range):
+    
+    if source_range not in ['image']:
+        raise ValueError('Incorrect value for `source_range` argument: {}'.format(source_range))
+    
+    if source_range == 'image':
+        image_min = int(image.min().astype(tf.int64))
+        image_max = int(image.max().astype(tf.int64))
+        
+    elif source_range == 'dtype':
+        image_min, image_max = dtype_limits(image, clip_negative=False)
+        
+    image, offset = _offset_array(image, image_min, image_max)
+    minlength=image_max - image_min + 1
+    hist = tf.math.bincount(image.ravel(), minlength)
+
+    bin_centers = tf.range(image_min,minlength)
+    
+    if source_range == 'image':
+        idx = max(image_min, 0)
+        hist = hist[idx:]
+        
+    return hist, bin_centers
 
 def histogram(image, nbins=256, source_range='image', normalize=False):
    
-    sh = image.shape
+    sh = tf.shape(image)
+    
     if len(sh) == 3 and sh[-1] < 4:
-        warn("This might be a color image. The histogram will be "
+        warnings.warn("This might be a color image. The histogram will be "
              "computed on the flattened image. You can instead "
              "apply this function to each color channel.")
-
     image = image.flatten()
+    
     # For integer types, histogramming with bincount is more efficient.
-    if np.issubdtype(image.dtype, np.integer):
+    if image.dtype == 'int':
         hist, bin_centers = _bincount_histogram(image, source_range)
+        
     else:
         if source_range == 'image':
             hist_range = None
