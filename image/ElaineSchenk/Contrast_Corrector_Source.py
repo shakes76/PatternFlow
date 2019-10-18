808a6af99192a400010a706a42e7b9362d29a708
44334969 <56623926+44334969@users.noreply.github.com>
2019-10-18 22:42:57 +1000

    Updated and Fixed Contrast_Sigmoid
    There was a bug with the inverse function implementation. 
    This has now been ported to TensorFlow. 
    The function is fully functional and in line with the Skikit-Image version. 
    Note: Perhaps future commits will consider optimisation in the TensorFlow implementation.

--- image/ElaineSchenk/Contrast_Corrector_Source.py
+++ image/ElaineSchenk/Contrast_Corrector_Source.py
@@ -1,24 +1,27 @@
 import tensorflow as tf
     
 def Contrast_Sigmoid(array,cutoff=0.5,gain=10.,inv=False):
-    #convert the numpy array to tensor 
+    
+    #First -> convert the numpy array to tensor 
     tensor = tf.convert_to_tensor(array)
+    
     tf.assert_non_negative(tensor) #ensure tensor values are all non negative
+    
     tensor_dtype = tensor.dtype #save type so can return same type as input
-    tensor = tf.cast(tensor, dtype=tf.float32)
-
-    #First -> define scale, sclae is a 1x1 tensor
+    
+    tensor = tf.cast(tensor, dtype=tf.float32) #its really frustrating to do this but otherwise divison by integer if array is an int
+    #Second-> define scale, scale is a 1x1 tensor
+    
     scale = tf.cast(tf.reduce_max(tensor) - tf.reduce_min(tensor), dtype=tf.float32) #scale to scale image by (to 0->1)
-    #Second -> Apply sigmoid function (checking for inverse (inv) parameter)
+    #Third -> Apply sigmoid function (checking for inverse (inv) parameter)
+    
     if inv:
-        out = (1 - 1 / (1. + tf.exp(gain * (cutoff - tensor / scale)))) * scale
+        out = (1 - tf.divide(1, tf.cast(1 + tf.exp(gain * (cutoff - tf.divide(tensor,scale))), dtype=tf.float32))) * scale
     else:
-        print(gain, cutoff, tensor, scale.eval())
+        #print(gain, cutoff, tensor, scale.eval()) #having issues with data types
         out = tf.divide(1, tf.cast(1 + tf.exp(gain * (cutoff - tf.divide(tensor, scale))),dtype=tf.float32 )) * scale
+    
     out = tf.cast(out, dtype=tensor_dtype) #fixing return type
+    
     return out
-    
-   
-    
-    # 
     
