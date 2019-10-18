da97df054a6bbcc127192e2ac1e90f017b6de774
YaoLaplace <1258165121@qq.com>
2019-10-18 15:08:01 +1000

    Update main.py to a function

--- transform/main.py
+++ transform/main.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
-
+import match_histograms as hist
+import tensorflow as tf
 from skimage import data
 from skimage import exposure
 from skimage.exposure import match_histograms
 
-reference = data.coffee()
-image = data.chelsea()
+def cumulative(image, reference):
+    image = tf.convert_to_tensor(image)
+    reference = tf.convert_to_tensor(reference)
 
-matched = match_histograms(image, reference, multichannel=True)
-
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
+    matched = hist.match_histograms(image, reference, multichannel=True)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3),
                                     sharex=True, sharey=True)
-for aa in (ax1, ax2, ax3):
-    aa.set_axis_off()
+    for aa in (ax1, ax2, ax3):
+        aa.set_axis_off()
 
-ax1.imshow(image)
-ax1.set_title('Source')
-ax2.imshow(reference)
-ax2.set_title('Reference')
-ax3.imshow(matched)
-ax3.set_title('Matched')
+    ax1.imshow(image)
+    ax1.set_title('Source')
+    ax2.imshow(reference)
+    ax2.set_title('Reference')
+    ax3.imshow(matched)
+    ax3.set_title('Matched')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
