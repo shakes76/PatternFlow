f8faed02500d63bb4bc15df0ab22ec1ed1ba609d
xiaoxuansaliha <xiaoxuanbusiness@gmail.com>
2019-10-20 23:13:17 +1000

    finished reconstructing the folder

--- transform/RadonXiaoxuanWang/radon_transform.py
+++ transform/RadonXiaoxuanWang/radon_transform.py
@@ -6,6 +6,44 @@ from skimage.transform import _warps as wp
 from skimage._shared.utils import convert_to_float
 
 def radon(image, theta=None):
+    """
+    Calculates the radon transform of an image given specified
+    projection angles.
+    Parameters
+    ----------
+    image : array_like
+        Input image. The rotation axis will be located in the pixel with
+        indices ``(image.shape[0] // 2, image.shape[1] // 2)``.
+    theta : array_like, optional
+        Projection angles (in degrees). If `None`, the value is set to
+        np.arange(180).
+    circle : boolean, optional
+        Assume image is zero outside the inscribed circle, making the
+        width of each projection (the first dimension of the sinogram)
+        equal to ``min(image.shape)``.
+    preserve_range : bool, optional
+        Whether to keep the original range of values. Otherwise, the input
+        image is converted according to the conventions of `img_as_float`.
+        Also see https://scikit-image.org/docs/dev/user_guide/data_types.html
+    Returns
+    -------
+    radon_image : ndarray
+        Radon transform (sinogram).  The tomography rotation axis will lie
+        at the pixel index ``radon_image.shape[0] // 2`` along the 0th
+        dimension of ``radon_image``.
+    References
+    ----------
+    .. [1] AC Kak, M Slaney, "Principles of Computerized Tomographic
+           Imaging", IEEE Press 1988.
+    .. [2] B.R. Ramesh, N. Srinivasa, K. Rajgopal, "An Algorithm for Computing
+           the Discrete Radon Transform With Some Applications", Proceedings of
+           the Fourth IEEE Region 10 International Conference, TENCON '89, 1989
+    Notes
+    -----
+    Based on code of Justin K. Romberg
+    (https://www.clear.rice.edu/elec431/projects96/DSP/bpanalysis.html)
+    """
+    
     image = convert_to_float(image, None)
     theta = np.linspace(0., 180., max(image.shape), endpoint=False)
 
