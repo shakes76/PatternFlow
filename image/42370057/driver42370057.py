2486d950fa563bfaa6dbea8cbbba679af9d500cc
iciac <47710784+iciac@users.noreply.github.com>
2019-11-06 11:47:22 +1000

    Update driver42370057.py
    update header and inline comment

--- image/42370057/driver42370057.py
+++ image/42370057/driver42370057.py
@@ -1,11 +1,19 @@
 
 """
-try:
-  # %tensorflow_version only exists in Colab.
-  tensorflow_version 2.x
-except Exception:
-  pass
-"""
+Author: Cameron Gordon, 42370057 
+Licence: GNU General Public Licence v3.0
+File provided free of copyright 
+Date: 6 Nov 2019
+
+Two algorithms from the Scikit Exposure module implemented in Tensorflow.
+
+tf_intensity_range implements the intensity_range function
+tf_rescale_intensity implements the rescale_intensity function
+The algorithms are contained in the algorithm42370057.py file. 
+
+The below code is the driver module. 
+It calls an example image face and performs the tf_rescale_intenstiy function, plotting the output. """
+
 import tensorflow as tf 
 from algorithm42370057 import *
 
@@ -27,7 +35,7 @@ def main():
     plt.imshow(face)
     
     image = face # example image 
-    intensity_test = tf_rescale_intensity(image) 
+    intensity_test = tf_rescale_intensity(image) # rescales using the test image 
     ax = plt.subplot(1,2,2)
     plt.imshow(intensity_test)
     plt.show()
