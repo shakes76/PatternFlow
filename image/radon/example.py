4eeb4f4051efefdf7516d01342f4ca44051100a4
E-Hern Lee <ehern.lee@gmail.com>
2019-11-07 16:23:22 +1000

    Update example.py

--- image/radon/example.py
+++ image/radon/example.py
@@ -1,11 +1,16 @@
+# Author: E-Hern Lee
+# License: CC0
+# Copyright: (c) 2019, E-Hern Lee
+# Created: 18/10/2019
+# Modified: 07/11/2019
+# Description: Displays canonical example for Radon transform, the Shepp-Logan phantom, and its Radon transform using source and ported algorithms
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import radon
 
 image = tf.image.decode_png(tf.io.read_file('test/phantom.png'))
 result = radon.radon(image)
-#result -= tf.math.reduce_min(result)
-#result /= tf.math.reduce_max(result)
 plt.figure(figsize=(10, 8))
 plt.subplot(1, 3, 1)
 plt.imshow(image)
