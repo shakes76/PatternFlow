033a11b8b06ba2964aa558ce7d98417519efe98c
Theo Duval <education35879871023@gmail.com>
2020-10-20 10:20:34 +1000

    Added training step and loop

--- recognition/44792701/train.py
+++ recognition/44792701/train.py
@@ -22,6 +22,9 @@ TRAIN = glob.glob("./keras_png_slices_data/keras_png_slices_train/*.png")
 VALIDATE = glob.glob("./keras_png_slices_data/keras_png_slices_validate/*.png")
 IMAGE_NAMES = TEST + TRAIN + VALIDATE
 
+# Keep track of the loss function to be used
+BCE = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
 # Clear any previous models
 tf.keras.backend.clear_session()
 
@@ -83,3 +86,106 @@ def generate_samples(generator, epoch):
         plt.axis('off')
 
     plt.savefig("./generated_images/Epoch-{}.png".format(epoch+1))
+
+
+def get_discrim_loss(real_out, fake_out):
+    """
+    Defines the loss function for the discriminator model. Uses binary cross
+    entropy as there are only two classes of images (real vs fake). The loss
+    needs to take into account the results of the discriminator on both the
+    real and fake images.
+    """
+
+    real = BCE(tf.ones_like(real_out), real_out)
+    fake = BCE(tf.zeros_like(fake_out), fake_out)
+
+    return(real + fake)
+
+
+def get_gen_loss(output):
+    """
+    Defines the loss function for the generator model. Also uses binary cross
+    entropy.
+    """
+    return(BCE(tf.ones_like(output), output))
+
+
+# Initialise both models
+generator = Generator()
+discriminator = Discriminator()
+
+# Save checkpoints
+checkpoint = tf.train.Checkpoint(generator=generator,
+                                 discriminator=discriminator,
+                                 gen_opt=generator.optimiser,
+                                 dis_opt=discriminator.optimiser)
+
+
+d_loss = tf.keras.metrics.Mean(name="discrim_loss")
+g_loss = tf.keras.metrics.Mean(name="gen_loss")
+
+
+@tf.function
+def train_step(real_images):
+    """
+    Defines a single step of training for a batch of images. Calculates the
+    loss for both models, applies the optimisers appropriately and records
+    the loss for a historical record.
+    """
+    noise = tf.random.normal([BATCH_SIZE, 100])
+    
+    # Keep two tapes for each model
+    with tf.GradientTape() as gen_tape:
+        with tf.GradientTape() as discrim_tape:
+            generated_images = generator(noise, training=True)
+            
+            decisions_real = discriminator(real_images, training=True)
+            decisions_fake = discriminator(generated_images, training=True)
+            
+            discrim_loss = get_discrim_loss(decisions_real, decisions_fake)
+            gen_loss = get_gen_loss(decisions_fake)
+            
+    # Get the respective gradients from the respective tapes
+    gen_gradients = gen_tape.gradient(gen_loss, 
+                                      generator.trainable_variables)
+
+    discrim_gradients = discrim_tape.gradient(discrim_loss, 
+                                              discriminator.trainable_variables)
+    
+    # Save loss metrics
+    d_loss(discrim_loss)
+    g_loss(gen_loss)
+    
+    # Apply the gradients
+    generator.optimiser.apply_gradients(zip(gen_gradients, 
+                                            generator.trainable_variables))
+
+    discriminator.optimiser.apply_gradients(zip(discrim_gradients, 
+                                                discriminator.trainable_variables))
+    
+    return(discrim_loss, gen_loss)
+    
+# List of ordered pairs with (d_loss, g_loss)
+loss = []
+
+for epoch in range(EPOCHS): 
+        
+    d_loss.reset_states()
+    g_loss.reset_states()
+    
+    start_time = time.time()
+    for i in range(0, len(IMAGE_NAMES), BATCH_SIZE):
+        images = load_images(IMAGE_NAMES[i:i+BATCH_SIZE])
+        (d_loss_step, g_loss_step) = train_step(images)
+            
+    generate_samples(generator, epoch)
+        
+    loss.append((d_loss.result(), g_loss.result()))
+        
+    # Print update messages
+    print("Epoch {} computed in {} seconds".format(epoch+1, time.time()-start_time))
+    print("Discriminator loss: {}".format(d_loss.result()))
+    print("Generator loss: {}".format(g_loss.result()))
+    
+    checkpoint.save(file_prefix="./checkpoints/checkpoint{}".format(epoch+1))
+  
