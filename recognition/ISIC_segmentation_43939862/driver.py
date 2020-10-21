6c1136ad9d3c07c31e83dc7d221b83bce6fe2249
Joseph Menesch <joe.menesch@gmail.com>
2020-10-21 16:52:46 +1000

    Updated pre-processing and started building model

--- recognition/ISIC_segmentation_43939862/driver.py
+++ recognition/ISIC_segmentation_43939862/driver.py
@@ -1,5 +1,8 @@
 import glob
 import tensorflow as tf
+from model import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Load data
 image_files = glob.glob('../../../ISIC2018_Data/ISIC2018_Images/*.jpg')
@@ -8,6 +11,7 @@ label_files = glob.glob('../../../ISIC2018_Data/ISIC2018_Labels/*.png')
 split_ratio = 0.8
 split_size = int(len(image_files)*split_ratio)
 
+#Shuffle before splitting?
 train_ds = tf.data.Dataset.from_tensor_slices((image_files[:split_size], 
                                                label_files[:split_size]))
 test_ds = tf.data.Dataset.from_tensor_slices((image_files[split_size:], 
@@ -20,10 +24,13 @@ test_ds = test_ds.shuffle(len(image_files)-split_size)
 def map_fn(image, label):
     img = tf.io.read_file(image)
     img = tf.io.decode_jpeg(img, channels = 0)
+    img = tf.image.resize(img, (256,256))
     img = tf.cast(img, tf.float32) / 255.0
     
     lbl = tf.io.read_file(label)
     lbl = tf.io.decode_png(lbl, channels = 0)
+    lbl = tf.image.resize(lbl, (256,256))
+    lbl = tf.cast(lbl, tf.uint8)
     lbl = tf.one_hot(lbl, depth = 2, axis = 2)
     lbl = tf.cast(lbl, tf.float32)
     lbl = tf.squeeze(lbl)
@@ -32,8 +39,29 @@ def map_fn(image, label):
 train_ds = train_ds.map(map_fn)
 test_ds = test_ds.map(map_fn)
 
-#Build model
+#Check batching is working
+for image, label in train_ds.take(1):
+    print('Image shape: ', image.numpy().shape)
+    print('Label shape: ', label.numpy().shape)
+    h, w, n_channels = image.numpy().shape
 
+#Print data size info
+print("\nData info:")
+print("height: %d" % h)
+print("width: %d" % w)
+print("channels: %d" % n_channels)
+
+image_batch, label_batch = next(iter(train_ds.batch(1)))
+test_image = np.asarray(image_batch[0][:,:,0])
+test_label = np.asarray(label_batch[0][:,:,0])
+plt.figure(figsize = (20,20))
+plt.subplot(1,2,1); plt.imshow(test_image); plt.title('Full image'); plt.axis('off')
+plt.subplot(1,2,2); plt.imshow(test_label); plt.title('Background mask'); plt.axis('off')
+
+#Build model
+input_layer, output_layer = ImprovedUnet(h, w, n_channels)
+model = tf.keras.Model(inputs = input_layer, outputs = output_layer)
+model.summary()
 
 #Compile model
 
--- recognition/ISIC_segmentation_43939862/model.py
+++ recognition/ISIC_segmentation_43939862/model.py
@@ -1,3 +1,57 @@
-
+import tensorflow as tf
 
 #Improved U-net model
+def ImprovedUnet(h, w, n_channels):
+    input_layer = tf.keras.layers.Input(shape = (h,w,n_channels))
+
+    c1 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same')(input_layer)
+    c1 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c1)
+    b1 = tf.keras.layers.BatchNormalization()(c1)
+    p1 = tf.keras.layers.MaxPooling2D(pool_size = (2,2))(b1)
+    
+    c2 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same')(p1)
+    c2 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c2)
+    b2 = tf.keras.layers.BatchNormalization()(c2)
+    p2 = tf.keras.layers.MaxPooling2D(pool_size = (2,2))(b2)
+    
+    c3 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same')(p2)
+    c3 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same')(c3)
+    b3 = tf.keras.layers.BatchNormalization()(c3)
+    p3 = tf.keras.layers.MaxPooling2D(pool_size = (2,2))(b3)
+    
+    c4 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same')(p3)
+    c4 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same')(c4)
+    b4 = tf.keras.layers.BatchNormalization()(c4)
+    p4 = tf.keras.layers.MaxPooling2D(pool_size = (2,2))(b4)
+    
+    c5 = tf.keras.layers.Conv2D(1024, (3,3), activation = 'relu', padding = 'same')(p4)
+    c5 = tf.keras.layers.Conv2D(1024, (3,3), activation = 'relu', padding = 'same')(c5)
+    b5 = tf.keras.layers.BatchNormalization()(c5)
+    
+    crop1 = tf.keras.layers.Conv2DTranspose(512, (3,3), strides = (2,2), padding = 'same')(b5)
+    conc1 = tf.keras.layers.concatenate([crop1, c4])
+    c6 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same')(conc1)
+    c6 = tf.keras.layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same')(c6)
+    b6 = tf.keras.layers.BatchNormalization()(c6)
+    
+    crop2 = tf.keras.layers.Conv2DTranspose(256, (3,3), strides = (2,2), padding = 'same')(b6)
+    conc2 = tf.keras.layers.concatenate([crop2, c3])
+    c7 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same')(conc2)
+    c7 = tf.keras.layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same')(c7)
+    b7 = tf.keras.layers.BatchNormalization()(c7)
+    
+    crop3 = tf.keras.layers.Conv2DTranspose(128, (3,3), strides = (2,2), padding = 'same')(b7)
+    conc3 = tf.keras.layers.concatenate([crop3, c2])
+    c8 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same')(conc3)
+    c8 = tf.keras.layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same')(c8)
+    b8 = tf.keras.layers.BatchNormalization()(c8)
+    
+    crop4 = tf.keras.layers.Conv2DTranspose(64, (3,3), strides = (2,2), padding = 'same')(b8)
+    conc4 = tf.keras.layers.concatenate([crop4, c1])
+    c9 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same')(conc4)
+    c9 = tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same')(c9)
+    b9 = tf.keras.layers.BatchNormalization()(c9)
+    
+    output_layer = tf.keras.layers.Conv2D(4, (1,1), activation = 'softmax')(b9)
+    
+    return (input_layer, output_layer)
