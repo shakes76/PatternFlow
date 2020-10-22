2866160f288c1b4e0edf6fc76109e0b4127e6ca6
Shekhar S. Chandra <shekhar.chandra@uq.edu.au>
2020-10-22 10:57:10 +1000

    Added layers module and networks to script.

--- recognition/Shakes-InfoVAE/test_infovae_mnist.py
+++ recognition/Shakes-InfoVAE/test_infovae_mnist.py
@@ -5,12 +5,19 @@ MNIST InfoVAE with MMD
 '''
 import tensorflow as tf 
 from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Input, Activation, ReLU, LeakyReLU, Conv2D, Conv2DTranspose, BatchNormalization, Flatten, Dense, Reshape
+from tensorflow.keras.initializers import GlorotNormal
+from tensorflow.keras.models import Model
+import layers 
 
 print('TensorFlow version:', tf.__version__)
 
 #parameters
 epochs = 2 #multiple of 3 + 1
 batch_size = 64
+depth = 32
+kernel = 3
+latent_size = 2
 
 #load data
 print("> Loading images ...")
@@ -27,11 +34,37 @@ x_test = x_test.reshape((len(x_test), 28, 28, 1))
 total_training, xSize, ySize, c = x_train.shape
 print(x_train.shape)
 
-#layers
-
+# Batch and shuffle the data
+train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(total_training).batch(batch_size)
 
 #Build networks
+#discriminator network
+def encoder_network(input_shape, z_dim, name='E'):
+    '''
+    Encodes images into latent space
+    '''
+    input=Input(input_shape, name=name+'input')
+    net=layers.Norm_Conv2D(input, depth, kernel_size=kernel, strides=2, activation=LeakyReLU(alpha=0.1)) #downsample
+    net=layers.Norm_Conv2D(net, depth*2, kernel_size=kernel, strides=2, activation=LeakyReLU(alpha=0.1)) #downsample
+    dense=Flatten()(net)
+    dense=Dense(1024, activation=LeakyReLU(alpha=0.1), kernel_initializer=GlorotNormal())(dense)
+    dense=Dense(128, activation=LeakyReLU(alpha=0.1), kernel_initializer=GlorotNormal())(dense)
+    latent=Dense(z_dim, kernel_initializer=GlorotNormal())(dense)
+    return Model(inputs=input, outputs=latent, name=name)
 
+def decoder_network(input_shape, activation, name='D'):
+    '''
+    Decodes latent space into images
+    '''
+    input=Input(input_shape, name=name+'input')
+    dense=Dense(128, activation=ReLU(), kernel_initializer=GlorotNormal())(input)
+    dense=Dense(1024, activation=ReLU(), kernel_initializer=GlorotNormal())(dense)
+    dense=Dense(7*7*depth*2, activation=ReLU(), kernel_initializer=GlorotNormal())(dense)
+    dense=Reshape((7, 7, depth*2))(dense)
+    net=layers.Norm_Conv2DTranspose(dense, depth*2, kernel_size=kernel, strides=2, activation=ReLU()) #upsample
+    net=layers.Norm_Conv2DTranspose(net, depth, kernel_size=kernel, strides=2, activation=ReLU()) #upsample
+    network=layers.Norm_Conv2D(net, 1, kernel_size=[1,1], strides=1, activation=activation) #RF 7 with stride
+    return Model(inputs=input, outputs=network, name=name)
 
 #Build models
 
