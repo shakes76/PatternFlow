fd9c52dbbec4415fa3b7fe65655d54485f843398
Vincentius Aditya Sundjaja <vasundjaja@gmail.com>
2020-10-23 00:04:55 +1000

    Made progress on the normal unet. Predicted image looks good

--- recognition/ISICs_UNet/model.py
+++ recognition/ISICs_UNet/model.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, Dropout, UpSampl
 
 ## Model for the UNet from https://arxiv.org/abs/1505.04597
 def unet(class_num, input_size=(256,256,3)):
-    INIT_FILTER = 32
+    INIT_FILTER = 16
     hn = 'he_normal'
     dropout = 0.2
 
@@ -17,28 +17,28 @@ def unet(class_num, input_size=(256,256,3)):
     conv2 = Conv2D(INIT_FILTER * 2, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Conv2D(INIT_FILTER * 3, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool2)
-    conv3 = Conv2D(INIT_FILTER * 3, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv3)
+    conv3 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool2)
+    conv3 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool3)
-    conv4 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv4)
+    conv4 = Conv2D(INIT_FILTER * 8, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool3)
+    conv4 = Conv2D(INIT_FILTER * 8, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv4)
     drop4 = Dropout(dropout)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    conv5 = Conv2D(INIT_FILTER * 5, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool4)
-    conv5 = Conv2D(INIT_FILTER * 5, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv5)
+    conv5 = Conv2D(INIT_FILTER * 16, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(pool4)
+    conv5 = Conv2D(INIT_FILTER * 16, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv5)
     drop5 = Dropout(dropout)(conv5)
 
-    up6 = Conv2D(INIT_FILTER * 4, (2,2), activation = 'relu', padding = 'same', kernel_initializer=hn)(UpSampling2D(size = (2,2))(drop5))
+    up6 = Conv2D(INIT_FILTER * 8, (2,2), activation = 'relu', padding = 'same', kernel_initializer=hn)(UpSampling2D(size = (2,2))(drop5))
     concat6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(concat6)
-    conv6 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv6)
+    conv6 = Conv2D(INIT_FILTER * 8, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(concat6)
+    conv6 = Conv2D(INIT_FILTER * 8, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv6)
 
-    up7 = Conv2D(INIT_FILTER * 3, (2,2), activation = 'relu', padding = 'same', kernel_initializer=hn)(UpSampling2D(size = (2,2))(conv6))
+    up7 = Conv2D(INIT_FILTER * 4, (2,2), activation = 'relu', padding = 'same', kernel_initializer=hn)(UpSampling2D(size = (2,2))(conv6))
     concat7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(INIT_FILTER * 3, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(concat7)
-    conv7 = Conv2D(INIT_FILTER * 3, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv7)
+    conv7 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(concat7)
+    conv7 = Conv2D(INIT_FILTER * 4, (3,3), activation = 'relu', padding = 'same', kernel_initializer=hn)(conv7)
 
     up8 = Conv2D(INIT_FILTER * 2, (2,2), activation = 'relu', padding = 'same', kernel_initializer=hn)(UpSampling2D(size = (2,2))(conv7))
     concat8 = concatenate([conv2,up8], axis = 3)
