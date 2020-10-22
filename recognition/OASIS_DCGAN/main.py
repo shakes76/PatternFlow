55c39be4470d60f67cc16797196013d8c545853d
peterngo901 <peter14585@gmail.com>
2020-10-22 19:43:56 +1000

    imported necessary packages and setup barebones pipeline

--- recognition/OASIS_DCGAN/main.py
+++ recognition/OASIS_DCGAN/main.py
@@ -1,7 +1,15 @@
-# Author: Peter Ngo
+'''
+OASIS DCGAN
+
+@author Peter Ngo
+'''
+import tensorflow as tf
+
 
 if __name__=="main":
     # Model Parameters
     no_filts = 32
     batch_size = 25
     epochs = 15
+
+    
