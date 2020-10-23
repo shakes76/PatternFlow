84cad0c29f633c2d1016c9c8002a563139c690b6
Anna LYU <annalyu@AnnadeMacBook-Pro.local>
2020-10-23 17:05:54 +1000

    commit again

--- recognition/lyubinapple/test.py
+++ recognition/lyubinapple/test.py
@@ -6,9 +6,4 @@
     Python Version: 4.7.4
 '''
 import tensorflow as tf
-import matplotlib.pyplot as plt
-# filters number, kernel_size(F), strides(S)
-# conv: ((W-F+2P)/S)+1. Pooling:((W-F)/S)+1
-a = (10, 64, 64, 3)
-x = tf.random.normal(a)
-print(x)
+print(tf.__version__)
