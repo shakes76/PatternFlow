f4b95c026b61e2b739ee7508afca633bfc8c2136
Erik Brand <s4432349@labs.eait.uq.edu.au>
2020-10-23 12:30:58 +1000

    Converted to 128x128 working well

--- recognition/KneeGAN/Driver.py
+++ recognition/KneeGAN/Driver.py
@@ -5,11 +5,11 @@ from matplotlib import pyplot
 batch_size = 128
 learning_rate_discriminator = 0.0002
 learning_rate_generator = 0.0002
-generator_input_dim = 8
+generator_input_dim = 16
 latent_dim = 256
 epochs = 30
 data_path = 'D:\\comp3710\\AKOA_Analysis\\'
 output_path = 'Resources/'
 
 
-gen_history, disc_history = train(data_path, output_path)
+gen_history, disc_history = train(data_path, output_path, generator_input_dim=generator_input_dim)
