8d8ff79a65936c57e68c2bf9df72b8c4068553e1
Zachary Oar <z.oar@uqconnect.edu.au>
2020-10-24 10:25:14 +1000

    Added a keras Sequence to generate images as batches for training, added onehot encoding, added a sanity check

--- recognition/45314669_Zachary_Oar_Problem5/model.py
+++ recognition/45314669_Zachary_Oar_Problem5/model.py
@@ -12,6 +12,38 @@ import math
 def get_filenames_from_dir(directory):
     return [f for f in listdir(directory) if isfile(join(directory, f))]
 
+
+def encode_y(y):
+    y = np.where(y < 0.5, 0, y)
+    y = np.where(y > 0.5, 1, y)
+
+    y = keras.utils.to_categorical(y, num_classes=2)
+    return y
+
+
+class SequenceGenerator(keras.utils.Sequence):
+    def __init__(self, x, y, batchsize):
+        self.x, self.y, self.batchsize = x, y, batchsize
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batchsize)
+
+    def __getitem__(self, idx):
+        x_names = self.x[idx * self.batchsize:(idx + 1) * self.batchsize]
+        y_names = self.y[idx * self.batchsize:(idx + 1) * self.batchsize]
+        
+        # open x image names, resize, normalise and make a numpy array
+        batch_x = np.array([np.asarray(Image.open("ISIC2018_Task1-2_Training_Input_x2/" 
+                + file_name).resize((256, 192))) for file_name in x_names]) / 255.0
+
+        # open y image names, resize, normalise, encode to one-hot and make a numpy array
+        batch_y = np.array([np.asarray(Image.open("ISIC2018_Task1_Training_GroundTruth_x2/" 
+                + file_name).resize((256, 192))) for file_name in y_names]) / 255.0
+        batch_y = encode_y(batch_y)
+
+        return batch_x, batch_y
+
+
 x_names = get_filenames_from_dir("ISIC2018_Task1-2_Training_Input_x2")
 y_names = get_filenames_from_dir("ISIC2018_Task1_Training_GroundTruth_x2")
 
@@ -21,6 +53,17 @@ x_train_val, x_test, y_train_val, y_test = train_test_split(x_names, y_names, te
 # 17% of the non-test images are set aside as the validation set
 x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size=0.17, random_state=42)
 
-print(len(x_train))
-print(len(x_test))
-print(len(x_val))
+train_gen = SequenceGenerator(x_train, y_train, 9)
+sanity_check_x, sanity_check_y = train_gen.__getitem__(0)
+
+# show some of the images as a sanity check
+plt.figure(figsize=(10, 10))
+for i in (0, 2, 4):
+    plt.subplot(3, 2, i + 1)
+    plt.imshow(sanity_check_x[i])
+    plt.axis('off')
+
+    plt.subplot(3, 2, i + 2)
+    plt.imshow(tf.argmax(sanity_check_y[i], axis=2))
+    plt.axis('off')
+plt.show()
