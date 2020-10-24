50ef6507a6f848d69fbf94dadd0561bcc5aebc47
Huizhen Tan <62126838+Huizhen-Tan@users.noreply.github.com>
2020-10-24 14:05:59 +1000

    Initial testing commit. print tf version.

---
+++ recognition/s4519808_q4/test.py
@@ -0,0 +1,29 @@
+"""
+COMP3710 Report 
+
+@author Huizhen 
+"""
+
+import tensorflow as tf
+
+print("TensorFlow Version: ", tf.__version__) 
+
+# parameters
+
+
+# layers
+
+
+# build networks
+
+
+# build model
+
+
+# trian
+
+
+# test
+
+
+# plot
