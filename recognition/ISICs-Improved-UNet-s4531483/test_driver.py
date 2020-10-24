e113a128ad794944978c0c45af80bb34f1b214a7
Matthew Costello <mcost45@hotmail.com>
2020-10-25 00:20:41 +1000

    Improved data generators to check on lab pc

--- recognition/ISICs-Improved-UNet-s4531483/test_driver.py
+++ recognition/ISICs-Improved-UNet-s4531483/test_driver.py
@@ -8,48 +8,38 @@ PATH_SEG_DATA = "data/mask"
 # IMAGE_WIDTH = 32
 SEED = 45
 BATCH_SIZE = 32
-
-
-image_data_generator = keras.ImageDataGenerator(
+DATA_GEN_ARGS = dict(
     rescale=1.0/255,
     shear_range=0.1,
     zoom_range=0.1,
     horizontal_flip=True,
     fill_mode='nearest',
     validation_split=0.2)
+TEST_TRAIN_GEN_ARGS = dict(
+    seed=SEED,
+    class_mode=None,
+    batch_size=BATCH_SIZE)
 
-mask_data_generator = keras.ImageDataGenerator(
-    rescale=1.0/255,
-    shear_range=0.1,
-    zoom_range=0.1,
-    horizontal_flip=True,
-    fill_mode='nearest',
-    validation_split=0.2)
+image_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
+mask_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
 
 image_train_gen = image_data_generator.flow_from_directory(
     PATH_ORIGINAL_DATA,
-    seed=SEED,
-    class_mode=None,
-    subset='training',
-    batch_size=BATCH_SIZE)
+    **TEST_TRAIN_GEN_ARGS,
+    subset='training')
 
 image_test_gen = image_data_generator.flow_from_directory(
     PATH_ORIGINAL_DATA,
-    seed=SEED,
-    class_mode=None,
-    subset='validation',
-    batch_size=BATCH_SIZE)
+    **TEST_TRAIN_GEN_ARGS,
+    subset='validation')
 
-mask_train_gen = image_data_generator.flow_from_directory(
+mask_train_gen = mask_data_generator.flow_from_directory(
     PATH_SEG_DATA,
-    seed=SEED,
-    class_mode=None,
-    subset='training',
-    batch_size=BATCH_SIZE)
+    **TEST_TRAIN_GEN_ARGS,
+    subset='training')
 
-mask_test_gen = image_data_generator.flow_from_directory(
+
+mask_test_gen = mask_data_generator.flow_from_directory(
     PATH_SEG_DATA,
-    seed=SEED,
-    class_mode=None,
-    subset='validation',
-    batch_size=BATCH_SIZE)
+    **TEST_TRAIN_GEN_ARGS,
+    subset='validation')
