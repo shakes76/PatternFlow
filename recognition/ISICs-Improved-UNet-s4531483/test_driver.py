69ecf7a4ad519324e5b334dbdb01d57a5a129ae8
Matthew Costello <mcost45@hotmail.com>
2020-10-25 00:42:49 +1000

    Added keras sequence to join multiple generators together allowing for multithreading

--- recognition/ISICs-Improved-UNet-s4531483/test_driver.py
+++ recognition/ISICs-Improved-UNet-s4531483/test_driver.py
@@ -20,26 +20,41 @@ TEST_TRAIN_GEN_ARGS = dict(
     class_mode=None,
     batch_size=BATCH_SIZE)
 
-image_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
-mask_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
 
-image_train_gen = image_data_generator.flow_from_directory(
-    PATH_ORIGINAL_DATA,
-    **TEST_TRAIN_GEN_ARGS,
-    subset='training')
+class ImageMaskSequence(keras.utils.Sequence):
+    def __init__(self, image_gen, mask_gen):
+        self.generators = [image_gen, mask_gen]
 
-image_test_gen = image_data_generator.flow_from_directory(
-    PATH_ORIGINAL_DATA,
-    **TEST_TRAIN_GEN_ARGS,
-    subset='validation')
+    def __len__(self):
+        return len(self.generators[1])
 
-mask_train_gen = mask_data_generator.flow_from_directory(
-    PATH_SEG_DATA,
-    **TEST_TRAIN_GEN_ARGS,
-    subset='training')
+    def __getitem__(self, item):
+        return [generator[item] for generator in self.generators]
 
 
-mask_test_gen = mask_data_generator.flow_from_directory(
-    PATH_SEG_DATA,
-    **TEST_TRAIN_GEN_ARGS,
-    subset='validation')
+if __name__ == "__main__":
+    image_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
+    mask_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_GEN_ARGS)
+
+    image_train_gen = image_data_generator.flow_from_directory(
+        PATH_ORIGINAL_DATA,
+        **TEST_TRAIN_GEN_ARGS,
+        subset='training')
+
+    image_test_gen = image_data_generator.flow_from_directory(
+        PATH_ORIGINAL_DATA,
+        **TEST_TRAIN_GEN_ARGS,
+        subset='validation')
+
+    mask_train_gen = mask_data_generator.flow_from_directory(
+        PATH_SEG_DATA,
+        **TEST_TRAIN_GEN_ARGS,
+        subset='training')
+
+    mask_test_gen = mask_data_generator.flow_from_directory(
+        PATH_SEG_DATA,
+        **TEST_TRAIN_GEN_ARGS,
+        subset='validation')
+
+    train_gen = ImageMaskSequence(image_train_gen, mask_train_gen)
+    test_gen = ImageMaskSequence(image_test_gen, mask_test_gen)
