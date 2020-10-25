4a0553acad55078227c07cd530e15975f91cb7d0
Matthew Costello <mcost45@hotmail.com>
2020-10-25 12:16:22 +1000

    Fixed layers_model return

--- recognition/ISICs-Improved-UNet-s4531483/layers_model.py
+++ recognition/ISICs-Improved-UNet-s4531483/layers_model.py
@@ -8,5 +8,6 @@ def improvedUNet(width, height, channels, classes=2):
     x2 = keras.layers.Activation("relu")(x1)
     x3 = keras.layers.Flatten()(x2)
     x4 = keras.layers.Dense(classes)(x3)
-    out = keras.layers.Activation("softmax")(x4)
-    return out
+    output = keras.layers.Activation("softmax")(x4)
+    unet = keras.Model(inputs=[input], outputs=[output])
+    return unet
