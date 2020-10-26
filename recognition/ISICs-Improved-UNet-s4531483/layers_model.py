982f3efae62e0417b3930b5817de0d4c66148877
Matthew Costello <mcost45@hotmail.com>
2020-10-26 13:36:39 +1000

    Modularize models and test_driver code

--- recognition/ISICs-Improved-UNet-s4531483/layers_model.py
+++ recognition/ISICs-Improved-UNet-s4531483/layers_model.py
@@ -10,67 +10,69 @@ CONV_PROPERTIES = dict(
     bias_regularizer=l2(L2_WEIGHT_DECAY),
     padding="same")
 
-def improvedUNet(width, height, channels):
+
+def improved_unet(width, height, channels):
     input = keras.Input(shape=(width, height, channels))
 
     x1 = keras.layers.Conv2D(16, (3, 3), **CONV_PROPERTIES, input_shape=(width, height, channels))(input)
     x2 = keras.layers.BatchNormalization()(x1)
     x3 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x2)
-    x4 = contextModule(x3, 16)
+    x4 = context_module(x3, 16)
     x5 = keras.layers.Add()([x3, x4])
 
     x6 = keras.layers.Conv2D(32, (3, 3), **CONV_PROPERTIES, strides=2)(x5)
     x7 = keras.layers.BatchNormalization()(x6)
     x8 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x7)
-    x9 = contextModule(x8, 32)
+    x9 = context_module(x8, 32)
     x10 = keras.layers.Add()([x8, x9])
 
     x11 = keras.layers.Conv2D(64, (3, 3), **CONV_PROPERTIES, strides=2)(x10)
     x12 = keras.layers.BatchNormalization()(x11)
     x13 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x12)
-    x14 = contextModule(x13, 64)
+    x14 = context_module(x13, 64)
     x15 = keras.layers.Add()([x13, x14])
 
     x16 = keras.layers.Conv2D(128, (3, 3), **CONV_PROPERTIES, strides=2)(x15)
     x17 = keras.layers.BatchNormalization()(x16)
     x18 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x17)
-    x19 = contextModule(x18, 128)
+    x19 = context_module(x18, 128)
     x20 = keras.layers.Add()([x18, x19])
 
     x21 = keras.layers.Conv2D(256, (3, 3), **CONV_PROPERTIES, strides=2)(x20)
     x22 = keras.layers.BatchNormalization()(x21)
     x23 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x22)
-    x24 = contextModule(x23, 256)
+    x24 = context_module(x23, 256)
     x25 = keras.layers.Add()([x23, x24])
-    x26 = upsamplingModule(x25, 128)
+    x26 = upsampling_module(x25, 128)
 
     x27 = keras.layers.Concatenate()([x20, x26])
-    x28 = localisationModule(x27, 128)
-    x29 = upsamplingModule(x28, 64)
+    x28 = localisation_module(x27, 128)
+    x29 = upsampling_module(x28, 64)
 
     x30 = keras.layers.Concatenate()([x15, x29])
-    x31 = localisationModule(x30, 64)
-    x32 = upsamplingModule(x31, 32)
+    x31 = localisation_module(x30, 64)
+    x32 = upsampling_module(x31, 32)
 
     x33 = keras.layers.Concatenate()([x10, x32])
-    x34 = localisationModule(x33, 32)
-    x35 = upsamplingModule(x34, 16)
+    x34 = localisation_module(x33, 32)
+    x35 = upsampling_module(x34, 16)
 
     x36 = keras.layers.Concatenate()([x5, x35])
     x37 = keras.layers.Conv2D(32, (3, 3), **CONV_PROPERTIES)(x36)
     x38 = keras.layers.BatchNormalization()(x37)
     x39 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x38)
 
-    u1 = upsamplingModule(x31, 32)
+    u1 = upsampling_module(x31, 32)
     s1 = keras.layers.Add()([u1, x34])
-    u2 = upsamplingModule(s1, 32)
+    u2 = upsampling_module(s1, 32)
     s2 = keras.layers.Add()([u2, x39])
 
     output = keras.layers.Conv2D(1, (1, 1), activation="sigmoid", **CONV_PROPERTIES)(s2)
-    unet = keras.Model(inputs=[input], outputs=[output])
-    return unet
+    u_net = keras.Model(inputs=[input], outputs=[output])
+    return u_net
 
-def contextModule(input, out_filter):
+
+def context_module(input, out_filter):
     x1 = keras.layers.Conv2D(out_filter, (3, 3), **CONV_PROPERTIES)(input)
     x2 = keras.layers.BatchNormalization()(x1)
     x3 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x2)
@@ -80,14 +82,16 @@ def contextModule(input, out_filter):
     x7 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x6)
     return x7
 
-def upsamplingModule(input, out_filter):
+
+def upsampling_module(input, out_filter):
     x1 = keras.layers.UpSampling2D(size=(2, 2))(input)
     x2 = keras.layers.Conv2D(out_filter, (3, 3), **CONV_PROPERTIES)(x1)
     x3 = keras.layers.BatchNormalization()(x2)
     x4 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x3)
     return x4
 
-def localisationModule(input, out_filter):
+
+def localisation_module(input, out_filter):
     x1 = keras.layers.Conv2D(out_filter, (3, 3), **CONV_PROPERTIES)(input)
     x2 = keras.layers.BatchNormalization()(x1)
     x3 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x2)
@@ -95,3 +99,8 @@ def localisationModule(input, out_filter):
     x5 = keras.layers.BatchNormalization()(x4)
     x6 = keras.layers.LeakyReLU(alpha=LEAKY_RELU_ALPHA)(x5)
     return x6
+
+
+if __name__ == "__main__":
+    print("Please run 'test_driver.py, not 'layers_model.py'.'")
+    exit(1)
--- recognition/ISICs-Improved-UNet-s4531483/test_driver.py
+++ recognition/ISICs-Improved-UNet-s4531483/test_driver.py
@@ -18,6 +18,8 @@ EPOCHS = 1
 LEARNING_RATE = 0.0004
 STEPS_PER_EPOCH_TRAIN = math.floor(2076 / BATCH_SIZE)
 STEPS_PER_EPOCH_TEST = math.floor(519 / BATCH_SIZE)
+IMAGE_MODE = "rgb"
+MASK_MODE = "grayscale"
 DATA_TRAIN_GEN_ARGS = dict(
     rescale=1.0/255,
     shear_range=0.15,
@@ -34,10 +36,11 @@ TEST_TRAIN_GEN_ARGS = dict(
     class_mode=None,
     batch_size=BATCH_SIZE,
     interpolation="nearest",
+    subset='training',
     target_size=(IMAGE_HEIGHT, IMAGE_WIDTH))
 
 
-if __name__ == "__main__":
+def pre_process_data():
     train_image_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_TRAIN_GEN_ARGS)
     train_mask_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_TRAIN_GEN_ARGS)
     test_image_data_generator = keras.preprocessing.image.ImageDataGenerator(**DATA_TEST_GEN_ARGS)
@@ -46,34 +49,41 @@ if __name__ == "__main__":
     image_train_gen = train_image_data_generator.flow_from_directory(
         PATH_ORIGINAL_DATA,
         **TEST_TRAIN_GEN_ARGS,
-        subset='training',
-        color_mode='rgb')
+        color_mode=IMAGE_MODE)
 
     image_test_gen = test_image_data_generator.flow_from_directory(
         PATH_ORIGINAL_DATA,
         **TEST_TRAIN_GEN_ARGS,
-        subset='training',
-        color_mode='rgb')
+        color_mode=IMAGE_MODE)
 
     mask_train_gen = train_mask_data_generator.flow_from_directory(
         PATH_SEG_DATA,
         **TEST_TRAIN_GEN_ARGS,
-        subset='training',
-        color_mode='grayscale')
+        color_mode=MASK_MODE)
 
     mask_test_gen = test_mask_data_generator.flow_from_directory(
         PATH_SEG_DATA,
         **TEST_TRAIN_GEN_ARGS,
-        subset='training',
-        color_mode='grayscale')
+        color_mode=MASK_MODE)
 
-    model = layers.improvedUNet(IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS)
+    return zip(image_train_gen, mask_train_gen), zip(image_test_gen, mask_test_gen)
+
+
+def plot_accuracy_loss(track):
+    plt.figure(0)
+    plt.plot(track.history['accuracy'])
+    plt.plot(track.history['loss'])
+    plt.title('Loss & Accuracy Curves')
+    plt.xlabel('Epoch')
+    plt.legend(['Accuracy', 'Loss'])
+    plt.show()
+
+
+def train_model_check_accuracy(train_gen, test_gen):
+    model = layers.improved_unet(IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS)
     model.summary()
     model.compile(optimizer=keras.optimizers.Adam(LEARNING_RATE),
                   loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
-
-    train_gen = zip(image_train_gen, mask_train_gen)
-    test_gen = zip(image_test_gen, mask_test_gen)
 
     track = model.fit(
         train_gen,
@@ -82,10 +92,16 @@ if __name__ == "__main__":
         shuffle=True,
         verbose=1,
     )
+    plot_accuracy_loss(track)
 
-    print("\nTest Accuracy:")
+    print("\nEvaluating test images...")
     test_loss, test_accuracy = model.evaluate(test_gen, steps=STEPS_PER_EPOCH_TEST, verbose=2)
+    print("Test Accuracy: " +  str(test_accuracy))
+    print("Test Loss: " + str(test_loss) + "\n")
+    return model
 
+
+def test_visualise_model_predictions(model, test_gen):
     test_pred = model.predict(test_gen, steps=STEPS_PER_EPOCH_TEST)
 
     mask = keras.preprocessing.image.img_to_array(test_pred[10])
@@ -94,15 +110,16 @@ if __name__ == "__main__":
     mask[ones] = 1
     mask[zeroes] = 0
 
-    plt.figure(0)
+    plt.figure(1)
     plt.imshow(mask, cmap='gray', vmin=0.0, vmax=1.0)
 
-    plt.figure(1)
-    plt.plot(track.history['accuracy'])
-    plt.plot(track.history['loss'])
-    plt.title('Loss & Accuracy Curves')
-    plt.xlabel('Epoch')
-    plt.legend(['Accuracy', 'Loss'])
-    plt.show()
 
+def main():
+    train_gen, test_gen = pre_process_data()
+    model = train_model_check_accuracy(train_gen, test_gen)
+    test_visualise_model_predictions(model, test_gen)
     print("COMPLETED.")
+
+
+if __name__ == "__main__":
+    main()
