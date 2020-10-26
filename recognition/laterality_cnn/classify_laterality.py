acb2049a4c975c93a4a9ea700a6e49f3ebe796c5
deliciousturtle <49872862+deliciousturtle@users.noreply.github.com>
2020-10-27 02:23:08 +1000

    Reverted to previous method for loading and processing data to allow segmentation into 3 partitions (train, val, test)

--- recognition/laterality_cnn/classify_laterality.py
+++ recognition/laterality_cnn/classify_laterality.py
@@ -9,56 +9,80 @@ import glob
 #Specify directory of data
 data_dir = os.path.join("C:", "Users", "delic", ".keras", "datasets", "AKOA_Analysis")
 
-#Split the dataset - 20% validation, 80% training
-batch_size = 32
-img_height = 228
-img_width = 260
+#Load all the filenames
+filenames = glob.glob(os.path.join(data_dir, '*', '*.png'))
+image_count = len(filenames)
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    labels = 'inferred',
-    validation_split=0.2,
-    subset='training',
-    seed=123,
-    image_size = (img_height, img_width),
-    batch_size = batch_size,
-    color_mode = 'grayscale'
-)
+#Split the dataset - 20% validation, 20% test, 60% training
+random.seed(123)
+random.shuffle(filenames)
+val_size = int(image_count * 0.2)
+test_size = int(image_count * 0.4)
+val_images = filenames[:val_size]
+test_images = filenames[val_size:test_size]
+train_images = filenames[test_size:]
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    labels = 'inferred',
-    validation_split=0.2,
-    subset='validation',
-    seed=123,
-    image_size = (img_height, img_width),
-    batch_size = batch_size,
-    color_mode = 'grayscale'
-)
+#Extract labels
+train_labels = [fn.split(os.path.sep)[-2] for fn in train_images]
+val_labels = [fn.split(os.path.sep)[-2] for fn in val_images]
+test_labels = [fn.split(os.path.sep)[-2] for fn in test_images]
 
-class_names = train_ds.class_names
+class_names = sorted(set(train_labels))
 num_classes = len(class_names)
 
+#Create tensorflow datasets
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+val_ds = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+
+#Map filenames and labels to data arrays
+img_height = 228
+img_width = 260
+def map_fn(filename, label):
+    # Load the raw data from the file as a string.
+    img = tf.io.read_file(filename)
+    # Convert the compressed string to a 3D uint8 tensor.
+    img = tf.image.decode_jpeg(img, channels=1) # channels=3 for RGB, channels=1 for grayscale
+    # Resize the image to the desired size.
+    img = tf.image.resize(img, (img_height, img_width))
+    # Standardise values to be in the [0, 1] range.
+    img = tf.cast(img, tf.float32) / 255.0
+    # One-hot encode the label.
+    one_hot = tf.cast(label == class_names, tf.uint8)
+    # Return the processed image and label.
+    return img, one_hot
+
+train_ds = train_ds.map(map_fn)
+val_ds = val_ds.map(map_fn)
+test_ds = test_ds.map(map_fn)
+
 # #Visualise data
+# image_batch, label_batch = next(iter(train_ds.batch(9)))
+
 # plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#   for i in range(9):
-#     ax = plt.subplot(3, 3, i + 1)
-#     plt.imshow(images[i].numpy().astype("uint8")[:,:,0])
-#     plt.title(class_names[labels[i]])
-#     plt.axis("off")
+# for i in range(9):
+#     plt.subplot(3, 3, i+1)
+#     plt.imshow(image_batch[i][:,:,0])
+#     label = tf.argmax(label_batch[i])
+#     plt.title(class_names[label])
+#     plt.axis('off')
 # plt.show()
 
-# for image_batch, labels_batch in train_ds:
-#     print(image_batch.shape)
-#     print(np.min(image_batch[0]), np.max(image_batch[0]))
-#     print(labels_batch.shape)
-#     break
-
-#Configure dataset for performance
+#Configure dataset for performance and shuffle
+batch_size = 32
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+train_ds = train_ds.cache()
+train_ds = train_ds.shuffle(len(train_images))
+train_ds = train_ds.batch(batch_size)
+train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache()
+val_ds = val_ds.shuffle(len(val_images))
+val_ds = val_ds.batch(batch_size)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache()
+test_ds = test_ds.shuffle(len(test_images))
+test_ds = test_ds.batch(batch_size)
+test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
 checkpoint_path = "training/ckpt01.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -73,24 +97,23 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
 )
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.experimental.preprocessing.Rescaling(1./255, input_shape= (img_height, img_width, 1)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 1)),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Dropout(0.20),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-    tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dropout(0.20),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(num_classes, activation='softmax')
 ])
 
 model.summary()
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1E-3),
@@ -102,8 +125,12 @@ model.compile(
 
 results = model.fit(train_ds, epochs=n_epochs, callbacks=[cp_callback], validation_data=val_ds)
 
-model.evaluate(val_ds, verbose=2)
+print("Training set: ")
 model.evaluate(train_ds, verbose=2)
+print("Validation set: ")
+model.evaluate(val_ds, verbose=2)
+print("Test set: ")
+model.evaluate(test_ds, verbose=2)
 
 plt.plot(results.history['accuracy'], label='accuracy')
 plt.plot(results.history['val_accuracy'], label='val_accuracy')
