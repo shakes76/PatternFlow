7e202e844281128770234cda9bf1337f036ecb10
Jiamin Li <lijiamin0725@gmail.com>
2020-10-27 15:37:52 +1000

    add jupyter file

--- recognition/s4511528/main.py
+++ recognition/s4511528/main.py
@@ -41,3 +41,77 @@ for i in range(len(masks)):
     y[i] = masks[i]
         
 y = y[:, :, :, np.newaxis]
+
+
+# Split train:validation:test = 6:2:2 ------->  1556:519:519
+X_train = X[0:1556,:,:,:]
+X_val = X[1556:1556+519,:,:,:]
+X_test = X[1556+519:2594,:,:,:]
+
+y_train = y[0:1556,:,:,:]
+y_val = y[1556:1556+519,:,:,:]
+y_test = y[1556+519:2594,:,:,:]
+
+
+# Define the F1-score
+def dice_coef(y_true, y_pred):
+    smooth = 1e-15
+    y_true = tf.keras.layers.Flatten()(y_true)
+    y_pred = tf.keras.layers.Flatten()(y_pred)
+    y_pred = tf.cast(tf.math.greater(y_pred, 0.5),tf.float32)
+    intersection = tf.reduce_sum(y_true * y_pred)
+    return (2.0 * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1.0 - dice_coef(y_true, y_pred)
+
+# Train the improved Unet model
+if __name__ == "__main__":
+    model = build_model()
+
+    metric = ['acc', dice_coef, Recall(), Precision()]
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=metric)
+    history = model.fit(x = X_train, y = y_train, validation_data=(X_val, y_val), batch_size=16, epochs=30)
+
+
+# Evaluate model
+results = model.evaluate(X_test, y_test, batch_size = 16)
+
+# Prediction
+predictions = model.predict(X_test)
+
+# Show results
+for i in range(len(predictions)):
+    predictions[i][predictions[i] > 0.5] = 1
+    predictions[i][predictions[i] <= 0.5] = 0
+
+# Plot Origional/ Segmentation/ Prediction pictures
+
+n = 10 
+plt.figure(figsize=(30, 10))
+for i in range(1, n+1):
+    ax = plt.subplot(1, n, i)
+    plt.imshow(X_test[i])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.figure(figsize=(30, 10))
+for i in range(1, n+1):
+    ax = plt.subplot(1, n, i)
+    plt.imshow(y_test[i])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.figure(figsize=(30, 10))
+for i in range(1, n+1):
+    ax = plt.subplot(1, n, i)
+    plt.imshow(predictions[i])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+# Calculate average test dice coefficient
+test_dice = dice_coef(y_test, predictions)
+print('Test Dice Coefficient: ', test_dice.numpy())
