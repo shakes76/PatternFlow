7cda17baa316728fadbce6d51675d502ee66a324
Jonathan <jonathangodbold1@gmail.com>
2020-10-27 16:34:36 +1000

    Added data cleanser to separate OASIS dataset into patient ID and patient images.

--- recognition/45339747/generator_script_45339747.py
+++ recognition/45339747/generator_script_45339747.py
@@ -1,7 +1,39 @@
 """
 Laterality classification of the OAI AKOA knee data set.
 
-@author Jonathan Godbold s4533974
+@author Jonathan Godbold, s4533974.
+
+Usage of this file is strictly for The University of Queensland.
+Date: 27/10/2020.
+
+Description:
+Imports the OASIS dataset and cleans the data for the driver script.
 
 """
-print("test")
+
+# Import Libraries.
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import *
+
+# Create two lists, one for the list of all image files, and one for the unique ID's.
+unique_list = []
+subset_path_AKOA = []
+train_path_AKOA = os.path.expanduser("H:/Desktop/AKOA_Analysis/")
+for path in os.listdir(train_path_AKOA):
+    if '.png' in path:
+        # Format the string to extract the ID for the patient.
+        unique_id = path
+        unique_id = unique_id.split("OAI")
+        unique_id = unique_id[1]
+        unique_id = unique_id.split("_")
+        unique_id = unique_id[0]
+        unique_list.append(unique_id)
+        subset_path_AKOA.append(os.path.join(train_path_AKOA, path))
+        
+unique_list = set(unique_list) # Remove any duplicates from the list.
+
+
+
