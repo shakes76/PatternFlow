7033db0a49aab84fe3bfb8e8a7718e237d2e5116
Christopher Bailey <c.bailey1@uqconnect.edu.au>
2020-10-27 12:41:40 +1000

    Stub required files and check repository correctly set up

---
+++ recognition/ISICs_UNet/README.md
@@ -0,0 +1,5 @@
+# Segmenting ISICs with UNet
+
+COMP3710 Report recognition problem 3 (Segmenting ISICs data set with UNet) solved in Tensorflow (TF)
+
+Created by Christopher Bailey (45576430)
---
+++ recognition/ISICs_UNet/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python
+
+"""
+Driver script for ISICs UNet recognition problem.
+
+Created by Christopher Bailey (45576430) for COMP3710 Report.
+"""
+
+
+import tensorflow as tf
+
+
+def main():
+    print(tf.__version__)
+    print("END")
+
+
+if __name__ == "__main__":
+    main()
