1a31d9ff3e53cdbd2ba20aa2b6366a6cd6e175a6
Max Hornigold <mchornigold@gmail.com>
2020-10-28 16:55:20 +1000

    Removed Images, figured out how to import data
    I removed the images from the repository. I also got most of the driver script and model working.

--- recognition/OASIS brain data set with an Improved UNet/Solution.py
+++ recognition/OASIS brain data set with an Improved UNet/Solution.py
@@ -1,54 +1,64 @@
-# import necessary modules
+"""
+Unet Model
+
+@author Max Hornigold
+"""
+
+
 import tensorflow as tf
 from tensorflow.keras import layers
 
 def unet_model(output_channels, f=6):
     """Creates and returns a UNET model"""
     
-    inputs = tf.keras.layers.Input()
+    inputs = tf.keras.layers.Input(shape=(256, 256, 1))
     
-    # Downsampling throughthe model...
-    d1 = layers.Conv2D(f, 3, padding='same', activation='relu')
-    d1 = layers.Conv2D(f, 3, padding='same', activation='relu')
+    # Downsampling through the model
+    d1 = layers.Conv2D(f, 3, padding='same', activation='relu')(inputs)
+    d1 = layers.Conv2D(f, 3, padding='same', activation='relu')(d1)
     
     d2 = layers.MaxPooling2D()(d1)
-    d2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')
-    d2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')
+    d2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')(d2)
+    d2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')(d2)
     
     d3 = layers.MaxPooling2D()(d2)
-    d3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')
-    d3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')
-                                
+    d3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')(d3)
+    d3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')(d3)
+      
     d4 = layers.MaxPooling2D()(d3)
-    d4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')
-    d4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')
-                                
+    d4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')(d4)
+    d4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')(d4)
+                               
     d5 = layers.MaxPooling2D()(d4)
-    d5 = layers.Conv2D(16*f, 3, padding='same', activation='relu')
-    d5 = layers.Conv2D(16*f, 3, padding='same', activation='relu')
+    d5 = layers.Conv2D(16*f, 3, padding='same', activation='relu')(d5)
+    d5 = layers.Conv2D(16*f, 3, padding='same', activation='relu')(d5)
     
     # Upsampling and establishing the skip connections
     u4 = layers.UpSampling2D()(d5)
     u4 = layers.concatenate([u4, d4])
-    u4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')
-    u4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')
+    u4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')(u4)
+    u4 = layers.Conv2D(8*f, 3, padding='same', activation='relu')(u4)
     
     u3 = layers.UpSampling2D()(d4)
     u3 = layers.concatenate([u3, d3])
-    u3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')
-    u3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')
+    u3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')(u3)
+    u3 = layers.Conv2D(4*f, 3, padding='same', activation='relu')(u3)
     
     u2 = layers.UpSampling2D()(d3)
     u2 = layers.concatenate([u2, d2])
-    u2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')
-    u2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')
+    u2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')(u2)
+    u2 = layers.Conv2D(2*f, 3, padding='same', activation='relu')(u2)
     
     u1 = layers.UpSampling2D()(d2)
     u1 = layers.concatenate([u1, d1])
-    u1 = layers.Conv2D(f, 3, padding='same', activation='relu')
-    u1 = layers.Conv2D(f, 3, padding='same', activation='relu')
+    u1 = layers.Conv2D(f, 3, padding='same', activation='relu')(u1)
+    u1 = layers.Conv2D(f, 3, padding='same', activation='relu')(u1)
     
     # This is the last layer of the model.
     outputs = layers.Conv2D(output_channels, 1, activation='softmax')(u1)
     
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+    # Create model using the layers
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    
+    # return the model
+    return model
--- recognition/OASIS brain data set with an Improved UNet/driver_script.py
+++ recognition/OASIS brain data set with an Improved UNet/driver_script.py
@@ -1,26 +1,49 @@
-# This is the test driver script.
-# This script shows example usage of the module created in solution.py
-# It creates relevant plots and visualisations
+"""
+ISIC 2018
 
+@author Max Hornigold
+"""
 
-# include a main method
-# can run the solution
-
-# can use numpy if needed.
-
-# want to show an example of my solution being run.
-
-# import the correct modules
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
+import math
 
 import zipfile
 import glob
 from IPython.display import clear_output
 
 from solution import unet_model
+
+def dice_coefficient(y_true, y_pred, smooth = 0.):
+    
+    # change the dimension to one
+    y_true_f = tf.keras.layers.Flatten(y_true)
+    y_pred_f = tf.keras.layers.Flatten(y_pred)
+    
+    # calculation for the loss function
+    intersection = tf.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.sum(y_true_f) + tf.sum(y_pred_f) + smooth)
+
+
+def dice_coefficient_loss(y_true, y_pred):
+    return 1. - dice_coefficient(y_true, y_pred)
+
+
+def display(display_list):
+    plt.figure(figsize=(10, 6))
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.imshow(display_list[i], cmap='gray')
+        plt.axis('off')
+    plt.show()
+
+
+def plot_data(ds, n):
+    for image, mask in ds.take(n):
+        display([tf.squeeze(image), tf.squeeze(mask)])
+        #display([tf.squeeze(image), tf.argmax(mask, axis=-1)])
 
 
 def decode_png(file_path):
@@ -30,34 +53,47 @@ def decode_png(file_path):
     return png
 
 
+def decode_jpg(file_path):
+    jpg = tf.io.read_file(file_path)
+    jpg = tf.image.decode_jpeg(jpg, channels=1)
+    jpg = tf.image.resize(jpg, (256, 256))
+    return jpg
+
+
 def process_path(image_fp, mask_fp):
-    image = decode_png(image_fp)
-    image = tf.cast(image, tf.float32) / 255.0
     
+    image = decode_jpg(image_fp)
+    image = tf.cast(image, tf.float32) / 255.0
+
     mask = decode_png(mask_fp)
-    mask = mask == [0, 85, 170, 255]
+    #mask = mask == [0, 85, 170, 255]
+    
     return image, mask
 
 
 def import_ISIC_data():
     """ Download the dataset """
-
-    # Download data
-    dataset_url = "https://cloudstor.aarnet.edu.au/sender/download.php?token=505165ed-736e-4fc5-8183-755722949d34&files_ids=10012238"
-    data_path = tf.keras.utils.get_file(origin=dataset_url, fname="ISIC2018_Task1-2_Training_Data.zip")
     
-    # extract the zip file
-    with zipfile.ZipFile(data_path) as masks_zf:
-        masks_zf.extractall()
-    
-    # List files
-    images = sorted(glob.glob("ISIC2018_Task1_Training_Input_x2/*.png"))
-    masks = sorted(glob.glob("ISIC2018_Task1_Training_GroundTruth_x2/*.png"))
+    # Get images and masks
+    images = sorted(glob.glob("C:/Users/Mchor/OneDrive/Desktop/All/Personal/UQ/COMP3710/Laboratory Report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1-2_Training_Input_x2/*.jpg"))
+    masks = sorted(glob.glob("C:/Users/Mchor/OneDrive/Desktop/All/Personal/UQ/COMP3710/Laboratory Report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1_Training_GroundTruth_x2/*.png"))
 
-    # split image and masks into training, validating and testing sets
-    train_images, val_images, test_images = np.split(images, 3)    
-    train_masks, val_masks, test_masks = np.split(masks, 3)
-      
+    # choose number of training, validate and test images to use
+    num_images = len(images)
+    num_training = math.ceil(0.5*num_images)
+    num_val = math.ceil(0.2*num_images)
+    num_test = math.ceil(0.2*num_images)
+    
+    # Split the images into train, validate and test datasets
+    train_images = [images[i] for i in range(0, num_training)]
+    val_images = [images[i] for i in range(num_training, num_training + num_val)]
+    test_images = [images[i] for i in range(num_training + num_val, num_training + num_val + num_test)]
+
+    # Split the masks into train, validate and test datasets
+    train_masks = [masks[i] for i in range(0, num_training)]
+    val_masks = [masks[i] for i in range(num_training, num_training + num_val)]
+    test_masks = [masks[i] for i in range(num_training + num_val, num_training + num_val + num_test)]
+    
     # make dataset from images and masks
     train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_masks))
     val_ds = tf.data.Dataset.from_tensor_slices((val_images, val_masks))
@@ -72,7 +108,8 @@ def import_ISIC_data():
     train_ds = train_ds.map(process_path)
     val_ds = val_ds.map(process_path)
     test_ds = test_ds.map(process_path)
-        
+    
+    # return training, validation and testing datasets
     return train_ds, val_ds, test_ds
     
 
@@ -97,14 +134,6 @@ def import_OASIS_data():
     return train_images, train_masks, val_images, val_masks, test_images, test_masks  
     
     
-def display(display_list):
-    plt.figure(figsize=(10, 10))
-    for i in range(len(display_list)):
-        plt.subplot(1, len(display_list), i+1)
-        plt.imshow(display_list[i], cmap='gray')
-        plt.axis('off')
-    plt.show()
-
 
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     """Helper function to plot a gallery of portraits"""
@@ -120,31 +149,33 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
         plt.yticks(())
 
 
-def show_predictions(model, ds, num=1):
+def make_and_plot_predictions(model, ds, num=1):
     for image, mask in ds.take(num):
+        #pred_mask = model.predict(image[tf.newaxis, ...])
+        #pred_mask = tf.argmax(pred_mask[0], axis=-1)
+        #display([tf.squeeze(image), tf.argmax(mask, axis=1), pred_mask])
+        
+        #predictions = model.predict(test_ds.batch(test_batch_size))
+        #predictions = np.argmax(predictions, axis=1)
+        
         pred_mask = model.predict(image[tf.newaxis, ...])
-        pred_mask = tf.argmax(pred_mask[0], axis=1)
-        display([tf.squeeze(image), tf.argmax(mask, axis=1), pred_mask])
+        display([tf.squeeze(image), tf.squeeze(mask), tf.squeeze(pred_mask)])
 
 
 class DisplayCallback(tf.keras.callbacks.Callback):
     
-    def on_epoch_end(self, epoch, val_ds, logs=None):
+    def on_epoch_end(self, model, epoch, val_ds, logs=None):
         clear_output(wait=True)
-        show_predictions(val_ds)
+        make_and_plot_predictions(model, val_ds, 1)
 
 
 def analyse_training_history(history):
-    
-    # analyse the model
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.ylim([0, 1])
     plt.legend(loc='lower right')
-    
-    return 0
 
 
 def analyse_predictions(predictions, X_test, Y_test, target_names):
@@ -163,52 +194,50 @@ def analyse_predictions(predictions, X_test, Y_test, target_names):
 
     print(classification_report(Y_test, predictions, target_names=target_names))
     
-    return 0
-
 
 def main():
     
     # import the data
     train_ds, val_ds, test_ds = import_ISIC_data()
-    return 0 ##################################################  
-        
+    
     # plot example image
-    for image, mask in train_ds.take(1):
-        display([tf.squeeze(image), tf.argmax(mask, axis=1)])
+    #plot_data(train_ds, 1)
     
     # create the model
     model = unet_model(4, f=4)
     
+    # show a summary of the model
+    #print(model.summary())
+    
     # compile the model
-    model.compile(optimizer='adam', loss='catagorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    #model.compile(optimizer='adam', loss=dice_coefficient_loss, metrics=['categorical_crossentropy'])
     
-    # show predictions
-    show_predictions(val_ds)
+    # specify the batch size to take
+    train_batch_size = 32
+    val_batch_size = 32
+    test_batch_size = 32
     
-    #
-    history = model.fit(train_ds.batch(32), epochs=3, validation_data=val_ds.batch(32), callbacks=[DisplayCallback()])
+    # specify the number of epochs
+    epochs = 2
     
-    #
-    show_predictions(test_ds, 3)
-
-    
+    # train the model using the training (and validating) data
+    #history = model.fit(train_ds.batch(train_batch_size), epochs=epochs, validation_data=val_ds.batch(val_batch_size), callbacks=[DisplayCallback()])
+    history = model.fit(train_ds.batch(train_batch_size), epochs=epochs, validation_data=val_ds.batch(val_batch_size))
     
     # analyse history of training the model
     #analyse_training_history(history)
     
-    # make predictions using the model
-    #predictions = model.predict(X_test)
-    #predictions = np.argmax(predictions, axis=1)
-    
-    # show plots of some of these predictions
-    
+    # make and show some predictions
+    make_and_plot_predictions(model, test_ds, 3)
     
     # numerically analyse the performance model
     #analyse_predictions(predictions, X_test, Y_test, target_names)
-    
-    return 0
-    
-    
+        
+
+
+#from tensorflow.keras.datasets.mnist
+
 
 if __name__ == "__main__":
     main()
