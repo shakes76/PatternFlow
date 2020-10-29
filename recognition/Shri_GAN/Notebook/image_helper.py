00e987b9c87f089bcc38e0d33c873437f30190c9
Shrikant Agrawal <s4562394@labs.eait.uq.edu.au>
2020-10-29 10:46:34 +1000

    Modified Image helper file to incorporate Glob and proper use of TF while image load.

--- recognition/Shri_GAN/Notebook/image_helper.py
+++ recognition/Shri_GAN/Notebook/image_helper.py
@@ -10,13 +10,19 @@ from PIL import Image
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
+import os
 
-
+import matplotlib.pyplot as plt
+import os, time  
+import numpy as np 
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+import tensorflow as tf  
 
 # In[8]:
 
 
-def load_img_from_dir(data_dir, img_format = 'png', imagecount = 0, tensor = True):
+def load_img_from_dir(data_dir, img_size = (128,128), img_format = 'png', imagecount = 0, tensor = True):
     '''
     Helper function to load data from the data directory.
     
@@ -35,8 +41,11 @@ def load_img_from_dir(data_dir, img_format = 'png', imagecount = 0, tensor = Tru
     image_list = []
     if imagecount !=0:
         for file in glob.glob(data_dir+'\*.'+img_format): 
-            im = np.asarray(Image.open(file))
+            im = Image.open(file)
+            im = im.resize(img_size) 
+            im = np.asarray(im)
             im = np.expand_dims(im, axis=2)
+            
             if tensor == True:
                 im = tf.image.convert_image_dtype(
                 im, tf.int64, saturate=False, name=None)
@@ -59,8 +68,35 @@ def load_img_from_dir(data_dir, img_format = 'png', imagecount = 0, tensor = Tru
             
             image_list.append(im)
             
-    return np.asarray(image_list)
+    return image_list
     
+
+def tf_load_img_from_dir(dir_data, img_shape = (128, 128, 1), img_format = 'png', imagecount = 0, tensor = True):
+    '''
+    Helper function to load data from the data directory.
+    
+    Params:
+    
+    data_dir: Folder Path/ Data Directory.
+    imagecount: number of images to be loaded. Default = complete dataset.
+    tensor: format of image. Default Tensor. If False then images will be loaded as numpy ndarray.
+        
+    '''
+    Ntrain = imagecount
+    nm_imgs       = np.sort(os.listdir(dir_data))
+    nm_imgs_train = nm_imgs[:Ntrain]
+    X_train = []
+    for i, myid in enumerate(nm_imgs_train):
+        image = load_img(dir_data + "/" + myid,
+                         target_size=img_shape[:2])
+        im = img_to_array(image)/255.0
+        im = tf.image.convert_image_dtype(im, tf.int64, saturate=False, name=None)
+        im = tf.keras.preprocessing.image.img_to_array(im)
+        image = tf.image.rgb_to_grayscale(im)
+        X_train.append(image)
+    X_train = np.array(X_train)
+    return(X_train)
+   
 
 
 def tf_plot_gallery(images, h, w,titles = 0, n_row=3, n_col=4):
