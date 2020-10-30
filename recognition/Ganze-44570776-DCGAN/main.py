9678e47e840d838e68d12519290287c6faf912c1
Ganze ZHENG <1062168846@qq.com>
2020-10-30 17:39:59 +1000

    Initial commit for testing that git is working properly.

---
+++ recognition/Ganze-44570776-DCGAN/main.py
@@ -0,0 +1,8 @@
+"""
+This is program for COMP3710 Report, resolving problem 6.
+[Create a generative model of the OASIS brain or the OAI AKOA knee data set using a DCGAN that
+has a “reasonably clear image” and a Structured Similarity (SSIM) of over 0.6.]
+"""
+
+if __name__ == "__main__":
+    print("This is a test message.")
