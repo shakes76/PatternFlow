47096cad2bcc111b5dc2b51f6616aacc0e5f2eb6
Lei Wang <lei.wang7@uq.edu.au>
2020-10-30 21:48:18 +1000

    Define the loss function of discriminator

--- recognition/s4548907/model.py
+++ recognition/s4548907/model.py
@@ -62,3 +62,9 @@ print (decision)
 #more parameter of the model
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
+# define the loss function of discriminator
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
