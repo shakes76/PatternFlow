bfc87baf460d159bacdaef458d934d291649b773
Lei Wang <lei.wang7@uq.edu.au>
2020-10-30 22:01:05 +1000

    Define the train step function

--- recognition/s4548907/model.py
+++ recognition/s4548907/model.py
@@ -93,3 +93,87 @@ num_examples_to_generate = 4
 # to visualize progress in the animated GIF)
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
 test_seed = tf.random.normal([32, noise_dim])
+
+# Define Train and Generate_save images
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)  # the result of discriminator
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def generate_and_save_images(model, epoch, test_input, path_save=None):
+  # Notice `training` is set to False.
+  # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4,4))
+
+    for i in range(predictions.shape[0]):
+        ax = plt.subplot(2, 2, i+1)
+        ax.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        ax.axis('off')
+    fig.suptitle("Image in the {:04d}".format(epoch))
+    
+    if path_save is not None:
+        plt.savefig(path_save,
+                    bbox_inches='tight',
+                    pad_inches=0)
+        plt.show()
+    else:
+        plt.show()
+
+        
+def train(dataset, epochs):
+    ssim =[]
+    for epoch in range(epochs):
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch)  #train each batch
+
+        # Produce images for the GIF as we go
+        display.clear_output(wait=True)
+       
+        ## save images 50 epoches
+        if epoch % 50 == 0:
+            path_save = "Result/image_at_epoch_{:04d}.png".format(epoch)
+        else:
+            path_save=None
+            
+        generate_and_save_images(generator,
+                                 epoch + 1,
+                                 seed,
+                                path_save=path_save)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 50 == 0:
+            checkpoint.save(file_prefix = checkpoint_prefix)
+            
+        ssim_epoch = get_ssim(generator)
+        ssim.append(ssim_epoch)
+        
+        print ('Time for epoch {} is {} sec, ssim is {}'.format(epoch + 1, time.time()-start, ssim_epoch))
+        
+        ssim_epoch = get_ssim(generator)
+        ssim.append(ssim_epoch)
+        
+
+  # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,epochs,seed)
+    return ssim
+    
+
