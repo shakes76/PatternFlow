38e1ab6cecfde511daf1a62810abed36bc88f0b8
Lei Wang <lei.wang7@uq.edu.au>
2020-10-30 21:56:02 +1000

    reorder the file

--- recognition/s4548907/model.py
+++ recognition/s4548907/model.py
@@ -62,12 +62,13 @@ print (decision)
 #more parameter of the model
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
+# define the loss function of generator
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
 # define the loss function of discriminator
 def discriminator_loss(real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
-# define the loss function of generator
-def generator_loss(fake_output):
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
