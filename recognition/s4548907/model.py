617813ba9dfd9e7b744787dd50de710e11104cf8
Lei Wang <lei.wang7@uq.edu.au>
2020-10-30 21:42:13 +1000

    First build the generator model

--- recognition/s4548907/model.py
+++ recognition/s4548907/model.py
@@ -4,3 +4,26 @@ Main function of the DCGAN:
     Discriminator
     trian setp (generate images and save, compute ssim)
 """
+
+# Build generator model
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(8*8*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Reshape((8, 8, 256)))
+
+    model.add(layers.Conv2DTranspose(128, (2,2), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    # in DCGAN the activation function is tanh
+    model.add(layers.Conv2DTranspose(1, (2, 2), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 64, 64, 1)
+    
+    return model
