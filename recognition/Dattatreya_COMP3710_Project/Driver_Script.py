442ef1d7ad7bfb1023a9a1f6eda0b43c25c371fe
Dattatreya Majumdar <s4558632@labs.eait.uq.edu.au>
2020-10-31 14:12:37 +1000

    Updated Driver Script

--- recognition/Dattatreya_COMP3710_Project/Driver_Script.py
+++ recognition/Dattatreya_COMP3710_Project/Driver_Script.py
@@ -19,7 +19,7 @@ from itertools import chain
 from skimage.io import imread, imshow, concatenate_images
 from skimage.transform import resize
 from skimage.morphology import label
-from sklearn.model_selection import train_test_split
+
 
 import tensorflow as tf
 
