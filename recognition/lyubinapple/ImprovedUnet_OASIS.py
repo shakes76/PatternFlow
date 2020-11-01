0ba81b69ae8db2be50ad4653872d5d0b56b36068
Bin Lyu <lyubinapple@gmail.com>
2020-11-01 14:40:47 +1000

    Develop the improved Unet model and calculate dice similarity coefficient

--- recognition/lyubinapple/ImprovedUnet_OASIS.py
+++ recognition/lyubinapple/ImprovedUnet_OASIS.py
@@ -12,6 +12,9 @@ from PIL import Image
 from matplotlib import pyplot
 import glob
 import matplotlib.pyplot as plt
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+from IPython.display import clear_output
 
 train_images = sorted(glob.glob("/Users/annalyu/Desktop/Data_Science/2020.2/COMP3710/Ass/Ass3Data/keras_png_slices_data/keras_png_slices_train/*.png"))
 train_masks = sorted(glob.glob("/Users/annalyu/Desktop/Data_Science/2020.2/COMP3710/Ass/Ass3Data/keras_png_slices_data/keras_png_slices_seg_train/*.png"))
@@ -58,3 +61,153 @@ def display(display_list):
 
 for image, mask in train_ds.take(1):
     display([tf.squeeze(image), tf.argmax(mask, axis=-1)])
+
+def unet_model(output_channels, f=64):
+    inputs = tf.keras.layers.Input(shape=(256,256,1))
+
+    d1_conv = tf.keras.layers.Conv2D(f, 3, padding='same')(inputs)
+    d1_conv = tf.keras.layers.LeakyReLU(0.01)(d1_conv)
+    d1 = tf.keras.layers.Conv2D(f, 3, padding='same')(d1_conv)
+    d1 = tf.keras.layers.LeakyReLU(0.01)(d1)
+    d1 = tf.keras.layers.Dropout(0.3)(d1)
+    d1 = tf.keras.layers.Conv2D(f, 3, padding='same')(d1)
+    d1 = tf.keras.layers.LeakyReLU(0.01)(d1)
+    d1 = tf.keras.layers.add([d1_conv, d1])
+    #d1 = d1_conv + d1
+
+    d2_conv = tf.keras.layers.Conv2D(2*f, 3, 2, padding='same')(d1)
+    d2_conv = tf.keras.layers.LeakyReLU(0.01)(d2_conv)
+    d2 = tf.keras.layers.Conv2D(2*f, 3, padding='same')(d2_conv)
+    d2 = tf.keras.layers.LeakyReLU(0.01)(d2)
+    d2 = tf.keras.layers.Dropout(0.3)(d2)
+    d2 = tf.keras.layers.Conv2D(2*f, 3, padding='same')(d2)
+    d2 = tf.keras.layers.LeakyReLU(0.01)(d2)
+    d2 = tf.keras.layers.add([d2_conv, d2])
+    
+    d3_conv = tf.keras.layers.Conv2D(4*f, 3, 2, padding='same')(d2)
+    d3_conv = tf.keras.layers.LeakyReLU(0.01)(d3_conv)
+    d3 = tf.keras.layers.Conv2D(4*f, 3, padding='same')(d3_conv)
+    d3 = tf.keras.layers.LeakyReLU(0.01)(d3)
+    d3 = tf.keras.layers.Dropout(0.3)(d3)
+    d3 = tf.keras.layers.Conv2D(4*f, 3, padding='same')(d3)
+    d3 = tf.keras.layers.LeakyReLU(0.01)(d3)
+    d3 = tf.keras.layers.add([d3_conv, d3])
+    
+    d4_conv = tf.keras.layers.Conv2D(8*f, 3, 2, padding='same')(d3)
+    d4_conv = tf.keras.layers.LeakyReLU(0.01)(d4_conv)
+    d4 = tf.keras.layers.Conv2D(8*f, 3, padding='same')(d4_conv)
+    d4 = tf.keras.layers.LeakyReLU(0.01)(d4)
+    d4 = tf.keras.layers.Dropout(0.3)(d4)
+    d4 = tf.keras.layers.Conv2D(8*f, 3, padding='same')(d4)
+    d4 = tf.keras.layers.LeakyReLU(0.01)(d4)
+    d4 = tf.keras.layers.add([d4_conv, d4])
+    
+    d5_conv = tf.keras.layers.Conv2D(16*f, 3, 2, padding='same')(d4)
+    d5_conv = tf.keras.layers.LeakyReLU(0.01)(d5_conv)
+    d5 = tf.keras.layers.Conv2D(16*f, 3, padding='same')(d5_conv)
+    d5 = tf.keras.layers.LeakyReLU(0.01)(d5)
+    d5 = tf.keras.layers.Dropout(0.3)(d5)
+    d5 = tf.keras.layers.Conv2D(16*f, 3, padding='same')(d5)
+    d5 = tf.keras.layers.LeakyReLU(0.01)(d5)
+    d5 = tf.keras.layers.add([d5_conv, d5])
+
+    #upsampling-u4
+    u4 = tf.keras.layers.UpSampling2D()(d5)
+    u4 = tf.keras.layers.Conv2D(8*f, 3, padding='same')(u4)
+    u4 = tf.keras.layers.LeakyReLU(0.01)(u4)
+    #concatenate-u4
+    u4 = tf.keras.layers.concatenate([u4, d4])
+    #localization-u4
+    u4 = tf.keras.layers.Conv2D(8*f, 3, padding='same')(u4)
+    u4 = tf.keras.layers.LeakyReLU(0.01)(u4)
+    u4 = tf.keras.layers.Conv2D(8*f, 1, padding='same')(u4)
+    u4 = tf.keras.layers.LeakyReLU(0.01)(u4)
+
+    #upsampling-u3
+    u3 = tf.keras.layers.UpSampling2D()(u4)
+    u3 = tf.keras.layers.Conv2D(4*f, 3, padding='same')(u3)
+    u3 = tf.keras.layers.LeakyReLU(0.01)(u3)
+    #concatenate-u3
+    u3 = tf.keras.layers.concatenate([u3, d3])
+    #localization-u3
+    u3 = tf.keras.layers.Conv2D(4*f, 3, padding='same')(u3)
+    u3 = tf.keras.layers.LeakyReLU(0.01)(u3)
+    u3 = tf.keras.layers.Conv2D(4*f, 1, padding='same')(u3)
+    u3 = tf.keras.layers.LeakyReLU(0.01)(u3)
+    #segmentation-u3
+    print("output_channels",output_channels)
+    seg3 = tf.keras.layers.Conv2D(output_channels, 1, padding='same')(u3)
+    seg3 = tf.keras.layers.LeakyReLU(0.01)(seg3)
+    seg3 = tf.keras.layers.UpSampling2D()(seg3)
+
+    #upsampling-u2
+    u2 = tf.keras.layers.UpSampling2D()(u3)
+    u2 = tf.keras.layers.Conv2D(2*f, 3, padding='same')(u2)
+    u2 = tf.keras.layers.LeakyReLU(0.01)(u2)
+    #concatenate-u2
+    u2 = tf.keras.layers.concatenate([u2, d2])
+    #localization-u2
+    u2 = tf.keras.layers.Conv2D(2*f, 3, padding='same')(u2)
+    u2 = tf.keras.layers.LeakyReLU(0.01)(u2)
+    u2 = tf.keras.layers.Conv2D(2*f, 1, padding='same')(u2)
+    u2 = tf.keras.layers.LeakyReLU(0.01)(u2)
+    #segmentation-u2
+    seg2 = tf.keras.layers.Conv2D(output_channels, 1, padding='same')(u2)
+    seg2 = tf.keras.layers.LeakyReLU(0.01)(seg2)
+    seg_sum = tf.keras.layers.add([seg3, seg2])
+    seg_sum = tf.keras.layers.UpSampling2D()(seg_sum)
+
+    #upsampling-u1
+    u1 = tf.keras.layers.UpSampling2D()(u2)
+    u1 = tf.keras.layers.Conv2D(f, 3, padding='same')(u1)
+    u1 = tf.keras.layers.LeakyReLU(0.01)(u1)
+    #concatenate-u1
+    u1 = tf.keras.layers.concatenate([u1, d1])
+    #localization-u1
+    u1 = tf.keras.layers.Conv2D(f, 3, padding='same')(u1)
+    u1 = tf.keras.layers.LeakyReLU(0.01)(u1)
+    u1 = tf.keras.layers.Conv2D(f, 1, padding='same')(u1)
+    u1 = tf.keras.layers.LeakyReLU(0.01)(u1)
+    #segmentation-u1
+    seg1 = tf.keras.layers.Conv2D(output_channels, 1, padding='same')(u1)
+    seg1 = tf.keras.layers.LeakyReLU(0.01)(seg1)
+    seg_sum = tf.keras.layers.add([seg1, seg_sum])
+    outputs = tf.keras.layers.Conv2D(output_channels, 1, activation='softmax')(seg_sum)
+
+    return tf.keras.Model(inputs=inputs, outputs=outputs)
+
+model = unet_model(4, f=4)
+model.summary()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_ds.batch(10),epochs=10,validation_data=val_ds.batch(10))
+
+def show_predictions(ds, num=1):
+    for image, mask in ds.take(num):
+        pred_mask = model.predict(image[tf.newaxis, ...])
+        pred_mask = tf.argmax(pred_mask[0], axis=-1)
+        display([tf.squeeze(image), tf.argmax(mask, axis=-1), pred_mask])
+
+def dice_coefficient(y_true, y_pred, smooth = 1):
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    union = tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f)
+    dice = (2. * intersection + smooth) / (union + smooth)
+    return dice
+
+def dice_coefficient_loss(y_true, y_pred):
+    return 1 - dice_coef(y_true, y_pred)
+
+def prediction(ds):
+    pred = []
+    true = []
+    for image, mask in ds:
+        pred_mask = model.predict(image[tf.newaxis, ...])
+        pred.append(pred_mask)
+        true_mask = tf.cast(mask, tf.float32)
+        true.append(true_mask)
+    return pred, true
+
+pred, true = prediction(test_ds)
+dice = dice_coefficient(true, pred)
+print("Dice similarity coefficient is: ", dice)
