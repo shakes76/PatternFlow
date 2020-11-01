3dfa9bf650a90e1deacef08906ae1ed3c605216f
deliciousturtle <49872862+deliciousturtle@users.noreply.github.com>
2020-11-01 13:48:55 +1000

    Added comments

--- recognition/laterality_cnn/classify_laterality.py
+++ recognition/laterality_cnn/classify_laterality.py
@@ -13,6 +13,7 @@ data_dir = os.path.join("C:", "Users", "delic", ".keras", "datasets", "AKOA_Anal
 filenames = glob.glob(os.path.join(data_dir, '*', '*.png'))
 image_count = len(filenames)
 
+#Split filenames by patiend id
 patient_files = dict()
 for fn in filenames:
     pid = fn.split(os.path.sep)[-1].split('_')[0]
@@ -21,15 +22,18 @@ for fn in filenames:
     else:
         patient_files[pid].append(fn)
 
+#Count number of patients
 patient_ids = list(patient_files.keys())
 patient_count = len(patient_ids)
 
+
+#Split datasets (validation, test, training)
 random.seed(123)
 not_valid = True
 while not_valid:
     random.shuffle(patient_ids)
 
-    #Split the dataset - 16% validation, 20% test, 64% training (of PATIENTS)
+    #Split the patients - 16% validation, 20% test, 64% training
     val_count = int(patient_count * 0.16)
     test_count = int(patient_count * 0.36)
     val_patients = patient_ids[:val_count]
@@ -40,6 +44,7 @@ while not_valid:
     test_images = []
     train_images = []
 
+    #Add every patient's filenames to their respective image datasets
     for pid in val_patients:
         val_images.extend(patient_files[pid])
     for pid in test_patients:
@@ -47,15 +52,16 @@ while not_valid:
     for pid in train_patients:
         train_images.extend(patient_files[pid])
 
-    print(len(val_patients), len(test_patients), len(train_patients))
-    print(len(val_images), len(test_images), len(train_images))
+    # print(len(val_patients), len(test_patients), len(train_patients))
+    # print(len(val_images), len(test_images), len(train_images))
 
     #Extract labels
     train_labels = [fn.split(os.path.sep)[-2] for fn in train_images]
     val_labels = [fn.split(os.path.sep)[-2] for fn in val_images]
     test_labels = [fn.split(os.path.sep)[-2] for fn in test_images]
-    not_valid = False
+    not_valid = False #Break while loop
 
+    #Count number of images in each dataset labelled 'right'
     train_right_count = 0
     for label in train_labels:
         if label == 'right':
@@ -71,14 +77,17 @@ while not_valid:
         if label == 'right':
             test_right_count += 1
 
+    #Ratios of 'right' labelled to 'left' labelled
     right_ratios = [train_right_count/len(train_labels), val_right_count/len(val_labels), test_right_count/len(test_labels)]
-    print(right_ratios)
+    # print(right_ratios)
 
+    #If too many or too few 'right' images, re-shuffle and re-split datasets.
     for ratio in right_ratios:
         if ratio > 0.7 or ratio < 0.3:
             print("Re-shuffling")
-            not_valid = True
+            not_valid = True #continue while loop
   
+#Class names list and number of classes
 class_names = sorted(set(train_labels))
 num_classes = len(class_names)
 
@@ -104,6 +113,7 @@ def map_fn(filename, label):
     # Return the processed image and label.
     return img, one_hot
 
+#Apply mapping to datasets
 train_ds = train_ds.map(map_fn)
 val_ds = val_ds.map(map_fn)
 test_ds = test_ds.map(map_fn)
@@ -121,7 +131,7 @@ test_ds = test_ds.map(map_fn)
 #     plt.axis('off')
 # plt.show()
 
-#Configure dataset for performance and shuffle
+#Configure dataset for performance and shuffling. Shuffle buffer = number of images
 batch_size = 32
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 train_ds = train_ds.cache()
@@ -137,9 +147,9 @@ test_ds = test_ds.shuffle(len(test_images))
 test_ds = test_ds.batch(batch_size)
 test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
+#Create ModelCheckpoint to save and load trained models
 checkpoint_path = "training/ckpt01.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
-
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_path,
@@ -148,7 +158,11 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(
     save_freq='epoch'
 )
 
+#Create subclasses for layers and model
 class ConvBlock(tf.keras.layers.Layer):
+    """
+    Subclass for a convolution block (2 convolution layers, 1 maxpooling layer and 1 dropout layer)
+    """
     def __init__(self, filters=32):
         super(ConvBlock, self).__init__()
         self.conv1 = tf.keras.layers.Conv2D(filters, (3, 3), activation='relu')
@@ -162,9 +176,12 @@ class ConvBlock(tf.keras.layers.Layer):
         x = self.pool(x)
         return self.drop(x)
 
-class ConvModel(tf.keras.Model):
+class CNNModel(tf.keras.Model):
+    """
+    Subclass for the CNN model. 3 convolution blocks, 1 flatten layer, 2 dense layers and 1 dropout layer.
+    """
     def __init__(self, num_classes=2):
-        super(ConvModel, self).__init__()
+        super(CNNModel, self).__init__()
         self.block1 = ConvBlock(32)
         self.block2 = ConvBlock(64)
         self.block3 = ConvBlock(128)
@@ -182,26 +199,34 @@ class ConvModel(tf.keras.Model):
         x = self.drop(x)
         return self.d2sm(x)
 
-model = ConvModel(num_classes=num_classes)
+#Create an instance of the model
+model = CNNModel(num_classes=num_classes)
 
-image_batch, label_batch = next(iter(val_ds))
+#Use the model to predict an image to automatically 'build' it and allow calling of model.summary()
+image_batch, label_batch = next(iter(val_ds.take(1)))
 predictions = model.predict(image_batch)
 
+#Check model summary
 model.summary()
 
+#Loss function
 loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
 
+#Compile the model
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1E-3),
     loss=loss_fn,
     metrics=['accuracy']
 )
 
+#Load saved weights
 results = model.load_weights(checkpoint_path)
 
+#Train the model
 n_epochs = 10
 # results = model.fit(train_ds, epochs=n_epochs, callbacks=[cp_callback], validation_data=val_ds)
 
+#Check final weights by evaluating on all sets
 print("Training set: ")
 model.evaluate(train_ds, verbose=2)
 print("Validation set: ")
@@ -209,6 +234,7 @@ model.evaluate(val_ds, verbose=2)
 print("Test set: ")
 model.evaluate(test_ds, verbose=2)
 
+#Plot accuracy vs validation accuracy and loss vs validation loss (for every epoch)
 # plt.plot(results.history['accuracy'], label='accuracy')
 # plt.plot(results.history['val_accuracy'], label='val_accuracy')
 # plt.xlabel("Epoch")
