be0fbe194ad96363c4e845733522540b87c5a8d7
Dhilan Singh <s4434872@labs.eait.uq.edu.au>
2020-11-02 02:04:44 +1000

    Initial commit of unet improved oasis brain segmentation, mostly empty

---
+++ recognition/s4434872-unet-improved/unet_improved_oasis_brain.py
@@ -0,0 +1,17 @@
+"""
+OASIS Brain Dataset Segmentation with Improved UNet, 
+with all labels having a minimum Dice Similarity Coefficient 
+of 0.9 on the test set.
+
+@author Dhilan Singh (44348724)
+
+Start Date: 01/11/2020
+"""
+import tensorflow as tf
+
+print('Tensorflow Version:', tf.__version__)
+
+
+
+
+print('End')
