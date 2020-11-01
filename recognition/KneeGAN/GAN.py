58db1c4574efffc0d90d7a9418ea18abc5408ac6
Erik Brand <s4432349@labs.eait.uq.edu.au>
2020-11-01 10:53:47 +1000

    Further Documentation

--- recognition/KneeGAN/GAN.py
+++ recognition/KneeGAN/GAN.py
@@ -26,27 +26,27 @@ def generator(input_dim, latent_dim):
     
     input_layer = tf.keras.Input(latent_dim)
 
+    # Dense Section
     net = tf.keras.layers.Dense(1024)(input_layer)
     net = tf.keras.layers.Dense(input_dim*input_dim*64)(input_layer)
+    # Reshape to image size
     net = tf.keras.layers.Reshape((input_dim,input_dim,64))(net)
     net = tf.keras.layers.ReLU()(net)
 
+    # ConvTranspose
     net = tf.keras.layers.Conv2DTranspose(32, (3,3), strides=(2,2), padding='same')(net)
-    #net = tf.keras.layers.Conv2D(32, (3,3), strides=(1,1), padding='same')(net)
     net = tf.keras.layers.ReLU()(net)
 
     net = tf.keras.layers.Conv2DTranspose(32, (3,3), strides=(2,2), padding='same')(net)
-    #net = tf.keras.layers.Conv2D(32, (3,3), strides=(1,1), padding='same')(net)
     net = tf.keras.layers.ReLU()(net)
 
     net = tf.keras.layers.Conv2DTranspose(32, (3,3), strides=(2,2), padding='same')(net)
-    #net = tf.keras.layers.Conv2D(16, (3,3), strides=(1,1), padding='same')(net)
     net = tf.keras.layers.ReLU()(net)
 
     net = tf.keras.layers.Conv2DTranspose(16, (3,3), strides=(2,2), padding='same')(net)
-    #net = tf.keras.layers.Conv2D(16, (3,3), strides=(1,1), padding='same')(net)
     net = tf.keras.layers.ReLU()(net)
 
+    # Single Channel Output
     net = tf.keras.layers.Conv2D(1, (3,3), strides=(1,1), padding='same')(net)
 
     model = tf.keras.Model(inputs=input_layer, outputs=net)
@@ -60,6 +60,7 @@ def discriminator(input_dim):
     input_layer = tf.keras.Input(input_dim)
     net = input_layer
 
+    # Downsample
     net = tf.keras.layers.Conv2D(16, (3,3), padding='same')(net)
     net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
     net = tf.keras.layers.MaxPooling2D((2,2), strides=(2,2))(net)
@@ -72,11 +73,13 @@ def discriminator(input_dim):
     net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
     net = tf.keras.layers.MaxPooling2D((2,2), strides=(2,2))(net)
     
+    # Dense Section
     net = tf.keras.layers.Flatten()(net)
 
     net = tf.keras.layers.Dense(256)(net)
     net = tf.keras.layers.LeakyReLU(alpha=0.2)(net)
 
+    # Single Output
     net = tf.keras.layers.Dense(1)(net)
 
     model = tf.keras.Model(inputs=input_layer, outputs=net)
