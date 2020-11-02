7ae18450cbe06afdff7d5bc384d6e9db7435acc0
peiran66 <lprlzj66@gmail.com>
2020-11-02 20:41:11 +1000

    add a context module function

---
+++ recognition/45223499_improved_unet/model.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# context module function
+def res_net_block(input_data, conv_size):
+    x = tfa.layers.InstanceNormalization()(input_data)
+    x = tf.keras.layers.LeakyReLU(alpha=0.1)(x)
+    x = tf.keras.layers.Conv2D(conv_size, kernel_size = 3, padding='same')(x) 
+    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tfa.layers.InstanceNormalization()(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.1)(x)
+    x = tf.keras.layers.Conv2D(conv_size, kernel_size = 3, padding='same')(x) 
+    return x
