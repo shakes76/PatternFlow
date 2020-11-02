98d967f64f5035ba5baa26fd8a6ad0c0cb0414e7
peiran66 <lprlzj66@gmail.com>
2020-11-02 20:44:41 +1000

    add a function to calculate dice coefficient.

--- recognition/45223499_improved_unet/model.py
+++ recognition/45223499_improved_unet/model.py
@@ -20,3 +20,13 @@ def res_net_block(input_data, conv_size):
 def segmentation_layer(x):
     seg = tf.keras.layers.Conv2D(1, (1,1), activation = 'sigmoid')(x)
     return seg
+
+# calculate dice coefficient
+def dice_coefficient(y_true, y_pred, smooth = 0):
+    y_true = tf.cast(y_true, tf.float32)
+    #change the dimension to one
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    #calculation for the loss function
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + smooth)
