dacdb8f1f6b7485f37705d315c7bfe549ea54423
Ishraque Zahin <s4512925@labs.eait.uq.edu.au>
2020-11-03 17:28:30 +1000

    progress

--- recognition/Ishraque-InfoVAE/config.py
+++ recognition/Ishraque-InfoVAE/config.py
@@ -3,4 +3,3 @@ train_dir = r"C:\Users\s4512925\COMP3710_data\train_x"
 test_dir = r"C:\Users\s4512925\COMP3710_data\test_x"
 valid_dir = r"C:\Users\s4512925\COMP3710_data\validate_x"
 output_dir = "D:/s4512925"
-img_size = 256
--- recognition/Ishraque-InfoVAE/data.py
+++ recognition/Ishraque-InfoVAE/data.py
@@ -1,5 +1,14 @@
+import glob
 import tensorflow as tf
+import numpy as np
+from PIL import Image
 from config import train_dir, test_dir, valid_dir
+
+# files = glob.glob(r"C:\Users\s4512925\COMP3710_data\keras_png_slices_train\*.png")
+# train_x = np.asarray([np.asarray(Image.open(f)) / 255 for f in files])
+# train_x = np.expand_dims(train_x, axis=-1)
+# train_dataset = tf.data.Dataset.from_tensor_slices(train_x).batch(batch_size)
+
 img_size = 256
 # Load images. NOTE: image_dataset_from_directory is only available in tf 2.3+
 img_height = img_size
@@ -12,7 +21,7 @@ test_x = tf.keras.preprocessing.image_dataset_from_directory(
     color_mode='grayscale',
     image_size=(img_height, img_width),
     )
-test_x = test_x.unbatch()
+# test_x = test_x.unbatch()
 
 train_x = tf.keras.preprocessing.image_dataset_from_directory(
     train_dir,
@@ -21,7 +30,7 @@ train_x = tf.keras.preprocessing.image_dataset_from_directory(
     color_mode='grayscale',
     image_size=(img_height, img_width),
     )
-train_x = train_x.unbatch()
+# train_x = train_x.unbatch()
 
 valid_x = tf.keras.preprocessing.image_dataset_from_directory(
     valid_dir,
@@ -30,4 +39,4 @@ valid_x = tf.keras.preprocessing.image_dataset_from_directory(
     color_mode='grayscale',
     image_size=(img_height, img_width),
     )
-valid_x = valid_x.unbatch()
+# valid_x = valid_x.unbatch()
