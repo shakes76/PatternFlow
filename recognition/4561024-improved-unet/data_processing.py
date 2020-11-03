cb16d2b01850e232b9886d289775ff4384d477a5
Aghnia Putri Prawira <s4561024@labs.eait.uq.edu.au>
2020-11-03 14:13:34 +1000

    Initial commit of all files (empty).

---
+++ recognition/4561024-improved-unet/data_processing.py
@@ -0,0 +1,5 @@
+'''
+Load and process OASIS brain data set
+
+@author Aghnia Prawira (45610240)
+'''
---
+++ recognition/4561024-improved-unet/improved_unet.py
@@ -0,0 +1,11 @@
+'''
+Improved UNet
+
+@author Aghnia Prawira (45610240)
+'''
+import tensorflow as tf
+
+print('Tensorflow version:', tf.__version__)
+
+def test():
+    print("Testing.")
