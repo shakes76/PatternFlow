63e652989667152573c9e76e2a732309b318be0f
Aghnia Putri Prawira <s4561024@labs.eait.uq.edu.au>
2020-11-03 16:07:51 +1000

    Load images as array

--- recognition/4561024-improved-unet/data_processing.py
+++ recognition/4561024-improved-unet/data_processing.py
@@ -4,5 +4,21 @@ Load and process OASIS brain data set
 @author Aghnia Prawira (45610240)
 '''
 
+import os
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
 def test():
     print("Testing data processing.")
+    
+def load_image(path):
+    image_array = []
+    for name in os.listdir(path):
+        # Loading and resizing image
+        filename = path + name
+        image = load_img(filename, target_size=(256, 256))
+        # Convert image pixels to array
+        image = img_to_array(image)
+        image = image/255
+        image_array.append(image)
+    return image_array
+        
