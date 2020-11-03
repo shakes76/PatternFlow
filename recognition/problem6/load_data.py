840a93a5d60d6d2b692af8bef33e3ab0f8d7bbe8
Lewis Ainslie <s4485827@labs.eait.uq.edu.au>
2020-11-03 16:00:48 +1000

    main and loading data setup

--- recognition/problem6/load_data.py
+++ recognition/problem6/load_data.py
@@ -1,3 +1,20 @@
+import tensorflow as tf
+import os
+
+def load_paths(directory):
+    training_paths = []
+    if not os.path.isfile(training_binary_path):
+        for filename in os.listdir(directory):  
+            path = os.path.join(directory,filename)
+            training_paths.append(path)
+            
+        random.shuffle(training_paths)
+        return training_paths
 
 
-
+def load_images(filename):
+    img = tf.io.read_file(filename)
+    img = tf.io.decode_png(img, channels = 1)
+    img = tf.image.resize(img, size = (256, 256))
+    img = img / 255.0
+    return img
---
+++ recognition/problem6/main.py
@@ -0,0 +1,7 @@
+import sys
+
+
+if __name__ == "__main__":
+    for arg in sys.argv:
+        print(arg)
+    
