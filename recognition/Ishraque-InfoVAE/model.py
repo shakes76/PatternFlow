444670caed7564938e599ea5cd73393b21b12b44
Ishraque Zahin <s4512925@labs.eait.uq.edu.au>
2020-11-03 16:14:56 +1000

    refactored model, implemented loss fn

--- recognition/Ishraque-InfoVAE/model.py
+++ recognition/Ishraque-InfoVAE/model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models, optimizers
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, models, optimizers, losses
 import tensorflow_probability as tfp
 from data import img_size
 
@@ -8,8 +9,8 @@ def get_encoder(latent_dim):
     c1 = layers.Conv2D(filters=32, kernel_size=3, input_shape=(2, 2), activation='relu')(input_layer)
     c2 = layers.Conv2D(filters=64, kernel_size=3, strides=(2, 2), activation='relu')(c1)
     d1 = layers.Flatten()(c2)
-    d2 = layers.Dense(latent_dim)
-    return models.Model(input=input_layer, output=d2)
+    d2 = layers.Dense(latent_dim)(d1)
+    return models.Model(inputs=input_layer, outputs=d2)
 
 def get_decoder(latent_dim):
     input_layer = layers.Input(shape=(latent_dim, )) # Ensure this matches output of encoder network
@@ -18,20 +19,53 @@ def get_decoder(latent_dim):
     c1 = layers.Conv2DTranspose(filters=64, kernel_size=3, strides=2, padding='same', activation='relu')(r1)
     c2 = layers.Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(c1)
     c3 = layers.Conv2DTranspose(filters=1, kernel_size=3, strides=1, padding='same')(c2)
-    return models.Model(input=input_layer, output=c3)
+    return models.Model(inputs=input_layer, outputs=c3)
 
 class InfoVAE():
     """Convolutional variational autoencoder."""
 
     def __init__(self, latent_dim):
-        super(InfoVAE, self).__init__()
         self.latent_dim = latent_dim
         self.encoder = get_encoder(latent_dim)
         self.decoder = get_decoder(latent_dim)
-        x = self.decoder(self.encoder)
-        self.model = models.Model(input=self.encoder, output=self.decoder)
+        i = keras.Input(shape=(img_size, img_size, 1))
+        e = self.encoder(i)
+        x = self.decoder(e)
+        self.model = keras.Model(inputs=i, outputs=x)
         self.optimizer = optimizers.Adam()
 
+    def _compute_kernel(self, x, y):
+        x_size = tf.shape(x)[0]
+        y_size = tf.shape(y)[0]
+        dim = tf.shape(x)[1]
+        tiled_x = tf.tile(tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1]))
+        tiled_y = tf.tile(tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1]))
+        return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float32))
+
+    def _compute_mmd(self, x, y):
+        x_kernel = self._compute_kernel(x, x)
+        y_kernel = self._compute_kernel(y, y)
+        xy_kernel = self._compute_kernel(x, y)
+        return tf.reduce_mean(x_kernel) + tf.reduce_mean(y_kernel) - 2 * tf.reduce_mean(xy_kernel)
+
+    def _encoder_loss(self, latent_encoding: tf.Tensor):
+        # actual_dist = tf.random.normal(shape=(batch_size, self.latent_dim))
+        actual_dist = tf.random.normal(shape=latent_encoding.shape)
+        return self._compute_mmd(actual_dist, latent_encoding)
+
+    @tf.function
+    def train(self, images: tf.Tensor):
+        loss = -1
+        with tf.GradientTape() as tape:
+            latent_encoding = self.encoder(images, training=True)
+            reconstruction = self.decoder(latent_encoding)
+            enc_loss = self._encoder_loss(latent_encoding)
+            rec_loss = losses.mean_squared_error(images, reconstruction)
+            loss = enc_loss + rec_loss
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+        return loss
+    
     @tf.function
     def sample(self, eps=None):
         if eps is None:
--- recognition/Ishraque-InfoVAE/run.py
+++ recognition/Ishraque-InfoVAE/run.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
-from data import train_x, test_x, valid_x
-
+# from data import train_x, test_x, valid_x
+from model import InfoVAE
 # normalise data
-train_x = train_x.map(lambda x: x / 255)
-test_x = test_x.map(lambda x: x / 255)
-valid_x = valid_x.map(lambda x: x / 255)
-
+# train_x = train_x.map(lambda x: x / 255)
+# test_x = test_x.map(lambda x: x / 255)
+# valid_x = valid_x.map(lambda x: x / 255)
+latent_dim = 2
+iv = InfoVAE(latent_dim)
