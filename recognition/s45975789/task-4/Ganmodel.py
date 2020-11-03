4f8c44c8c94acda5e6f068cad44012b7be376cb4
Pragatheeswari Velraj <s4597578@labs.eait.uq.edu.au>
2020-11-04 00:41:45 +1000

    Added Ganmodel.py->generator,discriminator. Task-6->updated with Ganmodel module.

---
+++ recognition/s45975789/task-4/Ganmodel.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D,LeakyReLU
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense, Input
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(16*16*3,  input_shape=(100,)))
+    model.add(layers.Dense(65536,  input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((16, 16, 256)))
+    #assert model.output_shape == (None, 28, 28, 256) # Note: None is the batch size
+
+    model.add(layers.Conv2DTranspose(256, (3, 3), strides=(1, 1), padding='same'))
+    #assert model.output_shape == (None, 16, 16, 256)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same'))
+    #assert model.output_shape == (None, 32, 32, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same'))
+    #assert model.output_shape == (None, 64, 64,64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), padding='same'))
+    #assert model.output_shape == (None, 128, 128, 32)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(16, (3, 3), strides=(2, 2), padding='same'))
+    #assert model.output_shape == (None, 256, 256, 16)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Conv2D(1, (1, 1), activation='elu'))
+    assert model.output_shape == (None, 256, 256, 1)
+    noise = Input(shape=(100,)) 
+    image = model(noise) 
+  
+    return Model(noise, image) 
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(16, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[256, 256, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+    model.add(Activation('sigmoid'))
+    #model.add(Activaion('softmax'))
+    image = Input(shape=[256, 256, 1]) 
+    v = model(image) 
+  
+    return Model(image, v) 
