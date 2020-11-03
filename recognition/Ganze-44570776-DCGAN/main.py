569a90c552f20291b0f8cf15e66cf827b0c0e909
Ganze ZHENG <1062168846@qq.com>
2020-11-03 12:31:37 +1000

    Added discriminator network

--- recognition/Ganze-44570776-DCGAN/main.py
+++ recognition/Ganze-44570776-DCGAN/main.py
@@ -2,6 +2,9 @@
 This is program for COMP3710 Report, resolving problem 6.
 [Create a generative model of the OASIS brain or the OAI AKOA knee data set using a DCGAN that
 has a “reasonably clear image” and a Structured Similarity (SSIM) of over 0.6.]
+
+@auther: Ganze Zheng
+@studentID: 44570776
 """
 
 import glob
@@ -35,64 +38,111 @@ images = np.array(images)
 
 # Check dataset shape and verify the images
 print(images.shape)
-plt.figure(figsize=(10, 10))
-for i in range(25):
-    plt.subplot(5, 5, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(images[i])
-plt.show()
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.imshow(images[i])
+# plt.show()
 
-# Normalise data to [-1, 1]
+# Normalise data to [0, 1]
 data = images.reshape((n_datasize, 256, 256, 1)).astype('float32')
-data = (data - 127.5) / 127.5
+data = data / 255.0
 print(data.shape)
 
 # Batch and shuffle the data
-X_train = tf.data.Dataset.from_tensor_slices(data).shuffle(n_datasize).batch(batch_size)
+X_train = tf.data.Dataset.from_tensor_slices(
+    data).shuffle(n_datasize).batch(batch_size)
 
 
 # Build networks
 # Generator network
-def generator_network(input_shape):
+def generator_network(input_shape, name='G'):
     """
     Generator network
     """
     input = Input(input_shape)
     dense = Dense(8*8*512)(input)
-    dense = Reshape((8, 8, 512))(dense)
+    norm = BatchNormalization()(dense)
+    activation = LeakyReLU()(norm)
+    dense = Reshape((8, 8, 512))(activation)
     # 8x8x512
-    net = Conv2DTranspose(256, (4, 4), strides=(2, 2), padding='same')(dense)
-    norm = BatchNormalization()(net)
+    deepConvNet = Conv2DTranspose(
+        256, (4, 4), strides=(2, 2), padding='same')(dense)
+    norm = BatchNormalization()(deepConvNet)
     activation = LeakyReLU()(norm)
     # 16x16x256
-    net = Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same')(activation)
-    norm = BatchNormalization()(net)
+    deepConvNet = Conv2DTranspose(
+        128, (4, 4), strides=(2, 2), padding='same')(activation)
+    norm = BatchNormalization()(deepConvNet)
     activation = LeakyReLU()(norm)
     # 32x32x128
-    net = Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same')(activation)
-    norm = BatchNormalization()(net)
+    deepConvNet = Conv2DTranspose(
+        64, (4, 4), strides=(2, 2), padding='same')(activation)
+    norm = BatchNormalization()(deepConvNet)
     activation = LeakyReLU()(norm)
     # 64x64x64
-    net = Conv2DTranspose(32, (4, 4), strides=(2, 2), padding='same')(activation)
-    norm = BatchNormalization()(net)
+    deepConvNet = Conv2DTranspose(
+        32, (4, 4), strides=(2, 2), padding='same')(activation)
+    norm = BatchNormalization()(deepConvNet)
     activation = LeakyReLU()(norm)
-    # 256x256x3
-    g_net = Conv2DTranspose(3, (4, 4), strides=(2, 2), padding='same', activation='tanh')(activation)
+    # 128x128x32
+    g_net = Conv2DTranspose(1, (4, 4), strides=(
+        2, 2), padding='same', activation='sigmoid')(activation)
+    # 256x256x1
 
-    return Model(inputs=input, outputs=g_net)
+    return Model(inputs=input, outputs=g_net, name=name)
+
+
+# Discriminator network
+def discriminator_network(input_shape, name='D'):
+    """
+    Discriminator network
+    """
+    input = Input(input_shape)
+    # 256x256x1
+    convNet = Conv2D(32, (4, 4), strides=(2, 2), padding='same')(input)
+    activation = LeakyReLU()(convNet)
+    norm = BatchNormalization()(activation)
+    # 128x128x32
+    convNet = Conv2D(64, (4, 4), strides=(2, 2), padding='same')(norm)
+    activation = LeakyReLU()(convNet)
+    norm = BatchNormalization()(activation)
+    # 64x64x64
+    convNet = Conv2D(128, (4, 4), strides=(2, 2), padding='same')(norm)
+    activation = LeakyReLU()(convNet)
+    norm = BatchNormalization()(activation)
+    # 32x32x128
+    convNet = Conv2D(256, (4, 4), strides=(2, 2), padding='same')(norm)
+    activation = LeakyReLU()(convNet)
+    norm = BatchNormalization()(activation)
+    # 16x16x256
+
+    flatten = Flatten()(norm)
+    d_net = Dense(1, activation='sigmoid')(flatten)
+
+    return Model(inputs=input, outputs=d_net, name=name)
 
 
 # Build models
-input_shape = (1, 1, 100)
+noise_shape = (100, )
+image_shape = (256, 256, 1)
 
 # Build generator
-generator = generator_network(input_shape)
+generator = generator_network(noise_shape)
 generator.summary()
 # Verify the model
-noise = tf.random.normal([1, 1, 1, 100])
-generated_images = generator(noise, training=False)
-plt.imshow(generated_images[0, :, :, 0])
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+plt.imshow(generated_image[0, :, :, 0])
 plt.show()
+
+# Build discriminator
+discriminator = discriminator_network(image_shape)
+discriminator.summary()
+# Verify the model
+decision = discriminator(generated_image)
+print(decision)
 
 print("End")
