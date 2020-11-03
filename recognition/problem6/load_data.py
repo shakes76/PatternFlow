24b495a8c8ed75eed9ad5a3cffc8947866500221
Lewis Ainslie <s4485827@labs.eait.uq.edu.au>
2020-11-03 16:42:25 +1000

    loading data and model completed

--- recognition/problem6/load_data.py
+++ recognition/problem6/load_data.py
@@ -1,20 +1,22 @@
 import tensorflow as tf
 import os
+import random
 
 def load_paths(directory):
-    training_paths = []
-    if not os.path.isfile(training_binary_path):
-        for filename in os.listdir(directory):  
-            path = os.path.join(directory,filename)
-            training_paths.append(path)
-            
-        random.shuffle(training_paths)
-        return training_paths
+    training_paths = {}
+
+    
+    for filename in sorted(os.listdir(directory)):  
+        path = os.path.join(directory,filename)
+        training_paths[path] = 1
+        
+      
+    return sorted(training_paths.keys())
 
 
 def load_images(filename):
     img = tf.io.read_file(filename)
-    img = tf.io.decode_png(img, channels = 1)
+    img = tf.io.decode_png(img, channels = 3)
     img = tf.image.resize(img, size = (256, 256))
     img = img / 255.0
     return img
--- recognition/problem6/main.py
+++ recognition/problem6/main.py
@@ -1,7 +1,16 @@
 import sys
 
+from load_data import *
 
 if __name__ == "__main__":
-    for arg in sys.argv:
-        print(arg)
+    path = sys.argv[1]
+    
+    path = os.path.join(path, 'keras_png_slices_data')
+    path = os.path.join(path, 'keras_png_slices_seg_train')
+    
+    image_paths = load_paths(path)
+
+    image_data = tf.data.Dataset.from_tensor_slices(image_paths)
+
+    image_data = image_data.map(load_images)
     
