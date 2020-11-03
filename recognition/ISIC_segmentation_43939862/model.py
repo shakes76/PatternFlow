fa955e365ca66b1896486527ed1a52d936440144
Joseph Menesch <s4393986@labs.eait.uq.edu.au>
2020-11-03 23:26:41 +1000

    Changed residual blocks to pre-activation

--- recognition/ISIC_segmentation_43939862/model.py
+++ recognition/ISIC_segmentation_43939862/model.py
@@ -17,68 +17,68 @@ import tensorflow as tf
 # seg = segmentation
 # =============================================================================
 def ImprovedUnet(h, w, n_channels):
-    F = [16,32,64,128,256]
+    F = [8,16,32,64,128]
     input_layer = tf.keras.layers.Input(shape = (h,w,n_channels))
         
     #Block 1 (Context)
     b1_ds = tf.keras.layers.Conv2D(F[0], (3,3), padding = 'same')(input_layer)
     b1_res = b1_ds
-    b1_m_c1 = tf.keras.layers.Conv2D(F[0], (3,3), padding = 'same')(b1_ds)
-    b1_m_c1n = tf.keras.layers.BatchNormalization()(b1_m_c1)
+    b1_m_c1n = tf.keras.layers.BatchNormalization()(b1_ds)
     b1_m_c1a = tf.keras.layers.LeakyReLU(alpha=0.01)(b1_m_c1n)
-    b1_m_do = tf.keras.layers.Dropout(rate=0.3)(b1_m_c1a)
-    b1_m_c2 = tf.keras.layers.Conv2D(F[0], (3,3), padding = 'same')(b1_m_do)
-    b1_m_c2n = tf.keras.layers.BatchNormalization()(b1_m_c2)
+    b1_m_c1 = tf.keras.layers.Conv2D(F[0], (3,3), padding = 'same')(b1_m_c1a)
+    b1_m_do = tf.keras.layers.Dropout(rate=0.3)(b1_m_c1)
+    b1_m_c2n = tf.keras.layers.BatchNormalization()(b1_m_do)
     b1_m_c2a = tf.keras.layers.LeakyReLU(alpha=0.01)(b1_m_c2n)
-    b1_out = b1_m_c2a + b1_res
+    b1_m_c2 = tf.keras.layers.Conv2D(F[0], (3,3), padding = 'same')(b1_m_c2a)
+    b1_out = b1_m_c2 + b1_res
     
     #Block 2 (Context)
     b2_ds = tf.keras.layers.Conv2D(F[1], (3,3), strides = (2,2), padding = 'same')(b1_out)
     b2_res = b2_ds
-    b2_m_c1 = tf.keras.layers.Conv2D(F[1], (3,3), padding = 'same')(b2_ds)
-    b2_m_c1n = tf.keras.layers.BatchNormalization()(b2_m_c1)
+    b2_m_c1n = tf.keras.layers.BatchNormalization()(b2_ds)
     b2_m_c1a = tf.keras.layers.LeakyReLU(alpha=0.01)(b2_m_c1n)
-    b2_m_do = tf.keras.layers.Dropout(rate=0.3)(b2_m_c1a)
-    b2_m_c2 = tf.keras.layers.Conv2D(F[1], (3,3), padding = 'same')(b2_m_do)
-    b2_m_c2n = tf.keras.layers.BatchNormalization()(b2_m_c2)
+    b2_m_c1 = tf.keras.layers.Conv2D(F[1], (3,3), padding = 'same')(b2_m_c1a)
+    b2_m_do = tf.keras.layers.Dropout(rate=0.3)(b2_m_c1)
+    b2_m_c2n = tf.keras.layers.BatchNormalization()(b2_m_do)
     b2_m_c2a = tf.keras.layers.LeakyReLU(alpha=0.01)(b2_m_c2n)
-    b2_out = b2_m_c2a + b2_res
+    b2_m_c2 = tf.keras.layers.Conv2D(F[1], (3,3), padding = 'same')(b2_m_c2a)
+    b2_out = b2_m_c2 + b2_res
     
     #Block 3 (Context)
     b3_ds = tf.keras.layers.Conv2D(F[2], (3,3), strides = (2,2), padding = 'same')(b2_out)
     b3_res = b3_ds
-    b3_m_c1 = tf.keras.layers.Conv2D(F[2], (3,3), padding = 'same')(b3_ds)
-    b3_m_c1n = tf.keras.layers.BatchNormalization()(b3_m_c1)
+    b3_m_c1n = tf.keras.layers.BatchNormalization()(b3_ds)
     b3_m_c1a = tf.keras.layers.LeakyReLU(alpha=0.01)(b3_m_c1n)
-    b3_m_do = tf.keras.layers.Dropout(rate=0.3)(b3_m_c1a)
-    b3_m_c2 = tf.keras.layers.Conv2D(F[2], (3,3), padding = 'same')(b3_m_do)
-    b3_m_c2n = tf.keras.layers.BatchNormalization()(b3_m_c2)
+    b3_m_c1 = tf.keras.layers.Conv2D(F[2], (3,3), padding = 'same')(b3_m_c1a)
+    b3_m_do = tf.keras.layers.Dropout(rate=0.3)(b3_m_c1)
+    b3_m_c2n = tf.keras.layers.BatchNormalization()(b3_m_do)
     b3_m_c2a = tf.keras.layers.LeakyReLU(alpha=0.01)(b3_m_c2n)
-    b3_out = b3_m_c2a + b3_res
+    b3_m_c2 = tf.keras.layers.Conv2D(F[2], (3,3), padding = 'same')(b3_m_c2a)
+    b3_out = b3_m_c2 + b3_res
     
     #Block 4 (Context)
     b4_ds = tf.keras.layers.Conv2D(F[3], (3,3), strides = (2,2), padding = 'same')(b3_out)
     b4_res = b4_ds
-    b4_m_c1 = tf.keras.layers.Conv2D(F[3], (3,3), padding = 'same')(b4_ds)
-    b4_m_c1n = tf.keras.layers.BatchNormalization()(b4_m_c1)
+    b4_m_c1n = tf.keras.layers.BatchNormalization()(b4_ds)
     b4_m_c1a = tf.keras.layers.LeakyReLU(alpha=0.01)(b4_m_c1n)
-    b4_m_do = tf.keras.layers.Dropout(rate=0.3)(b4_m_c1a)
-    b4_m_c2 = tf.keras.layers.Conv2D(F[3], (3,3), padding = 'same')(b4_m_do)
-    b4_m_c2n = tf.keras.layers.BatchNormalization()(b4_m_c2)
+    b4_m_c1 = tf.keras.layers.Conv2D(F[3], (3,3), padding = 'same')(b4_m_c1a)
+    b4_m_do = tf.keras.layers.Dropout(rate=0.3)(b4_m_c1)
+    b4_m_c2n = tf.keras.layers.BatchNormalization()(b4_m_do)
     b4_m_c2a = tf.keras.layers.LeakyReLU(alpha=0.01)(b4_m_c2n)
-    b4_out = b4_m_c2a + b4_res
+    b4_m_c2 = tf.keras.layers.Conv2D(F[3], (3,3), padding = 'same')(b4_m_c2a)
+    b4_out = b4_m_c2 + b4_res
     
     #Block 5 (Context)
     b5_ds = tf.keras.layers.Conv2D(F[4], (3,3), strides = (2,2), padding = 'same')(b4_out)
     b5_res = b5_ds
-    b5_m_c1 = tf.keras.layers.Conv2D(F[4], (3,3), padding = 'same')(b5_ds)
-    b5_m_c1n = tf.keras.layers.BatchNormalization()(b5_m_c1)
+    b5_m_c1n = tf.keras.layers.BatchNormalization()(b5_ds)
     b5_m_c1a = tf.keras.layers.LeakyReLU(alpha=0.01)(b5_m_c1n)
-    b5_m_do = tf.keras.layers.Dropout(rate=0.3)(b5_m_c1a)
-    b5_m_c2 = tf.keras.layers.Conv2D(F[4], (3,3), padding = 'same')(b5_m_do)
-    b5_m_c2n = tf.keras.layers.BatchNormalization()(b5_m_c2)
+    b5_m_c1 = tf.keras.layers.Conv2D(F[4], (3,3), padding = 'same')(b5_m_c1a)
+    b5_m_do = tf.keras.layers.Dropout(rate=0.3)(b5_m_c1)
+    b5_m_c2n = tf.keras.layers.BatchNormalization()(b5_m_do)
     b5_m_c2a = tf.keras.layers.LeakyReLU(alpha=0.01)(b5_m_c2n)
-    b5_out = b5_m_c2a + b5_res
+    b5_m_c2 = tf.keras.layers.Conv2D(F[4], (3,3), padding = 'same')(b5_m_c2a)
+    b5_out = b5_m_c2 + b5_res
     
     #Block 6 (Localization)
     b6_us = tf.keras.layers.Conv2DTranspose(F[3], (3,3), strides = (2,2), padding = 'same')(b5_out)
