0568221a90be4d91352e29b94ca6b79d14955893
Max Hornigold <mchornigold@gmail.com>
2020-11-04 17:08:23 +1000

    driver script comments + additional image

--- recognition/ISIC Data Set With UNet/driver_script.py
+++ recognition/ISIC Data Set With UNet/driver_script.py
@@ -15,7 +15,7 @@ from tensorflow.keras import backend as K
 
 
 def dice_coefficient(y_true, y_pred, smooth = 0.):
-    """Compute the dice coefficient for a prediction"""
+    """Computes the dice similarity coefficient for a prediction."""
     
     # change the dimension to one
     y_true_f = K.flatten(y_true)
@@ -28,25 +28,24 @@ def dice_coefficient(y_true, y_pred, smooth = 0.):
 
 
 def dice_coefficient_loss(y_true, y_pred):
-    """Compute the dice coefficient loss for a prediction"""
+    """Computes the dice similarity coefficient loss for a prediction."""
     return 1. - dice_coefficient(y_true, y_pred)
 
 
 def compute_dice_coefficients(model, ds):
-    """Compute the minimum and the average dice coefficient for all predictions
-    in the provided dataset"""
+    """Computes the average dice similarity coefficient for all predictions
+    made using the provided dataset."""
     DCEs = []
     for image, mask in ds:
         pred_mask = model.predict(image[tf.newaxis, ...])
         pred_mask = tf.math.round(pred_mask)
         DCE = dice_coefficient(mask, pred_mask)
         DCEs.append(DCE)
-    print("Minimum Dice Coefficient = ", min(DCEs))
     print("Average Dice Coefficient = ", sum(DCEs)/len(DCEs))
 
 
 def display(display_list):
-    """Display provided plots"""
+    """Displays plots of the provided data."""
     plt.figure(figsize=(10, 6))
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i+1)
@@ -56,14 +55,14 @@ def display(display_list):
 
 
 def display_data(ds, n=1):
-    """Display n images and masks from a given dataset"""
+    """Displays n images and masks from a given dataset."""
     for image, mask in ds.take(n):
         display([tf.squeeze(image), tf.squeeze(mask)])
 
 
 def display_predictions(model, ds, n=1):
-    """"Make n predictions using the model and the given dataset and display
-    these predictions"""
+    """Makes n predictions using the model and the given dataset and displays
+    these predictions."""
     for image, mask in ds.take(n):
         pred_mask = model.predict(image[tf.newaxis, ...])
         pred_mask = tf.math.round(pred_mask)
@@ -71,7 +70,7 @@ def display_predictions(model, ds, n=1):
 
 
 def analyse_training_history(history):
-    """Plot the acuraccy and validation accuracy of the model as it trains"""
+    """Plots the acuraccy and validation accuracy of the model as it trains."""
     plt.figure(figsize=(10, 6))
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
@@ -92,7 +91,7 @@ class DisplayCallback(tf.keras.callbacks.Callback):
 
 
 def decode_png(file_path):
-    """Decodes and resizes a png image"""
+    """Decodes and resizes a png image."""
     png = tf.io.read_file(file_path)
     png = tf.image.decode_png(png, channels=1)
     png = tf.image.resize(png, (256, 256))
@@ -100,7 +99,7 @@ def decode_png(file_path):
 
 
 def decode_jpg(file_path):
-    """Decodes and resizes a jpeg image"""
+    """Decodes and resizes a jpeg image."""
     jpg = tf.io.read_file(file_path)
     jpg = tf.image.decode_jpeg(jpg, channels=1)
     jpg = tf.image.resize(jpg, (256, 256))
@@ -108,7 +107,7 @@ def decode_jpg(file_path):
 
 
 def process_path(image_fp, mask_fp):
-    """Process sn image and a mask by decoding and normalising them"""
+    """Processes an image and a mask by decoding and normalising them."""
     image = decode_jpg(image_fp)
     image = tf.cast(image, tf.float32) / 255.0
     mask = decode_png(mask_fp)
@@ -118,7 +117,8 @@ def process_path(image_fp, mask_fp):
 
 
 def import_ISIC_data():
-    """ Download the dataset """
+    """ Downloads the ISIC dataset from a specified location. Manipulates the 
+    data into training, validating and testind datasets."""
     
     # Get images and masks
     images = sorted(glob.glob("C:/Users/Mchor/OneDrive/Desktop/All/Personal/UQ/COMP3710/Assessment/Laboratory Report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1-2_Training_Input_x2/*.jpg"))
