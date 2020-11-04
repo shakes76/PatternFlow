c9b67b3bd13537f1d0c2226d1ef77ef99fbf722a
matt-jones27 <45220137+matt-jones27@users.noreply.github.com>
2020-11-04 15:53:46 +1000

    Model added to segmentation.py

--- recognition/Segment-ISICs-IUNet-44335706/ISIC_segment.py
+++ recognition/Segment-ISICs-IUNet-44335706/ISIC_segment.py
@@ -36,14 +36,82 @@ validate_seq = iunet_sequence(x_images[train_index:validate_index], y_images[tra
 test_seq = iunet_sequence(x_images[validate_index:], y_images[validate_index:], VALIDATE_BATCH_SIZE)
 
 # Create model
+inputs = tf.keras.Input((256, 256, 1))
 
+# Start block
+layer = tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu")(inputs)
+layer = tf.keras.layers.BatchNormalization()(layer)
+previous_block = layer
 
+# List for storing layers for concatenating
+layers = []
 
 # Create model
+# Downsampling phase
+for depth in (64, 128, 256):
+	# First convolutional layer
+	layer = tf.keras.layers.Conv2D(depth, 3, padding="same", activation="relu")(layer)
+	layer = tf.keras.layers.BatchNormalization()(layer)
+	
+	# Add dropout in between blocks to prevent overfitting
+	layer = tf.keras.layers.Dropout(0.2)(layer)
+
+	# Second convolutional layer
+	layer = tf.keras.layers.Conv2D(depth, 3, padding="same", activation = "relu")(layer)
+	layer = tf.keras.layers.BatchNormalization()(layer)
+	
+	# Store layer in list
+	layers.append(layer)
+	
+	# Pool down
+	layer = tf.keras.layers.MaxPooling2D((2, 2))(layer)
+
+# Upsampling phase
+depths = (128, 64, 32)
+for i in range(3):
+	# First convolutional layer
+	layer = tf.keras.layers.Conv2DTranspose(depths[i], 3, padding="same", activation="relu")(layer)
+	layer = tf.keras.layers.BatchNormalization()(layer)
+	
+	# Add dropout in between blocks to prevent overfitting
+	layer = tf.keras.layers.Dropout(0.2)(layer)
+
+	# Second convolutional layer
+	layer = tf.keras.layers.Conv2DTranspose(depths[i], 3, padding="same", activation="relu")(layer)
+	layer = tf.keras.layers.BatchNormalization()(layer)
+	
+	# Normalise the batch and upsample
+	
+	layer = tf.keras.layers.UpSampling2D(2)(layer)
+	layer = tf.keras.layers.Concatenate(axis=3)([layer, layers[-i-1]])
+
+# Classify at the final output layer with softmax
+outputs = tf.keras.layers.Conv2D(2, 3, activation="softmax", padding="same")(layer)
+
+# Define the model
+model = tf.keras.Model(inputs, outputs)
+# print(model.summary())
 
 # Compile model
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.CategoricalCrossentropy(),
+    metrics=['accuracy'])
 
 # Train model
+history = model.fit(
+    train_seq,
+    epochs=2,
+    validation_data=validate_seq,
+)
+
+# Plot the loss and accuracy curves of the training
+plt.plot(history.history['accuracy'], label='training accuracy')
+plt.plot(history.history['val_accuracy'], label ='validation accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.0, 1])
+plt.legend(loc='upper left')
 
 # Evaluate model
-
+model.evaluate(test_data)
