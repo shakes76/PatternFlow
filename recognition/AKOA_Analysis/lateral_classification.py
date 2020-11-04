6ac7c9f68a82fac562ef0e14ad5b5c52ed2595b7
josh-lim1234 <joshlim1234@gmail.com>
2020-11-05 01:00:48 +0800

    move data split

--- recognition/AKOA_Analysis/lateral_classification.py
+++ recognition/AKOA_Analysis/lateral_classification.py
@@ -1,43 +1,10 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-import glob
-import PIL
-from PIL import Image
 import matplotlib as ml
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 
-def train(images, shape, epochs):
-    #Import the data
-
-    filelist = glob.glob(images)
-    image = np.array([np.array(Image.open(fname)) for fname in filelist])
-    
-    #Create label
-
-    label = []
-
-    #Populate label list
-
-    for fname in filelist:
-        if 'right' in fname.lower():
-            label.append(0)
-        elif 'R_I_G_H_T' in fname:
-            label.append(0)
-        elif 'left' in fname.lower():
-            label.append(1)
-        elif 'L_E_F_T' in fname:
-            label.append(1)
-            
-    #Convert label list to numpy array
-
-    label = np.array(label)
-
-    #Split label and images into test and training sets
-
-    X_train, X_test, y_train, y_test = train_test_split(image, label, test_size=0.25, random_state=42)
+def train(shape, epochs, image_train, label_train, image_test, label_test):
     
     #Create model
 
@@ -51,7 +18,7 @@ def train(images, shape, epochs):
 
     model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy', 'mse'])
 
-    history = model.fit(X_train, y_train, epochs=epochs, validation_split=0.2)
+    history = model.fit(image_train, label_train, epochs=epochs, validation_split=0.2)
 
     #Plot accuracy and validation accuracy
     
@@ -63,7 +30,7 @@ def train(images, shape, epochs):
     plt.legend(loc='lower right')    
     plt.savefig('graphs/accuracy.png')
 
-    test_loss, test_accm, test_mse = model.evaluate(X_test,  y_test, verbose=2)
+    test_loss, test_accm, test_mse = model.evaluate(image_test,  label_test, verbose=2)
     
     print('test_loss:', test_loss)
     print('test_accm:', test_accm)
