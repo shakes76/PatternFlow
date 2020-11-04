3378ce99c16d8c7e8ae88c542298d6fb0c20c980
Tristan Samson <s4397119@labs.eait.uq.edu.au>
2020-11-04 16:50:20 +1000

    Load and format a small set of input data in driver script

--- recognition/TBS-UNet_ISIC/driver.py
+++ recognition/TBS-UNet_ISIC/driver.py
@@ -1,4 +1,7 @@
 import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Get filepaths
 input_data_dir = "data\\reduced\\ISIC2018_Task1-2_Training_Input_x2"
@@ -7,5 +10,25 @@ gt_data_dir = "data\\reduced\\ISIC2018_Task1_Training_GroundTruth_x2"
 input_data_paths = os.listdir(input_data_dir)
 gt_data_paths = os.listdir(gt_data_dir)
 
-print(input_data_paths)
-print(gt_data_paths)
+if(len(input_data_paths) != len(gt_data_paths)):
+    print("Mismatch in data / labels")
+
+nTrain = len(input_data_paths)
+
+testImg = Image.open(input_data_dir + "\\" + input_data_paths[0])
+nx, ny = testImg.size
+trainData = np.empty([nTrain, ny, nx, 3])
+
+# iterate over data to construct full input data
+for i, path in enumerate(input_data_paths):
+    img = Image.open(input_data_dir + "\\" + path).resize((nx, ny))
+    trainData[i] = np.asarray(img) / 255.0
+
+nTypes = 2
+trainLabels = np.empty([nTrain, ny, nx, nTypes])
+
+# iterate over data to construct full input labels
+for i, path in enumerate(gt_data_paths):
+    img = Image.open(gt_data_dir + "\\" + path).resize((nx, ny))
+    trainLabels[i] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
+
