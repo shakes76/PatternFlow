2a9abf5441f4dd9be273113bbda1549fa9066790
Jiayu Zhou <s4547877@labs.eait.uq.edu.au>
2020-11-05 16:26:03 +1000

    dice coefficent

---
+++ recognition/76-report/dice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import keras.backend as K
+
+
+# In[ ]:
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f) + K.sum(y_pred_f)
+    return (2. * intersection + smooth) / (union + smooth)  
+
