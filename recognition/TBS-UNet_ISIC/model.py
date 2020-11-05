bc2c05b29c69cfed0f91ca04e68148bf9227cdba
Tristan Samson <s4397119@labs.eait.uq.edu.au>
2020-11-05 13:50:03 +1000

    Implemented and trained basic U-net model.
    - Trained model on a subset of provided data for testing purposes.
    - Gets good accuracy and faithful segmentations on sample images.
    - Decided to bitch it and just do the basic U-net (easy). Might try
        the improved U-Net tomorrow if I have time.

--- recognition/TBS-UNet_ISIC/model.py
+++ recognition/TBS-UNet_ISIC/model.py
@@ -1,15 +1,50 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
-input_layer = layers.Input((256, 192, 3))
+""" This part goes in the driver script """
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Get filepaths
+input_data_dir = "data\\reduced\\ISIC2018_Task1-2_Training_Input_x2"
+gt_data_dir = "data\\reduced\\ISIC2018_Task1_Training_GroundTruth_x2"
 
-"""
+input_data_paths = os.listdir(input_data_dir)
+gt_data_paths = os.listdir(gt_data_dir)
+
+if(len(input_data_paths) != len(gt_data_paths)):
+    print("Mismatch in data / labels")
+
+nTrain = len(input_data_paths)
+
+testImg = Image.open(input_data_dir + "\\" + input_data_paths[0])
+nx = 256
+ny = 192
+trainData = np.empty([nTrain, ny, nx, 3])
+
+# iterate over data to construct full input data
+for i, path in enumerate(input_data_paths):
+    img = Image.open(input_data_dir + "\\" + path).resize((nx, ny))
+    trainData[i] = np.asarray(img) / 255.0
+
+nTypes = 2
+trainLabels = np.empty([nTrain, ny, nx, nTypes])
+
+# iterate over data to construct full input labels
+for i, path in enumerate(gt_data_paths):
+    img = Image.open(gt_data_dir + "\\" + path).resize((nx, ny))
+    trainLabels[i] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
+#input_layer = layers.Input((256, 192, 3))
+
+""" This part goes in the model script"""
+
 ### STANDARD U-NET
 # down path
 
 # down-level 1
-input_layer = layers.Input((256,192,3))
+input_layer = layers.Input((192,256,3))
 conv_1 = layers.Conv2D(64, (3,3), activation='relu', padding='same')(input_layer)
 conv_2 = layers.Conv2D(64, (3,3), activation='relu', padding='same')(conv_1)
 mPool_1 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(conv_2)
@@ -57,8 +92,26 @@ uconv_4 = layers.Conv2DTranspose(64, (2, 2), strides=(2,2), padding='same')(conv
 cat_4 = layers.Concatenate()([uconv_4, conv_2])
 conv_17 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(cat_4)
 conv_18 = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(conv_17)
-conv_19 = layers.Conv2D(4, (1, 1), activation='softmax', padding='same')(conv_18)
+conv_19 = layers.Conv2D(2, (1, 1), activation='softmax', padding='same')(conv_18)
 
 unet = tf.keras.Model(inputs=input_layer, outputs=conv_19)
 unet.compile(optimizer='adam', loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False), metrics=['accuracy'])
-print(unet.summary())"""
+print(unet.summary())
+
+h = unet.fit(trainData, trainLabels, batch_size=16, epochs=10, validation_split=0.2)
+plt.plot(h.history['accuracy'])
+plt.plot(h.history['val_accuracy'])
+
+samplePrediction_1 = unet.predict(trainData[0:1])
+
+plt.figure()
+
+plt.imshow(trainData[0])
+plt.figure()
+print(samplePrediction_1[0,:,:,:].shape)
+plt.imshow(samplePrediction_1[0,:,:,0])
+plt.figure()
+plt.imshow(samplePrediction_1[0,:,:,1])
+plt.figure()
+plt.imshow(samplePrediction_1[0,:,:,:].argmax(axis=2))
+plt.show()
