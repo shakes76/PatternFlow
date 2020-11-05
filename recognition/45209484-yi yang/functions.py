d2e38a17edcf006304022c2a6ea8135465912d54
Yi0724 <73456187+Yi0724@users.noreply.github.com>
2020-11-06 01:22:02 +1000

    add dice function and plot image function

--- recognition/45209484-yi yang/functions.py
+++ recognition/45209484-yi yang/functions.py
@@ -33,3 +33,23 @@ def fit(model,x,y, epoch_size, batch):
 
     model.fit(x, y, epochs=epoch_size, batch_size=batch,
                     validation_split=0.2)
+
+# calculate dice coefficient
+def dice_coef(y_true, y_pred, smooth=1.):
+    y_true = tf.convert_to_tensor(y_true, dtype='float32')
+    y_pred = tf.convert_to_tensor(y_pred, dtype='float32')
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+# display results
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+    title= ['Input Image', 'True Mask', 'Predicted Mask']
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
