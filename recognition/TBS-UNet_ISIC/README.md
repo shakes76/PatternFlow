4905e519fcbe90872edd6b2beb2f64f6b5b97754
Tristan Samson <s4397119@labs.eait.uq.edu.au>
2020-11-05 21:53:08 +1000

    Splitting input data into train/val/test portions.
    - Also set up the basic structure of the log file.

--- recognition/TBS-UNet_ISIC/README.md
+++ recognition/TBS-UNet_ISIC/README.md
@@ -1,4 +1,14 @@
-# Segmentation of ISIC data set with Improved UNet
+# Segmentation of ISIC data set with U-Net
 COMP3710 Final Report Task
 
 Tristan Samson
+
+## Algorithm Description
+
+
+
+## Learning Behaviour
+
+
+
+## Example Outputs
--- recognition/TBS-UNet_ISIC/model.py
+++ recognition/TBS-UNet_ISIC/model.py
@@ -6,10 +6,11 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 # Get filepaths
-input_data_dir = "data\\reduced\\ISIC2018_Task1-2_Training_Input_x2"
-gt_data_dir = "data\\reduced\\ISIC2018_Task1_Training_GroundTruth_x2"
+input_data_dir = "data\\ISIC2018_Task1-2_Training_Input_x2"
+gt_data_dir = "data\\ISIC2018_Task1_Training_GroundTruth_x2"
 
 input_data_paths = os.listdir(input_data_dir)
 gt_data_paths = os.listdir(gt_data_dir)
@@ -17,26 +18,60 @@ gt_data_paths = os.listdir(gt_data_dir)
 if(len(input_data_paths) != len(gt_data_paths)):
     print("Mismatch in data / labels")
 
-nTrain = len(input_data_paths)
+nData = len(input_data_paths)
+trainProp = 0.85
+valProp = 0.1
+testProp = 0.05
+nTrain = math.floor(trainProp * nData)
+nVal = math.floor(valProp * nData)
+nTest = nData - nTrain - nVal
 
-testImg = Image.open(input_data_dir + "\\" + input_data_paths[0])
-nx = 256
-ny = 192
+print(nData, " total samples")
+print("Training on ", nTrain, " samples")
+print("Validating on ", nVal, " samples")
+print("Testing on ", nTest, " samples")
+
+
+# make dimensions as divisible by powers of two as possible while maintaining the aspect ratio of the input
+# (512 = 2^9, 384 = 3*2^7)
+nx = 512
+ny = 384
+
 trainData = np.empty([nTrain, ny, nx, 3])
+valData = np.empty([nVal, ny, nx, 3])
+testData = np.empty([nTest, ny, nx, 3])
+
 
 # iterate over data to construct full input data
 for i, path in enumerate(input_data_paths):
+    if i % 200 == 0:
+        print("loading input ", i)
     img = Image.open(input_data_dir + "\\" + path).resize((nx, ny))
-    trainData[i] = np.asarray(img) / 255.0
+    # split data into the 3 data sets - done this way so we have no excess mem allocation
+    if i < nTrain:
+        trainData[i] = np.asarray(img) / 255.0
+    elif i >= nTrain and i < nTrain + nVal:
+        valData[i - nTrain] = np.asarray(img) / 255.0
+    else:
+        testData[i - nTrain - nVal] = np.asarray(img) / 255.0
+
 
 nTypes = 2
 trainLabels = np.empty([nTrain, ny, nx, nTypes])
+valLabels = np.empty([nVal, ny, nx, nTypes])
+testLabels = np.empty([nTest, ny, nx, nTypes])
 
 # iterate over data to construct full input labels
 for i, path in enumerate(gt_data_paths):
+    if i % 200 == 0:
+        print("loading labels ", i)
     img = Image.open(gt_data_dir + "\\" + path).resize((nx, ny))
-    trainLabels[i] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
-#input_layer = layers.Input((256, 192, 3))
+    if i < nTrain:
+        trainLabels[i] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
+    elif i >= nTrain and i < nTrain + nVal:
+        valLabels[i - nTrain] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
+    else:
+        testLabels[i - nTrain - nVal] = np.eye(nTypes)[(np.asarray(img) * (1/255)).astype(int)]
 
 """ This part goes in the model script"""
 
@@ -44,7 +79,7 @@ for i, path in enumerate(gt_data_paths):
 # down path
 
 # down-level 1
-input_layer = layers.Input((192,256,3))
+input_layer = layers.Input((384,512,3))
 conv_1 = layers.Conv2D(64, (3,3), activation='relu', padding='same')(input_layer)
 conv_2 = layers.Conv2D(64, (3,3), activation='relu', padding='same')(conv_1)
 mPool_1 = layers.MaxPooling2D(pool_size=(2,2), padding='same')(conv_2)
@@ -98,20 +133,28 @@ unet = tf.keras.Model(inputs=input_layer, outputs=conv_19)
 unet.compile(optimizer='adam', loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False), metrics=['accuracy'])
 print(unet.summary())
 
-h = unet.fit(trainData, trainLabels, batch_size=16, epochs=10, validation_split=0.2)
+h = unet.fit(trainData, trainLabels, validation_data=(valData, valLabels), batch_size=4, epochs=10)
 plt.plot(h.history['accuracy'])
 plt.plot(h.history['val_accuracy'])
 
-samplePrediction_1 = unet.predict(trainData[0:1])
+samplePrediction_1 = unet.predict(trainData[100:101])
+samplePrediction_2 = unet.predict(trainData[200:201])
 
 plt.figure()
 
-plt.imshow(trainData[0])
+plt.imshow(trainData[100])
 plt.figure()
-print(samplePrediction_1[0,:,:,:].shape)
+
 plt.imshow(samplePrediction_1[0,:,:,0])
 plt.figure()
 plt.imshow(samplePrediction_1[0,:,:,1])
 plt.figure()
 plt.imshow(samplePrediction_1[0,:,:,:].argmax(axis=2))
+
+plt.figure()
+plt.imshow(samplePrediction_2[0,:,:,0])
+plt.figure()
+plt.imshow(samplePrediction_2[0,:,:,1])
+plt.figure()
+plt.imshow(samplePrediction_2[0,:,:,:].argmax(axis=2))
 plt.show()
