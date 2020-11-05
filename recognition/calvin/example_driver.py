f11b571745c8caded4a7581edee596b92e095d5f
Calvin-Shaji <calvin.shaji@uqconnect.edu.au>
2020-11-05 15:54:03 +1000

    changes in layout and evaluation - example_driver.py

--- recognition/calvin/example_driver.py
+++ recognition/calvin/example_driver.py
@@ -5,14 +5,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from model import *
 
 
-if __name__ == '__main__':
-    data_dir = 'keras_png_slices_data\\'
-    train_dir = 'keras_png_slices_train'
-    mask_dir = 'keras_png_slices_seg_train'
-
-    # train model
-    model = train_model(data_dir, train_dir, mask_dir)
-
+def evaluate(data_dir, valid_dir, seg_valid_dir):
     # image data generator parameters
     args = {
         'rotation_range': 0.2,
@@ -25,11 +18,56 @@ if __name__ == '__main__':
         'rescale': 1./255
     }
 
-    # create image data generator for test data (for prediction)
+    # create image data generator for validation data
+    valid_datagen = ImageDataGenerator(**args)
+    valid_generator = valid_datagen.flow_from_directory(
+        data_dir,
+        classes=[valid_dir],
+        class_mode=None,
+        color_mode='grayscale',
+        target_size=(256, 256),
+        batch_size=2,
+        save_to_dir=None,
+        save_prefix='image',
+        seed=1)
+
+    # create image data generator for validation data mask
+    seg_valid_datagen = ImageDataGenerator(**args)
+    seg_valid_generator = seg_valid_datagen.flow_from_directory(
+        data_dir,
+        classes=[seg_valid_dir],
+        class_mode=None,
+        color_mode='grayscale',
+        target_size=(256, 256),
+        batch_size=2,
+        save_to_dir=None,
+        save_prefix='image',
+        seed=1)
+
+    # create validation generator object
+    validf_generator = zip(valid_generator, seg_valid_generator)
+
+    # evaluate model on test data
+    model.evaluate(validf_generator, steps=1120)
+
+def predict(data_dir, test_dir, results_dir):
+    # image data generator parameters
+    args = {
+        'rotation_range': 0.2,
+        'width_shift_range': 0.05,
+        'height_shift_range': 0.05,
+        'shear_range': 0.05,
+        'zoom_range': 0.05,
+        'horizontal_flip': True,
+        'fill_mode': 'nearest',
+        'rescale': 1./255
+    }
+
+    # create image data generator for test data
     test_datagen = ImageDataGenerator(**args)
     test_generator = test_datagen.flow_from_directory(
-        'keras_png_slices_data\\',
-        classes=['keras_png_slices_test'],
+        data_dir,
+        classes=[test_dir],
         class_mode=None,
         color_mode='grayscale',
         target_size=(256, 256),
@@ -44,36 +82,26 @@ if __name__ == '__main__':
     # save prediction results
     for i, item in enumerate(results):
         img = item[:, :, 0]
-        io.imsave(os.path.join('keras_png_slices_data\\results\\', "%d_predict.png" % i), img_as_ubyte(img))
+        io.imsave(os.path.join(f'{data_dir}{results_dir}', '%d_predict.png' % i), img_as_ubyte(img))
 
-    # create image data generator for predicted results (for evaluation)
-    test_datagen = ImageDataGenerator(**args)
-    test_generator = test_datagen.flow_from_directory(
-        'keras_png_slices_data\\',
-        classes=['keras_png_slices_test'],
-        class_mode=None,
-        color_mode='grayscale',
-        target_size=(256, 256),
-        batch_size=2,
-        save_to_dir=None,
-        save_prefix='image',
-        seed=1)
 
-    # create image data generator for actual results (for evaluation) 
-    seg_test_datagen = ImageDataGenerator(**args)
-    seg_test_generator = seg_test_datagen.flow_from_directory(
-        'keras_png_slices_data\\',
-        classes=['keras_png_slices_seg_test'],
-        class_mode=None,
-        color_mode='grayscale',
-        target_size=(256, 256),
-        batch_size=2,
-        save_to_dir=None,
-        save_prefix='image',
-        seed=1)
+if __name__ == '__main__':
+    data_directory = 'keras_png_slices_data\\'
+    
+    train_directory = 'keras_png_slices_train'
+    seg_train_directory = 'keras_png_slices_seg_train'
+    valid_directory = 'keras_png_slices_validate'
+    seg_valid_directory = 'keras_png_slices_seg_validate'
+    test_directory = 'keras_png_slices_test'
+    seg_test_directory = 'keras_png_slices_seg_test'
 
-    # create test generator object
-    testf_generator = zip(test_generator, seg_test_generator)
+    results_directory = 'results\\'
 
-    # evaluate model on test data
-    model.evaluate(testf_generator, steps=544)
+    # train model
+    model = train_model(data_directory, train_directory, seg_train_directory)
+
+    # evaluate model on validation data
+    evaluate(data_directory, valid_directory, seg_valid_directory)
+
+    # predict on test data
+    predict(data_directory, test_directory, results_directory)
