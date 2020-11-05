1fefc8cbdd7ef621b5c38f1f218e42389b6c3cee
Jiayu Zhou <s4547877@labs.eait.uq.edu.au>
2020-11-05 17:08:47 +1000

    add comments

--- recognition/76-report/load.py
+++ recognition/76-report/load.py
@@ -17,11 +17,15 @@ from sklearn.model_selection import train_test_split
 
 def loaddata(input_PATH,input_ids,truth_PATH,truth_ids):
     """
-    
+    load data, resize to 256,256
+    split data set
+    normalize data
+    set images data to 0-1 label
+    return; three dataset and their x and y
     
     """
     image_size=[256,256]
-    
+    # data box to contain data later
     X_input = np.zeros((len(input_ids), image_size[0],image_size[1],3), dtype=np.uint8)
     Y_input = np.zeros((len(input_ids), image_size[0],image_size[1],1), dtype=np.uint8)
     sys.stdout.flush()
@@ -36,7 +40,7 @@ def loaddata(input_PATH,input_ids,truth_PATH,truth_ids):
         label = resize(label, (image_size[0], image_size[1]), mode='constant', preserve_range=True)
         label = np.expand_dims(label,-1)
         Y_input[n] = label
-        
+    #split dataset    
     X_train, X_test, y_train,y_test = train_test_split(X_input,Y_input, test_size=0.10)
     X_train, X_val, y_train,y_val = train_test_split(X_train,y_train, test_size=0.20)
     
@@ -46,7 +50,7 @@ def loaddata(input_PATH,input_ids,truth_PATH,truth_ids):
     y_train=y_train/255
     y_val=y_val/255
     y_test=y_test/255
-
+    # label data
     y_train[y_train>0.5]=1
     y_train[y_train<=0.5]=0
     y_val[y_val>0.5]=1
