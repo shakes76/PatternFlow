e96a07f164e1ad909dade7e77aab4fa019692692
Jaydon Hansen <jaydon.hansen@gmail.com>
2020-11-06 13:18:31 +1000

    Fixed file paths for training script

--- recognition/Jaydon-ISIC-UNet/train.py
+++ recognition/Jaydon-ISIC-UNet/train.py
@@ -6,15 +6,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from model import UNet
-from training_utils import DataGenerator
+from training_utils import *
 
-# Generate a test UNet model to check if it compiles correctly
 img_size = 128
 img_channels = 3
-batch_size = 20
+batch_size = 32
 
-base_path = "./data/ISIC2018_Task1-2_Training_Data/"
-image_path = "./data/ISIC2018_Task1-2_Training_Data/images"
+base_path = "/content/data/images"
+image_path = "/content/data/images/ISIC2018_Task1-2_Training_Input_x2"
 
 training_ids = []
 for file in os.walk(image_path):
@@ -24,6 +23,7 @@ for file in os.walk(image_path):
 # Get filenames and take file extensions off to leave IDs
 training_ids = training_ids[2]
 training_ids = [filename.split('.',1)[0] for filename in training_ids]
+print(training_ids)
 
 # Split into training, test and validation data
 validation_size = floor(len(training_ids) * 0.2)
@@ -32,17 +32,17 @@ test_size = floor(validation_size * 0.5)
 # Split data into test, train validation
 validation_test_ids = training_ids[:validation_size]
 test_ids = validation_test_ids[:test_size]
-validation_ids = validation_test_ids[test_size]
+validation_ids = validation_test_ids[test_size:]
 training_ids = training_ids[validation_size:]
 
-print("Total images: = ", len(test_ids + training_ids + validation_ids))
+#print("Total images: = ", len(test_ids + training_ids + validation_ids))
 print("Training images: ", len(training_ids))
 print("Testing images: ", len(test_ids))
 print("Validation images: ", len(validation_ids))
 
 
-# Instantiate a Generator
-gen = DataGenerator(training_ids, base_path, batch_size=batch_size, img_size=img_size)
+# Test generator
+gen = DataGenerator(training_ids, base_path, batch_size=batch_size, image_size=img_size)
 
 # Test the Generator
 testX, testY = gen.__getitem__(0)
@@ -69,5 +69,5 @@ validation_gen = DataGenerator(validation_ids, base_path, 3, 128)
 training_step_size = len(training_ids) // batch_size # Step size of training data
 validation_step_size = len(validation_ids) // batch_size # Step size of validation data
 
-h = model.fit(training_gen, validation_data = validation_gen, steps_per_epoch=training_step_size, validation_steps=validation_step_size, epochs = 20)
+h = model.fit_generator(training_gen, validation_data = validation_gen, steps_per_epoch=training_step_size, validation_steps=validation_step_size, epochs = 5)
 model.save("ISIC-UNet.h5")
