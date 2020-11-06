adb41e60fe63c3567e346b36d0e112fc25b3a066
Neal_45539873 <48573607+w895540347@users.noreply.github.com>
2020-11-06 21:57:08 +1000

    compile and train model

--- recognition/s4553987 Segment the ISICs data set with the Improved UNet/main.py
+++ recognition/s4553987 Segment the ISICs data set with the Improved UNet/main.py
@@ -1,6 +1,12 @@
 import cv2
 import glob
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import numpy as np
+import tensorflow as tf
+from random import shuffle, seed
+from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, \
+    Dropout, Input, concatenate, Add, UpSampling2D, Conv2DTranspose
 
 inputs = [cv2.imread(file) for file in glob.glob('Downloads/ISIC2018_Task1-2_Training_Input_x2/*.jpg')]
 outputs = [cv2.imread(file, cv2.IMREAD_GRAYSCALE) for file in glob.glob('Downloads/ISIC2018_Task1_Training_GroundTruth_x2/*.png')]
@@ -32,3 +38,25 @@ X_test = X[2197:2594,:,:,:]
 y_train = y[0:1800,:,:,:]
 y_val = y[1800:2197,:,:,:]
 y_test = y[2197:2594,:,:,:]
+
+model = improved_unet(256)
+
+#### Compile Model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dsc, 'accuracy'])
+
+
+model_callback=tf.keras.callbacks.ModelCheckpoint(filepath='best_checkpoint',
+                                              save_best_only= True,
+                                              save_weights_only=True,
+                                              monitor = 'val_accuracy',
+                                              mode='max')
+
+history = model.fit(x = X_train, y=y_train, epochs=30, verbose=1,
+                    validation_data=(X_val, y_val), batch_size = 8, callbacks=[model_callback])
+
+def dsc(y_true, y_pred):
+    intersection = tf.reduce_sum(tf.math.multiply(y_true, y_pred))
+    dsc = 2*intersection / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred))
+    return dsc
+
+
