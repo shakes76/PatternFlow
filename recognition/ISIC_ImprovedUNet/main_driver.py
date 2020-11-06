002d585e0a24e4520f20318c09ff7f895952bf21
Marko <marko.uksanovic1@gmail.com>
2020-11-06 18:36:19 +1000

    Created dice_similarity function

--- recognition/ISIC_ImprovedUNet/main_driver.py
+++ recognition/ISIC_ImprovedUNet/main_driver.py
@@ -12,6 +12,9 @@ path_data = "C:/Users/Marko/Desktop/data"
 path_labels = "C:/Users/Marko/Desktop/masks"
 img_size = 256
 
+training_images = sorted(glob.glob(path_data + '/*.jpg'))
+training_masks = sorted(glob.glob(path_labels + '/*.png'))
+
 def convert(path):
     x = tf.io.read_file(path)
     x = tf.image.decode_png(x, channels=1)
@@ -35,8 +38,18 @@ def prepare_data(X, Y):
 
     return data
 
-training_images = sorted(glob.glob(path_data + '/*.jpg'))
-training_masks = sorted(glob.glob(path_labels + '/*.png'))
+def dice_similarity(true, prediction):
+    X = tf.keras.layers.Flatten()(true)
+    Y = tf.keras.layers.Flatten()(prediction)
+
+    num = (2.0 * tf.math.reduce_sum(X * Y))
+    denom = tf.math.reduce_sum(X) + tf.math.reduce_sum(Y)
+
+    coefficient = tf.math.reduce_mean(num / denom)
+
+    return coefficient
+
+dice_sim_coeff = dice_similarity
 
 X_train_valid, X_test, Y_train_valid, Y_test = train_test_split(training_images, training_masks, test_size = 0.2, random_state=7)
 X_train, X_valid, Y_train, Y_valid = train_test_split(X_train_valid, Y_train_valid, test_size = 0.15, random_state=7)
@@ -46,5 +59,7 @@ Valid = prepare_data(X_train_valid, Y_train_valid)
 Test = prepare_data(X_test, Y_test)
 
 model = unet()
-model.compile(optimizer = keras.optimizers.Adam(lr = 0.0001), loss = 'categorical_crossentropy', metrics=dice_coef)
-model.fit(Train.batch(16), epochs=10, validation_data = Valid.batch(16))
+model.compile(optimizer = keras.optimizers.Adam(lr = 0.0001), loss = 'binary_crossentropy', metrics=dice_sim_coeff)
+model.fit(Train.batch(16), epochs=5, validation_data = Valid.batch(16))
+
+model.evaluate(Test)
