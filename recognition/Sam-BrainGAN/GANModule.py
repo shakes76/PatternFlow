ebbf2e900150775911ba39a248a61db06d3f84bb
Samuel Nolan <abcs.nolan@live.com>
2020-11-06 20:21:00 +1000

    Added the train_step function which performs a training iteration of each batch.

--- recognition/Sam-BrainGAN/GANModule.py
+++ recognition/Sam-BrainGAN/GANModule.py
@@ -87,3 +87,28 @@ def generator_loss(fake_output):
     return cross_entropy(tf.ones_like(fake_output * smooth), fake_output)
 
 
+
+# Notice the use of `tf.function`
+# This annotation causes the function to be "compiled".
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+    
+
+
+
