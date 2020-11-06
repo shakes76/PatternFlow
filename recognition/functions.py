02b2193e6761317101e07fe647652e5040579bba
pshivaji <s.parala@uqconnect.edu.au>
2020-11-06 19:42:56 +1000

    training on 5 epochs to check model working or not from results it looks like model is working fine

--- recognition/functions.py
+++ recognition/functions.py
@@ -34,27 +34,26 @@ def image_data(img_dir):
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(8*8*512, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
 
-    model.add(layers.Reshape((7, 7, 256)))
+    model.add(layers.Reshape((8, 8, 512)))
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    model.add(layers.Conv2DTranspose(256, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
 
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+    
     model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     
-    model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    
-    
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    # model.output_shape == (None, 56, 56, 1)
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(4, 4), padding='same', use_bias=False, activation='tanh'))
+    # model.output_shape == (None, 128, 128, 1)
 
     return model
 
@@ -65,12 +64,12 @@ def make_generator_model():
 def make_discriminator_model():
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[56, 56, 1]))
-    model.add(layers.LeakyReLU())
+                                     input_shape=[128, 128, 1]))
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.2))
 
     model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.2))
 
     model.add(layers.Flatten())
@@ -89,6 +88,7 @@ def make_discriminator_model():
 
 
 def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
@@ -105,6 +105,7 @@ def discriminator_loss(real_output, fake_output):
 
 
 def generator_loss(fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     return cross_entropy(tf.ones_like(fake_output), fake_output)
 
 
@@ -116,18 +117,19 @@ def generator_loss(fake_output):
 
 # In[9]:
 
-
+@tf.function
 def train_step(images):
+    BATCH_SIZE = 128
+    noise_dim = 100
     noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
+        generated_images = generator(noise, training=True)
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
 
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
-
-      gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
 
     gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
     gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
@@ -140,27 +142,27 @@ def train_step(images):
 
 
 def train(dataset, epochs):
-  for epoch in range(epochs):
-    start = time.time()
+    for epoch in range(epochs):
+        start = time.time()
 
-    for image_batch in dataset:
-      train_step(image_batch)
+        for image_batch in dataset:
+        train_step(image_batch)
 
-    # Produce images for the GIF as we go
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
+        # Produce images 
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
                              epoch + 1,
                              seed)
 
-    # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
-      checkpoint.save(file_prefix = checkpoint_prefix)
+       # Save the model every 20 epochs
+        if (epoch + 1) % 20 == 0:
+        checkpoint.save(file_prefix = checkpoint_prefix)
 
-    print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+        print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
 
-  # Generate after the final epoch
-  display.clear_output(wait=True)
-  generate_and_save_images(generator,
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator,
                            epochs,
                            seed)
 
@@ -186,7 +188,7 @@ def generate_and_save_images(model, epoch, test_input):
       plt.imshow(predictions[i, :, :, 0], cmap='gray')
       plt.axis('off')
 
-  #plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
   plt.show()
 
 
