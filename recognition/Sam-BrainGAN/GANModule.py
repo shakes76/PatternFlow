9f94b507b1215b6a2115a065ecad3c0721267943
Samuel Nolan <abcs.nolan@live.com>
2020-11-06 19:19:48 +1000

    Added generator and discriminator models to the GANModule. Added generator and discriminator test output to make sure it works.

---
+++ recognition/Sam-BrainGAN/GANModule.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+tf.__version__
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+from tensorflow.keras import layers
+
+
+BATCH_SIZE = 4
+noise_dim = 100
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(4 * 4 * 1024, use_bias=False, input_shape=(noise_dim,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((4, 4, 1024)))
+
+    model.add(layers.Conv2DTranspose(512, (1, 1), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(256, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+
+    return model
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=[256, 256, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+generator = make_generator_model()
+
+noise = tf.random.normal([1, noise_dim])
+generated_image = generator(noise, training=False)
+
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print(decision)
