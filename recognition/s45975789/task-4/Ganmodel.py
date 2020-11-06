0828db259a7d60b5a43addc72c1be7eb4bf978fd
Pragatheeswari Velraj <s4597578@labs.eait.uq.edu.au>
2020-11-06 11:12:33 +1000

    Added feature to plot training loss and made updates related to that.

--- recognition/s45975789/task-4/Ganmodel.py
+++ recognition/s45975789/task-4/Ganmodel.py
@@ -13,73 +13,65 @@ def make_generator_model():
     model.add(layers.ReLU())
 
     model.add(layers.Reshape((16, 16, 256)))
-    #assert model.output_shape == (None, 28, 28, 256) # Note: None is the batch size
-
+    
     model.add(layers.Conv2DTranspose(256, (3, 3), strides=(1, 1), padding='same'))
-    #assert model.output_shape == (None, 16, 16, 256)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
 
     model.add(layers.Conv2DTranspose(128, (3, 3), strides=(2, 2), padding='same'))
-    #assert model.output_shape == (None, 32, 32, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     
     model.add(layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), padding='same'))
-    #assert model.output_shape == (None, 64, 64,64)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     
     model.add(layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), padding='same'))
-    #assert model.output_shape == (None, 128, 128, 32)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     
     model.add(layers.Conv2DTranspose(16, (3, 3), strides=(2, 2), padding='same'))
-    #assert model.output_shape == (None, 256, 256, 16)
     model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     
     model.add(layers.Conv2D(1, (1, 1), activation='tanh'))
     assert model.output_shape == (None, 256, 256, 1)
-    noise = Input(shape=(100,)) 
-    image = model(noise) 
+
   
-    return Model(noise, image) 
+    return model 
 
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(16, (5, 5), strides=(2, 2), padding='same',
                                      input_shape=[256, 256, 1]))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
     
     model.add(layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
     
     model.add(layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
     
     model.add(layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
 
     model.add(layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
     
     model.add(layers.Conv2D(512, (3, 3), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
+    model.add(layers.LeakyReLU(alpha=0.2))
     model.add(layers.Dropout(0.3))
     
     model.add(layers.Flatten())
     model.add(layers.Dense(1))
     model.add(Activation('sigmoid'))
-    #model.add(Activaion('softmax'))
-    image = Input(shape=[256, 256, 1]) 
-    v = model(image) 
-  
-    return Model(image, v) 
+    
+    model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(0.0002,0.5),metrics=['accuracy'])
+      
+    return model 
