b5e8cb24f15e84374b31670d9f4516a337aaf298
Lewis Ainslie <s4485827@labs.eait.uq.edu.au>
2020-11-06 12:41:48 +1000

    changes with model and loading in. still not producing good results

--- recognition/problem6/load_data.py
+++ recognition/problem6/load_data.py
@@ -9,6 +9,7 @@ def load_paths(directory):
     for filename in sorted(os.listdir(directory)):  
         path = os.path.join(directory,filename)
         training_paths[path] = 1
+    
         
       
     return sorted(training_paths.keys())
@@ -17,6 +18,8 @@ def load_paths(directory):
 def load_images(filename):
     img = tf.io.read_file(filename)
     img = tf.io.decode_png(img, channels = 3)
-    img = tf.image.resize(img, size = (256, 256))
-    img = img / 255.0
+    #img = tf.image.resize(img, size = (256, 256))
+    img = tf.dtypes.cast(img, tf.float32)
+    img = (img - 127.5) / 127.5
+    #img = img.numpy().astype('float32')
     return img
--- recognition/problem6/model.py
+++ recognition/problem6/model.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.layers import UpSampling2D, Conv2D
 from tensorflow.keras.models import Sequential, Model, load_model
 from tensorflow.keras.optimizers import Adam
-
+import tensorflow as tf
 tfk2 = tf.keras.layers
 
 
@@ -55,10 +55,55 @@ def build_generator(input_shape):
     t = tfk2.Conv2D(64, 3, padding = "same", activation = tf.nn.leaky_relu)(t)
 
     t = tfk2.UpSampling2D()(t)
-    t = tfk2.Conv2D(3, 3, padding = "same", activation = "tanh")(t)
+    t = tfk2.Conv2D(3, 3, padding = "same", activation = tf.nn.leaky_relu)(t)
 
     t = tfk2.UpSampling2D()(t)
-    t = tfk2.Conv2D(3, 3, padding = "same", activation = "tanh")(t)
+    t = tfk2.Conv2D(3, 3, padding = "same", activation = 'tanh')(t)
+    #t = tfk2.Conv2D(3, 3, padding = "same")(t)
     model = tf.keras.Model(inputs = input_layer, outputs = t)
     model.summary()
     return model
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=False)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+
+def train_step(data, disc, gen, opt1, opt2):
+    
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        seed = tf.random.normal([32, 100])
+        gen_image = gen(seed, training=True)
+        #gen_image = (gen_image[0].numpy() + 1.0)*0.5
+        #gen_image = tf.reshape(gen_image, (1,256,256,3))
+        #gen_image = (gen_image + 1.0)*0.5
+        
+        #print(results)
+
+        real_output = disc(data, training=True)
+        fake_output = disc(gen_image, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(\
+            gen_loss, gen.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(\
+            disc_loss, disc.trainable_variables)
+
+        opt1.apply_gradients(zip(
+            gradients_of_generator, gen.trainable_variables))
+        opt2.apply_gradients(zip(
+            gradients_of_discriminator, 
+            disc.trainable_variables))
+
+        return gen_image
