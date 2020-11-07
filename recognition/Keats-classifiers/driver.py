e8400edc5c15e93431cae72fd8a48fdb586e1976
Harry Keightley <harrykeightley@outlook.com>
2020-11-08 02:23:11 +1000

    Segmented assignment python file into models and driver and added documentation

--- recognition/Keats-classifiers/driver.py
+++ recognition/Keats-classifiers/driver.py
@@ -1,7 +1,121 @@
+import argparse
+import tensorflow as tf
+import os
+from models import KneeClassifier
+
+def get_label(file_path):
+    """ Get the data label corresponding to the file path 
+
+    Parameters:
+        file_path (str): The file (image) to look at
+    
+    Returns:
+        A tensor with datatype float32 which is 1.0 if the image
+        is of the right knee and 0.0 otherwise.
+    """
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # Extract LEFT/RIGHT data from filename
+    parts = tf.strings.split(parts[-1], '.')
+    parts = tf.strings.split(parts[0], '_')
+    pattern = tf.constant("right")
+    
+    is_right = pattern == tf.strings.lower(parts[-1])
+    # convert from bool to float
+    return tf.cast(is_right, dtype='float32')
+
+def decode_img(img):
+    """ Attempts to get a 3D tensor corresponding to the image data
+
+    Parameters:
+        img (0-D string): The encoded image bytes
+
+    Returns:
+        3D grayscale image tensor with values normalised between 0.0 and 1.0
+    """
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_png(img, channels=1)
+    img = tf.cast(img, dtype='float32')
+    # cast and scale the tensor
+    img = tf.math.truediv(img, tf.constant(255.0))
+    
+    return img #tf.image.resize(img, [HEIGHT, WIDTH])
+
+def process_path(file_path):
+    """ Map function to be applied to dataset, 
+    returns image and label for a filepath 
+    
+    Parameters:
+        file_path (str): The filepath to generate data for
+
+    Returns:
+        (tuple<tf.Tensor, tf.Tensor>) A tuple of tensors for the image and label
+    """
+    label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+def configure_for_performance(dataset, batch_size=32):
+    """ Process the dataset to make it efficient to learn with. For proper learning
+    apparently its important to have quick access to batches, shuffle your data and have it 
+    cached. I just do what's recommended on the tf site.
+    """
+    dataset = dataset.cache()
+    dataset = dataset.shuffle(buffer_size=1000)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    return dataset
+        
+# Data processing
+def load_knee_data(path):
+    dataset = tf.data.Dataset.list_files(f"{path}/*.png")
+    
+    # Split the data into training and validation sets
+    data_count = len(os.listdir(path))
+    VALIDATION_SPLIT = 0.2
+    VALIDATION_SIZE = int(VALIDATION_SPLIT * data_count)
+
+    training_data = dataset.skip(VALIDATION_SIZE)
+    testing_data = dataset.take(VALIDATION_SIZE)
+
+    # See the size of our sets
+    print("Training set cardinality:", tf.data.experimental.cardinality(training_data).numpy())
+    print("Validation set cardinality:", tf.data.experimental.cardinality(testing_data).numpy())
+    
+    # map the filenames to image/label pairs for learning
+    training_data = training_data.map(process_path, 
+                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    testing_data = testing_data.map(process_path, 
+                                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    
+    # configure the datasets to make them efficient to learn with
+    training_data = configure_for_performance(training_data)
+    testing_data = configure_for_performance(testing_data)
+    
+    print("Training set cardinality:", tf.data.experimental.cardinality(training_data).numpy())
+    print("Validation set cardinality:", tf.data.experimental.cardinality(testing_data).numpy())
+
+    return training_data, testing_data
 
 def main():
-    pass
+    # Commandline argument parsing
+    parser = argparse.ArgumentParser(description='Create a ConvNet for determining OAI AKOA Knee laterality')
+    parser.add_argument('data_directory', help='The directory containing the extracted OAI AKOA data')
+    parser.add_argument('--relearn', help='If this flag is specified, relearn the weights on the network', action='store_true')
+    args = parser.parse_args()
 
+    # load and preprocess the training data
+    training_data, testing_data = load_knee_data(args.data_directory)
 
+    # Create and compile the classifier. Will train from scratch if specified
+    classifier = KneeClassifier(args.relearn, training_data, testing_data)
+    classifier.show_results(3, 3)
+
+    loss, accuracy = classifier.get_model().evaluate(testing_data, verbose=2)
+    print("Model accuracy on validation data: {:5.2f}%".format(100 * accuracy))
+
+   
 if __name__ == '__main__':
     main()
--- recognition/Keats-classifiers/models.py
+++ recognition/Keats-classifiers/models.py
@@ -1,21 +1,99 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-def generate_unet(shape, data):
-    pass
+import os
+from PIL import Image
 
-def generate_knee_model(data):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Input(shape=(32,)), # wrong,
-        tf.keras.layers.Conv2D(32, filter_size=3, activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2),
+class KneeClassifier(object):
+    """ A class which generates, compiles and learns weightings for a ConvNet to classify Knee Data """
 
-        tf.keras.layers.Conv2D(64, filter_size=3, activation='relu'),
-        tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2),
+    HEIGHT, WIDTH = 228, 260
+    CHECKPOINT = './checkpoints/result'
 
+    def __init__(self, relearn, training_data, testing_data):
+        """ Constructs the KneeClassifier
+
+        Parameters:
+            relearn (bool): If we should relearn the weights from scratch
+            training_data (tf.data.Dataset): The training data
+            testing_data (tf.data.Dataset): The validation set
+        """
+        self._training_data = training_data
+        self._testing_data = testing_data
+
+        self._generate_model()
+        self._compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+        # only relearn if specified, otherwise load weights from previous learning.
+        if relearn:
+            self._model.fit(
+                training_data,
+                epochs=40,
+                shuffle=True,
+                validation_data=testing_data,
+                #callbacks=[tf.keras.callbacks.TensorBoard()] # never ended up using this
+            )
+
+        else:
+            try:
+                self._model.load_weights(self.CHECKPOINT)
+            except:
+                print("ERROR Loading Weights, Make sure you are calling the script from inside the same directory")
+
+
+    def _generate_model(self):
+        """ Generates the tensorflow model """
+        self._model = tf.keras.Sequential([
+        tf.keras.Input(shape=(self.HEIGHT, self.WIDTH, 1)),
+        
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
+        
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
+        
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
+        
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(1024, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax'), # 10 class classifier
+        tf.keras.layers.Dense(100, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
-    return model
+    def _compile(self, **kwargs):
+        self._model.compile(**kwargs)
+
+    def get_model(self):
+        """ (tf.keras.Model) Return the generated model for some tutory tests"""
+        return self._model
+
+    def show_results(self, images_per_row, rows):
+        """ Shows the result of some random classifications from the validation set 
+        
+        Parameters: 
+            images_per_row (int): How many images to show per row
+            rows (int): How many rows of images to show
+        """
+        for images, labels in self._testing_data.cache().repeat().take(1):
+            # make some model predictions so we can see how our model fares
+            predictions = self._model.predict(images[:images_per_row + 1])
+
+            # added to make the image bigger
+            plt.figure(num=None, figsize=(14, 8), dpi=80, facecolor='w', edgecolor='k')
+
+            for i in range(1, len(predictions)):
+                for j in range(rows):
+                    ax = plt.subplot(3, len(predictions), i + j * len(predictions))
+                    # plot the predicted and true labels of the images, 1 means right knee and 0 means left
+                    ax.set_title(f"p: {round(predictions[i, 0], 1)}, t: {labels[i].numpy()}")
+                    plt.imshow(tf.reshape(images[i + j * len(predictions)], (self.HEIGHT, self.WIDTH)))
+                    plt.gray()
+                    # remove ugly axes
+                    ax.get_xaxis().set_visible(False)
+                    ax.get_yaxis().set_visible(False)
+
+    def save(self, path):
+        self._model.save_weights(path)
+
+
 
