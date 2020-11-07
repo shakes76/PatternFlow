740b47e7f9f1a76266feb113db62e8076e1525db
Yuris Cakranegara <yuris_aryansyah@yahoo.com>
2020-11-07 15:16:06 +1000

    added more script logging and changed SSIM calculation to get the maximum

--- recognition/Yuris Cakranegara-DCGAN/main.py
+++ recognition/Yuris Cakranegara-DCGAN/main.py
@@ -17,51 +17,63 @@ def main():
     img_shape = (256, 256, 1)
 
     # Load dataset
+    print("Loading dataset ...")
     loader = ImageDatasetLoader(train_dataset_dir, img_shape)
     X_train = loader.load_data()
 
     # Reshape and Rescale Images for DCGAN
     # Generator will use tanh activation function for the last layer, 
     # so we want to reshape X_train to be within -1 to 1 limits.
+    print("Preprocessing dataset ...")
     X_train = X_train/255
     X_train = X_train.reshape((-1, 256, 256, 1)) * 2. - 1.
 
     # Generate and store real images for comparison
+    img_dir = result_dir + "real_images.png"
+    print("Storing real images for comparison to: " + img_dir)
     _fig, a = plt.subplots(3,3, figsize=(10,10))
     images = X_train[:9]
     for i in range(len(images)):
         a[i//3][i%3].set_xticks([])
         a[i//3][i%3].set_yticks([])
         a[i//3][i%3].imshow(images[i], cmap="gray")
-    plt.savefig(result_dir + "real_images.png")
+    plt.savefig(img_dir)
 
     # Create and train the model
     model = OasisDCGAN(result_dir=result_dir)
+    print("Training the model ...")
     model.train(batch_size, epochs, X_train)
 
     # Create and store generator loss plot
+    print("Plotting generator loss ...")
     plt.clf()
     plt.title('Generator Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.plot(range(1, epochs+1), model.generator_loss)
-    plt.savefig("generator_loss.png")
+    plt.savefig(result_dir + "generator_loss.png")
 
     # Create and store discriminator loss plot
+    print("Plotting discriminator loss ...")
     plt.clf()
     plt.title('Discriminator Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.plot(range(1, epochs+1), model.discriminator_loss)
-    plt.savefig("discriminator_loss")
+    plt.savefig(result_dir + "discriminator_loss")
 
     # Calculate SSIM
-    noise = tf.random.normal(shape=[1, model.codings_size])
-    img1 = model.generator(noise)[0]
-    img2 = tf.convert_to_tensor(X_train[1])
+    noise = tf.random.normal(shape=[9, 100])
+    images = model.generator(noise)
+    img2 = tf.convert_to_tensor(X_train[0])
     img2 = tf.cast(img2, dtype=tf.float32)
-    ssim = tf.image.ssim(img1, img2, max_val=1)
-    print("Structural Similarity:", ssim)
+
+    max_ssim = 0.0
+    for img1 in images:
+        ssim = tf.image.ssim(img1, img2, max_val=1)
+        if ssim > max_ssim: 
+            max_ssim = ssim
+    tf.print("Structural Similarity:", max_ssim)
 
 if __name__ == "__main__":
     main()
