124791b911d7cae35f4f9f6870e4c7b70a54bad5
zhengzhao li <1020597814@qq.com>
2020-11-07 12:29:33 +1000

    Visualize the predictions

--- recognition/1/test.py
+++ recognition/1/test.py
@@ -24,3 +24,38 @@ history = model.fit_generator(
     validation_data=valGen,
     validation_steps=validate_steps,
 )
+
+# Load the best model.
+model = load_model('best_model.hdf5', compile=True)
+
+# Model
+example_image = "./data/keras_png_slices_test/case_441_slice_0.nii.png"
+example_label = "./data/keras_png_slices_seg_test/seg_441_slice_0.nii.png"
+
+# Read the images.
+img = io.imread(example_image, as_gray=True)
+img = img / 255.0
+img = np.reshape(img, (1,) + img.shape)
+
+# Get the predictions.
+prob_predictions = model.predict(img)
+final_predicitons = np.argmax(prob_predictions, axis=-1)
+
+# Convert the one-hot predictions to image colors.
+predictions = np.zeros(final_predicitons.shape, dtype=np.float32)
+predictions[final_predicitons == 1] = 85.0
+predictions[final_predicitons == 2] = 170.0
+predictions[final_predicitons == 3] = 255.0
+
+# Plot the image/prediction/groundtruth.
+plt.subplot(131)
+plt.imshow(img[0])
+plt.title('Image')
+plt.subplot(132)
+plt.imshow(predictions[0])
+plt.title('Prediction')
+plt.subplot(133)
+label = io.imread(example_label, as_gray=True)
+plt.imshow(label)
+plt.title('Groundtruth')
+plt.show()
