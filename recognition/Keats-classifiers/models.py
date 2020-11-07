68cc867d8814e45c8094ffe5f187b5c1037cb68e
Harry Keightley <s4392444@labs.eait.uq.edu.au>
2020-11-07 18:53:19 +1000

    Finally finished cleaning the intial data

--- recognition/Keats-classifiers/models.py
+++ recognition/Keats-classifiers/models.py
@@ -1,5 +1,21 @@
+import tensorflow as tf
+
 def generate_unet(shape, data):
     pass
 
 def generate_knee_model(data):
-    pass
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Input(shape=(32,)), # wrong,
+        tf.keras.layers.Conv2D(32, filter_size=3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2),
+
+        tf.keras.layers.Conv2D(64, filter_size=3, activation='relu'),
+        tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=2),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax'), # 10 class classifier
+    ])
+
+    return model
+
