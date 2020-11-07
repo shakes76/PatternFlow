bd9853bbde581bea223697959892d65eb0307ea2
zhengzhao li <1020597814@qq.com>
2020-11-07 12:27:47 +1000

    Model training

--- recognition/1/test.py
+++ recognition/1/test.py
@@ -10,3 +10,17 @@ def dice_coef_loss(trainGen,testGen):
   return -dice_coef(trainGen, testGen)
 
 model.compile(optimizer=Adam(lr=1e-4), loss=-dice_coef_loss, metrics=[dice_coef])
+
+# Set the proper checkpoint callback to save model.
+model_checkpoint = ModelCheckpoint(
+    'best_model.hdf5', monitor='loss', verbose=1, save_best_only=True)
+
+# Model training.
+history = model.fit_generator(
+    trainGen,
+    steps_per_epoch=train_steps,
+    epochs=epochs,
+    callbacks=[model_checkpoint],
+    validation_data=valGen,
+    validation_steps=validate_steps,
+)
