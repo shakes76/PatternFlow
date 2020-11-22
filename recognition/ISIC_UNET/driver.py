8787543db3e493a7398902abbf57f000efc2239d
izzygpeters <72848958+izzygpeters@users.noreply.github.com>
2020-11-22 19:01:39 +1000

    Moved driver script logic into __main__ function

--- recognition/ISIC_UNET/driver.py
+++ recognition/ISIC_UNET/driver.py
@@ -22,51 +22,52 @@ from load_images import get_datasets, view_imgs, view_preds
 from model import UNetModel
 from metrics import *
 
-### Loading image set into split datasets ###
+if __name__ == '__main__':
+    ### Loading image set into split datasets ###
 
-# Proportions of image set to use
-total_prop = 1
-val_prop = 0.1
-test_prop = 0.1
+    # Proportions of image set to use
+    total_prop = 1
+    val_prop = 0.1
+    test_prop = 0.1
 
-# Create datasets from images, based on given proportions
-train_ds, val_ds, test_ds = get_datasets(image_root_folder, 
-                                         total_prop, val_prop, test_prop)
+    # Create datasets from images, based on given proportions
+    train_ds, val_ds, test_ds = get_datasets(image_root_folder, 
+                                             total_prop, val_prop, test_prop)
 
-# View some training images and their segmentation maps
-view_imgs(train_ds, 3)
+    # View some training images and their segmentation maps
+    view_imgs(train_ds, 3)
 
-### Creating and training model ###
+    ### Creating and training model ###
 
-# Number of filters in first layer of model
-filters = 12
-# Batch size model will train on
-batch_size = 16
-# The size of the images
-# Must match the image size specifed in load_images.py
-image_size = 256
+    # Number of filters in first layer of model
+    filters = 12
+    # Batch size model will train on
+    batch_size = 16
+    # The size of the images
+    # Must match the image size specifed in load_images.py
+    image_size = 256
 
-# Build model
-model = UNetModel(filters)
-model.build((batch_size,image_size,image_size,3))
-model.summary() 
+    # Build model
+    model = UNetModel(filters)
+    model.build((batch_size,image_size,image_size,3))
+    model.summary() 
 
-# Compile model with adam optimiser
-adam_opt = tf.keras.optimizers.Adam(learning_rate=5*10**(-5))
-model.compile(optimizer=adam_opt,
-              loss='categorical_crossentropy',
-              metrics=[dsc_fore, dsc_back])
+    # Compile model with adam optimiser
+    adam_opt = tf.keras.optimizers.Adam(learning_rate=5*10**(-5))
+    model.compile(optimizer=adam_opt,
+                  loss='categorical_crossentropy',
+                  metrics=[dsc_fore, dsc_back])
 
-# Train model
-history = model.fit(train_ds.batch(batch_size),
-                    validation_data=val_ds.batch(batch_size),
-                    epochs=3)
+    # Train model
+    history = model.fit(train_ds.batch(batch_size),
+                        validation_data=val_ds.batch(batch_size),
+                        epochs=3)
 
-### Evaluating model and viewing results ###
+    ### Evaluating model and viewing results ###
 
-# Evalute model metrics on test set
-model.evaluate(test_ds.batch(batch_size))
+    # Evalute model metrics on test set
+    model.evaluate(test_ds.batch(batch_size))
 
-# View some test images, their true segmentations, and their unrounded and
-# rounded predicted segmentations
-view_preds(model, test_ds, 3)
+    # View some test images, their true segmentations, and their unrounded and
+    # rounded predicted segmentations
+    view_preds(model, test_ds, 3)
