c7216159b7fbc5100930891287573428e93f31bb
Dattatreya Majumdar <s4558632@labs.eait.uq.edu.au>
2020-11-23 16:58:53 +1000

    Unnecessary package imports removed from Driver Script

--- recognition/Dattatreya_COMP3710_Project/Driver_Script.py
+++ recognition/Dattatreya_COMP3710_Project/Driver_Script.py
@@ -6,34 +6,11 @@ Created on Wed Oct 28 14:39:40 2020
 """
 
 
-import os
-import random
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 
 
-from tqdm import tqdm_notebook, tnrange
-from itertools import chain
-from skimage.io import imread, imshow, concatenate_images
-from skimage.transform import resize
-from skimage.morphology import label
 
-
-import tensorflow as tf
-
-from keras.models import Model, load_model
-from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
-from keras.layers.core import Lambda, RepeatVector, Reshape
-from keras.layers.convolutional import Conv2D, Conv2DTranspose
-from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
-from keras.layers.merge import concatenate, add
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from keras.utils import to_categorical
-from keras import backend as K
 
 import UNET_Mod_Compile as UNC
 import sys
