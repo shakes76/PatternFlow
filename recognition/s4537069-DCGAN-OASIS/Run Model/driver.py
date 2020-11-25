a540838568f30f6bb2096afe3d4f5748be520ab7
Paiwand Karem <paiwandkarem@gmail.com>
2020-11-25 19:54:45 +1000

    Updated driver.py comments to adhere to feedback

--- recognition/s4537069-DCGAN-OASIS/Run Model/driver.py
+++ recognition/s4537069-DCGAN-OASIS/Run Model/driver.py
@@ -1,7 +1,14 @@
+
+# Import processing and model files to call in main
+# Import path to call path to dataset, sys to run file.
 import processing
 import model
 import sys
 from pathlib import Path 
+
+# Main function for driver file
+# Runs functions from model and processing files which take commandline input of path and epoch.
+# python driver.py path epochs
 
 def main(args):
     test_data, train_data, images = processing.pre_processing_data(args[1])
