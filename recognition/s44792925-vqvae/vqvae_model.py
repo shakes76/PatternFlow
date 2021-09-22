98c46660868e243d1d77d3c361e598479f9d6c56
Alex Patapan <alexpatapan@gmail.com>
2021-09-22 10:34:40 +1000

    creating initial files

---
+++ recognition/s44792925-vqvae/vqvae_model.py
@@ -0,0 +1,12 @@
+
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, InputLayer, Flatten, Dense, Reshape, BatchNormalization, Dropout
+
+def encoder():
+    pass
+
+def decoder():
+    pass
+
+def sampler():
+    pass
