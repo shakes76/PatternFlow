d04c3d0bf8f80e48f77d8ce5c66ec0466de6fa90
Alex Patapan <alexpatapan@gmail.com>
2021-09-23 10:09:39 +1000

    Updated readme + Added basic models for decoder and encoder

--- recognition/s44792925/README.md
+++ recognition/s44792925/README.md
@@ -6,4 +6,14 @@
 
 This model has been developed using Google Colab. Henceforth, all of the logic (such as mounting drives) assumes that setup. There are some preliminary steps if you would like to run it within that environment.
 
-1. You will need to upload the OASIS dataset to your Google Drive, along with model.py
+1. You will need to upload the OASIS dataset to your Google Drive, along with model.py. This coudl take acouple of hours depending on your internet speeds.
+2. You will also need to update the directory strings to be the relative location of your dataset
+
+### Running on Personal Computer
+
+If you would like to run this on your computer then please just do not run the first cell of main.ipynb
+
+
+## Architecture
+Below is an image taken from a Berkely post (https://ml.berkeley.edu/blog/posts/vq-vae/) which shows the VQ-VAE architecture.
+<p align="center"><img src='https://i.imgur.com/R9VMWD6.png'></p>
--- recognition/s44792925/model.py
+++ recognition/s44792925/model.py
@@ -2,13 +2,86 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, InputLayer, Flatten, Dense, Reshape, BatchNormalization, Dropout
 
-def encoder():
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=(256,256,1)))
 
-def decoder():
-    model = tf.keras.models.Sequential()
+class VQVAE(tf.keras.Model):
 
-def vector_quantizer():
-    #discrete instead of continuous
-    pass
+    def __init__(self, latent_dimension, kernel_size=3, strides=2):
+        self.encoder = self.build_encoder()
+        self.decoder = self.build_decoder()
+        self.vector_quant = self.build_vector_quantizer()
+
+    def build_encoder(self, latent_dimension, kernel_size, strides):
+        """Encoder is based off my Demo 2 VAE submission with some minor changes"""
+
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=(256,256,1)))
+
+        model.add(tf.keras.layers.Conv2D(64, kernel_size, strides, 'same', activation='relu')) # 'same' padding makes sure entire image is convoluted
+        model.add(tf.keras.layers.BatchNormalization()) #recentering 
+        model.add(tf.keras.layers.LeakyReLU())
+
+        model.add(tf.keras.layers.Conv2D(64, kernel_size, strides, 'same', activation='relu')) # 'same' padding makes sure entire image is convoluted
+        model.add(tf.keras.layers.BatchNormalization()) #recentering 
+        model.add(tf.keras.layers.LeakyReLU())
+
+        # compress image
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(latent_dimension * 2)) 
+
+        print(model.summary)
+        return model
+
+    def build_decoder(self, latent_dimension, kernel_size, strides):
+        """Decoder is based off my Demo 2 VAE submission with some minor changes"""
+
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=(latent_dimension,)))
+        model.add(tf.keras.layers.Dense(8*8*64))
+        model.add(tf.keras.layers.Reshape((8,8,64)))
+
+        model.add(tf.keras.layers.Conv2DTranspose(32, kernel_size, strides, 'same', activation='relu'))
+        model.add(tf.keras.layers.Conv2DTranspose(32, kernel_size, strides, 'same', activation='relu'))
+        model.add(tf.keras.layers.BatchNormalization()) #recentering 
+        model.add(tf.keras.layers.LeakyReLU()) # to avoid dead weights 
+
+        model.add(tf.keras.layers.Conv2DTranspose(32, kernel_size, strides, 'same', activation='relu'))
+        model.add(tf.keras.layers.Conv2DTranspose(32, kernel_size, strides, 'same', activation='relu'))
+        model.add(tf.keras.layers.BatchNormalization()) #recentering
+        model.add(tf.keras.layers.LeakyReLU()) # to avoid dead weights 
+
+        model.add(tf.keras.layers.Conv2DTranspose(3, kernel_size, strides, 'same', activaiton='sigmoid'))
+
+        print(model.summary)
+        return model
+
+    def build_vector_quantizer(self, z):
+        """
+        z: continuous input (output of encoder)
+
+        Returns z_quantized (discrete variable)
+
+        shape of z: (batch_size, img_height, img_width, 1)
+
+        We want to map z to a discrete 1 hot encoded vector which indexse
+        """
+
+        #discrete instead of continuous
+        #1 hot encoding which indexes closest e
+        pass
+
+
+        # flatten
+
+        # calc euclidean distance between 
+
+        # find closest encoding
+
+    def compute_loss(self):
+        pass
+
+
+    def forward(self, x):
+        """Method modified versin from https://github.com/MishaLaskin/vqvae/blob/master/models/vqvae.py"""
+        z_e = self.encoder(x)
+        embedding_loss, z_q, perplex = self.vector_quant()
+
