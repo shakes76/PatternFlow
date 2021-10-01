1686fe37e6b0bec449b1501e0fb54a15f605b696
NhatHuyNguyen <nhathuy.nguyen@gmail.com>
2021-10-01 16:29:16 +0700

    modify a bit

--- recognition/s4571730/dense_net.py
+++ recognition/s4571730/dense_net.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 
 def dense_block(dense_layers):
     block = tf.keras.Sequential()
-    for units in dense_layers[:-1]:
+    # for units in dense_layers[:-1]:
         # use GELU activation as in paper
-        block.add(layers.LayerNormalization())
-        block.add(layers.Dense(units, activation=tf.nn.gelu))
+    block.add(layers.LayerNormalization())
+    block.add(layers.Dense(dense_layers[0], activation=tf.nn.gelu))
 
         # Final linear layer
     block.add(layers.Dense(units=dense_layers[-1]))
