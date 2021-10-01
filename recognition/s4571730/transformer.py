94ccb7995ca4514179f09aa445c3532893c6aadd
NhatHuyNguyen <nhathuy.nguyen@gmail.com>
2021-10-01 15:48:38 +0700

    no multiple layers?

--- recognition/s4571730/transformer.py
+++ recognition/s4571730/transformer.py
@@ -3,33 +3,34 @@ import tensorflow as tf
 import copy
 from dense_net import dense_block
 
-def transformer_layer(latent_dim, projection_dim, num_heads, num_transformer_blocks, dense_layers):
-    inputs = layers.Input(shape=(latent_dim, projection_dim))
+def transformer_layer(latent_size, projection_size, num_heads, num_transformer_blocks, dense_layers):
+    inputs_orig = layers.Input(shape=(latent_size, projection_size))
 
-    input_orig = copy.deepcopy(inputs)
+    input_plus_output = copy.deepcopy(inputs_orig)
     # Create multiple layers of the Transformer block.
-    for _ in range(num_transformer_blocks):
+    # for _ in range(num_transformer_blocks):
 
-        # Layer norm
-        norm = layers.LayerNormalization()(inputs)
-        # Create QKV self-attention layer.
-        attention_output = layers.MultiHeadAttention(
-            num_heads, projection_dim)(norm, norm)
+    # Layer norm
+    norm = layers.LayerNormalization()(inputs_orig)
+    # Create QKV self-attention layer.
+    # Multihead becomes self-attetion when q = k = v. v = k if not supplied
+    attention_output = layers.MultiHeadAttention(
+        num_heads, projection_size)(norm, norm)
 
-        # pass to a linear layer
-        attention_output = layers.Dense(projection_dim) # ?
+    # pass to a linear layer
+    attention_output = layers.Dense(projection_size) # ?
 
-        # Add output to input
-        attention_output = layers.Add()([attention_output, input_orig])
+    # Add output to input
+    attention_output = layers.Add()([attention_output, inputs_orig])
 
-        # Apply layer normalization 2.
-        # x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+    # Apply layer normalization 2.
+    # x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
 
-        # Dense MLP block
-        output = dense_block(dense_layers)(attention_output)
+    # Dense MLP block
+    output = dense_block(dense_layers)(attention_output)
 
-        # Skip connection 2.
-        final_output = layers.Add()([output, attention_output])
+    # Skip connection 2.
+    input_plus_output = layers.Add()([output, attention_output])
 
     # Create the Keras model.
-    return tf.keras.Model(inputs=inputs, outputs=final_output)
+    return tf.keras.Model(inputs=inputs_orig, outputs=input_plus_output)
