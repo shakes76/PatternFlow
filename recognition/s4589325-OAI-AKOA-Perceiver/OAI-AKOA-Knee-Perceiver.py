99d7f4361dda45e064a7e2fc60d17cccf8d63e79
Ahmed <ahafeezbaig@gmail.com>
2021-10-02 21:43:35 +1000

    initial tensorflow setup

--- recognition/s4589325-OAI-AKOA-Perceiver/OAI-AKOA-Knee-Perceiver.py
+++ recognition/s4589325-OAI-AKOA-Perceiver/OAI-AKOA-Knee-Perceiver.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import tensorflow_addons as tfa
 
 print("hello")
 
--- recognition/s4589325-OAI-AKOA-Perceiver/slurp.sh
+++ recognition/s4589325-OAI-AKOA-Perceiver/slurp.sh
@@ -3,8 +3,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=16G
 
-#SBATCH --job-name="CelebA Image Generation DCGANs"
-#SBATCH --mail-user=username@uqconnect.edu.au
+#SBATCH --job-name="OAI AKOA Knee Perceiver"
+#SBATCH --mail-user=s4589325@uqconnect.edu.au
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
@@ -13,7 +13,6 @@
 
 nvidia-smi
 
-export PYTHONPATH=/usr/lib64/python3.6/site-packages
-export PYTHONPATH=/usr/lib/python3.6/site-packages
+source ../../../miniconda3/bin/activate s4589325
 
-python3 ./AD-Perceiver.py
+python3 ./OAI-AKOA-Knee-Perceiver.py
