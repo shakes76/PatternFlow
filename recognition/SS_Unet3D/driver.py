587638d2c093ff8fc6392c9aa79dbc4fbdc0db86
Shravan Shivpuri <s.shivpuri@uqconnect.edu.au>
2021-10-09 22:45:00 +1000

    Basic model defined (untested) but many supporting functions remaining. Driver not started.

--- recognition/SS_Unet3D/driver.py
+++ recognition/SS_Unet3D/driver.py
@@ -4,3 +4,6 @@ from model import UNetCSIROMalePelvic
 the_model = UNetCSIROMalePelvic("My Model")
 
 print(the_model.mdl.summary())
+
+#for layer in the_model.mdl.layers:
+#    print(layer.input_shape, '-->', layer.name, '-->', layer.output_shape)
--- recognition/SS_Unet3D/model.py
+++ recognition/SS_Unet3D/model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, Conv3D, Conv3DTranspose
-from tensorflow.keras.layers import Dropout, Input, MaxPooling3D, BatchNormalization, Concatenate, concatenate
+from tensorflow.keras.layers import Dropout, Input, MaxPooling3D, BatchNormalization, Concatenate
 from tensorflow.keras.models import Sequential
 from tensorflow import keras
+from tensorflow.keras.layers import concatenate
 
 
 class UNetCSIROMalePelvic:
@@ -11,21 +12,21 @@ class UNetCSIROMalePelvic:
     opt = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
     loss = tf.keras.losses.binary_crossentropy
 
-
+    # Holds a dictionary of nodes and the last node to be added to the DAG
     class ModelNodes():
-        nodes = [{}, []]
+        nodes = [{}, None]
         def __init__(self):
             pass
 
+        def find(self, node):
+            return self.nodes[0][node]
+
         def last(self):
-            return self.nodes[1][-1]
+            return self.nodes[1]
 
         def add(self, name, node):
             self.nodes[0][name] = node
-            self.nodes[1].append(node)
-
-
-
+            self.nodes[1] = node
 
     def __init__(self, given_name):
         # Set up model
@@ -40,7 +41,7 @@ class UNetCSIROMalePelvic:
             for i in range(1, layer_len + 1):
                 # Create Conv3D Layer
                 new_node_name = "L{}_{}_Conv3D_{}".format(layer_num, stage, i)
-                new_node = Conv3D(name=new_node_name, kernel_size=3, filters=num_maps*i,
+                new_node = Conv3D(name=new_node_name, kernel_size=3, filters=num_maps[i-1],
                                   activation='relu', kernel_initializer=self.init,
                                   padding='same')(mdl_nodes.last())
                 mdl_nodes.add(name=new_node_name, node=new_node)
@@ -48,76 +49,62 @@ class UNetCSIROMalePelvic:
                 new_node_name = "L{}_{}_BN_{}".format(layer_num, stage, i)
                 new_node = BatchNormalization(name=new_node_name)(mdl_nodes.last())
                 mdl_nodes.add(name=new_node_name, node=new_node)
+            pass
 
         def _analysis_block_trailer(mdl_nodes, layer_num):
             stage = stages[0]
             new_node_name = "L{}_{}_MaxPool".format(layer_num, stage)
-            new_node = MaxPooling3D(name=new_node_name, pool_size=2, strides=2)(mdl_nodes.last())
+            new_node = MaxPooling3D(name=new_node_name, pool_size=2, strides=2,padding='same')(mdl_nodes.last())
             mdl_nodes.add(name=new_node_name, node=new_node)
+            pass
 
-        def _synthesis_block_header(given_mdl, layer_num):
+        def _synthesis_block(mdl_nodes, layer_num, num_maps):
+            _synthesis_block_header(mdl_nodes, layer_num)
+            _conv_block(mdl_nodes, layer_num, num_maps, analysis=False)
+            pass
+
+        def _synthesis_block_header(mdl_nodes, layer_num):
             stage = stages[1]
-            fetch_layer = given_mdl.get_layer("L{}_{}_BN_{}".format(layer_num - 1, stages[0], layer_len))
-            #fetch_layer_filters = fetch_layer.output.shape[-1]
-            last_layer_filters = given_mdl.layers[-1].output.shape[-1]
-            given_mdl = Conv3DTranspose(kernel_size=2, strides=2, filters=last_layer_filters,
-                                          name="L{}_{}_Conv3DTranspose".format(layer_num, stage))(given_mdl)
-            # Concatenate
-            given_mdl = tf.keras.layers.Concatenate()([
-                    given_mdl.layers[-1].output,
-                    fetch_layer.output
-                    ])(given_mdl)
-            return given_mdl
-
-
-            #print('Test', fetch_layer_filters)
-            #print(fetch_layer.name)
-
+            concat_analysis_layer_name = "L{}_{}_BN_{}".format(layer_num - 1, stages[0], layer_len)
+            concat_analysis_layer = mdl_nodes.find(concat_analysis_layer_name)  # Search Dict and retrive node
+            last_layer_filters = mdl_nodes.last().shape[-1]
+            # Step 1 - De-convolution while maintaining same number of filters
+            new_node_name = "L{}_{}_Conv3DTranspose".format(layer_num, stage)
+            new_node = Conv3DTranspose(name=new_node_name, kernel_size=2, strides=2,
+                                       padding='same', filters=last_layer_filters)(mdl_nodes.last())
+            mdl_nodes.add(name=new_node_name, node=new_node)
+            # Step 2 - Concatenate with Analysis Conjugate
+            new_node_name = "L{}_{}_Concat".format(layer_num, stage)
+            new_node = concatenate(name=new_node_name, inputs=[mdl_nodes.last(), concat_analysis_layer])
+            mdl_nodes.add(name=new_node_name, node=new_node)
+            pass
 
         def _analysis_block(mdl_nodes, layer_num, num_maps):
             _conv_block(mdl_nodes, layer_num, num_maps, analysis=True)
             _analysis_block_trailer(mdl_nodes, layer_num)
+            pass
 
-        '''
-        self.mdl = Sequential(name=given_name)
-        self.mdl.add(Input(shape=(256, 256, 128, 1)))
-
-        _analysis_block(self.mdl, layer_num=1, num_maps=32)
-        _analysis_block(self.mdl, layer_num=2, num_maps=64)
-        _analysis_block(self.mdl, layer_num=3, num_maps=128)
-        # Last layer does not have a trailer
-        _conv_block(self.mdl, layer_num=4, num_maps=256)
-        # Begin Synthesis Arm
-        _synthesis_block_header(self.mdl, layer_num=4)
-        '''
-
-        mdl_nodes = self.ModelNodes()  # [{},list()]
-
-        mdl_input = Input(name="Input", shape=(256, 256, 128, 1))
+        # ==========================================================================================
+        # Begin creating model
+        input_shape = (256, 256, 128, 1)
+        # Create a model nodes tracker
+        mdl_nodes = self.ModelNodes()
+        # Input Layer
+        mdl_input = Input(name="Input", shape=input_shape)
         mdl_nodes.add("Input", mdl_input)
-
-        mdl_output = _analysis_block(mdl_nodes, layer_num=1, num_maps=32)
-
-        #mdl_output = _analysis_block(mdl_output, layer_num=2, num_maps=64)
-        #mdl_output = _analysis_block(mdl_output, layer_num=3, num_maps=128)
+        # Begin Analysis Arm of UNet
+        _analysis_block(mdl_nodes, layer_num=1, num_maps=[32, 64])
+        _analysis_block(mdl_nodes, layer_num=2, num_maps=[64, 128])
+        _analysis_block(mdl_nodes, layer_num=3, num_maps=[128, 256])
         # Last layer does not have a trailer
-        #mdl_output = _conv_block(mdl_output, layer_num=4, num_maps=256)
-        # Begin Synthesis Arm
-        #mdl_output = _synthesis_block_header(mdl_output, layer_num=4)
-
-
+        _conv_block(mdl_nodes, layer_num=4, num_maps=[256, 512])
+        # Begin Synthesis Arm of UNet
+        _synthesis_block(mdl_nodes, layer_num=4, num_maps=[256, 256])
+        _synthesis_block(mdl_nodes, layer_num=3, num_maps=[128, 128])
+        _synthesis_block(mdl_nodes, layer_num=2, num_maps=[64, 64])
+        # Instantiate & compile model object
         self.mdl = tf.keras.Model(inputs=mdl_input, outputs=mdl_nodes.last())
-        #print(mdl_output)
-
-
-        # Define the Layers
-        # Define the Layers
-        # ...
-        # ...
-        # Define the Layers
-        # Define the Layers
-        #self.mdl.compile(optimizer=self.opt, loss=self.loss, metrics=['accuracy'])
-        #self.mdl.build()
+        self.mdl.compile(optimizer=self.opt, loss=self.loss, metrics=['accuracy'])
         pass
 
     def train_batch(self, batch_size=32):
