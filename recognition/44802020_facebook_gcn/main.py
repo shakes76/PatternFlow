b4489e49c8e8d96793a05b694a76f43a50fe78aa
johnvparsons99 <johnvparsons@gmail.com>
2021-10-10 16:12:22 +1000

    Added initial main function and imported tensorflow to ensure that the project Python interpreter is working correctly.

---
+++ recognition/44802020_facebook_gcn/main.py
@@ -0,0 +1,14 @@
+
+import tensorflow as tf
+
+
+def main():
+    print("Tensorflow version:", tf.__version__)
+
+
+if __name__ == '__main__':
+    ### Loading image set into split datasets ###
+    main()
+
+
+
