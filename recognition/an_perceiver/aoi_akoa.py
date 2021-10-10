b4fb03cde55ae517ef788d340c243812a7da301d
Anthony North <anthony.jl.north@gmail.com>
2021-10-10 16:23:15 +1000

    add splits to aoi_akoa dataset

--- recognition/an_perceiver/aoi_akoa.py
+++ recognition/an_perceiver/aoi_akoa.py
@@ -3,6 +3,9 @@
 @author Anthony North
 """
 
+from itertools import accumulate, groupby, chain
+import re
+import tensorflow as tf
 import tensorflow_datasets as tfds
 
 # TODO(aoi_akoa): Markdown description  that will appear on the catalog page.
@@ -12,6 +15,8 @@ _DESCRIPTION = """
 # TODO(aoi_akoa): BibTeX citation
 _CITATION = """
 """
+
+_SPLIT_RATIOS = (0.70, 0.15, 0.15)
 
 
 class AoiAkoa(tfds.core.GeneratorBasedBuilder):
@@ -37,6 +42,8 @@ class AoiAkoa(tfds.core.GeneratorBasedBuilder):
                         shape=(228, 260, 1), encoding_format="png"
                     ),
                     "label": tfds.features.ClassLabel(names=["left", "right"]),
+                    "patient_id": tf.int32,
+                    "baseline": tf.int32,
                 }
             ),
             supervised_keys=("image", "label"),
@@ -47,20 +54,52 @@ class AoiAkoa(tfds.core.GeneratorBasedBuilder):
     def _split_generators(self, dl_manager: tfds.download.DownloadManager):
         """Returns SplitGenerators."""
 
-        # extract from manual download dir
-        path = dl_manager.extract(dl_manager.manual_dir / "akoa_analysis.zip")
+        # open archive at manual download dir
+        archive_path = dl_manager.manual_dir / "akoa_analysis.zip"
+        archive = dl_manager.iter_archive(archive_path)
 
+        # group by patient, ensuring no patient can fall into > 1 split
+        examples_by_patient = [
+            list(examples)
+            for _, examples in groupby(
+                self._generate_examples(archive), key=lambda x: x[1]["patient_id"]
+            )
+        ]
+
+        # cumulative total of images will determine split breaks
+        cumulative_lens = accumulate(map(len, examples_by_patient))
+        examples_by_patient = list(zip(examples_by_patient, cumulative_lens))
+
+        n_examples = max((x[-1] for x in examples_by_patient))
+        split_thresholds = [ratio * n_examples for ratio in accumulate(_SPLIT_RATIOS)]
+
+        # slice examples by begin & end
+        slice_examples = lambda begin, end: chain.from_iterable(
+            imgs
+            for imgs, cumlen in examples_by_patient
+            if cumlen > begin and cumlen <= end
+        )
+
+        # return train, test & validate splits
         return {
-            "train": self._generate_examples(path / "akoa_analysis"),
+            split: slice_examples(begin, end)
+            for split, begin, end in zip(
+                ["train", "validation", "test"],
+                [-1, *split_thresholds[:-1]],
+                split_thresholds,
+            )
         }
 
-    def _generate_examples(self, path):
+    def _generate_examples(self, archive):
         """Yields examples."""
 
-        for img_path in path.glob("*.png"):
-            yield img_path.name, {
-                "image": img_path,
-                "label": self._get_laterality(img_path.name),
+        for _, img in archive:
+            name = img.name.split("/")[-1]
+            yield name, {
+                "image": img,
+                "label": self._get_laterality(name),
+                "patient_id": self._get_patient_id(name),
+                "baseline": self._get_baseline(name),
             }
 
     def _get_laterality(self, name: str) -> str:
@@ -74,3 +113,19 @@ class AoiAkoa(tfds.core.GeneratorBasedBuilder):
 
         assert is_left or is_right
         return "left" if is_left else "right"
+
+    def _get_patient_id(self, name: str) -> int:
+        """Get patient identifier from image name."""
+
+        match = re.search(r"(?<=OAI)\d+", name, re.IGNORECASE)
+        assert match is not None
+
+        return int(match.group(0))
+
+    def _get_baseline(self, name: str) -> int:
+        """Get baseline from image name."""
+
+        match = re.search(r"(?<=Baseline_)\d+", name, re.IGNORECASE)
+        assert match is not None
+
+        return int(match.group(0))
