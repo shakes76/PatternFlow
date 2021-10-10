5acbe111d2591455c046f4d0600d21f4034e3b29
leecwj <37468566+leecwj@users.noreply.github.com>
2021-10-10 23:24:02 +1000

    created method to build u-net of arbitrary width/height/level

--- recognition/s4482727/model.py
+++ recognition/s4482727/model.py
@@ -1,98 +1,74 @@
 import tensorflow as tf
-
-IMG_HEIGHT = 572
-IMG_WIDTH = 572
-IMG_CHANNELS = 1
-
-
-def unet_conv2d(filters: int, name: str) -> tf.keras.layers.Conv2D:
-    return tf.keras.layers.Conv2D(filters=filters, kernel_size=(3, 3),
-                                  padding="valid",
-                                  activation=tf.keras.activations.relu,
-                                  name=name)
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, MaxPool2D, \
+    Concatenate, Cropping2D
+from tensorflow.keras.models import Model
+from tensorflow.keras import Input
+from typing import Tuple
 
 
-def unet_maxpool2d(name: str) -> tf.keras.layers.MaxPool2D:
-    return tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2,
-                                     name=name)
+def unet_conv2d(filters: int, name: str) -> Conv2D:
+    return Conv2D(filters=filters, kernel_size=(3, 3), padding="valid",
+                  activation=tf.keras.activations.relu,
+                  name=name)
 
 
-def unet_upconv(filters: int, name: str) -> tf.keras.layers.Conv2DTranspose:
-    return tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=(3, 3),
-                                           name=name, strides=(2, 2),
-                                           padding="same")
+def unet_maxpool2d(name: str) -> MaxPool2D:
+    return MaxPool2D(pool_size=(2, 2), strides=2, name=name)
 
 
-def build_model():
-    inputs = tf.keras.Input(
-        shape=(IMG_WIDTH, IMG_WIDTH, IMG_CHANNELS),
-        name="input")
+def unet_upconv(filters: int, name: str) -> Conv2DTranspose:
+    return Conv2DTranspose(filters=filters, kernel_size=(3, 3), name=name,
+                           strides=(2, 2), padding="same")
 
-    conv_1a = unet_conv2d(filters=64, name="conv_1a")(inputs)
-    conv_1b = unet_conv2d(filters=64, name="conv_1b")(conv_1a)
 
-    pool_1 = unet_maxpool2d(name="pool_1")(conv_1b)
-
-    conv_2a = unet_conv2d(128, "conv_2a")(pool_1)
-    conv_2b = unet_conv2d(128, "conv_2b")(conv_2a)
-
-    pool_2 = unet_maxpool2d("pool2")(conv_2b)
-
-    conv_3a = unet_conv2d(filters=256, name="conv_3a")(pool_2)
-    conv_3b = unet_conv2d(filters=256, name="conv_3b")(conv_3a)
-
-    pool_3 = unet_maxpool2d(name="pool_3")(conv_3b)
-
-    conv_4a = unet_conv2d(filters=512, name="conv_4a")(pool_3)
-    conv_4b = unet_conv2d(filters=512, name="conv_4b")(conv_4a)
-
-    pool_4 = unet_maxpool2d(name="pool_4")(conv_4b)
-
-    conv_5a = unet_conv2d(filters=1024, name="conv_5a")(pool_4)
-    conv_5b = unet_conv2d(filters=1024, name="conv_5b")(conv_5a)
-
-    up_conv4 = unet_upconv(filters=512, name="up_conv4")(conv_5b)
-    crop_4 = tf.keras.layers.Cropping2D(cropping=((4, 4), (4, 4)),
-                                        name="crop_4")(conv_4b)
-
-    concat_4 = tf.keras.layers.Concatenate(name="concat_4")([up_conv4, crop_4])
-    conv_4c = unet_conv2d(filters=512, name="conv_4c")(concat_4)
-    conv_4d = unet_conv2d(filters=512, name="conv_4d")(conv_4c)
-
-    up_conv3 = unet_upconv(filters=256, name="up_conv3")(conv_4d)
-    crop_3 = tf.keras.layers.Cropping2D(cropping=((16, 16), (16, 16)),
-                                        name="crop_3")(conv_3b)
-
-    concat_3 = tf.keras.layers.Concatenate(name="concat_3")([up_conv3, crop_3])
-    conv_3c = unet_conv2d(filters=256, name="conv_3c")(concat_3)
-    conv_3d = unet_conv2d(filters=256, name="conv_3d")(conv_3c)
-
-    up_conv2 = unet_upconv(filters=128, name="up_conv2")(conv_3d)
-    crop_2 = tf.keras.layers.Cropping2D(cropping=((40, 40), (40, 40)),
-                                        name="crop_2")(conv_2b)
-
-    concat_2 = tf.keras.layers.Concatenate(name="concat_2")([up_conv2, crop_2])
-    conv_2c = unet_conv2d(filters=128, name="conv_2c")(concat_2)
-    conv_2d = unet_conv2d(filters=128, name="conv_2d")(conv_2c)
-
-    up_conv1 = unet_upconv(filters=64, name="up_conv1")(conv_2d)
-    crop_1 = tf.keras.layers.Cropping2D(cropping=((88, 88), (88, 88)),
-                                        name="crop_1")(conv_1b)
-
-    concat_1 = tf.keras.layers.Concatenate(name="concat_1")([up_conv1, crop_1])
-    conv_1c = unet_conv2d(filters=64, name="conv_1c")(concat_1)
-    conv_1d = unet_conv2d(filters=64, name="conv_1d")(conv_1c)
-
-    outputs = tf.keras.layers.Conv2D(filters=2, kernel_size=(1, 1),
-                                     padding="same",
-                                     activation=tf.keras.activations.relu,
-                                     name="output")(conv_1d)
-
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-
+def build_unet(width: int, height: int, levels: int,
+               filters: int = 64) -> Model:
+    inputs = Input(shape=(width, height, 1), name="input")
+    _outputs = _build_unet(inputs, width, height, levels, filters=filters)
+    outputs = Conv2D(filters=2, kernel_size=(1, 1),
+                     padding="same",
+                     activation=tf.keras.activations.relu,
+                     name="output")(_outputs)
+    model = Model(inputs=inputs, outputs=outputs)
     return model
 
 
+def _build_unet(inputs, width, height, levels, filters=64):
+    conv_a = unet_conv2d(filters, "conv" + str(levels) + "a")(inputs)
+    conv_b = unet_conv2d(filters, "conv" + str(levels) + "b")(conv_a)
+
+    if levels == 1:
+        return conv_b
+    else:
+        pool = unet_maxpool2d("pool" + str(levels))(conv_b)
+        sub_unet = _build_unet(pool, (width - 4) // 2, (height - 4) // 2,
+                               levels - 1, filters * 2)
+        upconv = unet_upconv(filters, "upconv" + str(levels))(sub_unet)
+        crop = Cropping2D(name="crop" + str(levels),
+                          cropping=get_cropping(width, height, levels))(conv_b)
+        concat = Concatenate(name="concat" + str(levels))([upconv, crop])
+        conv_c = unet_conv2d(filters, "conv" + str(levels) + "c")(concat)
+        conv_d = unet_conv2d(filters, "conv" + str(levels) + "d")(conv_c)
+        return conv_d
+
+
+def unet_end_length(length: int, level: int) -> int:
+    if level == 1:
+        return length - 4
+    else:
+        return unet_end_length((length - 4) // 2, level - 1) * 2 - 4
+
+
+def get_cropping(width: int, height: int, levels: int) -> Tuple[int, int]:
+    new_width = unet_end_length(width, levels) + 4
+    new_height = unet_end_length(height, levels) + 4
+
+    cropping_x = (width - 4 - new_width) // 2
+    cropping_y = (height - 4 - new_height) // 2
+
+    return cropping_x, cropping_y
+
+
 if __name__ == "__main__":
-    model = build_model()
+    model = build_unet(572, 572, 5)
     model.summary()
