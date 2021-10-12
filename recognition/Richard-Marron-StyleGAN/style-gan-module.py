8180377149fd4bec275cb4160f145757fefa2502
Richard <r3marron@gmail.com>
2021-10-12 11:28:04 +1000

    Added module with model deinitions

---
+++ recognition/Richard-Marron-StyleGAN/style-gan-module.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential, layers
+
+class StyleGan():
+    def __init__(self):
+        pass
+    
+    
+    def discriminator_model(self):
+        """
+        Define the discriminator model for the Style GAN
+        """
+        model = Sequential()
+        #model.add(layers.Dense())
+        pass
+    
+    def generator_model(self):
+        """
+        Define the generator model for the Style GAN
+        """
+        model = Sequential()
+        pass
+    
+    def adaIN(self, x, y):
+        """
+        Calculate the adaIN value to be used in the model
+        Params: 
+            x : Vector which corresponds to feature map
+            y : Tuple of vectors (y_s, y_b) called styles
+        """
+        y_s, y_b = y
+        mu_x = tf.math.reduce_mean(x)
+        sig_x = tf.math.reduce_std(x)
+        return y_s * (x - mu_x)/sig_x + y_b
