faa8005851b0a9aea938967bbecbc97d4a01893f
johnvparsons99 <johnvparsons@gmail.com>
2021-10-12 22:15:52 +1000

    Began parsing data by using np.load() to open the facebook.npz file. Now loading and printing a summary of the Pages and Features, Edges, and Target.

--- recognition/44802020_facebook_gcn/main.py
+++ recognition/44802020_facebook_gcn/main.py
@@ -1,13 +1,54 @@
+from typing import Any
 
 import tensorflow as tf
+import numpy as np
+
+# Constants
+def summarise_data(data, aspect):
+    print(f"===== {aspect}=====")
+    aspect_d = data[aspect]
+    print(aspect_d.shape)  ## (22 470, 128)
+    print(aspect_d)
+    print(type(aspect_d))
+    print(type(aspect_d[0]))
+    print(aspect_d[0])
+    print("====================")
+
 
 
 def main():
     print("Tensorflow version:", tf.__version__)
+    print("Numpy version:", np.__version__)
+
+
+    file_path = r"C:\Users\johnv\Documents\Code Projects\Pattern Recognition\facebook.npz"
+
+    # Load in Data
+    data = np.load(file_path)
+
+    print(data.files)
+
+    features = data['features']
+    edges = data['edges']
+    target = data['target']
+
+    summarise_data(data, 'features')
+    summarise_data(data, 'edges')
+    summarise_data(data, 'target')
+
+    ## There are 22 470 Pages
+    ## Each with 128 features
+    ## Each falls into 1 of 4 categories
+    ## There are 342 004 Edges between the pages
+
+
+    for i in range(200):
+        print(target[i])
+
+
 
 
 if __name__ == '__main__':
-    ### Loading image set into split datasets ###
     main()
 
 
