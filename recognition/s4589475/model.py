12ca6eab9ceac062e45e526a159e35eb74be93e2
gwehr1 <91168723+gwehr1@users.noreply.github.com>
2021-10-13 10:51:53 +1000

    Modified Vector Quantizer layer to allow proper initialization of uniform prior p(z) and back-propagation via straight-through estimation. Also added latent loss function

--- recognition/s4589475/model.py
+++ recognition/s4589475/model.py
@@ -4,30 +4,20 @@ K = 512 #Number of codebook vectors / embeddings number - using recommended valu
 
 # Vector Quantizer Layer
 class Vector_Quantizer(tf.keras.layers.Layer):
-
-    def call(self, encoder_outputs):
+    
+    def __init__(self):
+        super().__init__()
         #define the size of our embeddings - has same length as latent space vectors, and width as defined by K (hyperparameter)
         codebook_shape = (latent_dims, K)
         #create the embedding object - initialise uniformly
         initializer = tf.keras.initializers.RandomUniform(minval=0., maxval=1.)
         #Declare our embeddings as a tensorflow variable that will be learnt
-        embeddings = tf.Variable(initializer(shape=codebook_shape, dtype='float32'), trainable = True)
+        self._embeddings = tf.Variable(initializer(shape=codebook_shape, dtype='float32'), trainable = True)
 
-        #Flatten all dimensions of the encoded vectors except the channels
-        #Encoded vectors = (B,H,W,C) -> (B*H*W, C) i.e. flattened, each of which will be quantized independently
-        encoded_vectors = tf.reshape(encoder_outputs, [-1, codebook_dims])
-
-        #Now can quantize each
-        quantized_vectors = quantize_vectors(encoded_vectors)
-        
-        # Reshape the flat vectors back into 2D for use in the convolutional decoder network
-        reshaped_quantized_vectors = tf.reshape(quantized_vectors, tf.shape(encoder_outputs))
-        return reshaped_quantized_vectors
-
-    def quantize_vectors(self, vectors, embeddings):
+    def quantize_vectors(self, vectors):
         #First need to calculate the distance between our input vectors and every vector in the codebook
         # Paper uses L2 norm: sqrt((x2-x1)^2 + (y2-y1)^2 +...)
-        distances = tf.reduce_sum(vector**2, axis=1, keepdims=True) - 2*tf.matmul(vector, embeddings) + tf.reduce_sum(embeddings**2, axis=0, keepdims=True) 
+        distances = tf.reduce_sum(vectors**2, axis=1, keepdims=True) - 2*tf.matmul(vectors, self._embeddings) + tf.reduce_sum(self._embeddings**2, axis=0, keepdims=True) 
 
         # Can now determine to which codebook vector our input vectors are closest to (minimum distance)
         indices = tf.argmin(distances, 1)
@@ -36,12 +26,30 @@ class Vector_Quantizer(tf.keras.layers.Layer):
         one_hot_indices = tf.one_hot(indices, K)
         
         # Apply indices to the embeddings - now have quantized the vectors
-        quantized_vectors = tf.mamul(one_hot_indices, embeddings, transpose_b=True)
+        quantized_vectors = tf.matmul(one_hot_indices, self._embeddings, transpose_b=True)
         
         return quantized_vectors
+    
+    def call(self, encoder_outputs):
+        #Flatten all dimensions of the encoded vectors except the channels
+        #Encoded vectors = (B,H,W,C) -> (B*H*W, C) i.e. flattened, each of which will be quantized independently
+        encoded_vectors = tf.reshape(encoder_outputs, [-1, latent_dims])
+
+        #Now can quantize each
+        quantized_vectors = self.quantize_vectors(encoded_vectors)
+        
+        # Reshape the flat vectors back into 2D for use in the convolutional decoder network
+        reshaped_quantized_vectors = tf.reshape(quantized_vectors, tf.shape(encoder_outputs))
+        
+        #Do straight-through estimation so that back-propagation can occur
+        # Going forward, output = encoder_output + quantized_vectors - encoder_output = quantized vectors (as desired)
+        # Going backward, we copy the gradients at the decoder back to the encoder
+        output = encoder_outputs + tf.stop_gradient(reshaped_quantized_vectors - encoder_outputs)
+        return output
+
 
 # Define the encoder and decoder networks
-def encoder():
+def encoder_network():
 
     # Encoder network - takes input images and encodes them into z
     inputs = tf.keras.layers.Input(shape=(image_height, image_width, channels))
@@ -54,13 +62,13 @@ def encoder():
     #conv = tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, activation='relu', padding = "same")(conv)
     
     #Final conv layer - image is now encoded into size of latent space
-    hidden = tf.keras.layers.Conv2D(latent_dims, kernel_size = 1, padding = "same")(conv)
+    hidden = tf.keras.layers.Conv2D(latent_dims, kernel_size = 1, padding = "same")(conv) #******************
     
     #Quantize the vectors through the Vector Quantizer Layer
     output = Vector_Quantizer()(hidden)
 
     # Build the encoder
-    final_network = tf.keras.Model(inputs, output)
+    final_network = tf.keras.Model(inputs, output, name="Encoder")
 
     return final_network
     
@@ -68,33 +76,53 @@ def encoder():
 #def encoder_loss():
 
 
-def decoder():
+def decoder_network():
     # Takes the latent space and upsamples until original size is reached
     #256 -> downsampled twice = 256/4 = 64
-    inputs = tf.keras.layers.Input(shape=(latent_dims,))
+    inputs = tf.keras.layers.Input(shape=(64,64,latent_dims))
+    #input_shape = encoder().output.shape[1:]
+    #inputs = tf.keras.layers.Input(shape=input_shape)
     
     # Now can upsample twice
-    conv = tf.keras.layers.Conv2DTranspose(32, kernel_size=3, strides=2, padding='same', activation='relu')(dense)
+    conv = tf.keras.layers.Conv2DTranspose(32, kernel_size=3, strides=2, padding='same', activation='relu')(inputs)
     conv = tf.keras.layers.Conv2DTranspose(16, kernel_size=3, strides=2, padding='same', activation='relu')(conv)
     
     #Output layer
     output = tf.keras.layers.Conv2D(1,kernel_size=[1,1], strides=1, activation='relu', padding = "same")(conv)
     
     #build the decoder
-    final_network = tf.keras.Model(inputs, output)
+    final_network = tf.keras.Model(inputs, output, name="Decoder")
     return final_network
 
 #def decoder_loss()
 
 def VQ_VAE_network():
     #Create the encoder and decoder components of VQ-VAE Model
-    encoder_network = encoder()
-    decoder_network = decoder()
+    encoder = encoder_network()
+    decoder = decoder_network()
 
     # Construct the overall VQ-VAE now
     inputs = tf.keras.layers.Input((image_height, image_width, channels))
-    hidden = encoder_network(inputs)
-    output = decoder_network(hidden)
+    hidden = encoder(inputs)
+    output = decoder(hidden)
 
-    vq_vae = tf.keras.Model(inputs=inputs, outputs=output)
-    optimizer = tf.keras.optimizers.Adam(1e-4)
+    vq_vae = tf.keras.Model(inputs=inputs, outputs=output, name="VQ-VAE Model")
+    
+    return vq_vae
+
+#Replace kl-divergence in traditional VAE with two terms as kl-divergence can't be minimized (is a constant)
+def latent_loss(encoder_output, quantized_vectors):
+    # latent commitment loss
+    # beta*||z_e(x)-sg[e]|| i.e. freeze the codebook vectors/embeddings and push the encoded vectors towards codebook vectors
+    mse1 = tf.keras.losses.MeanSquaredError()
+    e_loss = beta * mse1(tf.stop_gradient(quantized_vectors), encoder_output)
+
+    # latent codebook loss
+    #||sg[z_e(x)]-e|| i.e. freeze the encoded vectors and push codebook vectors to encoded vectors
+    mse2 = tf.keras.losses.MeanSquaredError()
+    q_loss = mse2(quantized_vectors, tf.stop_gradient(encoder_output))
+    
+    return q_loss + e_loss
+
+#First term of loss function - reconstruction loss
+#def reconstruction_loss()
