22acb0773127376761d290805fe19b93f77bfce4
Keith Dao <keith.dao.248@gmail.com>
2021-10-13 17:41:18 +1000

    Created the AdaIN layer as a Layer.

--- recoginition/45798906-OASIS-Brain-StyleGan/gan.py
+++ recoginition/45798906-OASIS-Brain-StyleGan/gan.py
@@ -13,8 +13,52 @@
 """
 
 import tensorflow as tf
+from tensorflow.keras.layers import (
+    Activation,
+    add,
+    Conv2D,
+    Conv2DTranspose,
+    Dense,
+    Input,
+    Layer,
+    LeakyReLU,
+    Reshape,
+)
+
+# Custom layers
+class AdaIN(Layer):
+    """Adaptive Instance Normalisation Layer."""
+
+    def __init__(self, epsilon: float = 1e-3):
+
+        super(AdaIN, self).__init__()
+        self.epsilon = epsilon
+
+    def build(self, input_shape):
+
+        dim = input_shape[-1]
+        if dim == None:
+            raise ValueError(
+                f"Excepted axis {-1} of the input tensor be defined, but got an input with shape {input_shape}."
+            )
+
+        super(AdaIn, self).build(input_shape)
+
+    def call(self, inputs):
+        """Apply the normalisation formula: gamma * (x - mean) / stddev + beta."""
+
+        x, beta, gamma = inputs
+
+        input_shape = x.shape
+        axes = list(range(1, len(input_shape) - 1))
+        mean = tf.math.reduce_mean(x, axes, keepdims=True)
+        stddev = tf.math.reduce_std(x, axes, keepdims=True) + self.epsilon
+        normalised = (x - mean) / stddev
+
+        return normalised * gamma + beta
 
 
+# Models
 def get_generator(
     input_dim: int, output_dim: tuple[int, int, int]
 ) -> tf.keras.Model:
