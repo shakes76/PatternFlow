17410530c84b28a8429a2df57d1e2c3f451d5ce7
Keith Dao <keith.dao.248@gmail.com>
2021-10-13 17:34:02 +1000

    Added basic structure of gan.py

---
+++ recoginition/45798906-OASIS-Brain-StyleGan/gan.py
@@ -0,0 +1,27 @@
+"""
+    gan.py
+
+    This file contains the functions used to generate the generator and discriminator for StyleGAN.
+
+    Requirements:
+        - TensorFlow 2.0
+
+    Author: Keith Dao
+    Date created: 13/10/2021
+    Date last modified: 13/10/2021
+    Python version: 3.9.7
+"""
+
+import tensorflow as tf
+
+
+def get_generator(
+    input_dim: int, output_dim: tuple[int, int, int]
+) -> tf.keras.Model:
+    pass
+
+
+def get_generator(
+    input_dim: tuple[int, int, int], output_dim: int
+) -> tf.keras.Model:
+    pass
