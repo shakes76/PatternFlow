2477e32088cfbd7294993c773844d1f3b9e5335a
Pritish Roy <pritishkumar.roy@uqconnect.edu.au>
2021-10-14 19:25:16 +1000

    visualise extracted patches from the dataset

---
+++ recognition/46252665_Perceiver_Transformer_OAI_AKOA/display/display_plot.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from perceiver.extract_patches import Patches
+from perceiver.load_data import LoadDataset
+from settings.config import *
+
+
+class PlotPatches:
+    """Visualise Patches"""
+    def __init__(self):
+        self.data = LoadDataset()
+
+        self.image = self.data.x_train[np.random.choice(
+            range(self.data.x_train.shape[0]))]
+
+    def plot_sample(self):
+        """Plot sample image"""
+        plt.figure(figsize=(4, 4))
+        plt.imshow(self.image.astype(np.uint8))
+        plt.axis("off")
+        plt.savefig(f'{FIGURE_LOCATION}sample.png')
+
+    def plot_patches(self):
+        """Plot patches of an sample image"""
+        image_tensor = tf.convert_to_tensor([self.image])
+        patches = Patches().call(image_tensor)
+
+        n = int(np.sqrt(patches.shape[1]))
+        plt.figure(figsize=(4, 4))
+        for i, patch in enumerate(patches[0]):
+            plt.subplot(n, n, i + 1)
+            patch_img = tf.reshape(patch, (PATCH_SIZE, PATCH_SIZE, 3))
+            plt.imshow(patch_img.numpy().astype("uint8"))
+            plt.axis("off")
+        plt.savefig(f'{FIGURE_LOCATION}patches.png')
+
+    def do_action(self):
+        """sequential set of actions"""
+        self.plot_sample()
+        self.plot_patches()
--- recognition/46252665_Perceiver_Transformer_OAI_AKOA/main.py
+++ recognition/46252665_Perceiver_Transformer_OAI_AKOA/main.py
@@ -1,3 +1,6 @@
+from display.display_plot import PlotPatches
+from perceiver.train import TransformerTrainer
+
 from pre_process.dataset_processor import ProcessDataset
 
 
@@ -5,6 +8,14 @@ def run():
     # Create Dataset
     dataset = ProcessDataset()
     dataset.do_action()
+
+    # Train
+    # transformer = TransformerTrainer()
+    # transformer.do_action()
+
+    # Plot Patches
+    plot = PlotPatches()
+    plot.do_action()
 
 
 if __name__ == '__main__':
