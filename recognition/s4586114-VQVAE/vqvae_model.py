410981a94be8ab744305fcac7d2b85fa3f06661f
Mallika Mukherji <s4586114@labs.eait.uq.edu.au>
2021-10-16 13:26:14 +1000

    Added encoder and decoder blocks to VQVAE model.

--- recognition/s4586114-VQVAE/vqvae_model.py
+++ recognition/s4586114-VQVAE/vqvae_model.py
@@ -64,3 +64,43 @@ class VectorQuantizer(layers.Layer):
         # Derive the indices for minimum distances.
         encoding_indices = tf.argmin(distances, axis=1)
         return encoding_indices
+    
+
+def get_encoder(latent_dim=16):
+    """
+    Encoder for VQVAE.
+    """
+    encoder_inputs = keras.Input(shape=(128, 128, 1))
+    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(
+        encoder_inputs
+    )
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    encoder_outputs = layers.Conv2D(latent_dim, 1, padding="same")(x)
+    return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
+
+
+def get_decoder(latent_dim=16):
+    """
+    Decoder for VQVAE.
+    """
+    latent_inputs = keras.Input(shape=get_encoder().output.shape[1:])
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
+        latent_inputs
+    )
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+    decoder_outputs = layers.Conv2DTranspose(1, 3, padding="same")(x)
+    return keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+
+def get_vqvae(latent_dim=16, num_embeddings=64):
+    """
+    Stand alone VQVAE model.
+    """
+    vq_layer = VectorQuantizer(num_embeddings, latent_dim, name="vector_quantizer")
+    encoder = get_encoder(latent_dim)
+    decoder = get_decoder(latent_dim)
+    inputs = keras.Input(shape=(128, 128, 1))
+    encoder_outputs = encoder(inputs)
+    quantized_latents = vq_layer(encoder_outputs)
+    reconstructions = decoder(quantized_latents)
+    return keras.Model(inputs, reconstructions, name="vq_vae")
