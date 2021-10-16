d97b63d9af78d8d55d8f5ae28a448e9c793a97b4
ryemigie <ryan.tongs@sizztech.com>
2021-10-16 21:31:14 +1000

    Bring in Mask R-CNN model
    Use public GitHub project that converts original Mask R-CNN code written in TensorFlow 1 to TensorFlow 2.0. Bring in a sample image from the dataset, and add a .gitignore file to ignore model files. Bring in dataset in my model file. Bring support for program and default arguments in driver file.

--- recognition/s4427545_ISICs/Mask_R_CNN.py
+++ recognition/s4427545_ISICs/Mask_R_CNN.py
@@ -1,10 +1,71 @@
 import tensorflow as tf
+import mrcnn
+import mrcnn.config
+import mrcnn.model
+import mrcnn.visualize
+import csv
+import os
+
+class BaseConfig(mrcnn.config.Config):
+    # static vars for config
+    NAME = "ISIC"
+    GPU_COUNT = 1
+	# Number of classes = number of classes + 1 (+1 for the background). The background class is named BG
+    NUM_CLASSES = 2
 
 class MaskRCNN():
+    CLASS_NAMES = ['BG', 'Lesion']
 
-    def __init__(self):
-        print('init')
+    def __init__(self, dir, batch_size):
+        self.dir = dir
+        self.batch_size = batch_size
 
     def train(self):
-        print("TF Version:", tf.__version__)
-        print(tf.config.list_physical_devices('GPU'))
+        self.print_info()
+        images = tf.keras.preprocessing.image_dataset_from_directory(
+            self.dir + 'ISIC2018_Task1-2_Training_Input_x2',
+            label_mode=None,
+            color_mode='rgb',
+            seed=42,
+            image_size=(1024, 1024),
+            shuffle=True,
+            batch_size=self.batch_size)
+        masks = tf.keras.preprocessing.image_dataset_from_directory(
+            self.dir + 'ISIC2018_Task1_Training_GroundTruth_x2',
+            label_mode=None,
+            color_mode='rgb',
+            seed=42,
+            image_size=(1024, 1024),
+            shuffle=True,
+            batch_size=self.batch_size)
+        self.build_model()
+
+    def build_model(self):
+        model = mrcnn.model.MaskRCNN(mode="inference", 
+                                    config=BaseConfig(),
+                                    model_dir=os.getcwd())
+        # Using COCO dataset trained weights
+        model.load_weights(filepath="mask_rcnn_coco.h5", 
+                        by_name=True)
+        # load the input image, convert it from BGR to RGB channel
+        image = csv.imread("sample_image.jpg")
+        image = csv.cvtColor(image, csv.COLOR_BGR2RGB)
+        r = model.detect([image], verbose=1)
+        r = r[0]
+        # visualise result
+        mrcnn.visualize.display_instances(image=image, 
+                                        boxes=r['rois'], 
+                                        masks=r['masks'], 
+                                        class_ids=r['class_ids'], 
+                                        class_names=self.CLASS_NAMES, 
+                                        scores=r['scores'])
+
+    def print_info(self):
+        print(f"TF Version: {tf.__version__}")
+        print(f'GPU: {tf.config.list_physical_devices("GPU")}')
+        print(f'Dataset directory: {self.dir}')
+
+    def generate_bounding_boxes(self, images):
+        coords = []
+        for image in images:
+            pass
--- recognition/s4427545_ISICs/driver.py
+++ recognition/s4427545_ISICs/driver.py
@@ -1,11 +1,20 @@
 from Mask_R_CNN import *
+import sys
+import os
 
-def main():
+# Uses https://github.com/ahmedfgad/Mask-RCNN-TF2
+
+def main(arg):
     # Firstly, call trainer and begin training
-    maskRCNN = MaskRCNN() # pass data generator into constructor
+    dir = os.path.expanduser('~/Datasets/ISIC2018_Task1-2_Training_Data/') # default
+    batch_size = 32
+    if len(arg) > 1:
+        dir = arg[1]
+        batch_size = int(arg[2])
+    maskRCNN = MaskRCNN(dir, batch_size) # pass data generator into constructor
     maskRCNN.train() # should have checkpoint logic in here
 
     # print graphs here
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
