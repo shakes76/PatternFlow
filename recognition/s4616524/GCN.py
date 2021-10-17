608a455d3e1f0698caca60935ef185fa7f0536b6
Patato <ienjoylife0525@gmail.com>
2021-10-17 19:42:37 +1000

    creaet inital adj matrix

--- recognition/s4616524/GCN.py
+++ recognition/s4616524/GCN.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import scipy.sparse as sp
 
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
@@ -12,8 +13,8 @@ class Facebook_Node_Classifier():
     def __init__(self, facebook_file: str):
         
         self.data_process(facebook_file)
-
-
+        self.create_adj()
+        
     def data_process(self, facebook_file: str):
         data = np.load(facebook_file)
 
@@ -30,3 +31,25 @@ class Facebook_Node_Classifier():
         self.target = torch.LongTensor(target)
         self.edges = edges
         
+    def create_adj(self):
+        #create an iniitial adj matrix for sparse matrix
+        adj = sp.coo_matrix((np.ones(self.n_edges), (self.edges[:, 0], self.edges[:, 1])))
+        
+        #make sure all element is 1 or 0
+        adj_t = torch.Tensor(adj.toarray())
+        adj_t[adj_t > 0] = 1
+
+        #check adj is semetric or not
+        assert sum(sum(adj_t != adj_t.T)) == 0, 'Adjacency matrix is not symetric'
+
+        self.adj = adj_t
+
+if __name__ == "__main__":
+    
+    facebook_path = 'facebook.npz'
+
+    classifer = Facebook_Node_Classifier(facebook_file=facebook_path)
+
+    print(torch.unique(classifer.adj.unique()))
+
+    
