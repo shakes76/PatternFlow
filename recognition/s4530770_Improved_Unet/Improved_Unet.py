4b165e99f0ffbe27d55b555d43ec0845bfd44a80
Cherian John <s4530770@labs.eait.uq.edu.au>
2021-10-17 12:09:02 +1000

    Completly bug fixed and tested network architecture

--- recognition/s4530770_Improved_Unet/Improved_Unet.py
+++ recognition/s4530770_Improved_Unet/Improved_Unet.py
@@ -1,20 +1,11 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, UpSampling2D, Conv2DTranspose, Concatenate, Input, LeakyReLU, SpatialDropout2D, Softmax
+from tensorflow.keras.layers import Conv2D, UpSampling2D, Concatenate, Input, LeakyReLU, SpatialDropout2D, Softmax
 from tensorflow_addons.layers import InstanceNormalization
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
 
 # Reference: https://github.com/pykao/Modified-3D-UNet-Pytorch/blob/63f0489e8d1fdd7ec6a203bcff095f12ea030824/model.py#L70
-def context_module(input_layer, filters, stride):
-    return Sequential([
-        input_layer,
-        LeakyReLU(),
-        Conv2D(filters, 3, strides=stride, padding="same"),
-        SpatialDropout2D(0.6),
-        Conv2D(filters, 3, strides=stride, padding="same")
-    ])
 
-
-def encoder_block(input_layer, filters, stride, switch=0):
+def encoder_block(input_layer, filters, stride):
     # Level 1 context pathway
     conv_layer = Conv2D(filters, 3, strides=stride, padding="same")(input_layer)
     residual = conv_layer
@@ -24,61 +15,53 @@ def encoder_block(input_layer, filters, stride, switch=0):
     dropout = SpatialDropout2D(0.6)(conv_layer)
     c_module = Conv2D(filters, 3, strides=1, padding="same")(dropout)
     # Element wise summation of convolution and context module
-    print(c_module.shape)
-    print(residual.shape)
     sum = tf.math.add(c_module, residual)
-    # Save for skip connection
-    context_1 = sum
     norm_layer = InstanceNormalization()(sum)
-    norm_layer = LeakyReLU()(norm_layer)
-    context_2 = norm_layer
-    if switch==0:
-        return norm_layer, context_2
-    else:
-        return norm_layer, context_1
+    return LeakyReLU()(norm_layer)
+
 
 def upsample_module(input_layer, filters):
-    return Sequential([
-        input_layer,
-        InstanceNormalization(),
-        LeakyReLU(),
-        UpSampling2D(2, interpolation="nearest"),
-        Conv2D(filters, 3, stride=1, padding="same"),
-        InstanceNormalization(),
-        LeakyReLU()
-    ])
+    norm = InstanceNormalization()(input_layer)
+    norm = LeakyReLU()(norm)
+    upsample = UpSampling2D(2, interpolation="nearest")(norm)
+    conv_layer = Conv2D(filters, 3, strides=1, padding="same")(upsample)
+    norm = InstanceNormalization()(conv_layer)
+    return LeakyReLU()(norm)
 
 def localisation_module(input_layer, context, filters):
     cat = Concatenate()([input_layer, context])
-    conv_layer = Conv2D(filters, kernel=3, stride=1, padding="same")(cat)
+    conv_layer = Conv2D(filters, 3, strides=1, padding="same")(cat)
     norm = InstanceNormalization()(conv_layer)
     norm = LeakyReLU()(norm)
     # Segmentation layer for deep supervision
     seg = norm
-    return Conv2D(filters, 3, stride=1, padding="same")(norm), seg
+    return Conv2D(filters/2, 1, strides=1, padding="same")(norm), seg
 
 def build_model(input_size):
     # Level 1 context pathway
     input_layer = Input(input_size)
-    out, context_1 = encoder_block(input_layer, 64, 1, switch=1)
-    context_1 = LeakyReLU()(context_1)
+    out = encoder_block(input_layer, 64, 1)
+    # Skip connection
+    context_1 = out
 
     # Level 2 context pathway
-    out, context_2 = encoder_block(out, 128, 2)
+    out = encoder_block(out, 128, 2)
+    context_2 = out
 
     # Level 3 context gateway
-    out, context_3 = encoder_block(out, 256, 2)
+    out = encoder_block(out, 256, 2)
+    context_3 = out
 
     # Level 4 context gateway
-    out, context_4 = encoder_block(out, 512, 2)
+    out = encoder_block(out, 512, 2)
+    context_4 = out
 
     # Level 5
-    # Dump Normalisation
-    dump, out = encoder_block(out, 1024, 2, switch=1)
+    out = encoder_block(out, 1024, 2)
     out = upsample_module(out, 512)
-    out = Conv2D(512, 1, stride=1, padding="same")(out)
-    out = InstanceNormalization()(out)
-    out = LeakyReLU()(out)
+    #out = Conv2D(512, 1, strides=1, padding="same")(out)
+    #out = InstanceNormalization()(out)
+    #out = LeakyReLU()(out)
 
     # Level 1 localistaion pathway
     # Dump segmentation layer at level 1, we don't need it yet
@@ -94,13 +77,15 @@ def build_model(input_size):
     out = upsample_module(out, 64)
 
     # Level 4 localisation pathway
-    out, dump = localisation_module(out, context_1, 64)
+    # Use seg output as new layer as we don't want a 1x1 convolution for the output
+    dump, out = localisation_module(out, context_1, 64)
 
     # Deep supervision
-    seg_1 = Conv2D(256, 3, stride=1, padding="same")(seg_1)
+    seg_1 = Conv2D(64, 1, strides=1, padding="same")(seg_1)
     seg_1 = UpSampling2D(2, interpolation="nearest")(seg_1)
-    seg_2 = Conv2D(256, 3, stride=1, padding="same")(seg_2)
+    seg_2 = Conv2D(64, 1, strides=1, padding="same")(seg_2)
     seg_layer = seg_1 + seg_2
     seg_layer = UpSampling2D(2, interpolation="nearest")(seg_layer)
     out = out + seg_layer
-    return Softmax()(out)
+    output = Softmax()(out)
+    return Model(inputs=input_layer, outputs=output, name="Improved Unet")
--- recognition/s4530770_Improved_Unet/test.py
+++ recognition/s4530770_Improved_Unet/test.py
@@ -1,4 +1,4 @@
 from Improved_Unet import build_model
 
-model = build_model((300, 400, 3))
+model = build_model((384, 512, 3))
 model.summary()
