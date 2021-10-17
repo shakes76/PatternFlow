633cb0d6987b69b084140fdcabccab9ee04b94d1
uly6mlv <uly6mlv@gmail.com>
2021-10-18 08:11:09 +1000

    Added GCNConv.py with GCN Convolution algorithm

---
+++ recognition/s4628887_Task_2_GCN/GCNConv.py
@@ -0,0 +1,211 @@
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import regularizers, activations, initializers, constraints, backend
+
+from tensorflow.keras import backend as K
+
+
+from spektral.layers import ops
+
+
+class Conv(Layer):
+  """Abstract N-D convolution layer (private, used as implementation base).
+  This layer creates a convolution kernel that is convolved
+  (actually cross-correlated) with the layer input to produce a tensor of
+  outputs. If `use_bias` is True (and a `bias_initializer` is provided),
+  a bias vector is created and added to the outputs. Finally, if
+  `activation` is not `None`, it is applied to the outputs as well.
+  Note: layer attributes cannot be modified after the layer has been called
+  once (except the `trainable` attribute).
+  Args:
+    rank: An integer, the rank of the convolution, e.g. "2" for 2D convolution.
+    filters: Integer, the dimensionality of the output space (i.e. the number
+      of filters in the convolution). Could be "None", eg in the case of
+      depth wise convolution.
+    kernel_size: An integer or tuple/list of n integers, specifying the
+      length of the convolution window.
+    strides: An integer or tuple/list of n integers,
+      specifying the stride length of the convolution.
+      Specifying any stride value != 1 is incompatible with specifying
+      any `dilation_rate` value != 1.
+    padding: One of `"valid"`,  `"same"`, or `"causal"` (case-insensitive).
+      `"valid"` means no padding. `"same"` results in padding with zeros
+      evenly to the left/right or up/down of the input such that output has the
+      same height/width dimension as the input. `"causal"` results in causal
+      (dilated) convolutions, e.g. `output[t]` does not depend on `input[t+1:]`.
+    data_format: A string, one of `channels_last` (default) or `channels_first`.
+      The ordering of the dimensions in the inputs.
+      `channels_last` corresponds to inputs with shape
+      `(batch_size, ..., channels)` while `channels_first` corresponds to
+      inputs with shape `(batch_size, channels, ...)`.
+    dilation_rate: An integer or tuple/list of n integers, specifying
+      the dilation rate to use for dilated convolution.
+      Currently, specifying any `dilation_rate` value != 1 is
+      incompatible with specifying any `strides` value != 1.
+    groups: A positive integer specifying the number of groups in which the
+      input is split along the channel axis. Each group is convolved
+      separately with `filters / groups` filters. The output is the
+      concatenation of all the `groups` results along the channel axis.
+      Input channels and `filters` must both be divisible by `groups`.
+    activation: Activation function to use.
+      If you don't specify anything, no activation is applied.
+    use_bias: Boolean, whether the layer uses a bias.
+    kernel_initializer: An initializer for the convolution kernel. If None, the 
+      default initializer (glorot_uniform) will be used. 
+    bias_initializer: An initializer for the bias vector. If None, the default
+      initializer (zeros) will be used.
+    kernel_regularizer: Optional regularizer for the convolution kernel.
+    bias_regularizer: Optional regularizer for the bias vector.
+    activity_regularizer: Optional regularizer function for the output.
+    kernel_constraint: Optional projection function to be applied to the
+        kernel after being updated by an `Optimizer` (e.g. used to implement
+        norm constraints or value constraints for layer weights). The function
+        must take as input the unprojected variable and must return the
+        projected variable (which must have the same shape). Constraints are
+        not safe to use when doing asynchronous distributed training.
+    bias_constraint: Optional projection function to be applied to the
+        bias after being updated by an `Optimizer`.
+  """
+
+  def __init__(self,
+               strides=1,
+               padding='valid',
+               data_format=None,
+               dilation_rate=1,
+               groups=1,
+               activation=None,
+               use_bias=True,
+               kernel_initializer='glorot_uniform',
+               bias_initializer='zeros',
+               kernel_regularizer=None,
+               bias_regularizer=None,
+               activity_regularizer=None,
+               kernel_constraint=None,
+               bias_constraint=None,
+               trainable=True,
+               name=None,
+               conv_op=None,
+               **kwargs):
+    super(Conv, self).__init__(
+        trainable=trainable,
+        name=name,
+        activity_regularizer=regularizers.get(activity_regularizer),
+        **kwargs)
+
+
+    self.activation = activations.get(activation)
+    self.use_bias = use_bias
+
+    self.kernel_initializer = initializers.get(kernel_initializer)
+    self.bias_initializer = initializers.get(bias_initializer)
+    self.kernel_regularizer = regularizers.get(kernel_regularizer)
+    self.bias_regularizer = regularizers.get(bias_regularizer)
+    self.kernel_constraint = constraints.get(kernel_constraint)
+    self.bias_constraint = constraints.get(bias_constraint)
+
+
+
+
+
+
+
+
+class GCNConv(Conv):
+    r"""
+    A graph convolutional layer (GCN) from the paper
+    > [Semi-Supervised Classification with Graph Convolutional Networks](https://arxiv.org/abs/1609.02907)<br>
+    > Thomas N. Kipf and Max Welling
+    **Mode**: single, disjoint, mixed, batch.
+    This layer computes:
+    $$
+        \X' = \hat \D^{-1/2} \hat \A \hat \D^{-1/2} \X \W + \b
+    $$
+    where \( \hat \A = \A + \I \) is the adjacency matrix with added self-loops
+    and \(\hat\D\) is its degree matrix.
+    **Input**
+    - Node features of shape `([batch], n_nodes, n_node_features)`;
+    - Modified Laplacian of shape `([batch], n_nodes, n_nodes)`; can be computed with
+    `spektral.utils.convolution.gcn_filter`.
+    **Output**
+    - Node features with the same shape as the input, but with the last
+    dimension changed to `channels`.
+    **Arguments**
+    - `channels`: number of output channels;
+    - `activation`: activation function;
+    - `use_bias`: bool, add a bias vector to the output;
+    - `kernel_initializer`: initializer for the weights;
+    - `bias_initializer`: initializer for the bias vector;
+    - `kernel_regularizer`: regularization applied to the weights;
+    - `bias_regularizer`: regularization applied to the bias vector;
+    - `activity_regularizer`: regularization applied to the output;
+    - `kernel_constraint`: constraint applied to the weights;
+    - `bias_constraint`: constraint applied to the bias vector.
+    """
+
+    def __init__(
+        self,
+        channels,
+        activation=None,
+        use_bias=True,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        **kwargs
+    ):
+        super().__init__(
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            **kwargs
+        )
+        self.channels = channels
+
+    def build(self, input_shape):
+        assert len(input_shape) >= 2
+        input_dim = input_shape[0][-1]
+        self.kernel = self.add_weight(
+            shape=(input_dim, self.channels),
+            initializer='random_normal',
+            name="kernel",
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint,
+        )
+        if self.use_bias:
+            self.bias = self.add_weight(
+                shape=(self.channels,),
+                initializer=self.bias_initializer,
+                name="bias",
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint,
+            )
+        self.built = True
+
+    def call(self, inputs, mask=None):
+        x, a = inputs
+
+        output = K.dot(x, self.kernel)
+        output = ops.modal_dot(a, output)
+
+        if self.use_bias:
+            output = K.bias_add(output, self.bias)
+        if mask is not None:
+            output *= mask[0]
+        output = self.activation(output)
+
+        return output
+
+    @property
+    def config(self):
+        return {"channels": self.channels}
+
