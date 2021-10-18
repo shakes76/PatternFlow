6066c4a9179904c915e139da82f31318e7899e52
David Phun <davidphun07@gmail.com>
2021-10-18 21:19:08 +0700

    Modify test script to check whether VQ VAE can produce validation scores during the training + the model can be saved after training

--- recognition/VQ_VAE_45182856/test_VQ_VAE.py
+++ recognition/VQ_VAE_45182856/test_VQ_VAE.py
@@ -6,14 +6,16 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Define directory
-img_folder = 'D:/UQ/Fourth Sem/Pattern-Analysis/Prac2/keras_png_slices_data/'
+train_img_folder = 'D:/UQ\Fourth Sem/Pattern-Analysis/Prac2/keras_png_slices_data/keras_png_slices_train'
+val_img_folder = 'D:/UQ/Fourth Sem/Pattern-Analysis/Prac2/keras_png_slices_data/keras_png_slices_validate'
+test_img_folder = 'D:/UQ/Fourth Sem/Pattern-Analysis/Prac2/keras_png_slices_data/keras_png_slices_test'
 
 # Define parameters for the data loader
 batch_size = 128
 h = 176
 w = 176
 train_loader = tf.keras.preprocessing.image_dataset_from_directory(
-    img_folder,
+    train_img_folder,
     label_mode=None,
     seed=123,
     color_mode='grayscale',
@@ -26,12 +28,22 @@ train_loader = tf.keras.preprocessing.image_dataset_from_directory(
 normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(scale=1./255, offset=-0.5)
 normalized_train_loader = train_loader.map(lambda x: normalization_layer(x))
 
-vq_vae = VQ_VAE(h, w, 1, n_encoded_features=96, embedding_dim=64, n_embeddings=128)
-# Print out the architecture of the encoder
-vq_vae.encoder.summary()
-# Print out the architecture of the decoder
-vq_vae.decoder.summary()
+vq_vae = VQ_VAE(img_h=h, img_w=w, img_c=1, n_encoded_features=96, embedding_dim=64, n_embeddings=128, recon_loss_type='SSIM', commitment_factor=0.25)
+# Print out the architecture of the VQ VAE
+vq_vae.vq_vae.summary()
+
+val_loader = tf.keras.preprocessing.image_dataset_from_directory(
+    val_img_folder,
+    label_mode=None,
+    seed=123,
+    color_mode='grayscale',
+    image_size=(h, w),
+    shuffle=True,
+    batch_size=64
+)
+normalized_val_loader = val_loader.map(lambda x: normalization_layer(x))
 
 optimizer = tf.keras.optimizers.Adam(lr=1e-3)
 vq_vae.compile(optimizer=optimizer)
-vq_vae.fit(normalized_train_loader, epochs=2)
+vq_vae.fit(normalized_train_loader, epochs=1, validation_data=normalized_val_loader)
+vq_vae.vq_vae.save('vq_vae')
