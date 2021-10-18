605508750b4b6c71b9c205308e28b0bb69949afd
SandyKang <q682520000@gmail.com>
2021-10-18 21:47:40 +1000

    model commit

--- recognition/s4561211/untitled16.py
+++ recognition/s4561211/untitled16.py
@@ -49,6 +49,22 @@ def load_data(path):
     return adj_trans, features, labels
 
 
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        
+        return F.log_softmax(x, dim=1)
+    
+
 class GraphConvolution(Module):
     
     def __init__(self, in_features, out_features):
