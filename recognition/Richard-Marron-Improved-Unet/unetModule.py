3f2971c482735ca8f13e16a92e675eabf715c4df
RichardMarron0 <r3marron@gmail.com>
2021-10-18 11:38:12 +1000

    Added first context block to Unet model

--- recognition/Richard-Marron-Improved-Unet/unetModule.py
+++ recognition/Richard-Marron-Improved-Unet/unetModule.py
@@ -7,20 +7,49 @@
 """
 
 import tensorflow as tf
+from tensorflow.keras import layers
 
 class ImprovedUNet():
     """Implements the Improved U-Net Model"""
     def __init__(self, input_shape: tuple, learning_rate: float=1e-4, 
-                 optimiser=tf.keras.optimizers.Adam(1e-4), loss: str="CategoricalCrossentropy"):
+                 optimiser=tf.keras.optimizers.Adam(1e-4), loss: str="CategoricalCrossentropy",
+                 leaky: float=1e-2, drop: float=3e-1):
+        
+        # Input must be 3-dimensional
+        assert len(input_shape) == 3
         self.input_shape = input_shape
         self.learning_rate = learning_rate
         self.optimizer = optimiser
         self.loss = loss
+        
+        # Leaky ReLU rate
+        self.leaky = leaky
+        # Drop-out rate
+        self.drop = drop
         self.metric = self.dice_function
     
     def model(self):
-        """Create the Improved U-Net Model"""
-        pass
+        """
+        Create and return the Improved U-Net Model.
+        Model based on design from the segmentation
+            paper, https://arxiv.org/pdf/1802.10508v1.pdf
+        """
+        ################### DOWNSAMPLING ###################
+        in_layer = layers.Input(shape=self.input_shape)
+        
+        # Initial convolution layer
+        block_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(in_layer)
+        
+        # Context Module (Pre-activation residual block)
+        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(block_1)
+        ctx_1 = layers.LeakyReLU(alpha=self.leaky)(ctx_1)
+        ctx_1 = layers.Dropout(rate=self.drop)(ctx_1)
+        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(ctx_1)
+        ctx_1 = layers.LeakyReLU(alpha=self.leaky)(ctx_1)
+        ctx_1 = layers.Dropout(rate=self.drop)(ctx_1)
+        
+        # Merge/Connect path before context block to after block
+        ctx_1 = layers.Add()([block_1, ctx_1])
     
     def dice_function(self, y_true, y_pred):
         """
@@ -29,7 +58,7 @@ class ImprovedUNet():
                 y_true : The true values to compare
                 y_pred : The predicted values to compare
             
-            Return : Dice coefficient of the images
+            Return : Dice coefficient between y_true and y_pred
         """
         # Convert the milti-dim. tensors into vectors
         y_true = tf.keras.flatten(y_true)
