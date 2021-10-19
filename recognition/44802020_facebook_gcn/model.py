72de9a6bf316298223765d1836eb6bf31f98c4cd
johnvparsons99 <62982818+johnvparsons99@users.noreply.github.com>
2021-10-20 07:36:35 +1000

    Finished code to convert data to tf.SparseTensor

--- recognition/44802020_facebook_gcn/model.py
+++ recognition/44802020_facebook_gcn/model.py
@@ -8,6 +8,22 @@ def get_neighbours(self, node):
     print(node)
 
 
+def denseNDArrayToSparseTensor(arr):
+    idx = np.where(arr != 0.0)
+    return tf.SparseTensor(np.vstack(idx).T, arr[idx], arr.shape)
+
+
+def convert_sparse_matrix_to_sparse_tensor(X):
+    coo = X.tocoo()
+    indices = np.mat([coo.row, coo.col]).transpose()
+    return tf.SparseTensor(indices, coo.data, coo.shape)
+
+
+def coo_matrix_to_sparse_tensor(coo):
+    indices = np.mat([coo.row, coo.col]).transpose()
+    return tf.SparseTensor(indices, coo.data, coo.shape)
+
+
 def Model(input_data):
 
     # This is the model
@@ -35,25 +51,32 @@ def Model(input_data):
     # node = input_data[0]
 
     # Construct Adjacency matrix
-    A = spr.csr_matrix((ones, (page_one, page_two)))
+    A = spr.coo_matrix((ones, (page_one, page_two)))
 
     print("Running Model")
-
     print(A)
     print("=====  =====")
     print(A.data)
     print(A.dtype)
-    print(A[2])
-    print(type(A[2]))
+    print(A.shape)
+    print(A.col)
+    print(A.row)
+
+    A_spr = coo_matrix_to_sparse_tensor(A)
+
+    # feats_sparse = spr.csr_matrix(feats)
+    # A_sp = tf.sparse.from_dense(A)
+    # F = tf.sparse.from_dense(feats)
+    # print(F.shape)
 
     print("===== Result =====")
 
+
     # newA = tf.sparse.SparseTensor(A)
     #
-    # res = tf.sparse.sparse_dense_matmul(A, feats)
-    # print(res.dtype)
-    # print(res)
-    # print(res.graph)
+    res = tf.sparse.sparse_dense_matmul(tf.cast(A_spr, float), feats)
+    print(res.dtype)
+    print(res)
 
 
 
