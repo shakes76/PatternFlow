4ab7edded1dd9ce4b8e1b5a26feab90ca6dbb183
克勞德 <masatada67890@gmail.com>
2021-10-19 18:36:47 +1000

    Add load and preprocess RGB images function

--- recognition/Segmentation-ISICs-ImprovedUnet-s4623205/run_Unet_ISICs.py
+++ recognition/Segmentation-ISICs-ImprovedUnet-s4623205/run_Unet_ISICs.py
@@ -1,4 +1,4 @@
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import cv2
 import sys
 from math import ceil
@@ -57,6 +57,45 @@ def get_min_imageshape(path):
     return min_shape
 
 
+def load_rgbimages(path, height, width):
+    """
+    Function to load and preprocess rgb images from path to memory
+
+    Parameters
+    ----------
+    path : string
+      Directory of where the target images are
+    height : integer
+      Parameter to resize the image height
+    width : integer
+      Parameter to resize the image width
+
+    Returns
+    -------
+    images : float32 numpy array
+      A data type float32 numpy array of the preprocessed images
+    """
+    img_paths = sorted(glob.glob(path))
+    length = len(img_paths)
+    count = 0
+    images = []
+
+    for img_path in img_paths:
+        # OpenCV reads images as BGR, convert it to RGB when reading
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (width, height))
+        b, g, r = cv2.split(img)
+        img = cv2.merge([r, g, b])
+        images.append(img)
+        progressbar(count, length)
+        count += 1
+
+    images = np.array(images, np.float32)
+    images = images / 255.
+
+    return images
+
+
 def main():
     image_path = r"C:\Users\masa6\Desktop\UQMasterDS\COMP3710\OpenProject\Project\Data\ISIC2018_Task1-2_Training_Input_x2\*.jpg"
 
@@ -67,6 +106,12 @@ def main():
     img_width = min_img_shape[1]
     print("\nMin Image Height:", img_height)
     print("Min Image Width:", img_width)
+
+    print("\nLoad and preprocess RGB images...")
+    images = load_rgbimages(image_path, img_height, img_width)
+    print("\nPlotting the first preprocessed RGB image...")
+    plt.imshow(images[0])
+    plt.show()
 
 
 if __name__ == "__main__":
