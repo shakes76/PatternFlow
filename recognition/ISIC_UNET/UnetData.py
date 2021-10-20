163b7a8e29ec256eb5d69330ac994358cf7ca2bc
Mark Coulon <s4579795@student.uq.edu.au>
2021-10-20 14:04:01 +1000

    Created functions to load the data in get it ready to be used by the model

--- recognition/ISIC_UNET/UnetData.py
+++ recognition/ISIC_UNET/UnetData.py
@@ -1,79 +1,71 @@
 import tensorflow as tf
-import os
 import glob
-import sklearn.utils
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot
 
 
-
-def load_image(file, shape):
+def load_image(file):
+    # loads the jpeg images into usable images for the model
     img = tf.io.read_file(file)
-    img = tf.image.decode_jpeg(img, channels=3)  # so it is in colour
-    img = tf.image.resize(img, shape)
+    img = tf.image.decode_jpeg(img, channels=1)
+    img = tf.image.resize(img, (256, 256))
     img = tf.cast(img, tf.float32) / 255.0  # need to divide
     return img
 
 
-def load_seg(file, shape):
+def load_seg(file):
+    # loads the png segmentation images into usable images for the model
     seg = tf.io.read_file(file)
     seg = tf.image.decode_png(seg, channels=1)  # gray scale
-    seg = tf.image.resize(seg, shape)
-    seg = tf.cast(seg, tf.float32)  # need to divide
-    seg = tf.math.round(seg / 255.0)  # need to put in binary
-    binary = (seg == [0.0, 1.0])  # @@@ dont think i need it
-    binary_seg = tf.cast(binary, tf.float32)
-    return binary_seg
+    seg = tf.image.resize(seg, (256, 256))
+    seg = tf.cast(seg, tf.float32) / 255.0  # need to divide
+    seg = tf.math.round(seg)  # round so that it is binary
+    return seg
 
 
 def process_images(image_path, mask_path):
-    size = (256, 256)
-    image = load_image(image_path, size)
-    segmentation = load_seg(mask_path, size)
+    # load both the images and segmentation images
+    image = load_image(image_path)
+    segmentation = load_seg(mask_path)
 
+    # reshape them both to be 256, 256, 1
+    image = tf.reshape(image, (256, 256, 1))
+    segmentation = tf.reshape(segmentation, (256, 256, 1))
     return image, segmentation
 
 
-# https://matplotlib.org/stable/tutorials/introductory/images.html
-def view_images(data, num):
-    matplotlib.pyplot.figure(figsize=(4, num*4))
-    loc = 0
-    for img, label in data.take(num):
-        matplotlib.pyplot.subplot(num, 2, 2*loc+1)
-        matplotlib.pyplot.imshow(img)
-        matplotlib.pyplot.subplot(num, 2, 2*loc+2)
-        matplotlib.pyplot.imshow(label[:,:,1], cmap='gray')
-        loc = loc + 1
-    matplotlib.pyplot.show()
-
-
-def prepare_images():  # x_path, mask_path
+def prepare_images(max_images):
+    # get the path of the images
     x_images = 'C:\\Users\\Mark\\Downloads\\ISIC2018_Task1-2_Training_Data\\ISIC2018_Task1-2_Training_Input_x2\\*.jpg'
     mask_images = 'C:\\Users\\Mark\\Downloads\\ISIC2018_Task1-2_Training_Data\\ISIC2018_Task1_Training_GroundTruth_x2\\*.png'
     x_images = sorted(glob.glob(x_images))
     mask_images = sorted(glob.glob(mask_images))
+    # take only a certain number of images to be used
+    new_x = x_images[0:max_images]
+    new_mask = mask_images[0:max_images]
 
-    print(len(x_images))
-    print(len(mask_images))
+    print("Length of total images split 49% in training, 30% testing and 21% validation:")
+    print(len(new_x))
+    print(len(new_mask))
+    # split the images into training, testing and validation
+    # 49% training, 30% testing, 21% val
+    X_train, X_test, y_train, y_test = train_test_split(new_x, new_mask, test_size=0.3, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=1)
 
-    X_train, X_test, y_train, y_test = train_test_split(x_images, mask_images, test_size=0.2, random_state=1)
-
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
-
+    # Put the images into a dataset from a list
     train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
     test_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test))
     val_ds = tf.data.Dataset.from_tensor_slices((X_val, y_val))
 
+    # shuffle the images
     final_train = train_ds.shuffle(len(X_train))
     final_test = test_ds.shuffle(len(X_test))
     final_val = val_ds.shuffle(len(X_val))
 
-
-
+    # put the images into a map
     final_train = final_train.map(process_images)
     final_test = final_test.map(process_images)
     final_val = final_val.map(process_images)
 
-    view_images(final_train, 3)
     return final_train, final_test, final_val
 
