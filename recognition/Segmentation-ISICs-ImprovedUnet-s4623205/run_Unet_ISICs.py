0dc8dde12d654861f31a70116795515e767edfbe
克勞德 <masatada67890@gmail.com>
2021-10-20 18:31:43 +1000

    Remove not needed test commands in driver script

--- recognition/Segmentation-ISICs-ImprovedUnet-s4623205/run_Unet_ISICs.py
+++ recognition/Segmentation-ISICs-ImprovedUnet-s4623205/run_Unet_ISICs.py
@@ -26,42 +26,6 @@ def main():
     split_ratio = [0.6, 0.2, 0.2]
     X_train, X_val, X_test, y_train, y_val, y_test = train_val_test_split(image_path, mask_path, new_imageshape, new_imageshape, split_ratio, randomstate=42)
 
-    # Plot the data
-    print("\nPlotting the first preprocessed RGB image and preprocessed mask of the train, validation and test set...")
-    # First data in train set
-    plt.subplot(3, 2, 1)
-    plt.imshow(X_train[0])
-    plt.title("First train image")
-    plt.subplot(3, 2, 2)
-    plt.imshow(y_train[0, :, :, 0], cmap="gray")
-    plt.title("First train mask")
-    # First data in validation set
-    plt.subplot(3, 2, 3)
-    plt.imshow(X_val[0])
-    plt.title("First validation image")
-    plt.subplot(3, 2, 4)
-    plt.imshow(y_val[0, :, :, 0], cmap="gray")
-    plt.title("First validation mask")
-    # First data in test set
-    plt.subplot(3, 2, 5)
-    plt.imshow(X_test[0])
-    plt.title("First test image")
-    plt.subplot(3, 2, 6)
-    plt.imshow(y_test[0, :, :, 0], cmap="gray")
-    plt.title("First test mask")
-    plt.tight_layout()
-    plt.show()
-
-    # Test the dice coefficient and dice loss function
-    # This is expected to be 1
-    print("\nDice coefficient between the first train mask:", dice_coef(y_train[0, :, :, 0], y_train[0, :, :, 0]).numpy())
-    # This is expected to be 0
-    print("Dice loss between the first train mask:", dice_loss(y_train[0, :, :, 0], y_train[0, :, :, 0]).numpy())
-    # This is expected to be between 0 and 1, but not 1
-    print("Dice coefficient between the first train mask and first test mask:", dice_coef(y_train[0, :, :, 0], y_test[0, :, :, 0]).numpy())
-    # This is expected to be between 0 and 1, but not 0
-    print("Dice loss between the first train mask and first test mask:", dice_loss(y_train[0, :, :, 0], y_test[0, :, :, 0]).numpy())
-
     # Construct baseline Unet model
     print("\nConstructing model...")
     model = SegModel((new_imageshape, new_imageshape, 3), random_seed=42, model="Unet")
