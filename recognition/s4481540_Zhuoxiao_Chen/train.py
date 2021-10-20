60874da209beb3e18af5c53f7ff1f952c8412e73
Zhuoxiao Chen <ivanchenzx@outlook.com>
2021-10-20 17:39:18 +1000

    import required libraries, set up required arguments by argparse.

--- recognition/s4481540_Zhuoxiao_Chen/train.py
+++ recognition/s4481540_Zhuoxiao_Chen/train.py
@@ -1,5 +1,22 @@
-import torch
 import argparse
+import random
+import math
+
+from dataset import MultiResolutionDataset
+from model import StyledGenerator, Discriminator
+
+import torch
+from torch import nn, optim
+from torch.nn import functional as F
+from torch.autograd import Variable, grad
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms, utils
+
+from tqdm import tqdm
+import numpy as np
+from PIL import Image
+
+
 # Hi
 print("Hello, world")
 print(torch.cuda.is_available())
@@ -7,14 +24,23 @@ print(torch.cuda.is_available())
 
 
 if __name__ == '__main__':
+    code_size = 512
+    batch_size = 16
+    n_critic = 1
+
     parser = argparse.ArgumentParser(description='Progressive Growing of GANs')
+
+    parser.add_argument('path', type=str, help='path of specified dataset')
+    parser.add_argument(
+        '--phase',
+        type=int,
+        default=40_000,
+        help='number of samples used for each training phases',
+    )
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--sched', action='store_true',
-                        help='use lr scheduling')
-    parser.add_argument('--init_size', default=8, type=int,
-                        help='initial image size')
-    parser.add_argument('--max_size', default=1024, type=int,
-                        help='max image size')
+    parser.add_argument('--sched', action='store_true', help='use lr scheduling')
+    parser.add_argument('--init_size', default=8, type=int, help='initial image size')
+    parser.add_argument('--max_size', default=512, type=int, help='max image size')
     parser.add_argument(
         '--ckpt', default=None, type=str, help='load from previous checkpoints'
     )
@@ -23,3 +49,15 @@ if __name__ == '__main__':
         action='store_true',
         help='use activate in from_rgb (original implementation)',
     )
+    parser.add_argument(
+        '--mixing', action='store_true', help='use mixing regularization'
+    )
+    parser.add_argument(
+        '--loss',
+        type=str,
+        default='wgan-gp',
+        choices=['wgan-gp', 'r1'],
+        help='class of gan loss',
+    )
+
+    args = parser.parse_args()
