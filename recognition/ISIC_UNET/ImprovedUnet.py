cd5d7fad038b9baefd5b39688934771b62511f43
Mark Coulon <s4579795@student.uq.edu.au>
2021-10-20 14:03:12 +1000

    Added comments to the Improved Unet

--- recognition/ISIC_UNET/ImprovedUnet.py
+++ recognition/ISIC_UNET/ImprovedUnet.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 
+# Improved U-Net based off https://arxiv.org/pdf/1802.10508v1.pdf
 def IUNET_model():
     inputs = tf.keras.Input(shape=[256, 256, 1])
 
+    # go down the U
     # step 1
     conv1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(inputs)
-
     context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(conv1)
     drop1 = tf.keras.layers.Dropout(0.3)(context1)
     context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(drop1)
@@ -13,9 +14,7 @@ def IUNET_model():
     sum1 = tf.math.add(conv1, context1)
 
     # step 2
-
     stride2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", strides=2, activation="relu")(sum1)
-
     context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(stride2)
     drop2 = tf.keras.layers.Dropout(0.3)(context2)
     context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(drop2)
@@ -23,9 +22,7 @@ def IUNET_model():
     sum2 = tf.math.add(stride2, context2)
 
     # step 3
-
     stride3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", strides=2, activation="relu")(sum2)
-
     context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(stride3)
     drop3 = tf.keras.layers.Dropout(0.3)(context3)
     context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(drop3)
@@ -33,18 +30,15 @@ def IUNET_model():
     sum3 = tf.math.add(stride3, context3)
 
     # step 4
-
     stride4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", strides=2, activation="relu")(sum3)
-
     context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(stride4)
     drop4 = tf.keras.layers.Dropout(0.3)(context4)
     context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(drop4)
 
     sum4 = tf.math.add(stride4, context4)
 
-    # step 5 lowest one
+    # step 5 lowest one, bottom of the U
     stride5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", strides=2, activation="relu")(sum4)
-
     context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(stride5)
     drop5 = tf.keras.layers.Dropout(0.3)(context5)
     context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(drop5)
@@ -53,6 +47,7 @@ def IUNET_model():
 
     up5 = tf.keras.layers.UpSampling2D(size=(2, 2))(sum5)
 
+    # Go back up the U
     # step 6 up 1
     conc6 = tf.keras.layers.concatenate([up5, sum4])
     loc6 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu") (conc6)
@@ -74,18 +69,19 @@ def IUNET_model():
     up8 = tf.keras.layers.UpSampling2D(size=(2, 2))(loc8)
 
     seg8 = tf.math.add(uploc7, loc8)
+    upseg8 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding="same")(seg8)
 
     # step 9 up 4
     conc9 = tf.keras.layers.concatenate([up8, sum1])
     conv9 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(conc9)
 
-    upseg8 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=3, strides=2, padding="same")(seg8)
     seg9 = tf.math.add(upseg8, conv9)
 
+    # do a sigmoid activation instead of softmax
     sig = tf.keras.layers.Conv2D(filters=1, kernel_size=1, activation='sigmoid')(seg9)
 
+    # create the model
     finalModel = tf.keras.Model(inputs, sig)
-
     return finalModel
 
 
