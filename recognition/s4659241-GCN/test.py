0994e9085955c8b961174beea4a07c82dae03ba0
cici <cici@cicis-Mac.local>
2021-10-21 12:28:24 +0800

    Add normalize function in test file.

--- recognition/s4659241-GCN/test.py
+++ recognition/s4659241-GCN/test.py
@@ -13,3 +13,15 @@ num_classes = len(np.unique(target))
 num_nodes = features.shape[0]
 num_features = features.shape[1]
 num_edges = edges.shape[0]
+
+def normalize(matrix):
+    row_sum = np.array(matrix.sum(1))
+    # 1/sum
+    sum_inv = np.power(row_sum, -1).flatten() 
+    # if it's infinite, change to 0
+    sum_inv[np.isinf(sum_inv)] = 0.   
+    # build diagonal matrix
+    diag_mat = sp.diags(sum_inv)
+    # normalized matrix: D^-1 * matrix
+    normalized = diag_mat.dot(matrix) 
+    return normalized
