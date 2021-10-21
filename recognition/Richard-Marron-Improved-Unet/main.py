Fixed normalisation & updated model loss/layers

recognition/Richard-Marron-Improved-Unet/main.py
@@ -13,15 +13,21 @@ import cv2
 from sklearn.model_selection import train_test_split
 from unetModule import ImprovedUNet
 
-def normalise_images(image_set):
+def normalise_images(image_set, ground_truth=False):
     """
     Normalise images between 0 and 1
         Params:
             image_set : Numpy array of images
+            ground_truth : If the set is the ground truth data
             
         Return : Normalised version of image_set
     """
-    return image_set / 255.0
+    if not ground_truth:
+        # Normalise without rounding
+        return image_set / 255.0
+    else:
+        # Round the normalisation since we want just 0 or 1 values
+        return np.around(image_set / 255.0)
 
 def load_images(path: str, ground_truth: bool=False, truncate: bool=False):
     """
@@ -47,11 +53,20 @@ def load_images(path: str, ground_truth: bool=False, truncate: bool=False):
         # Only take the first 1/4 of the images
         img_paths = img_paths[:len(img_paths)//4]
     print("Successfully loaded paths!") 
-    print("Converting images into numpy arrays...")
     # Read in images from path and return numpy array
-    return np.array([cv2.cvtColor(cv2.resize(cv2.imread(path, cv2.IMREAD_COLOR),
-                                             dsize=(512, 384)), 
-                                  cv2.COLOR_BGR2RGB) for path in img_paths], dtype=np.float32)
+    if not ground_truth:
+        # Read in RGB and resize to 512x384
+        print("Converting training images into numpy arrays...")
+        return np.array([cv2.cvtColor(cv2.resize(cv2.imread(path, cv2.IMREAD_COLOR),
+                                                 dsize=(512, 384)), 
+                                      cv2.COLOR_BGR2RGB) for path in img_paths], dtype=np.float32)
+    else:
+        # Read in grayscale and resize to 512x384
+        print("Converting ground truth images into numpy arrays...")
+        return np.expand_dims(np.array([cv2.resize(cv2.imread(path, cv2.IMREAD_GRAYSCALE),
+                                                             dsize=(512, 384)) for path in img_paths], 
+                                                 dtype=np.float32), 
+                                        axis=-1)
 
 def get_splits(input_set, truth_set):
     """
@@ -87,10 +102,10 @@ def main(debugging=False):
     """
     # Load normalised images
     input_images = normalise_images(load_images(path="./Data/ISIC2018_Task1-2_Training_Input_x2/", 
-                                                ground_truth=False, truncate=debugging))
+                                                ground_truth=False, truncate=debugging), ground_truth=False)
     # Load ground truth segmentation
-    gt_images = normalise_images(load_images(path="./Data/ISIC2018_Task1_Training_GroundTruth_x2/", 
-                                             ground_truth=True, truncate=debugging))
+    gt_images = normalise_images(load_images(path="./Data/ISIC2018_Task1_Training_GroundTruth_x2/",
+                                             ground_truth=True, truncate=debugging), ground_truth=True)
         
     # Print out some useful information
     print(f"Total input images: {input_images.shape}")
@@ -120,10 +135,18 @@ def main(debugging=False):
     print(f"Test Set Shape  \t: {test[0].shape}")
     print(f"Validation Set Shape \t: {valid[0].shape}")
     
-    hist = unet_model.fit(train[0], train[1], validation_data=valid, batch_size=10, epochs=100)
+    load_weights = False
+    if not load_weights:
+        hist = unet_model.fit(train[0], train[1], validation_data=valid, batch_size=10, epochs=100)
+    else:
+        # Load weights into model
+        unet_model.load_weights("./weights/test.h5")
     
-    unet_model.evaluate(test[0], test[1])
-    unet_model.save_weights("./weights/test")
+    unet_model.evaluate(test[0], test[1], batch_size=10)
+    test_image = unet_model.predict(test[0][0])
+    plt.figure()
+    plt.imshow(test_image)
+    unet_model.save_weights("./weights/test.h5")
     print("STOP")
     
 

recognition/Richard-Marron-Improved-Unet/unetModule.py
@@ -7,12 +7,12 @@
 """
 
 import tensorflow as tf
-from tensorflow.keras import layers, Model, backend
+from tensorflow.keras import layers, Model, backend, losses
 
 class ImprovedUNet():
     """Implements the Improved U-Net Model"""
     def __init__(self, input_shape: tuple, learning_rate: float=1e-4, 
-                 optimiser=tf.keras.optimizers.Adam(1e-4), loss: str="CategoricalCrossentropy",
+                 optimiser=tf.keras.optimizers.Adam(1e-4), loss=losses.BinaryCrossentropy(),
                  leaky: float=1e-2, drop: float=3e-1):
         
         # Input must be 3-dimensional
@@ -38,15 +38,16 @@ class ImprovedUNet():
         in_layer = layers.Input(shape=self.input_shape)
         
         # Initial convolution layer
-        block_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(in_layer)
+        block_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same")(in_layer)
         
         # First Context Module (Pre-activation residual block)
-        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(block_1)
+        ctx_1 = layers.BatchNormalization()(block_1)
         ctx_1 = layers.LeakyReLU(alpha=self.leaky)(ctx_1)
+        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same")(ctx_1)
         ctx_1 = layers.Dropout(rate=self.drop)(ctx_1)
-        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(ctx_1)
+        ctx_1 = layers.BatchNormalization()(ctx_1)
         ctx_1 = layers.LeakyReLU(alpha=self.leaky)(ctx_1)
-        ctx_1 = layers.Dropout(rate=self.drop)(ctx_1)
+        ctx_1 = layers.Conv2D(filters=16, kernel_size=3, padding="same")(ctx_1)
         
         # Merge/Connect path before context block to after block
         ctx_1 = layers.Add()([block_1, ctx_1])
@@ -56,57 +57,61 @@ class ImprovedUNet():
                                  padding="same", activation="relu")(ctx_1)
         
         # Second Context Module (Pre-activation residual block)
-        ctx_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(s_conv_1)
+        ctx_2 = layers.BatchNormalization()(s_conv_1)
         ctx_2 = layers.LeakyReLU(alpha=self.leaky)(ctx_2)
+        ctx_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same")(ctx_2)
         ctx_2 = layers.Dropout(rate=self.drop)(ctx_2)
-        ctx_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(ctx_2)
+        ctx_2 = layers.BatchNormalization()(ctx_2)
         ctx_2 = layers.LeakyReLU(alpha=self.leaky)(ctx_2)
-        ctx_2 = layers.Dropout(rate=self.drop)(ctx_2)
+        ctx_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same")(ctx_2)
         
         # Merge/Connect path before context block to after block
         ctx_2 = layers.Add()([s_conv_1, ctx_2])
         
         # Go down a level in the U-Net using strided convolution
         s_conv_2 = layers.Conv2D(filters=64, kernel_size=3, strides=2,
-                                 padding="same", activation="relu")(ctx_2)
+                                 padding="same")(ctx_2)
         
         # Third Context Module (Pre-activation residual block)
-        ctx_3 = layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(s_conv_2)
+        ctx_3 = layers.BatchNormalization()(s_conv_2)
         ctx_3 = layers.LeakyReLU(alpha=self.leaky)(ctx_3)
+        ctx_3 = layers.Conv2D(filters=64, kernel_size=3, padding="same")(ctx_3)
         ctx_3 = layers.Dropout(rate=self.drop)(ctx_3)
-        ctx_3 = layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(ctx_3)
+        ctx_3 = layers.BatchNormalization()(ctx_3)
         ctx_3 = layers.LeakyReLU(alpha=self.leaky)(ctx_3)
-        ctx_3 = layers.Dropout(rate=self.drop)(ctx_3)
+        ctx_3 = layers.Conv2D(filters=64, kernel_size=3, padding="same")(ctx_3)
         
         # Merge/Connect path before context block to after block
         ctx_3 = layers.Add()([s_conv_2, ctx_3])
         
         # Go down a level in the U-Net using strided convolution
         s_conv_3 = layers.Conv2D(filters=128, kernel_size=3, strides=2,
-                                 padding="same", activation="relu")(ctx_3)
+                                 padding="same")(ctx_3)
         
         # Fourth Context Module (Pre-activation residual block)
-        ctx_4 = layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(s_conv_3)
+        ctx_4 = layers.BatchNormalization()(s_conv_3)
         ctx_4 = layers.LeakyReLU(alpha=self.leaky)(ctx_4)
+        ctx_4 = layers.Conv2D(filters=128, kernel_size=3, padding="same")(ctx_4)
         ctx_4 = layers.Dropout(rate=self.drop)(ctx_4)
-        ctx_4 = layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(ctx_4)
+        ctx_4 = layers.BatchNormalization()(ctx_4)
         ctx_4 = layers.LeakyReLU(alpha=self.leaky)(ctx_4)
-        ctx_4 = layers.Dropout(rate=self.drop)(ctx_4)
+        ctx_4 = layers.Conv2D(filters=128, kernel_size=3, padding="same")(ctx_4)
         
         # Merge/Connect path before context block to after block
         ctx_4 = layers.Add()([s_conv_3, ctx_4])
         
         # Go down a level in the U-Net using strided convolution
         s_conv_4 = layers.Conv2D(filters=256, kernel_size=3, strides=2,
-                                 padding="same", activation="relu")(ctx_4)
+                                 padding="same")(ctx_4)
         
         # Fifth Context Module (Pre-activation residual block)
-        ctx_5 = layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(s_conv_4)
+        ctx_5 = layers.BatchNormalization()(s_conv_4)
         ctx_5 = layers.LeakyReLU(alpha=self.leaky)(ctx_5)
+        ctx_5 = layers.Conv2D(filters=256, kernel_size=3, padding="same")(ctx_5)
         ctx_5 = layers.Dropout(rate=self.drop)(ctx_5)
-        ctx_5 = layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(ctx_5)
+        ctx_5 = layers.BatchNormalization()(ctx_5)
         ctx_5 = layers.LeakyReLU(alpha=self.leaky)(ctx_5)
-        ctx_5 = layers.Dropout(rate=self.drop)(ctx_5)
+        ctx_5 = layers.Conv2D(filters=256, kernel_size=3, padding="same")(ctx_5)
         
         # Merge/Connect path before context block to after block
         ctx_5 = layers.Add()([s_conv_4, ctx_5])
@@ -114,76 +119,70 @@ class ImprovedUNet():
         ################### UPSAMPLING ###################
         # First Upsampling Module 
         up_1 = layers.Conv2DTranspose(filters=128, kernel_size=3, padding="same", 
-                                      strides=2, activation="relu")(ctx_5)
+                                      strides=2)(ctx_5)
         up_1 = layers.LeakyReLU(alpha=self.leaky)(up_1)
         
         # Link across U-Net via concatenation to define first localisation module
         loc_1 = layers.concatenate([ctx_4, up_1])
-        loc_1 = layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(loc_1)
-        loc_1 = layers.LeakyReLU(alpha=self.leaky)(loc_1)
-        # Halve the number of filters
-        loc_1 = layers.Conv2D(filters=64, kernel_size=1, padding="same", activation="relu")(loc_1)
+        loc_1 = layers.Conv2D(filters=128, kernel_size=3, padding="same")(loc_1)
+        loc_1 = layers.BatchNormalization()(loc_1)
         loc_1 = layers.LeakyReLU(alpha=self.leaky)(loc_1)
 
         # Second Upsampling Module 
         up_2 = layers.Conv2DTranspose(filters=64, kernel_size=3, padding="same", 
-                                      strides=2, activation="relu")(loc_1)
+                                      strides=2)(loc_1)
         up_2 = layers.LeakyReLU(alpha=self.leaky)(up_2)
         
         # Link across U-Net via concatenation to define second localisation module
         loc_2 = layers.concatenate([ctx_3, up_2])
-        loc_2 = layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(loc_2)
+        loc_2 = layers.Conv2D(filters=64, kernel_size=3, padding="same")(loc_2)
+        loc_2 = layers.BatchNormalization()(loc_2)
         loc_2 = layers.LeakyReLU(alpha=self.leaky)(loc_2)
         
         # Add peripheral connection for segmentation and then upscale
-        seg_1 = layers.Conv2D(filters=3, kernel_size=3, padding="same", activation="softmax")(loc_2)
-        seg_1 = layers.Conv2DTranspose(filters=3, kernel_size=3, padding="same", 
-                                       strides=2, activation="relu")(seg_1)
+        seg_1 = layers.Conv2D(filters=1, kernel_size=3, padding="same", activation="softmax")(loc_2)
+        seg_1 = layers.Conv2DTranspose(filters=1, kernel_size=3, padding="same", 
+                                       strides=2)(seg_1)
         
         # Halve the number of filters
-        loc_2 = layers.Conv2D(filters=32, kernel_size=1, padding="same", activation="relu")(loc_2)
+        loc_2 = layers.Conv2D(filters=32, kernel_size=1, padding="same")(loc_2)
         loc_2 = layers.LeakyReLU(alpha=self.leaky)(loc_2)
         
         # Third Upsampling Module 
         up_3 = layers.Conv2DTranspose(filters=32, kernel_size=3, padding="same", 
-                                      strides=2, activation="relu")(loc_2)
+                                      strides=2)(loc_2)
         up_3 = layers.LeakyReLU(alpha=self.leaky)(up_3)
         
         # Link across U-Net via concatenation to define third localisation module
         loc_3 = layers.concatenate([ctx_2, up_3])
-        loc_3 = layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(loc_3)
+        loc_3 = layers.Conv2D(filters=32, kernel_size=3, padding="same")(loc_3)
+        loc_3 = layers.BatchNormalization()(loc_3)
         loc_3 = layers.LeakyReLU(alpha=self.leaky)(loc_3)
         
         # Add peripheral connection for segmentation
-        seg_2 = layers.Conv2D(filters=3, kernel_size=3, padding="same", activation="softmax")(loc_3)
+        seg_2 = layers.Conv2D(filters=1, kernel_size=3, padding="same", activation="softmax")(loc_3)
         
         # Sum first segmentation layer with this one and upscale
         seg_partial = layers.Add()([seg_1, seg_2])
         seg_partial = layers.Conv2DTranspose(filters=3, kernel_size=3, padding="same", 
-                                             strides=2, activation="relu")(seg_partial)
-        
-        # Halve the number of filters
-        loc_3 = layers.Conv2D(filters=16, kernel_size=1, padding="same", activation="relu")(loc_3)
-        loc_3 = layers.LeakyReLU(alpha=self.leaky)(loc_3)
+                                             strides=2)(seg_partial)
         
         # Fourth Upsampling Module 
         up_4 = layers.Conv2DTranspose(filters=16, kernel_size=3, padding="same", 
-                                      strides=2, activation="relu")(loc_3)
+                                      strides=2)(loc_3)
         up_4 = layers.LeakyReLU(alpha=self.leaky)(up_4)
         
-        # Link across U-Net via concatenation and define the last convolution block
-        # TODO: Error shape mismatch [(None, 384, 511, 16), (None, 384, 512, 16)]
+        # Convolution block
         block_2 = layers.concatenate([ctx_1, up_4])
-        block_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(block_2)
-        
+        block_2 = layers.Conv2D(filters=32, kernel_size=3, padding="same")(block_2)
         
         # Segmentation layer
-        seg_3 = layers.Conv2D(filters=3, kernel_size=3, padding="same", activation="softmax")(block_2)
+        seg_3 = layers.Conv2D(filters=1, kernel_size=3, padding="same", activation="softmax")(block_2)
         # Sum all segmentation layers
         total = layers.Add()([seg_partial, seg_3])
         
         # Define output layer
-        out_layer = layers.Conv2D(filters=3, kernel_size=3, padding="same", activation="softmax")(total)
+        out_layer = layers.Conv2D(filters=1, kernel_size=3, padding="same", activation="softmax")(total)
         
         # Return the model created from all of these layers
         return Model(in_layer, out_layer)
@@ -202,4 +201,4 @@ class ImprovedUNet():
         y_pred = backend.flatten(y_pred)
         
         # Calculate the dice coefficient over binary vectors
-        return 2*backend.sum(y_true*y_pred)/(backend.sum(backend.square(y_true)) + backend.sum(backend.square(y_pred)))
+        return (2.0*backend.sum(y_true*y_pred))/(backend.sum(backend.square(y_true)) + backend.sum(backend.square(y_pred)))