e15ade8cf1b30716aac522ce731cd74efc01c7bd
克勞德 <masatada67890@gmail.com>
2021-10-21 13:57:41 +1000

    Add a construct Improved Unet function

--- recognition/Segmentation-ISICs-ImprovedUnet-s4623205/SegmentaionModel.py
+++ recognition/Segmentation-ISICs-ImprovedUnet-s4623205/SegmentaionModel.py
@@ -2,7 +2,7 @@ import os
 # Suppress the INFO message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Conv2DTranspose, concatenate
+from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Conv2DTranspose, concatenate, LeakyReLU, Dropout, Add, UpSampling2D
 from tensorflow.keras.initializers import he_normal
 from tensorflow.keras.optimizers import Adam
 
@@ -19,10 +19,19 @@ class SegModel:
         random_seed : integer
           The random seed to decide the random weights in the model
         model : string
-          The parameter to decide which model to use. "Unet" is the baseline Unet model.
+          The parameter to decide which model to use.
+          "Unet" is the baseline Unet model.
+          "Improved_Unet" is the improved version of Unet.
+
+        References
+        ----------
+        "Unet", https://arxiv.org/abs/1505.04597
+        "Improved_Unet", https://arxiv.org/abs/1802.10508v1
         """
         if model == "Unet":
             self.model = self.Unet(input_shape, random_seed)
+        elif model == "Improved_Unet":
+            self.model = self.Improved_Unet(input_shape, random_seed)
         else:
             raise ValueError("Model doesn't exist!")
 
@@ -90,6 +99,94 @@ class SegModel:
 
         conv10 = Conv2D(2, 3, strides=1, padding='same', activation='relu', kernel_initializer=he_norm)(conv9)
         outputs = Conv2D(1, 1, activation='sigmoid', kernel_initializer=he_norm)(conv10)
+
+        model = Model(inputs=inputs, outputs=outputs)
+
+        return model
+
+    def Improved_Unet(self, input_shape, random_seed):
+        """
+        Function to construct the Improved Unet model
+
+        Parameters
+        ----------
+        input_shape : tuple
+          The input shape of the model
+        random_seed : integer
+          The random seed to decide the random weights in the model
+
+        Returns
+        -------
+        model : Keras model class
+          The Improved Unet model itself
+        """
+        # Initialize the random seed using he_normal
+        he_norm = he_normal(seed=random_seed)
+
+        # Left side
+        inputs = Input(input_shape)
+        conv1 = Conv2D(16, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(inputs)
+        context1 = Conv2D(16, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(conv1)
+        context1 = Dropout(0.3)(context1)
+        context1 = Conv2D(16, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(context1)
+        add1 = Add()([conv1, context1])
+
+        conv2 = Conv2D(32, 3, strides=2, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(add1)
+        context2 = Conv2D(32, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(conv2)
+        context2 = Dropout(0.3)(context2)
+        context2 = Conv2D(32, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(context2)
+        add2 = Add()([conv2, context2])
+
+        conv3 = Conv2D(64, 3, strides=2, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(add2)
+        context3 = Conv2D(64, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(conv3)
+        context3 = Dropout(0.3)(context3)
+        context3 = Conv2D(64, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(context3)
+        add3 = Add()([conv3, context3])
+
+        conv4 = Conv2D(128, 3, strides=2, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(add3)
+        context4 = Conv2D(128, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(conv4)
+        context4 = Dropout(0.3)(context4)
+        context4 = Conv2D(128, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(context4)
+        add4 = Add()([conv4, context4])
+
+        conv5 = Conv2D(256, 3, strides=2, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(add4)
+        context5 = Conv2D(256, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01),kernel_initializer=he_norm)(conv5)
+        context5 = Dropout(0.3)(context5)
+        context5 = Conv2D(256, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01),kernel_initializer=he_norm)(context5)
+        add5 = Add()([conv5, context5])
+
+        # Right side
+        up6 = UpSampling2D(2)(add5)
+        up6 = Conv2D(128, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(up6)
+        concat6 = concatenate([add4, up6], axis=3)
+        local6 = Conv2D(128, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(concat6)
+        local6 = Conv2D(128, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(local6)
+
+        up7 = UpSampling2D(2)(local6)
+        up7 = Conv2D(64, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(up7)
+        concat7 = concatenate([add3, up7], axis=3)
+        local7 = Conv2D(64, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(concat7)
+        local7 = Conv2D(64, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(local7)
+
+        up8 = UpSampling2D(2)(local7)
+        up8 = Conv2D(32, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(up8)
+        concat8 = concatenate([add2, up8], axis=3)
+        local8 = Conv2D(32, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(concat8)
+        local8 = Conv2D(32, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(local8)
+
+        up9 = UpSampling2D(2)(local8)
+        up9 = Conv2D(16, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(up9)
+        concat9 = concatenate([add1, up9], axis=3)
+        conv9 = Conv2D(32, 3, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(concat9)
+
+        seg7 = Conv2D(1, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(local7)
+        upscale7 = UpSampling2D(2)(seg7)
+        seg8 = Conv2D(1, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(local8)
+        add8 = Add()([upscale7, seg8])
+        upscale8 = UpSampling2D(2)(add8)
+        seg9 = Conv2D(1, 1, strides=1, padding='same', activation=LeakyReLU(alpha=0.01), kernel_initializer=he_norm)(conv9)
+        add9 = Add()([upscale8, seg9])
+        outputs = Conv2D(1, 1, activation='sigmoid', kernel_initializer=he_norm)(add9)
 
         model = Model(inputs=inputs, outputs=outputs)
 
