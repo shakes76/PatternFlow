6d92ddb95cedc47d6b7ee0bc591a1eaa1329d3fc
unicorn10086 <13607910397@163.com>
2021-10-22 00:48:17 +1000

    load isic image

---
+++ recognition/45464948-ISICs-UNet/main.py
@@ -0,0 +1,7 @@
+import glob
+
+
+
+# load image ISIC image
+filelist_input = glob.glob("D:/2021S2/COMP3710/ass/report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1-2_Training_Input_x2/*.jpg")
+filelist_ground_truth = glob.glob("D:/2021S2/COMP3710/ass/report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1_Training_GroundTruth_x2/*.png")
