d8015454440ecd8f6929970540a99a54530b9097
Your Name <you@example.com>
2021-10-21 16:30:25 +1000

    Rename function unsample to upsamle. Correct spelling.

--- recognition/s4615653_improved_Unet/Unet_model.py
+++ recognition/s4615653_improved_Unet/Unet_model.py
@@ -9,7 +9,7 @@ def pool(conv):
     pool = tf.keras.layers.MaxPool2D((2, 2))(conv)
     return pool
 
-def unsample(inputs,filter):
+def upsample(inputs,filter):
     conv = tf.keras.layers.UpSampling2D(size=(2, 2))(inputs)
     conv = tf.keras.layers.Conv2D(filter, (2, 2), padding="same")(conv)
     return conv
@@ -32,19 +32,19 @@ def Unet():
 
     conv_5 = convolution(pool_4,512)
 
-    conv_6 = unsample(conv_5,256)
+    conv_6 = upsample(conv_5,256)
     conv_6 = tf.keras.layers.concatenate([conv_6, conv_4])
     conv_6 = convolution(conv_6, 256)
 
-    conv_7 = unsample(conv_6, 128)
+    conv_7 = upsample(conv_6, 128)
     conv_7 = tf.keras.layers.concatenate([conv_7, conv_3])
     conv_7 = convolution(conv_7, 128)
 
-    conv_8 = unsample(conv_7, 64)
+    conv_8 = upsample(conv_7, 64)
     conv_8 = tf.keras.layers.concatenate([conv_8, conv_2])
     conv_8 = convolution(conv_6, 64)
 
-    conv_9 = unsample(conv_8, 32)
+    conv_9 = upsample(conv_8, 32)
     conv_9 = tf.keras.layers.concatenate([conv_9, conv_1])
     conv_9 = convolution(conv_9, 32)
 
