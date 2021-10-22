e289ceffc3e9d041f98a67c78a93c20f16a73123
Andrew <s4589098@student.uq.edu.au>
2021-10-23 01:42:36 +1000

    dropout layers added to encoder methods and decoder methods created

--- recognition/s45890985_UNET3D/model.py
+++ recognition/s45890985_UNET3D/model.py
@@ -8,12 +8,23 @@ def relu_conv_block(input_layer, conv_size):
 
     return conv
 
-def encoder_block(input_layer, conv_size):
+def encoder_block(input_layer, conv_size, drop_out):
     en = relu_conv_block(input_layer,conv_size)
-    en = layers.Dropout(0.2)(en)
+    en = layers.Dropout(drop_out)(en)
     en = relu_conv_block(en,conv_size)
     en = layers.MaxPooling3D(pool_size=(2,2,2))(en)
 
     return en
+
+def decoder_block(input_layer, concat_layer, conv_size, drop_out):
+    de = layers.Conv3DTranspose(conv_size, (2,2),strides=(2,2,2), padding= 'same')(input_layer)
+    de = layers.concatenate([de, concat_layer])
+    de = relu_conv_block(de, conv_size)
+    de = layers.Dropout(drop_out)(de)
+    de = relu_conv_block(de, conv_size)
+
+    return de
+
+
 
 
