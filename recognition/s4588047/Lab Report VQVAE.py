2336c048c064b8072e2ad43d895aba2dbb24f76a
Yousif <yousifpatti@gmail.com>
2021-10-22 12:11:34 +1000

    Added VectorQuantizer and training history plot
    Added VectorQuantizer model implementation essentially the calculations are made in the call function however, the avg_probs and perplexity are yet to be integrated.
    Some code clean-up and debugging is added to figure out issues with a layer producing nan. Currently working on the gradient training.
    Added plot for the training loss using the history tracking.

--- recognition/s4588047/Lab Report VQVAE.py
+++ recognition/s4588047/Lab Report VQVAE.py
@@ -5,7 +5,7 @@
 # 
 # Autoencoder in TF Keras with the OASIS dataset
 
-# In[22]:
+# In[1]:
 
 
 import os
@@ -14,8 +14,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras import layers
-from tensorflow.keras import backend as K
-from tensorflow.keras.losses import binary_crossentropy
+from keras import backend as K
+from tensorflow.keras.losses import binary_crossentropy, mse
 from matplotlib import pyplot as plt
 import numpy as np
 print(tf.config.list_physical_devices('GPU'))
@@ -23,18 +23,18 @@ print(tf.config.list_physical_devices('GPU'))
 
 # Parameters for the network
 
-# In[23]:
+# In[2]:
 
 
 depth = 32 #how much encoding to apply, compression of factor 24.5 if MNIST 28*28
 length = 256*256
-batch_size = 32
+batch_size = 16
 
 original_dim = 256*256
-latent_dim = 16
+latent_dim = 128
 
 
-# In[24]:
+# In[3]:
 
 
 def load_data(path):
@@ -62,7 +62,7 @@ def load_data(path):
 
 # Load the dataset, reshape as an image and normalise it
 
-# In[25]:
+# In[4]:
 
 
 #load the data
@@ -77,13 +77,13 @@ x_train = tf.concat([x for x in train], axis=0)
 x_validate = tf.concat([x for x in validate], axis=0)
 
 x_train = x_train / 255.
-x_test = x_validate / 255.
+x_validate = x_validate / 255.
 
 print(x_train.shape)
 print(x_validate.shape)
 
 
-# In[26]:
+# In[5]:
 
 
 #test images
@@ -92,42 +92,42 @@ plt.imshow(x_train[1])
 plt.show()
 
 
-# In[27]:
+# In[6]:
 
 
 # mean of the data
 mean, var = tf.nn.moments(x_train, axes=[1])
 
 
-# In[28]:
+# In[7]:
 
 
 #Images encdoer
 Encoder = keras.Sequential(
     [
         keras.Input(shape=(128, 128, 3)),
-        layers.Conv2D(32, 3, strides=2, padding="same"),
-        layers.ReLU(),
-        layers.Conv2D(64, 3, strides=2, padding="same"),
-        layers.ReLU(),
-        layers.Conv2D(latent_dim, 1, strides=2, padding="same"),
+        layers.Conv2D(32, 3, strides=2, padding="same", activation="relu"),
+        #layers.ReLU(),
+        layers.Conv2D(64, 3, strides=2, padding="same", activation="relu"),
+        #layers.ReLU(),
+        layers.Conv2D(latent_dim, 3, strides=2, padding="same", activation="relu"),
     ],
     name="Encoder",
 )
 Encoder.summary()
 
 
-# In[29]:
+# In[8]:
 
 
 #Images decoder
 Decoder = keras.Sequential(
     [
         keras.Input(shape=Encoder.output.shape[1:]),
-        layers.Conv2DTranspose(64, 3, strides=2, padding="same"),
-        layers.ReLU(),
-        layers.Conv2DTranspose(32, 3, strides=2, padding="same"),
-        layers.ReLU(),
+        layers.Conv2DTranspose(64, 3, strides=2, padding="same", activation="relu"),
+        #layers.ReLU(),
+        layers.Conv2DTranspose(32, 3, strides=2, padding="same", activation="relu"),
+        #layers.ReLU(),
         layers.Conv2DTranspose(3, 3, strides=2, padding="same"),
     ],
     name="Decoder",
@@ -135,14 +135,55 @@ Decoder = keras.Sequential(
 Decoder.summary()
 
 
-# In[30]:
+class VectorQuantizer(layers.Layer):  
+    def __init__(self, embedding_dim, num_embeddings, commitment_cost=0.25, epsilon=1e-10, **kwargs):
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+        self.commitment_cost = commitment_cost
+        super(VectorQuantizer, self).__init__(**kwargs)
 
+    def build(self, input_shape):
+        # Add embedding weights.
+        self.w = self.add_weight(name='embedding',
+                                  shape=(self.embedding_dim, self.num_embeddings),
+                                  initializer='uniform',
+                                  trainable=True)
+        # Finalize building.
+        super(VectorQuantizer, self).build(input_shape)
 
-class VectorQuantizer(layers.Layer):
-    pass
+    def call(self, x):
+        # Covert into flatten representation (eg. (14944, 128, 128, 3) to (x, y))
+        flat_inputs = K.reshape(x, (-1, self.embedding_dim))
 
+        # Calculate distances of input by calcuating the distance of every encoded vector to the embedding space.
+        distances = (K.sum(flat_inputs**2, axis=1, keepdims=True)
+                     - 2 * K.dot(flat_inputs, self.w)
+                     + K.sum(self.w ** 2, axis=0, keepdims=True))
 
-# In[31]:
+        # Retrieve encoding indices.
+        encoding_indices = K.argmax(-distances, axis=1)
+        encodings = K.one_hot(encoding_indices, self.num_embeddings)
+        encoding_indices = K.reshape(encoding_indices, K.shape(x)[:-1])
+        quantized = self.quantize(encoding_indices)
+
+        # Metrics.
+        #avg_probs = K.mean(encodings, axis=0)
+        #perplexity = K.exp(- K.sum(avg_probs * K.log(avg_probs + epsilon)))
+        
+        commitment_loss = 1 * tf.reduce_mean(
+             (tf.stop_gradient(quantized) - x) ** 2)
+        codebook_loss = tf.reduce_mean((quantized - tf.stop_gradient(x)) ** 2)
+        self.add_loss(commitment_loss + codebook_loss)
+        
+        return x + K.stop_gradient(quantized- x)
+
+    @property
+    def embeddings(self):
+        return self.w
+
+    def quantize(self, encoding_indices):
+        w = K.transpose(self.embeddings.read_value())
+        return tf.nn.embedding_lookup(w, encoding_indices)
 
 
 #Images decoder
@@ -150,7 +191,7 @@ VQVAE = keras.Sequential(
     [
         keras.Input(shape=(128, 128, 3)),
         Encoder,
-        VectorQuantizer(64, latent_dim),
+        VectorQuantizer(128, latent_dim),
         Decoder,
         
     ],
@@ -158,33 +199,28 @@ VQVAE = keras.Sequential(
 )
 VQVAE.summary()
 
-
-# In[32]:
-
-
 class VQVAETrainer(keras.models.Model):
-    pass
 
 
-# In[ ]:
-
-
-vqvae_trainer = VQVAETrainer(var, latent_dim=16, num_embeddings=128)
+vqvae_trainer = VQVAETrainer(var)
 vqvae_trainer.compile(optimizer=keras.optimizers.Adam())
-vqvae_trainer.fit(x_train, epochs=5, batch_size=128)
+vqvae_trainer.fit(x_train, epochs=1, batch_size=128)
 
 
-# In[17]:
+# history = vqvae.fit(x_train, x_train, epochs=3, batch_size=batch_size)
 
 
-def show_subplot(original, reconstructed=None):
+# In[59]:
+
+
+def show_subplot(original, reconstructed):
     plt.subplot(1, 2, 1)
     plt.imshow(original)
     plt.title("Original")
     plt.axis("off")
 
     plt.subplot(1, 2, 2)
-    plt.imshow(reconstructed)
+    plt.imshow(tf.cast(reconstructed * 255.,'uint8'))
     plt.title("Reconstructed")
     plt.axis("off")
     
@@ -194,12 +230,27 @@ xtrain = tf.expand_dims(x_train, axis=1)
 print(xtrain[1].shape)
 
 
-trained_vqvae_model = vqvae_trainer.vqvae
+trained_vqvae_model = e
 
-recon = trained_vqvae_model.predict(xtrain[1])
+recon = e.predict(xtrain[1000])
 print(tf.squeeze(recon).shape)
 # for test_image in test_images:
 #     show_subplot(test_image)
 
-show_subplot(x_train[1], tf.squeeze(recon))
+show_subplot(x_train[1000], tf.squeeze(recon))
+
+
+# In[ ]:
+
+
+# Plot training results.
+loss = history.history['loss'] # Training loss.
+num_epochs = range(1, 1 + len(history.history['loss'])) # Number of training epochs.
+
+plt.figure(figsize=(16,9))
+plt.plot(num_epochs, loss, label='Training loss') # Plot training loss.
+
+plt.title('Training loss')
+plt.legend(loc='best')
+plt.show()
 
