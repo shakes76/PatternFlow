cce0961de4f580705c5b5755a1a2127f5823bee4
ltaylor231 <ltaylor231@hotmail.com>
2021-10-23 18:17:33 +1000

    last commit didn't seem to go through. Added testing data split. Implemented dice coeffecieent as loss function. Initial implementation of improved Unet implemented.

--- recognition/45830048_recognition/build.py
+++ recognition/45830048_recognition/build.py
@@ -3,26 +3,28 @@ Driver Script
 """
 
 import tensorflow as tf
+from tensorflow.python.ops.gen_batch_ops import batch
 from model import *
 import numpy as np
 import os
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 import matplotlib.pyplot as plt
+from tensorflow.keras import backend as K
 
 ############LOAD CELEB-A####################
-dir = "C:/ISIC_IMAGE_TEST"
-mask_dir = "C:/ISIC_MASK_TEST"
-batch_size = 16
-img_height = 640
-img_width = 480
+dir = "C:/ISIC2018_Task1-2_Training_Input_x2"
+mask_dir = "C:/ISIC2018_Task1_Training_GroundTruth_x2"
+batchs = 16
+img_height = 128
+img_width = 128
 
 y_train = tf.keras.utils.image_dataset_from_directory(mask_dir, validation_split=0.2,
   subset="training",
   labels=None,
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size,
+  batch_size=batchs,
   color_mode='grayscale')
 
 y_val = tf.keras.utils.image_dataset_from_directory(mask_dir, validation_split=0.2,
@@ -30,7 +32,7 @@ y_val = tf.keras.utils.image_dataset_from_directory(mask_dir, validation_split=0
   labels=None,
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size,
+  batch_size=batchs,
   color_mode='grayscale')
 
 X_train = tf.keras.utils.image_dataset_from_directory(dir, validation_split=0.2,
@@ -38,7 +40,7 @@ X_train = tf.keras.utils.image_dataset_from_directory(dir, validation_split=0.2,
   labels=None,
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size,
+  batch_size=batchs,
   color_mode='grayscale')
 
 X_val = tf.keras.utils.image_dataset_from_directory(dir, validation_split=0.2,
@@ -46,55 +48,83 @@ X_val = tf.keras.utils.image_dataset_from_directory(dir, validation_split=0.2,
   labels=None,
   seed=123,
   image_size=(img_height, img_width),
-  batch_size=batch_size,
+  batch_size=batchs,
   color_mode='grayscale')
 
 X_train = X_train.unbatch()
+X_val = X_val.unbatch()
+y_train = y_train.unbatch()
+y_val = y_val.unbatch()
+
+test_size = int(0.2 * 2076)
+X_test = X_train.take(test_size)
+X_train = X_train.skip(test_size)
+y_test = y_train.take(test_size)
+y_train = y_train.skip(test_size)
+
 a = tf.zeros([0, img_height, img_width, 1])
 for image in X_train:
   a = tf.concat([a, [image]], axis = 0)
 X_train = a
 
-X_val = X_val.unbatch()
 a = tf.zeros([0, img_height, img_width, 1])
 for image in X_val:
   a = tf.concat([a, [image]], axis = 0)
 X_val = a
 
-y_train = y_train.unbatch()
+a = tf.zeros([0, img_height, img_width, 1])
+for image in X_test:
+  a = tf.concat([a, [image]], axis = 0)
+X_test = a
+
 a = tf.zeros([0, img_height, img_width, 1])
 for image in y_train:
   a = tf.concat([a, [image]], axis = 0)
 y_train = a
 
-y_val = y_val.unbatch()
 a = tf.zeros([0, img_height, img_width, 1])
 for image in y_val:
   a = tf.concat([a, [image]], axis = 0)
 y_val = a
 
+a = tf.zeros([0, img_height, img_width, 1])
+for image in y_test:
+  a = tf.concat([a, [image]], axis = 0)
+y_test = a
+
 print(y_train)
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-  ax = plt.subplot(3, 3, i + 1)
-  plt.imshow(y_train.numpy()[i].astype("uint8"), cmap='gray')
-  plt.axis("off")
-plt.show()
+#plt.figure(figsize=(10, 10))
+#for i in range(9):
+#  ax = plt.subplot(3, 3, i + 1)
+#  plt.imshow(y_train.numpy()[i].astype("uint8"), cmap='gray')
+#  plt.axis("off")
+#plt.show()
+#
+#
+#plt.figure(figsize=(10, 10))
+#for i in range(9):
+#  ax = plt.subplot(3, 3, i + 1)
+#  plt.imshow(X_train.numpy()[i].astype("uint8"), cmap='gray')
+#  plt.axis("off")
+#plt.show()
 
+"""
+Calculate dice coeffecient
+"""
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + smooth) / (K.sum(K.square(y_true),-1) + K.sum(K.square(y_pred),-1) + smooth)
 
-plt.figure(figsize=(10, 10))
-for i in range(9):
-  ax = plt.subplot(3, 3, i + 1)
-  plt.imshow(X_train.numpy()[i].astype("uint8"), cmap='gray')
-  plt.axis("off")
-plt.show()
-
-
+"""
+dice coeffecient for use in loss function
+"""
+def dice_coef_loss(y_true, y_pred):
+    return 1-dice_coef(y_true, y_pred)
 
 model = improved_unet(img_height, img_width)
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-model.fit(X_train, y_train, validation_data =(X_val, y_val), epochs = 30)
+model.compile(optimizer = 'adam', loss = dice_coef_loss, metrics = ['accuracy', dice_coef])
+model.fit(X_train, y_train, validation_data =(X_val, y_val), epochs = 30, batch_size = 32)
 
 #no_train = 15
 #no_test         = 5
--- recognition/45830048_recognition/model.py
+++ recognition/45830048_recognition/model.py
@@ -5,11 +5,83 @@ Architecture for Improved Unet
 """
 from tensorflow import keras
 
+"""layers for context module"""
+def context_module(last, dims):
+    # Each context_module consists of two 3x3 conv layers and a dropout(0.3) in between.
+    
+    context = keras.layers.Conv2D(dims, kernel_size =3, padding = 'same')(last)
+    context = keras.layers.Dropout(.3)(context)
+    context = keras.layers.Conv2D(dims, kernel_size =3, padding = 'same')(context)
+
+    return context
+
+# add segmentation layers
+def segmentation_block(x):
+    seg = keras.layers.Conv2D(1, (1,1), activation = 'sigmoid')(x)
+    return seg
+
+def encode_block(last, dims, activation_function, stride_num):
+    conv = keras.layers.Conv2D(dims, (3,3), activation = activation_function, padding ='same', strides=stride_num)(last)
+    context = context_module(conv, dims)
+    sum = keras.layers.Add()([context, conv])
+    return sum
+
+def upsample_block(last, dims, activation_function):
+    # ...It is like a layer that combines the UpSampling2D and Conv2D layers into one layer. 
+    
+    # what twice means in paper (Answer from Piazza: kernel size 2 by 2)?
+    up = keras.layers.UpSampling2D( size=(2, 2) )(last)
+    conv = keras.layers.Conv2D(dims, (3,3), activation = activation_function, padding ='same')(up)
+    
+    return conv
+
+def local_block(last, dims, activation_function):
+    local = keras.layers.Conv2D(dims, (3,3), activation = activation_function, padding ='same')(last)
+    local = keras.layers.Conv2D(dims, (1,1), activation = activation_function, padding ='same')(local)
+    return local
+
 """
 Initial layers for improved unet model
 """
 def improved_unet(height, width):
     inputs = keras.layers.Input((height, width, 1))
-    outputs = keras.layers.Conv2D(1, 1, activation = 'sigmoid')(inputs)
+    activation_function = keras.layers.LeakyReLU(alpha=0.01)
+
+    # encoding
+    sum1 = encode_block(inputs, 16, activation_function, 1)
+
+    sum2 = encode_block(sum1, 32, activation_function, 2)
+
+    sum3 = encode_block(sum2, 64, activation_function, 2)
+
+    sum4 = encode_block(sum3, 128, activation_function, 2)
+
+    sum5 = encode_block(sum4, 256, activation_function, 2)
+    up1 = upsample_block(sum5, 128, activation_function)
+
+    concat1 = keras.layers.concatenate([sum4,up1])
+    local1 = local_block(concat1, 128, activation_function)
+    up2 = upsample_block(local1, 64, activation_function)
+
+    concat2 = keras.layers.concatenate([sum3,up2])
+    local2 = local_block(concat2, 64, activation_function)
+    up3 = upsample_block(local2, 32, activation_function)
+
+    concat3 = keras.layers.concatenate([sum2,up3])
+    local3 = local_block(concat3, 32, activation_function)
+    up4 = upsample_block(local3, 16, activation_function)
+
+    concat4 = keras.layers.concatenate([sum1,up4])
+    conv_final = keras.layers.Conv2D(32, (3,3), activation = activation_function, padding ='same')(concat4)
+
+    seg1 = segmentation_block(local2)
+    seg1 = keras.layers.UpSampling2D( size=(4, 4) )(seg1)
+    seg2 = segmentation_block(local3)
+    seg2 = keras.layers.UpSampling2D( size=(2, 2) )(seg2)
+    seg3 = segmentation_block(conv_final)
+    seg_sum = keras.layers.Add()([seg1, seg2, seg3])
+
+    #c7 = keras.layers.Add()([c6, seg3])
+    outputs = keras.layers.Conv2D(1, 1, activation = 'sigmoid')(seg_sum)
     model = keras.Model(inputs = inputs, outputs = outputs)
     return model
