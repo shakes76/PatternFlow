d421a31590b398415ab12a1961f04fd510300936
Korbin Daly <s4630726@labs.eait.uq.edu.au>
2021-10-24 00:56:13 +1000

    Updated image reading to also resize the image into a 4:3 aspect ratio

--- recognition/s4630726-imp-unet/unet_isic_r.py
+++ recognition/s4630726-imp-unet/unet_isic_r.py
@@ -5,29 +5,63 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, UpSampling2D, Concatenate, Conv2DTranspose, Reshape, Permute, Activation
 from tensorflow.keras.models import Model
 from sklearn.model_selection import train_test_split
+from skimage.transform import resize
 
 
-X = [] #list to store the input images for cnn
-Y = [] #list to store the ground truth to comapre the output of the cnn to.
+#List to store the input images for cnn
+X = [] 
+#List to store the ground truth to comapre the output of the cnn to.
+Y = [] 
 
-root = "C:/Users/s4630726/Downloads" #directory where ISIC data is stored
+#Directory where ISIC data is stored
+root = "C:/Users/s4630726/Downloads" 
 
+#Defining dimensions for cnn input and resizing
+width = 256 
+height = 192
+resize_dim = (height, width) 
 
+print("Iterating through training input...")
+#Iterate through folder reading each image and resizing them to a 4:3 aspect ratio of 256 x 192 then adding them to a global list
 path = os.path.join(root, "ISIC2018_Task1-2_Training_Input_x2") 
-for img in os.listdir(path):
+for img in os.listdir(path): 
     img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+    img_array = resize(img_array, resize_dim, order=1, preserve_range=True, anti_aliasing=False).astype('uint8')
     X.append(img_array)
-print(img)
-plt.imshow(img_array,cmap="gray")
-plt.show()
 
+
+
+print("Iterating though groundtruth...")
 path = os.path.join(root, "ISIC2018_Task1_Training_GroundTruth_x2") 
-for img in os.listdir(path):
-    img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
-    Y.append(img_array)
-print(img)
-plt.imshow(img_array,cmap="gray")
-plt.show()
+for img in os.listdir(path): 
+    img_array_2 = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+    #Resize anti_aliasing set to false so new pixel values aren't introduced in the segmentation map
+    #The function also changes the values [0,255] to [0,1] which is critical for the umap segmentaiton to work correctly
+    img_array_2 = resize(img_array_2, resize_dim, order=0, preserve_range=False, anti_aliasing=False).astype('uint8') 
+    Y.append(img_array_2)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42) #split the data into training and testing sets validations sets made in model.fit
 
+#print(img_array)
+#print(img)                             @for debugging purposes
+#plt.imshow(img_array,cmap="gray")
+#plt.show()
+#print(img_array_2)
+#print(img)                             
+#plt.imshow(img_array_2,cmap="gray")
+#plt.show()
+
+
+#Split the data into training and testing sets; validations sets are split during model.fit()
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42) 
+
+print("Converting arrays to tensors...")
+#Convert arrays to tensors
+X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)/255 
+X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)/255
+Y_train = tf.convert_to_tensor(Y_train, dtype=tf.int16)
+Y_test = tf.convert_to_tensor(Y_test, dtype=tf.int16)
+
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
