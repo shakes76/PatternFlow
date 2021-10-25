39e9ef1366ae689a312c8186cf089e359024b84e
SteveInUQ <gaozhu19940805@gmail.com>
2021-10-25 12:18:49 +0800

    Add data_preprocessing file, the file is used to load data, build graph and splite data into train, validation, and test dataset.

---
+++ recognition/s4521842_GCN/GCN/data_preprocessing.py
@@ -0,0 +1,97 @@
+import numpy as np
+import scipy.sparse as sp
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+
+class DataPreprocessing():
+    def __init__(self, path='dataset/facebook.npz'):
+        
+        self.path = path
+        self.data_edges = None
+        self.data_features = None
+        self.data_target = None
+        self.n_nodes = None
+        self.n_feature = None
+        
+        self.dataset = self.data_processing()
+        
+    
+    def load_data(self):
+        self.data = np.load(self.path, allow_pickle=True)
+        self.data_edges = self.data['edges']
+        self.data_features = self.data['features']
+        self.data_target = self.data['target']
+        
+        # the number of nodes
+        self.n_nodes = self.data_features.shape[0]
+        # the size of node features
+        self.n_feature = self.data_features.shape[1]
+
+        print('Shape of Edge data', self.data_edges.shape)
+        print('Shape of Feature data', self.data_features.shape)
+        print('Shape of Target data', self.data_target.shape)
+        print('-'*50)
+        print('Number of nodes: ', self.n_nodes)
+        print('Number of features of each node: ', self.n_feature)
+        print('Categories of labels: ', set(self.data_target))
+        
+    def normalize_adjacency(self, adjacency):
+        """calculate L=D^-0.5 * (A+I) * D^-0.5 """
+        # Increase self-connection
+        adjacency += sp.eye(adjacency.shape[0])
+        degree = np.array(adjacency.sum(1))
+        d_hat = sp.diags(np.power(degree, -0.5).flatten())
+
+        return d_hat.dot(adjacency).dot(d_hat).tocoo()
+    
+    def data_split(self, n_nodes):
+        train_idx = range(int((n_nodes)*0.5))
+        val_idx = range(int((n_nodes)*0.5), int((n_nodes)*0.75))
+        test_idx = range(int((n_nodes)*0.75), n_nodes)
+
+        train_mask = np.zeros(n_nodes, dtype=np.bool)
+        val_mask = np.zeros(n_nodes, dtype=np.bool)
+        test_mask = np.zeros(n_nodes, dtype=np.bool)
+
+        train_mask[train_idx] = True
+        val_mask[val_idx] = True
+        test_mask[test_idx] = True
+        
+        train_labels = self.data_target[train_idx]
+        val_labels = self.data_target[val_idx]
+        test_labels = self.data_target[test_idx]
+
+        return train_mask, val_mask, test_mask, train_labels, val_labels, test_labels
+    
+    def one_hot_encoding(self, labels):
+        
+        label_encoder = LabelBinarizer()
+        encoded_labels = label_encoder.fit_transform(labels)
+        return encoded_labels
+        
+    def data_processing(self):
+        
+        self.load_data()
+        
+        onehot_labels = self.one_hot_encoding(self.data_target)
+        
+        # build graph
+        # construct sparse matrix in COOrdinate format.
+        adjacency = sp.coo_matrix(
+            (np.ones(self.data_edges.shape[0]), (self.data_edges[:, 0], self.data_edges[:, 1])), 
+            shape=(self.data_target.shape[0], self.data_target.shape[0]),
+            dtype=np.float32
+        )
+        
+        # normalize adjacency
+        adjacency_norm = self.normalize_adjacency(adjacency)
+        
+        train_mask, val_mask, test_mask, train_labels, val_labels, test_labels= self.data_split(self.n_nodes)
+        
+        return self.data_features, self.data_target, adjacency_norm, train_mask, val_mask, test_mask, train_labels, val_labels, test_labels
+        
+    def get_data(self):
+        return self.dataset
+
+        
