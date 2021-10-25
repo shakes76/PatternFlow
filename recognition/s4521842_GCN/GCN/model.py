8af2600cc61274605745f7531adfa870259c6012
SteveInUQ <gaozhu19940805@gmail.com>
2021-10-26 02:38:38 +0800

    Fix a typo bug

--- recognition/s4521842_GCN/GCN/model.py
+++ recognition/s4521842_GCN/GCN/model.py
@@ -5,22 +5,22 @@ from tensorflow.keras import activations, initializers, regularizers
 
 class GraphConvolutionLayer(Layer):
     def __init__(self, input_dim, output_dim,
-                 activation='relu', 
+                 activation=None, 
                  bias=True, 
                  kernel_initializer="glorot_uniform",
-                 bias_initializer = 'zeros',
+                 bias_initializer ='zeros',
                  kernel_regularizer=None,
-                 bias_regularizer=None,
-                 **kwargs):
-        super(GraphConvolutionLayer, self).__init__(**kwargs)
+                 bias_regularizer=None):
+        super(GraphConvolutionLayer, self).__init__()
+        
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.activation = activations.get(activation)
+        self.activation = activation
         self.bias = bias
-        self.kernel_initializer = initializers.get(kernel_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.kernel_regularizer = regularizers.get(kernel_regularizer)
-        self.bias_regularizer = regularizers.get(bias_regularizer) 
+        self.kernel_initializer = kernel_initializer
+        self.bias_initializer = bias_initializer
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
         
         self.w = self.add_weight(name='kernal', 
                                  shape=(self.input_dim, self.output_dim), 
@@ -34,7 +34,7 @@ class GraphConvolutionLayer(Layer):
         else:
             self.b = None
             
-        self.build = True
+        self.built = True
 
     
     def call(self, inputs):
@@ -46,25 +46,26 @@ class GraphConvolutionLayer(Layer):
         if self.bias:
             output += self.b
             
-        return self.activation(output)
-
-
+        if self.activation is not None:
+            output = self.activation(output)
+            
+        return output
+    
 class GCN(Model):
-    def __in__(self):
-        super(GCN, self).__init__(input_dim, output_dim)
-        
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-        
-        self.graph_conv_1 = GraphConvolutionLayer(input_dim, 16,
-                    activation=tf.keras.activations.relu,
-                    kernel_regularizer=tf.keras.regularizers.l2(0.01))
+    def __init__(self):
+        super(GCN, self).__init__()
 
-        self.graph_conv_2 = GraphConvolutionLayer(16, output_dim)
+        self.graph_conv_1 = GraphConvolutionLayer(input_dim=128, 
+                                                  output_dim=16,
+                                                  activation=relu,
+                                                  kernel_regularizer=l2(0.01))
+
+        self.graph_conv_2 = GraphConvolutionLayer(input_dim=16, output_dim=4, activation=softmax)
+
 
     def call(self, inputs, training=False):
-
+        edges = inputs[1]
         H = self.graph_conv_1(inputs)
-        Y = self.graph_conv_2([H, inputs[1]])
+        Y = self.graph_conv_2([H, edges])
 
         return Y
