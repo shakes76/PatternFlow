2cbee2683752fac839af8ba4b8967640f6d3f81f
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-25 17:32:18 +1000

    Rename "input_images" to "images" and "output_images" to "masks" to avoid confusion

--- recognition/ISIC-ImprovedUnet-44961374/driver.py
+++ recognition/ISIC-ImprovedUnet-44961374/driver.py
@@ -7,7 +7,7 @@ import os # for operating system functionalities
 from sklearn.utils import shuffle, validation # for shuffling data 
 import math # for mathematical operations
 import tensorflow as tf # for DL functionalities 
-from tensorflow.python.client import device_lib
+
 
 print(tf. __version__) # check tf version
 print("GPUs in use: ", len(tf.config.list_physical_devices('GPU'))) # check if tf has access to GPU
@@ -17,12 +17,12 @@ def main():
     The main function that runs this script
     """
     # load data from ISIC dataset 
-    input_images = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1-2_Training_Input_x2/") if item.endswith('jpg')) # training input
-    output_images = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1_Training_GroundTruth_x2/") if item.endswith('png')) # ground truth
+    images = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1-2_Training_Input_x2/") if item.endswith('jpg')) # training input
+    masks = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1_Training_GroundTruth_x2/") if item.endswith('png')) # ground truth
 
     # suffle data before split to remove bias
-    input_images, output_images = shuffle(input_images, output_images)
-    total_length = len(input_images)
+    images, masks = shuffle(images, masks)
+    total_length = len(images)
     """
     split data into 80% train, 10% validation and 10% test
     total legnth = 2594
@@ -33,20 +33,19 @@ def main():
     """
     
     # data for train
-    train_inputs = input_images[:2074]
-    train_outputs = output_images[:2074]
-    train_data = tf.data.Dataset.from_tensor_slices((train_inputs, train_outputs))
+    train_images = images[:2074]
+    train_masks = masks[:2074]
+    train_data = tf.data.Dataset.from_tensor_slices((train_images, train_masks))
 
     # data for validation
-    val_inputs = input_images[2074:2074 + 260]
-    val_outputs = output_images[2074:2074 + 260]
-    val_data = tf.data.Dataset.from_tensor_slices((val_inputs, val_outputs))
+    val_images = images[2074:2074 + 260]
+    val_masks = masks[2074:2074 + 260]
+    val_data = tf.data.Dataset.from_tensor_slices((val_images, val_masks))
 
     # data for test
-    test_inputs = input_images[2074 + 260:]
-    test_outputs = output_images[2074 + 260:]
-    test_data = tf.data.Dataset.from_tensor_slices((test_inputs, test_outputs))
-    print(device_lib.list_local_devices())
+    test_images = images[2074 + 260:]
+    test_masks = masks[2074 + 260:]
+    test_data = tf.data.Dataset.from_tensor_slices((test_images, test_masks))
 
 # run main function
 if __name__ == "__main__":
