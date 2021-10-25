d7d15ba4646e1269db7aebd5faaccb61ae5da30b
SteveInUQ <gaozhu19940805@gmail.com>
2021-10-25 16:37:51 +0800

    Add GCN Model class fix bug in the class of GraphConvolutionLayer

--- recognition/s4521842_GCN/GCN/model.py
+++ recognition/s4521842_GCN/GCN/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer
 from tensorflow.keras import activations, initializers, regularizers
 
-class GraphConvolution(Layer):
+class GraphConvolutionLayer(Layer):
     def __init__(self, input_dim, output_dim,
                  activation='relu', 
                  bias=True, 
@@ -12,7 +12,7 @@ class GraphConvolution(Layer):
                  kernel_regularizer=None,
                  bias_regularizer=None,
                  **kwargs):
-        super(GraphConvolution, self).__init__(**kwargs)
+        super(GraphConvolutionLayer, self).__init__(**kwargs)
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.activation = activations.get(activation)
@@ -21,6 +21,7 @@ class GraphConvolution(Layer):
         self.bias_initializer = initializers.get(bias_initializer)
         self.kernel_regularizer = regularizers.get(kernel_regularizer)
         self.bias_regularizer = regularizers.get(bias_regularizer) 
+        
         self.w = self.add_weight(name='kernal', 
                                  shape=(self.input_dim, self.output_dim), 
                                  initializer=self.kernel_initializer, 
@@ -46,3 +47,24 @@ class GraphConvolution(Layer):
             output += self.b
             
         return self.activation(output)
+
+
+class GCN(Model):
+    def __in__(self):
+        super(GCN, self).__init__(input_dim, output_dim)
+        
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+        self.graph_conv_1 = GraphConvolutionLayer(input_dim, 16,
+                    activation=tf.keras.activations.relu,
+                    kernel_regularizer=tf.keras.regularizers.l2(0.01))
+
+        self.graph_conv_2 = GraphConvolutionLayer(16, output_dim)
+
+    def call(self, inputs, training=False):
+
+        H = self.graph_conv_1(inputs)
+        Y = self.graph_conv_2([H, inputs[1]])
+
+        return Y
