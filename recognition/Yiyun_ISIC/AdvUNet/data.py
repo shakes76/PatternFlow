bb7119c29753da1e6bfb0c3d698fbb06feb2dfb3
Nifu <frzmit@gmail.com>
2021-10-27 00:37:11 +1000

    Split dataset into training, validation and test

--- recognition/Yiyun_ISIC/AdvUNet/data.py
+++ recognition/Yiyun_ISIC/AdvUNet/data.py
@@ -17,3 +17,17 @@ def get_filenames(isic_dir):
 
     print("Number of images loaded:", len(features), len(labels))
     return features, labels
+
+
+def split_data(features, labels, training_split=0.6, validation_split=0.2, test_split=0.2):
+    # calculate the split size
+    num_train = int(training_split * len(features))
+    num_val = int(validation_split * len(features))
+    num_test = int(test_split * len(features))
+
+    train_features, val_features, test_features = tf.split(
+        features, [num_train, num_val, num_test])
+    train_labels, val_labels, test_labels = tf.split(
+        labels, [num_train, num_val, num_test])
+
+    return train_features, train_labels, val_features, val_labels, test_features, test_labels
