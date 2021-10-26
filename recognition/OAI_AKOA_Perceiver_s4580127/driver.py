7e6cb8b6a02d7e07df2c98d8e1562cc9e89f19d5
StopTheBoolean <jb26106@gmail.com>
2021-10-26 18:44:37 +1000

    Change data loader to use inbuilt tf image loading

--- recognition/OAI_AKOA_Perceiver_s4580127/driver.py
+++ recognition/OAI_AKOA_Perceiver_s4580127/driver.py
@@ -1,30 +1,26 @@
-import numpy as np
 import tensorflow as tf
-import glob
-from PIL import Image
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-def load_data(batch_size):
-    # Load images
-    image_size = (128, 128)  # Original size - 260 * 228 TODO: should we keep the same ratio in HxW
-    images = glob.glob("images/*.png")
-    # TODO: this is currently storing all images in VRAM (2GB!!!) - should probs be changed to better batch handling
-    images = np.array([np.array(Image.open(i).convert('L').resize(image_size)) for i in images])
 
-    # Create 'channels' dimension for CNNs
-    images = images[:, :, :, np.newaxis]
-    discriminator_input_dim = images.shape[1:]
-    dataset_size = images.shape[0]
+def load_data():
+    # Load images, split into train, val, test and normalise to (0, 1)
+    normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+    train_ds = image_dataset_from_directory("images", label_mode="binary", color_mode="grayscale", batch_size=22,
+                                            image_size=(128, 128), shuffle=True, validation_split=0.3,
+                                            subset="training", seed=2).map(lambda x, y: (normalization_layer(x), y))
+    val_ds = image_dataset_from_directory("images", label_mode="binary", color_mode="grayscale", batch_size=22,
+                                          image_size=(128, 128), shuffle=True, validation_split=0.3,
+                                          subset="validation", seed=2).map(lambda x, y: (normalization_layer(x), y))
+    test_ds = val_ds.shard(2, 0)
+    val_ds = val_ds.shard(2, 1)
 
-    # Normalise images 0-1
-    images = images/255
+    # Configure caching and prefetching
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+    test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
-    # Create TensorFlow Dataset
-    images = tf.data.Dataset.from_tensor_slices(images)
-    images = images.shuffle(buffer_size=batch_size)
-    images = images.repeat().batch(batch_size)
-    image_iter = iter(images)
-
-    return image_iter, discriminator_input_dim, dataset_size
+    return train_ds, val_ds, test_ds
 
 
 if __name__ == '__main__':
