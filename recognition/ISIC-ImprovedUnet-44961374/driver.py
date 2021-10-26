d73f9e896b22993e0e2ab698265e2d1780827505
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-26 13:15:17 +1000

    Complete processing module. Images are now decoded to Tensors
    Also added docstrings
    Cleaned up code

--- recognition/ISIC-ImprovedUnet-44961374/driver.py
+++ recognition/ISIC-ImprovedUnet-44961374/driver.py
@@ -7,47 +7,46 @@ import os # for operating system functionalities
 from sklearn.utils import shuffle, validation # for shuffling data 
 import math # for mathematical operations
 import tensorflow as tf # for DL functionalities 
-from process_data import process_data
+from process_data import process_data # for dataset processing functionalities
+import matplotlib.pyplot as plt # for plotting images
 
-# print(tf. __version__) # check tf version
-# print("GPUs in use: ", len(tf.config.list_physical_devices('GPU'))) # check if tf has access to GPU
+print(tf. __version__) # check tf version
+print("GPUs in use: ", len(tf.config.list_physical_devices('GPU'))) # check if tf has access to GPU
+
+IMG_PATH = "../../../isic-data/ISIC2018_Task1-2_Training_Input_x2/" # path of images (scans) from the dataset
+MASK_PATH = "../../../isic-data/ISIC2018_Task1_Training_GroundTruth_x2/" # path of mask images from the dataset
+
 
 def main():
     """
     The main function that runs this script
     """
     # load data from ISIC dataset 
-    images = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1-2_Training_Input_x2/") if item.endswith('jpg')) # training input
-    masks = sorted(item for item in os.listdir("../../../isic-data/ISIC2018_Task1_Training_GroundTruth_x2/") if item.endswith('png')) # ground truth
+    images = sorted(os.path.join(IMG_PATH, item) for item in os.listdir(IMG_PATH) if item.endswith('jpg')) # training input
+    masks = sorted(os.path.join(MASK_PATH, item) for item in os.listdir(MASK_PATH) if item.endswith('png')) # ground truth
 
-    # suffle data before split to remove bias
+    # suffle data before split to remove bias   
     images, masks = shuffle(images, masks)
     total_length = len(images)
-    """
-    split data into 80% train, 10% validation and 10% test
-    total legnth = 2594
-    train_size = 2594 * 0.8 = 2074
-    val_size = 260
-    test_size = 260
-    Note: Numbers were adjusted to fit the dataset
-    """
-    
+
+    # split data into 80% train, 10% validation and 10% test
     # data for train
     train_images = images[:2074]
     train_masks = masks[:2074]
     train_data = tf.data.Dataset.from_tensor_slices((train_images, train_masks))
+    train_data = train_data.map(process_data)
 
     # data for validation
     val_images = images[2074:2074 + 260]
     val_masks = masks[2074:2074 + 260]
     val_data = tf.data.Dataset.from_tensor_slices((val_images, val_masks))
+    val_data = val_data.map(process_data)
 
     # data for test
     test_images = images[2074 + 260:]
     test_masks = masks[2074 + 260:]
     test_data = tf.data.Dataset.from_tensor_slices((test_images, test_masks))
     test_data = test_data.map(process_data)
-    print(test_data)
 
 # run main function
 if __name__ == "__main__":
--- recognition/ISIC-ImprovedUnet-44961374/process_data.py
+++ recognition/ISIC-ImprovedUnet-44961374/process_data.py
@@ -1,5 +1,5 @@
 """
-This script contains various functions required for data processing for the UNet model. 
+This script contains various functions required for image data processing for the UNet model. 
 @author: Mujibul Islam Dipto
 """
 import os  
@@ -10,20 +10,55 @@ from tensorflow.image import resize, decode_jpeg, decode_png
 from tensorflow.io import read_file
 
 
-def normalize_img(img):
-    return tf.cast(img, tf.float32) / 255.0
+def process_image(image, x, y):
+    """Proccesses the image of a scan. The image is decoded to a uint8 tensor.
+
+    Args:
+        image (Tensor): Tensor representing the image
+        x (float): x dimension for resizing
+        y (float): y dimension for resizing
+
+    Returns:
+        uint8 tensor: decoded image
+    """
+    image = decode_jpeg(image, channels=1)
+    image = resize(image, (x, y))
+    image = tf.cast(image, tf.float32) / 255.0
+    return image
+
+
+def process_mask(mask, x, y):
+    """Processes the image of a mask. The image is decoded to a uint8 or uint16 tensor.
+
+    Args:
+        mask (Tensor): Tensor represting the mask
+        x (float): x dimension for resizing
+        y (float): y dimension for resizing
+
+    Returns:
+        uint8 or uint16 tensor: decoded mask
+    """
+    mask = decode_png(mask)
+    mask = resize(mask, (x, y))
+    return mask
 
 
 def process_data(image_path, mask_path):
-    # process the image
+    """Processes an image and mask tuple. 
+
+    Args:
+        image_path (str): location of the image
+        mask_path (str): location of the mask
+
+    Returns:
+        tuple: decoded image and mask
+    """
+    # handle images of scans
     image = read_file(image_path)
-    image = decode_jpeg(image, channels = 1)
-    image = resize(image, (256, 256))
-    image = normalize_img(image)
-    
-    # process the mask
+    image = process_image(image, 256, 256)
+
+    # handle images of masks
     mask = read_file(mask_path)
-    mask = decode_png(mask, channels = 1)
-    mask = resize(mask, (256, 256))
-    mask = mask == [0, 255]
-    return image, mask
+    mask = process_mask(mask, 256, 256)
+    valid_mask = mask == [0, 255]
+    return image, valid_mask
