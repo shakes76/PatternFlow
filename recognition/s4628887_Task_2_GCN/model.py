1d36c549a7e907a240f98909f487f0e557ebd008
uly6mlv <uly6mlv@gmail.com>
2021-10-27 09:29:20 +1000

    Updated model.py

--- recognition/s4628887_Task_2_GCN/model.py
+++ recognition/s4628887_Task_2_GCN/model.py
@@ -4,18 +4,17 @@ from tensorflow.keras.layers import Input, Dropout
 from GCNConv import GCNConv
 
 
-def GCN_model(F, N, num_classes):
+def GCN_model(F, N, num_classes, channels_1=32, channels_2=8, dropout=0.5):
     """
+
     :param F: Number of features
     :param N: Number of nodes
     :param num_classes: Number of classes
+    :param channels_1: Number of channels in the first GCN layer
+    :param channels_2: Number of channels in the second GCN layer
+    :param dropout: Dropout rate for the features
     :return: a multi-layer GCN model
     """
-
-    # Parameters
-    channels_1 = 32          # Number of channels in the first GCN layer
-    channels_2 = 8           # Number of channels in the second GCN layer
-    dropout = 0.5           # Dropout rate for the features
 
     # Model definition
     X_in = Input(shape=(F, ))
