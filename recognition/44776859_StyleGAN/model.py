4ef83fbae34bcebe0f637d3f58ad1704d12f1b5f
Chris <catkinsv3@gmail.com>
2021-10-26 21:47:33 +1000

    Docstrings added and minor corrections performed.

--- recognition/44776859_StyleGAN/model.py
+++ recognition/44776859_StyleGAN/model.py
@@ -11,6 +11,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
+
 # Set tf memory growth, verify version and cuda/gpu functionality.
 print(f'\nTensorflow version: {tf.__version__}')
 print(f'Tensorflow CUDA {"is" if tf.test.is_built_with_cuda() else "is not"} available.')
@@ -28,7 +29,7 @@ import tensorflow_datasets as tfds
 
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Add, Dense, Flatten, Reshape, LeakyReLU, ReLU, \
-    Conv2D, AveragePooling2D, UpSampling2D, BatchNormalization, Dropout
+    Conv2D, AveragePooling2D, UpSampling2D
 from tensorflow.keras.initializers import GlorotNormal
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 from tensorflow.keras.optimizers import Adam
@@ -44,34 +45,32 @@ from matplotlib.ticker import FuncFormatter
 from IPython import display
 import time
 
-dark1 = '#191b26'
-dark2 = '#151722'
-white1 = '#fff'
-white2 = '#b3b5be'
-red1 = '#cd6152'
-red2 = '#cd6152'
-green1 = '#67a39a'
-green2 = '#253037'
-blue1 = '#4a64fd'
-blue2 = '#1b1f39'
+# Colours used in plots.
+dark = '#191b26'
+darker = '#151722'
+cream = '#b3b5be'
+mint = '#67a39a'
 
-mpl.rcParams['text.color'] = white2
-mpl.rcParams['axes.labelcolor'] = white2
-mpl.rcParams['axes.facecolor'] = dark1
-mpl.rcParams['axes.edgecolor'] = white2
-mpl.rcParams['figure.facecolor'] = dark2
-mpl.rcParams['xtick.color'] = white2
-mpl.rcParams['ytick.color'] = white2
+# Setting matplotlib to dark mode.
+mpl.rcParams['text.color'] = cream
+mpl.rcParams['axes.labelcolor'] = cream
+mpl.rcParams['axes.facecolor'] = dark
+mpl.rcParams['axes.edgecolor'] = cream
+mpl.rcParams['figure.facecolor'] = darker
+mpl.rcParams['xtick.color'] = cream
+mpl.rcParams['ytick.color'] = cream
 
 
 class AdaIN(tf.keras.layers.Layer):
+    """
+    Apply instanced scale and bias to normalised input.
+    """
     def __init__(self, epsilon=1e-5, **kwargs):
         super(AdaIN, self).__init__(**kwargs)
-        self.epsilon = epsilon
+        self.epsilon = epsilon  # To avoid dividing by zero.
 
     def call(self, inputs, *args, **kwargs):
         """
-        Apply instanced scale and bias to normalised input.
         Normalises x, then applies individual scale and bias to each channel(feature) of x,
         for every element in the batch.
         """
@@ -90,27 +89,32 @@ class AdaIN(tf.keras.layers.Layer):
 
 
 class StyleGAN:
+    """Tensorflow 2 StyleGAN implementation."""
+
     def __init__(self, dataset=None, dataset_path=None, dataset_name='', target_image_dims=(64, 64),
                  epochs=999, batch_size=32, z_length=100, save_progress_plots=True, show_progress_plots=True,
                  progress_plot_batch_interval=50, save_model_checkpoints=False, model_checkpoint_interval=15,
                  save_directory='./output', print_model_summaries=True, running_in_notebook=False):
         """
+        Initialise generator and discriminator, and read in dataset.
 
-        :param dataset:
-        :param dataset_path:
-        :param dataset_name:
-        :param target_image_dims:
-        :param epochs:
-        :param batch_size:
-        :param z_length:
-        :param save_progress_plots:
-        :param show_progress_plots:
-        :param progress_plot_batch_interval:
-        :param save_model_checkpoints:
-        :param model_checkpoint_interval:
-        :param save_directory:
-        :param print_model_summaries:
-        :param running_in_notebook:
+        :param dataset: (Optional) pass in your own dataset.
+        :param dataset_path: Path to local dataset. Path should lead to a folder of images.
+        :param dataset_name: Key words to describe the dataset (eg. 'oasis', 'knee', 'mnist').
+        :param target_image_dims: The desired dimensions to reshape the input to and shape the output to.
+        Dimensions must be square and must not exceed the input data dimensions.
+        :param epochs: Number of epochs to train for.
+        :param batch_size: Number of samples per batch. Lower this if training fails due to OOM.
+        :param z_length: Length of the latent vector z. 100 is sufficient for MNIST Digits, but larger datasets
+        require ~512 to avoid mode collapse.
+        :param save_progress_plots: Whether to export progress plots.
+        :param show_progress_plots: Whether to show progress plots (only set to true if running in jupyter notebook)
+        :param progress_plot_batch_interval: How often to generate a new progress plot.
+        :param save_model_checkpoints: Whether or not to save model checkpoints.
+        :param model_checkpoint_interval: How many epochs to have elapsed before saving a new model checkpoint.
+        :param save_directory: Path to save progress plots and checkpoints to.
+        :param print_model_summaries: Whether to print summaries of models upon creation.
+        :param running_in_notebook: Whether this is being executed in a jupyter notebook.
         """
         self.dataset = dataset
         self.epochs = epochs
@@ -135,6 +139,7 @@ class StyleGAN:
         self.max_graphed = 50  # Number of loss values to use in progress plot.
         self.colorbar_norm = mpl.colors.TwoSlopeNorm(vmin=-1.05, vcenter=-0.1, vmax=1.05)
 
+        # Whether to use MNIST dataset from tfds or read in dataset from provided path.
         if dataset is None and dataset_path:
             self.dataset = self.get_local_image_dataset(dataset_path, target_image_dims)
         elif any(name in dataset_name for name in ('mnist', 'digit')):
@@ -153,6 +158,7 @@ class StyleGAN:
         self.generator = self.make_generator()
         self.discriminator = self.make_discriminator()
 
+        # Determine number of trainable weights in each model.
         self.num_gen_weights = int(np.sum([np.prod(v.get_shape()) for v in self.generator.trainable_weights]))
         self.num_disc_weights = int(np.sum([np.prod(v.get_shape()) for v in self.discriminator.trainable_weights]))
 
@@ -165,11 +171,12 @@ class StyleGAN:
                                               generator=self.generator,
                                               discriminator=self.discriminator)
 
+        # Common seed comprising input z and noise, to be used repeatedly within progress plots.
         self.progress_seed = self.generate_inputs(batch_size=8)
 
         self.g_losses, self.d_real_losses, self.d_fake_losses = [], [], []
         self.g_loss_avg, self.d_fake_loss_avg, self.d_real_loss_avg, self.d_loss_avg = 0, 0, 0, 0
-        self.balance = 0
+        self.balance = 0  # Adversarial balance.
 
         self.start_time = 0
         self.iteration = 0
@@ -177,6 +184,7 @@ class StyleGAN:
         self.current_batch = 0
         self.current_epoch = 0
 
+        # Determine variance of dataset (within shuffled sample).
         self.dataset_variance, self.dataset_variance_sum = self._calculate_variance(
             list(self.dataset.take(1).as_numpy_iterator())[0])
 
@@ -189,11 +197,15 @@ class StyleGAN:
         self.output_model_plots()
 
     def get_hyperparameters(self, dataset_name):
+        """
+        Retrieves the optimised hyperparameters for the named dataset.
+        :param dataset_name: Key words describing dataset.
+        """
         dataset_name = dataset_name.lower()
         print()
         if any(name in dataset_name for name in ('mnist', 'digits')):
             print('MNIST Digits hyperparameter presets loaded.')
-            # Tested at (28,28,1)
+            # Tested at (28,28,1) image size.
             params = {
                 'learning_rate_gen': 0.0002,
                 'learning_rate_disc': 0.0002,
@@ -243,15 +255,24 @@ class StyleGAN:
         return params
 
     def get_mnist_dataset(self):
-        print('MNIST Digits dataset loaded.')
+        """Retrieve MNIST Digits dataset from tfds. Normalise, shuffle and batch the dataset."""
         dataloader = tfds.load('mnist', as_supervised=True)
         dataset = dataloader['train']
         # Cast [0,255] images to [-1,1].
         dataset = dataset.map(lambda image, label: Rescaling(scale=1. / 127.5, offset=-1)(image))
         dataset = dataset.shuffle(self.batch_size).batch(self.batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+        print('MNIST Digits dataset loaded.')
         return dataset
 
     def get_local_image_dataset(self, path, image_dims, make_grayscale=True):
+        """
+        Retrieve dataset from provided path, before rescaling pixel values to [-1,1] and converting to grayscale.
+
+        :param path: Path to dataset (must be a folder containing only images).
+        :param image_dims: Dimensions to scale images to.
+        :param make_grayscale: Whether or not to convert images to grayscale.
+        :return:
+        """
         print(f'Loading dataset from {path} at {image_dims} resolution.')
         dataset = tf.keras.preprocessing.image_dataset_from_directory(path,
                                                                       labels=None,
@@ -267,6 +288,7 @@ class StyleGAN:
         return dataset
 
     def check_dataset(self):
+        """Retrieve and plot one sample from the active dataset."""
         print('Sample from dataset:')
         batch = self.dataset.take(1)
         image = list(batch.as_numpy_iterator())[0][0]
@@ -276,17 +298,24 @@ class StyleGAN:
         plt.show()
 
     def _calculate_variance(self, images, epsilon=1e-7):
-        """Calculate pixel-wise variance among all permutations of image pairs."""
+        """
+        Calculate pixel-wise variance among all permutations of image pairs.
+
+        :param images: Array of images to use.
+        :param epsilon: Small value to avoid dividing by zero.
+        :return: pixel-wise variance, and sum of variance.
+        """
         diff, count = np.zeros(shape=images[0].shape), 0
-        for x in itertools.combinations(images, 2):
+        for x in itertools.combinations(images, 2):  # Compute all permutations of pairs.
             diff = diff + np.abs(x[0] - x[1]) + epsilon
             count += 1
-            if count > 500:
+            if count > 500:  # Short-circuit if the sample is too large.
                 break
         diff = diff / count
         return diff, np.sum(diff)
 
     def output_model_plots(self):
+        """Write image of model architectures to directory."""
         tf.keras.utils.plot_model(self.generator, show_shapes=True,
                                   to_file=self.save_directory + '/generator_plot.png')
         tf.keras.utils.plot_model(self.discriminator, show_shapes=True,
@@ -312,6 +341,13 @@ class StyleGAN:
         return int(x), count
 
     def _disc_block(self, x, size, i, reduce=True):
+        """
+        Modular discriminator block
+        :param x: Input matrix.
+        :param size: Number of filters.
+        :param i: Which numbered block this is.
+        :param reduce: Whether to apply pooling.
+        """
         x = Conv2D(filters=size, kernel_size=3, strides=1, padding='same',
                    kernel_initializer=GlorotNormal(), activation=None,
                    name=f'conv{i}')(x)
@@ -320,9 +356,20 @@ class StyleGAN:
         return LeakyReLU(0.2)(x)
 
     def _gen_block(self, x, w, noise, size, i):
+        """
+        Modular generator block.
+        :param x: Input matrix.
+        :param w: Latent w vector.
+        :param noise: Gaussian noise matrix.
+        :param size: Number of neurons/filters.
+        :param i: Which numbered block this is.
+        """
+        # Affine transformation A:
         scale = Dense(size, name=f'scale{i}')(w)
         bias = Dense(size, name=f'bias{i}')(w)
+        # Transformation B:
         noise = Dense(size, name=f'noise{i}')(noise)
+
         x = UpSampling2D(name=f'upscale{i}')(x)
         x = Conv2D(filters=size, kernel_size=3, strides=1, padding='same',
                    kernel_initializer=GlorotNormal(), use_bias=False,
@@ -330,9 +377,9 @@ class StyleGAN:
         x = Add(name=f'add_noise{i}')([x, noise])
         x = AdaIN(name=f'ada_in{i}')([x, scale, bias])
         return ReLU(name=f'relu{i}')(x)
-        # return LeakyReLU(0.2, name=f'leaky{i}')(x)
 
     def make_generator(self):
+        """Construct generator (synthesis network)"""
         # Process all inputs - const vector, z vectors and noise matrices.
         const = Input(shape=(self.z_length,), name='const')
         z, noise, size = [], [], self.start_size
@@ -369,6 +416,7 @@ class StyleGAN:
         return model
 
     def make_discriminator(self):
+        """Construct discriminator."""
         image = Input([self.image_size, self.image_size, self.num_channels], name='image_in')
 
         # Start block
@@ -389,12 +437,19 @@ class StyleGAN:
         return model
 
     def generate_inputs(self, batch_size):
+        """
+        Randomly generated latent z vectors and noise matrices to use as inputs to the generator.
+        :param batch_size: Number of samples to return.
+        """
         const = tf.random.normal(mean=0., stddev=1., shape=(batch_size, self.z_length))
         z, noise, size = [], [], self.start_size
         for i in range(self.num_blocks):
+            # Latent z vectors.
             z.append(tf.random.normal(mean=0., stddev=1., shape=(batch_size, self.z_length)))
-            noise.append(tf.random.uniform(minval=-1., maxval=1.,
-                                           shape=(batch_size, size * 2, size * 2, self.num_channels)))
+            # Random noise matrices.
+            noise.append(
+                tf.random.normal(mean=0., stddev=1., shape=(batch_size, size * 2, size * 2, self.num_channels)))
+            # Double the size of noise matrices each iteration, as each block outputs an up-scaled image.
             size *= 2
         return [const] + z + noise
 
@@ -411,7 +466,11 @@ class StyleGAN:
             self.d_fake_loss_avg = round(average(self.d_fake_losses, weights=weights), 3)
             self.d_real_loss_avg = round(average(self.d_real_losses, weights=weights), 3)
         self.d_loss_avg = (self.d_fake_loss_avg + self.d_real_loss_avg) / 2
+        # Balance is the difference between the weighted average of generator and discriminator losses.
+        # Negative balance implies a weak discriminator, while positive balance implies a weak generator.
+        # Ideal balance is 0.
         self.balance = self.g_loss_avg - self.d_loss_avg
+        # Bound the value between [-1,1] so we don't draw off the edge of the bar.
         if self.balance < -1:
             self.balance = -1
         elif self.balance > 1:
@@ -422,14 +481,17 @@ class StyleGAN:
         Plot sample generated images over losses, balance and variance during training.
         """
         if not self.jupyter:
+            # Matplotlib does not automatically close plots outside of jupyter.
             plt.close('all')
 
         self._update_avg_losses()
 
         # Generate generator sample images.
         gen_images = self.generator(self.progress_seed, training=False)
+
         # Calculate pixel-wise variance among generated samples.
         sample_variance, var_sum = self._calculate_variance(gen_images)
+        # Convert sum of variance to a percentage of dataset variance.
         var_percent = (var_sum / self.dataset_variance_sum) * 100
 
         fig = plt.figure(constrained_layout=False, figsize=(14.5, 11))
@@ -475,7 +537,7 @@ class StyleGAN:
             self.g_losses = self.g_losses[len(self.g_losses) - self.max_graphed:]
             self.d_real_losses = self.d_real_losses[len(self.d_real_losses) - self.max_graphed:]
             self.d_fake_losses = self.d_fake_losses[len(self.d_fake_losses) - self.max_graphed:]
-        ax8.plot(self.g_losses, c=green1, label=f'Generator loss')
+        ax8.plot(self.g_losses, c=mint, label=f'Generator loss')
         ax8.plot(self.d_real_losses, c='purple', label=f'Discriminator loss vs real')
         ax8.plot(self.d_fake_losses, c='#ff884d', label=f'Discriminator loss vs fake')
         # Put loss value at tail end of each loss line.
@@ -501,12 +563,14 @@ class StyleGAN:
         cb.ax.yaxis.set_ticks_position('none')
         cb.ax.set_ylabel('Adversarial Balance', fontsize=12, labelpad=2)
         cb.ax.yaxis.set_label_position('left')
-        # cb.ax.yaxis.tick_left()
+        # Plot acceptable midpoints.
         cb.ax.axhline(y=0.15, c='gray', linestyle='--', linewidth=1)
         cb.ax.axhline(y=-0.15, c='gray', linestyle='--', linewidth=1)
+        # Plot actual balance line.
         cb.ax.axhline(y=self.balance, c='black', linestyle='-', linewidth=7)
         cb.ax.axhline(y=self.balance, c='lime' if -0.15 < self.balance < 0.15 else 'red', linestyle='-', linewidth=5)
 
+        # Plot generator variance.
         ax10.set_title(f'Generator variance: {int(round(var_percent)):03}%', size=14)
         ax10.imshow(sample_variance, cmap='viridis')
         ax10.axes.xaxis.set_visible(False)
@@ -578,6 +642,7 @@ class StyleGAN:
         return g_loss, d_real_loss, d_fake_loss
 
     def train(self):
+        """Training loop."""
         self.start_time = time.time()
         for epoch in range(1, self.epochs + 1):
             epoch_start_time = time.time()
@@ -599,18 +664,21 @@ class StyleGAN:
                         display.clear_output(wait=True)
                     self._plot_gan_progress()
                 if not self.jupyter or not self.show_progress_plots:
+                    # Print training progress.
                     print(
                         f'\rEpoch {epoch:03} / {self.epochs} | batch {i:03} / {self.num_batches} | '
                         f'g_loss: {round(g_loss, 4):.4f} | d_fake_loss: {round(d_fake_loss, 4):.4f} | '
                         f'd_real_loss: {round(d_real_loss, 4):.4f} | '
                         f'Time taken: {round(((time.time() - epoch_start_time) / 60), 2):.2f} minutes', end='')
             print()
+
             # Save model checkpoint every n epochs.
             if self.save_model_checkpoints and epoch % self.model_checkpoint_interval == 0:
                 self.checkpoint.save(file_prefix=self.checkpoint_prefix)
 
 
 if __name__ == '__main__':
+    # This is used for testing. The driver script is where you should execute training.
     gan = StyleGAN(dataset=None,
 
                    # dataset_path=None,
