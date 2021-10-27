9cb85be7562b350de609062e1ccaa1d134649ff1
Tannishpage <tannishpage1@gmail.com>
2021-10-27 22:49:26 +1000

    rewrote the create_generator function in data_preprocess.py, using the built-in keras functions to load images weren't working. Now create_generator will load all the image files and their groundtruths using opencv as it's faster. Now main.py (The driver file) loads data, splits it to train, test, split, creates the model and trains it. Then it plots the training metrics.

--- recognition/45827422_ISIC_Segmenting_Improved_UNET/data_preprocess.py
+++ recognition/45827422_ISIC_Segmenting_Improved_UNET/data_preprocess.py
@@ -7,7 +7,8 @@ validation. Then create and return generators for those
 import sys
 import os
 import shutil
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+#from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import cv2
 import tensorflow as tf
 import numpy as np
 
@@ -15,50 +16,17 @@ def create_generator(path_to_data, path_to_gt, img_size, nums):
     # Creating Train generators
     X_train = []
     Y_train = []
-    for i, file in enumerate(os.listdir(path_to_data)):
-        if i >= nums:
-            break
-        ximg = load_img(os.path.join(path_to_data, file), target_size=(128, 128), color_mode="grayscale")
-        ximg = img_to_array(ximg)/255.0
+    for i, file in enumerate([f for f in os.listdir(path_to_data) if f.endswith(".jpg")]):
+        ximg = cv2.imread(os.path.join(path_to_data, file), 0)
+        yimg = cv2.imread(os.path.join(path_to_gt, file.replace(".jpg", "_segmentation.png")), 0)
+        ximg = cv2.resize(ximg, img_size)
+        yimg = cv2.resize(yimg, img_size)
+        ximg = ximg/255.0
+        yimg = yimg/255.0
         X_train.append(ximg)
-
-    for i, file in enumerate(os.listdir(path_to_gt)):
-        if i >= nums:
-            break
-        yimg = load_img(os.path.join(path_to_gt, file), target_size=(128, 128), color_mode="grayscale")
-        yimg = img_to_array(yimg)/255.0
         Y_train.append(yimg)
+
     Y_train = np.array(Y_train)
     Y_train = tf.keras.utils.to_categorical(Y_train, num_classes=2)
 
     return np.array(X_train), Y_train
-
-
-def move_files(files, from_dest, destination):
-    for file in files:
-        shutil.copy(os.path.join(from_dest, file), destination)
-
-def process_data_folders(path_to_data):
-    data_files = [f for f in os.listdir(path_to_data) if f.endswith(".jpg")]
-    num_files = len(data_files)
-
-    # The groundtruth files are named similarly to the data_files.
-    # ISIC_<Number>.jpg
-    # ISIC_<Number>_segmentation.png
-    # Split data into train, test, validation
-
-    training_split = int(num_files*0.6)
-    validation_split = training_split + int(num_files*0.1)
-    testing_split = validation_split + int(num_files*0.3)
-
-    training = data_files[0:training_split]
-    training_gt = [t.replace(".jpg", "_segmentation.png") for t in training]
-    validation = data_files[training_split:validation_split]
-    validation_gt = [t.replace(".jpg", "_segmentation.png") for t in training]
-    testing = data_files[validation_split:]
-    testing_gt = [t.replace(".jpg", "_segmentation.png") for t in training]
-
-    return (training, training_gt), (validation, validation_gt), (testing, testing_gt)
-
-#/home/tannishpage/Documents/COMP3710_DATA/ISIC2018_Task1-2_Training_Input_x2/
-#/home/tannishpage/Documents/COMP3710_DATA/ISIC2018_Task1_Training_GroundTruth_x2/
--- recognition/45827422_ISIC_Segmenting_Improved_UNET/main.py
+++ recognition/45827422_ISIC_Segmenting_Improved_UNET/main.py
@@ -1,47 +1,41 @@
 import tensorflow as tf
 from data_preprocess import *
 from improved_unet import *
-
+import matplotlib.pyplot as plt
 
 home = "/home/tannishpage/Documents/COMP3710_DATA"
 data_folder = "ISIC2018_Task1-2_Training_Input_x2/"
 gt_folder = "ISIC2018_Task1_Training_GroundTruth_x2/"
-"""
-train, val, test = process_data_folders(os.path.join(home, data_folder))
 
-move_files(train[0], os.path.join(home, data_folder),
-            os.path.join(home, "train/data/images"))
-move_files(train[1], os.path.join(home, gt_folder),
-            os.path.join(home, "train/groundtruth/images"))
-
-move_files(val[0], os.path.join(home, data_folder),
-            os.path.join(home, "val/data/images"))
-move_files(val[1], os.path.join(home, gt_folder),
-            os.path.join(home, "val/groundtruth/images"))
-
-move_files(test[0], os.path.join(home, data_folder),
-            os.path.join(home, "test/data/images"))
-move_files(test[1], os.path.join(home, gt_folder),
-            os.path.join(home, "test/groundtruth/images"))
-"""
-train = create_generator(os.path.join(home, "train/data/images"),
-                        os.path.join(home, "train/groundtruth/images"),
+data = create_generator(os.path.join(home, data_folder),
+                        os.path.join(home, gt_folder),
                         (128, 128), 750)
-
-val = create_generator(os.path.join(home, "val/data/images"),
-                        os.path.join(home, "val/groundtruth/images"),
-                        (128, 128), 100)
-
-test = create_generator(os.path.join(home, "test/data/images"),
-                        os.path.join(home, "test/groundtruth/images"),
-                        (128, 128), 250)
-
 
 
 model = create_model((128, 128, 1))
 #model.summary()
 
-training_results = model.fit(train[0], train[1],
-                            epochs=10,
-                            validation_data=val,
-                            batch_size=32)
+train_split = int(0.7*len(data[0]))
+test_split = train_split + int(0.2*len(data[0]))
+val_split = test_split + int(0.1*len(data[0]))
+Xtrain = data[0][0:train_split]
+Ytrain = data[1][0:train_split]
+Xtest = data[0][train_split:test_split]
+Ytest = data[1][train_split:test_split]
+Xval = data[0][test_split:val_split]
+Yval = data[1][test_split:val_split]
+
+training_results = model.fit(Xtrain, Ytrain,
+                            epochs=25,
+                            validation_data=(Xval, Yval),
+                            batch_size=64)
+plt.figure(1)
+plt.subplot(2, 1, 1)
+plt.plot(range(len(training_results.history['loss'])),training_results.history['loss'], label='loss')
+plt.plot(range(len(training_results.history['val_loss'])),training_results.history['val_loss'], label='val_loss')
+plt.legend()
+plt.subplot(2, 1, 2)
+plt.plot(range(len(training_results.history['dice_similarity'])),training_results.history['dice_similarity'], label='dice_similarity')
+plt.plot(range(len(training_results.history['val_dice_similarity'])),training_results.history['val_dice_similarity'], label='val_dice_similarity')
+plt.legend()
+plt.show()
