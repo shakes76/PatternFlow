e98fecce8e21c1c76b6e3772a1192b3890c54741
Kristian Thomassen <kethomassen@gmail.com>
2021-10-27 10:23:31 +1000

    Tidy up driver.py slightly

--- recognition/OASIS_VQVAE/driver.py
+++ recognition/OASIS_VQVAE/driver.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 import argparse
 
+IMG_SIZE = 256
+
 class SSIMCallback(tf.keras.callbacks.Callback):
     def __init__(self, validation_data, shift=0.0):
         super(SSIMCallback, self).__init__()
@@ -52,23 +54,39 @@ def plot_history(history):
     plt.show()
 
 
-def show_image_and_reconstruction(original, cb, reconstructed):
-    plt.subplot(1, 3, 1)
-    plt.imshow(original, cmap="gray")
-    plt.title("Original")
-    plt.axis("off")
+def show_reconstruction_examples(model, images, shift):
+    reconstructions = model.predict(images)
 
-    plt.subplot(1, 3, 2)
-    plt.imshow(cb)
-    plt.title("Codebook")
-    plt.axis("off")
+    encoder_outputs = model.encoder().predict(images)
+    encoder_outputs_flat = encoder_outputs.reshape(-1, encoder_outputs.shape[-1])
 
-    plt.subplot(1, 3, 3)
-    plt.imshow(reconstructed, cmap="gray")
-    plt.title("Reconstructed")
-    plt.axis("off")
+    codebook_indices = get_closest_embedding_indices(model.quantizer().embeddings(), encoder_outputs_flat)
+    codebook_indices = codebook_indices.numpy().reshape(encoder_outputs.shape[:-1])
 
-    plt.show()
+    for i in range(len(images)):
+        # add the shift back to the images to undo the initial shifting (e.g. go from [-0.5, 0.5] to [0,1])
+        original_image = tf.reshape(images[i], (1, IMG_SIZE, IMG_SIZE, 1)) + shift
+        reconstructed_image = tf.reshape(reconstructions[i], (1, IMG_SIZE, IMG_SIZE, 1)) + shift
+        codebook_image = codebook_indices[i]
+
+        plt.subplot(1, 3, 1)
+        plt.imshow(tf.squeeze(original_image), cmap="gray")
+        plt.title("Original")
+        plt.axis("off")
+
+        plt.subplot(1, 3, 2)
+        plt.imshow(codebook_image)
+        plt.title("Codebook")
+        plt.axis("off")
+
+        plt.subplot(1, 3, 3)
+        plt.imshow(tf.squeeze(reconstructed_image), cmap="gray")
+        plt.title("Reconstructed")
+        plt.axis("off")
+
+        plt.show()
+        ssim = tf.math.reduce_sum(tf.image.ssim(original_image, reconstructed_image, max_val=1.0)).numpy()
+        print("SSIM: ", ssim)
 
 
 def load_images(location, image_size, batch_size):
@@ -84,13 +102,16 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="VQVAE trainer")
 
-    parser.add_argument("--data", required=True, type=str, help="Location of unzipped OASIS dataset. Folder should contain the folders 'keras_png_slices_train' and 'keras_png_slices_validate'.")
+    parser.add_argument("--data", 
+        type=str, 
+        default="keras_png_slices_data", 
+        help="Location of unzipped OASIS dataset. Folder should contain the folders 'keras_png_slices_train' and 'keras_png_slices_validate'.")
 
     parser.add_argument("--K", "--num-embeddings", type=int, default=512, help="Number of embeddings, described as K in the VQVAE paper (default: 512)")
     parser.add_argument("--D", "--embedding-dim", type=int, default=2, help="Size of the embedding vectors, described as D in the VQVAE paper (default: 2)")
     parser.add_argument("--beta", type=float, default=1.5, help="Committment cost, described as beta in VQVAE paper (default: 1.5)")
 
-    parser.add_argument("--epochs", type=int, default=30, help="Number of epochs to train for (default: 30)")
+    parser.add_argument("--epochs", type=int, default=1, help="Number of epochs to train for (default: 30)")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training (default: 32)")
     parser.add_argument("--learning_rate", type=float, default=2e-4, help="Learning rate for ADAM optimiser (default 2e-4)")
 
@@ -99,8 +120,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # load OASIS images from folder
-    IMG_SIZE = 256
-
     dataset             = load_images(args.data + "/keras_png_slices_train", (IMG_SIZE, IMG_SIZE), args.batch_size)
     dataset_validation  = load_images(args.data + "/keras_png_slices_validate", (IMG_SIZE, IMG_SIZE), args.batch_size)
 
@@ -120,7 +139,7 @@ if __name__ == "__main__":
     vqvae_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate))
 
     # fit it
-    history = vqvae_model.fit(dataset, 
+    history = vqvae_model.fit(dataset_validation, #dataset, 
                               epochs=args.epochs, 
                               batch_size=args.batch_size, 
                               callbacks=[SSIMCallback(dataset_validation, args.shift)])
@@ -128,26 +147,7 @@ if __name__ == "__main__":
     # plot history
     plot_history(history)
 
-    # visualise some reconstructions
-    num_batches_to_show = 2
-    num_images_per_batch_to_show = 5
-
-    test_images_batches = dataset.take(num_batches_to_show)
-    for test_images in test_images_batches.as_numpy_iterator():
-        reconstructions = vqvae_model.predict(test_images)
-
-        encoder_outputs = vqvae_model.encoder().predict(test_images)
-        encoder_outputs_flat = encoder_outputs.reshape(-1, encoder_outputs.shape[-1])
-
-        codebook_indices = get_closest_embedding_indices(vqvae_model.quantizer().embeddings(), encoder_outputs_flat)
-        codebook_indices = codebook_indices.numpy().reshape(encoder_outputs.shape[:-1])
-
-        for i in range(num_images_per_batch_to_show):
-            # add the shfit back to the images to undo the initial shifting (e.g. go from [-0.5, 0.5] to [0,1])
-            original_image = tf.reshape(test_images[i], (1, IMG_SIZE, IMG_SIZE, 1)) + args.shift
-            reconstructed_image = tf.reshape(reconstructions[i], (1, IMG_SIZE, IMG_SIZE, 1)) + args.shift
-            codebook_image = codebook_indices[i]
-
-            show_image_and_reconstruction(tf.squeeze(original_image), codebook_image, tf.squeeze(reconstructed_image))
-            ssim = tf.math.reduce_sum(tf.image.ssim(original_image, reconstructed_image, max_val=1.0)).numpy()
-            print("SSIM: ", ssim)
+    # plot some example images, codebooks, and reconstructions
+    # Need to use this loop hack because of tf.Dataset limitations I can't find a workaround of yet
+    for test_images in dataset.take(1).as_numpy_iterator():
+        show_reconstruction_examples(vqvae_model, test_images[1:10], args.shift)
