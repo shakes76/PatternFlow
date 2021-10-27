3049ecc819d66d3237bbd49703af853e3c8e02d0
Tompnyx <tompnyx@outlook.com>
2021-10-27 23:33:41 +1000

    Changed certain parameters of the model and implemented correct element-wise sums and segmentation layers so that the model now works and compiles properly. Renamed variables in the model so that they represent which layer they are present on, rather than what iteration they are present on.

--- recognition/ISIC_Segmentation_UNet_s4587209/model.py
+++ recognition/ISIC_Segmentation_UNet_s4587209/model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.layers import Conv2D, Dropout, Conv2DTranspose, Input, concatenate, UpSampling2D
+from tensorflow.keras.layers import concatenate, Conv2D, Dropout, Input, UpSampling2D
 
 
 def improved_unet(height, width, channels):
@@ -7,104 +7,110 @@ def improved_unet(height, width, channels):
     act = 'relu'
     kern_init = 'he_normal'
     pad = 'same'
-    stride = 2
     drop = 0.3
     # Building the model
     inputs = Input((height, width, channels))
 
-    # Contraction
-
     # 3x3x3 convolution - 16 filters
     conv1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(inputs)
     # Context module
-    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(conv1)
+    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv1)
     context1 = Dropout(drop)(context1)
-    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(context1)
-    # Concatenation
-    concat1 = concatenate([conv1, context1])
+    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(context1)
+    # Element-wise sum
+    out1 = conv1 + context1
 
     # 3x3x3 stride 2 convolution - 32 filters
-    conv2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(concat1)
+    conv2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out1)
     # Context module
-    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(conv2)
+    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv2)
     context2 = Dropout(drop)(context2)
-    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(
+    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
         context2)
-    # Concatenation
-    concat2 = concatenate([conv2, context2])
+    # Element-wise sum
+    out2 = conv2 + context2
 
     # 3x3x3 stride 2 convolution - 64 filters
-    conv3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(concat2)
+    conv3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out2)
     # Context module
-    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(conv3)
+    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv3)
     context3 = Dropout(drop)(context3)
-    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(
+    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
         context3)
-    # Concatenation
-    concat3 = concatenate([conv3, context3])
+    # Element-wise sum
+    out3 = conv3 + context3
 
     # 3x3x3 stride 2 convolution - 128 filters
-    conv4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(concat3)
+    conv4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out3)
     # Context module
-    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(conv4)
+    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv4)
     context4 = Dropout(drop)(context4)
-    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(
+    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
         context4)
-    # Concatenation
-    concat4 = concatenate([conv4, context4])
+    # Element-wise sum
+    out4 = conv4 + context4
 
     # 3x3x3 stride 2 convolution - 256 filters
-    conv5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(concat4)
+    conv5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out4)
     # Context module
-    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(conv5)
+    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv5)
     context5 = Dropout(drop)(context5)
-    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad, strides=stride)(
+    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
         context5)
-    # Concatenation
-    concat5 = concatenate([conv5, context5])
+    # Element-wise sum
+    out5 = conv5 + context5
     # Up-sampling - 128 filters
-    upsamp1 = Conv2DTranspose(fil * 8, kern, activation=act, kernel_initializer=kern_init,
-                              padding=pad, strides=stride)(concat5)  # CHANGE THIS
+    upsamp5 = UpSampling2D(size=(2, 2))(out5)
+    upsamp5 = Conv2D(fil * 8, kern, padding=pad, strides=(2, 2))(upsamp5)
 
     # Expansive path
 
     # Concatenation
-    concat6 = concatenate([upsamp1, concat4])
+    concat6 = concatenate([upsamp5, out4])
     # Localisation - 128 filters
-    local1 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat6)
-    local1 = Conv2D(fil * 8, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local1)
+    local6 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat6)
+    local6 = Conv2D(fil * 8, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local6)
     # Up-sampling - 64 filters
-    upsamp2 = Conv2DTranspose(fil * 4, kern, activation=act, kernel_initializer=kern_init,
-                              padding=pad, strides=stride)(local1)
+    upsamp6 = UpSampling2D(size=(2, 2))(local6)
+    upsamp6 = Conv2D(fil * 4, (2, 2), padding=pad, strides=(2, 2))(upsamp6)
 
     # Concatenation
-    concat7 = concatenate([upsamp2, concat3])
+    concat7 = concatenate([upsamp6, out3])
     # Localisation - 64 filters
-    local2 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat7)
-    local2 = Conv2D(fil * 4, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local2)
+    local7 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat7)
+    local7 = Conv2D(fil * 4, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local7)
     # Segmentation
-    segment1 = local2
+    segment7 = local7
     # Up-sampling - 32 filters
-    upsamp3 = Conv2DTranspose(fil * 2, kern, activation=act, kernel_initializer=kern_init,
-                              padding=pad, strides=stride)(local2)
+    upsamp7 = UpSampling2D(size=(2, 2))(local7)
+    upsamp7 = Conv2D(fil * 2, (2, 2), padding=pad, strides=(2, 2))(upsamp7)
 
     # Concatenation
-    concat8 = concatenate([upsamp3, concat2])
+    concat8 = concatenate([upsamp7, out2])
     # Localisation - 32 filters
-    local3 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat8)
-    local3 = Conv2D(fil * 2, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local3)
+    local8 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat8)
+    local8 = Conv2D(fil * 2, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local8)
     # Segmentation
-    segment2 = local3
+    segment8 = local8
     # Up-sampling - 16 filters
-    upsamp4 = Conv2DTranspose(fil, kern, activation=act, kernel_initializer=kern_init,
-                              padding=pad, strides=stride)(local3)
+    upsamp8 = UpSampling2D(size=(2, 2))(local8)
+    upsamp8 = Conv2D(fil, (2, 2), padding=pad, strides=(2, 2))(upsamp8)
 
     # Concatenation
-    concat9 = concatenate([upsamp4, concat1])
+    concat9 = concatenate([upsamp8, out1])
     # 3x3x3 convolution - 32 filters
-    conv6 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat9)
+    conv9 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat9)
     # Segmentation
-    segment3 = conv6
+    segment9 = conv9
 
-    outputs = Conv2D(1, (1, 1), activation='softmax')(conv6)
+    # Upscale Segmented Layers and apply
+    segment7 = UpSampling2D(size=(2, 2))(segment7)
+    segment7 = Conv2D(fil * 2, (1, 1), strides=(2, 2))(segment7)
+    segment8 = segment8 + segment7
+    segment8 = UpSampling2D(size=(2, 2))(segment8)
+    segment8 = Conv2D(fil, (1, 1), strides=(2, 2))(segment8)
+    segment9 = segment9 + segment8
+
+    # Softmax
+    outputs = Conv2D(1, (1, 1), activation='softmax')(segment9)
     return inputs, outputs
