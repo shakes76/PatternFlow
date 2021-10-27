Implement structural similarity calculation with reconstructed images Fix typos and bugs in the model Add image load limiter for quicker testing

recognition/45801150_Task6/src/VQVAE.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose
@@ -16,11 +17,10 @@ def create_encoder(latent_dimensions):
     encoder.add(Conv2D(latent_dimensions, 1, padding="same"))
     return encoder
 
-def create_decoder(encoder: Sequential, latent_dimensions):
+def create_decoder():
     decoder = Sequential(name="Decoder")
-    input_shape = (32, 32, 16)
-    decoder.add(Conv2DTranspose(64, 3, activation="relu", strides="2", padding="same", pe=input_shape))
-    decoder.add(Conv2DTranspose(32, 3, activation="relu", strides="2", padding="same"))
+    decoder.add(Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same"))
+    decoder.add(Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same"))
     decoder.add(Conv2DTranspose(1, 3, padding="same"))
     return decoder
 
@@ -42,7 +42,7 @@ class VectorQuantiser(keras.layers.Layer):
         similarity = tf.matmul(inputs, self.embeddings)
         distances = (
             tf.reduce_sum(inputs ** 2, axis=1, keepdims=True)
-            + tf.reduce_sum(self.embedding_dimensions ** 2, axis=0)
+            + tf.reduce_sum(self.embeddings ** 2, axis=0)
             - 2 * similarity
         )
         return tf.argmin(distances, axis=1)
@@ -117,14 +117,59 @@ class VQVae(keras.models.Sequential):
 
         return losses
 
-def train_vqvae(vqvae: VQVae):
-    x_train, x_test = load_oasis_data.get_data()
-
-    x_train = np.expand_dims(x_train, -1)
-    x_train_scaled = (x_train / 255.0) - 0.5
-
-    data_variance = np.var(x_train / 255.0)
-    vqvae = VQVae(data_variance, latent_dimensions=32, num_embeddings=64)
+def train_vqvae(x_train, variance):
+    vqvae = VQVae(variance, latent_dimensions=32, num_embeddings=64)
     vqvae.compile(optimizer=keras.optimizers.Adam())
-    vqvae.fit(x_train_scaled, epochs=2, batch_size=128)
+    vqvae.fit(x_train, epochs=2, batch_size=128)
+    return vqvae
+
+
+
+def compare_reconstructions(vqvae: VQVae, test_images, n_images):
+    indices = np.random.choice(len(test_images), n_images)
+    test_samples = test_images[indices]
+
+    reconstructed = vqvae.predict(test_samples)
+
+    calculate_ssim(test_samples, reconstructed)
+    # Output image comparisons
+    for i in range(n_images):
+        original_image = test_samples[i].squeeze()
+        reconstructed_image = reconstructed[i].squeeze()
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(original_image + 0.5)
+        plt.title("Original")
+        plt.axis("off")
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(reconstructed_image + 0.5)
+        plt.title("Reconstructed")
+        plt.axis("off")
+
+        # plt.show()
+        plt.savefig(f"reconstructions_{i}.png")
+        plt.close()
+
+def normalise_255(samples):
+    return 255 * (samples - np.min(samples)) / np.ptp(samples)
+
+def calculate_ssim(original_images, reconstructed_images):
+    similarity = tf.image.ssim(normalise_255(original_images), normalise_255(reconstructed_images), max_val=255)
+    print("Structured similarity is:", similarity)
+
+x_train, x_test = load_oasis_data.get_data()
+
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+
+x_train_scaled = (x_train / 255.0) - 0.5
+x_test_scaled = (x_test / 255.0) - 0.5
+
+variance = np.var(x_train / 255.0)
+
+vqvae = train_vqvae(x_train_scaled, variance)
+compare_reconstructions(vqvae, x_test, 10)
+
+
 

recognition/45801150_Task6/src/load_oasis_data.py
@@ -16,20 +16,31 @@ def get_data():
 
     train = []
     test = []
+    max_load = 100
+    i = 0
     for root_name, dir_names, file_names in os.walk(dataset_prefix + train_suffix):
         file_names.sort()
         for file_name in file_names:
+            i += 1
+            if i > max_load * 0.8:
+                break
             img = img_to_array(load_img(root_name + "/" + file_name, color_mode="grayscale"))
             train.append(img)
-
+        print(f"\rLoaded {i} train images", end='')
+    print()
+    i = 0
     for root_name, dir_names, file_names in os.walk(dataset_prefix + test_suffix):
         file_names.sort()
         for file_name in file_names:
+            i += 1
+            if i > max_load * 0.2:
+                break
             img = img_to_array(load_img(root_name + "/" + file_name, color_mode="grayscale"))
             test.append(img)
+            print(f"\rLoaded {i} test images", end='')
+    print()
 
-    return np.array(train), np.array(test)
+    return np.array(train).squeeze(), np.array(test).squeeze()
 
-train, test = get_data()
 
 