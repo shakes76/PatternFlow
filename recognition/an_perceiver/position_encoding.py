08ed74e7b7f70e1cdbab55504062510a6fdc48e9
Anthony North <anthony.jl.north@gmail.com>
2021-10-27 16:22:27 +1000

    test position encoding, refactor for clarity

--- recognition/an_perceiver/position_encoding.py
+++ recognition/an_perceiver/position_encoding.py
@@ -5,16 +5,25 @@ import math
 def fourier_position_encode(index_shape: tuple[int, ...], num_bands: int):
     """Uniformly sampled Fourier frequency position encodings."""
 
+    num_pixels = tf.reduce_prod(index_shape)
     positions = get_spatial_positions(index_shape)
     bands = tf.stack([tf.linspace(1.0, dim / 2, num_bands) for dim in index_shape])
 
-    rads = math.pi * tf.reshape(
+    freq = tf.reshape(
         # each postion * each band
         positions[:, :, tf.newaxis] * bands,
-        [-1, tf.reduce_prod(bands.shape)],
+        # flatten to pixels x [len(index_shape) * num_bands]
+        [num_pixels, -1]
     )
 
-    encodings = tf.concat([tf.sin(rads), tf.cos(rads)], axis=-1)
+    encodings = tf.concat(
+        [
+            tf.sin(math.pi * freq),
+            tf.cos(math.pi * freq),
+        ],
+        axis=-1,
+    )
+
     return tf.concat([positions, encodings], axis=-1)
 
 
