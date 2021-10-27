ed6417d2ec6f5cb7b9063ef1d8a02a087a113a3c
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-27 11:58:21 +1000

    Started working on creating improved unet model in model.py

---
+++ recognition/ISIC-ImprovedUnet-44961374/model.py
@@ -0,0 +1,20 @@
+"""
+This script creates an Improved U-Net model.
+Specifications of the model can be found at: https://arxiv.org/abs/1802.10508v1
+@author: Mujibul Islam Dipto
+"""
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, Dropout, Input, concatenate, \
+    Add, UpSampling2D, Conv2DTranspose, LeakyReLU
+
+def context_module():
+    pass
+
+def upsampling_module():
+    pass
+
+def localization_module():
+    pass
+
+def create_model():
+    pass
