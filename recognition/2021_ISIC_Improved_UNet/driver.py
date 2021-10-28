64d4cd89e1b0dd1eefdedd9ef6357e4a0dd56077
default-jamc <default_jamc@outlook.com>
2021-10-28 23:51:56 +1000

    Added in accuracy and loss plots, and prediction plots. Changed input image size, did general code documentation. Changed dice coefficient loss function

--- recognition/2021_ISIC_Improved_UNet/driver.py
+++ recognition/2021_ISIC_Improved_UNet/driver.py
@@ -14,12 +14,16 @@ from model import *
 # image thresholding: https://www.tensorflow.org/api_docs/python/tf/where
 # optimizer: https://www.tensorflow.org/api_docs/python/tf/keras/optimizers/Adam
 # dice coefficient: https://towardsdatascience.com/metrics-to-evaluate-your-semantic-segmentation-model-6bcb99639aa2
+# dice coefficient 2: https://medium.com/@karan_jakhar/100-days-of-code-day-7-84e4918cb72c
 # input dataset to fit(): https://www.tensorflow.org/guide/data
 # plotting predictions: https://www.tensorflow.org/tutorials/load_data/images
 
-# Data loading variables
+# Data loading and processing variables
 ISIC_DATA = "./ISIC2018_Task1-2_Training_Input_x2/*.jpg" 
 ISIC_MASKS = "./ISIC2018_Task1_Training_GroundTruth_x2/*.png"
+
+IMAGE_HEIGHT = 192
+IMAGE_WIDTH = 256
 
 # Data managing variables
 BATCH_SIZE = 32
@@ -27,37 +31,59 @@ DATASET_SIZE = 2594
 
 # Network variables
 OPT_LEARNING_RATE = 5e-4
-EPOCHS = 1
+EPOCHS = 13
 
 
 def preprocessData(filenames):
+    """
+        Loads and preprocesses the images. The images must be: 
+            - decoded
+            - reshaped to [IMAGE_HEIGHT, IMAGE_WIDTH] (chosen size)
+            - normalised (pixels must be between 0 and 1)
+            
+        @param filenames: the names of all of the image files
+        
+        @return the newly processed images
+    """
     raw_data = tf.io.read_file(filenames)
     
     # Decode images
     raw_image = tf.io.decode_jpeg(raw_data, channels=3)
         
     # Resize the images
-    raw_image = tf.image.resize(raw_image, [256,256])
+    raw_image = tf.image.resize(raw_image, [IMAGE_HEIGHT, IMAGE_WIDTH])
     
     # Normalise
     raw_image = raw_image / 255.0
+    
     return raw_image
     
     
 def preprocessMasks(filenames):
+    """
+        Loads and preprocesses the masks. The masks must be: 
+            - decoded and reduced to a single colour channel
+            - reshaped to [IMAGE_HEIGHT, IMAGE_WIDTH] (chosen size)
+            - normalised and thresholded (pixels must be 0 or 1)
+            
+        @param filenames: the names of all of the mask image files
+        
+        @return the newly processed masks
+    """
     raw_data = tf.io.read_file(filenames)
     
     # Decode images
     raw_image = tf.io.decode_png(raw_data, channels=1)
         
     # Resize the images
-    raw_image = tf.image.resize(raw_image, [256,256])
+    raw_image = tf.image.resize(raw_image, [IMAGE_HEIGHT, IMAGE_WIDTH])
     
     # Normalise
     raw_image = raw_image / 255.0
     
     # Threshold image to 0-1
     raw_image = tf.where(raw_image > 0.5, 1.0, 0.0)
+    
     return raw_image
     
     
@@ -96,6 +122,13 @@ def loadData():
 
 
 def splitData(dataset):
+    """
+        Splits the dataset into the 70 / 15 / 15 split. 
+        
+        @param dataset: the entire dataset
+        
+        @return the training, testing and validation datasets, now split 70 / 15 / 15
+    """
     print("Function handling the splitting and batching of data.")
     
     # Define the sizes for a 70 / 15 / 15 split
@@ -117,7 +150,16 @@ def splitData(dataset):
     
     
 def performBatching(train, test, validation):
-    # Perform batching
+    """
+        Performs batching of the three data sets; training, testing and validation.
+        The size of the batches is defined in the variable BATCH_SIZE
+        
+        @param train: the training dataset
+        @param test: the testing dataset
+        @param validation: the validation dataset
+        
+        @return the original datasets passed in, now batched.
+    """
     training_set = train.batch(BATCH_SIZE)
     testing_set = test.batch(BATCH_SIZE)
     validation_set = validation.batch(BATCH_SIZE)
@@ -137,11 +179,17 @@ def diceCoefficient(y_true, y_pred):
         @param y_pred: the output predicted by the model
         @return the dice coefficient for the prediction, based on the true output.
     """
-    overlap = tf.keras.backend.sum(y_true * y_pred, axis=[1, 2, 3])
+    #overlap = tf.keras.backend.sum(y_true * y_pred, axis=[1, 2, 3])
     
-    totalPixels = tf.keras.backend.sum(y_true, axis=[1, 2, 3]) +  tf.keras.backend.sum(y_pred, axis=[1, 2, 3])
+    #totalPixels = tf.keras.backend.sum(y_true, axis=[1, 2, 3]) +  tf.keras.backend.sum(y_pred, axis=[1, 2, 3])
     
-    diceCoeff = tf.keras.backend.mean((2.0 * overlap + 1) / (totalPixels + 1), axis=0)
+    #diceCoeff = tf.keras.backend.mean((2.0 * overlap + 1) / (totalPixels + 1), axis=0)
+    
+    
+    y_true_f = tf.keras.backend.flatten(y_true)
+    y_pred_f = tf.keras.backend.flatten(y_pred)
+    intersection = tf.keras.backend.sum(y_true_f * y_pred_f)
+    diceCoeff = (2. * intersection + 1.) / (tf.keras.backend.sum(y_true_f) + tf.keras.backend.sum(y_pred_f) + 1.)
     
     return diceCoeff
     
@@ -161,8 +209,81 @@ def diceLoss(y_true, y_pred):
 def generatePredictions(test_data, model):
     """
         Generates and displays predictions of the model from the test set.
+        local_batch amount of figures are displayed, which are a single row with 3 images.
+        The images are:
+            1. The original image input to the network 
+            2. The ground truth mask for that input 
+            3. The resultant mask determined by the trained network.
+        
+        @param test_data: the unbatched test data for the network
+        @param model: the network 
     """  
+    local_batch = 20
+    titles = ['Input image', 'Ground Truth Mask','Resultant Mask']
+    test_data_batched = test_data.batch(local_batch)
+    test_image, test_mask = next(iter(test_data_batched))  
+    mask_prediction = model.predict(test_image)
+    
+    # Plot the original image, ground truth and result from the network.
+    for i in range(local_batch):
+        
+        plt.figure(figsize=(10,10))
+        
+        # Plot the test image
+        plt.subplot(1, 3, 1)
+        plt.imshow(test_image[i])
+        plt.title("Input Image")
+        plt.axis("off")
+        
+        # Plot the test mask
+        plt.subplot(1, 3, 2)
+        plt.imshow(test_mask[i])
+        plt.title("Ground Truth Mask")
+        plt.axis("off")
+        
+        # Plot the resultant mask
+        plt.subplot(1, 3, 3)
+        
+        # Display 0 or 1 for classes
+        prediction = tf.where(mask_prediction[i] > 0.5, 1.0, 0.0)
+        plt.imshow(prediction)
+        #print(np.unique(mask_prediction[i]))
+        plt.title("Resultant Mask")
+        plt.axis("off")
+        
+        plt.show()
+
     return 1
+
+
+def plotHistory(history):
+    """
+        Plots the value vs epoch graphs for the Dice Coefficient and Dice 
+        Coefficient loss throughout training and validation.
+        
+        @param history: the loss and coefficient history of the model 
+                        throughout training.
+    """
+    print(history.history.keys())
+    modelHistory = history.history
+    
+    # Loss plots
+    plt.plot(modelHistory['loss'])
+    plt.plot(modelHistory['val_loss'])
+    plt.title('Dice Coefficient Loss')
+    plt.ylabel('Loss (%)')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'validation'], loc='upper right')
+    plt.show()
+    
+    # Accuracy plots
+    plt.plot(modelHistory['diceCoefficient'])
+    plt.plot(modelHistory['val_diceCoefficient'])
+    plt.title('Dice Coefficient')
+    plt.ylabel('DSC')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'validation'], loc='upper left')
+    plt.show()
 
 def main():
     # Dependencies
@@ -183,17 +304,51 @@ def main():
     # Compile the model, model.compile()
     print("Compiling model...")
     adamOptimizer = tf.keras.optimizers.Adam(learning_rate=OPT_LEARNING_RATE)
-    model.compile(optimizer=adamOptimizer, loss=diceLoss, metrics=[diceCoefficient, 'accuracy'])
+    model.compile(optimizer=adamOptimizer, loss=diceLoss, metrics=[diceCoefficient])
     print("Model compilation complete.")
     
     # Train the model, model.fit()
     print("Training the model...")
-    history = model.fit(train_data_batched, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=validation_data_batched)
+    history = model.fit(train_data_batched, epochs=EPOCHS, validation_data=validation_data_batched)
     print("Model training complete.")
+    
+    plotHistory(history)
     
     # Evaluate performance on test, model.evaluate()
     print("Evaluating the model on the test set...")
-    model.evaluate(test_data_batched)
+    i = 0
+    lossV = []
+    coefficientV = []
+    under = 0
+    fine = 0
+    for test_image, test_mask in test_data.batch(1):
+        loss, coefficient = model.evaluate(test_image, test_mask)
+        lossV.append(loss)
+        coefficientV.append(coefficient)
+        
+        if (coefficient < 0.8):
+            under += 1
+        else:
+            fine += 1
+        
+        i += 1
+    
+    percentageFine = ((fine / i) * 100);
+    averageDC = sum(coefficientV) / len(coefficientV)
+    print(">>> Evaluating Test Set \n Test dataset size: " + str(i))
+    print("Amount fine: " + str(fine))
+    print("Amount under 0.8: " + str(under))
+    print("Average Dice Coefficient: " + str(averageDC))
+    print("---- " + str(percentageFine) + "% of Test Set has 0.8 Dice Coefficient or above ----")
+    
+    plt.hist(coefficientV)
+    plt.title("Dice Coefficients of Test Set for Total Epochs: " + str(EPOCHS))
+    plt.ylabel('Frequency')
+    plt.xlabel('Dice Coefficient')
+    plt.show()
+        
+ 
+    
     print("Model evaluation complete.")
     
     # Perform predictions, model.predict()
