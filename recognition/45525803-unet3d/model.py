0ea268cf3b9b1c4b6364d521d5680c8b4bba07f0
Joshua Knowles <s4552580@student.uq.edu.au>
2021-10-28 11:35:10 +1000

    Add 3D UNet model and dice coefficient

---
+++ recognition/45525803-unet3d/model.py
@@ -0,0 +1,70 @@
+from tensorflow.keras.layers import Input, Conv3D, MaxPool3D, UpSampling3D, concatenate
+from tensorflow.keras import backend as K
+
+def unet3d_model(input_size=(256,256,128,1), n_classes=6):
+    """
+    3D U-Net model, implemented exactly as described in https://arxiv.org/abs/1606.06650
+    """
+
+    inputs = Input(input_size)
+    
+    conv1 = Conv3D(32, (3,3,3), activation='relu', padding='same')(inputs)
+    conv1 = Conv3D(64, (3,3,3), activation='relu', padding='same')(conv1)
+    
+    pool1 = MaxPool3D((2,2,2))(conv1)
+    
+    conv2 = Conv3D(64, (3,3,3), activation='relu', padding='same')(pool1)
+    conv2 = Conv3D(128, (3,3,3), activation='relu', padding='same')(conv2)
+    
+    pool2 = MaxPool3D((2,2,2))(conv2)
+    
+    conv3 = Conv3D(128, (3,3,3), activation='relu', padding='same')(pool2)
+    conv3 = Conv3D(256, (3,3,3), activation='relu', padding='same')(conv3)
+    
+    pool3 = MaxPool3D((2,2,2))(conv3)
+    
+    conv4 = Conv3D(256, (3,3,3), activation='relu', padding='same')(pool3)
+    conv4 = Conv3D(256, (3,3,3), activation='relu', padding='same')(conv4)
+    
+    upconv4 = UpSampling3D((2,2,2))(conv4)
+    upconv4 = Conv3D(256, (2,2,2), activation='relu', padding='same')(upconv4)
+    
+    concat_3_5 = concatenate([conv3, upconv4], axis=4)
+    conv5 = Conv3D(256, (3,3,3), activation='relu', padding='same')(concat_3_5)
+    conv5 = Conv3D(256, (3,3,3), activation='relu', padding='same')(conv5)
+    
+    upconv5 = UpSampling3D((2,2,2))(conv5)
+    upconv5 = Conv3D(256, (2,2,2), activation='relu', padding='same')(upconv5)
+    
+    concat_2_6 = concatenate([conv2, upconv5], axis=4)
+    conv6 = Conv3D(256, (3,3,3), activation='relu', padding='same')(concat_2_6)
+    conv6 = Conv3D(256, (3,3,3), activation='relu', padding='same')(conv6)
+    
+    upconv6 = UpSampling3D((2,2,2))(conv6)
+    upconv6 = Conv3D(256, (2,2,2), activation='relu', padding='same')(upconv6)
+    
+    concat_1_7 = concatenate([conv1, upconv6], axis=4)
+    conv7 = Conv3D(256, (3,3,3), activation='relu', padding='same')(concat_1_7)
+    conv7 = Conv3D(256, (3,3,3), activation='relu', padding='same')(conv7)
+    
+    output_seg = Conv3D(n_classes, (1,1,1), activation='softmax')(conv7)
+    
+    unet3d = tf.keras.Model(inputs=inputs, outputs=output_seg)
+    
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0005)
+    unet3d.compile (optimizer=opt, loss='CategoricalCrossentropy' , metrics=[dice_coefficient])
+    
+    return unet3d
+
+def dice_coefficient(y_true, y_pred):
+    """
+    Implementation of the Sørensen–Dice coefficient.
+    """
+    
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    
+    intersection = K.sum(y_true_f * y_pred_f)
+    total_area = K.sum(y_true_f) + K.sum(y_pred_f)
+    
+    return 2 * intersection / total_area
