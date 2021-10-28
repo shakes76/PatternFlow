fcfd6a60cfb41ba7159d0523637b79004ffde378
Alanlxc <70080479+Alanlxc@users.noreply.github.com>
2021-10-28 20:53:14 +0800

    Create the test script, which will use the code named-GCN model work

---
+++ recognition/GCN_S4654830/GCN-test-script.py
@@ -0,0 +1,9 @@
+from Data_prepare import *
+from GCN_Layer import *
+from  GCN_model_define import *
+from GCN_test_train import *
+# this is a script to used in final running
+# training model process / will print accuracy about loss and Validation and training
+train()
+
+
