d997f2d40c76edbf9a228f80d160ea5d2b1bc570
Tompnyx <tompnyx@outlook.com>
2021-10-28 23:11:10 +1000

    The model will now exceed a Dice Similarity Coefficient of 0.8! Removed Kernel Initializer and activation from each Conv2D to fix static loss and improve performance. Modified the dice loss function to give the correct result and added soft dice as a metric for the model. Modularised certain functions in driver.py to implement conditional operation. Added the ability to save and reuse a model that had previously been trained. Reduced condition where an early stopping would occur (4 to 3)

--- recognition/ISIC_Segmentation_UNet_s4587209/driver.py
+++ recognition/ISIC_Segmentation_UNet_s4587209/driver.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.metrics import classification_report
@@ -11,10 +11,16 @@ from model import improved_unet
 # Constants
 ISIC2018_data_link = "https://cloudstor.aarnet.edu.au/sender/download.php?token=b66a9288-2f00-4330-82ea-9b8711d27643&files_ids=14200406"
 download_directory = os.getcwd() + '\ISIC2018_Task1-2_Training_Data.zip'
+
 num_display_examples = 3
+print_original_images = False
+print_processed_images = False
+use_saved_model = True
+save_model = False
+
+shuffle = False
 training_split = 0.8
 validation_split = 0.1
-shuffle = False
 shuffle_size = 50
 
 img_height = img_width = 180
@@ -76,20 +82,49 @@ def show_original_images(rows, index=0):
     plt.show()
 
 
-def soft_dice_loss(y_true, y_pred):
+def create_ds():
+    # Calculates the size of each test, train, and validation subset
+    files_ds_size = len(list(files_ds))
+    train_ds_size = int(training_split * files_ds_size)
+    val_ds_size = int(validation_split * files_ds_size)
+    test_ds_size = files_ds_size - train_ds_size - val_ds_size
+    # Prints the size of all the subsets
+    print("Training size: %d" % train_ds_size)
+    print("Validation size: %d" % val_ds_size)
+    print("Testing size: %d" % test_ds_size)
+    # Splits the dataset into test, validate, and train subsets
+    train = files_ds.take(train_ds_size)
+    val = files_ds.skip(train_ds_size).take(val_ds_size)
+    test = files_ds.skip(train_ds_size).skip(val_ds_size)
+    return train, val, test
+
+
+def soft_dice(y_true, y_pred, epsilon=1.0):
     """
     Code adapted from:
     "An overview of semantic image segmentation.", Jeremy Jordan, 2021.
     [Online]. Available: https://www.jeremyjordan.me/semantic-segmentation/. [Accessed: 26-Oct-2021].
     """
-    y_true = tf.convert_to_tensor(y_true)
-    y_pred = tf.convert_to_tensor(y_pred)
-
     axes = tuple(range(1, len(y_pred.shape) - 1))
     numerator = 2. * tf.math.reduce_sum(y_pred * y_true, axes)
     denominator = tf.math.reduce_sum(tf.math.square(y_pred) + tf.math.square(y_true), axes)
+    return tf.reduce_mean((numerator + epsilon) / (denominator + epsilon))
 
-    return 1 - tf.reduce_mean(numerator / denominator)
+
+def soft_dice_loss(y_true, y_pred):
+    return 1 - soft_dice(y_true, y_pred)
+
+
+def initialise_model():
+    # Creates the improved UNet model
+    unet_model = improved_unet(img_height, img_width, n_channels)
+    # Sets the training parameters for the model
+    unet_model.compile(optimizer="adam", loss=[soft_dice_loss], metrics=[soft_dice])
+    # Prints a summary of the model compiled
+    unet_model.summary()
+    # Plots a summary of the model's architecture
+    tf.keras.utils.plot_model(unet_model, show_shapes=True)
+    return unet_model
 
 
 # Downloads files if not present
@@ -101,44 +136,46 @@ mask_file_list = list(glob.glob('datasets/ISIC2018_Task1_Training_GroundTruth_x2
 # Show original images and masks
 print("Size of Training Pictures: %d\nSize of Segmented Pictures: %d\n"
       % (len(list(image_file_list)), len(list(mask_file_list))))
-# Prints the original photos
-show_original_images(num_display_examples)
+# Prints a subset of the original images and masks if specified
+if print_original_images:
+    show_original_images(num_display_examples)
 # Creates a dataset that contains all the files
 data_dir = os.getcwd() + '/datasets'
 files_ds = tf.data.Dataset.from_tensor_slices((image_file_list, mask_file_list))
 files_ds = files_ds.map(lambda x, y: (process_images(x, False), process_images(y, True))).batch(1)
 
-# Prints the normalised images and masks
-display(num_display_examples, files_ds)
+# Prints a subset of the processed images and masks if specified
+if print_processed_images:
+    display(num_display_examples, files_ds)
 
 # Shuffles the dataset if specified
 if shuffle:
     files_ds = files_ds.shuffle(shuffle_size)
 
-# Calculates the size of each test, train, and validation subset
-files_ds_size = len(list(files_ds))
-train_ds_size = int(training_split * files_ds_size)
-val_ds_size = int(validation_split * files_ds_size)
-test_ds_size = files_ds_size - train_ds_size - val_ds_size
-# Prints the size of all the subsets
-print("Training size: %d" % train_ds_size)
-print("Validation size: %d" % val_ds_size)
-print("Testing size: %d" % test_ds_size)
-# Splits the dataset into test, validate, and train subsets
-train_ds = files_ds.take(train_ds_size)
-val_ds = files_ds.skip(train_ds_size).take(val_ds_size)
-test_ds = files_ds.skip(train_ds_size).skip(val_ds_size)
+# Creates datasets of Training, Validation, and Testing data
+train_ds, val_ds, test_ds = create_ds()
 
-# Creates the improved UNet model
-model = improved_unet(img_height, img_width, n_channels)
-# Sets the training parameters for the model
-model.compile(optimizer="adam", loss=soft_dice_loss, metrics=["accuracy"])
-# Prints a summary of the model compiled
-model.summary()
-# Plots a summary of the model's architecture
-tf.keras.utils.plot_model(model, show_shapes=True)
+# Uses a saved model if specified
+if use_saved_model:
+    # Retrieve saved model
+    model = tf.keras.models.load_model('Saved_Model', custom_objects={'soft_dice': soft_dice,
+                                                                      'soft_dice_loss': soft_dice_loss})
+    # Prints a summary of the model compiled
+    model.summary()
+else:
+    # Initialise the model
+    model = initialise_model()
+    # Creates a condition where training will stop when there is no progress on val_loss over 3 epochs
+    callback = EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
+    # Trains the model
+    history = model.fit(train_ds, batch_size=batch_size, epochs=epochs, validation_data=val_ds, shuffle=shuffle,
+                        callbacks=callback)
+    # Save the model if specified
+    if save_model:
+        model.save('Saved_Model')
 
-# Creates a condition where training will stop when there is no progress on val_loss in 3 epochs
-callback = EarlyStopping(monitor='val_loss', patience=3)
-# Trains the model
-history = model.fit(train_ds, batch_size=batch_size, epochs=epochs, validation_data=val_ds, callbacks=callback)
+# Evaluates the model
+loss, acc = model.evaluate(test_ds, verbose=2)
+# Uses the test dataset to test the model on the predicted masks
+predictions = model.predict(test_ds)
+print(predictions.shape)
--- recognition/ISIC_Segmentation_UNet_s4587209/model.py
+++ recognition/ISIC_Segmentation_UNet_s4587209/model.py
@@ -5,58 +5,57 @@ from tensorflow.keras.layers import concatenate, Conv2D, Dropout, Input, UpSampl
 def improved_unet(height, width, channels):
     fil = 16
     kern = (3, 3)
-    act = 'relu'
-    kern_init = 'he_normal'
+    
     pad = 'same'
     drop = 0.3
     # Building the model
     inputs = Input((height, width, channels))
 
     # 3x3x3 convolution - 16 filters
-    conv1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(inputs)
+    conv1 = Conv2D(fil, kern, padding=pad)(inputs)
     # Context module
-    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv1)
+    context1 = Conv2D(fil, kern, padding=pad)(conv1)
     context1 = Dropout(drop)(context1)
-    context1 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(context1)
+    context1 = Conv2D(fil, kern, padding=pad)(context1)
     # Element-wise sum
     out1 = conv1 + context1
 
     # 3x3x3 stride 2 convolution - 32 filters
-    conv2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out1)
+    conv2 = Conv2D(fil * 2, kern, padding=pad)(out1)
     # Context module
-    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv2)
+    context2 = Conv2D(fil * 2, kern, padding=pad)(conv2)
     context2 = Dropout(drop)(context2)
-    context2 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
+    context2 = Conv2D(fil * 2, kern, padding=pad)(
         context2)
     # Element-wise sum
     out2 = conv2 + context2
 
     # 3x3x3 stride 2 convolution - 64 filters
-    conv3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out2)
+    conv3 = Conv2D(fil * 4, kern, padding=pad)(out2)
     # Context module
-    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv3)
+    context3 = Conv2D(fil * 4, kern, padding=pad)(conv3)
     context3 = Dropout(drop)(context3)
-    context3 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
+    context3 = Conv2D(fil * 4, kern, padding=pad)(
         context3)
     # Element-wise sum
     out3 = conv3 + context3
 
     # 3x3x3 stride 2 convolution - 128 filters
-    conv4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out3)
+    conv4 = Conv2D(fil * 8, kern, padding=pad)(out3)
     # Context module
-    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv4)
+    context4 = Conv2D(fil * 8, kern, padding=pad)(conv4)
     context4 = Dropout(drop)(context4)
-    context4 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
+    context4 = Conv2D(fil * 8, kern, padding=pad)(
         context4)
     # Element-wise sum
     out4 = conv4 + context4
 
     # 3x3x3 stride 2 convolution - 256 filters
-    conv5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(out4)
+    conv5 = Conv2D(fil * 16, kern, padding=pad)(out4)
     # Context module
-    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(conv5)
+    context5 = Conv2D(fil * 16, kern, padding=pad)(conv5)
     context5 = Dropout(drop)(context5)
-    context5 = Conv2D(fil * 16, kern, activation=act, kernel_initializer=kern_init, padding=pad)(
+    context5 = Conv2D(fil * 16, kern, padding=pad)(
         context5)
     # Element-wise sum
     out5 = conv5 + context5
@@ -69,8 +68,8 @@ def improved_unet(height, width, channels):
     # Concatenation
     concat6 = concatenate([upsamp5, out4])
     # Localisation - 128 filters
-    local6 = Conv2D(fil * 8, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat6)
-    local6 = Conv2D(fil * 8, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local6)
+    local6 = Conv2D(fil * 8, kern, padding=pad)(concat6)
+    local6 = Conv2D(fil * 8, (1, 1), padding=pad)(local6)
     # Up-sampling - 64 filters
     upsamp6 = UpSampling2D(size=(2, 2))(local6)
     upsamp6 = Conv2D(fil * 4, (2, 2), padding=pad, strides=(2, 2))(upsamp6)
@@ -78,8 +77,8 @@ def improved_unet(height, width, channels):
     # Concatenation
     concat7 = concatenate([upsamp6, out3])
     # Localisation - 64 filters
-    local7 = Conv2D(fil * 4, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat7)
-    local7 = Conv2D(fil * 4, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local7)
+    local7 = Conv2D(fil * 4, kern, padding=pad)(concat7)
+    local7 = Conv2D(fil * 4, (1, 1), padding=pad)(local7)
     # Segmentation
     segment7 = local7
     # Up-sampling - 32 filters
@@ -89,8 +88,8 @@ def improved_unet(height, width, channels):
     # Concatenation
     concat8 = concatenate([upsamp7, out2])
     # Localisation - 32 filters
-    local8 = Conv2D(fil * 2, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat8)
-    local8 = Conv2D(fil * 2, (1, 1), activation=act, kernel_initializer=kern_init, padding=pad)(local8)
+    local8 = Conv2D(fil * 2, kern, padding=pad)(concat8)
+    local8 = Conv2D(fil * 2, (1, 1), padding=pad)(local8)
     # Segmentation
     segment8 = local8
     # Up-sampling - 16 filters
@@ -100,7 +99,7 @@ def improved_unet(height, width, channels):
     # Concatenation
     concat9 = concatenate([upsamp8, out1])
     # 3x3x3 convolution - 32 filters
-    conv9 = Conv2D(fil, kern, activation=act, kernel_initializer=kern_init, padding=pad)(concat9)
+    conv9 = Conv2D(fil, kern, padding=pad)(concat9)
     # Segmentation
     segment9 = conv9
 
@@ -113,5 +112,5 @@ def improved_unet(height, width, channels):
     segment9 = segment9 + segment8
 
     # Softmax
-    outputs = Conv2D(1, (1, 1), activation='softmax')(segment9)
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(segment9)
     return tf.keras.Model(inputs=[inputs], outputs=[outputs])
