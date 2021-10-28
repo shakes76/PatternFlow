41d13da4525e84f445776853226cebc3a6f3d623
Your Name <413277180@qq.com>
2021-10-28 22:03:09 +1000

    First of all, I have to load and normalize the data-set.

---
+++ recognition/s4584245-Yue/load_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as fun
+import torch.nn.init as init
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
