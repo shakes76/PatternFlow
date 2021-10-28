39b127d49c23edc71c0ca9d0f8b3f5cf8c4f0c76
Nifu <frzmit@gmail.com>
2021-10-28 12:37:39 +1000

    Move variables to constants

--- recognition/Yiyun_ISIC/AdvUNet/main.py
+++ recognition/Yiyun_ISIC/AdvUNet/main.py
@@ -3,24 +3,36 @@
 from model import AdvUNet
 from data import get_filenames, split_data, create_datasets
 
+# The root path of the datasets.
+# Please put the following two folders:
+# -  ISIC2018_Task1_Training_GroundTruth and
+# -  ISIC2018_Task1-2_Training_Input
+# under this root path:
+ISIC_DIR = "./datasets/ISIC2018"
+
+# height, width
+IMAGE_HEIGHT = 192
+IMAGE_WIDTH = 256
+NUM_CLASSES = 1
+
+
 def main():
     # load datasets
-    features, labels = get_filenames(isic_dir="./datasets/ISIC2018")
+    features, labels = get_filenames(isic_dir=ISIC_DIR)
     train_features, train_labels, val_features, val_labels, test_features, test_labels = split_data(
         features, labels, validation_split=0.2, test_split=0.2)
 
     # create datasets
     train_dataset, val_dataset, test_dataset = create_datasets(
         train_features, train_labels, val_features, val_labels,
-        test_features, test_labels, [192, 256], 1)
+        test_features, test_labels, [IMAGE_HEIGHT, IMAGE_WIDTH], NUM_CLASSES)
 
     for image, label in train_dataset.take(1):
         print("Dataset shapes:", image.shape, label.shape)
         break
 
-    model = AdvUNet()
-    print(model.model.summary())
-    
+    # model = AdvUNet()
+
 
 if __name__ == "__main__":
     main()
