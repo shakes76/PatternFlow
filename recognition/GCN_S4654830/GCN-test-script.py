f7460ce5fed49d0f6aa48dd0b50028df9f80cdf8
Alanlxc <70080479+Alanlxc@users.noreply.github.com>
2021-10-28 20:54:52 +0800

    Added the TSNE visualization code to shwo the classify results

--- recognition/GCN_S4654830/GCN-test-script.py
+++ recognition/GCN_S4654830/GCN-test-script.py
@@ -7,3 +7,14 @@ from GCN_test_train import *
 train()
 
 
+# result visualization as a plot use TSNE method.
+
+from sklearn.manifold import TSNE
+# the result is the classfy results, which can be present as 4 classes
+test_accuracy, test_data, test_labels = test(tensor_test_mask)
+tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+low_dim_embs = tsne.fit_transform(test_data)
+plt.title('TSNE result')
+plt.scatter(low_dim_embs[:,0], low_dim_embs[:,1], marker='o', c=test_labels)
+plt.savefig("tsne.png")
+plt.show()
