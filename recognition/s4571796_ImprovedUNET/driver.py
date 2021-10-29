6e6a45c312d9c97a6dc5f3735e04e3c0843cf537
Joshua <joshuasooyx@gmail.com>
2021-10-29 10:05:58 +1000

    Added model and driver script

---
+++ recognition/s4571796_ImprovedUNET/driver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Joshua Yu Xuan Soo"
+__studentID__ = "s4571796"
+__email__ = "s4571796@uqconnect.edu.au"
+
+"""
+Driver Script for Image Segmentation on ISIC Melanoma Dataset by utilizing
+an Improved UNET Model as defined in the imports.
+
+References: 
+    https://arxiv.org/pdf/1802.10508v1.pdf
+
+"""
+
+def main():
+    """Driver Main Function"""
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
---
+++ recognition/s4571796_ImprovedUNET/model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Joshua Yu Xuan Soo"
+__studentID__ = "s4571796"
+__email__ = "s4571796@uqconnect.edu.au"
+
+"""
+Improved UNET Model for Image Segmentation on ISIC Melanoma Dataset.
+
+References: 
+    https://arxiv.org/pdf/1802.10508v1.pdf
+
+"""
+
