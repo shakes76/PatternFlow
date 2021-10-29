73440086de831cc8513c43c69e191061fdb6525b
Joshua <joshuasooyx@gmail.com>
2021-10-29 14:45:10 +1000

    Added Context, Localization, Segmentation Module functions in Model

--- recognition/s4571796_ImprovedUNET/model.py
+++ recognition/s4571796_ImprovedUNET/model.py
@@ -13,3 +13,65 @@ References:
 
 """
 
+# Imports
+from keras.models import Model
+from keras.layers import Input, Conv2D, Conv2DTranspose, Dropout, Concatenate, LeakyReLU, UpSampling2D, BatchNormalization
+import tensorflow as tf
+
+# Functions
+
+def context_module(input, num_filters):
+    """Performs the convolution in the context module
+
+    Arguments:
+    input: The image to perform convolution on
+    num_filters: The number of filters used to perform convolution
+    """
+    # Store the initial values of the image
+    temp = input
+
+    # Used 3*3 filter size
+    # Activation Function is LeakyReLU
+    x = Conv2D(num_filters, 3, padding="same", activation=LeakyReLU(alpha=0.1))(input)
+
+    # BatchNormalization to reduce Overfitting
+    x = BatchNormalization()(x)
+
+    # Dropout to reduce Overfitting, value = 0.3
+    x = Dropout(0.1)(x)
+
+    # UNET Architecture performs Convolution2D two times for a single layer
+    x = Conv2D(num_filters, 3, padding="same", activation=LeakyReLU(alpha=0.1))(x)
+
+    # Add the initial values to the convoluted result element wise
+    y = tf.math.add(temp, x)
+
+    return y
+
+def localization_module(input, num_filters):
+    """Performs the convolution in the localization module
+
+    Arguments:
+    input: The image to perform convolution on
+    num_filters: The number of filters used to perform convolution
+    """
+    # Used 3*3 filter size
+    # Activation Function is LeakyReLU
+    x = Conv2D(num_filters, 3, padding="same", activation=LeakyReLU(alpha=0.1))(input)
+
+    # UNET Architecture performs Convolution2D two times for a single layer
+    x = Conv2D(num_filters, 1, padding="same", activation=LeakyReLU(alpha=0.1))(x)
+
+    return x
+
+def segmentation_module(input, output_filters):
+    """Performs the convolution in the localization module
+
+    Arguments:
+    input: The image to perform convolution on
+    output_filters: The number of classes of the output image
+    """
+    # Used 1*1 filter size
+    x = Conv2D(output_filters, 1, padding="same", activation=LeakyReLU(alpha=0.1))(input)
+
+    return x
