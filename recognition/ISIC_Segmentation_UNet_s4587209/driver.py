f7d5dc71fa00b104145bb4139ae1388423b685d7
Tompnyx <tompnyx@outlook.com>
2021-10-30 00:09:44 +1000

    Found issue with data preprocessing where the mask images were being normalised like they had colour channels. Implemented Instance Normalisation (Currently in Batch form) and Leaky ReLU for UNet model. Fixed issue where batch was set to 1 accidentally earlier in driver.py. Implemented predicted mask plotting compared to the true mask. Added the ability to save the generated plots and a related conditional statement. Changed names of dice methods to closer resemble the objective of the task.

--- recognition/ISIC_Segmentation_UNet_s4587209/driver.py
+++ recognition/ISIC_Segmentation_UNet_s4587209/driver.py
@@ -1,10 +1,10 @@
 import glob
 import os
 import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import SGD
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from sklearn.metrics import classification_report
 
 from model import improved_unet
 
@@ -13,18 +13,21 @@ ISIC2018_data_link = "https://cloudstor.aarnet.edu.au/sender/download.php?token=
 download_directory = os.getcwd() + '\ISIC2018_Task1-2_Training_Data.zip'
 
 num_display_examples = 3
-print_original_images = False
-print_processed_images = False
-use_saved_model = True
-save_model = False
+print_original_images = True
+print_processed_images = True
+print_predicted_images = True
+save_photos = True
 
-shuffle = False
+use_saved_model = False
+save_model = True
+
+shuffle = True
 training_split = 0.8
 validation_split = 0.1
 shuffle_size = 50
 
-img_height = img_width = 180
-batch_size = 32
+img_height = img_width = 150
+batch_size = 4
 n_channels = 3
 epochs = 10
 
@@ -32,7 +35,7 @@ epochs = 10
 def process_images(file_path, is_mask):
     # Decodes the image at the given file location
     if is_mask:
-        image = tf.image.decode_png(tf.io.read_file(file_path), channels=1)
+        image = tf.image.decode_png(tf.io.read_file(file_path), channels=0)
     else:
         image = tf.image.decode_jpeg(tf.io.read_file(file_path), channels=3)
     # Converts the image to float32
@@ -41,45 +44,61 @@ def process_images(file_path, is_mask):
     image_resized = tf.image.resize(image_converted, size=(img_height, img_width))
     # Normalises input image
     if is_mask:
-        image_final = tf.cast(image_resized, tf.float32) / 255.0
-    else:
         image_final = image_resized
+    else:
+        image_final = tf.cast(image_resized, tf.float32) / 255.0
     return image_final
 
 
 # Plots images to subplot given position
 def plot_images(pic_array, rows, index, original, cols=2):
-    title = ['Original Input', 'True Mask']
+    title = ['Original Input', 'True Mask', 'Predicted Mask']
     for i in range(len(pic_array)):
         plt.subplot(rows, cols, index + 1)
-        if index < 2:
+        if index < cols:
             plt.title(title[index])
         if original:
             plt.imshow(mpimg.imread(pic_array[i]))
         else:
-            plt.imshow(tf.keras.preprocessing.image.array_to_img(pic_array[i]))
+            plt.imshow(tf.keras.utils.array_to_img(pic_array[i]))
         plt.axis('off')
         index += 1
 
 
 # Displays a database ds given how many rows
-def display(rows, ds, index=0):
-    plt.figure(figsize=(5, 5))
+def show_processed_images(rows, ds, index=0):
+    fig = plt.figure(figsize=(5, 5))
     for images, masks in ds.take(rows):
-        image, mask = images[0], masks[0]
+        image = images
+        mask = masks
         plot_images([image, mask], rows, index, False)
         index += 2
     plt.show()
+    if save_photos:
+        fig.savefig('Results/ProcessedExample.png', dpi=fig.dpi)
 
 
 # Displays original images as read into the program
 def show_original_images(rows, index=0):
-    plt.figure(figsize=(5, 5))
+    fig = plt.figure(figsize=(5, 5))
     for i in range(rows):
         image, mask = image_file_list[i], mask_file_list[i]
         plot_images([image, mask], rows, index, True)
         index += 2
     plt.show()
+    if save_photos:
+        fig.savefig('Results/OriginalExample.png', dpi=fig.dpi)
+
+
+def show_predicted_images(rows, unet_model, index=0):
+    fig = plt.figure(figsize=(5, 5))
+    for image, mask in test_ds.batch(batch_size).take(num_display_examples):
+        pred_mask = tf.cast(unet_model.predict(image), tf.float32)
+        plot_images([image[0], mask[0], pred_mask[0]], rows, index, False, cols=3)
+        index += 3
+    plt.show()
+    if save_photos:
+        fig.savefig('Results/PredictedExample.png', dpi=fig.dpi)
 
 
 def create_ds():
@@ -99,7 +118,7 @@ def create_ds():
     return train, val, test
 
 
-def soft_dice(y_true, y_pred, epsilon=1.0):
+def dice_sim_coef(y_true, y_pred, epsilon=1.0):
     """
     Code adapted from:
     "An overview of semantic image segmentation.", Jeremy Jordan, 2021.
@@ -111,20 +130,58 @@ def soft_dice(y_true, y_pred, epsilon=1.0):
     return tf.reduce_mean((numerator + epsilon) / (denominator + epsilon))
 
 
-def soft_dice_loss(y_true, y_pred):
-    return 1 - soft_dice(y_true, y_pred)
+def dice_sim_coef_loss(y_true, y_pred):
+    return 1 - dice_sim_coef(y_true, y_pred)
 
 
 def initialise_model():
     # Creates the improved UNet model
     unet_model = improved_unet(img_height, img_width, n_channels)
     # Sets the training parameters for the model
-    unet_model.compile(optimizer="adam", loss=[soft_dice_loss], metrics=[soft_dice])
+    unet_model.compile(optimizer=SGD(lr=0.01), loss=[dice_sim_coef_loss],
+                       metrics=[dice_sim_coef])
     # Prints a summary of the model compiled
     unet_model.summary()
     # Plots a summary of the model's architecture
     tf.keras.utils.plot_model(unet_model, show_shapes=True)
     return unet_model
+
+
+def plot_performance_loss_model(model_history):
+    """
+    Code adapted from:
+    "Image segmentation", TensorFlow, 2021.
+    [Online]. Available: https://www.tensorflow.org/tutorials/images/segmentation. [Accessed: 28-Oct-2021].
+    """
+    fig = plt.figure()
+    val_loss = model_history.history['val_loss']
+    train_loss = model_history.history['loss']
+    plt.plot(model_history.epoch, train_loss, 'r', label='Training loss')
+    plt.plot(model_history.epoch, val_loss, 'bo', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss Value')
+    plt.ylim([0, 1])
+    plt.legend()
+    plt.show()
+    if save_photos:
+        fig.savefig('Results/ModelLossPerformance.png', dpi=fig.dpi)
+
+
+def plot_performance_model(model_history):
+    fig = plt.figure()
+    dice = model_history.history['dice_sim_coef']
+    val_dice = model_history.history['val_dice_sim_coef']
+    plt.plot(model_history.epoch, dice, 'r', label='Training')
+    plt.plot(model_history.epoch, val_dice, 'b', label='Validation')
+    plt.title('Dice Similarity Coefficient over Epochs ')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy (Dice Similarity Coefficient')
+    plt.ylim([0, 1])
+    plt.legend(loc='lower right')
+    plt.show()
+    if save_photos:
+        fig.savefig('Results/ModelPerformance.png', dpi=fig.dpi)
 
 
 # Downloads files if not present
@@ -142,11 +199,12 @@ if print_original_images:
 # Creates a dataset that contains all the files
 data_dir = os.getcwd() + '/datasets'
 files_ds = tf.data.Dataset.from_tensor_slices((image_file_list, mask_file_list))
-files_ds = files_ds.map(lambda x, y: (process_images(x, False), process_images(y, True))).batch(1)
+files_ds = files_ds.map(lambda x, y: (process_images(x, False), process_images(y, True)),
+                        num_parallel_calls=tf.data.AUTOTUNE)
 
 # Prints a subset of the processed images and masks if specified
 if print_processed_images:
-    display(num_display_examples, files_ds)
+    show_processed_images(num_display_examples, files_ds)
 
 # Shuffles the dataset if specified
 if shuffle:
@@ -158,24 +216,30 @@ train_ds, val_ds, test_ds = create_ds()
 # Uses a saved model if specified
 if use_saved_model:
     # Retrieve saved model
-    model = tf.keras.models.load_model('Saved_Model', custom_objects={'soft_dice': soft_dice,
-                                                                      'soft_dice_loss': soft_dice_loss})
+    model = tf.keras.models.load_model('Saved_Model',
+                                       custom_objects={'dice_sim_coef': dice_sim_coef,
+                                                       'dice_sim_coef_loss': dice_sim_coef_loss})
     # Prints a summary of the model compiled
     model.summary()
 else:
     # Initialise the model
     model = initialise_model()
-    # Creates a condition where training will stop when there is no progress on val_loss over 3 epochs
-    callback = EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
+    # Creates a condition where training will stop when there is no progress on val_loss over 6 epochs
+    callback = EarlyStopping(monitor='val_dice_sim_coef', patience=5, restore_best_weights=True)
     # Trains the model
-    history = model.fit(train_ds, batch_size=batch_size, epochs=epochs, validation_data=val_ds, shuffle=shuffle,
-                        callbacks=callback)
+    history = model.fit(train_ds.batch(batch_size), batch_size=batch_size, epochs=epochs,
+                        validation_data=val_ds.batch(batch_size), shuffle=shuffle, callbacks=callback)
+    # Plots the performance of the model (Loss vs Dice Loss)
+    plot_performance_model(history)
+    plot_performance_loss_model(history)
     # Save the model if specified
     if save_model:
         model.save('Saved_Model')
 
+if print_predicted_images:
+    show_predicted_images(num_display_examples, model)
+
 # Evaluates the model
-loss, acc = model.evaluate(test_ds, verbose=2)
-# Uses the test dataset to test the model on the predicted masks
-predictions = model.predict(test_ds)
-print(predictions.shape)
+loss, acc = model.evaluate(test_ds.batch(batch_size), verbose=2)
+# # Uses the test dataset to test the model on the predicted masks
+# predictions = model.predict(test_ds.batch(batch_size)
--- recognition/ISIC_Segmentation_UNet_s4587209/model.py
+++ recognition/ISIC_Segmentation_UNet_s4587209/model.py
@@ -1,107 +1,175 @@
 import tensorflow as tf
-from tensorflow.keras.layers import concatenate, Conv2D, Dropout, Input, UpSampling2D
+from tensorflow.keras.layers import concatenate, Conv2D, BatchNormalization, Dropout, Input, LeakyReLU, UpSampling2D
 
 
 def improved_unet(height, width, channels):
     fil = 16
     kern = (3, 3)
-    
     pad = 'same'
     drop = 0.3
+    alp = 0.01
     # Building the model
     inputs = Input((height, width, channels))
 
     # 3x3x3 convolution - 16 filters
     conv1 = Conv2D(fil, kern, padding=pad)(inputs)
+    # Instance Normalisation
+    bat1 = BatchNormalization()(conv1)
+    # Leaky ReLU
+    relu1 = LeakyReLU(alpha=alp)(bat1)
     # Context module
-    context1 = Conv2D(fil, kern, padding=pad)(conv1)
+    context1 = Conv2D(fil, kern, padding=pad)(relu1)
     context1 = Dropout(drop)(context1)
     context1 = Conv2D(fil, kern, padding=pad)(context1)
+    # Instance Normalisation
+    bat1 = BatchNormalization()(context1)
+    # Leaky ReLU
+    relu1 = LeakyReLU(alpha=alp)(bat1)
     # Element-wise sum
-    out1 = conv1 + context1
+    out1 = conv1 + relu1
 
     # 3x3x3 stride 2 convolution - 32 filters
     conv2 = Conv2D(fil * 2, kern, padding=pad)(out1)
+    # Instance Normalisation
+    bat2 = BatchNormalization()(conv2)
+    # Leaky ReLU
+    relu2 = LeakyReLU(alpha=alp)(bat2)
     # Context module
-    context2 = Conv2D(fil * 2, kern, padding=pad)(conv2)
+    context2 = Conv2D(fil * 2, kern, padding=pad)(relu2)
     context2 = Dropout(drop)(context2)
-    context2 = Conv2D(fil * 2, kern, padding=pad)(
-        context2)
+    context2 = Conv2D(fil * 2, kern, padding=pad)(context2)
+    # Instance Normalisation
+    bat2 = BatchNormalization()(context2)
+    # Leaky ReLU
+    relu2 = LeakyReLU(alpha=alp)(bat2)
     # Element-wise sum
-    out2 = conv2 + context2
+    out2 = conv2 + relu2
 
     # 3x3x3 stride 2 convolution - 64 filters
     conv3 = Conv2D(fil * 4, kern, padding=pad)(out2)
+    # Instance Normalisation
+    bat3 = BatchNormalization()(conv3)
+    # Leaky ReLU
+    relu3 = LeakyReLU(alpha=alp)(bat3)
     # Context module
-    context3 = Conv2D(fil * 4, kern, padding=pad)(conv3)
+    context3 = Conv2D(fil * 4, kern, padding=pad)(relu3)
     context3 = Dropout(drop)(context3)
-    context3 = Conv2D(fil * 4, kern, padding=pad)(
-        context3)
+    context3 = Conv2D(fil * 4, kern, padding=pad)(context3)
+    # Instance Normalisation
+    bat3 = BatchNormalization()(context3)
+    # Leaky ReLU
+    relu3 = LeakyReLU(alpha=alp)(bat3)
     # Element-wise sum
-    out3 = conv3 + context3
+    out3 = conv3 + relu3
 
     # 3x3x3 stride 2 convolution - 128 filters
     conv4 = Conv2D(fil * 8, kern, padding=pad)(out3)
+    # Instance Normalisation
+    bat4 = BatchNormalization()(conv4)
+    # Leaky ReLU
+    relu4 = LeakyReLU(alpha=alp)(bat4)
     # Context module
-    context4 = Conv2D(fil * 8, kern, padding=pad)(conv4)
+    context4 = Conv2D(fil * 8, kern, padding=pad)(relu4)
     context4 = Dropout(drop)(context4)
-    context4 = Conv2D(fil * 8, kern, padding=pad)(
-        context4)
+    context4 = Conv2D(fil * 8, kern, padding=pad)(context4)
+    # Instance Normalisation
+    bat4 = BatchNormalization()(context4)
+    # Leaky ReLU
+    relu4 = LeakyReLU(alpha=alp)(bat4)
     # Element-wise sum
-    out4 = conv4 + context4
+    out4 = conv4 + relu4
 
     # 3x3x3 stride 2 convolution - 256 filters
     conv5 = Conv2D(fil * 16, kern, padding=pad)(out4)
+    # Instance Normalisation
+    bat5 = BatchNormalization()(conv5)
+    # Leaky ReLU
+    relu5 = LeakyReLU(alpha=alp)(bat5)
     # Context module
-    context5 = Conv2D(fil * 16, kern, padding=pad)(conv5)
+    context5 = Conv2D(fil * 16, kern, padding=pad)(relu5)
     context5 = Dropout(drop)(context5)
-    context5 = Conv2D(fil * 16, kern, padding=pad)(
-        context5)
+    context5 = Conv2D(fil * 16, kern, padding=pad)(context5)
+    # Instance Normalisation
+    bat5 = BatchNormalization()(context5)
+    # Leaky ReLU
+    relu5 = LeakyReLU(alpha=alp)(bat5)
     # Element-wise sum
-    out5 = conv5 + context5
+    out5 = conv5 + relu5
     # Up-sampling - 128 filters
     upsamp5 = UpSampling2D(size=(2, 2))(out5)
     upsamp5 = Conv2D(fil * 8, kern, padding=pad, strides=(2, 2))(upsamp5)
 
     # Expansive path
 
+    # Instance Normalisation
+    bat6 = BatchNormalization()(upsamp5)
+    # Leaky ReLU
+    relu6 = LeakyReLU(alpha=alp)(bat6)
     # Concatenation
-    concat6 = concatenate([upsamp5, out4])
+    concat6 = concatenate([relu6, out4])
     # Localisation - 128 filters
     local6 = Conv2D(fil * 8, kern, padding=pad)(concat6)
     local6 = Conv2D(fil * 8, (1, 1), padding=pad)(local6)
+    # Instance Normalisation
+    bat6 = BatchNormalization()(local6)
+    # Leaky ReLU
+    relu6 = LeakyReLU(alpha=alp)(bat6)
     # Up-sampling - 64 filters
-    upsamp6 = UpSampling2D(size=(2, 2))(local6)
+    upsamp6 = UpSampling2D(size=(2, 2))(relu6)
     upsamp6 = Conv2D(fil * 4, (2, 2), padding=pad, strides=(2, 2))(upsamp6)
 
+    # Instance Normalisation
+    bat7 = BatchNormalization()(upsamp6)
+    # Leaky ReLU
+    relu7 = LeakyReLU(alpha=alp)(bat7)
     # Concatenation
-    concat7 = concatenate([upsamp6, out3])
+    concat7 = concatenate([relu7, out3])
     # Localisation - 64 filters
     local7 = Conv2D(fil * 4, kern, padding=pad)(concat7)
     local7 = Conv2D(fil * 4, (1, 1), padding=pad)(local7)
+    # Instance Normalisation
+    bat7 = BatchNormalization()(local7)
+    # Leaky ReLU
+    relu7 = LeakyReLU(alpha=alp)(bat7)
     # Segmentation
-    segment7 = local7
+    segment7 = relu7
     # Up-sampling - 32 filters
-    upsamp7 = UpSampling2D(size=(2, 2))(local7)
+    upsamp7 = UpSampling2D(size=(2, 2))(relu7)
     upsamp7 = Conv2D(fil * 2, (2, 2), padding=pad, strides=(2, 2))(upsamp7)
 
+    # Instance Normalisation
+    bat8 = BatchNormalization()(upsamp7)
+    # Leaky ReLU
+    relu8 = LeakyReLU(alpha=alp)(bat8)
     # Concatenation
-    concat8 = concatenate([upsamp7, out2])
+    concat8 = concatenate([relu8, out2])
     # Localisation - 32 filters
     local8 = Conv2D(fil * 2, kern, padding=pad)(concat8)
     local8 = Conv2D(fil * 2, (1, 1), padding=pad)(local8)
+    # Instance Normalisation
+    bat8 = BatchNormalization()(local8)
+    # Leaky ReLU
+    relu8 = LeakyReLU(alpha=alp)(bat8)
     # Segmentation
-    segment8 = local8
+    segment8 = relu8
     # Up-sampling - 16 filters
-    upsamp8 = UpSampling2D(size=(2, 2))(local8)
+    upsamp8 = UpSampling2D(size=(2, 2))(relu8)
     upsamp8 = Conv2D(fil, (2, 2), padding=pad, strides=(2, 2))(upsamp8)
 
+    # Instance Normalisation
+    bat9 = BatchNormalization()(upsamp8)
+    # Leaky ReLU
+    relu9 = LeakyReLU(alpha=alp)(bat9)
     # Concatenation
-    concat9 = concatenate([upsamp8, out1])
+    concat9 = concatenate([relu9, out1])
     # 3x3x3 convolution - 32 filters
     conv9 = Conv2D(fil, kern, padding=pad)(concat9)
+    # Instance Normalisation
+    bat9 = BatchNormalization()(conv9)
+    # Leaky ReLU
+    relu9 = LeakyReLU(alpha=alp)(bat9)
     # Segmentation
-    segment9 = conv9
+    segment9 = relu9
 
     # Upscale Segmented Layers and apply
     segment7 = UpSampling2D(size=(2, 2))(segment7)
