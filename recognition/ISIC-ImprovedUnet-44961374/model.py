27ece3717648638338fe08b911984068c6a13e3e
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-29 21:31:21 +1000

    added strides as a parameter for create_conv2d function, completed upto level 2 of contracting path, renamed some variables

--- recognition/ISIC-ImprovedUnet-44961374/model.py
+++ recognition/ISIC-ImprovedUnet-44961374/model.py
@@ -6,6 +6,7 @@ Specifications of the model can be found at: https://arxiv.org/abs/1802.10508v1
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization, \
     Dropout, Input, concatenate, Add, UpSampling2D, Conv2DTranspose, LeakyReLU
+from tensorflow.python.eager.context import context
 from tensorflow_addons.layers import InstanceNormalization
 from tensorflow.keras.layers import  Conv2D
 
@@ -17,10 +18,12 @@ LEAKY_RELU_ALPHA = 1e-2 # slope of LeakyReLU
 IMAGE_ROWS = 256 # image x dimension
 IMAGE_COLS = 256 # image y dimension
 IMAGE_CHANNELS = 1 # greyscale
-KERNEL_SIZE = (3,3) # size of kernel
-INIT_NO_FILTERS = 16
+KERNEL_SIZE = (3, 3) # size of kernel
+INIT_NO_FILTERS = 16 # initial number of filters
+INIT_STRIDES = (1, 1) # initial strides
 
-def create_conv2d(input_layer, filters, kernel_size):
+
+def create_conv2d(input_layer, filters, kernel_size, strides):
     """Creates a Conv2D layer based on the Improved UNet architecture. 
     Please look at the README.md for more details.
 
@@ -32,7 +35,7 @@ def create_conv2d(input_layer, filters, kernel_size):
     Returns:
         keras.layer.Conv2D: the conv2d layer that has been created
     """
-    conv2d_layer = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', activation=LeakyReLU(alpha=LEAKY_RELU_ALPHA))(input_layer)
+    conv2d_layer = Conv2D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same', activation=LeakyReLU(alpha=LEAKY_RELU_ALPHA))(input_layer)
     return conv2d_layer
 
 
@@ -50,12 +53,12 @@ def context_module(input_layer, filters):
     Returns:
         [keras.layer]: final layer of this module
     """
-    instance_norm_layer1 = InstanceNormalization()(input_layer)
-    conv_layer1 = create_conv2d(instance_norm_layer1, filters, KERNEL_SIZE)
-    dropout = Dropout(P_DROP)(conv_layer1)
-    instance_norm_layer2 = InstanceNormalization()(dropout)
-    conv_layer2 = create_conv2d(instance_norm_layer2, filters, KERNEL_SIZE)
-    return conv_layer2
+    instance_norm_layer_1 = InstanceNormalization()(input_layer)
+    conv_layer_1 = create_conv2d(instance_norm_layer_1, filters, KERNEL_SIZE, INIT_STRIDES)
+    dropout = Dropout(P_DROP)(conv_layer_1)
+    instance_norm_layer_2 = InstanceNormalization()(dropout)
+    conv_layer_2 = create_conv2d(instance_norm_layer_2, filters, KERNEL_SIZE, INIT_STRIDES)
+    return conv_layer_2
 
 def upsampling_module(input_layer, filters):
     """Creates an upsampling module based on the Improved Unet architecture.
@@ -69,7 +72,7 @@ def upsampling_module(input_layer, filters):
         keras.layer: final layer of this module
     """
     upsampling_layer = UpSampling2D(KERNEL_SIZE)(input_layer)
-    conv2d = create_conv2d(upsampling_layer, filters, KERNEL_SIZE)
+    conv2d = create_conv2d(upsampling_layer, filters, KERNEL_SIZE, INIT_STRIDES)
     return conv2d
 
 def localization_module(input_layer, filters):
@@ -83,17 +86,23 @@ def localization_module(input_layer, filters):
     Returns:
         keras.layer: final layer of this module
     """
-    conv_layer1 = create_conv2d(input_layer, filters, KERNEL_SIZE)
-    conv_layer2 = create_conv2d(conv_layer1, filters, (1,1))
-    return conv_layer2
+    conv_layer_1 = create_conv2d(input_layer, filters, KERNEL_SIZE, INIT_STRIDES)
+    conv_layer_2 = create_conv2d(conv_layer_1, filters, (1,1))
+    return conv_layer_2
 
 def create_model():
     ########## INPUT ##########
     input_layer = Input(shape=(IMAGE_ROWS, IMAGE_COLS, IMAGE_CHANNELS))
 
     ########## CONTRACTING PATH ##########
-    conv_layer1 = create_conv2d(input_layer, INIT_NO_FILTERS, KERNEL_SIZE)
-    context1 = context_module(conv_layer1, INIT_NO_FILTERS)
+    # level 1
+    conv_layer_1 = create_conv2d(input_layer, INIT_NO_FILTERS, KERNEL_SIZE, INIT_STRIDES)
+    context_1 = context_module(conv_layer_1, INIT_NO_FILTERS)
+    add_layer_1= Add()([conv_layer_1, context_1])
+    # level 2
+    conv_layer_2 = create_conv2d(add_layer_1, INIT_NO_FILTERS * 2, KERNEL_SIZE, (2,2))
+    context_2 = context_module(conv_layer_2, INIT_NO_FILTERS * 2)
+    add_layer_2 = Add()([conv_layer_2, context_2]) 
 
 create_model()
 
