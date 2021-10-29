1825621f1a1319879363bffeb543edf5cc8b251b
Your Name <you@example.com>
2021-10-29 17:57:30 +1000

    Modify plot function to plot more figures.

--- recognition/s4615653_improved_Unet/plots.py
+++ recognition/s4615653_improved_Unet/plots.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-def plots(data, model):
+def plots(data, model,num):
     # function to plot images of inputs, ground-truth and predictions, and compared them.
-    inputs_batch, truth_batch = next(iter(data.batch(3)))
+    inputs_batch, truth_batch = next(iter(data.batch(num)))
 
     prediction = model.predict(inputs_batch)
 
     plt.figure(figsize =(15, 15))
-    for i in range(3):
+    for i in range(num):
         # inputs images
-        plt.subplot(3, 3, 3*i+1)
+        plt.subplot(num, 3, 3*i+1)
         plt.imshow(inputs_batch[i])
         plt.axis('off')
 
         # ground-truth images
-        plt.subplot(3, 3, 3*i+2)
+        plt.subplot(num, 3, 3*i+2)
         plt.imshow(tf.argmax(truth_batch[i],axis=-1),camp="gray")
         plt.axis('off')
 
         # predict images
-        plt.subplot(3, 3, 3*i+3)
+        plt.subplot(num, 3, 3*i+3)
         plt.imshow(tf.argmax(prediction[i],axis=-1),camp="gray")
         plt.axis('off')
 
