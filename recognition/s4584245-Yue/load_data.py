3ccfa958e5500514c297df4877fbf7f06123789d
Your Name <413277180@qq.com>
2021-10-29 11:24:29 +1000

    Finish the load data function. I also print the shape of feature,edges and target.

--- recognition/s4584245-Yue/load_data.py
+++ recognition/s4584245-Yue/load_data.py
@@ -1,12 +1,27 @@
-#Import the packages.
-import numpy as np
+#Import the necessay packages.
 import numpy as np
 import scipy.sparse as sp
 import torch
 
-import torch.nn as nn
-import torch.nn.functional as fun
-import torch.nn.init as init
-import torch.optim as optim
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelBinarizer
+fb = np.load('facebook.npz')
+#The details of the data-set
+target = fb['target']
+print(target.shape)
+edges = fb['edges']
+print(edges.shape)
+feature = fb['features']
+print(feature.shape)
+
+def load_data():
+    """
+    load the data
+    """
+    fb = np.load("facebook.npz")
+    n = len(target)
+    x = np.zeros((n, n), dtype=np.float32)
+    for i in edges:
+        x[i[0]][i[1]] = 1
+    return sp.csr_matrix(x), sp.csr_matrix(feature, dtype=np.float32), target
+
+load_data()
+
