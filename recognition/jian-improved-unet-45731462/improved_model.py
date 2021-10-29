9c6c3d864b702867c4731419992a2faa1a8b3969
Lee Jian Yang <leejyang00@gmail.com>
2021-10-29 11:54:00 +1000

    model up and running, training now begins

--- recognition/jian-improved-unet-45731462/improved_model.py
+++ recognition/jian-improved-unet-45731462/improved_model.py
@@ -20,10 +20,10 @@ def context_module(input, filters):
     Returns:
         [type]: [description]
     """
-    conv_one = Conv2D(filters, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(input)
+    conv_one = Conv2D(filters, kernel_size=3, padding="same", activation=LeakyReLU(alpha=0.1))(input)
     conv_one_batch = BatchNormalization()(conv_one)
     drop_one = Dropout(0.3)(conv_one_batch)
-    conv_two = Conv2D(filters, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(drop_one)
+    conv_two = Conv2D(filters, kernel_size=3, padding="same", activation=LeakyReLU(alpha=0.1))(drop_one)
     return conv_two
 
 def localisation_module(input, filters):
@@ -54,37 +54,42 @@ def model(height, width, input_channel, desired_channnel):
     # layer 1
     conv1 = Conv2D(16, kernel_size=3, padding="same", activation=LeakyReLU(alpha=0.1))(input)
     context1 = context_module(conv1, 16)
-    layer1 = Add()([conv1, context1])
+    layer1 = tf.math.add(conv1, context1)
+
 
     # layer 2
     conv2 = Conv2D(32, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(layer1)
     context2 = context_module(conv2, 32)
     layer2 = Add()([conv2, context2])
+    layer2 = tf.math.add(conv2, context2)
+
 
     # layer 3
     conv3 = Conv2D(64, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(layer2)
     context3 = context_module(conv3, 64)
-    layer3 = Add()([conv3, context3])
+    layer3 = tf.math.add(conv3, context3)
+
 
     # layer 4
     conv4 = Conv2D(128, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(layer3)
     context4 = context_module(conv4, 128)
-    layer4 = Add()([conv3, context3])
+    layer4 = tf.math.add(conv4, context4)
 
     ## BRIDGE
     conv_bridge = Conv2D(filters=256, kernel_size=3, strides=(2, 2), padding="same", activation=LeakyReLU(alpha=0.1))(layer4)
     context_bridge = context_module(conv_bridge, 256)
-    bridge = Add()([conv_bridge, context_bridge])
+    bridge = tf.math.add(conv_bridge, context_bridge)
+
     upsample_bridge = Conv2DTranspose(filters=128, kernel_size=3, strides=(2, 2), padding="same")(bridge)
 
     ## DECODING
     # layer 4
-    concat_1 = Concatenate()([upsample_bridge, context4])
+    concat_1 = Concatenate()([upsample_bridge, layer4])
     localized_1 = localisation_module(concat_1, 128)
     upsample_1 = Conv2DTranspose(filters=64, kernel_size=(3, 3), strides=2, padding="same")(localized_1)
 
     # layer 3
-    concat_2 = Concatenate()([upsample_1, context3])
+    concat_2 = Concatenate()([upsample_1, layer3])
     localized_2 = localisation_module(concat_2, 64)
     upsample_2 = Conv2DTranspose(filters=32, kernel_size=(3, 3), strides=2, padding="same")(localized_2)
 
@@ -99,7 +104,7 @@ def model(height, width, input_channel, desired_channnel):
 
     # element-wise sum with upscale_1 and upscale localized 3 
     segment_2 = Conv2D(64, kernel_size=(1, 1), padding="same")(localized_3)
-    sum_1 = Add()[(upscale_1, segment_2)]
+    sum_1 = tf.math.add(upscale_1, segment_2)
     upscale_2 = UpSampling2D((2, 2))(sum_1)
 
     # layer 1
@@ -108,9 +113,10 @@ def model(height, width, input_channel, desired_channnel):
 
     # segment and element-wise sum
     segment_3 = Conv2D(64, kernel_size=(1, 1), padding="same")(conv_last)
-    sum2 = Add()[(upscale_2, segment_3)]
+    sum_2 = tf.math.add(upscale_2, segment_3)
 
-    output = Conv2D(desired_channnel, (1, 1), activation="sigmoid")(sum2)
+
+    output = Conv2D(desired_channnel, (1, 1), activation="softmax")(sum_2)
 
     improved_model = Model(input, output)
 
--- recognition/jian-improved-unet-45731462/preprocessing.py
+++ recognition/jian-improved-unet-45731462/preprocessing.py
@@ -1,18 +1,21 @@
 """
-Model Architecture of the Improved Unet
+[INSERT]
 
 @author Jian Yang Lee
 @email jianyang.lee@uqconnect.edu.au
 """
-
 
 import numpy as np
 import cv2
 import os
 import numpy as np
 from sklearn.model_selection import train_test_split
+from improved_model import model
+import tensorflow as tf
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
-def to_numpy(path):
+
+def to_numpy(path, image_type):
     """
     [INSERT]
     """
@@ -20,40 +23,82 @@ def to_numpy(path):
 
     for filename in os.listdir(path):
         if filename != ".DS_Store":
-            read_img = cv2.imread(os.path.join(path, filename), 0)
-            new_img = cv2.resize(read_img, (128, 128))
+            if image_type == "bw":
+                read_img = cv2.imread(os.path.join(path, filename), 0)
+            elif image_type == "rgb":
+                read_img = cv2.imread(os.path.join(path, filename), cv2.IMREAD_COLOR)
+            new_img = cv2.resize(read_img, (128, 96))
             numpy_images.append(new_img)
-    
+
     return np.array(numpy_images)
 
+def encode_bw(input):
+    """[summary]
+
+    Args:
+        input ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    dim, height, width = input.shape
+
+    # converts to 1D
+    input = np.ravel(input)
+
+    # ensures the input image is either values of 0 or 1
+    input[(input >= 0) & (input < 128)] = 0
+    input[(input >= 128) & (input < 256)] = 1
+
+    return input.reshape(dim, height, width)
 
 
-def preprocess_data(train_ratio=0.7, valid_ratio=0.15, test_ratio=0.15):
+def main():
 
-    inputs = to_numpy("jian-improved-unet-45731462/Input_x2")
-    labels = to_numpy("jian-improved-unet-45731462/GroundTruth_x2")
+    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-    x_train, x_test, y_train, y_test = train_test_split(inputs, labels, test_size=1-train_ratio)
-    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + valid_ratio))
+    # read images from file directory, and ensure variables with 4 dimensions
+    inputs = to_numpy("jian-improved-unet-45731462/Input_x2", "rgb")
+    labels = to_numpy("jian-improved-unet-45731462/GroundTruth_x2", "bw")
 
-
-    # maintain aspect ratio of image when .shape
-
-
-    # print(x_train.shape)
-    # print(x_val.shape)
-    # print(x_test.shape)
-
+    # converts label mask to either 0 or 1 value, expand to 4 dimensions
+    labels = encode_bw(labels)
+    labels = np.expand_dims(labels, axis=3)
+    labels = to_categorical(labels, num_classes=2)
 
     # print(inputs.shape)
     # print(labels.shape)
 
-    print(labels[0])
+    # train test split
+    train = 0.7
+    valid= 0.15
+    test = 0.15
 
-def main():
-    preprocess_data()
+    x_train, x_test, y_train, y_test = train_test_split(inputs, labels, test_size=1-train)
+    x_valid, x_test, y_valid, y_test = train_test_split(x_test, y_test, test_size=test/(test + valid))
+
+    height = 96
+    width = 128
+    input_channel = 3
+    desired_channel = 2
+
+    ready_model = model(height, width, input_channel, desired_channel)
+    ready_model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy'])
+
+    # model summary
+    print(ready_model.summary())
 
 
+    # # train model
+    history = ready_model.fit(x_train, y_train, 
+               batch_size=2, verbose=1, 
+               epochs=15, 
+               validation_data=(x_valid, y_valid), 
+               shuffle=False)
+
+    # # save model
+    ready_model.save("Improved_Jian_Epoch15_Batch2.hdf5")
 
 
 if __name__ == "__main__":
