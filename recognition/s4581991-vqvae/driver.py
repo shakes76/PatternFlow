e417cd0ce211e43499ab5a1fdf9606800db04e1d
Richal Verma <richalverma00@gmail.com>
2021-10-29 12:38:52 +1000

    Add function stubs for MNIST VQ VAE

---
+++ recognition/s4581991-vqvae/driver.py
@@ -0,0 +1,26 @@
+
+# The model will be optimised using MNIST before moving on to the
+
+def load_and_preprocess_mnist_data():
+    '''
+    Loads and preprocesses MNIST data
+    '''
+    pass
+
+def run_minist():
+    '''
+    Train and test the VQ VAE model on MNIST data
+    '''
+    pass
+
+def train():
+    '''
+    Train the VQ VAE model and return relavent stats
+    '''
+    pass
+
+def test():
+    '''
+    Test the VQ VAE model and return relavent stats
+    '''
+    pass
