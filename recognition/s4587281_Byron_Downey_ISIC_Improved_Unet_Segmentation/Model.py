1bfd4948b80db0e094c2d3d4605e1ddb33c57e26
ByronDowney <byronelliottdowney@gmail.com>
2021-10-30 23:18:16 +0000

    Fixed bug with the splitting of the dataset. Changed approach to resizing/padding again, now resizing everything to (324,324) because computer was running out of memory when images were too large e.g. (3354, 2230)

--- recognition/s4587281_Byron_Downey_ISIC_Improved_Unet_Segmentation/Model.py
+++ recognition/s4587281_Byron_Downey_ISIC_Improved_Unet_Segmentation/Model.py
@@ -7,7 +7,8 @@ from tensorflow.keras.layers import Input, ZeroPadding2D, Conv2D, Dropout, Leaky
 from tensorflow.keras import Model
 from tensorflow.math import reduce_sum
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.image import resize_with_pad
+from tensorflow.image import resize
+from tensorflow.data.Dataset import zip
 
 num_epochs = argv[1]
 batch_size = argv[2]
@@ -15,12 +16,10 @@ train_split = argv[3
 val_split = argv[4]
 test_split = argv[5]
 dataset_size = argv[6]
-image_height = argv[7]
-image_width = argv[8]
-x_data_location = argv[9]
-y_data_location = argv[10]
+x_data_location = argv[7]
+y_data_location = argv[8]
 if len(argv) != 8 or not math.isclose(float(train_split) + float(validation_split) + float(test_split), 1):
-  print("Usage: Model.py [num_epochs] [batch_size] [train_split] [validation_split] [test_split] [dataset_size] [largest_image_height] [largest_image_width] [x_data_location] [y_data_location].\nPlease ensure train, validation and test split add up to 1 (e.g. 0.7, 0.15, 0.15)\nlargest_image_height and largest_image_width should be the largest height and width values of all the input images, respectively. This is to ensure all images are padded to the a valid size. Alternatively, you can just input very large values but this may cause problems")
+  print("Usage: Model.py [num_epochs] [batch_size] [train_split] [validation_split] [test_split] [dataset_size]  [x_data_location] [y_data_location].\nPlease ensure train, validation and test split add up to 1 (e.g. 0.7, 0.15, 0.15)")
 
 #Load images into Tensorflow Datasets
 x_dataset = tf.keras.utils.image_dataset_from_directory(x_data_location, labels=None)
@@ -30,17 +29,30 @@ y_dataset = tf.keras.utils.image_dataset_from_directory(y_data_location, labels=
 x_train = x_dataset.take(math.floor(train_split * dataset_size))
 y_train = y_dataset.take(math.floor(train_split * dataset_size))
 
-#contains validation and test data. Is used simply to extract the validation and test parts.
+#contains validation and test data
 val_and_test = x_dataset.skip(math.floor(train_split * dataset_size))
 val_and_test = y_dataset.skip(math.floor(train_split * dataset_size))
 
 #contains validation data
-x_val = x_dataset.take(math.floor(val_split * dataset_size))
-y_val = y_dataset.take(math.floor(val_split * dataset_size))
+x_val = val_and_test.take(math.floor(val_split * dataset_size))
+y_val = val_and_test.take(math.floor(val_split * dataset_size))
 
 #contains test data
-x_test = x_dataset.skip(math.floor(val_split * dataset_size))
-y_test = y_dataset.skip(math.floor(val_split * dataset_size))
+x_test = val_and_test.skip(math.floor(val_split * dataset_size))
+y_test = val_and_test.skip(math.floor(val_split * dataset_size))
+
+def resize_for_Unet(image):
+  resized_image = resize(image, (324, 324))
+  return resized_image
+
+x_train = x_train.map(resize_for_Unet)
+y_train = y_train.map(resize_for_Unet)
+
+x_val = x_val.map(resize_for_Unet)
+y_val = y_val.map(resize_for_Unet)
+
+x_test = x_test.map(resize_for_Unet)
+y_test = y_test.map(resize_for_Unet)
 
 #normalise all images
 def normalise(image):
@@ -56,39 +68,12 @@ y_val = y_val.map(normalise)
 x_test = x_test.map(normalise)
 y_test = y_test.map(normalise)
 
-##pad images to be usable by Unet (both dimensions must be divisble by 81)
-
-def pad_for_Unet(image):
-  padded_image = resize_with_pad(image, image_height + height_padding, image_width + width_padding)
-  return padded_image
-
-height_padding = 0
-width_padding = 0
-
-if image_height%81 != 0:
-  height_padding =  81 - (image_height%81)
-
-if image_width%81 != 0:
-  width_padding =  81 - (image_width%81)
-
-x_train = x_train.map(pad_for_Unet)
-y_train = y_train.map(pad_for_Unet)
-
-x_val = x_val.map(pad_for_Unet)
-y_val = y_val.map(pad_for_Unet)
-
-x_test = x_test.map(pad_for_Unet)
-y_test = y_test.map(pad_for_Unet)
+train = zip((x_train, y_train))
+val = zip((x_val, y_val))
+test = zip((x_test, y_test))
 
 #Defining Model Structure with Keras
 base_channels = 16
-dropout_rate = 0.3
-leaky_relu_slope = 0.01
-kernel_size = (3,3)
-upsampling_kernel_size = (3,3)
-
-#Defining Model Structure with Keras
-base_channels = 4
 dropout_rate = 0.3
 leaky_relu_slope = 0.01
 kernel_size = (3,3)
@@ -188,7 +173,6 @@ def dice_coef_loss(true, predicted):
     denominator = (reduce_sum(true_flat) + reduce_sum(predicted_flat) + 1.)
     return 1. - numerator / denominator
     
-#define dice coefficent loss function
 callback = EarlyStopping(monitor='val_loss', patience = 2)
 unet.compile(optimizer='adam', loss=dice_coef_loss, metrics=['accuracy'])
 
