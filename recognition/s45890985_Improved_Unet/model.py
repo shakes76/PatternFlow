6c9efe35d9c9e02a01badf58251dc63622e105cb
Andrew <s4589098@student.uq.edu.au>
2021-10-30 18:08:52 +1000

    placeholder methods created for various elements of the improved unet

--- recognition/s45890985_Improved_Unet/model.py
+++ recognition/s45890985_Improved_Unet/model.py
@@ -1,18 +1,28 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-
-def conv_module(input_layer, conv_depth):
-    return conv
-def segmentation_layer(input_layer):
-    return seg
-def localization_module():
-    return loc
-def encoder_block():
-    return en
-def decoder_block():
-    return de
+#method for created the improved unet model
 def improved_unet():
     return model
+
+#methods for different modules of the unet
+def conv_relu_block(input_layer, conv_depth, conv_size = (3,3)):
+    return conv
+def context_module(input_layer, conv_depth):
+    return conv
+def localization_module():
+    return loc
+def upsampling_module():
+    return up
+def segmentation_layer():
+    return seg
+
+#method for blocks in the encoder path
+def encoder_block():
+    return en
+#method for blocks in the decoder path
+def decoder_block():
+    return de
+
 
 
 
