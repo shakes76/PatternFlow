9e8463fa4d15c90447dcfb32803e946b58b874a0
AndrewLuong6 <51429490+AndrewLuong6@users.noreply.github.com>
2021-10-31 01:05:00 +1000

    Added comments to driver.py
    Added docstring comments to driver as well as moving general function calls into a main function.

--- recognition/45820188-UNET/driver.py
+++ recognition/45820188-UNET/driver.py
@@ -1,14 +1,33 @@
 from model import build_model
+from keras import backend as K
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-batch_size = 16
-depth = 16
-epochs = 5
-n = 192
-m = 256
+def dice_coefficient(y_true, y_pred, smooth=1):
+    """
+    Dice Coefficient required model compiling metrics
+
+    Source: https://medium.com/@karan_jakhar/100-days-of-code-day-7-84e4918cb72c
+    """
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f) + K.sum(y_pred_f)
+    return (2. * intersection + smooth) / (union + smooth)
+
+def dice_coefficient_loss(y_true, y_pred):
+    """
+    Calculates the dice coefficient loss. 
+    """
+    return 1 - dice_coefficient(y_true, y_pred)
 
 def process_images(path, segmentation):
+    """
+    Uses Keras function to convert a directory of images 
+    into a Keras Dataset
+
+    Shuffles the images with a seed of my student ID
+    """
     return tf.keras.preprocessing.image_dataset_from_directory(
         directory = path, 
         labels=None,
@@ -16,63 +35,72 @@ def process_images(path, segmentation):
         batch_size = batch_size,
         validation_split = 0.1,
         subset=segmentation,
-        image_size = (n,m),
+        image_size = (n, m),
         color_mode = 'grayscale',
         shuffle = True,
         seed = 45820188
     )
 
-model = build_model(input_shape=(n,m,1), depth=depth)
+def plot_prediction(model, X_test, y_test):
+    """
+    Takes the model and predicts the output from the given image set
 
-# Full Dataset
-X_train_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1-2_Training_Input_x2", "training")
-y_train_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1_Training_GroundTruth_x2", "training")
+    Plots the images with Original vs Prediction vs Expected
+    """
+    prediction = model.predict(X_test)
+    print(prediction.shape)
+    print(prediction[0].shape)
+    plt.figure(figsize=(10, 10))
+    n = 4
+    for i in range(n):
+        plt.subplot(n, 3, i*3+1)
+        plt.imshow(X_test[i])
+        plt.axis('off')
+        plt.title("Original", size=11)
 
-X_test_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1-2_Training_Input_x2", "validation")
-y_test_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1_Training_GroundTruth_x2", "validation")
+        plt.subplot(n, 3, i*3+2)
+        plt.imshow(prediction[i])
+        plt.axis('off')
+        plt.title("Prediction", size=11)
+
+        plt.subplot(n, 3, i*3+3)
+        plt.imshow(y_test[i])
+        plt.axis('off')
+        plt.title("Expected", size=11)
+    plt.show()
+
+if __name__ == "__main__":
+    batch_size = 16
+    depth = 16
+    epochs = 1
+    n = 192
+    m = 256
+
+    # Load the Improved UNET Model
+    model = build_model(input_shape=(n, m, 1), depth=depth)
+
+    # Full Dataset
+    X_train_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1-2_Training_Input_x2", "training")
+    y_train_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1_Training_GroundTruth_x2", "training")
+
+    X_test_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1-2_Training_Input_x2", "validation")
+    y_test_ds = process_images("C:\ISIC Dataset\Full Set\ISIC2018_Task1_Training_GroundTruth_x2", "validation")
 
 
-# Smaller subset of data
-# X_train_ds = process_images("C:\ISIC Dataset\Smaller\Train", "training")
-# y_train_ds = process_images("C:\ISIC Dataset\Smaller\Seg", "training")
+    # Smaller subset of data
+    # X_train_ds = process_images("C:\ISIC Dataset\Smaller\Train", "training")
+    # y_train_ds = process_images("C:\ISIC Dataset\Smaller\Seg", "training")
 
-# X_test_ds = process_images("C:\ISIC Dataset\Smaller\Train", "validation")
-# y_test_ds = process_images("C:\ISIC Dataset\Smaller\Seg", "validation")
+    # X_test_ds = process_images("C:\ISIC Dataset\Smaller\Train", "validation")
+    # y_test_ds = process_images("C:\ISIC Dataset\Smaller\Seg", "validation")
 
+    # Converts the tf dataset into array of images that can be used by the model
+    X_train = tf.concat([x for x in X_train_ds], axis=0)
+    y_train = tf.concat([x for x in y_train_ds], axis=0)
+    X_test = tf.concat([x for x in X_test_ds], axis=0)
+    y_test = tf.concat([x for x in y_test_ds], axis=0)
 
-X_train = tf.concat([x for x in X_train_ds], axis=0)
-y_train = tf.concat([x for x in y_train_ds], axis=0)
-X_test = tf.concat([x for x in X_test_ds], axis=0)
-y_test = tf.concat([x for x in y_test_ds], axis=0)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005), loss=dice_coefficient_loss, metrics=[dice_coefficient])
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_data=(X_test, y_test))
 
-# Check that data has been processed correctly
-plt.figure(figsize=(10, 10))
-for images in y_test_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.axis("off")
-plt.show()
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_data=(X_test, y_test))
-
-# Final Output of images
-# Compares original with the expected and actual output
-prediction = model.predict(X_test)
-plt.figure(figsize=(10, 10))
-n = 4
-for i in range(n):
-    plt.subplot(n, 3, i*3+1)
-    plt.imshow(X_test[i])
-    plt.axis('off')
-    plt.title("Original", size=11)
-    plt.subplot(n, 3, i*3+2)
-    plt.imshow(prediction[i])
-    plt.axis('off')
-    plt.title("Prediction", size=11)
-    plt.subplot(n, 3, i*3+3)
-    plt.imshow(y_test[i])
-    plt.axis('off')
-    plt.title("Expected", size=11)
-plt.show()
+    plot_prediction(model, X_test, y_test)
