2fadebcc4c5b41b5a55d922df5a147fb8e8f43da
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2021-10-31 00:27:38 +1000

    Create adjacency matrix & load nodes features

--- recognition/45616756-GCN/driver.py
+++ recognition/45616756-GCN/driver.py
@@ -1,9 +1,21 @@
 import numpy as np
+from scipy.sparse import coo_matrix, csr_matrix
 
 
 def main():
     # Load Facebook dataset
     data = np.load('./data/facebook.npz')
+
+    # Create an adjacency matrix representation based on the edges
+    facebook_edges = data['edges']
+    adjacency_matrix = np.zeros((22470, 22470), dtype='float32')
+    for edge in facebook_edges:
+        adjacency_matrix[edge[0]][edge[1]] = 1
+    adjacency_matrix = coo_matrix(adjacency_matrix)
+
+    # Nodes features
+    facebook_features = data['features']
+    facebook_features = csr_matrix(facebook_features)
 
 
 if __name__ == '__main__':
