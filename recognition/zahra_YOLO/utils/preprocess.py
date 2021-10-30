c27da6d755d92acb9613f433c67664756e02325e
zaragolshani <zara.goli@hotmail.com>
2021-10-30 19:50:54 +1100

    update the script to adjust the bounding box coordinates after padding and scaling images

--- recognition/zahra_YOLO/utils/preprocess.py
+++ recognition/zahra_YOLO/utils/preprocess.py
@@ -1,5 +1,17 @@
 #This script reads all the images and resizes the image without changing the aspect ratio.
 #in this script we adjust the size of the image by adding padding to the top, bottom, left, and right of the image
+import glob
+import random
+import os
+import numpy as np
+
+import torch
+
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+
 
 class ListDataset(Dataset):
     def __init__(self, list_path, img_size=416):
@@ -17,30 +29,45 @@ class ListDataset(Dataset):
 
         img_path = self.img_files[index % len(self.img_files)].rstrip()
         img = Image.open(img_path)
-        height , width = img.size
+        #img.size[0] = width
+        width, height = img.size
         img_size=416  
-        ratio = min(img_size/img.size[0], img_size/img.size[1])
+        max_v = max(height, width)
+        ratio = img_size / max_v
         imw = round(img.size[0] * ratio)
         imh = round(img.size[1] * ratio)
         
-        #this is the padding for the left, top, right and bottom borders respectively.
+        if imh>imw: 
+            dim_diff =imh - imw
+            pad_left, pad_right = dim_diff // 2, dim_diff - dim_diff // 2
+            pad=(pad_left,0,pad_right,0)
+        elif imw>imh:
+            dim_diff =imw - imh
+            pad_top, pad_bottom = dim_diff // 2, dim_diff - dim_diff // 2
+            pad=(0,pad_top,0,pad_bottom)
+    
+ #this is the padding for the left, top, right and bottom borders respectively.
         img_transforms = transforms.Compose([ transforms.Resize((imh, imw)),
-         transforms.Pad((max(int((imh-imw)/2),0), max(int((imw-imh)/2),0), max(int((imh-imw)/2),0), max(int((imw-imh)/2),0)),
-                        (128,128,128)),
+         transforms.Pad(pad,(128,128,128)),
          transforms.ToTensor(),
          ])
+        
+
     # convert image to Tensor
         image_tensor = img_transforms(img).float()
     # convert image to Tensor
         input_img = img_transforms(img).float()
-        
-        
+
+        #---------
+        #  Label
+        #---------
+          
+    
 # this script extract the coordinates of the bounding box we created in the annotation file for the original image which is unpadded
 #and unscaled, then updates the left-bottom and top-right coordinate of the bounding box according to the scaled and padded image
 
         label_path = self.label_files[index % len(self.img_files)].rstrip()
-        pad = ((pad1, pad2), (0, 0), (0, 0)) if height <= width else ((0, 0), (pad1, pad2), (0, 0))
-        padded_height, padded_width, _ = input_img.shape
+        # _,padded_height, padded_width = input_img.shape
         
         annotation = None
         if os.path.exists(label_path):
@@ -51,18 +78,16 @@ class ListDataset(Dataset):
             x2 = width * (annotation[:, 1] + annotation[:, 3]/2)
             y2 = height * (annotation[:, 2] + annotation[:, 4]/2)
             
+            x1 += pad[0]
+            y1 += pad[3]
+            x2 += pad[2]
+            y2 += pad[1]
             
-            
-            # Adjust for added padding
-            x1 += pad[1][0]
-            y1 += pad[0][0]
-            x2 += pad[1][0]
-            y2 += pad[0][0]
             # Calculate ratios from coordinates
-            annotation[:, 1] = ((x1 + x2) / 2) / padded_width
-            annotation[:, 2] = ((y1 + y2) / 2) / padded_height
-            annotation[:, 3] *= width / padded_width
-            annotation[:, 4] *= height / padded_height
+            annotation[:, 1] = ((x1 + x2) / 2) / max_v
+            annotation[:, 2] = ((y1 + y2) / 2) / max_v
+            annotation[:, 3] *= width / max_v
+            annotation[:, 4] *= height / max_v
         
         # Fill matrix
         filled_labels = np.zeros((self.max_objects, 5))
