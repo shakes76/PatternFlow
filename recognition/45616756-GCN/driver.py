0039e64bc85c71990a1f40d1eb5f7e2076acd333
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2021-10-31 00:38:16 +1000

    Add adjacency and features matrix normalization

--- recognition/45616756-GCN/driver.py
+++ recognition/45616756-GCN/driver.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import coo_matrix, csr_matrix
+from scipy.sparse import coo_matrix, csr_matrix, eye, diags
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
@@ -31,6 +31,25 @@ def main():
     validation_target, test_target = train_test_split(
         test_target, train_size=0.20, test_size=None, stratify=test_target
     )
+
+    # Normalize the adjacency matrix
+    a_tilde = adjacency_matrix + eye(22470, dtype='float32')  # adjacency matrix + self-loop
+    d = diags(np.array(a_tilde.sum(axis=1)).flatten())  # degree matrix
+    degrees_inverse = np.power(d.diagonal(), -1)
+    d_inverse = diags(degrees_inverse)  # degree matrix inverse
+    adjacency_matrix = d_inverse.dot(a_tilde).tocoo()
+
+    # Normalize the adjacency matrix (Ver.2)
+    # A_tilde = adjacency_matrix + np.eye(22470)
+    # D_tilde = np.matrix(np.diag(np.array(np.sum(A_tilde, axis=0))[0]))
+    # D_tilde_invroot = np.linalg.inv(sqrtm(D_tilde))
+    # A_hat = np.matmul(np.matmul(A_tilde, D_tilde_invroot), D_tilde_invroot)
+
+    # Normalize the features matrix
+    d = diags(np.array(facebook_features.sum(axis=1)).flatten())  # degree matrix
+    degrees_inverse = np.power(d.diagonal(), -1)
+    d_inverse = diags(degrees_inverse)  # degree matrix inverse
+    facebook_features = d_inverse.dot(facebook_features)
 
 
 if __name__ == '__main__':
