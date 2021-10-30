Fixed up a few minor problems, added batch normalisation and finished everything.

recognition/ISIC_UNET/ImprovedUnet.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow.keras.layers as layers
 
 # Improved U-Net based off https://arxiv.org/pdf/1802.10508v1.pdf
 def IUNET_model():
@@ -6,42 +7,48 @@ def IUNET_model():
 
     # go down the U
     # step 1
-    conv1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(inputs)
-    context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(conv1)
+    conv1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation=layers.LeakyReLU())(inputs)
+
+    batch1 = layers.BatchNormalization()(conv1)
+    context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation=layers.LeakyReLU())(batch1)
     drop1 = tf.keras.layers.Dropout(0.3)(context1)
-    context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation="relu")(drop1)
+    context1 = tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding="same", activation=layers.LeakyReLU())(drop1)
 
     sum1 = tf.math.add(conv1, context1)
 
     # step 2
-    stride2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", strides=2, activation="relu")(sum1)
-    context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(stride2)
+    stride2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", strides=2, activation=layers.LeakyReLU())(sum1)
+    batch2 = layers.BatchNormalization()(stride2)
+    context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation=layers.LeakyReLU())(batch2)
     drop2 = tf.keras.layers.Dropout(0.3)(context2)
-    context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(drop2)
+    context2 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation=layers.LeakyReLU())(drop2)
 
     sum2 = tf.math.add(stride2, context2)
 
     # step 3
-    stride3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", strides=2, activation="relu")(sum2)
-    context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(stride3)
+    stride3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", strides=2, activation=layers.LeakyReLU())(sum2)
+    batch3 = layers.BatchNormalization()(stride3)
+    context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation=layers.LeakyReLU())(batch3)
     drop3 = tf.keras.layers.Dropout(0.3)(context3)
-    context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(drop3)
+    context3 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation=layers.LeakyReLU())(drop3)
 
     sum3 = tf.math.add(stride3, context3)
 
     # step 4
-    stride4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", strides=2, activation="relu")(sum3)
-    context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(stride4)
+    stride4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", strides=2, activation=layers.LeakyReLU())(sum3)
+    batch4 = layers.BatchNormalization()(stride4)
+    context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation=layers.LeakyReLU())(batch4)
     drop4 = tf.keras.layers.Dropout(0.3)(context4)
-    context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu")(drop4)
+    context4 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation=layers.LeakyReLU())(drop4)
 
     sum4 = tf.math.add(stride4, context4)
 
     # step 5 lowest one, bottom of the U
-    stride5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", strides=2, activation="relu")(sum4)
-    context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(stride5)
+    stride5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", strides=2, activation=layers.LeakyReLU())(sum4)
+    batch5 = layers.BatchNormalization()(stride5)
+    context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation=layers.LeakyReLU())(batch5)
     drop5 = tf.keras.layers.Dropout(0.3)(context5)
-    context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation="relu")(drop5)
+    context5 = tf.keras.layers.Conv2D(filters=256, kernel_size=3, padding="same", activation=layers.LeakyReLU())(drop5)
 
     sum5 = tf.math.add(stride5, context5)
 
@@ -50,22 +57,22 @@ def IUNET_model():
     # Go back up the U
     # step 6 up 1
     conc6 = tf.keras.layers.concatenate([up5, sum4])
-    loc6 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation="relu") (conc6)
-    loc6 = tf.keras.layers.Conv2D(filters=128, kernel_size=1, padding="same", activation="relu") (loc6)
+    loc6 = tf.keras.layers.Conv2D(filters=128, kernel_size=3, padding="same", activation=layers.LeakyReLU())(conc6)
+    loc6 = tf.keras.layers.Conv2D(filters=128, kernel_size=1, padding="same", activation=layers.LeakyReLU())(loc6)
     up6 = tf.keras.layers.UpSampling2D(size=(2, 2))(loc6)
 
     # step 7 up 2
     conc7 = tf.keras.layers.concatenate([up6, sum3])
-    loc7 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation="relu")(conc7)
-    loc7 = tf.keras.layers.Conv2D(filters=64, kernel_size=1, padding="same", activation="relu")(loc7)
+    loc7 = tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding="same", activation=layers.LeakyReLU())(conc7)
+    loc7 = tf.keras.layers.Conv2D(filters=64, kernel_size=1, padding="same", activation=layers.LeakyReLU())(loc7)
     up7 = tf.keras.layers.UpSampling2D(size=(2, 2))(loc7)
 
     uploc7 = tf.keras.layers.Conv2DTranspose(filters=32, kernel_size=3, padding='same')(up7)  # tf.keras.layers.UpSampling2D(size=(2, 2))(loc7)
 
     # step 8 up 3
     conc8 = tf.keras.layers.concatenate([up7, sum2])
-    loc8 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(conc8)
-    loc8 = tf.keras.layers.Conv2D(filters=32, kernel_size=1, padding="same", activation="relu")(loc8)
+    loc8 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation=layers.LeakyReLU())(conc8)
+    loc8 = tf.keras.layers.Conv2D(filters=32, kernel_size=1, padding="same", activation=layers.LeakyReLU())(loc8)
     up8 = tf.keras.layers.UpSampling2D(size=(2, 2))(loc8)
 
     seg8 = tf.math.add(uploc7, loc8)
@@ -73,7 +80,7 @@ def IUNET_model():
 
     # step 9 up 4
     conc9 = tf.keras.layers.concatenate([up8, sum1])
-    conv9 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation="relu")(conc9)
+    conv9 = tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding="same", activation=layers.LeakyReLU())(conc9)
 
     seg9 = tf.math.add(upseg8, conv9)
 

recognition/ISIC_UNET/UnetData.py
@@ -44,13 +44,13 @@ def prepare_images(max_images):
     new_x = x_images[0:max_images]
     new_mask = mask_images[0:max_images]
 
-    print("Length of total images split 49% in training, 30% testing and 21% validation:")
+    print("Length of total images split 60% in training, 20% testing and 20% validation:")
     print(len(new_x))
     print(len(new_mask))
     # split the images into training, testing and validation
-    # 49% training, 30% testing, 21% val
-    X_train, X_test, y_train, y_test = train_test_split(new_x, new_mask, test_size=0.3, random_state=1)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=1)
+    # 60% training, 20% testing, 20% val
+    X_train, X_test, y_train, y_test = train_test_split(new_x, new_mask, test_size=0.2, random_state=1)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 
     # Put the images into a dataset from a list
     train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))

recognition/ISIC_UNET/driver.py
@@ -4,8 +4,6 @@ from recognition.ISIC_UNET.ImprovedUnet import *
 from recognition.ISIC_UNET.UnetData import *
 import matplotlib.pyplot
 
-from recognition.ISIC_UNET.IUnet import *
-
 
 # https://www.fatalerrors.org/a/dice-loss-in-medical-image-segmentation.html
 def dsc_co(ytrue, ypred):
@@ -14,7 +12,7 @@ def dsc_co(ytrue, ypred):
     true_flat = tf.keras.backend.flatten(ytrue)
     pred_flat = tf.keras.backend.flatten(ypred)
     inter = tf.keras.backend.sum(true_flat * pred_flat)
-    return 2 * (inter + smooth) / (tf.keras.backend.sum(true_flat + pred_flat) + smooth)
+    return 2 * (inter + smooth) / (tf.keras.backend.sum(true_flat) + tf.keras.backend.sum(pred_flat) + smooth)
 
 
 def dsc_loss(ytrue, ypred):
@@ -91,11 +89,11 @@ if __name__ == '__main__':
             print(e)
 
     # sets the max amount of images
-    max_images = 100
+    max_images = 1800
 
     # https://stackoverflow.com/questions/52123670/neural-network-converges-too-fast-and-predicts-blank-results
     # sets the learning rate
-    learning_rate = 4*10**(-5)
+    learning_rate = 3*10**(-5)
 
     # prepare a given number of images
     final_train, final_test, final_val = prepare_images(max_images)
@@ -105,15 +103,15 @@ if __name__ == '__main__':
 
     # compile the model with a specified learning rate, loss and given metrics
     model.compile(tf.keras.optimizers.Adam(lr=learning_rate), loss=dsc_loss, metrics=['accuracy', dsc_co])
-    model.summary() # gives a summary of the model to debug
+    model.summary()  # gives a summary of the model to help with debugging
 
     # training the model with a given batch size and number of epochs
     print("Training model")
-    history = model.fit(final_train.batch(7), epochs=10, validation_data=final_val.batch(7))
+    history = model.fit(final_train.batch(8), epochs=15, validation_data=final_val.batch(8))
 
     # evaluates the model with a given batch size
     print("Evaluating model")
-    evaluation = model.evaluate(final_test.batch(7))
+    evaluation = model.evaluate(final_test.batch(8))
 
     # calls the function which plots all the graphs
     plot_graphs(history, evaluation)