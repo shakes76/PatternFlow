a75da30b7b367776bf55996bed45faa25d0e472d
Andrew <s4589098@student.uq.edu.au>
2021-10-31 04:06:53 +1000

    minor errors fixed in model.py for improved unet

--- recognition/s45890985_Improved_Unet/model.py
+++ recognition/s45890985_Improved_Unet/model.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
-import tnsorflow_addons as tfa
+import tensorflow_addons as tfa
 from tensorflow.keras import layers, models, activations
 
 #method for created the improved unet model
-def improved_unet(width, heigh, channels):
-    inputs = layers.Input((width,height,channels))
+def improved_unet(width, height, channels):
+    inputs = layers.Input((width, height, channels))
 
     #encoding path
-    en1 = layers.Conv2D(16, (3, 3), padding='same')(inputs)
+    en1 = conv_relu_block(inputs, 16)
     en1 = context_module(en1, 16)
     en2 = encoder_block(en1, 32)
     en3 = encoder_block(en2, 64)
@@ -20,17 +20,19 @@ def improved_unet(width, heigh, channels):
     de3 = decoder_block(de2, en2, 32)
     de4 = upsampling_module(de3, 16)
     de4 = layers.concatenate([en1, de4])
-    de4 = layers.Conv2D(32, (3,3), padding='same')(de4)
+    de4 = layers.Conv2D(32, (3, 3), padding='same')(de4)
     de4 = segmentation_layer(de4)
 
     #segmentation output
-    seg1 = de2
-    seg2 = de3
+    seg1 = segmentation_layer(de2)
+    seg1 = layers.UpSampling2D((2, 2))(seg1)
+    seg2 = segmentation_layer(de3)
     seg2 = layers.Add()([seg1, seg2])
+    seg2 = layers.UpSampling2D((2, 2))(seg2)
     seg3 = de4
     seg3 = layers.Add()([seg2, seg3])
 
-    outputs = activations.softmax(seg3)
+    outputs = layers.Conv2D(1, (1, 1),  activation='sigmoid')(seg3)
 
     model = models.Model(inputs, outputs)
     return model
@@ -41,8 +43,8 @@ def improved_unet(width, heigh, channels):
 def conv_relu_block(input, conv_depth, conv_size=(3,3)):
     #convolutions with leaky relu, and normalisation
     conv = layers.Conv2D(conv_depth, conv_size, padding='same')(input)
-    conv = layers.batchNormalisation()(conv)
-    conv = layers.LeakyReLu(alpha=0.01)(conv)
+    conv = tfa.layers.InstanceNormalization()(conv)
+    conv = layers.LeakyReLU(alpha=0.01)(conv)
     return conv
 
 def context_module(input, conv_depth):
@@ -66,16 +68,17 @@ def upsampling_module(input, conv_depth):
     up = layers.Conv2D(conv_depth, (3, 3), padding='same')(up)
     return up
 def segmentation_layer(input):
-    seg = layers.Conv2D(1, (1, 1), activation = 'softmax')(input)
+    seg = layers.Conv2D(1, (1, 1), activation = 'sigmoid')(input)
     return seg
 
 #method for blocks in the encoder path
 def encoder_block(input, conv_depth):
-    en = layers.Conv2D(conv_depth, (3, 3), padding='same')(input)
+    en = layers.Conv2D(conv_depth, (3, 3), strides=2, padding='same')(input)
     en = context_module(en, conv_depth)
     return en
 
 def decoder_block(input, concat, conv_depth):
+
     de = upsampling_module(input, conv_depth)
     de = layers.concatenate([concat, de])
     de = localization_module(de, conv_depth)
