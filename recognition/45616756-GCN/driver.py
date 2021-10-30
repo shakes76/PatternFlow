ef5d7212938adc221440bca537df53603bbbdb11
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2021-10-31 00:30:46 +1000

    Convert categorical value and split target

--- recognition/45616756-GCN/driver.py
+++ recognition/45616756-GCN/driver.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.sparse import coo_matrix, csr_matrix
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
 
 def main():
@@ -16,6 +18,19 @@ def main():
     # Nodes features
     facebook_features = data['features']
     facebook_features = csr_matrix(facebook_features)
+
+    # Convert each categorical value (One-hot encoding)
+    facebook_target = data['target']
+    lb = preprocessing.LabelBinarizer()
+    facebook_target = lb.fit_transform(facebook_target)
+
+    # Split the target (20:20:60)
+    train_target, test_target = train_test_split(
+        facebook_target, train_size=0.20, test_size=None, stratify=facebook_target
+    )
+    validation_target, test_target = train_test_split(
+        test_target, train_size=0.20, test_size=None, stratify=test_target
+    )
 
 
 if __name__ == '__main__':
