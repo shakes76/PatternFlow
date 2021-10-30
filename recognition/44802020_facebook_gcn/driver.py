d94cd3b5f29bfe4084b4e51a32da7511367d4538
johnvparsons99 <62982818+johnvparsons99@users.noreply.github.com>
2021-10-31 00:08:07 +1000

    Reorganised and tidied up code.

--- recognition/44802020_facebook_gcn/driver.py
+++ recognition/44802020_facebook_gcn/driver.py
@@ -1,18 +1,15 @@
-import keras.layers
-
 import myGraphModel
 import tensorflow as tf
+
+from tensorflow.keras import losses
 import tensorflow.keras.optimizers as op
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Softmax, Input
-from tensorflow.keras import losses, layers, models, activations
 import scipy.sparse as spr
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
 import numpy as np
 
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
-# Constants
+
 def coo_matrix_to_sparse_tensor(coo):
     indices = np.mat([coo.row, coo.col]).transpose()
     return tf.SparseTensor(indices, coo.data, coo.shape)
@@ -38,148 +35,11 @@ def summarise_data(data, aspect):
     print("====================")
 
 
-def main():
-    print("Tensorflow version:", tf.__version__)
-    print("Numpy version:", np.__version__)
-
-    # file_path = r"C:\Users\johnv\Documents\Code Projects\Pattern Recognition\facebook.npz"
-
-    file_path = r"C:\Users\johnv\Documents\University\COMP3710\Pattern Flow Project\facebook.npz"
-
-    # Load in Data
-    data = np.load(file_path)
-
-    # print(data.files)
-
-    # features = data['features']
-    # edges = data['edges']
-    # target = data['target']
-
-    # summarise_data(data, 'features')
-    # summarise_data(data, 'edges')
-    # summarise_data(data, 'target')
-
-    # There are 22 470 Pages
-    # Each with 128 features
-    # Each falls into 1 of 4 categories
-    # There are 342 004 Edges between the pages
-
-    # Split Data
-
-
-    # ================== TEST MODEL ========================
-    # This is the model
-    page_one = [0, 0, 1, 2, 0, 4, 2, 3, 3, 1]
-    page_two = [4, 2, 3, 3, 1, 0, 0, 1, 2, 0]
-
-    name = [0, 1, 2, 3, 4]
-
-    page_one += name
-    page_two += name
-
-    ones = tf.ones_like(page_one)
-
-    feats = [[0.3, 2.2, -1.7],
-             [4., -1.3, -1.2],
-             [0.3, 2.2, 0.5],
-             [0.5, 0.7, 3.5],
-             [2.0, 5.2, 0.6]
-             ]
-
-    labels = [0,
-             0,
-             1,
-             3,
-             2
-              ]
-
-    # Construct Adjacency matrix
-    a_bar = spr.coo_matrix((ones, (page_one, page_two)))
-    a_bar.setdiag(1)
-    a_bar = coo_matrix_to_sparse_tensor(a_bar)
-
-    # ================== REAL MODEL ========================
-    # Adjacency Matrix
-    page_one = data['edges'][:, 0]
-    page_two = data['edges'][:, 1]
-
-    # # Features
-    feats = tf.convert_to_tensor(data['features'])
-
-    # Labels
-    labels = tf.convert_to_tensor(data['target'])
-
-    # Split Data
-    split = 18000
-    # train_adj_p1, test_adj_p1 = page_one[:split], page_one[split:]
-    # train_adj_p2, test_adj_p2 = page_two[:split], page_two[split:]
-
-    train_labels, test_labels = labels[:split], labels[split:]
-    train_feats, test_feats = feats[:split], feats[split:]
-
-    ones = tf.ones_like(page_one)
-
-    a_bar = spr.coo_matrix((ones, (page_one, page_two)))
-    a_bar.setdiag(1)
-
-    a_dense = a_bar.todense()
-
-    print(a_dense)
-    print(a_dense.shape)
-
-    a_bar = a_dense[:18000, :18000]
-    a_bar_test = a_dense[18000:, 18000:]
-    print("Done Splitting")
-
-    a_bar = spr.coo_matrix(a_bar)
-    a_bar_test = spr.coo_matrix(a_bar_test)
-    print("Done converting to coo")
-
-    # Normalize
-    a_bar = normalize_adjacency_matrix(a_bar=a_bar)
-    a_bar_test = normalize_adjacency_matrix(a_bar=a_bar_test)
-
-    # Convert to Sparse Tensor
-    a_bar = coo_matrix_to_sparse_tensor(a_bar)
-    a_bar_test = coo_matrix_to_sparse_tensor(a_bar_test)
-
-    # ================== REAL MODEL DONE ===================
-
-    print("===== Running Model =====")
-
-    # Construct Model
-    my_model = Sequential()
-    print(a_bar)
-    #my_model = myGraphModel.MyModel(a_bar, a_bar_test, train_feats.shape)
-    loss_fn = losses.SparseCategoricalCrossentropy(from_logits=True)
-    opt = op.Adam(learning_rate=0.05)
-
-    my_model.add(Input(shape=tf.Tensor.get_shape(train_feats)))
-
-    my_model.add(myGraphModel.FaceGCNLayer(a_bar, a_bar_test))
-    my_model.add((Dense(64)))
-    my_model.add(myGraphModel.FaceGCNLayer(a_bar, a_bar_test))
-    my_model.add(Dense(4))
-
-    my_model.compile(optimizer=opt, loss=loss_fn, metrics=['accuracy'])
-    # my_model.
-    my_model.fit(train_feats,
-                 train_labels,
-                 epochs=50,
-                 batch_size=22470, shuffle=False
-                 )
-
-    print(my_model.summary())
-
-    # Evaluate
-    my_model.evaluate(test_feats,
-                      test_labels,
-                      batch_size=22470)
-
+def plot_tsne(labels, feats):
     # TSNE
+    print("Executing TSNE, this might take a moment...")
     tsne = TSNE(2)
-    tsne_data = tsne.fit_transform(test_feats)
-    labels = test_labels
+    tsne_data = tsne.fit_transform(feats)
     print(tsne_data.shape)
 
     plt.figure(figsize=(6, 5))
@@ -190,8 +50,92 @@ def main():
     plt.show()
 
 
+def parse_data(data, test_split):
+    # Adjacency Matrix
+    # Split EdgeList into two tensors
+    page_one = data['edges'][:, 0]
+    page_two = data['edges'][:, 1]
+    # Features
+    feats = tf.convert_to_tensor(data['features'])
+    # Labels
+    labels = tf.convert_to_tensor(data['target'])
+
+    # Convert split percentage into integer
+    split = int(round(labels.shape[0] * test_split))
+    train_labels, test_labels = labels[:split], labels[split:]
+    train_feats, test_feats = feats[:split], feats[split:]
+
+    # Convert EdgeList to Sparse Adjacency Matrix
+    ones = tf.ones_like(page_one)  # Create Ones Matrix to set
+    a_bar = spr.coo_matrix((ones, (page_one, page_two)))  # Convert to SciPy COO Matrix
+    a_bar.setdiag(1)  # Make all nodes adjacent to themselves
+
+    a_dense = a_bar.todense()  # Convert to Dense to  easily split into test/train
+
+    # Re-create two adjacency matrices for training/testing
+    a_bar = a_dense[:split, :split]
+    a_bar_test = a_dense[split:, split:]
+
+    # Convert back to COO Matrix
+    a_bar = spr.coo_matrix(a_bar)
+    a_bar_test = spr.coo_matrix(a_bar_test)
+
+    # Normalize
+    a_bar = normalize_adjacency_matrix(a_bar=a_bar)
+    a_bar_test = normalize_adjacency_matrix(a_bar=a_bar_test)
+
+    # Convert to Sparse Tensor
+    a_bar = coo_matrix_to_sparse_tensor(a_bar)
+    a_bar_test = coo_matrix_to_sparse_tensor(a_bar_test)
+
+    return train_feats, train_labels, a_bar, test_feats, test_labels, a_bar_test
+
+
+def main():
+    print("Tensorflow version:", tf.__version__)
+    print("Numpy version:", np.__version__)
+
+    file_path = r"C:\Users\johnv\Documents\University\COMP3710\Pattern Flow Project\facebook.npz"
+
+    # Load in Data
+    data = np.load(file_path)
+
+    # There are 22 470 Pages
+    # Each with 128 features
+    # Each falls into 1 of 4 categories
+    # There are 342 004 Edges between the pages
+
+    test_split = 0.2
+    train_feats, train_labels, a_bar, test_feats, test_labels, a_bar_test = parse_data(data, test_split)
+
+    # ================== REAL MODEL ========================
+    print("=============== Building Model ===============")
+    # Construct Model
+    my_model = myGraphModel.makeMyModel(a_bar, a_bar_test, train_feats)
+
+    loss_fn = losses.SparseCategoricalCrossentropy(from_logits=True)
+    opt = op.Adam(learning_rate=0.05)
+    my_model.compile(optimizer=opt, loss=loss_fn, metrics=['accuracy'])
+
+    # ================== RUN MODEL ========================
+    # Train Model
+    my_model.fit(train_feats,
+                 train_labels,
+                 epochs=50,
+                 batch_size=22470, shuffle=False
+                 )
+
+    print(my_model.summary())
+
+    # Evaluate Model
+    my_model.evaluate(test_feats,
+                      test_labels,
+                      batch_size=22470)
+
+    # Plot TSNE
+    plot_tsne(test_labels, test_feats)
+
+
 if __name__ == '__main__':
     main()
-
-
 
--- recognition/44802020_facebook_gcn/myGraphModel.py
+++ recognition/44802020_facebook_gcn/myGraphModel.py
@@ -1,14 +1,20 @@
 import keras.initializers.initializers_v1
-import scipy
-import scipy.sparse as spr
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
 
 
-@tf.function
-def printIncident(incident):
-    print("MyFunc")
-    print(incident)
-    return incident
+def makeMyModel(a_bar, a_bar_test, train_feats):
+    my_model = Sequential()
+
+    my_model.add(Input(shape=tf.Tensor.get_shape(train_feats)))
+
+    my_model.add(FaceGCNLayer(a_bar, a_bar_test))
+    my_model.add((Dense(64)))
+    my_model.add(FaceGCNLayer(a_bar, a_bar_test))
+    my_model.add(Dense(4))
+
+    return my_model
 
 
 class FaceGCNLayer(tf.keras.layers.Layer):
@@ -16,14 +22,8 @@ class FaceGCNLayer(tf.keras.layers.Layer):
         super(FaceGCNLayer, self).__init__()
         self.adj_m = adj_m
         self.test_adj_m = test_adj_m
-        # self.weights1 = []
-        # self.build(input_shape=input_shape)
 
     def build(self, input_shape):
-        print("Building")
-        print(input_shape)
-        print(input_shape[-1])
-        #1:
         self.weights1 = self.add_weight("weights1",
                                        shape=(1, input_shape[-1]),
                                        initializer=keras.initializers.initializers_v1.RandomNormal)
@@ -39,36 +39,3 @@ class FaceGCNLayer(tf.keras.layers.Layer):
             z = ax * self.weights1
 
         return z
-
-
-class MyModel(tf.keras.Model):
-
-    def __init__(self, adj, eval_adj, input_shape):
-        super(MyModel, self).__init__()
-        self.adj = adj
-        self.eval_adj = eval_adj
-        self.my_input_shape = input_shape
-
-        self.input1 = tf.keras.layers.Input(shape=self.my_input_shape)
-
-        self.dense1 = tf.keras.layers.Dense(96)
-        self.gcn1 = FaceGCNLayer(self.adj, input_shape)
-        self.dense2 = tf.keras.layers.Dense(64)
-        self.gcn2 = FaceGCNLayer(self.adj, input_shape)
-        self.dense3 = tf.keras.layers.Dense(4)
-
-    def set_input_shape(self, new_shape):
-        self.my_input_shape = new_shape
-        self.input1 = tf.keras.layers.Input(shape=self.my_input_shape)
-
-    def call(self, inputs):
-        # x = tf.keras.layers.Input(inputs)
-        x = self.dense1(inputs)
-        x = self.gcn1.call(x, self.adj)
-        x = self.dense2(x)
-        x = self.gcn2.call(x, self.adj)
-
-        return self.dense3(x)
-
-
-
