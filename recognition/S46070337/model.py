703965d99d35e95cd49e0edbdd716927e4bbff81
hyb19951213 <93240253+hyb19951213@users.noreply.github.com>
2021-11-01 03:18:35 +1000

    rewrite model to deal with bugs upon model.py

--- recognition/S46070337/model.py
+++ recognition/S46070337/model.py
@@ -1,81 +1,95 @@
-import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
 from tensorflow.keras import layers
-from tensorflow.keras.layers import LeakyReLU, Conv2D, Conv1D, MaxPooling2D, Dropout, UpSampling2D, UpSampling3D, \
-    concatenate, Conv2DTranspose, Add
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+import tensorflow_addons as tfa
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
-def unet_model():
+# Build module function
+def block(input_data, conv_size):
+    conv1 = tfa.layers.InstanceNormalization()(input_data)
+    conv2 = tf.keras.layers.LeakyReLU(alpha=0.01)(conv1)
+    conv3 = tf.keras.layers.Conv2D(conv_size, kernel_size=3, padding='same')(conv2)
+    conv4 = tf.keras.layers.Dropout(0.3)(conv3)
+    conv5 = tfa.layers.InstanceNormalization()(conv4)
+    conv6 = tf.keras.layers.LeakyReLU(alpha=0.01)(conv5)
+    conv7 = tf.keras.layers.Conv2D(conv_size, kernel_size=3, padding='same')(conv6)
+    return conv7
 
+
+# Add segmentation layers
+def segmentation_layer(input_data):
+    seg_layers = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(input_data)
+    return seg_layers
+
+
+def unet_model(image_height, image_width, image_channels):
+    input_data = tf.keras.layers.Input((image_height, image_width, image_channels))
+    function_for_activating = tf.keras.layers.LeakyReLU(alpha=0.01)
+    # Encoding Blocks
+    conv1_1 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(input_data)
+    conv1_2 = block(conv1_1, 16)
+    add1 = layers.Add()([conv1_2, conv1_1])
     
-    inputs = tf.keras.Input(shape=(256, 256, 3))
-    conv1 = Conv2D(16, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(inputs)    
-    conv1_1 = Conv2D(16, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv1)
-    pool1 = Dropout(0.3)(conv1_1)
-    conv1_2 = Conv2D(16, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(pool1)
-    add1 = Add()([conv1, conv1_2])
+    conv2_1 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same', strides=2)(add1)
+    conv2_2 = block(conv2_1, 32)
+    add2 = layers.Add()([conv2_2, conv2_1])
     
-    conv2 = Conv2D(32, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same', strides=(2, 2))(add1)
-    conv2_1 = Conv2D(32, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv2)
-    pool2 = Dropout(0.3)(conv2_1)
-    conv2_2 = Conv2D(32, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(pool2)
-    add2 = Add()([conv2, conv2_2])
-
-    conv3 = Conv2D(64, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same', strides=(2, 2))(add2)
-    conv3_1 = Conv2D(64, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv3)
-    pool3 = Dropout(0.3)(conv3_1)
-    conv3_2 = Conv2D(64, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(pool3)
-    add3 = Add()([conv3, conv3_2])
-
-    conv4 = Conv2D(128, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same', strides=(2, 2))(add3)
-    conv4_1 = Conv2D(128, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv4)
-    pool4 = Dropout(0.3)(conv4_1)
-    conv4_2 = Conv2D(128, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(pool4)
-    add4 = Add()([conv4, conv4_2])
-
-    conv5= Conv2D(256, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same', strides=(2, 2))(add4)
-    conv5_1 = Conv2D(256, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv5)
-    pool5 = Dropout(0.3)(conv5_1)
-    conv5_2 = Conv2D(256, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(pool5)
-    add5 = Add()([conv5, conv5_2])
+    conv3_1 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same', strides=2)(add2)
+    conv3_2 = block(conv3_1, 64)
+    add3 = layers.Add()([conv3_2, conv3_1])
     
-    #Upsampling layers
-    up1 = UpSampling2D()(add5)
-    con1 = concatenate([up1, add4])
-    up1_1 = Conv2D(128, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(con1)
-    up1_2 = Conv2D(128, (1, 1), activation=LeakyReLU(alpha=0.01), padding='same')(up1_1)
-
-    up2 = UpSampling2D()(up1_2)
-    con2 = concatenate([up2, add3])
-    up2_1 = Conv2D(64, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(con2)
-    up2_2 = Conv2D(64, (1, 1), activation=LeakyReLU(alpha=0.01), padding='same')(up2_1)
+    conv4_1 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same', strides=2)(
+        add3)
+    conv4_2 = block(conv4_1, 128)
+    add4 = layers.Add()([conv4_2, conv4_1])
     
-    #Layers Segmentation
-    seg1_1 = Conv2D(4, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(up2_2)
-    seg1_2 = UpSampling2D()(seg1_1)
+    conv5_1 = tf.keras.layers.Conv2D(256, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same', strides=2)(
+        add4)
+    conv5_2 = block(conv5_1, 256)
+    add5 = layers.Add()([conv5_2, conv5_1])
 
-    up3 = UpSampling2D()(up2_2)
-    con3 = concatenate([up3, add2])
-    up3_1 = Conv2D(32, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(con3)
-    up3_2 = Conv2D(32, (1, 1), activation=LeakyReLU(alpha=0.01), padding='same')(up3_1)
-
-    seg2 = Conv2D(4, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(up3_2)
-
-    up4 = UpSampling2D()(up3_2)
-    con4 = concatenate([up4, add1])
-
-    conv6 = Conv2D(32, (1, 1), activation=LeakyReLU(alpha=0.01), padding='same')(con4)
-
-    seg3 = Conv2D(4, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(conv6)
-
-    #Combine Segmentation
-    add_seg1 = Add()([seg1_2, seg2])
-    add_seg2 = UpSampling2D()(add_seg1)
-    add_seg3 = Add()([add_seg2, seg3])
-
-    output_layer = Conv2D(1, 1, activation="sigmoid")(add_seg3)
-    unetmodel = tf.keras.Model(inputs=inputs, outputs=output_layer)
+    # Decoding and Upsampling
+    up1 = tf.keras.layers.UpSampling2D(size=(2, 2))(add5)
+    up1_conv1 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up1)
+    up1_conv2 = tf.keras.layers.concatenate([add4, up1_conv1])
+    up1_conv3 = tf.keras.layers.Conv2D(128, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up1_conv2)
+    up1_conv4 = tf.keras.layers.Conv2D(128, (1, 1), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up1_conv3)
+    
+    up2 = tf.keras.layers.UpSampling2D(size=(2, 2))(up1_conv4)
+    up2_conv1 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up2)
+    up2_conv2 = tf.keras.layers.concatenate([add3, up2_conv1])
+    up2_conv3 = tf.keras.layers.Conv2D(64, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up2_conv2)
+    up2_conv4 = tf.keras.layers.Conv2D(64, (1, 1), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up2_conv3)
+    
+    segmentation_slice1 = segmentation_layer(up2_conv4)
+    segmentation_slice2 = tf.keras.layers.UpSampling2D(size=(2, 2))(segmentation_slice1)
+    
+    up3 = tf.keras.layers.UpSampling2D(size=(2, 2))(up2_conv4)
+    up3_conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up3)
+    up3_conv2 = tf.keras.layers.concatenate([add2, up3_conv1])
+    up3_conv3 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up3_conv2)
+    up3_conv4 = tf.keras.layers.Conv2D(32, (1, 1), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up3_conv3)
+    
+    segmentation_slice3 = segmentation_layer(up3_conv4)
+    
+    add6 = layers.Add()([segmentation_slice2, segmentation_slice3])
+    conv6 = tf.keras.layers.UpSampling2D(size=(2, 2))(add6)
+    
+    up4 = tf.keras.layers.UpSampling2D(size=(2, 2))(up3_conv4)
+    up4_conv1 = tf.keras.layers.Conv2D(16, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up4)
+    up4_conv2 = tf.keras.layers.concatenate([add1, up4_conv1])
+    up4_conv3 = tf.keras.layers.Conv2D(32, (3, 3), activation=tf.keras.layers.LeakyReLU(alpha=0.01), padding='same')(up4_conv2)
+    
+    segmentation_slice4 = segmentation_layer(up4_conv3)
+    add7 = layers.Add()([conv6, segmentation_slice4])
+    
+    outputs = tf.keras.layers.Conv2D(1, (1, 1), activation='sigmoid')(add7)
+    unetmodel = tf.keras.Model(inputs=[input_data], outputs=[outputs])
+    unetmodel.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+                  loss=["binary_crossentropy"],
+                  metrics=["accuracy"])
     unetmodel.summary()
+    
     return unetmodel
