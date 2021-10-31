9f885c2c110ba4aa14bea2dc263e95ec8af41105
tomdx <thomasle0@outlook.com>
2021-10-31 15:59:15 +1000

    Add PixelCNN creation and training to driver

--- recognition/45801150_Task6/src/driver.py
+++ recognition/45801150_Task6/src/driver.py
@@ -1,19 +1,32 @@
 import numpy as np
 import VQVAE
 import load_oasis_data
+import PixelCNN
 
-x_train, x_test, x_val = load_oasis_data.get_data()
+latent_dimensions = 16
+num_embeddings = 128
+print(f"latent_dimensions: {latent_dimensions}, num_embeddings={num_embeddings}")
+
+x_train, x_test, x_val = load_oasis_data.get_data(10)
 
 x_train = np.expand_dims(x_train, -1)
 x_test = np.expand_dims(x_test, -1)
 x_val = np.expand_dims(x_val, -1)
 
-x_train_normalised = (x_train / 255.0) - 0.5
-x_test_normalised = (x_test / 255.0) - 0.5
-x_val_normalised = (x_val / 255.0) - 0.5
+x_train_normalised = (x_train / 255.0)
+x_test_normalised = (x_test / 255.0)
+x_val_normalised = (x_val / 255.0)
 
 variance = np.var(x_train / 255.0)
 
-vqvae = VQVAE.train_vqvae(x_train_normalised, variance, x_val_normalised)
+vqvae = VQVAE.VQVae(variance, latent_dimensions, num_embeddings)
+VQVAE.train_vqvae(vqvae, x_train_normalised, x_val_normalised, 2)
 VQVAE.compare_reconstructions(vqvae, x_test_normalised, 10)
+
+encoder = vqvae.get_layer("encoder")
+encoder_output_shape = encoder.predict(x_test[0:1]).shape
+pixel_cnn = PixelCNN.create_pixel_cnn(encoder_output_shape, num_embeddings)
+
+PixelCNN.train_pixel_cnn(pixel_cnn, vqvae, x_train_normalised, 2)
+
 
