d7b05f825f4ed140d29ba13e127f08fb9217f73f
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-31 21:14:55 +1000

    add display function to utilities.py for displaying results
    fixed incorrect type name on docstring of dice_similarity

--- recognition/ISIC-ImprovedUnet-44961374/utilities.py
+++ recognition/ISIC-ImprovedUnet-44961374/utilities.py
@@ -3,9 +3,24 @@ This script contains functions required for dsiplaying images and calculating DS
 @author: Mujibul Islam Dipto
 """
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-def display_image():
-    pass
+
+def display(data, figsize, cmap):
+    """Displays a scan, mask and predicted mask of a lesion from given data.
+
+    Args:
+        data (list): list containing scan, mask and predicted mask Tensors
+        figsize (tuple): size of the figure to be displayed
+        cmap (str): colormap for the plot
+    """
+    plt.figure(figsize=figsize)
+    for i, img in enumerate(data):
+        plt.subplot(1, len(data), i + 1)
+        plt.imshow(img, cmap=cmap)
+        plt.axis("off")
+    plt.show()
+
 
 def dice_similarity(x, y):
     """Calculates the Dice Similarity Coefficient between two images, where:
@@ -13,8 +28,8 @@ def dice_similarity(x, y):
     More information about DSC can be found here: https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
     Function adapted from: https://medium.com/@karan_jakhar/100-days-of-code-day-7-84e4918cb72c
     Args:
-        x (numpy array): image 1
-        y (numpy array): image 2
+        x (Tensor): image 1
+        y (Tensor): image 2
 
     Returns:
         float: calculated DSC between these two images
