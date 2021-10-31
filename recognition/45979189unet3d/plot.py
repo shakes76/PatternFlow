a80a5d02f40a2a596b78a4bf4b0e98f00c350907
aCoalBall <59113878+aCoalBall@users.noreply.github.com>
2021-11-01 04:35:35 +1000

    Delete plot.py
    wrong files uploaded

--- recognition/45979189unet3d/plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-epoch = range(1, 9)
-val_dice = [2.545/16, 4.631/16, 4.530/16, 5.83/16, 5.58/16, 7.65/16, 7.01/16, 7.44/16]
-plt.title('validation DSC')
-plt.plot(epoch, val_dice)
-
-plt.show()
