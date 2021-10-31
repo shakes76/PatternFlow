4202c1259a4bd0cfc8923107ee922e7ec879bf52
AnqiYan <781236437@qq.com>
2021-10-31 10:01:00 +0800

    Modified DSC function and Added DSC_loss function. Added some comments

--- recognition/s4413599/DSC_FUNCTION.py
+++ recognition/s4413599/DSC_FUNCTION.py
@@ -1,19 +1,29 @@
 import tensorflow as tf
+from tensorflow.keras import backend
 
-
-def DSC(y_pred, y_true):
+def DSC(y_true, y_pred, smooth = 1.):
     '''
+    Function used to calculate dice similarity coefficient
+    :param y_true:
+        actual y value
     :param y_pred:
         Predicted y value
-    :param y_true:
-        Actual y value
+    :param smooth:
     :return:
-        Print the dice coefficient
     '''
-    logical_and = tf.logical_and(y_pred, y_true)
-    logical_and = tf.cast(logical_and, tf.float32)
-    intersection = tf.reduce_sum(logical_and, axis= (1, 2))
-    union = tf.reduce_sum(tf.cast(y_pred, tf.float32), axis= (1, 2)) + tf.reduce_sum(tf.cast(y_true, tf.float32), axis= (1, 2))
-    dice = (2 * intersection)/union
-    dice = tf.reduce_mean(dice)
-    print("THE Dice coefficient is {0:.2f}%".format(dice * 100))
+    # Cast the input true value to the float number for calculation
+    y_true = tf.cast(y_true, tf.float32)
+    # Flatten the true y value
+    y_true = backend.flatten(y_true)
+    # Flatten the predicted y value
+    y_pred = backend.flatten(y_pred)
+    # Get the intersection of the predicted y value and true y value
+    intersection = backend.sum(y_true * y_pred)
+    # Get the intersection of the predicted y value and true y value
+    union = backend.sum(y_true) + backend.sum(y_pred)
+    return (2. * intersection + smooth) / (union + smooth)
+
+def DSC_LOSS(y_true, y_pred):
+    """Function used to Calculate the DSC LOSS"""
+    return 1. - DSC(y_true, y_pred)
+
