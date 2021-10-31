af7a89b34967472e5fdee071793817d6ccc42ce3
Carrick Cai <55621126+CarrickC@users.noreply.github.com>
2021-10-31 18:23:02 +1000

    Add tqdm progressing bar and separate logging for different loss sections to train helper method.

--- recognition/45678044/helper.py
+++ recognition/45678044/helper.py
@@ -8,14 +8,23 @@ import matplotlib.pyplot as plt
 import pytorch_msssim.ssim as cal_ssim
 from tqdm import tqdm
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def train(model, optim, epoch_size, train_loader, valid_loader=None):
-    train_loss = []
-    model.train()
+    train_status = {'total_loss': [], 'reconst_loss': [], 'vq_loss': []}
     
     for epoch in range(epoch_size):
-        epoch_loss = []
+        model.train()
+        total = 0
+        reconst = 0
+        vq = 0
         
-        for batch, (imgs, _) in enumerate(train_loader):
+        train_loop = tqdm(enumerate(train_loader), total=len(train_loader))
+        train_loop.set_description(f"Epoch [{epoch+1}/{epoch_size}]")
+        
+        for batch, imgs in train_loop:
+            imgs = imgs.to(device)
+            
             encoded, decoded, vq_loss = model(imgs)
           
             reconst_loss = F.mse_loss(decoded, imgs)
@@ -26,12 +35,17 @@ def train(model, optim, epoch_size, train_loader, valid_loader=None):
             loss.backward()
             optim.step() 
             
-            epoch_loss.append(loss.detach().cpu().numpy())
+            total += loss.item()
+            reconst += reconst_loss.item()
+            vq += vq_loss.item()
+            
+            train_loop.set_postfix(reconst_loss=reconst/(batch+1))
       
-        train_loss.append(np.mean(epoch_loss))
-        print('Epoch: ', epoch + 1, '| Loss: ', train_loss[-1])
+        train_status['total_loss'].append(total/(batch+1))
+        train_status['reconst_loss'].append(reconst/(batch+1))
+        train_status['vq_loss'].append(vq/(batch+1))
         
-    return train_loss
+    return train_status
     
     
 def test(model, data_loader, optim, epoch_size):
