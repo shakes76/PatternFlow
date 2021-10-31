c317b54130e0526e7e0e784a2e061357860b67f2
Khac Duy Nguyen <khacduy.nguyen@uqconnect.edu.au>
2021-11-01 00:27:58 +0700

    Refactor

--- recognition/s4622574/attention.py
+++ recognition/s4622574/attention.py
@@ -1,12 +1,12 @@
 from tensorflow.keras import layers
 import tensorflow as tf
 
-def MLP_module(attention, queryDim):
-    attention = layers.LayerNormalization()(attention)
+def MLP_module(input, queryDim):
+    input = layers.LayerNormalization()(input)
 
-    outputs = layers.Dense(queryDim, activation=tf.nn.gelu)(attention)
+    result = layers.Dense(queryDim, activation=tf.nn.gelu)(input)
 
-    return layers.Dense(queryDim)(outputs)
+    return layers.Dense(queryDim)(result)
 
 def attention_mechanism(latentDim, inputDim, queryDim):
 
@@ -16,9 +16,13 @@ def attention_mechanism(latentDim, inputDim, queryDim):
     latents = layers.LayerNormalization()(normalizeData)
     normalizedInput = layers.LayerNormalization()(originalSample)
 
-    query = layers.Dense(queryDim)(latents)
-    key = layers.Dense(queryDim)(normalizedInput)
-    value = layers.Dense(queryDim)(normalizedInput)
+    # query = layers.Dense(queryDim)(latents)
+    # key = layers.Dense(queryDim)(normalizedInput)
+    # value = layers.Dense(queryDim)(normalizedInput)
+
+    value = projection(queryDim, normalizedInput)
+    key = projection(queryDim, normalizedInput)
+    query = projection(queryDim, latents)
     
     attention = layers.Attention(use_scale=True)(
         [query, key ,value]
@@ -33,9 +37,15 @@ def attention_mechanism(latentDim, inputDim, queryDim):
     # outputs = layers.Dense(queryDim, activation=tf.nn.gelu)(attention)
 
     # outputs = layers.Dense(queryDim)(outputs)
-    
+
     result = MLP_module(attention, queryDim)
 
     result = layers.Add()([result, attention])
 
-    return tf.keras.Model(inputs=[normalizeData, originalSample], outputs=result)
+    model = tf.keras.Model(inputs=[normalizeData, originalSample], outputs=result)
+
+    return model
+
+def projection(queryDim, input):
+    output = layers.Dense(queryDim)(input)
+    return output
