df16dbb4d133c19f315bf034fa6b19f0c4a4a1ca
kplume <kplume17@gmail.com>
2021-11-01 06:17:50 +1000

    Created model and plotted results of training vs validation

--- recognition/45882999_Improved_UNet/Driver.py
+++ recognition/45882999_Improved_UNet/Driver.py
@@ -2,8 +2,7 @@ import os
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import glob
-import Model
-
+from Model import improved_model
 img_input = "C:/Users/s4588299/Downloads/ISIC2018_Task1-2_Training_Input_x2"
 img_output = "C:/Users/s4588299/Downloads/ISIC2018_Task1_Training_GroundTruth_x2"
 
@@ -51,6 +50,38 @@ def process_path(scan_path, label_path):
     label = tf.cast(label, tf.float32) / 255.0
     label = tf.math.round(label)
     return scan, label
+	
+"""
+Plots the loss at each epoch for the training data vs the validation data
+Parameters:
+	history: The data collected from the model
+"""
+def plot_loss(history):
+    plt.figure(figsize=(10, 5))
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Test vs Validation Loss')
+    plt.legend(loc='upper right')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.savefig("loss.png")
+    plt.show()
+
+"""
+Plots the accuracy at each epoch for the training data vs the validation data
+Parameters:
+	history: The data collected from the model
+"""
+def plot_acc(history):
+    plt.figure(figsize=(10, 5))
+    plt.plot(history.history['accuracy'], label='Training Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Test vs Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.savefig("accuracy.png")
+    plt.show()
 
 #Load data
 inputs = sorted(glob.glob(img_input + "/*.jpg"))
@@ -77,4 +108,12 @@ train_ds = train_ds.map(process_path)
 val_ds = val_ds.map(process_path)
 test_ds = test_ds.map(process_path)
 
-print("YOO")
+def main():
+    
+    model = improved_model()
+    model.compile(optimizer = "adam", loss = "binary_crossentropy", metrics = ["accuracy"])
+    #print(model.summary())
+    history = model.fit(train_ds.batch(16), epochs = 15, validation_data = val_ds.batch(16))
+    plot_loss(history)
+    plot_acc(history)
+main()
