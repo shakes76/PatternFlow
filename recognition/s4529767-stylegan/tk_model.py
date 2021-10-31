386b67626fa2b887ab11a3d951b302314951667a
kamenii <kamenimatematika@gmail.com>
2021-10-31 15:50:48 +1000

    Added StyleBasedGenerator and Discriminator modules. Training parameters updated.

--- recognition/s4529767-stylegan/tk_model.py
+++ recognition/s4529767-stylegan/tk_model.py
@@ -1,15 +1,8 @@
-import math
-
 import torch
 import torch.nn as nn
+import math
 
-# Implementing the network described in paper
-
-# Implementing (b) Style-based generator presented at Figure 1
-# https://miro.medium.com/max/692/0*nzmrf7VMLsTWt8SX
-# from the original StyleGan paper
-
-# "We first map the input to an intermediate latent space W,
+# We first map the input to an intermediate latent space W,
 # which then controls the generator through adaptive
 # instance normalization (AdaIN) at each convolution layer.
 # Gaussian noise is added after each convolution, before
@@ -19,7 +12,9 @@ import torch.nn as nn
 # ers and the synthesis network g consists of 18 layers—two for 22
 # each resolution (4 − 1024). The output of the last layer is
 # converted to RGB using a separate 1 × 1 convolution, similar to
-# Karras et al. [30]. "
+# Karras et al. [30]. Our generator has a total of 26.2M trainable
+# parameters, compared to 23.1M in the traditional generator.
+
 
 def scale(module):
     '''
@@ -70,7 +65,7 @@ class FullyConnected(nn.Module):
     '''
     def __init__(self, in_dim, out_dim):
         super().__init__()
-
+        # we initialize all weights of ..., fully-connected, and affine transform layers using N (0, 1)." [page 9]
         linear_layer = nn.Linear(in_dim, out_dim)
         linear_layer.weight.data.normal_()
         linear_layer.bias.data.zero_()
@@ -88,7 +83,7 @@ class ConvLayer(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__()
         conv2d = nn.Conv2d(*args, **kwargs)
-        # "initialize all weights ... using N(0, 1)" [page 9]
+        # "initialize all weights of the convolutional, ... using N(0, 1)" [page 9]
         conv2d.weight.data.normal_()
         conv2d.bias.data.zero_()
 
@@ -121,10 +116,10 @@ class B(nn.Module):
 
     def __init__(self, channels):
         super().__init__()
-        self.w = nn.Parameter(torch.zeros((1, channels, 1, 1)))
+        self.weight = nn.Parameter(torch.zeros((1, channels, 1, 1)))
 
     def forward(self, noise):
-        return noise * self.w
+        return noise * self.weight
 
 
 class AdaIn(nn.Module):
@@ -140,53 +135,109 @@ class AdaIn(nn.Module):
         self.norm2d = nn.InstanceNorm2d(x)
 
     def forward(self, image, style):
-        factor, bias = style.chunk(2, 1)
-        return self.norm2d(image) * factor + bias
+        # "Learned affine transformations then specialize w to styles y = (ys, yb) that control adaptive instance
+        # normalization (AdaIN) [27, 17, 21, 16] operations after each convolution layer of the synthesis network g."
+        ys, yb = style.chunk(2, 1)
+        return self.norm2d(image) * ys + yb
 
 
 class FirstSynthesisNetworkBlock(nn.Module):
     '''
     The first block of the Synthesis network g with constant input. Start from 4 x 4 x 512
+    "starting the image synthesis from a learned 4 × 4 × 512 constant tensor" [page 2]
     '''
 
     def __init__(self, channels, latent_dim, input_dim):
         super().__init__()
 
+        self.adain = AdaIn(channels)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        # "synthesis network g consists of 18 layers — two for each resolution (4^2 − 1024^2)."
+        self.constant = nn.Parameter(torch.randn(1, channels, input_dim, input_dim))
+        self.style1 = A(latent_dim, channels)  # first style generator in the block (learned affine transform)
+        self.noise1 = scale(B(channels))
+
+        self.conv = ConvLayer(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)  # conv layer
+        self.style2 = A(latent_dim, channels)  # second style generator for the block
+        self.noise2 = scale(B(channels))
+
     def forward(self, w, noise):
-        pass
+        x = self.constant.repeat(noise.shape[0], 1, 1, 1)
+        x = x + self.noise1(noise)
+        x = self.adain(x, self.style1(w))
+        x = self.leaky_relu(x)
+
+        x = self.conv(x)
+        x = x + self.noise2(noise)
+        x = self.adain(x, self.style2(w))
+        x = self.leaky_relu(x)
+
+        return x
+
 
 class SynthesisNetworkBlock(nn.Module):
     '''
-    General "Synthesis Network g" block
+    General Synthesis Network g block
     '''
 
     def __init__(self, in_channels, out_channels, latent_dim):
         super().__init__()
-        pass
+
+        self.adain = AdaIn(out_channels)
+        # "we use leaky ReLU [41] with α = 0.2 and equalized learning rate [30] for all layers"
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        # "synthesis network g consists of 18 layers — two for each resolution (4^2 − 1024^2)."
+        self.conv1 = ConvLayer(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        self.noise1 = scale(B(out_channels))
+        self.style1 = A(latent_dim, out_channels)  # first style generator in the block (learned affine transform)
+
+        self.conv2 = ConvLayer(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        self.noise2 = scale(B(out_channels))
+        self.style2 = A(latent_dim, out_channels)  # second style generator for the block
 
     def forward(self, previous_result, w, noise):
-        pass
+        x = self.conv1(previous_result)
+        x = x + self.noise1(noise)
+        x = self.adain(x, self.style1(w))
+        x = self.leaky_relu(x)
+
+        x = self.conv2(x)
+        x = x + self.noise2(noise)
+        x = self.adain(x, self.style2(w))
+        x = self.leaky_relu(x)
+
+        return x
 
 
 class DiscriminatorBlock(nn.Module):
     '''
-    Progressive Discriminator Convolution Block (consists of two ConvLayer blocks)
+    Progressive Discriminator Convolution Block
     '''
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, layer1_kernel_size, layer1_padding,
+                 layer2_kernel_size, layer2_padding):
         super().__init__()
-        pass
+
+        self.conv = nn.Sequential(
+            ConvLayer(in_channels=in_channels, out_channels=out_channels, kernel_size=layer1_kernel_size,
+                      padding=layer1_padding),
+            nn.LeakyReLU(0.2),
+            ConvLayer(in_channels=out_channels, out_channels=out_channels, kernel_size=layer2_kernel_size,
+                      padding=layer2_padding),
+            nn.LeakyReLU(0.2)
+        )
 
     def forward(self, image):
-        pass
+        return self.conv(image)
 
 
 class MappingNetwork(nn.Module):
     '''
-    Mapping network has number_of_mapping_network_blocks fully connected layers
-    Maps latent z to w.
-    "...the mapping f is implemented using an 8-layer MLP" [page 2]
-
+    Mapping network constists of number_of_mapping_network_blocks fully connected layers. Maps latent z to w.
+    "Our mapping network consists of 8 fully-connected layers, and the dimensionality of all input and output
+    activations — including z and w — is 512"
     '''
 
     def __init__(self, number_of_mapping_network_blocks, latent_dim):
@@ -210,28 +261,120 @@ class StyleBasedGenerator(nn.Module):
 
     def __init__(self, number_of_mapping_network_blocks, latent_dim, input_dim):
         super().__init__()
-        pass
+
+        self.mapping_network = MappingNetwork(number_of_mapping_network_blocks, latent_dim)
+
+        self.synthesis_network = nn.ModuleList([
+            FirstSynthesisNetworkBlock(512, latent_dim, input_dim),
+            SynthesisNetworkBlock(512, 512, latent_dim),
+            SynthesisNetworkBlock(512, 512, latent_dim),
+            SynthesisNetworkBlock(512, 512, latent_dim),
+            SynthesisNetworkBlock(512, 256, latent_dim),
+            SynthesisNetworkBlock(256, 128, latent_dim),
+            SynthesisNetworkBlock(128, 64, latent_dim),
+            SynthesisNetworkBlock(64, 32, latent_dim),
+            SynthesisNetworkBlock(32, 16, latent_dim)
+        ])
+        self.to_rgbs = nn.ModuleList([
+            ConvLayer(512, 3, 1),
+            ConvLayer(512, 3, 1),
+            ConvLayer(512, 3, 1),
+            ConvLayer(512, 3, 1),
+            ConvLayer(256, 3, 1),
+            ConvLayer(128, 3, 1),
+            ConvLayer(64, 3, 1),
+            ConvLayer(32, 3, 1),
+            ConvLayer(16, 3, 1)
+        ])
 
     def forward(self, z,
-                level=0,  # The number of layers in the progressive GAN
-                alpha=-1,
-                noise=None):
+                level,  # The number of layers starting from from 4 x 4
+                alpha,
+                noise):  # truncation param
 
-        pass
+        w = [self.mapping_network(latent) for latent in z]
 
+        x = 0
+
+        for i, conv in enumerate(self.synthesis_network):
+            current_latent = w[0]
+
+            if i > 0 and level > 0:
+                upsample = nn.functional.interpolate(x, scale_factor=2, mode='bilinear',
+                                                            align_corners=False)
+                x = conv(upsample, current_latent, noise[i])
+            else:
+                x = conv(current_latent, noise[i])
+
+            if i == level:
+                x = self.to_rgbs[i](x)
+
+                if i > 0 and 0 <= alpha < 1:
+                    result_prev = self.to_rgbs[i - 1](upsample)
+                    x = alpha * x + (1 - alpha) * result_prev
+
+                break
+
+        return x
 
 class Discriminator(nn.Module):
     '''
-    Discriminator (as seen in classic GANs)
+    Discriminator
     '''
 
     def __init__(self):
         super().__init__()
-        pass
+        self.number_of_layers = 8
+
+        self.discriminator_blocks = nn.ModuleList([
+            DiscriminatorBlock(16, 32, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(32, 64, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(64, 128, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(128, 256, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(256, 512, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(512, 512, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(512, 512, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=3, layer2_padding=1),
+            DiscriminatorBlock(513, 512, layer1_kernel_size=3, layer1_padding=1, layer2_kernel_size=4, layer2_padding=0)
+        ])
+
+        self.from_rgbs = nn.ModuleList([
+            ConvLayer(3, 16, 1),
+            ConvLayer(3, 32, 1),
+            ConvLayer(3, 64, 1),
+            ConvLayer(3, 128, 1),
+            ConvLayer(3, 256, 1),
+            ConvLayer(3, 512, 1),
+            ConvLayer(3, 512, 1),
+            ConvLayer(3, 512, 1)
+        ])
+
+        self.fully_connected = FullyConnected(512, 1)
 
     def forward(self, image,
-                level=0,  # The number of layers in the progressive GAN
-                alpha=-1):
+                level,  # The number of layers starting from from 4 x 4
+                alpha):
 
-        pass
+        for i in range(level, -1, -1):
+            current_layer = self.number_of_layers - i - 1
 
+            if i == level:
+                x = self.from_rgbs[current_layer](image)
+
+            if i == 0:
+                var = x.var(0, unbiased=False) + 1e-10
+                mean_std = torch.sqrt(var).mean().expand(x.size(0), 1, 4, 4)
+                x = torch.cat([x, mean_std], 1)
+
+            # convolution
+            x = self.discriminator_blocks[current_layer](x)
+
+            if i > 0:
+                x = nn.functional.interpolate(x, scale_factor=0.5, mode='bilinear')
+                if i == level and 0 <= alpha < 1:
+                    result_next = self.from_rgbs[current_layer + 1](image)
+                    result_next = nn.functional.interpolate(result_next, scale_factor=0.5, mode='bilinear')
+
+                    x = alpha * x + (1 - alpha) * result_next
+
+        x = x.squeeze(2).squeeze(2)
+        return self.fully_connected(x)
--- recognition/s4529767-stylegan/tk_train.py
+++ recognition/s4529767-stylegan/tk_train.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 import torch
+from torch import optim
+from torchvision import datasets
 
 from tk_model import Discriminator
 from tk_model import StyleBasedGenerator
-
 
 @dataclass
 class ModelState:
@@ -21,35 +22,47 @@ class StyleGanTrainer:
         self.device = torch.device(device)
 
         self.state = ModelState()
-        self.state.progressive_level = 1 # start from 8 x 8, skipping 4 x 4 level
 
-        # We first train discriminator discriminator_cycles times (cycles), then one of the generator,
-        # then again discriminator_cycles of the discriminator, then one generator
-        # discriminator needs a little bit more training so it is not overpowered by the generator
+        # "We implement progressive grow ing the same way as Karras et al. [30], but we start from 8^2
+        # images instead of 4^2"
+        self.state.progressive_level = 1
+
+        # discriminator is trained only at every critic_iteration iterations
+        # We first train discriminator discriminator_iteration cycles, then one generator, and keep repeating that
+        # discriminator needs a little bit more training so it is not overpowered by the generator [Udemy GANs course]
         #TODO do this progressively too?
-        self.critic_iteration = 5
+        self.generator_iteration = 5
 
         # maximum number of progressive levels
         self.max_progressive_level = 7
-        self.start_image_resolution = 4
+        self.start_image_resolution = 4  # we define starting level using state.progressive_level
         self.input_dim = 4
 
         # values provided in the paper
+        # "and the dimensionality of all input and output activations — including z and w — is 512."
         self.z_dim = 512
+
         self.number_of_mapping_network_fc_layers = 8
 
         # input - images folder
-        self.image_folder_path = './images/'
+        self.input_path = './images/'
         # output - trained models folder
-        self.save_folder_path = './models/'
+        self.models_path = './models/'
         # save checkpoint/model
         self.continue_from_previous_checkpoint = True
 
-        self.total_samples = 100000000  # for training
+        # the number of training samples
+        # ... and training length of 150,000 images." [page 9]
+        self.images_per_progress_level = 150000  # for progressing to the next level
+        # "we thus increase the training time from 12M to 25M images" [page 9]
+        self.training_time = 25000000  # total for training
 
         self.gen_losses = []
         self.disc_losses = []
+        self.show_loss_steps = 10
 
+        # "we found that setting the learning rate to 0.002 instead of 0.003 for 512^2 and 1024^2 leads to
+        # better stability" [page 9]
         self.learning_rate = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
         self.default_learning_rate = 0.001
 
@@ -63,6 +76,24 @@ class StyleGanTrainer:
                                         self.input_dim).to(self.device)
         self.discriminator = Discriminator().to(self.device)
 
+        # Optimizers
+        # "We use the learning rate of 10−3, minibatch size of 8, Adam optimizer, and ..."
+        self.gen_optim = optim.Adam([{
+            'params': self.generator.synthesis_network.parameters(),
+            'lr': 0.001
+        }, {
+            'params': self.generator.to_rgbs.parameters(),
+            'lr': 0.001
+        }], lr=0.001, betas=(0.0, 0.99))
+        self.gen_optim.add_param_group({
+            'params': self.generator.mapping_network.parameters(),
+            'lr': 0.001 * 0.01,
+            'mul': 0.01
+        })
+        self.disc_optim = optim.Adam(self.discriminator.parameters(), lr=0.001, betas=(0.0, 0.99))
+        self.dataset = datasets.ImageFolder(self.input_path)
+
+
     def get_images(self, dataset, batch_size, image_size):
         pass
 
@@ -73,13 +104,13 @@ class StyleGanTrainer:
     def train(self):
 
         # Training loop
-        while self.state.used_sample < self.total_samples:
+        while self.state.used_sample < self.training_time:
             self.state.iteration += 1
 
             # -------- Update discriminator, maximise log(D(x)) + log(1 - D(G(z))) ---------
             # TODO
 
-            if self.state.iteration % self.critic_iteration == 0:
+            if self.state.iteration % self.discriminator_iteration == 0:
                 # update generator after training discriminator_cycles of discriminator
 
                 # ------ Update Generator, maximise log(D(G(z))) --------
@@ -94,8 +125,8 @@ class StyleGanTrainer:
         return
 
     def fit(self, continue_from_previous_checkpoint=False, start_point=0):
-        if continue_from_previous_checkpoint:
-            self.load_model(start_point)
+        # if continue_from_previous_checkpoint:
+        #     self.load_model(start_point)
 
         self.state.startpoint = self.state.used_sample
 
