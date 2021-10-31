731fed5d74b76684dbf2dc1908c4003fc3171b41
MujiDipto <m.dipto@uqconnect.edu.au>
2021-10-31 15:40:16 +1000

    completed dice_similarity function in utilities.py

--- recognition/ISIC-ImprovedUnet-44961374/utilities.py
+++ recognition/ISIC-ImprovedUnet-44961374/utilities.py
@@ -2,9 +2,25 @@
 This script contains functions required for dsiplaying images and calculating DSC.
 @author: Mujibul Islam Dipto
 """
+import tensorflow as tf
 
 def display_image():
     pass
 
-def dice_similarity():
-    pass
+def dice_similarity(x, y):
+    """Calculates the Dice Similarity Coefficient between two images, where:
+    DSC = (2 * |x intersection y|) / |x| + |y|
+    More information about DSC can be found here: https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+    Function adapted from: https://medium.com/@karan_jakhar/100-days-of-code-day-7-84e4918cb72c
+    Args:
+        x (numpy array): image 1
+        y (numpy array): image 2
+
+    Returns:
+        float: calculated DSC between these two images
+    """
+    intersection = tf.reduce_sum(x * y) # |x intersection y|
+    numerator = 2 * intersection #  (2 * |x intersection y|)
+    denominator = (tf.reduce_sum(x) + tf.reduce_sum(y)) #  |x| + |y|
+    dsc = numerator / denominator
+    return dsc
