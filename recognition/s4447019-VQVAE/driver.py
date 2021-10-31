e06b58fe7552d26f90ef91739dc388f9facbe30f
Michelle Menesch <m.menesch@uq.net.au>
2021-10-31 21:56:45 +1000

    Added the vector quantizer class based on the Keras tutorial to driver.py. Reference supplied in code comments.

--- recognition/s4447019-VQVAE/driver.py
+++ recognition/s4447019-VQVAE/driver.py
@@ -5,6 +5,7 @@ from tensorflow.keras import layers
 import glob
 import matplotlib.pyplot as plt
 import tensorflow_probability as tfp
+from skimage.metrics import structural_similarity
 
 
 ### PRE-PROCESSING FROM DEMO 2 ################################
@@ -60,6 +61,68 @@ for i in range(9):
 plt.show()
 ###############################################################
 
+
+
+###### VQ from Keras tutorial #################################
+# Vector Quantizer class from Kera's tutorial
+# Reference: https://keras.io/examples/generative/vq_vae/
+
+class VectorQuantizer(layers.Layer):
+    def __init__(self, num_embeddings, embedding_dim, beta=0.25, **kwargs):
+        super().__init__(**kwargs)
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+        self.beta = (beta)
+        # The commitment loss coefficient, beta, is best kept between [0.25, 2]
+        # as per Neural Discrete Representation Learning (page 4)
+
+        # Initialisation of embeddings to be quantised
+        w_init = tf.random_uniform_initializer() #Initialisation of random values
+        self.embeddings = tf.Variable( #Definition of the embeddings tensor variable
+            initial_value=w_init(shape=(self.embedding_dim, self.num_embeddings), dtype="float32"),
+            trainable=True,
+            name="embeddings_vqvae",
+        )
+
+    def call(self, x):
+        # Calculate the input shape of the inputs and
+        # then flatten the inputs keeping `embedding_dim` intact
+        input_shape = tf.shape(x)
+        flattened = tf.reshape(x, [-1, self.embedding_dim])
+
+        # Quantization.
+        encoding_indices = self.get_code_indices(flattened)
+        encodings = tf.one_hot(encoding_indices, self.num_embeddings)
+        quantized = tf.matmul(encodings, self.embeddings, transpose_b=True)
+        quantized = tf.reshape(quantized, input_shape)
+
+        # Calculate vector quantization loss and add that to the layer. You can learn more
+        # about adding losses to different layers here:
+        # https://keras.io/guides/making_new_layers_and_models_via_subclassing/. Check
+        # the original paper to get a handle on the formulation of the loss function.
+        commitment_loss = self.beta * tf.reduce_mean(
+            (tf.stop_gradient(quantized) - x) ** 2
+        )
+        codebook_loss = tf.reduce_mean((quantized - tf.stop_gradient(x)) ** 2)
+        self.add_loss(commitment_loss + codebook_loss)
+
+        # Straight-through estimator.
+        quantized = x + tf.stop_gradient(quantized - x)
+        return quantized
+
+    def get_code_indices(self, flattened_inputs):
+        # Calculate L2-normalized distance between the inputs and the codes.
+        similarity = tf.matmul(flattened_inputs, self.embeddings)
+        distances = (
+            tf.reduce_sum(flattened_inputs ** 2, axis=1, keepdims=True)
+            + tf.reduce_sum(self.embeddings ** 2, axis=0)
+            - 2 * similarity
+        )
+
+        # Derive the indices for minimum distances.
+        encoding_indices = tf.argmin(distances, axis=1)
+        return encoding_indices
+###############################################################
 
 
 
