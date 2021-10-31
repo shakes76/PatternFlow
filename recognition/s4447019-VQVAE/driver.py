b8abb57d11358282a77ca27d74461d24d79827d3
Michelle Menesch <m.menesch@uq.net.au>
2021-10-31 14:35:52 +1000

    Added dependencies/libraries to driver.py and model.py files

--- recognition/s4447019-VQVAE/driver.py
+++ recognition/s4447019-VQVAE/driver.py
@@ -1,1 +1,7 @@
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import glob
+import matplotlib.pyplot as plt
+import tensorflow_probability as tfp
