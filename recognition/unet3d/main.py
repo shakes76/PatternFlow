198f37c279c434c5dbade09523bdd4f1760e473e
xiaofangchen <xiaofang.chen@thoughtworks.com>
2021-11-01 04:03:09 +0800

    improve code design and documentation

--- recognition/unet3d/main.py
+++ recognition/unet3d/main.py
@@ -1,117 +1,86 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+# customized functions
 from methods import *
 from model import *
-import tensorflow as tf
-gpus = tf.config.experimental.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(gpus[0], True)
-
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
-
-
-import matplotlib.pyplot as plt
-
-class DataGenerator(tf.keras.utils.Sequence):
-    'Generates data for Keras'
-    def __init__(self, list_IDs, labels=None, batch_size=1, dim=(256,256,128), n_channels=1,
-                 n_classes=None, shuffle=False):
-        'Initialization'
-        self.dim = dim
-        self.batch_size = batch_size
-        self.labels = labels
-        self.list_IDs = list_IDs
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.shuffle = shuffle
-        self.on_epoch_end()
-
-    def __len__(self):
-        'Denotes the number of batches per epoch'
-        return int(tf.floor(len(self.list_IDs) / self.batch_size))
-
-    def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
-        # Find list of IDs
-        list_IDs_temp = [self.list_IDs[k] for k in indexes]
-
-        # Generate data
-        X, y = self.__data_generation(list_IDs_temp)
-
-        return X, y
-
-    def on_epoch_end(self):
-        seed = 42
-        tf.random.set_seed(seed)
-        'Updates indexes after each epoch'
-        self.indexes = tf.range(len(self.list_IDs))
-        if self.shuffle == True:
-            self.indexes = tf.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
-        # Initialization
-        # X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        X = []
-        Y = []
-
-        # Generate data
-        for i, ID in enumerate(list_IDs_temp):
-            
-            # Store mri case
-            x = process_nii('./dataset/semantic_MRs_anon/' + ID + '_LFOV.nii.gz', norma=True)
-            X.append(x)
-
-
-            # Store mri label
-            y = process_nii('./dataset/semantic_labels_anon/' + ID + '_SEMANTIC_LFOV.nii.gz')
-            Y.append(y)
-
-        X = tf.stack(X)
-        Y = tf.stack(y)
-        return X, Y
 
 
 if __name__ == "__main__":
-    target_dir = "./dataset/semantic_MRs_anon"
-    train_list, val_list, test_list = train_val_test_split(target_dir)
-        
 
-    # Generators
-    train_generator = DataGenerator(train_list[6:])
+    # only uncomment this line the first time you run the main.py
+    # images with irregular shape will be resize and save to replace 
+    # resize_and_replace_volumes()
+
+    # images are splited into tarin/val/test sets on patient basis 
+    target_dir = "./unet3d/dataset/semantic_MRs_anon"
+    train_list, val_list, test_list = train_val_test_split(target_dir)
+
+
+    # create 3d Generators for model inputs
+    train_generator = DataGenerator(train_list)
     validation_generator = DataGenerator(val_list)
     test_generator = DataGenerator(test_list)
     print(len(train_generator), len(validation_generator), len(test_generator))
 
+    # initialize an instance of 3d unet model or load  exiting Model
     model = Unet_3d()
-
-    learning_rate = 0.001
-    epochs = 1
-    decay_rate = 0.0000001
-    model.compile(optimizer=tf.keras.optimizers.Adam(lr=learning_rate, decay = decay_rate), loss="sparse_categorical_crossentropy", metrics=[dice_coef])
+    # model = tf.keras.models.load_model('./unet3d/results/models/model.h5', compile=False)
+ 
+    # model configuration: try different optimizer, learning rate, loss .....
+    # model.compile(optimizer=tf.keras.optimizers.Adam(lr=learning_rate, decay = decay_rate), loss="sparse_categorical_crossentropy", metrics=[dice_coef])
+    model.compile(
+    optimizer=tf.keras.optimizers.Adam(lr=0.001, decay = 0.0000001),
+    loss="sparse_categorical_crossentropy",
+    metrics=dice_coef,
+    )
     print(model.summary())
-    # model.compile(
-    # optimizer='sgd',
-    # loss=dice_coefficient,
-    # metrics=dice_coefficient,
-    # )
 
-    history = model.fit(train_generator, epochs=15, validation_data=validation_generator,shuffle=False)
-    model.save("./results/models/unet1")
+    history = model.fit(train_generator, epochs=10, validation_data=validation_generator, shuffle=True)
+
+    model_name = "unet_aug1"
+    model.save("./unet3d/results/models/" + model_name + ".h5")
 
     _ = plt.plot(history.history['loss'])
     _ = plt.plot(history.history['val_loss'])
-    _ = plt.title('Train & Validation loss')
+    _ = plt.title('Train & Validation Loss')
     _ = plt.ylabel('loss')
     _ = plt.xlabel('epoch')
     _ = plt.legend(['train', 'validation'], loc='upper right')
-    plt.savefig('./results/images/unet1_loss.png')
+    plt.show()
+    plt.savefig('./unet3d/results/images/' + model_name + '_loss.png')
+
 
     test_results = model.evaluate(test_generator, verbose=0)
+    print("############ Test loss:", test_results[0], "########### Test ")
+    print(model.metrics_names)
     print(test_results)
+
+
+    # compare prediction with groud truth
+    # take one case from test sets
+    x, y = test_generator[0]
+    y_pred = model.predict(x, batch_size=1)
+    print("y_pred: ", y_pred.shape)
+
+    y_pred = tf.argmax(y_pred, axis=-1)
+    print("y_pred: ", y_pred.shape)
+
+    for i in [0, 32, 64, 127]:
+    # for i in [64,]:
+        # prediction
+        _ = fig, (ax1, ax2) = plt.subplots(1,2, figsize = (10, 6))
+        _ = ax1.imshow(y[0,:,:,i])
+        _ = ax1.set_title('True label')
+
+        # True lablel
+        _ = ax2.imshow(y_pred[0,:,:,i])
+        _ = ax2.set_title('Predicted label')
+        plt.savefig('./unet3d/results/images/' + "The " + str(i+1) + 'th slice of' + model_name + '.png')
+
 
 
 
--- recognition/unet3d/methods.py
+++ recognition/unet3d/methods.py
@@ -1,7 +1,6 @@
 import os
 import re
 import nibabel as nib
-# from matplotlib import pyplot as plt
 from scipy import ndimage
 import tensorflow as tf
 
@@ -11,7 +10,6 @@ def read_filename_from(directory):
     """"
     Read image names from target directory and sort by case no. then week no.
     """
-
     files = os.listdir(directory)
     files = [fname[:-12] for fname in files if fname.endswith('nii.gz')]
     files.sort(key=lambda var:[int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
@@ -21,13 +19,11 @@ def read_filename_from(directory):
 
 def train_val_test_split(directory):
     """
-    Classify images into dictionary by case number
-
-    eg. {"case_004":['Case_004_Week0_LFOV.nii.gz', 'Case_004_Week1_LFOV.nii.gz', 'Case_004_Week2_LFOV.nii.gz'], 
-    "case_002": [...]}
+    Split images into train/validatiion/test sets on paticient basis
     """
     files = read_filename_from(directory)
     case_dict = {}
+    # classify images by paticient into dict
     for f in files:
         if f[:8] not in case_dict:
             case_dict[f[:8]] = [f]
@@ -38,11 +34,14 @@ def train_val_test_split(directory):
     test = []
     for key, cases in case_dict.items():
         num = len(cases)
+        # if one patient has only one image then allocate to training set
         if num == 1:
             train.append(cases[0])
+        # if one patient has two images then allocate to training set and val set
         if num == 2:
             train.append(cases[0])
             val.append(cases[1])
+        # if one patient has more two  images then allocate one to val set and one to test set, and the rest allocated to training set.
         if num >=3:
             train += cases[:-2]
             val.append(cases[-2])
@@ -51,35 +50,38 @@ def train_val_test_split(directory):
     return train, val, test
 
 def get_filepath_from(directory):
-    """Get complete file paths for each set"""
+    """
+    Get complete file paths for each set
+    """
 
     train, val, test = train_val_test_split(directory)
 
     return [os.path.join(directory, fname) for fname in train], [os.path.join(directory, fname) for fname in val], [os.path.join(directory, fname) for fname in test]
             
 
-# def read_nii_file(path):
-
-#     # read nii image as numpy data
-#     return nib.load(path).get_fdata()
 
 def read_nii_file(path):
-
-    # read nii image as numpy data
+    """
+    read nii image as numpy data
+    """
     return nib.load(path).get_fdata()
 
     
 def normalize(volume):
-    """Interest area"""
-
+    """
+    Scale and normalize img data
+    """
     min, max = 0, 500
+    # min-max scalling
     volume[volume < min] = min
     volume[volume > max] = max
     volume = (volume-min)/(max-min)
     return volume
 
 def resize_volume(img):
-    """Resize across z-axis"""
+    """
+    Resize img to desired shape
+    """
     desired_shape = (256, 256, 128)
     
     if img.shape == desired_shape:
@@ -97,13 +99,99 @@ def resize_volume(img):
         img = ndimage.zoom(img.astype(int), (width_factor, height_factor, depth_factor), order=1)
     return img
 
+def resize_and_replace_volumes(imgs=[ "datasets/semantic_labels_anon/Case_019_Week1_SEMANTIC_LFOV.nii.gz", "datasets/HipMRI_study_complete_release_v1/semantic_MRs_anon/Case_019_Week1_LFOV.nii.gz"]):
+     """
+     Resize irregular img and replace the origin
+     """
+     for img_path in imgs:
+        img = nib.load(img_path)
+        data = img.get_fdata()
+        if data.shape != (256, 256, 128):
+            # resize volume to desired shape
+            resized_data = resize_volume(data)
+            # convert image to NIfTI1 format 
+            resized_img = nib.Nifti1Image(resized_data.astype(int), img.affine, img.header)
+            resized_img.header.get_data_shape()
+            # save and replace the orign
+            nib.save(resized_img, img_path)
+
+        
 def process_nii(path, norma = False):
+    """
+    Read nii.gz file; normalize data; apply agumentation; add channel with size 1;
+    """
     volume = read_nii_file(path)
     if norma:
         volume = normalize(volume)
     # volume = resize_volume(volume)
+    volume = tf.image.random_flip_up_down(volume, 3)
     volume = tf.expand_dims(volume, axis=3)
     return volume
+
+# Ref: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+class DataGenerator(tf.keras.utils.Sequence):
+    """
+    3D DataGenerator inherit from keras
+    """
+    
+    def __init__(self, list_IDs, labels=None, batch_size=1, dim=(256,256,128), n_channels=1,
+                 n_classes=None, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(tf.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # List of IDs (name of imgs)
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        seed = 42
+        tf.random.set_seed(seed)
+        'Updates indexes after each epoch'
+        self.indexes = tf.range(len(self.list_IDs))
+        if self.shuffle == True:
+            self.indexes = tf.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        # X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        X = []
+        Y = []
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            
+            # Store mri case
+            x = process_nii('./unet3d/dataset/semantic_MRs_anon/' + ID + '_LFOV.nii.gz', norma=True)
+            X.append(x)
+
+            # Store mri label
+            y = process_nii('./unet3d/dataset/semantic_labels_anon/' + ID + '_SEMANTIC_LFOV.nii.gz')
+            Y.append(y)
+
+        X = tf.stack(X)
+        Y = tf.stack(Y)
+        return X, Y
 
 
 
