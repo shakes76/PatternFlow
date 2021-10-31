9bfbf46af9190f960f6dd10833ecb7d04c7569f6
yexincheng <50694493+yexincheng@users.noreply.github.com>
2021-11-01 08:58:56 +1000

    comments on train.py

--- recognition/45662959_XinchengYE_StyleGAN/train.py
+++ recognition/45662959_XinchengYE_StyleGAN/train.py
@@ -1,8 +1,11 @@
+"""
+:papram  path: a path of a checkpoint, this is obligatory
+:param
+"""
 from io import BytesIO
 import lmdb
 from PIL import Image
 import matplotlib.pyplot as plt
-from math import sqrt
 import os
 import argparse
 import random
@@ -10,7 +13,6 @@ import math
 from tqdm import tqdm
 
 import torch
-from torch import nn
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.nn import init
@@ -19,6 +21,8 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms, utils
 from torch.autograd import Variable, grad
 from torch.autograd import Function
+from model import StyledGenerator, Discriminator
+import time
 
 
 class MultiResolutionDataset(Dataset):
@@ -56,15 +60,18 @@ class MultiResolutionDataset(Dataset):
         return img
 
 
-def sample_data(dataset, batch_size, image_size=4):
-    dataset.resolution = image_size
+def sample_data(dataset, batch_size, resolution=4):
+    """
+    Get certain images with input resolution from the dataset.
+    :param dataset: the whole dataset
+    :param batch_size
+    :param resolution
+    :return: the dataset only containing images with input resolution
+    """
+    dataset.resolution = resolution
     loader = DataLoader(dataset, shuffle=True, batch_size=batch_size, num_workers=2, drop_last=True)
 
     return loader
-
-
-from model import StyledGenerator, Discriminator
-import time
 
 
 def requires_grad(model, flag=True):
@@ -73,6 +80,13 @@ def requires_grad(model, flag=True):
 
 
 def accumulate(model1, model2, decay=0.999):
+    """
+    Exponential Moving Average (EMA) of generator, can improve the robustness
+    model1_{t} = decay * model_{t-1} + (1-decay) * model2_{t}
+    :param model1: g_running, a shadow generator which will not participate in training directly
+    :param model2: generator.module, a real generator used in training
+    :param decay: weight, usually [0.9, 0.999]
+    """
     par1 = dict(model1.named_parameters())
     par2 = dict(model2.named_parameters())
 
@@ -87,18 +101,23 @@ def adjust_lr(optimizer, lr):
 
 
 def train(args, dataset, generator, discriminator):
+    """
+    Main train loop.
+    :param args:
+    :param dataset: the whole dataset
+    :param generator
+    :param discriminator
+    """
     print('Starting training loop...')
-    step = int(math.log2(args.init_size)) - 2  # 8:1,16:2 32:3 64:4 128:5 256:6
+    step = int(math.log2(args.init_size)) - 2   # training resolution 8:1,16:2 32:3 64:4 128:5 256:6
     resolution = 4 * 2 ** step
-    loader = sample_data(
-        dataset, args.batch.get(resolution, args.batch_default), resolution
-    )
+    loader = sample_data(dataset, args.batch.get(resolution, args.batch_default), resolution)
     data_loader = iter(loader)
 
     adjust_lr(g_optimizer, args.lr.get(resolution, 0.001))
     adjust_lr(d_optimizer, args.lr.get(resolution, 0.001))
 
-    pbar = tqdm(range(3_000_000))
+    progress_bar = tqdm(range(3_000_000))   # total epochs over all resolutions
 
     requires_grad(generator, False)
     requires_grad(discriminator, True)
@@ -107,7 +126,7 @@ def train(args, dataset, generator, discriminator):
     gen_loss_val = 0
     grad_loss_val = 0
 
-    alpha = 0
+    alpha = 0   # interpolation between previous resolutions and new (larger) resolutions
     used_sample = 0
 
     max_step = int(math.log2(args.max_size)) - 2
@@ -115,7 +134,7 @@ def train(args, dataset, generator, discriminator):
     t0 = time.time()
     train_time = []
 
-    for i in pbar:
+    for i in progress_bar:
         discriminator.zero_grad()
 
         alpha = min(1, 1 / args.phase * (used_sample + 1))
@@ -143,7 +162,7 @@ def train(args, dataset, generator, discriminator):
             )
             data_loader = iter(loader)
             t = time.time() - t0
-            print('running time', t)
+            print('\nrunning time', t)
             train_time.append(t)
             t0 = time.time()
             torch.save(
@@ -172,11 +191,13 @@ def train(args, dataset, generator, discriminator):
         b_size = real_image.size(0)
         real_image = real_image.cuda()
 
-        if args.loss == 'wgan-gp':
-            real_predict = discriminator(real_image, step=step, alpha=alpha)
-            real_predict = real_predict.mean() - 0.001 * (real_predict ** 2).mean()
-            (-real_predict).backward()
+        # calculate wgan-gp loss of discriminator to classify a real image
+        real_predict = discriminator(real_image, step=step, alpha=alpha)
+        real_predict = real_predict.mean() - 0.001 * (real_predict ** 2).mean()
+        (-real_predict).backward()
 
+        # (F) mixing regularization to localize the style
+        # use more than 1 random latent codes for generation
         if args.mixing and random.random() < 0.9:
             gen_in11, gen_in12, gen_in21, gen_in22 = torch.randn(
                 4, b_size, code_size, device='cuda'
@@ -191,31 +212,27 @@ def train(args, dataset, generator, discriminator):
             gen_in1 = gen_in1.squeeze(0)
             gen_in2 = gen_in2.squeeze(0)
 
+        # generate a fake image
         fake_image = generator(gen_in1, step=step, alpha=alpha)
         fake_predict = discriminator(fake_image, step=step, alpha=alpha)
 
-        if args.loss == 'wgan-gp':
-            fake_predict = fake_predict.mean()
-            fake_predict.backward()
-
-            eps = torch.rand(b_size, 1, 1, 1).cuda()
-            x_hat = eps * real_image.data + (1 - eps) * fake_image.data
-            x_hat.requires_grad = True
-            hat_predict = discriminator(x_hat, step=step, alpha=alpha)
-            grad_x_hat = grad(
-                outputs=hat_predict.sum(), inputs=x_hat, create_graph=True
-            )[0]
-            grad_penalty = (
-                    (grad_x_hat.view(grad_x_hat.size(0), -1).norm(2, dim=1) - 1) ** 2
-            ).mean()
-            grad_penalty = 10 * grad_penalty
-            grad_penalty.backward()
-            if i % 10 == 0:
-                grad_loss_val = grad_penalty.item()
-                disc_loss_val = (-real_predict + fake_predict).item()
-
+        # calculate wgan-gp loss of discriminator to classify a fake image and backpropagation
+        fake_predict = fake_predict.mean()
+        fake_predict.backward()
+        eps = torch.rand(b_size, 1, 1, 1).cuda()
+        x_hat = eps * real_image.data + (1 - eps) * fake_image.data
+        x_hat.requires_grad = True
+        hat_predict = discriminator(x_hat, step=step, alpha=alpha)
+        grad_x_hat = grad(outputs=hat_predict.sum(), inputs=x_hat, create_graph=True)[0]
+        grad_penalty = ((grad_x_hat.view(grad_x_hat.size(0), -1).norm(2, dim=1) - 1) ** 2).mean()
+        grad_penalty = 10 * grad_penalty
+        grad_penalty.backward()
+        if i % 10 == 0:
+            grad_loss_val = grad_penalty.item()
+            disc_loss_val = (-real_predict + fake_predict).item()
         d_optimizer.step()
 
+        # calculate the loss of generator and backpropagation
         if (i + 1) % n_critic == 0:
             generator.zero_grad()
 
@@ -223,11 +240,9 @@ def train(args, dataset, generator, discriminator):
             requires_grad(discriminator, False)
 
             fake_image = generator(gen_in2, step=step, alpha=alpha)
-
             predict = discriminator(fake_image, step=step, alpha=alpha)
 
-            if args.loss == 'wgan-gp':
-                loss = -predict.mean()
+            loss = -predict.mean()
 
             if i % 10 == 0:
                 gen_loss_val = loss.item()
@@ -244,7 +259,6 @@ def train(args, dataset, generator, discriminator):
 
         if (i + 1) % 1000 == 0:
             images = []
-
             gen_i, gen_j = args.gen_sample.get(resolution, (10, 5))
 
             with torch.no_grad():
@@ -254,7 +268,6 @@ def train(args, dataset, generator, discriminator):
                             torch.randn(gen_j, code_size).cuda(), step=step, alpha=alpha
                         ).data.cpu()
                     )
-
             utils.save_image(
                 torch.cat(images, 0),
                 f'sample/{str(i + 1).zfill(6)}.png',
@@ -262,6 +275,7 @@ def train(args, dataset, generator, discriminator):
                 normalize=True,
                 range=(-1, 1),
             )
+
         if not os.path.exists('checkpoint'):
             os.mkdir('checkpoint')
 
@@ -275,13 +289,13 @@ def train(args, dataset, generator, discriminator):
             f'G: {gen_loss_val:.3f}; D: {disc_loss_val:.3f}; Grad: {grad_loss_val:.3f}; Alpha: {alpha:.5f}'
         )
 
-        pbar.set_description(state_msg)
+        progress_bar.set_description(state_msg)
 
 
 if __name__ == '__main__':
     code_size = 512
-    batch_size = 16
-    n_critic = 1
+    n_critic = 1    # the number of critic (discriminator) iterations per generator iteration
+                    # In WGAN-PG paper, they use n_critic = 5
 
     parser = argparse.ArgumentParser(description='train StyleGAN')
 
@@ -289,34 +303,26 @@ if __name__ == '__main__':
     parser.add_argument(
         '--phase',
         type=int,
-        default=600_000,
+        default=600_000,    # phase should be large enough, it also controls alpha, otherwise generator will be unstable
         help='number of samples used for each training phases',
     )
     parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
-    parser.add_argument('--sched', action='store_true', help='use lr scheduling')
-    parser.add_argument('--init_size', default=125, type=int, help='initial image size')
+    parser.add_argument('--sched',
+                        action='store_true',
+                        help='use lr and batch size scheduling for different resolutions')
+    parser.add_argument('--init_size', default=8, type=int, help='initial image size')
     parser.add_argument('--max_size', default=256, type=int, help='max image size')
-    parser.add_argument(
-        '--ckpt', default=None, type=str, help='load from previous checkpoints'
-    )
+    parser.add_argument('--ckpt', default=None, type=str, help='load from previous checkpoints')
     parser.add_argument(
         '--no_from_rgb_activate',
         action='store_true',
         help='use activate in from_rgb (original implementation)',
     )
-    parser.add_argument(
-        '--mixing', action='store_true', help='use mixing regularization'
-    )
-    parser.add_argument(
-        '--loss',
-        type=str,
-        default='wgan-gp',
-        help='class of gan loss',
-    )
-
+    parser.add_argument('--mixing', action='store_true', help='use mixing regularization')
     args = parser.parse_args()
     # args.path = "AKOA_PRE"
     # args.ckpt = 'checkpoint/train_step-7'
+
     generator = nn.DataParallel(StyledGenerator(code_size)).cuda()
     discriminator = nn.DataParallel(
         Discriminator(from_rgb_activate=not args.no_from_rgb_activate)
@@ -338,7 +344,6 @@ if __name__ == '__main__':
 
     accumulate(g_running, generator.module, 0)
 
-    # args.ckpt = 'checkpoint/train_step-7.model'
     if args.ckpt is not None:
         print('Loading checkpoint...')
         ckpt = torch.load(args.ckpt)
@@ -358,7 +363,6 @@ if __name__ == '__main__':
     )
 
     dataset = MultiResolutionDataset(args.path, transform)
-    args.sched = True
 
     if args.sched:
         args.lr = {128: 0.0015, 256: 0.002, 512: 0.003, 1024: 0.003}
@@ -373,3 +377,5 @@ if __name__ == '__main__':
     args.batch_default = 32
 
     train(args, dataset, generator, discriminator)
+    print('Completed training on all resolutions')
+
