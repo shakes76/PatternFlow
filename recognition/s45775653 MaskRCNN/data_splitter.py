a3d42011b5aa8bf3ba2dc181b678b16af47d8aa1
Christian <christian.burbon@gmail.com>
2021-10-31 14:21:56 +1000

    Update data_splitter.py
    Move running the datasplitter function to Test Driver - Mask RCNN

--- recognition/s45775653 MaskRCNN/data_splitter.py
+++ recognition/s45775653 MaskRCNN/data_splitter.py
@@ -31,11 +31,12 @@ def file_cleaner(save_path):
         for file in files:
             os.remove(os.path.join(root, file))
 
-def data_augmenter(dataset,load_train_path,save_train_path,load_seg_path,save_seg_path,resize):
+def data_augmenter(dataset,load_train_path,save_train_path,load_seg_path,save_seg_path,max_res=700*700,resize):
     
     # generate augmenters
     seq_h = iaa.Sequential([iaa.Fliplr(1)])
     seq_v = iaa.Sequential([iaa.Flipud(1)])
+    seq_hv = iaa.Sequential([iaa.Fliplr(1),iaa.Flipud(1)])
 
     for x in dataset:
         
@@ -49,12 +50,12 @@ def data_augmenter(dataset,load_train_path,save_train_path,load_seg_path,save_se
         else:
             # 700*700 is the known max resolution local GPU can handle during training
             # when less than max size, retain resolution
-            if (700*700) <= x_img.shape[0]*x_img.shape[1]:
+            if max_res <= x_img.shape[0]*x_img.shape[1]:
                 img_w = int(x_img.shape[1] * resize)
                 img_h = int(x_img.shape[0] * resize)
             
             # when more than max size, decrease resolution
-            elif (700*700) > x_img.shape[0]*x_img.shape[1]:
+            elif max_res > x_img.shape[0]*x_img.shape[1]:
                 img_w = int(x_img.shape[1] * 1)
                 img_h = int(x_img.shape[0] * 1)
         
@@ -66,7 +67,7 @@ def data_augmenter(dataset,load_train_path,save_train_path,load_seg_path,save_se
         
         # flip image vertically
         x_img_v = seq_v(image=x_img)
-        
+          
         # save images
         cv2.imwrite(os.path.join(save_train_path,x),x_img)
         cv2.imwrite(os.path.join(save_train_path,x[:-4]+'_h.jpg'),x_img_h)
@@ -74,37 +75,36 @@ def data_augmenter(dataset,load_train_path,save_train_path,load_seg_path,save_se
         
         # DO training ground truth
         img_file = glob.glob(os.path.join(load_seg_path,x[:-4]+"*"))
-        x_img = cv2.imread(img_file[0])
+        seg_img = cv2.imread(img_file[0])
         
         if resize == 1:
-            img_w = int(x_img.shape[1] * 1)
-            img_h = int(x_img.shape[0] * 1)
+            img_w = int(seg_img.shape[1] * 1)
+            img_h = int(seg_img.shape[0] * 1)
         else:
             # 700*700 is the known max resolution local GPU can handle during training
             # when less than max size, retain resolution
-            if (700*700) <= x_img.shape[0]*x_img.shape[1]:
-                img_w = int(x_img.shape[1] * resize)
-                img_h = int(x_img.shape[0] * resize)
+            if max_res <= seg_img.shape[0]*seg_img.shape[1]:
+                img_w = int(seg_img.shape[1] * resize)
+                img_h = int(seg_img.shape[0] * resize)
             
             # when more than max size, decrease resolution
-            elif (700*700) > x_img.shape[0]*x_img.shape[1]:
-                img_w = int(x_img.shape[1] * 1)
-                img_h = int(x_img.shape[0] * 1)
+            elif max_res> seg_img.shape[0]*seg_img.shape[1]:
+                img_w = int(seg_img.shape[1] * 1)
+                img_h = int(seg_img.shape[0] * 1)
         
         # resize the image
-        x_img = cv2.resize(x_img,(img_w,img_h))
+        seg_img = cv2.resize(seg_img,(img_w,img_h))
             
         # flip image horizontally
-        x_img_h = seq_h(image=x_img)
+        x_seg_h = seq_h(image=seg_img)
         
         # flip image vertically
-        x_img_v = seq_v(image=x_img)
-        
+        x_seg_v = seq_v(image=seg_img)
+
         # save images
         cv2.imwrite(os.path.join(save_seg_path,x),x_img)
-        cv2.imwrite(os.path.join(save_seg_path,x[:-4]+'_h.png'),x_img_h)
-        cv2.imwrite(os.path.join(save_seg_path,x[:-4]+'_v.png'),x_img_v)
-
+        cv2.imwrite(os.path.join(save_seg_path,x[:-4]+'_h.png'),x_seg_h)
+        cv2.imwrite(os.path.join(save_seg_path,x[:-4]+'_v.png'),x_seg_v)
 #%%
 def datasplitter(load_train_path,load_seg_path,
                  save_train_path,save_seg_train_path,
@@ -147,7 +147,7 @@ def datasplitter(load_train_path,load_seg_path,
     # TEST SET
     
     file_cleaner(save_test_path) # clean the train image save path
-    file_cleaner(save_seg_test_path # clean the train mask image save path   
+    file_cleaner(save_seg_test_path) # clean the train mask image save path   
     data_augmenter(X_test,load_train_path,save_test_path,load_seg_path,save_seg_test_path,resize) # DO augmentation
         
     print('Running Test - Full Image')
@@ -165,43 +165,3 @@ def datasplitter(load_train_path,load_seg_path,
         x_seg = cv2.imread(seg_file[0])
         cv2.imwrite(os.path.join(save_seg_test_full_path,seg_file[0][-29:]),x_seg)
         
-#%%    
-    
-train_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1-2_Training_Input_x2'
-seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/ISIC2018_Task1_Training_GroundTruth_x2'
-train_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/train_img'
-train_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/train_seg'
-val_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/val_img'
-val_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/val_seg'
-test_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/test_img'
-test_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/test_seg'
-test_img_full_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/test_img_full'
-test_seg_full_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/test_seg_full'
-
-
-# Train Set
-datasplitter(train_path,seg_path,
-             train_img_path,train_seg_path,
-             val_img_path,val_seg_path,
-             test_img_path,test_seg_path, 
-             test_img_full_path,test_seg_full_path, 
-             resize=0.25)
-
-print('Done')
-
-#%%
-
-# samp_train_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_train_img'
-# samp_train_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_train_seg'
-# samp_val_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_val_img'
-# samp_val_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_val_seg'
-# samp_test_img_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_test_img'
-# samp_test_seg_path = 'D:/UQ Data Science/Subjects/Semester 4/COMP3710 - Pattern Recognition/Final Report/ISIC2018_Task1-2_Training_Data/Data Split/samp_test_seg'
-
-
-# # Train Set
-# datasplitter(train_path,seg_path,
-#              samp_train_img_path,samp_train_seg_path,
-#              samp_val_img_path,samp_val_seg_path,
-#              samp_test_img_path,samp_test_seg_path,
-#              resize=0.25)
