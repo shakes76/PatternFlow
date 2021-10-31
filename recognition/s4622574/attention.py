1355b838f0493220d61416b311499b444ed7b0f1
Khac Duy Nguyen <khacduy.nguyen@uqconnect.edu.au>
2021-10-31 22:35:10 +0700

    attention - Feed into MLP block

--- recognition/s4622574/attention.py
+++ recognition/s4622574/attention.py
@@ -1,30 +1,41 @@
 from tensorflow.keras import layers
-import tensorflow as tf 
+import tensorflow as tf
+
+def MLP_module(attention, queryDim):
+    attention = layers.LayerNormalization()(attention)
+
+    outputs = layers.Dense(queryDim, activation=tf.nn.gelu)(attention)
+
+    return layers.Dense(queryDim)(outputs)
 
 def attention_mechanism(latentDim, inputDim, queryDim):
 
-    normalizeData = layers.Input((latentDim, queryDim))
+    normalizeData = layers.Input(shape=(latentDim, queryDim))
+    originalSample = layers.Input(shape=(inputDim, queryDim))
+    
     latents = layers.LayerNormalization()(normalizeData)
-
-    originalSample = layers.Input((inputDim, queryDim))
     normalizedInput = layers.LayerNormalization()(originalSample)
 
     query = layers.Dense(queryDim)(latents)
     key = layers.Dense(queryDim)(normalizedInput)
     value = layers.Dense(queryDim)(normalizedInput)
     
-    attention = layers.Attention(use_scale=True)([query, key ,value])
+    attention = layers.Attention(use_scale=True)(
+        [query, key ,value]
+    )
     
     attention = layers.Dense(queryDim)(attention)
 
     attention = layers.Add()([attention, latents])
 
-    attention = layers.LayerNormalization()(attention)
+    # attention = layers.LayerNormalization()(attention)
 
-    outputs = layers.Dense(queryDim, activation=tf.nn.gelu)(attention)
+    # outputs = layers.Dense(queryDim, activation=tf.nn.gelu)(attention)
 
-    outputs = layers.Dense(queryDim)(outputs)
+    # outputs = layers.Dense(queryDim)(outputs)
+    
+    result = MLP_module(attention, queryDim)
 
-    outputs = layers.Add()([outputs, attention])
+    result = layers.Add()([result, attention])
 
-    return tf.keras.Model(inputs=[normalizeData, originalSample], outputs=outputs)
+    return tf.keras.Model(inputs=[normalizeData, originalSample], outputs=result)
