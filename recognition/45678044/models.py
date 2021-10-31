7ea489183b785d5864c57866431bc3796184f299
Carrick Cai <55621126+CarrickC@users.noreply.github.com>
2021-10-31 17:16:39 +1000

    Correct the implementation of vector quantization

--- recognition/45678044/models.py
+++ recognition/45678044/models.py
@@ -27,16 +27,19 @@ class VQVAE(nn.Module):
         
     def vector_quantize(self, z_e):
         z_e = z_e.permute(0, 2, 3, 1).contiguous()
+        z_e_shape = z_e.shape
+
+        flat_z_e = z_e.view(-1, self.D)
         
-        distances = torch.abs(
-            torch.sum(z_e.view(-1, self.D).pow(2), dim=1, keepdim=True) - 
-            torch.sum(self.codebook.weight.pow(2), dim=1))
+        distances = (torch.sum(flat_z_e**2, dim=1, keepdim=True) 
+                    + torch.sum(self.codebook.weight**2, dim=1)
+                    - 2 * torch.matmul(flat_z_e, self.codebook.weight.t()))
         
         embedding_indices = torch.argmin(distances, dim=1, keepdim=True)
         q_ont_hot = torch.zeros(distances.shape)
         q_ont_hot.scatter_(1, embedding_indices, 1)
         
-        z_q = torch.matmul(q_ont_hot, self.codebook.weight).view(z_e.shape)
+        z_q = torch.matmul(q_ont_hot, self.codebook.weight).view(z_e_shape)
         z_q = z_e + (z_q - z_e).detach()
         
         return z_q.permute(0, 3, 1, 2).contiguous()
