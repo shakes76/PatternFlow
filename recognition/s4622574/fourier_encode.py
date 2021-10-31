9ab7ba87da3dfdd2eed49db045a40617eb4cb490
Khac Duy Nguyen <khacduy.nguyen@uqconnect.edu.au>
2021-11-01 01:11:57 +0700

    Improve Encoding

--- recognition/s4622574/fourier_encode.py
+++ recognition/s4622574/fourier_encode.py
@@ -8,55 +8,71 @@ class FourierEncode(layers.Layer):
         self.max_freq = max_freq
         self.freq_ban = freq_ban
 
+    def encoding(transformedFeature, basis, fred_ban, num_images):
+        transformedFeature = tf.reshape(transformedFeature, (1, basis[0], basis[1], 2 * (2 * freq_ban + 1)))
+
+        return tf.repeat(transformedFeature, repeats=num_images, axis=0)
+
+
     def call(self, patientData):
         
         # num_images, *basis, _ = patientData.shape
 
-        num_images = patientData.shape[0]
-        *basis = patientData.shape[1]
+        num_images, *basis, tempt = patientData.shape
         
 
         basis = tuple(basis)
 
         basis_feature = list(map(lambda x: tf.linspace(-1.0, 1.0, num=x), basis))
 
-        fearture = tf.stack(tf.meshgrid(*basis_feature, indexing="ij"), axis=-1)
+        feature = tf.stack(tf.meshgrid(*basis_feature, indexing="ij"), axis=-1)
 
 
-        transformedFeature = self._fourier_encode(fearture)
+        transformedFeature = self._fourier_encode(feature)
 
-        # del fearture
+        # del feature
 
-        transformedFeature = tf.reshape(transformedFeature, (1, basis[0], basis[1], 2 * (2 * self.freq_ban + 1)))
+        # transformedFeature = tf.reshape(transformedFeature, (1, basis[0], basis[1], 2 * (2 * self.freq_ban + 1)))
 
 
-        transformedFeature = tf.repeat(transformedFeature, repeats=num_images, axis=0)
+        # transformedFeature = tf.repeat(transformedFeature, repeats=num_images, axis=0)
 
+        transformedFeature = encoding(transformedFeature, basis, self.freq_ban, num_images)
+
+        # transformedData = tf.concat((patientData, transformedFeature), axis=-1)
+
+
+        # transformedData = tf.reshape(transformedData, (num_images, basis[0]*basis[1], -1)) 
+        transformedData = getCombinedData(patientData, transformedFeature, num_imaes, basis)
+        return transformedData
+
+    def getCombinedData(patientData, transformedFeature, basis, num_images):
         transformedData = tf.concat((patientData, transformedFeature), axis=-1)
 
 
-        transformedData = tf.reshape(transformedData, (num_images, basis[0]*basis[1], -1)) 
-        return transformedData
+        return tf.reshape(transformedData, (num_images, basis[0] * basis[1], -1)) 
+        
 
 
 
 
-    def _fourier_encode(self, fearture):
+    def _fourier_encode(self, feature):
 
-        # fearture = tf.expand_dims(fearture, -1)
-        fearture = tf.cast(tf.expand_dims(fearture, -1), dtype=tf.float32)
-        sampleFeature = fearture
+        # feature = tf.expand_dims(feature, -1)
+        feature = tf.cast(tf.expand_dims(feature, -1), dtype=tf.float32)
+        sampleFeature = feature
         
         scaledKernel = tf.experimental.numpy.logspace(start=0.0, 
                 stop=math.log(self.max_freq / 2) / math.log(10),
                 num=self.freq_ban, dtype=tf.float32,
         )
 
-        scaledKernel = tf.reshape(scaledKernel, (*((1,) * (len(fearture.shape) - 1)), self.freq_ban))
+        scaledKernel = tf.reshape(scaledKernel, (*((1,) * (len(feature.shape) - 1)), self.freq_ban))
 
 
-        fearture = math.pi * scaledKernel * feature 
+        feature = math.pi * scaledKernel * feature 
 
-        fearture = tf.concat([tf.math.sin(fearture), tf.math.cos(fearture)], axis=-1)
-        fearture = tf.concat((fearture, sampleFeature), axis=-1)
-        return fearture
+        feature = tf.concat([tf.math.sin(feature), tf.math.cos(feature)], axis=-1)
+        feature = tf.concat((feature, sampleFeature), axis=-1)
+        return feature
+
