52a455e8f8d3d35b19bd2d2378064e6ecaa83e49
kplume <kplume17@gmail.com>
2021-10-31 22:27:46 +1000

    Created Localization Module

--- recognition/45882999_Improved_UNet/Model.py
+++ recognition/45882999_Improved_UNet/Model.py
@@ -29,3 +29,16 @@ def upsampling_module(input, filters):
     up = layers.UpSampling2D((2, 2))(input)
     up = layers.Conv2D(filters, (3, 3), padding = "same", activation = LeakyReLU(alpha = 0.01))(up)
     return up
+	
+"""
+Return a localization module made of two 3x3 convolutions
+Parameters:
+    input: The layer prior to this module
+    filters: The number of filters for this module
+Returns:
+    Localization module
+"""
+def localization_module(input, filters):
+    conv1 = layers.Conv2D(filters, (3, 3), padding = "same", activation = LeakyReLU(alpha = 0.01))(input)
+    conv2 = layers.Conv2D(filters, (1, 1), padding = "same", activation = LeakyReLU(alpha = 0.01))(conv1)
+    return conv2
