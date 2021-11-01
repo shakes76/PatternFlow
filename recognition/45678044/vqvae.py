daa93634554a8245a125b55e80465923a66886e7
Carrick Cai <55621126+CarrickC@users.noreply.github.com>
2021-11-01 11:20:40 +1000

    Add argument parser to driver script

--- recognition/45678044/vqvae.py
+++ recognition/45678044/vqvae.py
@@ -1,4 +1,4 @@
-from models import VQVAE
+from modules import *
 from helper import *
 import torch
 import torch.nn as nn
@@ -6,49 +6,66 @@ import torchvision
 from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main(args):
+    print('Loading training data')
+    train_data = preload_imgs(
+        '\data\keras_png_slices_data\keras_png_slices_train')
+    train_loader = torch.utils.data.DataLoader(train_data, 
+                                               batch_size=args.batch, 
+                                               shuffle=True, num_workers=0)
+    print('Finished')
+    print('Loading testing data')
+    valid_data = preload_imgs(
+        '\data\keras_png_slices_data\keras_png_slices_validate')
+    valid_loader = torch.utils.data.DataLoader(valid_data, 
+                                               batch_size=args.batch,
+                                               shuffle=True, num_workers=0)
+    print('Finished')
+    print('Loading validation data')
+    test_data = preload_imgs(
+        '\data\keras_png_slices_data\keras_png_slices_test')
+    test_loader = torch.utils.data.DataLoader(test_data, 
+                                              batch_size=32, 
+                                              shuffle=True, num_workers=0)
+    print('Finished')
+    
+    print('Start to train VQVAE')
+    vqvae = VQVAE(1, args.k, args.d).to(args.device)
+    optim = torch.optim.Adam(params=vqvae.parameters(), lr=args.lr)
+    train_status = train(vqvae, optim, args.epoch, train_loader)
+    print('Finished')
+    
+    
+    print('Start to train PixelCNN prior')
+    
+    
 
-# transform = transforms.Compose(
-#     [transforms.Grayscale(num_output_channels=1),
-#      transforms.ToTensor(),
-#      ])
-
-# train_data = datasets.MNIST(
-#     root = './data',
-#     train = True,                         
-#     transform = transform, 
-#     download = True,            
-# )
-
-# test_data = datasets.MNIST(
-#     root = './data', 
-#     train = False, 
-#     transform = transform,
-#     download = True,  
-# )
-
-train_data = preload_imgs('D:\学习\COMP3710\demo2\keras_png_slices_data\keras_png_slices_train')
-valid_data = preload_imgs('D:\学习\COMP3710\demo2\keras_png_slices_data\keras_png_slices_validate')
-test_data = preload_imgs('D:\学习\COMP3710\demo2\keras_png_slices_data\keras_png_slices_test')
-
-EPOCH_SIZE = 50
-BATCH_SIZE = 128
-LR = 0.002
-K = 512
-D = 64
-
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, 
-                                           shuffle=True, num_workers=0)
-
-valid_loader = torch.utils.data.DataLoader(valid_data, batch_size=BATCH_SIZE,
-                                           shuffle=True, num_workers=0)
-
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE, 
-                                          shuffle=True, num_workers=0)
-
-vqvae = VQVAE(1, K, D).to(device)
-optim = torch.optim.Adam(params=vqvae.parameters(), lr=LR)
-train_status = train(vqvae, optim, EPOCH_SIZE, train_loader)
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='VQVAE')
+    
+    parser.add_argument('--epoch', type=int, default=50,
+        help='Epoch size for training vqvae (default: 50)')
+    parser.add_argument('--batch', type=int, default=32,
+        help='Batch size for training vqvae (default: 32)')
+    parser.add_argument('--lr', type=float, default=0.002,
+        help='learning rate for training vqvae (default: 0.002)')
+    
+    parser.add_argument('--k', type=float, default=512,
+        help='Num of latent vectors (default: 512)')
+    parser.add_argument('--d', type=float, default=64,
+        help='Dim of latent vectors (default: 64)')
+    
+    args = parser.parse_args()
+    
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+    if not os.path.exists('./images'):
+        os.makedirs('./images')
+        
+    main(args)
+    
