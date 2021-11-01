f1897de12642bb729b2f2b88e86b317d423302b4
Khac Duy Nguyen <khacduy.nguyen@uqconnect.edu.au>
2021-11-01 08:39:57 +0700

    Remove redundant parts Transformer

--- recognition/s4622574/transformer.py
+++ recognition/s4622574/transformer.py
@@ -1,37 +1,37 @@
-from tensorflow.keras import layers
-import tensorflow as tf 
-import copy
+# from tensorflow.keras import layers
+# import tensorflow as tf 
+# import copy
 
-def transformer_layer(latent_size, proj_size, num_heads, num_trans_blocks):
-    inputs_orig = layers.Input(shape=(latent_size, proj_size))
+# def transformer_layer(latent_size, proj_size, num_heads, num_trans_blocks):
+#     inputs_orig = layers.Input(shape=(latent_size, proj_size))
 
-    input_plus_output = copy.deepcopy(inputs_orig)
+#     input_plus_output = copy.deepcopy(inputs_orig)
 
-    for _ in range(num_trans_blocks):
+#     for _ in range(num_trans_blocks):
 
-        norm = layers.LayerNormalization()(inputs_orig)
-
-
-        attention = layers.MultiHeadAttention(
-            num_heads, proj_size)(norm, norm)
+#         norm = layers.LayerNormalization()(inputs_orig)
 
 
-        attention = layers.Dense(proj_size)(attention)
+#         attention = layers.MultiHeadAttention(
+#             num_heads, proj_size)(norm, norm)
 
 
-        attention = layers.Add()([attention, inputs_orig])
+#         attention = layers.Dense(proj_size)(attention)
 
 
-        attention = layers.LayerNormalization()(attention)
+#         attention = layers.Add()([attention, inputs_orig])
 
 
-        outputs = layers.Dense(proj_size, activation=tf.nn.gelu)(attention)
+#         attention = layers.LayerNormalization()(attention)
 
 
-        outputs = layers.Dense(proj_size)(outputs)
+#         outputs = layers.Dense(proj_size, activation=tf.nn.gelu)(attention)
 
 
-        input_plus_output = layers.Add()([outputs, attention])
+#         outputs = layers.Dense(proj_size)(outputs)
 
 
-    return tf.keras.Model(inputs=inputs_orig, outputs=input_plus_output)
+#         input_plus_output = layers.Add()([outputs, attention])
+
+
+#     return tf.keras.Model(inputs=inputs_orig, outputs=input_plus_output)
