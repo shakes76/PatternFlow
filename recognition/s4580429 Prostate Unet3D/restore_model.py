ffb617b0bbcf4a40008db00bef4f2f9c58af8dee
borova-siska <codybaldry@hotmail.com>
2021-11-01 14:37:09 +1000

    renamed train.py, updated restore_model.py

--- recognition/s4580429 Prostate Unet3D/restore_model.py
+++ recognition/s4580429 Prostate Unet3D/restore_model.py
@@ -1,67 +1,39 @@
-"""  imports  """
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow import keras as K
-from tensorflow._api.v2 import data
-from tensorflow.core.framework.types_pb2 import DataType
-from tensorflow.python.data.ops.dataset_ops import TakeDataset
-device = tf.config.experimental.list_physical_devices("GPU")
-if device == '[]':
-    raise SystemError("No GPU!!")
-print("Found GPU at: {}".format(device))
+"""
+Model loading file to evaluate test dataset with given pre-trained model.
 
-import segmentation_models_3D as sm
+@author Cody Baldry
+@student_number 45804290
+@date 1 November 2021
+"""
+# imports
+import tensorflow as tf
+from tensorflow import keras as K
+
 from matplotlib import pyplot as plt
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import load_model
 import nibabel as nib
 from math import floor, ceil
 import glob
 import re
+import sys
 
-""" model parameters """
-shape = nib.load("/home/Student/s4580429/dataset/semantic_MRs_anon/Case_004_Week0_LFOV.nii.gz").get_fdata().shape
-channels = 3
-n_classes = 6
-BACKBONE = 'resnet50'  #Try vgg16, efficientnetb7, inceptionv3, resnet50
-encoder_weights = 'imagenet'
-activation = 'softmax'
+from model import unet_3d
 
-LR = 0.0001
-batch_size = 1
-batch_num = 10
-optim = K.optimizers.Adam(LR)
-
+# scans data and labels folders for files
 def getScans(dataFolder, labelsFolder):
     dataFiles = sorted(glob.glob(dataFolder + "*"))
     labelsFiles = sorted(glob.glob(labelsFolder + "*"))
     
     return list(zip(dataFiles, labelsFiles))
 
-fileNames = getScans("/home/Student/s4580429/dataset/semantic_MRs_anon/", "/home/Student/s4580429/dataset/semantic_labels_anon/")
-total_images = len(fileNames)
-
-""" get each group of cases' positions """
-ind_of_first_case = []
-i = 0
-lastCase = None
-for path, _ in fileNames:
-    x = re.search("Case_(\d+)_", path).group(1)
-    if lastCase != x:
-        ind_of_first_case.append(i)
-        lastCase = x
-    i += 1
-
-dataset = tf.data.Dataset.from_tensor_slices((tf.constant(range(len(fileNames))), tf.constant(range(len(fileNames)))))
-
-preprocess_input = sm.get_preprocessing(BACKBONE)
-
-def im_file_to_tensor(dataPath, labelPath):
+# lazily convert paths to files into images / labels
+def im_file_to_tensor(dataPath, labelPath, fileNames, channels, n_classes, shape):
     def _im_file_to_tensor(dataPath, labelPath):
         dp, lp = fileNames[dataPath]
         image = tf.convert_to_tensor(nib.load(dp).get_fdata().astype('float32'))
         image = tf.cast(image, tf.float32)
         image = tf.stack((image,)*channels, axis=-1)
-        image = preprocess_input(image)
         image = tf.expand_dims(image, axis=0)
         labels = tf.convert_to_tensor(nib.load(lp).get_fdata().astype('float32'))
         labels = to_categorical(tf.expand_dims(labels, axis=3), num_classes=n_classes)
@@ -75,64 +47,102 @@ def im_file_to_tensor(dataPath, labelPath):
     label.set_shape((1,) + shape + (n_classes,))
     return data, label
 
-dataset = dataset.map(im_file_to_tensor,
-    num_parallel_calls=tf.data.AUTOTUNE)
+# dice coefficient function
+def dice_coefficient(y_true, y_pred):
+    y_true = K.backend.flatten(y_true)
+    y_pred = K.backend.flatten(y_pred)
+    intersection = K.backend.sum(K.backend.abs(y_true * y_pred), axis=-1)
+    return (2. * intersection + 1) / (K.backend.sum(K.backend.square(y_true),-1)
+            + K.backend.sum(K.backend.square(y_pred),-1) + 1)
 
-# TODO try to add shuffle
-i_train = floor(0.7 * total_images)
-while i_train not in ind_of_first_case:
-    i_train += 1
-rem = total_images - i_train - 1
-i_test = floor(i_train + rem/2)
-while i_test not in ind_of_first_case:
-    i_test += 1
-i_test -= i_train
+# dice loss
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coefficient(y_true, y_pred)
 
-train_ds = dataset.take(i_train)
-test_ds = dataset.skip(i_train).take(i_test)
-val_ds = dataset.skip(i_train + i_test)
+def main(path_to_model):
+    # model parameters
+    shape = nib.load("./dataset/semantic_MRs_anon/Case_004_Week0_LFOV.nii.gz").get_fdata().shape
+    channels = 3
+    n_classes = 6
 
-print("Successfully created, processed and split dataset!")
+    LR = 0.0001
+    batch_size = 1
+    batch_num = 15
+    optim = K.optimizers.Adam(LR)
 
-dice_loss = sm.losses.DiceLoss(class_weights=tf.ones((n_classes,))/n_classes) 
-focal_loss = sm.losses.CategoricalFocalLoss()
-total_loss = dice_loss + (1 * focal_loss)
+    fileNames = getScans("./dataset/semantic_MRs_anon/",
+        "./dataset/semantic_labels_anon/")
+    total_images = len(fileNames)
 
-metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+    # get each group of cases' positions
+    ind_of_first_case = []
+    i = 0
+    lastCase = None
+    for path, _ in fileNames:
+        x = re.search("Case_(\d+)_", path).group(1)
+        if lastCase != x:
+            ind_of_first_case.append(i)
+            lastCase = x
+        i += 1
 
-model = load_model('/home/Student/s4580429/segment_out/3D_model_vgg16_5epochs_full3d_parr.h5', compile=False)
+    dataset = tf.data.Dataset.from_tensor_slices((tf.constant(range(len(fileNames))), tf.constant(range(len(fileNames)))))
 
-#y_pred = model.predict(test_ds)
+    dataset = dataset.map((lambda x, y : im_file_to_tensor(x, y, fileNames, channels, n_classes, shape)),
+            num_parallel_calls=tf.data.AUTOTUNE)
 
-import random
-test_img_number = random.randint(0, len(test_ds))
-test_img = None
-ground_truth = None
-for image, label in test_ds.take(1):
-    test_img = image
-    ground_truth = label
+    # train/test/val split
+    i_train = floor(0.7 * total_images)
+    while i_train not in ind_of_first_case:
+        i_train += 1
+    rem = total_images - i_train - 1
+    i_test = floor(i_train + rem/2)
+    while i_test not in ind_of_first_case:
+        i_test += 1
+    i_test -= i_train
 
-test_pred1 = model.predict(test_img)
-test_prediction1 = tf.argmax(test_pred1, axis=4)[0,:,:,:]
-ground_truth_argmax = tf.argmax(ground_truth, axis=4)
-print("test_img:", test_img)
-print("test_pred:", test_prediction1)
-print("ground_truth:", ground_truth_argmax)
+    train_ds = dataset.take(i_train)
+    test_ds = dataset.skip(i_train).take(i_test)
+    val_ds = dataset.skip(i_train + i_test)
 
-aff = nib.load("/home/Student/s4580429/dataset/semantic_labels_anon/Case_004_Week0_SEMANTIC_LFOV.nii.gz").affine
-nib.save(nib.Nifti1Image(test_prediction1.numpy(), aff), "/home/Student/s4580429/segment_out/labels_pred.nii")
+    print("train", train_ds.cardinality())
+    print("test", test_ds.cardinality())
+    print("validation", val_ds.cardinality())
 
-import random
-test_slice = random.randint(floor(128 * (1/3)), ceil(128 * (2/3)))
+    print("Successfully created, processed and split dataset!")
 
-plt.figure(figsize=(12, 8))
-plt.subplot(231)
-plt.title('Testing Image')
-plt.imshow(test_img[0, :,:,test_slice,0], cmap='gray')
-plt.subplot(232)
-plt.title('Testing Label')
-plt.imshow(ground_truth_argmax[0,:,:,test_slice])
-plt.subplot(233)
-plt.title('Prediction on test image')
-plt.imshow(test_prediction1[:,:,test_slice])
-plt.savefig("/home/Student/s4580429/segment_out/compar.png")
+    model = load_model(path_to_model, compile=False)
+
+    # evaluate performance against test set
+    test_loss, test_acc = model.evaluate(test_ds, verbose=2)
+    print(test_loss, test_acc)
+
+    # save a test comparison slice for visualisation
+    import random
+    test_img_number = random.randint(0, len(test_ds))
+    test_img = None
+    ground_truth = None
+    for image, label in test_ds.take(1):
+        test_img = image
+        ground_truth = label
+
+    test_pred1 = model.predict(test_img)
+    test_prediction1 = tf.argmax(test_pred1, axis=4)[0,:,:,:]
+    ground_truth_argmax = tf.argmax(ground_truth, axis=4)
+
+    test_slice = random.randint(floor(128 * (1/3)), ceil(128 * (2/3)))
+
+    plt.figure(figsize=(12, 8))
+    plt.subplot(231)
+    plt.title('Testing Image')
+    plt.imshow(test_img[0, :,:,test_slice,0], cmap='gray')
+    plt.subplot(232)
+    plt.title('Testing Label')
+    plt.imshow(ground_truth_argmax[0,:,:,test_slice])
+    plt.subplot(233)
+    plt.title('Prediction on test image')
+    plt.imshow(test_prediction1[:,:,test_slice])
+    plt.savefig("./segment_out/compare.png")
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    main(arg)
