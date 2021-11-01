360321fe46a5b1428c83a4ce5213c1c5b59a0a41
Khac Duy Nguyen <khacduy.nguyen@uqconnect.edu.au>
2021-11-01 11:25:50 +0700

    Documentation

--- recognition/s4622574/main.py
+++ recognition/s4622574/main.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-import matplotlib.pyplot as plt
-from perceiver import fitModel, Perceiver
-import random, os
-from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from perceiver import Perceiver, fitModel
+import os
+from tensorflow.keras.preprocessing.image import load_img
 import numpy as np
 
 def generate_samples(instanceIds, dataset_proportion):
-
+    """Split data into training and testing set.
+    Note: Imgs of the same person are not in both sets
+    """
     samples = dict()
     trainIndex = []
     testIndex = []
@@ -18,8 +19,7 @@ def generate_samples(instanceIds, dataset_proportion):
             samples[fileName].append(fileId)
         else:
             samples[fileName] = [fileId]
-
-    completedTrainSet = False
+    completedTrainSet = False #flag for processingtrain set
     for imgIndex in samples.values():
         for fileId in imgIndex:
             if not completedTrainSet:
@@ -36,24 +36,22 @@ def generate_samples(instanceIds, dataset_proportion):
     return trainIndex, testIndex
 
 def loadInput(dir_data, dataset_proportion):
-
+    """Initialize tensors for xtrain, ytrain, xtest, ytest"""
+    #18680 instances
     all_instanceId = os.listdir(dir_data)
-
     trainIndex, testIndex = generate_samples(all_instanceId, dataset_proportion)
-
     # random.shuffle(trainIndex)
     # random.shuffle(testIndex)
 
     def load_samples(instanceIds):
-
+        #load tensor X, y from images
         input = []
         list_of_labels = []
-        for i, fileId in enumerate(instanceIds):
+        for i, fileId in enumerate(instanceIds): #label
             image = load_img(dir_data + "/" + fileId, target_size=((64, 64)), color_mode="grayscale")
-
             image = img_to_array(image)
             input.append(image)
-            if "LEFT" in fileId or "L_E_F_T" in fileId or "Left" in fileId or "left" in fileId:
+            if "Left" in fileId or "LEFT" in fileId or "left" in fileId or "L_E_F_T" in fileId:
                 label = 0
             else:
                 label = 1
@@ -70,53 +68,27 @@ def loadInput(dir_data, dataset_proportion):
     return trainX, trainY, valX, valY, testX, testY
 
 def create_dataset(image_dir, img_size):
-
+    """Create train, val, test dataset"""
     split_train = image_dataset_from_directory(image_dir, validation_split=0.2, color_mode='grayscale',
                                                     subset="training", seed=46, label_mode='int',
                                                     batch_size=1, image_size=img_size)
-
     split_val = image_dataset_from_directory(image_dir, validation_split=0.2, color_mode='grayscale',
                                                       subset="validation", seed=46, label_mode='int',
                                                       batch_size=1, image_size=img_size)
-
+    # Split val set to into test set
     foldSize = tf.data.experimental.cardinality(split_val)
     split_test = split_val.take(foldSize // 5)
     split_val = split_val.skip(foldSize // 5)
-
+    #shuffle Imgs
     split_train = split_train.map(process).prefetch(AUTO_TUNE)
     split_val = split_val.map(process).prefetch(AUTO_TUNE)
     split_test = split_test.map(process).prefetch(AUTO_TUNE)
     return split_train, split_val, split_test
 
 def process(inputImg, groundTruth):
+    """Normalize Image"""
     inputImg = tf.cast(inputImg / 255. ,tf.float32)
     return inputImg, groundTruth
-
-def plot_data(history):
-    # Plotting the Learning curves
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    plt.figure(figsize=(8, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(acc, label='Training Accuracy')
-    plt.plot(val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.xlabel("Epochs")
-    plt.ylabel('Accuracy')
-    plt.ylim([min(plt.ylim()), 1])
-    plt.title('Training and Validation Accuracy')
-    plt.subplot(2, 1, 2)
-    plt.plot(loss, label='Training Loss')
-    plt.plot(val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.xlabel("Epochs")
-    plt.ylabel('Cross Entropy')
-    plt.ylim([0, 1.0])
-    plt.title('Training and Validation Loss')
-    plt.xlabel('epoch')
-    plt.show()
 
 if __name__ == "__main__":
 
@@ -133,24 +105,3 @@ if __name__ == "__main__":
 
     history = fitModel(perceiverTransformer, train_set=(trainX, trainY),
             val_set=(valX, valY), test_set=(testX, testY), batch_size=32)
-
-
-    plot_data(history)
-    print("Evaluation")
-    # Retrieve a batch of images from the test set
-    testData, testClass = testX[:32], testY[:32]
-    testData = testData.reshape((32, 64, 64, 1))
-    evaluation = perceiverTransformer.predict_on_batch(testData).flatten()
-    testClass = testClass.flatten()
-
-    evaluation = tf.where(evaluation < 0.5, 0, 1).numpy()
-    label = {0: "left", 1: "right"}
-
-    plt.figure(figsize=(10, 10))
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(testData[i], cmap="gray")
-        plt.title("pred: " + label[evaluation[i]] + ", real: " + label[testClass[i]])
-        plt.axis("off")
-    plt.show()
-    print("Finished")
