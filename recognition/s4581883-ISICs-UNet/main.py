36f3b5fb404a7ce7da1c703a81d81ec5f918b249
wahsnitram <marty@basfuel.com.au>
2021-11-01 12:52:48 +1000

    Basic methods for calculating dice coefficients implemented
    dice_coefficient and dice_coefficient_loss implemented for future use.

--- recognition/s4581883-ISICs-UNet/main.py
+++ recognition/s4581883-ISICs-UNet/main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import pathlib
 import keras
 from tensorflow import keras
 from keras import layers, preprocessing
@@ -10,3 +11,14 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 from tensorflow.python.keras.layers.convolutional import Conv2DTranspose
 from tensorflow.python.ops.numpy_ops import np_config
 np_config.enable_numpy_behavior()
+
+
+def dice_coefficient(true, pred, axis = (1,2,3)):
+    intersect = (2 * tf.reduce_sum(true * pred, axis))
+    union = tf.reduce_sum(true, axis) + tf.reduce_sum(pred, axis)
+    dice_coeff = tf.reduce_mean(intersect / union)
+    return dice_coeff
+
+def dice_coefficient_loss(true, pred):
+    return 1 - dice_coefficient(true, pred)
+
