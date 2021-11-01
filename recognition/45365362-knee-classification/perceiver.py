b0c353e92fdf16709fd98346bf968c9b4fd9df5a
dojster27 <45154205+dojster27@users.noreply.github.com>
2021-11-01 12:47:06 +1000

    adding constants cross attention

--- recognition/45365362-knee-classification/perceiver.py
+++ recognition/45365362-knee-classification/perceiver.py
@@ -10,7 +10,14 @@ input_shape = (228, 260, 3)
 X_train, y_train, X_test, y_test = process_data("AKOA_Analysis\AKOA_Analysis", 80, 20)
 
 PATCH_SIZE = 2
-PATCH_COUNT = (128 // PATCH_SIZE) ** 2 
+PATCH_COUNT = (128 // PATCH_SIZE) ** 2
+PROJECTION_DIMENSION = 256
+LATENT_DIMENSOINS = 256
+ffn_units = [
+    PROJECTION_DIMENSION,
+    PROJECTION_DIMENSION,
+]
+
 
 # feed forward
 def get_feed_forward_network(hidden_units, dropout_rate):
@@ -44,14 +51,47 @@ class Patches(layers.Layer):
         return patches
 
 class PatchEncoder(layers.Layer):
-    def __init__(self, projection_dim):
+    def __init__(self):
         super(PatchEncoder, self).__init__()
-        self.projection = layers.Dense(units=projection_dim)
+        self.projection = layers.Dense(units=PROJECTION_DIMENSION)
         self.position_embedding = layers.Embedding(
-            input_dim=PATCH_COUNT, output_dim=projection_dim
+            input_dim=PATCH_COUNT, output_dim=PROJECTION_DIMENSION
         )
 
     def call(self, patches):
         positions = tf.range(start=0, limit=PATCH_COUNT, delta=1)
         encoded = self.projection(patches) + self.position_embedding(positions)
         return encoded
+
+
+def get_cross_attention(
+    data_dim, ffn_units, dropout_rate
+):
+
+    inputs = {
+        "latent_array": layers.Input(shape=(LATENT_DIMENSOINS, PROJECTION_DIMENSION)),
+        "data_array": layers.Input(shape=(data_dim, PROJECTION_DIMENSION)),
+    }
+
+
+    latent_array = layers.LayerNormalization(epsilon=1e-6)(inputs["latent_array"])
+    data_array = layers.LayerNormalization(epsilon=1e-6)(inputs["data_array"])
+
+    query = layers.Dense(units=PROJECTION_DIMENSION)(latent_array)
+    key = layers.Dense(units=PROJECTION_DIMENSION)(data_array)
+    value = layers.Dense(units=PROJECTION_DIMENSION)(data_array)
+
+    attention_output = layers.Attention(use_scale=True, dropout=0.1)(
+        [query, key, value], return_attention_scores=False
+    )
+
+    attention_output = layers.Add()([attention_output, latent_array])
+
+    attention_output = layers.LayerNormalization(epsilon=1e-6)(attention_output)
+    feed_forward_network = get_feed_forward_network(hidden_units=ffn_units, dropout_rate=dropout_rate)
+   
+    outputs = feed_forward_network(attention_output)
+    outputs = layers.Add()([outputs, attention_output])
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
