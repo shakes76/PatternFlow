975821ea0658fcbe3f48a1d2e52a49fa306e09ec
Paris Qian <1759658+qiansen1386@users.noreply.github.com>
2021-11-02 14:49:24 +1000

    Add test script

---
+++ recognition/s4574278/test.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+import torch
+import utils
+from torchsummary import summary
+
+from model import ModelSize, YOLOModel
+
+to_check = ""
+save_path = ""
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = YOLOModel(num_classes=1, model_size=ModelSize.S).to(device)
+    model.load_state()
+    summary(model, input_size=(3, 512, 512))
+    image = Image.open(to_check)
+    box = model.see(image)
+
+    utils.draw_bbox(np.asarray(image), [box], save_path)
