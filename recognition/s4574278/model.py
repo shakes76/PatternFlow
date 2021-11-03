5fea5cb6da71b64b14656474117d457e2584751c
Paris Qian <1759658+qiansen1386@users.noreply.github.com>
2021-11-04 08:50:28 +1000

    Refactor model.py, move higher level logic to the top of the file

--- recognition/s4574278/model.py
+++ recognition/s4574278/model.py
@@ -1,10 +1,10 @@
 from enum import Enum
-import math
 import torch
+import torchvision
 from torch import nn
 from typing import List
 
-from torch.utils.data import dataset
+import data
 
 """
 Assume input width/height to be 416/480/512 (could be any the multiple of 32)
@@ -43,36 +43,143 @@ class YoloxModel(nn.Module):
     def forward(self, x):
         # PAFPN
         features = self.backbone.forward(x)
-        return self.head.forward(features)
+        return self.head(features)
 
-    def load_state(self, file, map_location=None):
+    def init_state(self, file=None, map_location=None):
+        if file is None:
+            self.apply(self._init_weight)
+            print("Model generated")
         if map_location is None:
             map_location = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.load_state_dict(torch.load(file, map_location=map_location))
+            self.load_state_dict(torch.load(file, map_location=map_location))
+            print(f"Model loaded from {file}")
+
+    @staticmethod
+    def _init_weight(module: nn.Module):
+        for m in module.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.eps = 1e-3
+                m.momentum = 0.03
+            elif isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(module.weight.data, gain=2e-2)
 
     def save_state(self, file):
         torch.save(self.state_dict(), file)
 
+    def post_process(self, outputs) -> torch.Tensor:
+        """
+            Apply NMS => select bounding boxes from all the candidates, and
+            Restore Image size
+        """
+        grids = []
+        strides = []
+        grid_shapes = [output.shape[-2:] for output in outputs]
+        #   outputs
+        #   n, 4 + 1 + num_classes, 64, 64 => n, 4 + 1 + num_classes, 4096
+        #   Same for 32, 32 & 16, 16 layers
+        #   concat together  4096 + 1024 + 256 => n, 5376, 5 + num_classes
+        outputs = torch.cat([output.flatten(start_dim=2) for output in outputs], dim=2).permute(0, 2, 1)
+
+        # compress to probability 
+        outputs[:, :, 4:] = torch.sigmoid(outputs[:, :, 4:])
+        for h, w in grid_shapes:
+            # Build the grid
+            ys, xs = torch.meshgrid([torch.arange(h), torch.arange(w)])
+            # ---------------------------#
+            #   1, 4096, 2
+            #   1, 1024, 2
+            #   1, 256, 2
+            # ---------------------------#   
+            grid = torch.stack((xs, ys), 2).view(1, -1, 2)
+            shape = grid.shape[:2]
+
+            grids.append(grid)
+            strides.append(torch.full((shape[0], shape[1], 1), self.input_shape[0] / h))
+
+        # concat grids and strides
+        grids = torch.cat(grids, dim=1).type(outputs.type())
+        strides = torch.cat(strides, dim=1).type(outputs.type())
+
+        # apply offset & stride to bbox = (the predicted offsets + grid anchor coordinate) x stride size
+        outputs[..., :2] = (outputs[..., :2] + grids) * strides
+        # use Exponential function to restore the width and height
+        outputs[..., 2:4] = torch.exp(outputs[..., 2:4]) * strides
+
+        # Normalization by dividing by image shape 
+        outputs[..., [0, 2]] = outputs[..., [0, 2]] / self.input_shape[1]
+        outputs[..., [1, 3]] = outputs[..., [1, 3]] / self.input_shape[0]
+
+        # # Restore image shape
+        # outputs[..., [0,1,2,3]]=bbox_to_corner()
+        return outputs
+
+
+class YoloXDecoder:
+    def __init__(self, model_weights_path, classes: List[str], model_size=ModelSize.S, input_shape=None, confidence=0.5,
+                 nms_threshold=0.5):
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = YoloxModel(classes, model_size, input_shape).to(device)
+        self.model.init_state(model_weights_path)
+        self.model.evel()
+        self.classes = classes
+        self.num_classes = len(self.classes)
+        self.input_shape = self.model.input_shape
+        self.nms_threshold = nms_threshold
+        self.confidence_bar = confidence
+
     def see(self, image):
         image_shape = image.shape()[0:2]
         images = torch.unsqueeze(image, 0)
-        image_tensors, _ = dataset.resize(images, image_shape, [])
+        is_resized, _ = data.resize(images, self.input_shape, [])
         with torch.no_grad():
-            outputs = self.forward(image_tensors)
-            outputs = self.post_process(outputs)
-        boxes = nms(
+            outputs = self.model(is_resized)
+            outputs = self.model.post_process(outputs)
+        boxes = self.nms(
             outputs,
             self.num_classes,
             self.input_shape,
             image_shape,
-            self.letterbox_image,
-            conf_thres=self.confidence,
-            nms_thres=self.nms_iou,
+            is_resized,
+            confidence_bar=self.confidence_bar,
+            nms_threshold=self.nms_threshold,
         )
         return boxes[0]
 
-    def post_process(self, outputs) -> torch.Tensor:
-        """Restore Image size,"""
+    def nms(self, pred, num_classes, input_shape, image_shape, is_resized, confidence_bar=0.5,
+            nms_threshold=0.4):
+        # n, n_points, n_class+5
+        boxes_corner = bbox_to_corner(pred)
+
+        output = [None for _ in range(len(pred))]
+        # Loop through inputs
+        for i, image_pred in enumerate(pred):
+            #   cls_p  [n_points, 1]    confidence
+            #   cls_index   [n_points, 1]    class
+            cls_p, cls_index = torch.max(image_pred[:, 5:5 + num_classes], 1, keepdim=True)
+
+            # filter by confidence
+            conf_mask = (image_pred[:, 4] * cls_p[:, 0] >= confidence_bar).squeeze()
+
+            if not image_pred.size(0):
+                continue
+            # If still have more boxes
+            # remember: n_points x 7
+            # 7 => x1, y1, x2, y2, obj_conf, cls_p, cls_index
+            detections = torch.cat((image_pred[:, :5], cls_p, cls_index.float()), 1)
+            detections = detections[conf_mask]
+
+            nms_out_index = torchvision.ops.batched_nms(
+                detections[:, :4],
+                detections[:, 4] * detections[:, 5],
+                detections[:, 6],
+                nms_threshold,
+            )
+
+            output[i] = detections[nms_out_index]
+        return output
+
+    def mAP(self, targets):
+        """Calculate IOU nms for 0.5 and 0.75"""
         pass
 
 
@@ -167,6 +274,140 @@ class PAFPN(nn.Module):
         output_5 = self.csp3(p_out0)
 
         return (output_3, output_4, output_5)
+
+
+##########################################################
+#   PAFPN: Path Aggregation feature pyramid network
+##########################################################
+
+
+class DetectionHead(nn.Module):
+    def __init__(self, num_classes=50, width=1.0, in_channels=[256, 512, 1024]) -> None:
+        super().__init__()
+
+        self.cls_conv_layer = nn.ModuleList()
+        self.reg_conv_layer = nn.ModuleList()
+        self.cls_predictor = nn.ModuleList()
+        self.box_predictor = nn.ModuleList()
+        self.obj_predictor = nn.ModuleList()
+        self.stems = nn.ModuleList()
+
+        # Loop through 3 levels of Heads (default)
+        for i in range(len(in_channels)):
+            self.stems.append(
+                ConvBlock(
+                    in_channels=int(in_channels[i] * width),
+                    out_channels=int(256 * width),
+                    kernel_size=1,
+                    stride=1,
+                )
+            )
+            self.cls_conv_layer.append(
+                nn.Sequential(
+                    *[
+                        ConvBlock(
+                            in_channels=int(256 * width),
+                            out_channels=int(256 * width),
+                            kernel_size=3,
+                            stride=1,
+                        ),
+                        ConvBlock(
+                            in_channels=int(256 * width),
+                            out_channels=int(256 * width),
+                            kernel_size=3,
+                            stride=1,
+                        ),
+                    ]
+                )
+            )
+            self.cls_predictor.append(
+                nn.Conv2d(
+                    int(256 * width),
+                    num_classes,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                )
+            )
+
+            self.reg_conv_layer.append(
+                nn.Sequential(
+                    *[
+                        ConvBlock(
+                            int(256 * width),
+                            int(256 * width),
+                            kernel_size=3,
+                            stride=1,
+                        ),
+                        ConvBlock(
+                            int(256 * width),
+                            int(256 * width),
+                            kernel_size=3,
+                            stride=1,
+                        ),
+                    ]
+                )
+            )
+            self.box_predictor.append(
+                nn.Conv2d(
+                    int(256 * width),
+                    4,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                )
+            )
+            self.obj_predictor.append(
+                nn.Conv2d(
+                    int(256 * width),
+                    1,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                )
+            )
+
+    def forward(self, inputs):
+        """
+        inputs:
+            feature3  64, 64, 256
+            feature4  32, 32, 512
+            feature5  16, 16, 1024
+        outputs:
+            feature3 64, 64, (4 + 1 + num_classes)
+            feature3 32, 32, (4 + 1 + num_classes)
+            feature3 16, 16, (4 + 1 + num_classes)
+        """
+        outputs = []
+        for i, x in enumerate(inputs):
+            # cross-channel fusion with 1x1 kernel 
+            # => end_channel: 256
+            x = self.stems[i](x)
+
+            # predict class
+            class_features = self.cls_conv_layer[i](x)
+            # => 
+            #   num_classes, 64, 64
+            #   num_classes, 32, 32
+            #   num_classes, 16, 16
+            class_output = self.cls_predictor[i](class_features)
+
+            # extract features for box and obj
+            box_features = self.reg_conv_layer[i](x)
+
+            # predict bbox
+            # 4, 64, 64
+            # 4, 32, 32
+            # 4, 16, 16
+            box_output = self.box_predictor[i](box_features)
+
+            # predict if object exist
+            obj_output = self.obj_predictor[i](box_features)
+
+            output = torch.cat([box_output, obj_output, class_output], 1)
+            outputs.append(output)
+
+        return outputs
 
 
 ##########################################################
@@ -348,134 +589,6 @@ class SPPBottleneck(nn.Module):
 
 
 ##########################################################
-#   PAFPN: Path Aggregation feature pyramid network
-##########################################################
-
-
-class DetectionHead(nn.Module):
-    def __init__(self, num_classes=50, width=1.0, in_channels=[256, 512, 1024]) -> None:
-        super().__init__()
-
-        self.cls_conv_layer = nn.ModuleList()
-        self.reg_conv_layer = nn.ModuleList()
-        self.cls_predictor = nn.ModuleList()
-        self.box_predictor = nn.ModuleList()
-        self.obj_predictor = nn.ModuleList()
-        self.stems = nn.ModuleList()
-
-        # Loop through 3 levels of Heads (default)
-        for i in range(len(in_channels)):
-            self.stems.append(
-                ConvBlock(
-                    in_channels=int(in_channels[i] * width),
-                    out_channels=int(256 * width),
-                    kernel_size=1,
-                    stride=1,
-                )
-            )
-            self.cls_conv_layer.append(
-                nn.Sequential(
-                    *[
-                        ConvBlock(
-                            in_channels=int(256 * width),
-                            out_channels=int(256 * width),
-                            kernel_size=3,
-                            stride=1,
-                        ),
-                        ConvBlock(
-                            in_channels=int(256 * width),
-                            out_channels=int(256 * width),
-                            kernel_size=3,
-                            stride=1,
-                        ),
-                    ]
-                )
-            )
-            self.cls_predictor.append(
-                nn.Conv2d(
-                    int(256 * width),
-                    num_classes,
-                    kernel_size=1,
-                    stride=1,
-                    padding=0,
-                )
-            )
-
-            self.reg_conv_layer.append(
-                nn.Sequential(
-                    *[
-                        ConvBlock(
-                            int(256 * width),
-                            int(256 * width),
-                            kernel_size=3,
-                            stride=1,
-                        ),
-                        ConvBlock(
-                            int(256 * width),
-                            int(256 * width),
-                            kernel_size=3,
-                            stride=1,
-                        ),
-                    ]
-                )
-            )
-            self.box_predictor.append(
-                nn.Conv2d(
-                    int(256 * width),
-                    4,
-                    kernel_size=1,
-                    stride=1,
-                    padding=0,
-                )
-            )
-            self.obj_predictor.append(
-                nn.Conv2d(
-                    int(256 * width),
-                    1,
-                    kernel_size=1,
-                    stride=1,
-                    padding=0,
-                )
-            )
-
-    def forward(self, inputs):
-        """
-        inputs:
-            feature3  64, 64, 256
-            feature4  32, 32, 512
-            feature5  16, 16, 1024
-        outputs:
-            feature3 64, 64, (4 + 1 + num_classes)
-            feature3 32, 32, (4 + 1 + num_classes)
-            feature3 16, 16, (4 + 1 + num_classes)
-        """
-        outputs = []
-        for i, x in enumerate(inputs):
-            # cross-channel fusion with 1x1 kernel
-            x = self.stems[i](x)
-
-            # predict class
-            class_features = self.cls_conv_layer[i](x)
-            # Output
-            #   80,80,num_classes
-            #
-            class_output = self.cls_predictor[i](class_features)
-
-            # extract features for box and obj
-            box_features = self.reg_conv_layer[i](x)
-
-            # predict bbox
-            box_output = self.box_predictor[i](box_features)
-
-            # predict if object exist
-            obj_output = self.obj_predictor[i](box_features)
-
-            output = torch.cat([box_output, obj_output, class_output], 1)
-            outputs.append(output)
-        return outputs
-
-
-##########################################################
 #   General Building blocks
 ##########################################################
 
@@ -516,3 +629,27 @@ class ConvBlock(nn.Module):
 
         def forward(self, x):
             return self.layers(x)
+
+
+##########################################################
+# Helpers
+##########################################################
+
+
+def bbox_to_corner(boxes: torch.Tensor):
+    """convert cxcywh (center-x, center-y, width, height) bbox to xyxy(top-left, bottom-right)
+    Input: 
+        batch, boxes, [cx, cy, w, h]
+        center_x : boxes[:, :, 0],
+        center_y : boxes[:, :, 1],
+        width    : boxes[:, :, 2],
+        height   : boxes[:, :, 3],
+    """
+    half_width = boxes[:, 2] / 2
+    half_height = boxes[:, :, 3] / 2
+    boxes_corner = boxes.detach().clone().cuda()
+    boxes_corner[:, :, 0] = boxes[:, :, 0] - half_width
+    boxes_corner[:, :, 1] = boxes[:, :, 1] - half_height
+    boxes_corner[:, :, 2] = boxes[:, :, 0] + half_width
+    boxes_corner[:, :, 3] = boxes[:, :, 1] + half_height
+    return boxes_corner
