9fd8b2a687263163e54a8af75af8457e73722fb3
ZIHAN LI <75516735+lingxi211Code@users.noreply.github.com>
2021-11-06 15:33:26 +0800

    Update mainTorun.py

--- recognition/s4544520/OASIS_StyleGAN/mainTorun.py
+++ recognition/s4544520/OASIS_StyleGAN/mainTorun.py
@@ -7,12 +7,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Face testing")
     # network init
     parser.add_argument("--batch_size", type=int, default=12, help="batch size")
-    parser.add_argument("--nc", type=int, default=1, help="number of channels of the generated image")
-    parser.add_argument("--nz", type=int, default=512, help="dimension of the input noise")
-    parser.add_argument("--init_size", type=int, default=4, help="the initial size of the generated image")
-    parser.add_argument("--size", type=int, default=128, help="the final size of the generated image")
-    parser.add_argument("--stage_epoch", type=int, default=40, help="number of transition epochs")
-    parser.add_argument("--lr", type=float, default=0.001, help="initial learning rate")
+    parser.add_argument("--nc", type=int, default=1, help="channel of output")
+    parser.add_argument("--nz", type=int, default=512, help="dimension of the latent space")
+    parser.add_argument("--init_size", type=int, default=4, help="the initial size of output")
+    parser.add_argument("--size", type=int, default=128, help="the size of output")
+    parser.add_argument("--stage_epoch", type=int, default=40, help="number of stage epochs")
+    parser.add_argument("--lr", type=float, default=0.001, help="initial lr")
 
     arg = parser.parse_args()
     trainer = Trainer(arg)
