e947ad8ddad4cdac05f408a435b3f3bd1c599269
ZIHAN LI <75516735+lingxi211Code@users.noreply.github.com>
2021-11-06 15:32:07 +0800

    Update mainTorun.py

--- recognition/s4544520/OASIS_StyleGAN/mainTorun.py
+++ recognition/s4544520/OASIS_StyleGAN/mainTorun.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     parser.add_argument("--nz", type=int, default=512, help="dimension of the input noise")
     parser.add_argument("--init_size", type=int, default=4, help="the initial size of the generated image")
     parser.add_argument("--size", type=int, default=128, help="the final size of the generated image")
-    parser.add_argument("--stage_epoch", type=int, default=10, help="number of transition epochs")
+    parser.add_argument("--stage_epoch", type=int, default=40, help="number of transition epochs")
     parser.add_argument("--lr", type=float, default=0.001, help="initial learning rate")
 
     arg = parser.parse_args()
