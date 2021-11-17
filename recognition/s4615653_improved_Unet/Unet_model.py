0ce437549c5329d95ca5b7a26afbc59acdd87b6f
Your Name <you@example.com>
2021-11-17 20:50:35 +1000

    Rewrite the Improved Unet model.

--- recognition/s4615653_improved_Unet/Unet_model.py
+++ recognition/s4615653_improved_Unet/Unet_model.py
@@ -1,64 +1,107 @@
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+from tensorflow_addons.layers import InstanceNormalization
 
-def convolution(inputs, filters):
+def convolution(inputs, filters,stride):
     # function to implement convolution layer
-    conv = tf.keras.layers.Conv2D(filters, (3, 3), padding='same', activation='relu')(inputs)
-    conv = tf.keras.layers.Conv2D(filters, (3, 3), padding='same', activation='relu')(conv)
+    conv = tf.keras.layers.Conv2D(filters, (3, 3), strides = stride, padding='same', activation='relu')(inputs)
     return conv
 
-def pool(conv):
-    # function to implement pool layer.
-    pool = tf.keras.layers.MaxPool2D((2, 2))(conv)
-    return pool
+def context_module(inputs,filters):
+    # the context module in UNet module.
+    inputs_norm = InstanceNormalization()(inputs)
+    output = tf.keras.layers.Conv2D(filters,(3, 3), activation= LeakyReLU(alpha=0.01), padding='same')(inputs_norm)
+    output = Dropout(0.3)(output)
+    output = InstanceNormalization()(output)
+    output = tf.keras.layers.Conv2D(filters, (3, 3), activation=LeakyReLU(alpha=0.01), padding='same')(output)
+
+    final_output = inputs + output
+
+    return final_output
+
 
 def upsample(inputs,filter):
     #  function to implement unsample layer.
-    conv = tf.keras.layers.UpSampling2D(size=(2, 2))(inputs)
-    conv = tf.keras.layers.Conv2D(filter, (2, 2), padding="same")(conv)
+    conv = tf.keras.layers.UpSampling2D(size=(2,2))(inputs)
+    conv = tf.keras.layers.Conv2D(filter, (3,3), strides= 1, activation = LeakyReLU(alpha=0.01),  padding="same")(conv)
     return conv
+
+def localization_module(inputs, filters):
+    # the localization module in UNet module.
+    conv1 = tf.keras.layers.Conv2D(filters,(3, 3), activation= LeakyReLU(alpha=0.01), padding='same')(inputs)
+    norm1 = InstanceNormalization()(conv1)
+
+    conv2 = tf.keras.layers.Conv2D(filters,(3, 3), activation= LeakyReLU(alpha=0.01), padding='same')(norm1)
+    norm2 = InstanceNormalization()(conv2)
+
+    return norm1, norm2
+
 
 def Unet():
     # build the Unet model.
 
-    inputs = tf.keras.layers.Input(shape=(512, 512, 3))
+    inputs = tf.keras.layers.Input(shape=(256, 256, 3))
 
-    # downsampling
 
-    conv_1 = convolution(inputs, 4)
-    pool_1 = pool(conv_1)
+    # context section
 
-    conv_2 = convolution(pool_1, 8)
-    pool_2 = pool(conv_2)
+    conv_1 = convolution(inputs, 4, 1)
+    con1_context = context_module(conv_1,4)
 
-    conv_3 = convolution(pool_2, 16)
-    pool_3 = pool(conv_3)
+    conv_2 = convolution(con1_context, 8, 2)
+    con2_context = context_module(conv_2,8)
 
-    conv_4 = convolution(pool_3, 32)
-    pool_4 = pool(conv_4)
+    conv_3 = convolution(con2_context, 16, 2)
+    con3_context = context_module(conv_3,16)
 
-    conv_5 = convolution(pool_4, 64)
+    conv_4 = convolution(con3_context, 32,2)
+    con4_context = context_module(conv_4, 32)
 
-    # upsampling
+    conv_5 = convolution(con4_context, 64, 2)
+    con5_context = context_module(conv_5, 64)
 
-    conv_6 = upsample(conv_5, 32)
-    conv_6 = tf.keras.layers.concatenate([conv_6, conv_4])
-    conv_6 = convolution(conv_6, 32)
 
-    conv_7 = upsample(conv_6, 16)
-    conv_7 = tf.keras.layers.concatenate([conv_7, conv_3])
-    conv_7 = convolution(conv_7, 16)
+    # localization
 
-    conv_8 = upsample(conv_7, 8)
-    conv_8 = tf.keras.layers.concatenate([conv_8, conv_2])
-    conv_8 = convolution(conv_8, 8)
+    local_1_conv = upsample(con5_context,32)
 
-    conv_9 = upsample(conv_8, 4)
-    conv_9 = tf.keras.layers.concatenate([conv_9, conv_1])
-    conv_9 = convolution(conv_9, 4)
+    local_2_concat = concatenate([local_1_conv, con4_context])
 
-    outputs = tf.keras.layers.Conv2D(2, (1, 1), activation='sigmoid', padding="same")(conv_9)
+    norm, local_2_localization = localization_module(local_2_concat,32)
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+    local_2_conv = upsample(local_2_localization,16)
 
 
 
+    local_3_concat = concatenate([local_2_conv, con3_context])
+
+    norm_1,local_3_localization = localization_module(local_3_concat, 16)
+
+    local_3_conv = upsample(local_3_localization, 8)
+
+
+    local_4_concat = concatenate([local_3_conv, con2_context])
+
+    norm_2,local_4_localization = localization_module(local_4_concat, 8)
+
+    local_4_conv = upsample(local_4_localization, 4)
+
+
+    final_concat = concatenate([local_4_conv,con1_context])
+    final_conv = tf.keras.layers.Conv2D(8,(3, 3), activation= LeakyReLU(alpha=0.01), padding='same')(final_concat)
+
+    # segmentation section
+
+    seg_1 = Conv2D(2,(1,1))(norm_1)
+    seg_1_up = UpSampling2D(interpolation='bilinear')(seg_1)
+
+    seg_2 = Conv2D(2, (1, 1))(norm_2)
+    seg_2_up = UpSampling2D(interpolation='bilinear')(seg_2+seg_1_up)
+
+    seg_3 = Conv2D(2, (1, 1))(final_conv)
+    final_conv = seg_2_up + seg_3
+
+    output = Activation('sigmoid')(final_conv)
+
+    return Model(inputs=inputs,outputs=output)
