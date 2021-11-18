738a08cb83d9dd7708149a9cdfeda59d7e8a604e
yuexiangzh <91938530+yuexiangzh@users.noreply.github.com>
2021-11-18 14:53:29 +1000

    I have fix the bug test_driver_script.py. The reason why it does not run is that I did not clear the Class Train().

--- recognition/s4584245-Yue/test_driver_script.py
+++ recognition/s4584245-Yue/test_driver_script.py
@@ -4,10 +4,10 @@ from sklearn.manifold import TSNE
 from PIL import Image
 
 def tsne():
-    test_accuracy, test_data, test_labels = test(tensor_test_mask)
-    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    test_accuracy, test_data, test_labels = train.test(tensor_test_mask)
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=10000)
     low_dim_embs = tsne.fit_transform(test_data)
-    plt.title('tsne result')
+    plt.title('GCN tsne result(3710)')
     plt.scatter(low_dim_embs[:,0], low_dim_embs[:,1], marker='o', c=test_labels)
     #Save the image.
     plt.savefig("tsne.png")
@@ -16,8 +16,10 @@ def result():
     im = Image.open("tsne.png")
     im.show()
 
-# Train model
-train()
+# Train model class and draw the accuracy and loss.
+train = Train()
+train.train()
+train.los_acc()
 # TSNE embeddings plot
 tsne()
 # The TSNE result image.
