5270b85de7948cf782380953a2104969fb10014c
Thomas Le <55824662+tomdx@users.noreply.github.com>
2021-11-20 19:41:20 +1000

    Add comments to visualiser.py

--- recognition/45801150_Task6_VQVAE/src/visualiser.py
+++ recognition/45801150_Task6_VQVAE/src/visualiser.py
@@ -4,6 +4,9 @@ import numpy as np
 from VQVAE import VQVae
 
 def show_vqvae_training_loss(vqvae: VQVae):
+    """
+    Shows graph of VQVAE training loss
+    """
     plt.plot(vqvae.total_loss_list)
     plt.plot(vqvae.reconstruction_loss_list)
     plt.plot(vqvae.vq_loss_list)
@@ -15,6 +18,18 @@ def show_vqvae_training_loss(vqvae: VQVae):
     plt.close()
 
 def compare_reconstructions(vqvae: VQVae, x_test_normalised, n_images):
+    """
+    Returns a list of sample images, and a list of corresponding VQVAE reconstructions
+    
+    Variables
+        vqvae: Vector quantised autoencoder
+        x_test_normalised: List of normalised test images
+        n_images: Number of images to return
+        
+    Return value
+        (test_samples, reconstructed): list of images from the test set, and a list of their
+                                       respective reconstructions
+    """
     indices = np.random.choice(len(x_test_normalised), n_images)
     test_samples = x_test_normalised[indices]
 
@@ -24,10 +39,16 @@ def compare_reconstructions(vqvae: VQVae, x_test_normalised, n_images):
 
 
 def calculate_ssim(original_images, reconstructed_images):
+    """
+    Calculate and print the average structured similarity between original and reconstructed images
+    """
     similarity = tf.reduce_mean(tf.image.ssim(original_images, reconstructed_images, max_val=1))
     print("Structured similarity is:", similarity)
 
 def show_reconstructions(n_images, test_samples, reconstructed):
+    """
+    Create and save an image comparing a number of test samples and their reconstructions
+    """
     for i in range(n_images):
         original_image = test_samples[i].squeeze()
         reconstructed_image = reconstructed[i].squeeze()
@@ -46,6 +67,9 @@ def show_reconstructions(n_images, test_samples, reconstructed):
         plt.close()
 
 def show_generated_images(n_images, priors, generated):
+    """
+    Create and save an image containing a number of priors and generated images
+    """
     for i in range(n_images):
         plt.subplot(1, 2, 1)
         plt.imshow(priors[i], cmap="gray")
