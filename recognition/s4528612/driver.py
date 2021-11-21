1f22a3923cb34cc13cca51fda30b29fa9cbe65ed
Majurran <91605563+Majurran@users.noreply.github.com>
2021-11-21 20:20:01 +1000

    Added Final Comments based on Feedback

--- recognition/s4528612/driver.py
+++ recognition/s4528612/driver.py
@@ -12,15 +12,19 @@ import os
 import random, os
 import numpy as np
 from model import Perceiver
+
+"""
+Main Function running the training of the model and the visualisations of the predictions.
+"""
 def main():
-
-
+    # File Path Containing Knee Images
     IMAGE_DIR = '../input/knee-data/AKOA_Analysis/'
+    # Epochs for training the perceiver
     EPOCHS = 10
-
+    # Produce the sets for training, validation and testing.
     X_train, y_train, X_val, y_val, X_test, y_test = process_dataset(IMAGE_DIR, 0.8,0.04)
+    # Perceiver Instantiation
     perceiver = Perceiver()
-    
     # Ensure training set doesn not exceed batch size to avoid fourier encoder error
     X_train = X_train[0:len(X_train) // 32 * 32]
     y_train = y_train[0:len(y_train) // 32 * 32]
@@ -28,30 +32,27 @@ def main():
     y_val = y_val[0:len(y_val) // 32 * 32]
     X_test = X_test[0:len(X_test) // 32 * 32]
     y_test = y_test[0:len(y_test) // 32 * 32]
-    
+    # Run Perceiver Training Function, returning accuracy data
     history = train_perceiver(perceiver,  X_train, y_train,X_val, y_val,X_test, y_test,epochs=EPOCHS)
-
-    
-    
+    # Plot Accuracies
     accuracy = history.history['acc']
     validation_accuracy = history.history['val_acc']
     plt.figure()
-
-    #Plot Accuracies
     plt.plot(accuracy, label='Training Accuracy')
     plt.plot(validation_accuracy, label='Validation Accuracy')
     plt.xlabel("Epochs")
     plt.ylabel('Accuracy')
     plt.title('Training and Validation Accuracy of Perceiver on OAI AKOA Dataset Laterality classification')
     plt.show()
-    
+    # Reshape Test Set to Visualize images
     test_images = X_test[:32].reshape((32, 73, 64, 1))
     test_labels = y_test[:32].flatten()
-    
+    # Return Predictions
     predictions = tf.where(perceiver.predict_on_batch(test_images).flatten() < 0.5, 0, 1).numpy()
-    
+    # Plot Knees and predictions of the test set
     for i in range(32):
         plt.imshow(X_test[i], cmap="gray")
+        # Classifications of the Knee
         laterality = {0: "right", 1: "left"} 
         if predictions[i] < 0.5:
             plt.title("Predicted Right," "Actually: " + laterality[test_labels[i]])
@@ -60,21 +61,32 @@ def main():
 
         plt.show() # This Line may only produce one plot at a time on a normal python environment. On Kaggle it shows all images. 
 
-
+"""
+Shuffles the input dictionary
+Params:
+    dictionary: Dictionary to be shuffled
+Returns: A shuffled version of the input directory
+"""
 def shuffle_dictionary(dictionary):
     items = list(dictionary.items())
     random.shuffle(items)
     return dict(items)
 
-
+"""
+Given the image directory, the train and validation split, returns the training, testing and validation sets.
+Params:
+    directory: Directory containing the knee images
+    train: The training split as a decimal less than 1.
+    validation: The validation split as a decimal less than 1.
+Returns: The training, testing and validation sets.
+"""
 def process_dataset(directory, train,validation):
     left = 0
     right = 0
     labels = []
-
-
     patient_knee_collection = dict({})
 
+    # Parse all knee images and sort based on their patient_id to ensure that the same patient doesn't end up in both the test and training set. 
     for dirname, _, filenames in os.walk(directory):
         for filename in filenames:
             patient_id = filename.split('_')[0]
@@ -82,20 +94,19 @@ def process_dataset(directory, train,validation):
                 patient_knee_collection[patient_id].append(filename) 
             else:
                 patient_knee_collection[patient_id] = [filename]
-
+    # Shuffle the Dictionary
     patient_knee_collection = shuffle_dictionary(patient_knee_collection)
-
+    # Initializing sets
     x_train = []
     y_train = [] 
     x_test = [] 
     y_test = []
     x_val = [] 
     y_val = []
-    #j = 0
-    
+    # Parse all knees and sort and add to relevent sets
     for index, patient in enumerate(patient_knee_collection.items()):
         for i in range(len(patient[1])):
-
+            # Training Set
             if (index/len(list(patient_knee_collection.items()))) < train:
                 x_train.append(img_to_array(load_img(directory + patient[1][i], target_size=(73,64), color_mode="grayscale")))
                 if re.search('right', patient[1][i].replace("_",""), re.IGNORECASE):
@@ -104,7 +115,7 @@ def process_dataset(directory, train,validation):
                 else:
                     left += 1
                     y_train.append(1)
-
+            #  Validation Set
             if (index/len(list(patient_knee_collection.items()))) < (train+validation):
                 x_test.append(img_to_array(load_img(directory + patient[1][i], target_size=(73,64), color_mode="grayscale")))
                 if re.search('right', patient[1][i].replace("_",""), re.IGNORECASE):
@@ -113,6 +124,7 @@ def process_dataset(directory, train,validation):
                 else:
                     left += 1
                     y_test.append(1)
+            # Test Set
             else:
                 x_val.append(img_to_array(load_img(directory + patient[1][i], target_size=(73,64), color_mode="grayscale")))
                 if re.search('right', patient[1][i].replace("_",""), re.IGNORECASE):
@@ -124,7 +136,7 @@ def process_dataset(directory, train,validation):
 
                     
                     
-        
+    # Convert Sets to numpy arrays and normalize the image arrays    
     x_train = np.array(x_train)
     x_train /= 255.0
     y_train = np.array(y_train)
@@ -134,6 +146,7 @@ def process_dataset(directory, train,validation):
     x_val = np.array(x_val)
     x_val /= 255.0
     y_val = np.array(y_val)
+    
     print(x_train.shape)
     print(y_train.shape)
     print(x_val.shape)
@@ -142,10 +155,17 @@ def process_dataset(directory, train,validation):
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 
-
+"""
+Given the image directory, the train and validation split, returns the training, testing and validation sets.
+Params:
+    directory: Directory containing the knee images
+    train: The training split as a decimal less than 1.
+    validation: The validation split as a decimal less than 1.
+Returns: The training, testing and validation sets.
+"""
 
 def train_perceiver(perceiver,X_train, y_train,X_val, y_val,X_test, y_test,epochs):
-
+    # Compile Perceiver with Lamb Optimizer 
     perceiver.compile(optimizer=tfa.optimizers.LAMB(learning_rate=0.001, weight_decay_rate=0.0001,), loss=tf.keras.losses.BinaryCrossentropy(), metrics=[tf.keras.metrics.BinaryAccuracy(name="acc")])
     history = perceiver.fit(
         X_train, y_train,
@@ -154,54 +174,11 @@ def train_perceiver(perceiver,X_train, y_train,X_val, y_val,X_test, y_test,epoch
         validation_data=(X_val, y_val),
         validation_batch_size=32
     )
+    # Calculate Test Accuracy (Final Result)
     _, accuracy = perceiver.evaluate(X_test, y_test)
     print("Test accuracy:" + str(accuracy))
     return history
 
 
-
-def main():
-
-    IMAGE_DIR = '../input/knee-data/AKOA_Analysis/'
-    EPOCHS = 10
-    X_train, y_train, X_val, y_val, X_test, y_test = process_dataset(IMAGE_DIR, 0.8,0.04)
-    perceiver = Perceiver()
-    
-    X_train = X_train[0:len(X_train) // 32 * 32]
-    y_train = y_train[0:len(y_train) // 32 * 32]
-    X_val =  X_val[0:len(X_val) // 32 * 32]
-    y_val = y_val[0:len(y_val) // 32 * 32]
-    X_test = X_test[0:len(X_test) // 32 * 32]
-    y_test = y_test[0:len(y_test) // 32 * 32]
-    
-    history = train_perceiver(perceiver,  X_train, y_train,X_val, y_val,X_test, y_test,epochs=EPOCHS)
-
-    
-    
-    accuracy = history.history['acc']
-    validation_accuracy = history.history['val_acc']
-    plt.figure()
-
-    plt.plot(accuracy, label='Training Accuracy')
-    plt.plot(validation_accuracy, label='Validation Accuracy')
-    plt.xlabel("Epochs")
-    plt.ylabel('Accuracy')
-    plt.title('Training and Validation Accuracy of Perceiver on OAI AKOA Dataset Laterality classification')
-    plt.show()
-    
-    test_images = X_test[:32].reshape((32, 73, 64, 1))
-    test_labels = y_test[:32].flatten()
-    
-    predictions = tf.where(perceiver.predict_on_batch(test_images).flatten() < 0.5, 0, 1).numpy()
-    
-    for i in range(32):
-        plt.imshow(X_test[i], cmap="gray")
-        laterality = {0: "right", 1: "left"} 
-        if predictions[i] < 0.5:
-            plt.title("Predicted Right," "Actually: " + laterality[test_labels[i]])
-        else:
-            plt.title("Predicted Left," "Actually: " + laterality[test_labels[i]])
-
-        plt.show() # This Line may only produce one plot at a time on a normal python environment. On Kaggle it shows all images. 
 if __name__ == "__main__":
     main()
--- recognition/s4528612/model.py
+++ recognition/s4528612/model.py
@@ -4,6 +4,10 @@ import tensorflow.keras as keras
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
+"""
+Produces Attention Component of Cross-Attention Layer
+Returns: Attention Component of Cross-Attention Layer
+"""
 def attention_component(inputs,input_normalized):
     #Produces the attention layers
     attention_layer = layers.MultiHeadAttention(8, 27)(input_normalized, input_normalized)
@@ -15,6 +19,11 @@ def attention_component(inputs,input_normalized):
     attention_layer = layers.LayerNormalization()(attention_layer)
 
     return attention_layer, layers.Dense(27, activation=tf.nn.gelu)(attention_layer)
+
+"""
+Returns the cross-attention layer
+Returns: Cross-attention Layer
+"""
 def cross_attention():
     image_size = 73 * 64
     # Number of Pixels in the Scaled Image
@@ -36,8 +45,8 @@ def cross_attention():
     attention = layers.Attention(use_scale=True, dropout=0.1)(
         query_key_value_vector
     )
+    # Add Input to Output
     attention = layers.Add()([attention, latent_array])
-
     attention = layers.LayerNormalization(epsilon=1e-6)(attention)
     
     # Feedforward network.
@@ -49,24 +58,33 @@ def cross_attention():
 
     return keras.Model(inputs=[latent_input,data_array_input], outputs=outputs)
 
-#Reshape The Fourier Encoder to The array shape then perform calculation
+"""
+Given an image, a Fourier Encoding is applied to the image to retain position data.
+Params:
+    image: Input image to have the Fourier Encoding applied.
+Returns: Image with Fourier Encoding applied
+"""
 def fourier_encode(image):
     batch_size, *axis, _ = image.shape
     axis_pos = list(map(lambda size: tf.linspace(-1.0, 1.0, num=size), axis))
     array = tf.stack(tf.meshgrid(*axis_pos, indexing="ij"), axis=-1)   
     array = tf.cast(tf.expand_dims(array,-1), dtype=tf.float32)
     array_copy = array # Tensors aren't reference types 
+    #Fourier Encoding Calculation
     encode = tf.reshape(tf.experimental.numpy.logspace(start=0.0,stop=0.69, num=6, dtype=tf.float32,),(*((1,) * (len(array.shape) - 1)), 6)) 
     array =  3.14 * array * encode
-    
     # Repeat the Fourier Encoding For the number of times in the batch and reshape to the correct dimension.
     layer = tf.repeat(tf.reshape(tf.concat((tf.concat([tf.math.sin(array), tf.math.cos(array)], axis=-1),array_copy),axis=-1),
                        (1, image.shape[1], image.shape[2], 2*(2*6+1))), repeats=image.shape[0], axis=0)
 
     return tf.reshape(tf.concat((image, layer), axis=-1), (image.shape[0], image.shape[1]*image.shape[2], -1)) 
 
-# Create Transformer Layer    
+"""
+Creates transformer layer
+Returns: Transformer Layer
+""" 
 def transformer_layer():
+    # Initialise component
     inputs = layers.Input(shape=(256, 27))
     input_normalized = layers.LayerNormalization()(inputs)
     i = 0
@@ -76,11 +94,19 @@ def transformer_layer():
         i += 1
     return tf.keras.Model(inputs=inputs, outputs=input_output_sum)
 
+"""
+Overall Perceiver Model Class
+"""
 class Perceiver(tf.keras.Model):
 
     def __init__(self):
         super(Perceiver, self).__init__()
 
+    """
+        Builds the Perceiver
+        Params:
+            Input_shape: Input Shape of the Image
+    """
     def build(self, input_shape):
         self.latent_array = self.add_weight(
             shape=(256, 27),
@@ -106,7 +132,7 @@ class Perceiver(tf.keras.Model):
             fourier_encode(inputs)
         ]
         i = 0
-        
+        # Produces 8 Transformer layers
         while i < 8:
             latent_array = self.transformer(self.cross_attention(cross_attention))
             cross_attention[0] = latent_array
