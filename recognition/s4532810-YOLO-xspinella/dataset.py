6beeafd1fffd9c9727b43eafd436fb08b3f3a9bf
root <xavierspinella@gmail.com>
2022-09-23 15:43:21 +1000

    gitignore file added, testing to verify that the dataset won't be pushed/committed

---
+++ recognition/s4532810-YOLO-xspinella/dataset.py
@@ -0,0 +1,23 @@
+import gdown
+from zipfile import ZipFile
+import os
+
+### Download and save zipped training, testing and validation sets ###
+# Define zip file location and name
+Train_output = "ISIC_data/Train_data.zip"
+Test_output = "ISIC_data/Test_data.zip"
+Validation_output = "ISIC_data/Valid_data.zip"
+
+if not(os.path.exists(Train_output) and os.path.exists(Test_output) and os.path.exists(Validation_output)):
+    # Define download urls
+    Train_url = "https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Training_Data.zip"
+    Test_url = "https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Test_v2_Data.zip"
+    Validation_url = "https://isic-challenge-data.s3.amazonaws.com/2017/ISIC-2017_Validation_Data.zip"
+
+    # Download and save zipped sets, as specified above
+    gdown.download(Train_url, Train_output, quiet=True)
+    gdown.download(Test_url, Test_output, quiet=True)
+    gdown.download(Validation_url, Validation_output, quiet=True)
+
+# with ZipFile("celeba_gan/data.zip", "r") as zipobj:
+#     zipobj.extractall("celeba_gan")
