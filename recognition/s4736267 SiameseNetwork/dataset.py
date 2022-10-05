23240346e7cf97cca6b16ab4e64ddc81e4916391
lukaslobmaier <lukas.lobmaier@arcor.de>
2022-10-05 13:10:27 +1000

    Custom Dataloader for Dataset

--- recognition/s4736267 SiameseNetwork/dataset.py
+++ recognition/s4736267 SiameseNetwork/dataset.py
@@ -1,27 +1,38 @@
-#dataset.py
+#%matplotlib inline
+#%config InlineBackend.figure_format = 'retina'
 
-#containing the data loader for loading and preprocessing your data.
-
-
+import matplotlib.pyplot as plt
+from pandas.core.common import flatten
+import copy
+import numpy as np
+import random
 
 import torch
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torchvision.models as models
-import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import transforms, models
+from torch.utils.data import Dataset, DataLoader
 
+#import albumentations as A
+#from albumentations.pytorch import ToTensorV2
+import cv2
 
-from torch.utils.data import Dataset
+import glob
+#from tqdm import tqdm
+
+import random
+
+#######################################################
+#                  Mean and Standard Derivation Calculation
+#######################################################
+
 
 def mean_std_calculation(loader):
     mean = 0.
     std = 0.
-    for images, _ in loader:
+    for i, data in enumerate(loader, 0):
+        images= data[0]
         batch_samples = images.size(0) # batch size (the last batch can have smaller size!)
         images = images.view(batch_samples, images.size(1), -1)
         mean += images.mean(2).sum(0)
@@ -31,48 +42,186 @@ def mean_std_calculation(loader):
     std /= len(loader.dataset)
 
     return mean, std
-#otherway to load grayscale image
-def image_loader(path: str) -> Image.Image:
-    # open path as file to avoid ResourceWarning 
-    with open(path, 'rb') as f:
-        img = Image.open(f)
-        return img
 
-def train_set(batch_size=32):
-	
 
-	#With data augmentation
-	transformation_train = transforms.Compose([		                                
-        transforms.Grayscale(num_output_channels=1),
-        transforms.Resize(size=(105,105)),
-		transforms.ToTensor(),
-		transforms.Normalize(mean=0.1155, std=0.2224)
+#######################################################
+#                  Data Preparation
+####################################################### 
+
+def train_valid_data(train_data_path = 'ADNI_AD_NC_2D/AD_NC/train',TRAIN_DATA_SIZE = 100,VALID_DATA_SIZE = 10):
+    
+    train_image_paths = [] #to store image paths in list
+    for data_path in glob.glob(train_data_path + '/*'):
+        #classes.append(data_path.split('/')[-1]) 
+        train_image_paths.append(glob.glob(data_path + '/*'))
+    
+    train_image_paths = list(flatten(train_image_paths))
+    random.shuffle(train_image_paths)
+
+    train_image_paths, valid_image_paths = train_image_paths[:TRAIN_DATA_SIZE],train_image_paths[TRAIN_DATA_SIZE:(TRAIN_DATA_SIZE+VALID_DATA_SIZE)] 
+    #print(TRAIN_DATA_SIZE)
+    return train_image_paths, valid_image_paths
+
+def test_data(test_data_path = 'ADNI_AD_NC_2D/AD_NC/test',DATA_SIZE = 10):
+    
+    test_image_paths = []
+    for data_path in glob.glob(test_data_path + '/*'):
+        test_image_paths.append(glob.glob(data_path + '/*'))
+
+    test_image_paths = list(flatten(test_image_paths))
+    random.shuffle(test_image_paths)
+
+    return test_image_paths[:DATA_SIZE]
+
+
+##########################################################
+
+
+
+def classification_data(data_path = 'ADNI_AD_NC_2D/AD_NC/test'):
+    
+    image_path_AD = []
+    image_path_NC = []
+
+    image_path_AD.append(glob.glob(data_path + '/AD/*.jpeg')[0])    
+    image_path_NC.append(glob.glob(data_path + '/NC/*.jpeg')[0])
+    
+    
+    #image_path_AD = list(flatten(image_path_AD))
+    #image_path_NC = list(flatten(image_path_NC))    
+
+    image_NC = cv2.imread(image_path_NC[0], cv2.IMREAD_GRAYSCALE)
+    image_AD = cv2.imread(image_path_AD[0], cv2.IMREAD_GRAYSCALE)
+
+    transform_clas_data=trans_test()
+
+    image_NC = transform_clas_data(image_NC)
+    image_AD = transform_clas_data(image_AD)
+
+    return image_NC, image_AD
+
+
+#######################################################
+#               Define Dataset Class
+#######################################################
+
+class Dataset(Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = cv2.imread(image_filepath, cv2.IMREAD_GRAYSCALE)
+        
+        
+        label = image_filepath.split('/')[-2]
+        label = 0 if label =='AD' else 1
+        
+
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
+
+class DatasetTrain(Dataset):
+    def __init__(self, image_paths, transform=None, DATA_SIZE = 100):
+        self.image_paths = image_paths
+        self.transform = transform
+        self.size = DATA_SIZE 
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx_1):
+        image_filepath = self.image_paths[idx_1]
+        image_1 = cv2.imread(image_filepath, cv2.IMREAD_GRAYSCALE)
+        
+        label_1 = image_filepath.split('/')[-2]
+        label_1 = 0 if label_1=='AD' else 1
+        
+        idx_2 = random.randint(0,self.size-1)
+
+        image_filepath = self.image_paths[idx_2]
+        image_2 = cv2.imread(image_filepath, cv2.IMREAD_GRAYSCALE)
+        
+        label_2 = image_filepath.split('/')[-2]
+        label_2 = 0 if label_2=='AD' else 1
+
+        if label_1 == label_2:
+            label=1
+        else:
+            label=0
+
+        if self.transform:
+            image_1 = self.transform(image_1)
+            image_2 = self.transform(image_2)
+        
+        return image_1, image_2, label
+
+#######################################################
+#                  Create Transformations
+#######################################################    
+def trans_train():
+    transformation_train = transforms.Compose([
+        transforms.ToPILImage(),
+        #transforms.RandomCrop((105,105)),                                     
+        #transforms.Grayscale(num_output_channels=1),
+        #transforms.Resize(size=(105,105)),
+        transforms.crop(self,top=30,left=70,height=165,width=140)
+        transforms.ToTensor(),
+        transforms.Normalize(mean=0.1143, std=0.2130)
     ])
 
-	#Define Training Set
-	train_set = datasets.ImageFolder("~/COMP3710/Assignment3/ADNI_AD_NC_2D/AD_NC/train", transform = transformation_train)
-	
-	#Defining Dataloader
-	train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    return transformation_train
 
-	return train_loader
-
-def test_set(batch_size=32):
-	
-	#Testing without data augmentation
-	transformation_test = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),
+def trans_valid():
+    transformation_valid = transforms.Compose([
+        transforms.ToPILImage(),                                     
+        #transforms.Grayscale(num_output_channels=1),
         transforms.Resize(size=(105,105)),
-		transforms.ToTensor(),
-		transforms.Normalize(mean=0.1155, std=0.2224)
-	])
+        transforms.ToTensor(),
+        #transforms.Normalize(mean=0.000, std=1.000)
+    ])
 
-	#Define Test Set
-	test_set = datasets.ImageFolder("~/COMP3710/Assignment3/ADNI_AD_NC_2D/AD_NC/test", transform = transformation_test,loader=image_loader)
-	
-	#Defining Dataloader
-	test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
+    return transformation_valid
 
-	return test_loader
+def trans_test():
+    transformation_test = transforms.Compose([
+        transforms.ToPILImage(),                                     
+        #transforms.Grayscale(num_output_channels=1),
+        transforms.Resize(size=(105,105)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=0.1143, std=0.2130)
+    ])
+
+    return transformation_test
 
 
+def dataset(batch_size=64, TRAIN_SIZE = 100, VALID_SIZE= 10, TEST_SIZE=10):
+
+    #Preparation
+    
+    train_image_paths, valid_image_paths = train_valid_data(TRAIN_DATA_SIZE = TRAIN_SIZE,VALID_DATA_SIZE = VALID_SIZE)
+    test_image_paths = test_data(DATA_SIZE=TEST_SIZE)
+
+    transformation_train = trans_train()
+    transformation_valid = trans_valid()
+    transformation_test = trans_test()
+    #Dataset
+    
+    train_dataset = DatasetTrain(train_image_paths,transformation_train,DATA_SIZE=TRAIN_SIZE)
+    valid_dataset = DatasetTrain(valid_image_paths,transformation_valid,DATA_SIZE=VALID_SIZE) 
+    test_dataset = Dataset(test_image_paths,transformation_test)
+
+    #Dataloaders
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+
+    valid_loader = DataLoader(valid_dataset, batch_size, shuffle=True)
+
+    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
+
+    return train_loader, valid_loader, test_loader
