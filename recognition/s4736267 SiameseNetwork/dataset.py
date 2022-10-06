Fixed issues with clas_dataset, working test cycle with clas_dataset, minor bug fixes with memory leakage

recognition/s4736267 SiameseNetwork/dataset.py
@@ -102,24 +102,27 @@ def test_data(test_data_path = 'ADNI_AD_NC_2D/AD_NC/test',DATA_SIZE = 20):
     length = len(image_paths) 
     number_of_sets = int(length/20)
 
-    #test_image_paths.sort()
-    image_paths_new=[None]*length
+    randompos = range(number_of_sets)
+    randompos = list(randompos)
+    random.shuffle(randompos)
 
+    image_paths_new=[None]*length
+    #print(len(image_paths_new))
     i = 0 
     while  i < number_of_sets:
         placeholder=image_paths[0+i*20:20+i*20]
         placeholder.sort(key=len, reverse=False)
-        image_paths_new[0+i*20:20+i*20]=placeholder
+        j = int(randompos[i])
+        image_paths_new[0+j*20:20+j*20]=placeholder
         i = i+1
 
     image_paths=image_paths_new
 
-    print("DATA SIZE TEST",DATA_SIZE)
     return image_paths[:DATA_SIZE]
 
 
 #TODO give back images from filepaths / custom dataset or something  Maybe just use the first twenty entries of data loader ?
-def clas_data(clas_data_path = 'ADNI_AD_NC_2D/AD_NC/test'):    
+def clas_data(clas_data_path = 'ADNI_AD_NC_2D/AD_NC_TEST/test'):    
     
     image_paths = []
 
@@ -145,18 +148,9 @@ def clas_data(clas_data_path = 'ADNI_AD_NC_2D/AD_NC/test'):
         image_paths_new[0+i*20:20+i*20]=placeholder
         i = i+1
     
-    image_paths=image_paths_new
-
-    image_paths_AD = image_paths[0:20]        #First Set should be AD
-    #print(len(image_paths_AD))
-    image_paths_NC = image_paths[length-20:]  #Last Set should be NC
-        
-    #print("")
-    #print(image_paths_AD)
-    #print("")
-    #print(image_paths_NC)
-
-    return image_paths_AD, image_paths_NC
+    image_paths=image_paths_new[0:20] + image_paths_new[length-20:]
+    
+    return image_paths
 
 
 #######################################################
@@ -197,8 +191,8 @@ class DatasetClas(Dataset):
         return self.size
 
     def __getitem__(self, idx):
-        image_filepath = self.image_paths[0][idx]
-        
+        image_filepath = self.image_paths[idx]
+        #print("DatasetClas image_filepath",image_filepath, " with index", idx)
         image = cv2.imread(image_filepath, cv2.IMREAD_GRAYSCALE)
         
         
@@ -211,8 +205,7 @@ class DatasetClas(Dataset):
         
         slice_number = idx%20
 
-
-        return image, label, slice_number
+        return image
 
 
 class DatasetTrain(Dataset):
@@ -261,15 +254,18 @@ class DatasetTrain(Dataset):
         return image_1, image_2, label
 
 #######################################################
-def clas_output(clas_dataset,slice_number):
+def clas_output(clas_dataset,slice_number,input_data):
     i=0
-    
+    clas_image_AD = torch.zeros_like(input_data)
+    clas_image_NC = torch.zeros_like(input_data)
 
-    for i in slice_number:
-        class_image_NC = clas_dataset[slice_number[i]]
-        class_image_AD = clas_dataset[slice_number[i]]
+    for i in range(20):
+        
+        clas_image_AD[i] = clas_dataset[slice_number[i]]
+        clas_image_NC[i] = clas_dataset[(slice_number[i+20]+20)] #Second Set represents NC
 
-    return class_image_NC, class_image_AD
+
+    return clas_image_AD, clas_image_NC
 
 #######################################################
 #                  Create Transformations
@@ -282,7 +278,7 @@ def trans_train():
         transforms.Resize(size=(105,105)),
         #transforms.crop(self,top=30,left=70,height=165,width=140)
         transforms.ToTensor(),
-        transforms.Normalize(mean=0.1143, std=0.2130)
+        #transforms.Normalize(mean=0.1143, std=0.2130)
     ])
 
     return transformation_train
@@ -304,7 +300,7 @@ def trans_test():
         #transforms.Grayscale(num_output_channels=1),
         transforms.Resize(size=(105,105)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=0.1143, std=0.2130)
+        #transforms.Normalize(mean=0.1143, std=0.2130)
     ])
 
     return transformation_test
@@ -315,7 +311,7 @@ def trans_clas():
         #transforms.Grayscale(num_output_channels=1),
         transforms.Resize(size=(105,105)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=0.1143, std=0.2130)
+        #transforms.Normalize(mean=0.1143, std=0.2130)
     ])
 
     return transformation_test
@@ -339,6 +335,7 @@ def dataset(batch_size=64, TRAIN_SIZE = 200, VALID_SIZE= 20, TEST_SIZE=20):
     valid_dataset = DatasetTrain(valid_image_paths,transformation_valid) 
     test_dataset = Dataset(test_image_paths,transformation_test)
     clas_dataset = DatasetClas(clas_image_paths,transformation_clas)
+
 
     #Dataloaders
     train_loader = DataLoader(train_dataset, batch_size, shuffle=True,num_workers=1)

recognition/s4736267 SiameseNetwork/test.py
@@ -27,7 +27,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 epoch_range = 1
 
 batch_size=120
-train_factor=20#00
+train_factor=2#00
 valid_factor=1#0
 test_factor=10#0
 
@@ -37,9 +37,13 @@ modulo=round(train_factor/10) +1#Print frequency while training
 import dataset #as data
 train_loader, valid_loader, test_loader, clas_dataset =dataset.dataset(batch_size,TRAIN_SIZE = 20*train_factor, VALID_SIZE= 20*valid_factor, TEST_SIZE=20*test_factor)
 
+#Importing CNN Model
+import modules
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-print(clas_dataset[2])
+net = modules.Net()
+net = net.to(device)
 
 # No backpropagtion , No need for calculating gradients, => Faster calculation
 with torch.no_grad():
@@ -53,9 +57,17 @@ with torch.no_grad():
         labels= data[1].to(device).to(torch.float32)
         slice_number = data[2].to(device) 
 
-        print("slice_number", slice_number)
+        class_image_NC, class_image_AD = dataset.clas_output(clas_dataset,slice_number,inputs)
 
-        class_image_NC, class_image_AD = dataset.clas_output(clas_dataset,slice_number)
+        output1,output2 = net(inputs,class_image_NC)#.squeeze(1)
+        euclidean_distance_NC = F.pairwise_distance(output1, output2)
 
-        print(torch.sum(class_image_AD[0]))
+        output1,output2 = net(inputs,class_image_AD)#.squeeze(1)
+        euclidean_distance_AD = F.pairwise_distance(output1, output2)
 
+        euclidean_distance_MAX = torch.ge(euclidean_distance_AD,euclidean_distance_NC)
+        
+        correct_tensor=torch.eq(labels,euclidean_distance_MAX)
+
+        correct_run = torch.sum(correct_tensor)
+        correct += correct_run

recognition/s4736267 SiameseNetwork/train.py
@@ -27,18 +27,19 @@ net = modules.Net()
 net = net.to(device)
 
 #Constants
-epoch_range = 200
+epoch_range = 20#00
+
 
 batch_size=120
-train_factor=1000#00
-test_factor=1#0
+train_factor=100#00
+test_factor=10#0
 valid_factor=1
 
-modulo=round(train_factor/10) +1#Print frequency while training
+modulo=round(train_factor*20/(batch_size*10))+1 #Print frequency while training
 
 #Importing Custom Dataloader
-import dataset as data
-train_loader, valid_loader, test_loader, clas_dataset =data.dataset(batch_size,TRAIN_SIZE = 20*train_factor, VALID_SIZE= 20*valid_factor, TEST_SIZE=20*test_factor)
+import dataset 
+train_loader, valid_loader, test_loader, clas_dataset =dataset.dataset(batch_size,TRAIN_SIZE = 20*train_factor, VALID_SIZE= 20*valid_factor, TEST_SIZE=20*test_factor)
 
 
 
@@ -93,13 +94,13 @@ for epoch in range(epoch_range):  # loop over the dataset multiple times
         loss = criterion(output1,output2,labels)
         loss.backward()
 
-        #nn.utils.clip_grad_value_(net.parameters(), 100000)
+        nn.utils.clip_grad_value_(net.parameters(), 10)
 
         optimizer.step()
 
         #Training loss
         training_loss[epoch]=training_loss[epoch]+loss#.detach().item()
-        total += batch_size
+        total += torch.numel(labels)
 
         #Update where running
         if i % (modulo) == modulo-1:
@@ -126,49 +127,39 @@ with torch.no_grad():
         inputs= data[0].to(device) 
         
         labels= data[1].to(device).to(torch.float32)
-        slice_number = data[0].to(device) 
+        slice_number = data[2].to(device) 
 
-        print("slice_number", slice_number)
+        clas_image_AD, clas_image_NC = dataset.clas_output(clas_dataset,slice_number,inputs)
 
-        class_image_NC, class_image_AD = modules.clas_output(clas_dataset,slice_number)
+        output1,output2 = net(inputs,clas_image_AD)#.squeeze(1)
+        euclidean_distance_AD = F.pairwise_distance(output1, output2)
 
-        #outputs = net.forward_once(inputs).squeeze(1)
-        output1,output2 = net(inputs,class_image_NC)#.squeeze(1)
+        print("euc_AD",euclidean_distance_AD)
+
+        output1,output2 = net(inputs,clas_image_NC)#.squeeze(1)
         euclidean_distance_NC = F.pairwise_distance(output1, output2)
 
+        print("euc_NC",euclidean_distance_NC)
+
+        predicted_labels = torch.ge(euclidean_distance_AD,euclidean_distance_NC)*1
         
+        print("lab",labels)
 
-        output1,output2 = net(inputs,class_image_AD)#.squeeze(1)
-        euclidean_distance_AD = F.pairwise_distance(output1, output2)
-        euclidean_distance_MAX = torch.ge(euclidean_distance_AD,euclidean_distance_NC)
-        
-        print("MAX",euclidean_distance_MAX)
+        print("pred_lab", predicted_labels)
 
-        #print(euclidean_distance_NC)
-        #print(euclidean_distance_AD)
-        print("labels",labels)
+        correct_tensor=torch.eq(labels,predicted_labels)
 
-        correct_tensor=torch.eq(labels,euclidean_distance_MAX)
-
-        print("correct tensor", correct_tensor)
+        print("cor_ten", correct_tensor)
 
         correct_run = torch.sum(correct_tensor)
-        print("correct_run", correct_run)
-        #outputs= torch.round(outputs)
         correct += correct_run
-        print(correct)
+        total += torch.numel(labels)
 
-        total += batch_size
+        print("")
+        print("")
 
-        #Testing loss to compare it to training loss
-        
-        #loss = criterion(outputs, labels)
-        #testing_loss[epoch]=testing_loss[epoch]+loss.detach().item()
-
-    test_accuracy[epoch] = (correct/total)
-    testing_loss[epoch]=testing_loss[epoch]/total
-    print(f'Testing Loss: {testing_loss[epoch]}')
-    print(f'Test Accuracy: {test_accuracy[epoch]}', flush=True)
+    test_accuracy = (correct/total)
+    print(f'Test Accuracy: {test_accuracy}', flush=True)
 
 
     #if epoch % 50 == 49:
@@ -178,7 +169,7 @@ with torch.no_grad():
         #   torch.save(test_accuracy, 'test_accuracy_18.pt') 
         #   torch.save(testing_loss, 'testing_loss_18.pt')
 
-    del inputs, labels, output1, output2, total, correct
+    del inputs, labels,predicted_labels, output1, output2, total, correct, euclidean_distance_AD, euclidean_distance_NC
     gc.collect()
     
 