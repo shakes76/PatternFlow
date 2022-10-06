3bcfc743e6f76b2cd3694b979dc57a906aa0351a
s4696730 <s4696730@login0.compute.eait.uq.edu.au>
2022-10-06 23:21:32 +1000

    Made the data loader check for previously loaded file to speed up loading. Added handy print messages to document progress of training.

--- recognition/s46967307-VQVAE/model/dataset.py
+++ recognition/s46967307-VQVAE/model/dataset.py
@@ -1,8 +1,19 @@
+print("Beginning Training")
+
 import tensorflow as tf
 import os
 import cv2
+import pickle
 
 def load_data():
+    # Check if data already loaded:
+    if os.path.exists("data.pkl"):
+        print("Found previously loaded data, using that.")
+        with open('data.pkl', 'rb') as f:
+            loaded_dict = pickle.load(f)
+        return loaded_dict
+    print("Couldn't find previously loaded data, loading from file. This may take a minute or two")
+
     # Expects keras_png_slices_data folder to be in this directory, unzipped
     DIR = "./keras_png_slices_data/keras_png_slices"
     CAT = ["test", "train", "validate", "seg_test", "seg_train", "seg_validate"]
@@ -26,5 +37,8 @@ def load_data():
     data["test"] = tf.cast(tf.convert_to_tensor(raw_data["test"]), tf.float32) / 255.0
     data["train"] = tf.cast(tf.convert_to_tensor(raw_data["train"]), tf.float32)
     data["validate"] = tf.cast(tf.convert_to_tensor(raw_data["validate"]), tf.float32) / 255.0
+
+    with open('data.pkl', 'wb') as f:
+        pickle.dump(data, f)
 
     return data
--- recognition/s46967307-VQVAE/model/train.py
+++ recognition/s46967307-VQVAE/model/train.py
@@ -1,5 +1,6 @@
 from modules import AE
 from dataset import load_data
+import tensorflow as tf
 
 # Initialize the Model, Print a summary.
 model = AE()
@@ -9,8 +10,13 @@ print(model.decoder.summary())
 
 # Load the Data.
 data = load_data()
-print(data)
 
 # Begin model training and validation.
+model.fit((tf.concat(data["train"], data["test"])), 
+        (tf.concat(data["train"], data["test"])),
+        epochs=5,
+        shuffle=True,
+        batch_size=32)
 
 # Save model.
+model.save("CKPT")
