48e94e097a076a4cba55af8b2888f53d01545759
hannahscholz <scholzhannah5@gmail.com>
2022-10-07 14:34:15 +1000

    Have all data in the directories, working on removing image files that are unnecessary and resizing all images to 256x256.

--- recognition/46616780_YOLO_Hannah_Scholz/dataset.py
+++ recognition/46616780_YOLO_Hannah_Scholz/dataset.py
@@ -1,4 +1,5 @@
 # File containing the data loader for loading and preprocessing your data
+import os
 from os import listdir
 from PIL import Image
 
@@ -6,19 +7,17 @@ IMAGE_SIZE = 256
 
 
 def load_images(image_path):
-
     file_names = listdir(image_path)
-    print(file_names)
 
-    for filename in file_names:
-        if filename.endswith(".DS_Store"):
+    for new_filename in file_names:
+        if new_filename.endswith(".DS_Store"):
             continue
 
-        with Image.open(image_path + filename) as img:
+        with Image.open(image_path + new_filename) as img:
             width, height = img.size
-            print(width)
-            print(height)
 
+            # Change the shape of the images so all the images are the same size
+            # And all the mask sizes too 256x256
             if width > height:
                 img = img.resize((IMAGE_SIZE, round(height * (IMAGE_SIZE / width))))
             else:
@@ -27,19 +26,21 @@ def load_images(image_path):
 
             bounding_box(img)
 
-    # Change the shape of the images so all the images are the same size
-    # And all the mask sizes too 256x256
 
 
-# Steps to compute center:
-# 1. Compute xMin, xMax, yMin, yMax
-# 2. Average xMin and xMax, and yMin and yMax to compute the centre of bounding box
-# 3. Divide the x average by the width of the image, and the y average by the height of the image
-# Width and height computations are one-liners and are below
 def bounding_box(image):
     print(image.size)
 
 
-validation_path = "Datasets/Validation/validation_mask/"
-# your images in an array
-load_images(validation_path)
+training_data_path = "Datasets/Training/training_data/"
+for filename in os.listdir(training_data_path):
+    if filename.endswith(".png"):
+        os.remove(training_data_path + filename)
+load_images(training_data_path)
+
+
+testing_data_path = "Datasets/Testing/testing_data/"
+for filename in os.listdir(testing_data_path):
+    if filename.endswith(".png"):
+        os.remove(testing_data_path + filename)
+load_images(testing_data_path)
