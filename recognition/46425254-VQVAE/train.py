9a69d7277e640ac185e19eb819c2c423399e887f
Quentin Chang <quentinchang1168@gmail.com>
2022-10-08 01:43:07 +1000

    made a new function that displays SSIM between a decoded and original image, after some research, it is learnt that simply inserting noise into the decoder will not generate images

--- recognition/46425254-VQVAE/train.py
+++ recognition/46425254-VQVAE/train.py
@@ -8,6 +8,7 @@ import modules
 import dataset
 
 import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity as ssim
 
 
 
@@ -36,7 +37,10 @@ class Training():
         fixed_noise = torch.randn((1, 256, 32, 32)).to(self.device)
         model = self.model
         adam = self.optimizer
-        for epoch in range(epochs):
+        epoch = 0
+        
+        while epoch != epochs:
+            
             sub_step = 0
             for i, _ in self.training_data:
                 i = i.view(-1, 3, 256, 256).to(self.device)
@@ -60,48 +64,56 @@ class Training():
                         f"Loss : {total_loss:.4f}"
                     )
                     
+                    self.gen_fake(fixed_noise)
                     
-                    # generate an image every epoch
-                    real_img = self.data[0][0]
-                    real_img = real_img.to("cpu")
-                    _, ax = plt.subplots(1,2)
-                    real_grid = torchvision.utils.make_grid(real_img, normalize = True)
-                    ax[0].imshow(real_grid.permute(1,2,0))
-                    real_img = real_img.view(-1, 3, 256,256).to(self.device)
-                    decoded_img , _ = model(real_img)
-                    decoded_grid = torchvision.utils.make_grid(decoded_img.to("cpu"), normalize = True)
-                    ax[1].imshow(decoded_grid.permute(1,2,0))
-                    plt.show()
-                    
-                    
-                    
-                sub_step += 1
         
-        #save the model if a path is specified for save
+                
+                
+                sub_step += 1
+            epoch += 1
+        #save if user interrupts
+         
+                    
+        # save anyways if loop finishes by itself
         if self.save != None:
             torch.save(model.state_dict(), self.save)
-            
     
     def gen_fake(self, fixed_noise):
         
-        fake = self.model.decoder(fixed_noise).reshape(-1, 3, 256, 256)
+        fake = self.model.decoder(fixed_noise).view(-1, 3, 256, 256).to(self.device).detach()
         
         fake_img_grid = torchvision.utils.make_grid(fake, normalize = True)
-        fake_img_grid = fake_img_grid.cpu().numpy()
-        plt.imshow(np.transpose(fake_img_grid, (1,2,0)), interpolation='nearest')
+        plt.imshow(fake_img_grid.to("cpu").permute(1,2,0))
         plt.show()
         
         
+    def gen_real(self):
+        # generate an image every epoch
+        real_img = self.data[0][0]
+        _, ax = plt.subplots(1,2)
         
-        
+        real_img = real_img.view(-1, 3, 256,256).to(self.device).detach()
+        real_grid = torchvision.utils.make_grid(real_img, normalize = True)
+        decoded_img , _ = self.model(real_img)
+        decoded_img = decoded_img.view(-1, 3, 256,256).to(self.device).detach()
+        decoded_grid = \
+            torchvision.utils.make_grid(decoded_img, normalize = True)
+        decoded_grid = decoded_grid.to("cpu").permute(1,2,0)
+        real_grid = real_grid.to("cpu").permute(1,2,0)
+        ax[1].imshow(decoded_grid)
+        ax[0].imshow(real_grid)
+        plt.show()
+        print(ssim(real_grid.numpy(), decoded_grid.numpy(), channel_axis = -1))
         
         
         
 path = r"C:\Users\blobf\COMP3710\PatternFlow\recognition\46425254-VQVAE\keras_png_slices_data\train"
+
+trained = r"C:\Users\blobf\COMP3710\PatternFlow\recognition\46425254-VQVAE\trained_model\bruh.pt"
 lr = 0.0002
 epochs  = 50
 
-trainer = Training(lr, epochs, path)
+trainer = Training(lr, epochs, path, save = trained)
 trainer.train()
 
 trainer.gen_fake(1)
