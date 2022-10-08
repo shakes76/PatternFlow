3c3232a372daaf01dbb85b13a7aa1421082e9e3e
jonathanjross <jonathan.ross@uqconnect.edu.au>
2022-10-09 09:23:25 +1000

    dataset.py - crop images from base adni dataset

--- recognition/vit-transformer-s4630996/dataset.py
+++ recognition/vit-transformer-s4630996/dataset.py
@@ -2,7 +2,54 @@
 Assumptions:
 ADNI Data is from Blackboard
 ADNI DATA has been unzipped
+New folders for cropped images have been created
+New folders for training and validation data copy from original have bee created
 """
+
+########################################################################################################
+###############################    Crop images from base ADNI dataset   ################################
+########################################################################################################
+
+from os import listdir
+from PIL import Image
+
+
+# get a list of files to crop
+uncropped_files_train_AD = listdir(r"AD_NC\train\AD")
+uncropped_files_train_NC = listdir(r"AD_NC\train\NC")
+uncropped_files_test_AD = listdir(r"AD_NC\test\AD")
+uncropped_files_test_NC = listdir(r"AD_NC\test\NC")
+
+# define parameters for crop
+left = 30
+top = 10
+right = 230
+bottom = 210
+
+# crop and save in new directory - train AD
+for file in uncropped_files_train_AD:
+    image = Image.open(r"AD_NC\train\AD\{}".format(file))
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save(r"AD_NC_cropped\train\AD\{}".format(file))
+
+# crop and save in new directory - train NC
+for file in uncropped_files_train_NC:
+    image = Image.open(r"AD_NC\train\NC\{}".format(file))
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save(r"AD_NC_cropped\train\NC\{}".format(file))
+
+# crop and save in new directory - test AD
+for file in uncropped_files_test_AD:
+    image = Image.open(r"AD_NC\test\AD\{}".format(file))
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save(r"AD_NC_cropped\test\AD\{}".format(file))
+
+# crop and save in new directory - test NC
+for file in uncropped_files_test_NC:
+    image = Image.open(r"AD_NC\test\NC\{}".format(file))
+    cropped = image.crop((left, top, right, bottom))
+    cropped.save(r"AD_NC_cropped\test\NC\{}".format(file))
+
 
 
 ########################################################################################################
