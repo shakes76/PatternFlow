f0437f62744932dee6c8a24b8dd0cdf45b96ce66
Mimimitsu <649914733@qq.com>
2022-10-08 19:06:36 +0800

    upload the dataset.py file

---
+++ recognition/46281740_improved_unet/dataset.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+def decode_image(filename):
+    # loading the image file
+    image = tf.io.read_file(filename)
+    image = tf.image.decode_png(image, channels=1)
+    image = tf.image.resize(image, (256, 256))
+    return image
+
+def process_image(image, seg):
+    # process the image data
+    image = decode_image(image)
+    image = image/255.0
+    
+    seg = decode_image(seg)
+    # tensorflow decode
+    seg = tf.cast(seg == [0.0, 85.0, 170.0, 255.0], tf.float32)
+    return image, seg
