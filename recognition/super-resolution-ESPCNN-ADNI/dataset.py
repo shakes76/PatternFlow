b234b12c9b25f9a9ad3a955b0a45eaa1a91c5542
lucenas <samlucena2@gmail.com>
2022-10-09 11:24:36 +1000

    ESPCNN: dataset.py style updates

--- recognition/super-resolution-ESPCNN-ADNI/dataset.py
+++ recognition/super-resolution-ESPCNN-ADNI/dataset.py
@@ -14,6 +14,7 @@ from keras.utils import img_to_array
 from keras.utils import image_dataset_from_directory
 from IPython.display import display
 
+
 def download_data() -> str:
     """Download the ADNI-MRI data and return the resulting download path
     
@@ -35,13 +36,14 @@ def download_data() -> str:
     dataset_url = \
         "https://cloudstor.aarnet.edu.au/plus/s/L6bbssKhUoUdTSI/download"
     data_dir = keras.utils.get_file(
-        origin=dataset_url, 
-        fname="ADNI-MRI", 
+        origin=dataset_url,
+        fname="ADNI-MRI",
         extract=True
     )
     root_dir = os.path.join(data_dir, "../AD_NC")
 
     return root_dir
+
 
 def get_datasets(data_path: str) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
     """Return train and test datasets from the given data directory
@@ -53,33 +55,34 @@ def get_datasets(data_path: str) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
         Tuple[tf.data.Dataset, tf.data.Dataset]: Tuple of train and test
         datasets (train, test)
     """
-    
+
     image_size = (256, 240)
     batch_size = 32
-    
+
     train_ds = image_dataset_from_directory(
         data_path,
         image_size=image_size,
         batch_size=batch_size,
         color_mode="grayscale",
     )
-    
+
     test_ds = image_dataset_from_directory(
         data_path,
         image_size=image_size,
         batch_size=batch_size,
         color_mode="grayscale",
     )
-    
+
     return train_ds, test_ds
 
+
 def preview_data(dataset: tf.data.Dataset) -> None:
-    """Construct a matplotlib figure to preview some of the given dataset
+    """Construct a matplotlib figure to preview some given dataset
 
     Args:
         dataset (tf.data.Dataset): Dataset to preview
     """
-    # Preview images (from https://www.tensorflow.org/tutorials/load_data/images)
+    # Preview images (https://www.tensorflow.org/tutorials/load_data/images)
     plt.figure(figsize=(10, 10))
     for images, labels in dataset.take(1):
         for i in range(9):
