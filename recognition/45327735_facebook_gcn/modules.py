7377323af3347a648711e3da69fc5fb32d99d289
not-crispy <68049748+not-crispy@users.noreply.github.com>
2022-10-09 10:07:19 +1000

    Implemented GNN architecture. WARNING: architecture class is untested.

--- recognition/45327735_facebook_gcn/modules.py
+++ recognition/45327735_facebook_gcn/modules.py
@@ -13,6 +13,77 @@ from keras import layers
 from keras import Sequential
 import tensorflow as tf
 
+class GNN(tf.keras.Model):
+    """
+    Builds the GNN model using GraphConvLayers and vanilla Feed Forward Networks. Predicts the class of each node.
+    """
+    def __init__(self, graph_input, num_classes, hidden_nodes, aggregation_type="sum", combination_type="concat",
+                 dropout_rate=0.2, normalize=True, *args, **kwargs,):
+        super(GNN, self).__init__(*args, **kwargs)
+
+        # Unpack graph input into edges, features and weights data
+        edges, features, weights = graph_input
+        self.edges = edges
+        self.features = features
+        self.weights = weights
+
+        # Set an identity matrix of weights if not provided.
+        if self.weights is None:
+            self.weights = tf.ones(shape=edges.shape[1])
+        # Force weights to sum to 1.
+        self.weights = self.weights / tf.math.reduce_sum(self.edge_weights)
+
+        # Build model architecture
+        self._architecture(hidden_nodes, dropout_rate, aggregation_type, combination_type, normalize, num_classes)
+
+    def _architecture(self, hidden_nodes, dropout_rate, aggregation_type, combination_type, normalize, num_classes):
+        """
+        Defines the architecture of the GNN.
+
+        - Feed Forward Networks are used to process input and output representations.
+        - GraphConv layers are used to process graph data.
+        - A n-node Dense layer (where n = number of classes) outputs the final prediction
+        """
+        # Create a process layer.
+        self.preprocess = create_ffn(hidden_nodes, dropout_rate, name="preprocess")
+
+        # Create the GraphConv layers.
+        self.convs = []
+        for i in range(0, 3):
+            new_layer = GraphConvLayer(hidden_nodes, dropout_rate, aggregation_type,
+                                    combination_type, normalize, name="graph_conv"+str(i))
+            self.convs.append(new_layer)
+
+        # Create a postprocess layer.
+        self.postprocess = create_ffn(hidden_nodes, dropout_rate, name="postprocess")
+
+        # Create a compute logits layer.
+        self.predict_labels = layers.Dense(units=num_classes, name="predict_labels")
+
+    def call(self, input_node_indices):
+        """
+        Constructs the architecture of the model, including residual single-skip connections between GraphConv layers.
+        """
+        # Preprocess the node_features to produce node representations.
+        x = self.preprocess(self.node_features)
+
+        # Apply graph layers
+        for i, conv in enumerate(self.convs):
+            # Apply graph conv layer
+            applied_layer = conv((x, self.edges, self.edge_weights))
+            # Skip connection
+            x = applied_layer + x
+
+        # Postprocess final node embedding
+        x = self.postprocess(x)
+
+        # Fetch node embeddings for the input node_indices
+        node_embeddings = tf.gather(x, input_node_indices)
+
+        # Predict labels
+        return self.predict_labels(node_embeddings)
+
+
 """
 The following GraphConvLayer and create_ffn source code is from Keras. Comments by me.
 
