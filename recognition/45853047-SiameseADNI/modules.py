4e139f672ccf5c731fc5eeee9fa9312ff4807822
Dominic Thompson <dom.thompson458@gmail.com>
2022-10-09 11:40:25 +1000

    cleanup

--- recognition/45853047-SiameseADNI/modules.py
+++ recognition/45853047-SiameseADNI/modules.py
@@ -71,7 +71,6 @@ def classification_model(subnet):
     return classifier
 
 def contrastive_loss(y, y_pred):
-
     square = tf.math.square(y_pred)
     margin = tf.math.square(tf.math.maximum(1 - (y_pred), 0))
     return tf.math.reduce_mean((1 - y) * square + (y) * margin
@@ -113,24 +112,3 @@ def siamese(height: int, width: int):
     model.compile(loss=contrastive_loss, metrics=['accuracy'],optimizer=opt)
 
     return model
-
-def loss(margin=1):
-    def contrastive_loss(y_true, y_pred):
-        """Calculates the constrastive loss.
-
-        Arguments:
-            y_true: List of labels, each label is of type float32.
-            y_pred: List of predictions of same length as of y_true,
-                    each label is of type float32.
-
-        Returns:
-            A tensor containing constrastive loss as floating point value.
-        """
-
-        square_pred = tf.math.square(y_pred)
-        margin_square = tf.math.square(tf.math.maximum(margin - (y_pred), 0))
-        return tf.math.reduce_mean(
-            (1 - y_true) * square_pred + (y_true) * margin_square
-        )
-
-    return contrastive_loss
