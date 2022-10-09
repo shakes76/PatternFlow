85fdc14f4f8048f4d6a9b741aca86a6ce46d3b39
PrismTanker <69773486+PrismTanker@users.noreply.github.com>
2022-10-09 22:04:56 +1000

    cleaned up comments and added appropriate error raising to custom layers

--- recognition/s4641356_OASIS_StyleGAN/GANutils.py
+++ recognition/s4641356_OASIS_StyleGAN/GANutils.py
@@ -1,36 +1,32 @@
 from PIL import Image
 import numpy as np
 
-# class GANUtilities():
-"""
-Various helper functions for use with and around GAN and its output.
-Packaged into a class due to some import issues
-"""
-def denormalise(data: np.ndarray, mean: float) -> np.ndarray:
+
+def denormalise(data: np.array, mean: float) -> np.array:
     """
     Take output of GAN and undo data preprocessing procedure to return
     a matrix of integer greyscale image pixel intensities suitable to
     convert directly into an image.
 
     Args:
-        data (np.ndarray): normalised data
+        data (np.array): normalised data
         mean (float): grop mean used to centralise original normalised data
 
     Returns:
-        np.ndarray: denormalized data
+        np.array: denormalized data
     """
-    data = np.array(data) #cast to ndarray from any array like (Allows Tensor Compatibility)
+    data = np.array(data) #cast to numpy array from any array like (Allows Tensor Compatibility)
     decentered = data + mean
     return (decentered * 255).astype(np.uint8)
 
 
-def create_image(data: np.ndarray, name: str = None, output_folder: str = "output/") -> Image:
+def create_image(data: np.array, name: str = None, output_folder: str = "output/") -> Image:
     """
     Creates an new PNG image from a generated data matrix.
     Saves image to output folder if a name is specified
 
     Args:
-        data (np.ndarray): uint8 single channel matrix of greyscale image pixel intensities
+        data (np.array): uint8 single channel matrix of greyscale image pixel intensities
         name (str or NoneType, optional): name to save image as in output folder, If None image is not saved. Defaults to None.
         output_folder (str, optional): path of output folder. Defaults to "output/".
 
--- recognition/s4641356_OASIS_StyleGAN/modules.py
+++ recognition/s4641356_OASIS_StyleGAN/modules.py
@@ -13,22 +13,34 @@ class adaIN(tf.keras.layers.Layer):
         """
         super().__init__(**kwargs)
 
-    def build(self, input_shape):
+    def build(self, input_shape: np.array) -> None:
         """
         Called automatically on first 'call' passing the shape of input
         To allow weights to be allocated lazily
+
         Args:
-            input (_type_): _description_ TODO
+            input (np.array): Shape of input passed to layer
         """
         pass
 
-    def call(self, input):
+    def call(self, input: list[tf.Tensor,tf.Tensor]) -> tf.Tensor:
         """
-        Preforms the layer's desired operation using trained weights
+        Performs deterministic adaIN
 
         Args:
-            input (_type_): _description_ TODO [x (nxn),y (2,)]
+            input (list[tf.Tensor,tf.Tensor]): list of tensors, the first is the working image layer, 
+                    the second is a (,2) tensor containing the corespondingfeature scale and bias
+
+        Raises:
+            ValueError: If the second input tensor is not exactly two values
+
+        Returns:
+            tf.Tensor: image layer scaled and biased (same dimensions as first input tensor)
         """
+
+        if not (input[1].shape[1] == 2):
+            raise ValueError("Second input must be of shape (,2), recieved {}".format(input[1].shape))
+
         x,y = input
         yscale,ybias = tf.split(y,2,axis = 1)#axes shifted by 1 to account for batches
         mean = tf.math.reduce_mean(x)
@@ -46,31 +58,36 @@ class addNoise(tf.keras.layers.Layer):
         """
         super().__init__(**kwargs)
 
-    def build(self, input_shape):
+    def build(self, input_shape: np.array) -> None:
         """
         Called automatically on first 'call' passing the shape of input
         To allow weights to be allocated lazily
+
         Args:
-            input (_type_): _description_ TODO
+            input (np.array): Shape of input passed to layer
         """
         #This layer has a single weight to train, the scaling of the noise
         self.noise_weight = self.add_weight(shape = [1], initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=1.0), name = "noise_weight") #inherited from Layer
 
-    def call(self, input):
+    def call(self, input: list[tf.Tensor,tf.Tensor]) -> tf.Tensor:
         """
         Preforms the layer's desired operation using trained weights
 
         Args:
-            input (_type_): _description_ TODO [x (nxn), noise (nxn)]
+            input (list[tf.Tensor,tf.Tensor]): List of two Tensors, the first is the current working image layer, and the second is the corresponding matrix of noise to add to it. 
+                    The two tensors must have matching dimensions
+
+        Raises:
+            ValueError: If the two input tensors do not have matching dimension
+
+        Returns:
+            tf.Tensor: The image tensor with the noise scaled by learnt weight added to it
         """
+        if not (input[0].shape == input[1].shape):
+            raise ValueError("Inputs must be of same shape, recieved the following: Input 1: {}, Input 2: {}".format(input[0].shape,input[1].shape))
+
         x,noise = input
         return x + (self.noise_weight*noise)
-
-class StyleGANGenerator:
-    pass
-
-class StyleGANDiscriminator:
-    pass
 
 class StyleGAN(tf.keras.Model):
     pass
