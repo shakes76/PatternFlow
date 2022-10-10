435d92844d7210d1ab4c62a177f35a9f71827e41
lucenas <samlucena2@gmail.com>
2022-10-10 23:15:01 +1000

    ESPCNN: modules.py Add callback, improve style
    Add class ESPCNCallback which stores and evaluates psnr loss function for
    the model.
    Style has been improved according to PEP8.

--- recognition/super-resolution-ESPCNN-ADNI/modules.py
+++ recognition/super-resolution-ESPCNN-ADNI/modules.py
@@ -1,26 +1,70 @@
-"""model.py
+"""modules.py
 
-Functions to retrieve the super-resolution model.
+The source code for components of the super-resolution model. 
 """
 
+import math
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 
+def get_model(
+    upscale_factor: int = 3,
+    img_width: int = 256,
+    img_height: int = 240,
+    channels: int = 1,
+) -> keras.Model:
+    """Return a super-resolution model
 
-def get_model(upscale_factor=3, channels=1) -> keras.Model:
+    Args:
+        upscale_factor (int, optional): Multiple to upscale the final output. 
+            Defaults to 3.
+        img_width (int, optional): Input image width. Defaults to 256.
+        img_height (int, optional): Input image height. Defaults to 240.
+        channels (int, optional): Number of colour channels. Defaults to 1.
+
+    Returns:
+        keras.Model: Super-resolution model.
+    """
     # https: // keras.io / examples / vision / super_resolution_sub_pixel /
     conv_args = {
         "activation": "relu",
-        "kernel_initialiser": "Orthogonal",
+        "kernel_initializer": "Orthogonal",
         "padding": "same",
     }
 
-    inputs = keras.Input(shape=(None, None, channels))
-    x = layers.Conv2D(64, 5, **conv_args)(inputs)
-    x = layers.Conv2D(64, 3, **conv_args)(x)
-    x = layers.Conv2D(32, 3, **conv_args)(x)
-    x = layers.Conv2D(channels * (upscale_factor ** 2), 3, **conv_args)(x)
-    outputs = tf.nn.depth_to_space(x, upscale_factor)
+    inputs = keras.Input(shape=(img_width, img_height, channels))
+    next_layer = layers.Conv2D(64, 5, **conv_args)(inputs)
+    next_layer = layers.Conv2D(64, 3, **conv_args)(next_layer)
+    next_layer = layers.Conv2D(32, 3, **conv_args)(next_layer)
+    next_layer = layers.Conv2D(
+        channels * (upscale_factor ** 2), 
+        3, 
+        **conv_args
+    )(next_layer)
+
+    outputs = tf.nn.depth_to_space(next_layer, upscale_factor)
 
     return keras.Model(inputs, outputs)
+
+
+class ESPCNCallback(keras.callbacks.Callback):
+    """Callback methods for the ESPCN model"""
+
+    def __init__(self):
+        """Initialise callback class and attributes"""
+        super(ESPCNCallback, self).__init__()
+        self.psnr = np.array([])
+
+    def on_epoch_begin(self, epoch, logs=None):
+        """On epoch begin, initialise array to store psnr values."""
+        self.psnr = np.array([])
+
+    def on_epoch_end(self, epoch, logs=None):
+        """On epoch end, print PSNR value"""
+        print(f"Mean PSNR for epoch: {np.mean(self.psnr):2f}")
+
+    def on_test_batch_end(self, batch, logs=None):
+        """On batch end, append the next psnr value"""
+        self.psnr = np.append(self.psnr, 10 * math.log10(1 / logs["loss"]))
