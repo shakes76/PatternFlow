48f4d34c0ea7516b0c7246ab13ea1de53f4b9f20
PrismTanker <69773486+PrismTanker@users.noreply.github.com>
2022-10-11 00:58:56 +1000

    implemented a folder priming function in preparation to implement saving per training epoch

--- recognition/s4641356_OASIS_StyleGAN/GANutils.py
+++ recognition/s4641356_OASIS_StyleGAN/GANutils.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import os
+import shutil
 
 
 def denormalise(data: np.array, mean: float) -> np.array:
@@ -40,7 +42,7 @@ def create_image(data: np.array, name: str = None, output_folder: str = "output/
     return im
 
 def random_generator_inputs(num_images: int,latent_dim: int,noise_start: int,noise_end: int) -> list[np.array]:
-    """_summary_
+    """_summary_ TODO
 
     Args:
         num_images (int): _description_
@@ -60,3 +62,16 @@ def random_generator_inputs(num_images: int,latent_dim: int,noise_start: int,noi
                 tf.random.normal(shape =  (num_images,curr_res,curr_res,latent_dim))]
         curr_res = curr_res*2
     return [latent_vectors] + noise_inputs
+
+def make_fresh_folder(folder_path: str) -> None:
+    """
+    Generates a folder at the specified location, wiping any existing contents
+    If the specified folder already existed. Parents are created if neccessary,
+    but are not cleared if they exist already.
+
+    Args:
+        folder_path (str): path to specified folder. If no folder exists at the location a new one is created. If a folder exists at the location it is wiped.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
