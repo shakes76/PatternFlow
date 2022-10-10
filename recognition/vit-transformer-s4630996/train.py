edff9ef268382355c61108cbd107a51d91365cc8
jonathanjross <jonathan.ross@uqconnect.edu.au>
2022-10-10 15:30:50 +1000

    train.py - added file header with assumptions, key functions, and references

---
+++ recognition/vit-transformer-s4630996/train.py
@@ -0,0 +1,16 @@
+"""
+Assumptions:
+
+Steps / Key Functions:
+1. Instantiate model
+2. Select Optimzer
+3. Compile model
+4. Create Checkpoint callback
+5. Train the model
+
+
+References:
+1) https://keras.io/examples/vision/image_classification_with_vision_transformer/
+2) https://towardsdatascience.com/understand-and-implement-vision-transformer-with-tensorflow-2-0-f5435769093
+
+"""
