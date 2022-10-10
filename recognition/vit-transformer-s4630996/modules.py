4d249975efb6e025dd4acdcde0b625ad0069d24a
jonathanjross <jonathan.ross@uqconnect.edu.au>
2022-10-10 12:25:09 +1000

    modules.py - Created file header with assumptions, key functions, and references

---
+++ recognition/vit-transformer-s4630996/modules.py
@@ -0,0 +1,17 @@
+"""
+Assumptions:
+
+Steps / Key Functions:
+1. Augment Data 
+2. Create Patches
+3. Embed Patches
+4. Create MLP 
+5. Create Transformer Encoder
+5. Create ViT
+
+References:
+1) https://keras.io/examples/vision/image_classification_with_vision_transformer/
+2) https://towardsdatascience.com/understand-and-implement-vision-transformer-with-tensorflow-2-0-f5435769093
+
+"""
+
