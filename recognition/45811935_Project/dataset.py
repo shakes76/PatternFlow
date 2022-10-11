c14acb15f43c994ce766f8d5819def040a55e412
arkr01 <s4581193@student.uq.edu.au>
2022-10-11 12:40:47 +1000

    Initial dataset.py changes

---
+++ recognition/45811935_Project/dataset.py
@@ -0,0 +1,32 @@
+"""
+    Load and pre-process all data.
+
+    Author: Adrian Rahul Kamal Rajkamal
+"""
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+FILE_PATH = "./ADNI_AD_NC_2D/"
+IMG_DIMENSION = 256
+BATCH_SIZE = 32
+
+
+def load_image_data(path, img_dim, batch_size):
+    """
+    Load image data (in our case, the ADNI dataset).
+
+    Args:
+        path: absolute path to image data (unzipped).
+        img_dim: Size of images (assume square)
+        batch_size: Size of each batch
+
+    Returns:
+        A tf.data.Dataset of the image files.
+
+    """
+    return image_dataset_from_directory(path,
+                                        label_mode=None,
+                                        image_size=(img_dim, img_dim),
+                                        color_mode="rgb",
+                                        batch_size=batch_size,
+                                        shuffle=True)
