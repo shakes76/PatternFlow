0c9dc73cea456009668e900c75f4934f619edb90
4lon <alon.nusem@gmail.com>
2022-10-11 18:37:48 +1000

    Added template for VAE

--- recognition/44801582_OASIS_VAE/modules.py
+++ recognition/44801582_OASIS_VAE/modules.py
@@ -1,3 +1,47 @@
 """
-Source code of the components of your model. Each component must be implementated as a class or a function
+Source code of the components of your model. Each component must be implemented as a class or a function
 """
+
+import torch
+import torch.nn as nn
+
+
+class VQVAE(nn.Module):
+    def __init__(self, latent, device):
+        super(VQVAE, self).__init__()
+
+        self.device = device
+        self.encoder = Encoder(latent)
+        self.decoder = Decoder(latent)
+
+    def forward(self, x):
+        x = x.to(self.device)
+        z = self.encoder(x)
+        return self.decoder(z)
+
+
+class Encoder(nn.Module):
+    def __init__(self, latent):
+        super(Encoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+        )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        # Not a finished implementation, need to implement re-parametrisation
+
+        return z
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent):
+        super(Decoder, self).__init__()
+
+        self.decoder = nn.Sequential(
+        )
+
+    def forward(self, z):
+        x = self.decoder(z)
+        x = torch.sigmoid(x)
+        return x
