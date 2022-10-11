ee088ae5c6ae31ec7b17e4cea9f499a647ef486d
Sanchit Jain <sanchitjain1632@gmail.com>
2022-10-11 22:37:45 +1000

    Functions added for further processing of UNET

--- recognition/s4746168_report_assessment/modules.py
+++ recognition/s4746168_report_assessment/modules.py
@@ -11,4 +11,25 @@ model = models.Sequential([
     layers.Conv2D(128, 3, padding="same", activation="relu")
 ])
 
+
+def down_sample_block(x, n_filters):
+    f = model(x, n_filters)
+    p = layers.MaxPool2D(2, 2)(f)
+    p = layers.Dropout(0.3)(p)
+
+    return f, p
+
+
+def up_sample_block(x, conv_features, n_filters):
+    # upsample
+    x = layers.Conv2DTranspose(n_filters, 3, 2, padding="same")(x)
+    # concatenate
+    x = layers.concatenate([x, conv_features])
+    # dropout
+    x = layers.Dropout(0.3)(x)
+    # Conv2D twice with ReLU activation
+    x = model(x, n_filters)
+
+    return x
+
 model.summary()
