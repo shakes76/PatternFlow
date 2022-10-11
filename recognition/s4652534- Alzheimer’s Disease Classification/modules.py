7a4ebf0a9087d6a5f9ed2da1c6486e2e1f057707
Bo Peng <b.peng1@uqconnect.edu.au>
2022-10-11 02:19:44 +0000

    implement class BasicBlock  for building the block network in ResNet

--- recognition/s4652534- Alzheimer’s Disease Classification/modules.py
+++ recognition/s4652534- Alzheimer’s Disease Classification/modules.py
@@ -24,3 +24,35 @@ def conv3x3(in_planes, out_planes, stride=1):
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
