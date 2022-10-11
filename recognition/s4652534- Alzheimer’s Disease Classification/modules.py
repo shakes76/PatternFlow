358de575bdc264f944f06b35ec7ff9c4e7c7bcf1
Bo Peng <b.peng1@uqconnect.edu.au>
2022-10-11 02:20:14 +0000

    delete conv1x1 function

--- recognition/s4652534- Alzheimer’s Disease Classification/modules.py
+++ recognition/s4652534- Alzheimer’s Disease Classification/modules.py
@@ -21,11 +21,6 @@ def conv3x3(in_planes, out_planes, stride=1):
                      padding=1, bias=False)
 
 
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
 class BasicBlock(nn.Module):
     expansion = 1
 
