00fac7f0cbb9f7f08eacc88472ffa18a6345ec88
s4696541 <s4696541@login0.compute.eait.uq.edu.au>
2022-10-11 15:10:02 +1000

    ADD: Added basic model testing and tensorflow check

--- recognition/s4696541_Classify_Alzheimers/predict.py
+++ recognition/s4696541_Classify_Alzheimers/predict.py
@@ -1,3 +1,22 @@
 """
 Example usage of trained alzheimers classification model.
 """
+import tensorflow as tf
+from modules import AlzheimerModel
+
+assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
+
+if __name__ == "__main__":
+    az_model = AlzheimerModel(
+        patch_size=16, 
+        num_layers=16,
+        num_heads=12,
+        d_model=64,
+        d_mlp=3000,
+        head_layers=1000,
+        dropout_rate=0.1,
+        num_classes=2
+    )
+    az_model.build((1, 240, 240, 3))
+
+    print(az_model.summary())
--- recognition/s4696541_Classify_Alzheimers/train.py
+++ recognition/s4696541_Classify_Alzheimers/train.py
@@ -1,3 +1,7 @@
 """
 Source code for training, validating, testing, and saving alzheimers classification model.
 """
+import tensorflow as tf
+
+tf.get_logger().setLevel('INFO')
+assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
