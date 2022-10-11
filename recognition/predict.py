2eebf1d33000fe7b998707469e46a63d62978a49
michaelholmes <michaelholmes@hcloudh.com>
2022-10-11 17:07:04 +1000

    fixed error, no image size

--- recognition/predict.py
+++ recognition/predict.py
@@ -7,9 +7,9 @@ from torch.optim import Adam
 #Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('model', help="Path to model", type=str)
-parser.add_argument('--output', '-o', help="Output path to save images to", type=int)
-parser.add_argument('--num_images', '-t', help="Number of images to create", type=int)
-parser.add_argument('--name', '-n', help="Name prefix for images", type=int)
+parser.add_argument('--output', '-o', help="Output path to save images to", type=str)
+parser.add_argument('--num_images', '-i', help="Number of images to create", type=int)
+parser.add_argument('--name', '-n', help="Name prefix for images", type=str)
 parser.add_argument('--seed', '-s', help="Random seed. Passing the same random seed yields deterministic images", type=int)
 args = parser.parse_args()
 
@@ -33,7 +33,7 @@ else:
     seed = None
 
 #create model
-model = Unet(image_size)
+model = Unet(64)
 
 #Create trainer, pass in temp parameters
 trainer = Trainer(model, img_size=64, timesteps=300, start=0.0001, end=0.02)
