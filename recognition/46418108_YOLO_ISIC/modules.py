3196618716035ef86a645b8dcc2f1394981a0908
Niklas <nikmatha@gmail.com>
2022-10-11 15:47:58 +1000

    Started to create the model architecture for YOLO. All layers are done excpect the output layer.

--- recognition/46418108_YOLO_ISIC/modules.py
+++ recognition/46418108_YOLO_ISIC/modules.py
@@ -6,15 +6,117 @@ from tensorflow.keras.models import Sequential
 import matplotlib 
 
 class YOLO():
-    def __init__(self):
-        pass
-
+    def __init__(self, height, width):
+        self._height = height
+        self._width = width
+        self._model = self.model()
 
     def model(self):
+
+        # First layer
         model = Sequential()
+        model.add(Conv2D(64, (7,7), strides=(2,2),input_shape=(self._width, self._height, 3)))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(MaxPooling2D((2, 2), strides=(2,2)))
+
+        # Second layer
+        model.add(Conv2D(192, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(MaxPooling2D((2, 2), strides=(2,2)))
+
+        # Third layer
+        model.add(Conv2D(128, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(256, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
         
+        model.add(Conv2D(256, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(512, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(MaxPooling2D((2, 2), strides=(2,2)))
+
+        # Fourth layer
+        model.add(Conv2D(256, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(512, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(256, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(512, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(256, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(512, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
 
 
+        model.add(Conv2D(256, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(512, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
 
+        model.add(Conv2D(512, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
 
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(MaxPooling2D((2, 2), strides=(2,2)))
+
+        # Fifth layer
+        model.add(Conv2D(512, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(512, (1,1), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        model.add(Conv2D(1024, (3,3), strides=(2,2), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        # Sixth layer
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+        model.add(Conv2D(1024, (3,3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(LeakyReLU(alpha=0.1))
+
+        # Output layer
+        model.add(Flatten())
+
+        model.summary()
         return model
