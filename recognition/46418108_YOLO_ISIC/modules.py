76bf866c0060b036c8e701b5c602062ec45f9e4a
Niklas <nikmatha@gmail.com>
2022-10-11 14:13:40 +1000

    Added main class for YOLO, and created the function model() where the model architecture is defined.

---
+++ recognition/46418108_YOLO_ISIC/modules.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Dense, InputLayer, Dropout, Flatten, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.models import Sequential
+import matplotlib 
+
+class YOLO():
+    def __init__(self):
+        pass
+
+
+    def model(self):
+        model = Sequential()
+        
+
+
+
+
+        return model
