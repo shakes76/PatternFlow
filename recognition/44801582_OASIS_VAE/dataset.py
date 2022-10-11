0d501b0ea21f385c03f7983ca48c41be29c22941
4lon <alon.nusem@gmail.com>
2022-10-11 18:39:39 +1000

    Corrected error in dataset class location, moved from train to dataset.

--- recognition/44801582_OASIS_VAE/dataset.py
+++ recognition/44801582_OASIS_VAE/dataset.py
@@ -1,3 +1,33 @@
 """
 Data loader for loading and preprocessing your data
 """
+import os
+from torch.utils.data import Dataset
+import torchvision
+
+
+class OASISDataset(Dataset):
+    def __init__(self, data_dir):
+        # root = "keras_png_slices_data/keras_png_slices_"
+        # if dataset == "train":
+        #     self.data = os.listdir(root + "train")
+        # elif dataset == "test":
+        #     self.data = os.listdir(root + "test")
+        # elif dataset == "validate":
+        #     self.data = os.listdir(root + "validate")
+        # else:
+        #     print("Invalid type used, defaulting to train")
+        #     self.data = os.listdir(root + "train")
+        self.data = os.listdir(data_dir)
+
+        self.transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+        ])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        image = torchvision.io.read_image(self.data[index])
+        image = self.transform(image)
+        return image
--- recognition/44801582_OASIS_VAE/train.py
+++ recognition/44801582_OASIS_VAE/train.py
@@ -3,35 +3,3 @@ Source code for training, validating, testing and saving your model. The model
 should be imported from “modules.py” and the data loader should be imported from “dataset.py”. Make
 sure to plot the losses and metrics during training.
 """
-import os
-
-import cv2
-from torch.utils.data import Dataset
-import torchvision
-
-
-class OASISDataset(Dataset):
-    def __init__(self, dir, dataset):
-        # root = "keras_png_slices_data/keras_png_slices_"
-        # if dataset == "train":
-        #     self.data = os.listdir(root + "train")
-        # elif dataset == "test":
-        #     self.data = os.listdir(root + "test")
-        # elif dataset == "validate":
-        #     self.data = os.listdir(root + "validate")
-        # else:
-        #     print("Invalid type used, defaulting to train")
-        #     self.data = os.listdir(root + "train")
-        self.data = os.listdir(dir)
-
-        self.transform = torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-        ])
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        image = torchvision.io.read_image(self.data[index])
-        image = self.transform(image)
-        return image
