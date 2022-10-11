fb1ed16a79d56aea1f0f083dd31ea5d51228dd50
Utkarsh Sharma <sharmautkarsh0504@gmail.com>
2022-10-12 09:18:32 +1000

    dataset Loader

---
+++ recognition/s4646780_OASIS_VQVAE/dataset.py
@@ -0,0 +1,38 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import cv2
+
+
+class OASISDataset(Dataset):
+    def __init__(self, root_dir, transforms=None, train=False, test=False, validation=False):
+        self.root_dir = root_dir
+        if train:
+            self.dataset = os.path.join(self.root_dir, "keras_png_slices_train")
+        if test:
+            self.dataset = os.path.join(self.root_dir, "keras_png_slices_test")
+        if validation:
+            self.dataset = os.path.join(self.root_dir, "keras_png_slices_validate")
+        self.image_paths = [os.path.join(self.dataset, name) for name in os.listdir(self.dataset)]
+        self.transform = transforms
+
+    def __len__(self):
+        return len([name for name in os.listdir(self.dataset)])
+
+    def __getitem__(self, index):
+        return 0
+
+    def __getitem__(self, idx):
+        image_filepath = self.image_paths[idx]
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image
+
+    @staticmethod
+    def image_loader(path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
