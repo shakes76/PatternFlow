29dbcb05b488988700d2a45fdf9bb1f0babb16bf
Dennis Khoo <dennis@d-i89-142-16.student.eduroam.uq.edu.au>
2022-10-11 13:21:42 +1000

    Implemented dataset module reading and processing ADNI data

--- recognition/45808140_VQVAE/dataset.py
+++ recognition/45808140_VQVAE/dataset.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
-import numpy as np
-import matplotlib.pyplot as plt
-import os
 
-h = 64
-w = 64
+def load_data(root_path):
 
+    train_data = keras.utils.image_dataset_from_directory('%s/train/' % root_path, labels=None, color_mode='rgb', batch_size=32)
+    test_data = keras.utils.image_dataset_from_directory('%s/test/' % root_path, labels=None, color_mode='rgb', batch_size=32)
+
+    #normalise images
+    train_data = train_data.map(lambda x: (x / 255.0))
+    test_data = test_data.map(lambda x: (x / 255.0))
+
+    return (train_data, test_data)
