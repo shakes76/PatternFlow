904d635074074186746b7990a7a9a251260de327
Dennis Khoo <dennis@d-i89-142-16.student.eduroam.uq.edu.au>
2022-10-12 18:16:48 +1000

    Modified dataset implementation to use grayscale, and calculate variance

--- recognition/45808140_VQVAE/dataset.py
+++ recognition/45808140_VQVAE/dataset.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
 def load_data(root_path):
 
-    train_data = keras.utils.image_dataset_from_directory('%s/train/' % root_path, labels=None, color_mode='rgb', batch_size=32)
-    test_data = keras.utils.image_dataset_from_directory('%s/test/' % root_path, labels=None, color_mode='rgb', batch_size=32)
+    train_data = keras.utils.image_dataset_from_directory('%s/train/' % root_path, labels=None, color_mode='grayscale', batch_size=32)
+    test_data = keras.utils.image_dataset_from_directory('%s/test/' % root_path, labels=None, color_mode='grayscale', batch_size=32)
 
-    #normalise images
-    train_data = train_data.map(lambda x: (x / 255.0))
-    test_data = test_data.map(lambda x: (x / 255.0))
+    #normalise training data
+    normalization_layer = tf.keras.layers.Rescaling(1./255.0)
+    train_data = train_data.map(lambda x: normalization_layer(x))
 
-    return (train_data, test_data)
+    train_variance = np.var(np.concatenate(list(train_data)))
+
+    return (train_data, test_data, train_variance)
