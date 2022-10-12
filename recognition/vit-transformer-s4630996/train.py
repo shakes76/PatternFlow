9cd3ea942fa851386e0af115e72dc7c021297da7
jonathanjross <jonathan.ross@uqconnect.edu.au>
2022-10-12 11:42:51 +1000

    train.py - updated hyperparameters for fork integration test

--- recognition/vit-transformer-s4630996/train.py
+++ recognition/vit-transformer-s4630996/train.py
@@ -27,26 +27,26 @@ def main():
 
     # data
     BATCH_SIZE = 128
-    IMAGE_SIZE = 196  # We'll resize input images to this size
+    IMAGE_SIZE = 200  # We'll resize input images to this size
     NUM_CLASS = 2
     INPUT_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 1)
 
     # patches
-    PATCH_SIZE = 14  
+    PATCH_SIZE = 10  
     NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
 
     # transformer-econder
-    PROJECTION_DIM = 768
-    NUM_HEADS = 12
-    NUM_ENCODER_LAYERS = 12
+    PROJECTION_DIM = 64
+    NUM_HEADS = 4
+    NUM_ENCODER_LAYERS = 4
 
     # mlp head
-    MLP_HEAD_UNITS = [3072]  # Size of the dense layers of the final classifier
+    MLP_HEAD_UNITS = [256]  # Size of the dense layers of the final classifier
 
     # model
     LEARNING_RATE = 0.001
     WEIGHT_DECAY = 0.0001
-    NUM_EPOCHS = 200
+    NUM_EPOCHS = 50
     DROPOUTS = {"mha": 0.2, "encoder_mlp": 0.2, "mlp_head": 0.5}
 
 
