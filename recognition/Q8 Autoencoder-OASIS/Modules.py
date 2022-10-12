7fc0069757863941b66da25304f6eca2969d62d6
unknown <william.balthes@uqconnect.edu.au>
2022-10-12 18:18:04 +1000

    Finished Convolution Layer Construction

--- recognition/Q8 Autoencoder-OASIS/Modules.py
+++ recognition/Q8 Autoencoder-OASIS/Modules.py
@@ -18,14 +18,14 @@ def VQVAE(TRAINDATA):
 #test_data=torch.tensor(x_test)#Y=(data[:,3])
 #y_test=torch.tensor(y_test)
 #test_data=np.concatenate((x_test,y_test),axis=1)
- batch_size = 10
+ batch_size = 100
  num_train = len(TRAINDATA)
  indices = list(range(num_train))
-#valid_indices=list(range(len(test_data)))
+
  np.random.shuffle(indices)
-#np.random.shuffle(valid_indices)
+
  train_index= indices
-#valid_index=valid_indices# define samplers for obtaining training and validation batches
+
  train_sampler = SubsetRandomSampler(train_index)
 
  train_loader = torch.utils.data.DataLoader(TRAINDATA, batch_size = batch_size,
@@ -34,19 +34,20 @@ def VQVAE(TRAINDATA):
 #dataloader = DataLoader(DatasetWrapper(x), batch_size=batch_size, shuffle=False)
 
  class indeed(nn.Module):
-   def __init__(self, numChannels, classes):
+   def __init__(self, numembedding, embeddingdim):
         # call the parent constructor
         super(indeed, self).__init__()
         # initialize first set of CONV => RELU => POOL layers
+        self.numembedding=numembedding
+        self.embeddingdim=embeddingdim
+        self.layer0=nn.Conv2d(3,32,kernel=3,stride=1,padding='same')
+        self.layer1=nn.Conv2d(3,64,kernel=3,stride=1,padding='same')
+        self.layer2=nn.BatchNorm2d(64)
+        self.layer3=nn.Conv2d(3,64,kernel=3,stride=1,padding='same')
+        self.layer4=nn.Conv2d(3,32,kernel=3,stride=1,padding='same')
         
-        self.layer=nn.Linear(196608,250)
-        self.layer0=nn.Conv2d(10,10,(3,3))
-        self.layer1=nn.Linear(90,10)
-        self.layer2=nn.Linear(90,10)
-        self.layer3=nn.Linear(10,100)
-        self.layer4=nn.Linear(100,250)
         
-        self.layer5=nn.Linear(250,196608)
+        
    def VAVAE(x,numembedding,embeddingdim,commitcost):
         x = x.permute(0, 2, 3, 1).contiguous()
         embedding=nn.Embedding(numembedding,embeddingdim)
@@ -55,13 +56,13 @@ def VQVAE(TRAINDATA):
         flat_input = x.view(-1, embeddingdim)
         dist=torch.tensor(np.empty((x.shape[0],embedding.weight.shape[0])))
         for i in range(0,numembedding):
-         dist[:,i]=torch.linalg.vector_norm(x.float(),embedding.weight[i,:]*torch.ones(x.shape[0],x.shape[1].float()))
+         dist[:,i]=torch.linalg.vector_norm(x.float(),embedding.weight[i,:]*torch.ones(x.shape[0],x.shape[1].float()))**2
         indexes=torch.argmin(dist,dim=1)
         quant=embedding.weight[indexes,:]
         loss1 = F.mse_loss(quant.detach(), x)
         loss2 =commitcost* F.mse_loss(quant, x.detach())
-        Loss=loss1+
-        quantized = x + (quant - x).detach()
+        Loss=loss1+loss2
+        quant = x + (quant - x).detach()
         return Loss, quant.permute(0, 3, 1, 2).contiguous()
 
 
@@ -77,38 +78,32 @@ def VQVAE(TRAINDATA):
         x=x.reshape((len(x),10,5,5))
         x=torch.nn.functional.relu(self.layer0(x))
         x=x.reshape((len(x),90))
-        #mu=self.layer1(x)
+        Loss,z=VAE(x,self.numembedding,self.embeddingdim,commitcost)
         #logvariance=self.layer2(x)
         
-        z=self.sampling(mu,logvariance)
+        #z=self.sampling(mu,logvariance)
         x=torch.nn.functional.relu(self.layer3(z))
         x=torch.sigmoid(self.layer5(torch.nn.functional.relu(self.layer4(x))))#x=self.relu0(x)
         
         
-        # return the output predictions
-        return x,mu,logvariance
+        
+        return x,Loss
  model = indeed(numChannels=1,classes=10)
  model.to(device)
  EPOCHS=12
-# initialize our optimizer and loss function
+
  opt = torch.optim.Adam(model.parameters(), lr=0.0001,weight_decay=0.0005)
- lossFn1 = nn.BCELoss(reduction='sum')
- def lossFn(loss1,mu,logvar):
-   
-   return KL+loss1
-#ACC=np.empty((EPOCHS))
+ lossFn1 = nn.BCELoss(reduction='mean')
+
+
  E=np.empty((EPOCHS))
  for e in range(0, EPOCHS):
   print(e)
-  acc=0   # set the model in training mode
+  acc=0  
   model.train()
-    # initialize the total training and validation loss
+    
   totalTrainLoss = 0
-  totalValLoss = 0
-    # initialize the number of correct predictions in the training
-    # and validation step
-  trainCorrect = 0
-  valCorrect = 0
+ 
   i=0   # loop over the training set
   for x in train_loader:
         #i=i+1
@@ -116,9 +111,9 @@ def VQVAE(TRAINDATA):
         #(x, y) = (x.to(device), y.to(device))
         # perform a forward pass and calculate the training loss
         #l=len(x)
-        pred,mu,logvar = model(x.float().to(device))
-        loss1=lossFn1(pred.to(device),x.float().to(device))
-        loss = lossFn(loss1,mu,logvar)
+        pred,Loss1 = model(x.float().to(device))
+        Loss2=lossFn1(pred.to(device),x.float().to(device))
+        loss = Loss1+Loss2
         # zero out the gradients, perform the backpropagation step,
         # and update the weights
         opt.zero_grad()
@@ -126,14 +121,14 @@ def VQVAE(TRAINDATA):
         opt.step()
         # add the loss to the total training loss so far and
         # calculate the number of correct predictions
-        totalTrainLoss += loss
+        totalbinaryentropyloss += len(x)*Loss2*256*256*3
         #m=nn.Softmax(dim=1)
         #acc=acc+np.sum(np.array(torch.argmax(m(pred.cpu()),dim=1))==torch.argmax(x[:,3072:3082],dim=1).detach().numpy())/len(x)
 
    
    
  #ACC[e]=acc/i
-  E[e]=totalTrainLoss/(len(TRAINDATA)*256*256*3)
+  E[e]=totalbinaryentropyloss/(len(TRAINDATA)*256*256*3)
  import matplotlib.pyplot as plt
  from matplotlib.pyplot import figure
 
