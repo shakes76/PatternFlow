ccc8c72d2f36b603bfbee8704597ecd895f16d9e
liamjoelsmith <liam-joel-smith@outlook.com>
2022-10-12 16:23:28 +1000

    Started README and implemented validation loss

--- recognition/StableDiffusion/README.md
+++ recognition/StableDiffusion/README.md
@@ -1,3 +1,48 @@
-# Stable Diffusion
+# Stable Diffusion on the ADNI dataset
 
-An implementation of stable diffusion.
+## Design Layout
+
+`imports.py` contains the basic imports requried throughout our implementation.
+
+`dataset.py` contains the data loader for loading and preprocessing the ADNI dataset.
+
+`modules.py` contains the implementation of the stable diffusion model.
+
+`train.py` contains the code for training, validating, testing and saving of the model.
+
+`predict.py` contains example usage of the trained stable diffusion model.
+
+## Data Loading and Preprocessing
+
+To handle data loading, we use `torch.utils.data.DataLoader` and `torch.utils.data.Dataset`. 
+`Dataset` allows for the storage of our image data, while `Dataloader` wraps an iterable around the pre-defined `Dataset`, allowing simplified access to our image data such as when training our model.
+In terms of preprocessing, we apply the transformation `torchvision.transforms.ToTensor()` within our `Dataset`, which converts a `PIL` Image with shape (HxWxC) in the range [0,255] to a  `torch.FloatTensor` with shape (CxHxW) and range [0,1]. This portion of our data loading implementation can be found within `dataset.py`.
+
+Before passing our (batches of) image data into our model, one final set of transformations is applied. These are taken from `torchvision.transforms`, and include: `Grayscale` (ensures the images only have one colour channel), `Resize` (ensure all images have same size), `CenterCrop`, and a linear scaling into [-1,1] via an applying `Lambda(lambda t: (t * 2) - 1)`.
+
+Since our aim is to generate a number of images via our Stable Diffusion model, and given that our dataset is split into brain MRI images with and without Alzheimer's disease, we chose to simply combine both partitions to increase the number of training images available.
+
+## Model Details
+
+Our model of choice is an implementation of the Stable Diffusion, as proposed in the paper https://arxiv.org/abs/2112.10752.
+
+## Training
+
+Throughout training, we implemented F1 loss via `torch.nn.functional.l1_loss`.
+
+Shown below is the generated loss curve, training for 80 epochs total.
+
+![Training Loss graph](images/L-loss-80.png)
+
+<!-- <img src="images/L-loss-80.png" alt="Training Loss graph" width=80%> -->
+
+
+
+
+## Validation
+
+## Testing
+
+## Usage
+
+## Dependencies
--- recognition/StableDiffusion/train.py
+++ recognition/StableDiffusion/train.py
@@ -3,7 +3,7 @@ from modules import *
 
 plt.style.use('ggplot')
 
-def train(trainloader):
+def train(trainloader, valloader):
     model = UNet()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f'Device: {device}')
@@ -13,6 +13,7 @@ def train(trainloader):
     epochs = 40
 
     loss_vals = []
+    val_vals  = []
 
     for epoch in range(epochs):
         model.train(True)
@@ -33,33 +34,49 @@ def train(trainloader):
 
         print(f'  [{epoch+1}, {index + 1:5d}] loss: {running_loss / len(trainloader):.4f}', flush=True)
         loss_vals.append(running_loss / len(trainloader))
+        val_vals.append(validate(valloader))
 
         # save model and update loss figure every 5 epochs
-        if epoch % 5 == 0:
-            torch.save(model, "DiffusionModel" + str(epoch))
+        if epoch % 1 == 0:
+            torch.save(model, "NewDiffusionModel" + str(epoch))
 
             # create loss graph
             fig, ax = plt.subplots()
-            plt.plot(loss_vals)
+            plt.plot(loss_vals, val_vals)
             ax.set_ylabel('Loss')
-            ax.set_title('Stable Diffusion Training Error')
+            ax.set_title('Stable Diffusion Training/Validation Error')
             ax.set_xlabel('Epoch')
+            ax.legend('Training Loss', 'Validation Loss', loc='upper right')
             plt.show()
-            plt.savefig("loss.png") 
+            plt.savefig("newest-loss.png") 
 
+def validate(valloader):
+    model = UNet()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-def validate():
-    return None
+    val_vals = []
+    running_loss = 0.0
+    for index, batch in tqdm(enumerate(valloader), total=len(valloader)):
+        batch = batch.to(device)
+        # sample t uniformally for every image in the batch
+        t = torch.randint(0, TIMESTEPS, (1,), device=device).long()
+        loss = get_loss(model, batch, t)
+        noisy_image, noise = forward_diffusion(batch, t)
+        predicted_noise = model(noisy_image, t)
+        loss = get_loss(predicted_noise, noise)
+        running_loss += loss.item()
 
-def test():
-    return None
+    print(f'  [validation loss: {running_loss / len(valloader):.4f}', flush=True)
+    val_vals.append(running_loss / len(valloader))
+    return val_vals
 
 if __name__ == "__main__":
     print("Output-------------------------------------")
     print("Loading Dataset...", flush=True)
     total_dataloader = load_dataset(batch_size=16)
+    val_dataloader   = load_dataset(batch_size=16, ad_train_path="ADNI_DATA/AD_NC/test/AD", nc_train_path="ADNI_DATA/AD_NC/test/NC")
     print("Loaded Dataset", flush=True)
     print("Training.....", flush=True)
-    train(total_dataloader)
+    train(total_dataloader, val_dataloader)
     print("Finished Training", flush=True)
-
