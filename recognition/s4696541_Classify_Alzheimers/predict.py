1fb9b0c1ba3f6e76fdd493fa30b79c42779f9057
s4696541 <s4696541@login0.compute.eait.uq.edu.au>
2022-10-12 13:08:08 +1000

    ADD: Added training for model, saving model and loading for prediction. Added .gitignore file for generated files

--- recognition/s4696541_Classify_Alzheimers/predict.py
+++ recognition/s4696541_Classify_Alzheimers/predict.py
@@ -8,17 +8,10 @@ from dataset import *
 assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
 
 if __name__ == "__main__":
-    az_model = AlzheimerModel(
-        num_patches=NUM_PATCHES, 
-        num_layers=16,
-        num_heads=12,
-        d_model=64,
-        d_mlp=3000,
-        head_layers=1000,
-        dropout_rate=0.1,
-        num_classes=2
-    )
-    az_model.build((None, NUM_PATCHES, PATCH_SIZE*PATCH_SIZE*3))
+    az_model = keras.models.load_model("az_model")
 
-    for images, labels in testing_dataset().take(1):
-        print(az_model.predict(images))
+    testing_ds = testing_dataset()
+
+    loss, acc = az_model.evaluate(testing_ds)
+
+    print(f"Model accuracy: {acc*100}, Model loss: {loss}")
--- recognition/s4696541_Classify_Alzheimers/train.py
+++ recognition/s4696541_Classify_Alzheimers/train.py
@@ -2,6 +2,63 @@
 Source code for training, validating, testing, and saving alzheimers classification model.
 """
 import tensorflow as tf
+import tensorflow.keras as keras
+from dataset import *
+import matplotlib.pyplot as plt
+
+from modules import AlzheimerModel
 
 tf.get_logger().setLevel('INFO')
 assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
+
+EPOCHS = 20
+
+if __name__ == "__main__":
+    az_model = AlzheimerModel(
+        num_patches=NUM_PATCHES, 
+        num_layers=12,
+        num_heads=12,
+        d_model=64,
+        d_mlp=3000,
+        head_layers=1000,
+        dropout_rate=0.15,
+        num_classes=2
+    )
+
+    az_model.compile(
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        optimizer=keras.optimizers.Adam(),
+        metrics=['accuracy']
+    )
+
+    train_ds = training_dataset()
+    validate_ds = validation_dataset()
+
+    history = az_model.fit(train_ds, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=validate_ds)
+
+    az_model.save("az_model")
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    
+    epochs = range(1, EPOCHS+1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.show()
+    plt.savefig("model_accuracy.png")
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
+    plt.savefig("model_loss.png")
