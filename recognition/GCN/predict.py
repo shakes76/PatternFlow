d7dc731b13fd407c79e28f797343ec372f97f8ea
maryam-m-k <maryamkkhan62@gmail.com>
2022-10-12 10:21:10 +1000

    Added imports to predict.py

---
+++ recognition/GCN/predict.py
@@ -0,0 +1,15 @@
+'''example usage of trained model: print any results and/or provide visualisations where applicable'''
+
+import modules as m
+import train as t
+# import networkx as nx
+import numpy as np
+# import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import scipy
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.module import Module
+from torch.nn.parameter import Parameter
