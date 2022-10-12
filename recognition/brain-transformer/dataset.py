fdb23d08cd4c7f10445176a9543e2f4c360af3f6
Dan <dong.lee2@uqconnect.edu.au>
2022-10-12 20:22:19 +1000

    updated dataloader to normalise output

--- recognition/brain-transformer/dataset.py
+++ recognition/brain-transformer/dataset.py
@@ -4,10 +4,16 @@ test_directory = 'test'
 train_directory = 'train'
 
 def load_test():
-    return tf.keras.utils.image_dataset_from_directory(test_directory)
+    data = tf.keras.utils.image_dataset_from_directory(test_directory)
+    norm = tf.keras.layers.Rescaling(1./255)
+    norm_data = data.map(lambda x,y:(norm(x),y))
+    return norm_data
 
 
 def load_train_data():
-    train_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(256, 240),color_mode='grayscale',seed = 12, validation_split=0.25, subset='training')
-    valid_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(256, 240),color_mode='grayscale',seed = 12, validation_split=0.25, subset='validation')
+    train_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(240, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='training')
+    valid_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(240, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='validation')
+    norm = tf.keras.layers.Rescaling(1./255)
+    train_data = train_data.map(lambda x,y:(norm(x),y))
+    valid_data = valid_data.map(lambda x,y:(norm(x),y))
     return train_data, valid_data
