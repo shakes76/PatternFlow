0b2c062e843f5ef5e8d7995ba0a2355c2a8e0e74
liamjoelsmith <liam-joel-smith@outlook.com>
2022-10-13 11:42:43 +1000

    Fixed minor bugs in predict.py

--- recognition/StableDiffusion/predict.py
+++ recognition/StableDiffusion/predict.py
@@ -2,7 +2,9 @@ from imports import *
 from dataset import ImageLoader
 from modules import *
 
+import matplotlib.animation as animation
 plt.style.use('ggplot')
+
 
 """
 Return a slightly less denoised copy of a given image via our UNet network
@@ -27,12 +29,11 @@ def reverse_diffusion_step(model, x, t, t_index):
 Reverse the diffusion process
 """
 @torch.no_grad()
-def reverse_diffusion(model, shape=(1, 1, 256, 256), plot="None"):
+def reverse_diffusion(model, plot, shape=(1, 1, 256, 256)):
     device = next(model.parameters()).device
 
-    if plot != "None":
-        fig = plt.figure(figsize=(15,15))
-        plt.axis("off")
+    fig = plt.figure(figsize=(15,15))
+    plt.axis("off")
 
     reverse_transform = Compose([
                     Lambda(lambda t: (t + 1) / 2),
@@ -41,7 +42,6 @@ def reverse_diffusion(model, shape=(1, 1, 256, 256), plot="None"):
 
     # sample noise from a Gaussian distribution
     img = torch.randn(shape, device=device)
-    imgs = []
 
     if plot == "plot_diffusion_process":
         rows = 5
@@ -49,8 +49,8 @@ def reverse_diffusion(model, shape=(1, 1, 256, 256), plot="None"):
         stepsize = int(TIMESTEPS/num_images)
         counter = 1
     elif plot == "image_grid":
-        rows = 2
-        cols = 2
+        rows = 3**2  # make this a square number
+        cols = 3
         stepsize = int(TIMESTEPS/1)
         counter = 1
     elif plot == "image_gif":
@@ -62,33 +62,43 @@ def reverse_diffusion(model, shape=(1, 1, 256, 256), plot="None"):
 
     # use our network to gradually denoise the noisy image
     # and save a copy of this progress every "stepsize" steps
-    for i in range(0, TIMESTEPS)[::-1]:
-        t = torch.full((shape[0],), i, device=device, dtype=torch.long)
-        img = reverse_diffusion_step(model, img, t, i)
-        if i % stepsize == 0:
-            if plot == "plot_diffusion_process":
-                ax = plt.subplot(rows,num_images,counter)
-                ax.axis("off")
-                plt.imshow(reverse_transform(img[0]).permute(1,2,0).detach().cpu(), cmap="gray")
-                counter+=1
-            elif plot == "image_grid":
-                ax = plt.subplot(rows,cols,counter)
-                ax.axis("off")
-                plt.imshow(reverse_transform(img[0].detach().cpu()), cmap="gray")
-                counter+=1
-            elif plot == "image_gif":
-                im = plt.imshow(reverse_transform(img[0]).permute(1,2,0).detach().cpu(), cmap="gray", animated=True)
-                ims.append([im])
-            else:
-                imgs.append(reverse_transform(img).permute(1,2,0).detach().cpu(), cmap="gray")
-    return imgs
+    for i in tqdm(range(1, rows + 1)):
+        img = torch.randn((1,1,256,256)).cuda()
+        for j in reversed(range(0, TIMESTEPS)):
+            t = torch.full((1,), j, device=device, dtype=torch.long)
+            with torch.no_grad():
+                img = reverse_diffusion_step(model, img, t, j)
+            if j % stepsize == 0:
+                if plot == "plot_diffusion_process":
+                    ax = plt.subplot(rows,num_images,counter)
+                    ax.axis("off")
+                    plt.imshow(reverse_transform(img[0]).permute(1,2,0).detach().cpu(), cmap="gray")
+                    counter+=1
+                elif plot == "image_grid":
+                    ax = plt.subplot(int(math.sqrt(rows)),cols,counter)
+                    ax.axis("off")
+                    plt.imshow(reverse_transform(img[0].permute(1,2,0).detach().cpu()), cmap="gray")
+                    counter+=1
+                elif plot == "image_gif":
+                    im = plt.imshow(reverse_transform(img[0]).permute(1,2,0).detach().cpu(), cmap="gray", animated=True)
+                    ims.append([im])
+    if plot == "plot_diffusion_process":
+        plt.show()    
+        plt.savefig("diffusion_process.png")  
+    elif plot == "image_grid":
+        plt.show()    
+        plt.savefig("image_grid.png")  
+    elif plot == "image_gif":
+        animate = animation.ArtistAnimation(fig, ims, interval=100, blit=True, repeat_delay=10000)
+        animate.save('image_gif.gif')
+        plt.show()
 
 if __name__ == "__main__":
-    model = torch.load("DiffusionModel")
+    model = torch.load("DiffusionModel")  # replace "DiffusionModel" with the path to the trained model
     """
     Predict options include:
     -> 'plot_diffusion_process'
     -> 'image_grid'
     -> 'image_gif'
     """
-    reverse_diffusion(model, "image_grid")
+    reverse_diffusion(model, plot="image_gif")
