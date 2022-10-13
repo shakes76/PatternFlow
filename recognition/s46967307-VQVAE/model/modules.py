1626628916f0b557716fab0b7493e57976d13a68
McArthur-Alford <mcarthur.alford@gmail.com>
2022-10-14 01:47:07 +1000

    Swapped GAN for PixelCNN model which seems more effective

--- recognition/s46967307-VQVAE/model/modules.py
+++ recognition/s46967307-VQVAE/model/modules.py
@@ -6,105 +6,73 @@ image_shape = (256, 256, 1)
 num_embeddings = 32
 beta = 2.0
 
+class PixelConvLayer(tf.keras.layers.Layer):
+    def __init__(self, mask_type, **kwargs):
+        super(PixelConvLayer, self).__init__()
+        self.mask_type = mask_type
+        self.conv = tf.keras.layers.Conv2D(**kwargs)
+    
+    def build(self, input_shape):
+        self.conv.build(input_shape)
 
-class GAN(tf.keras.Model):
-    def __init__(self, vq, **kwargs):
-        super(GAN, self).__init__(**kwargs)
+        kernel_shape = self.conv.kernel.get_shape()
+        self.mask = np.zeros(shape=kernel_shape)
+        self.mask[:kernel_shape[0] // 2, ...] = 1.0 # Sets points above center to 1
+        self.mask[kernel_shape[0] // 2, :kernel_shape[1] // 2, ...] = 1.0 # Sets points left of center to 0
 
-        self.vq = vq
+        # Pixelcnn uses 3 channels on mask B, but we only have grayscale data
+        # Only change is to include centre pixel in mask
+        if self.mask_type == "B":
+            self.mask[kernel_shape[0] // 2, kernel_shape[1] // 2, ...] = 1.0
+    
+    def call(self, inputs):
+        self.conv.kernel.assign(self.conv.kernel * self.mask)
+        return self.conv(inputs)
 
-        # Define generator model, should map random 32x32x1 noise to 32x32x1 indexes into
-        # the vqvae's embeddings in range [0..=embeddings]
-        input = tf.keras.layers.Input(
-            shape=(32, 32, 1), batch_size=None, name="input")
-        x = tf.keras.layers.Conv2D(
+def get_pixel_cnn(kernel_size, input_shape):
+    inputs = tf.keras.Input(shape=input_shape, dtype=tf.float64)
+    #onehot = tf.one_hot(inputs, num_embeddings)
+    x = PixelConvLayer(
+        mask_type="A",
+        filters=128,
+        kernel_size=kernel_size,
+        activation="relu",
+        padding="same")(inputs)
+
+    for _ in range(8):
+        x = PixelConvLayer(
+            mask_type="B",
             filters=32,
-            kernel_size=3,
+            kernel_size=1,
             strides=1,
-            activation='relu',
-            padding="same",
-            name="noise1")(input)
-        x = tf.keras.layers.Conv2D(
-            filters=1,
-            kernel_size=6,
-            strides=1,
-            activation=tf.keras.layers.ReLU(max_value=num_embeddings),
-            padding="same",
-            name="to_latent")(x)
-        x = tf.keras.layers.Lambda(
-           lambda y: tf.math.round(y)
-        )(x)
-
-        self.generator = tf.keras.Model(input, x, name="encoder")
-
-        # Define the discriminator model, should take a 256x256x1 image, do some shrinking
-        # convolution, and then dense layer to a single logit output from 0 to 1
-        input = tf.keras.layers.Input(
-            shape=image_shape, batch_size=None, name="input")
-        x = tf.keras.layers.Conv2D(
-            filters=32,
-            kernel_size=3,
-            strides=1,
-            activation='relu',
-            padding="same",
-            name="conv1")(input)
-        x = tf.keras.layers.Flatten()(x)
-        x = tf.keras.layers.Dense(64, activation='relu')(x)
-        x = tf.keras.layers.Dense(1, activation='softmax')(x)
-
-        self.discriminator = tf.keras.Model(input, x, name="discriminator")
-
-    def train_step(self, train_data):
-        # x is images of real brains (256,256,1)
-        x, _ = train_data
-
-        # y is random noise of size (len(x), 32, 32, 1)
-        y = tf.random.uniform((tf.shape(x)[0], 32, 32, 1))
-
-        # Generate Images
-        g = tf.cast(tf.math.round(self.generator(y)),
-                    dtype=tf.int64)  # Generated images
-        g = tf.reshape(g, shape=(8, 32, 32))
-        g = tf.gather(self.vq.get_layer(name="vq").variables[0], g)
-        g = self.vq.decoder(g)
-
-        with tf.GradientTape() as tape:
-            # Discriminate
-            rd = self.discriminator(x)  # Discriminator real inputs
-            fd = self.discriminator(g)  # Discriminator fake inputs
-
-            dl = tf.keras.losses.BinaryCrossentropy()(
-                tf.concat([tf.ones_like(rd),tf.zeros_like(fd)], axis=0), 
-                tf.concat([rd, fd], axis=0)
-            )
-
-        d_grad = tape.gradient(dl, self.discriminator.trainable_variables)
-
-        self.optimizer.apply_gradients(zip(d_grad, self.discriminator.trainable_variables))
-
-        # y is random noise of size (len(x), 32, 32, 1)
-        y = tf.random.uniform((tf.shape(x)[0], 32, 32, 1))
-
-        # Generate Images
-        g = tf.cast(tf.math.round(self.generator(y)), dtype=tf.int64)
-        g = tf.reshape(g, shape=(8, 32, 32))
-        g = tf.gather(self.vq.get_layer(name="vq").variables[0], g)
-        g = self.vq.decoder(g)
+            activation="relu",
+            padding="valid")(x)
         
-        fd = self.discriminator(g)
+        x = tf.keras.layers.BatchNormalization()(x)
 
-        with tf.GradientTape() as tape:
-            gl = self.generator(y) - tf.stop_gradient(self.generator(y) - tf.keras.losses.BinaryCrossentropy()(
-                fd,
-                tf.zeros_like(fd)
-            ))
-        
-        g_grad = tape.gradient(gl, self.generator.trainable_variables)
+    # Flatten each pixel down to the number of embeddings
+    x= tf.keras.layers.Conv2D(
+        filters=1,
+        kernel_size=1,
+        strides=1,
+        padding="valid")(x)
+    
+    return tf.keras.Model(inputs, x)
 
-        self.optimizer.apply_gradients(zip(g_grad, self.generator.trainable_variables))
+def get_indices(embeddings, inputs_flat):
+    results = tf.vectorized_map(
+        lambda y:
+        tf.vectorized_map(
+            lambda x:
+            tf.norm(tf.math.subtract(x, y)),
+            embeddings),
+        inputs_flat)
 
-        return {"discriminator_loss": dl, "generator_loss": gl }
+    results = tf.math.argmin(results, axis=1)
+    results = tf.matmul(tf.one_hot(
+        results, num_embeddings), embeddings)
 
+    return results
 class VQ(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super(VQ, self).__init__(**kwargs)
@@ -117,32 +85,17 @@ class VQ(tf.keras.layers.Layer):
                 shape=(num_embeddings, latent_dims), dtype="float32")
         )
 
-    def get_indices(self, inputs_flat):
-        results = tf.vectorized_map(
-            lambda y:
-            tf.vectorized_map(
-                lambda x:
-                tf.norm(tf.math.subtract(x, y)),
-                self.embeddings),
-            inputs_flat)
+    def call(self, inputs):
+        inputs_shape = tf.shape(inputs)
+        inputs_flat = tf.reshape(inputs, shape=(-1, latent_dims))
 
-        results = tf.math.argmin(results, axis=1)
-        results = tf.matmul(tf.one_hot(
-            results, num_embeddings), self.embeddings)
+        results = get_indices(self.embeddings, inputs_flat)
 
         codebook_loss = tf.reduce_mean(
             tf.square(tf.stop_gradient(results) - inputs_flat))
         commitment_loss = tf.reduce_mean(
             tf.square(results - tf.stop_gradient(inputs_flat))) * beta
         self.add_loss(commitment_loss + codebook_loss)
-
-        return results
-
-    def call(self, inputs):
-        inputs_shape = tf.shape(inputs)
-        inputs_flat = tf.reshape(inputs, shape=(-1, latent_dims))
-
-        results = self.get_indices(inputs_flat)
 
         # Reshape results back into compressed image
         results = tf.reshape(results, shape=inputs_shape)
--- recognition/s46967307-VQVAE/model/predict.py
+++ recognition/s46967307-VQVAE/model/predict.py
@@ -1,35 +1,78 @@
 import os
+from tkinter import W
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from modules import AE, GAN
+from modules import AE, get_pixel_cnn, latent_dims, get_indices
 from dataset import load_data
 
-model = tf.keras.models.load_model("model.ckpt")
+model: AE = tf.keras.models.load_model("model.ckpt")
+print(model.summary())
 
-#n = 5
+data = load_data()
+
+#predictions = model.predict(data["test"])
+#encoded = get_indices(model.vq.variables[0], tf.reshape(model.encoder.predict(data["test"]), shape=(-1,latent_dims)))
+#encoded = tf.reshape(encoded, shape=(-1, 32, 32))
+#
+#n = 30
 #plt.tight_layout()
 #fig, axs = plt.subplots(n, 2, figsize=(256,256))
 #for i in range(n):
-#    noise = tf.random.uniform(shape=(1,32,32,1), dtype=tf.int64, maxval=32)
-#    noise2 = tf.random.uniform(shape=(1,32,32,1), dtype=tf.int64, maxval=32)
-#    noise = tf.reshape(tf.gather(model.vq.get_layer("vq").embeddings, noise), shape=(1,32,32,8))
-#    noise2 = tf.reshape(tf.gather(model.vq.get_layer("vq").embeddings, noise2), shape=(1,32,32,8))
-#    axs[i,0].imshow(tf.reshape(model.decoder.predict(noise), shape=(256,256)))
-#    axs[i,1].imshow(tf.reshape(model.decoder.predict(noise2), shape=(256,256)))
+#    axs[i,0].imshow(predictions[i])
+#    axs[i,1].imshow(encoded[i])
 #plt.savefig("out.png", dpi=50)
+#exit()
 
-gan = GAN(model)
-gan.compile(optimizer='adam')
-print(gan.generator.summary())
-print(gan.discriminator.summary())
+pixelcnn = None
 
-data = load_data()
-gan.fit(data["train"],
-        data["train"],
-        epochs=2,
-        shuffle=True,
-        batch_size=8)
+if not os.path.exists("pixelcnn.ckpt"):
+    pixelcnn = get_pixel_cnn(kernel_size=32, input_shape=(32,32,1))
 
-gan.save("gan.ckpt")
+    # We have to quarter the amount of training data to avoid running out of vram (I think)
+    encoded = tf.reshape(model.encoder.predict(data["train"][0:int(len(data["train"])/4)]), shape=(-1, latent_dims))
+    indices = get_indices(model.vq.variables[0], encoded)
+    indices = tf.reshape(indices, shape=(-1,32,32))
+
+    pixelcnn.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.BinaryCrossentropy(),
+    )
+    pixelcnn.fit(
+        x=indices,
+        y=indices,
+        batch_size=8,
+        epochs=3,
+        validation_split=0.1
+    )
+else:
+    pixelcnn = tf.keras.models.load_model("pixelcnn.ckpt")
+    
+print(pixelcnn.summary())
+
+# Testing PixelCNN for random noise to brain:
+n = 5
+plt.tight_layout()
+fig, axs = plt.subplots(n, 3, figsize=(256,256))
+for i in range(n):
+    encoded = get_indices(model.vq.variables[0], 
+        tf.reshape(model.encoder.predict(data["train"][0:int(len(data["train"])/4)]), shape=(-1,latent_dims)))
+    indices = tf.reshape(encoded, shape=(-1, 32, 32, 1))
+    #indicies = tf.zeros_like(indices)
+
+    #indices = tf.random.uniform(shape=(1,32,32,1), dtype=tf.int64, maxval=32)
+    predicted_embeddings = pixelcnn.predict(tf.expand_dims(indices[n], 0))
+    predicted_embeddings = tf.math.round(predicted_embeddings)
+    predicted_embeddings = tf.cast(predicted_embeddings, dtype=tf.int64)
+    embeddings = tf.gather(model.vq.variables[0], predicted_embeddings)
+    embeddings = tf.reshape(embeddings, shape=(1,32,32,8))
+    prediction = model.decoder.predict(embeddings)
+    axs[i,0].imshow(tf.reshape(indices[n], shape=(32,32)))
+    axs[i,1].imshow(tf.reshape(predicted_embeddings, shape=(32,32)))
+    axs[i,2].imshow(tf.reshape(prediction, shape=(256,256)))
+
+plt.savefig("out.png", dpi=50)
+
+if not os.path.exists("pixelcnn.ckpt"):
+    pixelcnn.save("pixelcnn.ckpt")
