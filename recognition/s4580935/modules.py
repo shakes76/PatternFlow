0c4d978c525fd0dc3fa16af0a44d2ad5d6ed7521
DarkWolf45 <nicolas01carrasco@gmail.com>
2022-10-13 15:48:01 +1000

    Add class for VQVAE model

--- recognition/s4580935/modules.py
+++ recognition/s4580935/modules.py
@@ -95,3 +95,58 @@ def decoder():
     decoder.add(Conv2DTranspose(1, 3, padding="same"))
     return decoder
 
+class VQVAE(keras.models.Sequential):
+    def __init__(self, train_variance, latent_dim=256, num_embeddings=256, **kwargs):
+        super(VQVAE, self).__init__(**kwargs)
+        self.train_variance = train_variance
+        self.latent_dim = latent_dim
+        self.num_embeddings = num_embeddings
+        #Create model sequence
+        self.vqvae = VectorQuantizer(self.num_embeddings, self.latent_dim, name="vQuantiser")
+        encoder = encoder(latent_dim, name="encoder")
+        decoder = decoder(name="decoder")
+        #Construct the components for the model
+        self.add(encoder)
+        self.add(self.vqvae)
+        self.add(decoder)
+        #Initialise loss components
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.vq_loss_tracker = keras.metrics.Mean(name="vq_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.vq_loss_tracker,
+        ]
+
+    def train_step(self, x):
+        with tf.GradientTape() as tape:
+            # Outputs from the VQ-VAE.
+            reconstructions = self.vqvae(x)
+
+            # Calculate the losses.
+            reconstruction_loss = (
+                tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
+            )
+            total_loss = reconstruction_loss + sum(self.vqvae.losses)
+
+        # Backpropagation.
+        grads = tape.gradient(total_loss, self.vqvae.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.vqvae.trainable_variables))
+
+        # Loss tracking.
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.vq_loss_tracker.update_state(sum(self.vqvae.losses))
+
+        # Log results.
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "vqvae_loss": self.vq_loss_tracker.result(),
+        }
