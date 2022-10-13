8fbaf35f9d995fb0205995ece712ca17cc908749
lucenas <samlucena2@gmail.com>
2022-10-13 12:29:10 +1000

    ESPCNN: train.py remove checkpoints

--- recognition/super-resolution-ESPCNN-ADNI/train.py
+++ recognition/super-resolution-ESPCNN-ADNI/train.py
@@ -39,7 +39,6 @@ def train_model(
     train_ds: tf.data.Dataset,
     test_ds: tf.data.Dataset,
     epochs: int,
-    checkpoint: str | None = None,
 ) -> keras.Model:
     """Train the super-resolution model, saving the best checkpoint
 
@@ -47,9 +46,6 @@ def train_model(
         train_ds (tf.data.Dataset): high-res training dataset
         test_ds (tf.data.Dataset): high-res testing dataset
         epochs (int): number of epochs to train for
-        checkpoint (str | None): Optional path to a checkpoint file. If given,
-            the model will use the checkpoint specified instead of training from
-            the beginning. Defaults to None.
 
     Returns:
         keras.Model: The trained model
@@ -60,33 +56,12 @@ def train_model(
     preview_data(down_train_ds, "Training dataset: downsampled image vs target")
     preview_data(down_test_ds, "Testing dataset: downsampled image vs target")
 
-    early_stopping_callback = keras.callbacks.EarlyStopping(
-        monitor="loss",
-        patience=10
-    )
-
-    checkpoint_filename = CHECKPOINT_FILEPATH + "-{epoch:04d}.ckpt"
-
-    model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filename,
-        save_weights_only=True,
-        verbose=1,
-    )
-
     model = get_model()
     model.summary()
 
-    if checkpoint:
-        model = model.load_weights(checkpoint)
-        print(f"Loaded checkpoint weights from {checkpoint} into model")
-
     test_image = get_image_from_dataset(test_ds)
 
-    callbacks = [
-        ESPCNCallback(test_image),
-        early_stopping_callback,
-        model_checkpoint_callback
-    ]
+    callbacks = [ESPCNCallback(test_image)]
     loss_fn = keras.losses.MeanSquaredError()
     optimizer = keras.optimizers.Adam(learning_rate=0.001)
 
