468c388a979506bfe57fa5fc172460a33220a312
DarkWolf45 <nicolas01carrasco@gmail.com>
2022-10-13 13:51:02 +1000

    Make functions in dataset look better. Comment functions and have a main to test

--- recognition/s4580935/dataset.py
+++ recognition/s4580935/dataset.py
@@ -1,64 +1,63 @@
 import tensorflow as tf
-#from tensorflow import keras
-#from tensorflow.keras import datasets, layers, models, backend
-#import matplotlib.pyplot as plt
-#from PIL import Image
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, models, backend
+import matplotlib.pyplot as plt
+from PIL import Image
 import numpy as np
 import IPython.display as display
 import pathlib
 import glob
 
+#Load the data from the given file path
 def FetchData(path):
     images = sorted(glob.glob(path))
     return images
 
 def ImageExtract(images):
+    """
+    Pre-process the images for the VQVAE Model to train on
+    Return the np array of the correct dimensions needed (number of images, image height, image width)
+"""
     Array = []
     for x in images:
+        #Extract images of correct type and sizes
         sample_image = tf.io.read_file(str(x))
         sample_image = tf.image.decode_png(sample_image)
         sample_image = tf.image.convert_image_dtype(sample_image, tf.float32)
         sample_image = tf.image.resize(sample_image, [256, 256])
         y = tf.shape(sample_image)[1]
         y = y // 2
+        #add needed axis
         image = sample_image[:, y:, :]
         image = tf.cast(sample_image, tf.float32)
         image = np.squeeze(image)
         Array.append([image])
+        #Convert to appropriate np array
+        Array = np.asarray(Array)
+        Array = np.squeeze(Array)
     return Array
 
+def combine(train, test):
+    """
+    Combine the given train and test set together into a single larger test set
+    Scale image pixels
+    Return combined constructed array
+    """
+    Oasis = np.concatenate([train, test], axis=0)
+    Oasis = np.expand_dims(Oasis, -1).astype("float32") / 255
+    return Oasis
 
 
 def main():
     train_images = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_train\\*')
-    train_labels = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_seg_train\\*')
-
     test_images = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_test\\*')
-    test_labels = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_seg_test\\*')
-
     validate_images = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_validate\\*')
-    validate_labels = FetchData('D:\\University\\2022 Sem 2\\COMP3710\\Project Report\\BranchFOlder\\keras_png_slices_data\\keras_png_slices_data\\keras_png_slices_seg_validate\\*')
 
-    train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-    test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-    validate_ds = tf.data.Dataset.from_tensor_slices((validate_images, validate_labels))
-    print(len(train_images))
-    print(len(test_images))
-    print(len(validate_images))
+    train = ImageExtract(train_images)
+    test = ImageExtract(test_images)
+    validate = ImageExtract(validate_images)
 
-    train_ds = train_ds.shuffle(len(train_images))
-    val_ds = validate_ds.shuffle(len(validate_images))
-    trainArray = ImageExtract(train_images)
-    testArray = ImageExtract(test_images)
-    validateArray = ImageExtract(validate_images)
-    train = np.asarray(trainArray)
-    train = np.squeeze(train)
-
-    test = np.asarray(testArray)
-    test = np.squeeze(test)
-
-    validate = np.asarray(validateArray)
-    validate = np.squeeze(validate)
+    Oasis = combine(train, test)
 
 if __name__ =="__main__":
     main();
