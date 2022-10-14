bc1584909e70829b508d503a6a63a3b150b36813
Quantum-Kitten <Baynesdylan@gmail.com>
2022-10-14 19:21:14 +1000

    Added data and loading
    - Added the data to the repository
    - Added loading the data in dataset
    - Allows for loading a defined percentage of the data
    - Basic testing output for the data

--- recognition/s4582280-UNET/dataset.py
+++ recognition/s4582280-UNET/dataset.py
@@ -1,1 +1,78 @@
 #dataset.py
+
+# Imports
+import numpy as np
+import os, shutil
+from pathlib import Path
+from PIL import Image
+from os import listdir
+from PIL import Image
+from matplotlib import pyplot as plt
+
+# Reads the directory for images of a specified dimension and converts
+# To a stacked array of images
+# INPUT:
+#   Path
+#       Filepath for a directory of images
+# RETURN:
+#   numpy array of images
+def read_dir(path, split):
+    # Load the path into memory
+    images_dir = Path(path).expanduser()
+
+    # Reducing the size of the images so they don't nuke my ram,
+    # (I only have 8GB sorry!)
+    dim = (256, 256)
+
+    # Load list of data to check
+    X_image_train = []
+    direct = listdir(images_dir)
+    direct = direct[0:int(len(direct)*split)]
+
+    for fname in direct:
+        # Generate the file path per file name
+        fpath = os.path.join(images_dir, fname)
+        im = Image.open(fpath)
+        resized = im.resize(dim)
+        X_image_train.append(resized)
+
+    # Converting the image to numpy array
+    X_image_array=[]
+    for x in range(len(X_image_train)):
+        X_image=np.array(X_image_train[x],dtype='uint8')
+        X_image_array.append(X_image)
+
+    # Return the new array
+    return np.stack(X_image_array)
+
+# Loads the isic dataset
+# INPUT:
+#   % of data to load, size
+#       Default: 1 (100%)
+#       Load a percetange of each dataset for quicker testing, scaled 0 - 1
+# OUTPUT:
+#   training_data
+#       numpy array of training images
+#   testing_data
+#       numpy arrat of testing images
+def load_isic(size=1):
+    # Declare routes to data to download
+    training_data_route = r".\Data\ISIC-2017_Training_Data"
+    testing_data_route= r".\Data\ISIC-2017_Test_v2_Data"
+
+    # Load training data from route
+    tr_data = read_dir(training_data_route, size)
+    print("Training: ", tr_data.shape)
+
+    # Load testing data from route
+    te_data = read_dir(testing_data_route, size)
+    print("Testing: ", te_data.shape)
+    return tr_data, te_data
+    
+# Test loading data and displaying
+tr, te = load_isic(size=0.1)
+
+plt.imshow(tr[0])
+plt.show()
+plt.imshow(te[0])
+plt.show()
