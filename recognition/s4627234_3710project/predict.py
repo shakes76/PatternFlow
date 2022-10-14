faadb255b3a590ab293643da9ee6416b83a6a9c4
DaveD123321 <davedai@d-i89-148-75.student.eduroam.uq.edu.au>
2022-10-14 11:03:33 +1000

    change the folder name of the files

---
+++ recognition/s4627234_3710project/predict.py
@@ -0,0 +1,44 @@
+
+import os 
+import SimpleITK as sitk
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import train
+import dataset
+import modules
+
+def image_show(file_path, images_paths, image_num):
+    ''' Show the image_num'th image from file_path with printing out its shape'''
+    image_array=sitk.ReadImage(os.path.join(file_path,images_paths[image_num]))
+    print(sitk.GetArrayFromImage(image_array).shape)
+    plt.imshow(sitk.GetArrayFromImage(image_array))
+    plt.show() 
+
+def image_save():
+    pass
+
+def main():
+    train_x, train_y, test_x, test_y = dataset.load_dataset()
+    model = train.training()
+
+    pred = model.predict(test_x)
+    gt = tf.convert_to_tensor(test_y,dtype=tf.float32)
+    print (modules.DSC(gt,pred))
+
+    r=221
+
+    fig = plt.figure()
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    ax = fig.add_subplot(1, 3, 1)
+    ax.imshow(test_x[r])
+
+    result=pred[r]>0.5
+    ax = fig.add_subplot(1, 3, 2)
+    ax.imshow(result*255, cmap="gray")
+    ax = fig.add_subplot(1, 3, 3)
+    ax.imshow(test_y[r])
+    pass
+
+if __name__ == "main":
+    main()
---
+++ recognition/s4627234_3710project/train.py
@@ -0,0 +1,23 @@
+import dataset
+import modules
+from tensorflow.keras.optimizers import Adam
+
+
+def training():
+
+    train_x, train_y, test_x, test_y = dataset.load_dataset()
+    # print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+
+    model = modules.UNet()
+
+    # fit the model with normal learning rate
+    model.compile(optimizer = "adam", loss = modules.DSC_loss, metrics = [modules.DSC]) 
+    history = model.fit(train_x, train_y,  validation_data= (test_x, test_y),
+                            batch_size=8,shuffle='True',epochs=10)
+
+    # decrease the learning rate to do the further learning
+    model.compile(optimizer = Adam(learning_rate = 1e-4), loss = modules.DSC_loss, metrics = [modules.DSC]) 
+    history2 = model.fit(train_x, train_y,  validation_data= (test_x, test_y),
+                            batch_size=8,shuffle='True',epochs=10)
+
+    return model
