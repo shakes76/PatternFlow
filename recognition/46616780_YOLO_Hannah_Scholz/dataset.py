f015269234c73ca75bae1ecff762288977c93d6b
hannahscholz <scholzhannah5@gmail.com>
2022-10-14 13:10:40 +1000

    Working on generating bounding boxes have correct bounding box coordinates now. Plot shows correct size. Working on other images now.

--- recognition/46616780_YOLO_Hannah_Scholz/dataset.py
+++ recognition/46616780_YOLO_Hannah_Scholz/dataset.py
@@ -4,7 +4,10 @@ from os import listdir
 
 import numpy as np
 from PIL import Image as im
-from matplotlib import pyplot as plt, patches
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
+
+import cv2
 
 IMAGE_SIZE = 640
 
@@ -36,35 +39,39 @@ def load_resize_images(image_path):
         # Another function to open mask data and call the bounding box function
         print(img.size)
 
-        new_images(image_path, filename)
-
-
-def new_images(mask_directory, filename):
-    # Name of the file without jpg ending
-    name = filename.replace('.png', '')
-
-    print("File name: " + name)
-
-    # Open mask directory images
-    with im.open(mask_directory + filename) as img:
-        print(img.size)
-
-        # Need image in a numpy array
-        image_array = np.array(img)
-
         # Create figure and axes
-        fig, ax = plt.subplots()
+        plt.imshow(img)
+        ax = plt.gca()
 
-        ax.imshow(img)
-
-        bounding_box_info = generate_bounding_box(image_array)
+        bounding_box_info = generate_bounding_box(img)
 
         print(bounding_box_info)
 
+        # Bounding box width:
+        bounding_width = bounding_box_info[2] * 640
+        print(bounding_width)
+
+        bounding_height = bounding_box_info[3] * 640
+        print(bounding_height)
+
+        x = (bounding_box_info[0]*640)/2
+        y = (bounding_box_info[1]*640)/2
+        print(x)
+        print(y)
+
+        x_min = bounding_box_info[4]
+        print(x_min)
+        x_max = bounding_box_info[5]
+        print(x_max)
+
+        y_min = bounding_box_info[6]
+        print(y_min)
+
+        y_max = bounding_box_info[7]
+        print(y_max)
+
         # Create a Rectangle patch
-        rect = patches.Rectangle((bounding_box_info[0], bounding_box_info[1]), bounding_box_info[2],
-                                 bounding_box_info[3],
-                                 linewidth=1, edgecolor='r', facecolor='none')
+        rect = Rectangle((x_min, y_min), bounding_width, bounding_height, linewidth=1, edgecolor='r', facecolor='none')
 
         # Add the patch to the Axes
         ax.add_patch(rect)
@@ -75,51 +82,59 @@ def new_images(mask_directory, filename):
 
 
 def generate_bounding_box(image):
-    print(image.shape)
+    image_array = np.array(image)
 
     # Compute the xMin, xMax, yMin, yMax
     # Average xMin and xMax  and yMin and yMax to compute the centre  of the bounding box
     # Divide the x average by the width of the image and the y average by the height of the image
+    height, width = image.size
 
-    height, width = image.shape
+    # Image colours are 0 = black, 255 = white
+    # Lesion is in white
+    pix = image.load()
 
-    # argwhere funds the indices of array elements that are non-zero, grouped by element
-    mask_y_indices = np.sort(np.argwhere(image > 1)[:, 0])
-    print("Mask y indices: " + mask_y_indices)
+    x_min = 640
+    y_min = 640
 
-    mask_x_indices = np.sort(np.argwhere(image > 1)[:, 1])
-    print("Mask x indices: " + mask_x_indices)
+    x_max = 0
+    y_max = 0
 
-    # X- axis
-    x_min = mask_x_indices[0]
-    print("X min: " + x_min)
+    # Iterate through each pixel the number of pixels is size of image
+    for y in range(IMAGE_SIZE):
+        for x in range(IMAGE_SIZE):
+            # Determine if each pixel is white or black
+            if pix[x, y] == 255:
+                # Colour is white
+                if x < x_min:
+                    x_min = x
+                elif x > x_max:
+                    x_max = x
+                if y < y_min:
+                    y_min = y
+                elif y > y_max:
+                    y_max = y
 
-    x_max = mask_x_indices[-1]
-    print("x max: " + x_max)
+    # min function min()
+    print("X min: ", x_min)
+    print("X max: ", x_max)
 
-    # Y- axis
-    y_min = mask_y_indices[-1]
-    print("y min: " + y_min)
-
-    # The top left is 0
-    y_max = mask_y_indices[0]
-    print("y max: " + y_max)
+    print("Y min: ", y_min)
+    print("Y max: ", y_max)
 
     # Normalise
-    x_avg_normalised = ((x_min + x_max) / 2) / width
-    print("x avg norm: " + x_avg_normalised)
+    x_avg_normalised = ((x_min + x_max) / 2) / IMAGE_SIZE
+    print("x avg norm: ", x_avg_normalised)
 
-    y_avg_normalised = ((y_min + y_max) / 2) / height
-    print("y avg norm: " + y_avg_normalised)
-
+    y_avg_normalised = ((y_min + y_max) / 2) / IMAGE_SIZE
+    print("y avg norm: ", y_avg_normalised)
 
     # Bounding Box dimensions
     width_box = (x_max - x_min) / width
     height_box = (y_max - y_min) / height
-    print("box width: " + width_box)
-    print("box height: " + height_box)
+    print("box width: ", width_box)
+    print("box height: ", height_box)
 
-    return x_avg_normalised, y_avg_normalised, width_box, height_box
+    return x_avg_normalised, y_avg_normalised, width_box, height_box, x_min, x_max, y_min, y_max
 
 
 def clean_up_directory(image_directory):
