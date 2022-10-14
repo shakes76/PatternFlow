0dce740606d01e0f462497da6219b0aed21c93af
rohanjivrajani <88996099+rohanjivrajani@users.noreply.github.com>
2022-10-14 13:15:27 +1000

    Added logic to scale the images according to parameter

---
+++ recognition/46240091_Rohan/dataset.py
@@ -0,0 +1,19 @@
+import os
+from tensorflow import keras
+import numpy as np
+
+SHAPE = (128,128)
+
+def data_loader(dir_path, scale_flag):
+    all_files = os.listdir(dir_path)
+    image_list = []
+    for i in all_files:
+        image = keras.preprocessing.image.load_img(dir_path + "/" + i, grayscale = True, 
+        target_size=SHAPE)
+        if scale_flag:
+          image_list.append(
+                keras.preprocessing.image.img_to_array(image) / 255)
+        else:
+          image_list.append(
+                keras.preprocessing.image.img_to_array(image))
+    return np.array(image_list)
