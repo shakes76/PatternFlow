e9c8f32e8873a2cd5e0d1160e8ff443c82107549
eedward02 <109264069+eedward02@users.noreply.github.com>
2022-10-14 13:11:54 +1000

    added a function which produces comparison plots between original image, naive resizing and my output

--- recognition/s4646438_Report/predict.py
+++ recognition/s4646438_Report/predict.py
@@ -7,8 +7,25 @@ from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 class Visualiser(object):
     '''Class which allows the visualisation of the output of the model'''
+    
+    def produce_comparison_plot(self, input_path):
+      '''
+      Produces a set of images. Original image, a scaled up version using bicubic resizing and the output from my model
+      '''
+      #load in the test image, pad and resize it to pass into the model
+      original_image = img_to_array(load_img(input_path))
+      original_image = tf.image.pad_to_bounding_box(original_image, 0, 0, target_image_width, target_image_width)
+      test_image = np.expand_dims(original_image, axis=0)
+      original_image = array_to_img(original_image)
+      test_image = input_downsample(test_image, target_image_width, upsample_factor)
+      scaled = np.squeeze(test_image, axis=0)
+      scaled = array_to_img(tf.image.resize(scaled, (target_image_width, target_image_width), method='bicubic'))
+      
+      display(original_image)
+      display(scaled)
+      display(model.predict(test_image))
 
-    def plot_results(img, prefix, title):
+    def plot_results(self, img, prefix, title):
         """
         Plot the result with zoom-in area.
         FUNCTION TAKEN FROM REFERENCE 1
