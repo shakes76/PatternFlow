FIX: Reduced model image size to lower computation. Added minor batch normalisation to obtain low accuracy, working model.

recognition/s4696541_Classify_Alzheimers/dataset.py
@@ -11,11 +11,12 @@ TRAIN_FILE_PATH = "../../../AD_NC/train"
 TEST_FILE_PATH = "../../../AD_NC/test"
 
 BATCH_SIZE = 64
-IMAGE_DIM = 240
+IMAGE_DIM = 180
 IMAGE_SIZE = (IMAGE_DIM, IMAGE_DIM)
 SEED = 1337
-PATCH_SIZE = 16
+PATCH_SIZE = 10
 NUM_PATCHES = (IMAGE_DIM//PATCH_SIZE) ** 2
+D_MODEL = (PATCH_SIZE**2) * 3
 
 def training_dataset() -> tf.data.Dataset:
     dataset = keras.preprocessing.image_dataset_from_directory(TRAIN_FILE_PATH, labels='inferred', batch_size=BATCH_SIZE, image_size=IMAGE_SIZE)
@@ -47,11 +48,12 @@ if __name__ == "__main__":
     plt.figure(figsize=(10, 10))
     ds = training_dataset()
     images, label = next(iter(ds))
-    n = int(math.sqrt(images.shape[1]))
-    for i, patch in enumerate(images[0]):
-        plt.subplot(n, n, i + 1)
+    n = int(math.sqrt(NUM_PATCHES))
+    patches = make_patch(images)
+    for j, patch in enumerate(patches[0]):
+        plt.subplot(n, n, j + 1)
         patch_img = tf.reshape(patch, (PATCH_SIZE, PATCH_SIZE, 3))
         plt.imshow(patch_img.numpy().astype("uint8"))
         plt.axis("off")
 
-    plt.savefig("dataset_patching_example.png")
+    plt.savefig(f"dataset_patching_example.png")

recognition/s4696541_Classify_Alzheimers/modules.py
@@ -2,8 +2,8 @@
 Contains source code of components of alzheimers classification model.
 """
 import tensorflow as tf
-import tensorflow.keras as keras
-from tensorflow.keras import layers
+import keras
+from keras import layers
 import numpy as np
 
 from dataset import IMAGE_DIM, make_patch
@@ -12,6 +12,8 @@ class AlzheimerModel(keras.Model):
     def __init__(self, num_patches, num_layers, num_heads, d_model, d_mlp, head_layers, dropout_rate, num_classes):
         """Initialise the model"""
         super().__init__()
+
+        self.b_norm = layers.BatchNormalization()
 
         #Data augmentation
         self.augmentation = keras.Sequential([
@@ -25,27 +27,29 @@ class AlzheimerModel(keras.Model):
 
         #Nx Transformer Encoders
         self.encoders = [TransformerEncoder(num_heads, d_model, d_mlp, dropout_rate) for _ in range(num_layers)]
-
+        
         #MLP Head
         self.mlp = MultiLayerPerceptron(head_layers, d_model, dropout_rate)
 
         #OUTPUT
         self.classification = layers.Dense(num_classes, activation="softmax")
     
-    def call(self, x):
+    def call(self, x, training=True):
         """Use the model with .fit"""
-        x = self.augmentation(x)
+        x = self.augmentation(x, training=training)
 
         x = make_patch(x)
 
-        x = self.pos_encoder(x)
+        x = self.pos_encoder(x, training=training)
+
+        x = self.b_norm(x, training=training)
 
         for encoder in self.encoders:
-            x = encoder(x)
+            x = encoder(x, training=training)
         
-        head_mlp = self.mlp(x[:, 0, :])
+        x = self.mlp(x[:, 0, :], training=training)
         
-        output = self.classification(head_mlp)
+        output = self.classification(x, training=training)
 
         return output
 
@@ -56,12 +60,12 @@ class PositionalEncoder(layers.Layer):
         self.d_model = d_model
 
         self.projection = layers.Dense(d_model)
-        self.cls_token = self.add_weight("cls_token", shape=(1, 1, d_model))
+        self.cls_token = tf.Variable(initial_value=tf.random.normal([1, 1, d_model]))
         self.pos_embedding = layers.Embedding(num_patches+1, d_model)
     
-    def call(self, x):
+    def call(self, x, training=True):
         positions = tf.tile([tf.range(0, self.num_patches+1)], [tf.shape(x)[0], 1])
-        pos_emb = self.pos_embedding(positions)
+        pos_emb = self.pos_embedding(positions, training=training)
 
         linear_map = self.projection(x)
         cls_token = tf.tile(self.cls_token, [tf.shape(x)[0], 1, 1]) #[BATCH_SIZE, 1, self.d_model]
@@ -82,10 +86,10 @@ class TransformerEncoder(layers.Layer):
         self.self_attention = SelfAttention(heads, d_model, dropout_rate)
         self.multi_layer_perceptron = MultiLayerPerceptron(d_mlp, d_model, dropout_rate)
 
-    def call(self, x):
+    def call(self, x, training=True):
         """Apply the transformer encoder on data inputs"""
-        self_attention = self.self_attention(x)
-        result = self.multi_layer_perceptron(self_attention)
+        self_attention = self.self_attention(x, training=training)
+        result = self.multi_layer_perceptron(self_attention, training=training)
         return result
         
 class SelfAttention(layers.Layer):
@@ -97,10 +101,10 @@ class SelfAttention(layers.Layer):
         self.mha = layers.MultiHeadAttention(num_heads=heads, key_dim=d_model, dropout=dropout_rate)
         self.add = layers.Add()
 
-    def call(self, x):
+    def call(self, x, training=True):
         """Apply self attention on data inputs"""
-        x_norm = self.layer_normalisation(x)
-        self_attention = self.mha(query=x_norm, value=x_norm, key=x_norm)
+        x_norm = self.layer_normalisation(x, training=training)
+        self_attention = self.mha(query=x_norm, value=x_norm, key=x_norm, training=training)
 
         result = self.add([x, self_attention])
 
@@ -114,15 +118,16 @@ class MultiLayerPerceptron(layers.Layer):
         self.layer_normalisation = layers.LayerNormalization()
         self.mlp = keras.Sequential([
             layers.Dense(d_mlp, activation="gelu"),
+            layers.Dropout(dropout_rate),
             layers.Dense(d_model),
             layers.Dropout(dropout_rate)
         ])
         self.add = layers.Add()
 
-    def call(self, x):
+    def call(self, x, training=True):
         """Apply MLP to data inputs"""
-        x_norm = self.layer_normalisation(x)
-        mlp = self.mlp(x_norm)
+        x_norm = self.layer_normalisation(x, training=training)
+        mlp = self.mlp(x_norm, training=training)
         result = self.add([x, mlp])
 
         return result

recognition/s4696541_Classify_Alzheimers/predict.py
@@ -8,26 +8,7 @@ from dataset import *
 assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
 
 if __name__ == "__main__":
-    #az_model = keras.models.load_model("az_model")
-
-    az_model = AlzheimerModel(
-        num_patches=NUM_PATCHES, 
-        num_layers=6,
-        num_heads=12,
-        d_model=768,
-        d_mlp=3072,
-        head_layers=1000,
-        dropout_rate=0.2,
-        num_classes=2
-    )
-
-    az_model.compile(
-        loss=keras.losses.SparseCategoricalCrossentropy(),
-        optimizer=keras.optimizers.Adam(),
-        metrics=['accuracy']
-    )
-
-    az_model.build([1, 240, 240, 3])
+    az_model = keras.models.load_model("az_model")
 
     print("Using model:\n", az_model.summary())
 

recognition/s4696541_Classify_Alzheimers/train.py
@@ -11,25 +11,28 @@ from modules import AlzheimerModel
 tf.get_logger().setLevel('INFO')
 assert len(tf.config.list_physical_devices("GPU")) >= 1, "No GPUs found"
 
-EPOCHS = 20
+EPOCHS = 25
 
 if __name__ == "__main__":
     az_model = AlzheimerModel(
         num_patches=NUM_PATCHES, 
-        num_layers=12,
-        num_heads=12,
-        d_model=768,
-        d_mlp=3072,
-        head_layers=3072,
-        dropout_rate=0.2,
+        num_layers=6,
+        num_heads=6,
+        d_model=D_MODEL,
+        d_mlp=3000,
+        head_layers=300,
+        dropout_rate=0.35,
         num_classes=2
     )
 
     az_model.compile(
         loss=keras.losses.SparseCategoricalCrossentropy(),
-        optimizer=keras.optimizers.Adam(),
+        optimizer=keras.optimizers.Adam(0.0003),
         metrics=['accuracy']
     )
+
+    az_model.build((1, IMAGE_DIM, IMAGE_DIM, 3))
+    print(az_model.summary())
 
     train_ds = training_dataset()
     test_ds = testing_dataset()