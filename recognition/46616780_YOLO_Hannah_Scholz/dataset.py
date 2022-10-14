97a066cbba14fbf22f09e5138d535809fb21ea0c
hannahscholz <scholzhannah5@gmail.com>
2022-10-14 12:05:20 +1000

    Working on generating bounding boxes for mask images, had some errors in image size, now should be 640x640. Working with validation mask images first.

--- recognition/46616780_YOLO_Hannah_Scholz/dataset.py
+++ recognition/46616780_YOLO_Hannah_Scholz/dataset.py
@@ -4,6 +4,7 @@ from os import listdir
 
 import numpy as np
 from PIL import Image as im
+from matplotlib import pyplot as plt, patches
 
 IMAGE_SIZE = 640
 
@@ -28,10 +29,9 @@ def load_resize_images(image_path):
 
             # Change the shape of the images so all the images have a maximum axis
             # of 640 and keep the aspect ratio
-            if width > height:
-                img = img.resize((IMAGE_SIZE, round(height * (IMAGE_SIZE / width))))
-            else:
-                img = img.resize((round(width * (IMAGE_SIZE / height)), IMAGE_SIZE))
+            img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
+            # Save image in the same
+            img.save(image_path + filename)
 
         # Another function to open mask data and call the bounding box function
         print(img.size)
@@ -40,26 +40,38 @@ def load_resize_images(image_path):
 
 
 def new_images(mask_directory, filename):
-
-    masks = {}
     # Name of the file without jpg ending
     name = filename.replace('.png', '')
 
-    print(name)
-
+    print("File name: " + name)
 
     # Open mask directory images
     with im.open(mask_directory + filename) as img:
-        masks['base'] = img
-        keys = list(masks.keys())
+        print(img.size)
 
-        for key in keys:
-            new_mask = masks[key]
-            # Need image in a numpy array
-            image_array = np.array(new_mask)
+        # Need image in a numpy array
+        image_array = np.array(img)
 
-            bounding_box_info = generate_bounding_box(image_array)
-            print(bounding_box_info)
+        # Create figure and axes
+        fig, ax = plt.subplots()
+
+        ax.imshow(img)
+
+        bounding_box_info = generate_bounding_box(image_array)
+
+        print(bounding_box_info)
+
+        # Create a Rectangle patch
+        rect = patches.Rectangle((bounding_box_info[0], bounding_box_info[1]), bounding_box_info[2],
+                                 bounding_box_info[3],
+                                 linewidth=1, edgecolor='r', facecolor='none')
+
+        # Add the patch to the Axes
+        ax.add_patch(rect)
+
+        plt.show()
+
+        return
 
 
 def generate_bounding_box(image):
@@ -73,24 +85,39 @@ def generate_bounding_box(image):
 
     # argwhere funds the indices of array elements that are non-zero, grouped by element
     mask_y_indices = np.sort(np.argwhere(image > 1)[:, 0])
+    print("Mask y indices: " + mask_y_indices)
+
     mask_x_indices = np.sort(np.argwhere(image > 1)[:, 1])
+    print("Mask x indices: " + mask_x_indices)
 
     # X- axis
     x_min = mask_x_indices[0]
+    print("X min: " + x_min)
+
     x_max = mask_x_indices[-1]
+    print("x max: " + x_max)
 
     # Y- axis
     y_min = mask_y_indices[-1]
+    print("y min: " + y_min)
+
     # The top left is 0
     y_max = mask_y_indices[0]
+    print("y max: " + y_max)
 
     # Normalise
     x_avg_normalised = ((x_min + x_max) / 2) / width
+    print("x avg norm: " + x_avg_normalised)
+
     y_avg_normalised = ((y_min + y_max) / 2) / height
+    print("y avg norm: " + y_avg_normalised)
+
 
     # Bounding Box dimensions
     width_box = (x_max - x_min) / width
     height_box = (y_max - y_min) / height
+    print("box width: " + width_box)
+    print("box height: " + height_box)
 
     return x_avg_normalised, y_avg_normalised, width_box, height_box
 
@@ -108,13 +135,16 @@ def main():
     # clean_up_directory(VALIDATION_DATA_PATH)
     # clean_up_directory(TESTING_DATA_PATH)
     # clean_up_directory(TRAINING_DATA_PATH)
-    #
-    # # Resize all images
-    # load_resize_images(VALIDATION_DATA_PATH)
+
+    # Resize all images
+    load_resize_images(VALIDATION_MASK_PATH)
+
     # load_resize_images(TESTING_DATA_PATH)
     # load_resize_images(TRAINING_DATA_PATH)
 
     load_resize_images(VALIDATION_MASK_PATH)
+
+    # One hot encode into two categories
 
 
 if __name__ == '__main__':
