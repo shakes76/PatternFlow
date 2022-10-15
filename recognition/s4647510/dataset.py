befad7c0ffc5476dd37fa7f01b9593148ea22e26
Levi Hockey <41940464+lhock17@users.noreply.github.com>
2022-10-15 11:40:16 +1000

    Fixed merge conflict in dataset.py

--- recognition/s4647510/dataset.py
+++ recognition/s4647510/dataset.py
@@ -1,12 +1,7 @@
 import random
 from PIL import Image
 import numpy as np
-
-batch_size = 32
-img_path = "images"
-
-data_train = ""
-data_test = ""
+import os
 
 def preprocess(img):
     img = np.array(img).astype('float32')
@@ -14,24 +9,52 @@ def preprocess(img):
     img = img[:, :, np.newaxis]
     return img
 
-data = []
-for i in range(batch_size):
-    #randomly pick an image from the training datset
-    img = random.choice(data_train)
-    img = Image.open(img)
-    #preprocess the image
-    img = preprocess(img)
-    data.append(img)
-data = np.array(data).astype('float32')
+def load_data():
+    batch_size = 8
+    img_path = "/content/drive/MyDrive/COMP3710 Lab Report/images"
 
-test = []
-for i in range(batch_size):
-    #randomly pick an image from the training datset
-    img = random.choice(data_test)
-    img = Image.open(img)
-    #preprocess the image
-    img = preprocess(img)
-    test.append(img)
-test = np.array(test).astype('float32')
+    data_train = []
+    train_path = os.path.join(img_path, "train/")
+    for filename in os.listdir(train_path):
+        image_id = filename[5:]
+        data_train.append(os.path.join(train_path, filename))
 
-data_variance = np.var(data / 255.0)
+    data_test = []
+    test_path = os.path.join(img_path, "test/")
+    for filename in os.listdir(test_path):
+        image_id = filename[5:]
+        data_test.append(os.path.join(test_path, filename))
+        
+    data_validate = []
+    validate_path = os.path.join(img_path, "validate/")
+    for filename in os.listdir(validate_path):
+        image_id = filename[5:]
+        data_validate.append(os.path.join(validate_path, filename))
+    
+    train = []
+    for i in range(batch_size):
+        img = random.choice(data_train)
+        img = Image.open(img)
+        img = preprocess(img)
+        train.append(img)
+    train = np.array(train).astype('float32')
+
+    test = []
+    for i in range(batch_size):
+        img = random.choice(data_test)
+        img = Image.open(img)
+        img = preprocess(img)
+        test.append(img)
+    test = np.array(test).astype('float32')
+
+    validate = []
+    for i in range(batch_size):
+        img = random.choice(data_validate)
+        img = Image.open(img)
+        img = preprocess(img)
+        test.append(img)
+    validate = np.array(validate).astype('float32')
+    
+    data_variance = np.var(train / 255.0)
+    
+    return (train, test, validate)
