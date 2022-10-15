47a7c9d77b614094505acd5f93320c2cefd0915b
unknown <wangjoshua8@gmail.com>
2022-10-15 16:14:38 +1000

    Implement Patch_Layer for splitting image data into patches.

--- recognition/46965611-ADNI-Brain-Visual-Transformer/modules.py
+++ recognition/46965611-ADNI-Brain-Visual-Transformer/modules.py
@@ -6,3 +6,23 @@ Components of the visual transformer model.
 Author: Joshua Wang (Student No. 46965611)
 Date Created: 11 Oct 2022
 """
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+class Patch_Layer(Layer):
+	def __init__(self, patch_size):
+		super(Patch_Layer, self).__init__()
+		self.patch_size = patch_size
+
+	def call(self, images):
+		batch_size = tf.shape(images)[0]
+		patches = tf.image.extract_patches(
+			images=images,
+			sizes=[1, self.patch_size, self.patch_size, 1],
+			strides=[1, self.patch_size, self.patch_size, 1],
+			rates=[1, 1, 1, 1],
+			padding='VALID'
+		)
+		patch_dims = patches.shape[-1]
+		patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+		return patches
