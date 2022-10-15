600d662fcb4b80d45621f85c06f9dc0ba38b757a
MikStap <111872333+MikStap@users.noreply.github.com>
2022-10-15 14:20:17 +1000

    DataSet
    Can now load in data as keras objects

---
+++ recognition/AA_VQVAE_Mine/dataset.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_probability as tfp
+import tensorflow as tf
+
+import os
+
+
+im_root = path = os.path.join(os.getcwd(), "recognition\AA_VQVAE_Mine\DataSets\AD_NC")
+
+
+training_set = tf.keras.utils.image_dataset_from_directory(
+                        os.path.join(im_root,"train"),
+                        labels='inferred',
+                        label_mode='categorical',
+                        color_mode='grayscale',
+                        batch_size=None,
+                        image_size=(256, 256),
+                        shuffle=True,
+                        seed=46,
+                        validation_split=0.3,
+                        subset='training',
+                        interpolation='bilinear',
+                        crop_to_aspect_ratio=True
+                    )
+
+validation_set = tf.keras.utils.image_dataset_from_directory(
+                        os.path.join(im_root,"train"),
+                        labels='inferred',
+                        label_mode='categorical',
+                        color_mode='grayscale',
+                        batch_size=None,
+                        image_size=(256, 256),
+                        shuffle=True,
+                        seed=46,
+                        validation_split=0.3,
+                        subset='validation',
+                        interpolation='bilinear',
+                        crop_to_aspect_ratio=True
+                    )
+
+test_set = tf.keras.utils.image_dataset_from_directory(
+                    os.path.join(im_root,"test"),
+                    labels='inferred',
+                    label_mode='categorical',
+                    color_mode='grayscale',
+                    batch_size=None,
+                    image_size=(256, 256),
+                    shuffle=True,
+                    seed=46,
+                    interpolation='bilinear',
+                    crop_to_aspect_ratio=True
+                )
+
+class_names = training_set.class_names
+print(class_names)
+
+'''
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+x_val = np.expand_dims(x_val, -1)
+x_train_scaled = (x_train / 255.0) - 0.5
+x_test_scaled = (x_test / 255.0) - 0.5
+x_val_scaled = (x_val / 255.0) - 0.5
+
+data_variance = np.var(x_train / 255.0)
+'''
+
+
+#And plot images
+plt.figure(figsize=(10, 10))
+for images, labels in training_set.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        #plt.title(class_names[label_list[i]])
+        plt.axis("off")
+
+plt.show()
