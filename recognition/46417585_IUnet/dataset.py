2e022fcb114ea7513cfd6de9825e30b947af2bd1
James Seymour <james.seymour@cubiko.com.au>
2022-10-15 17:14:28 +1000

    dataset: use singular greyscale channel, binary encode ground truth

--- recognition/46417585_IUnet/dataset.py
+++ recognition/46417585_IUnet/dataset.py
@@ -9,9 +9,9 @@ def parsed_dataset(pathname):
     return (
         tf.data.Dataset.from_tensor_slices(sorted(glob(pathname)))
         .map(tf.io.read_file)
-        .map(tf.image.decode_jpeg)
+        .map(lambda img: tf.io.decode_png(img, channels=1))
         .map(lambda img: tf.image.resize(img, (256, 256)))
-        .map(lambda img: divide(img, 255))
+        .map(lambda img: divide(img, 255))  # Normalise greyscale colour -> [0, 1]
     )
 
 
@@ -19,10 +19,20 @@ x_test, y_test, x_train, y_train, x_validation, y_validation = map(
     parsed_dataset,
     [
         "isic_dataset/ISIC-2017_Test_v2_Data/ISIC_*.jpg",
-        "isic_dataset/ISIC-2017_Test_v2_Data/ISIC_*.png",
+        "isic_dataset/ISIC-2017_Test_v2_Part1_GroundTruth/ISIC_*.png",
         "isic_dataset/ISIC-2017_Training_Data/ISIC_*.jpg",
-        "isic_dataset/ISIC-2017_Training_Data/ISIC_*.png",
+        "isic_dataset/ISIC-2017_Training_Part1_GroundTruth/ISIC_*.png",
         "isic_dataset/ISIC-2017_Validation_Data/ISIC_*.jpg",
-        "isic_dataset/ISIC-2017_Validation_Data/ISIC_*.png",
+        "isic_dataset/ISIC-2017_Validation_Part1_GroundTruth/ISIC_*.png",
     ],
+)
+
+
+def ground_truth_postprocess(dataset: tf.data.Dataset):
+    "For a dataset of images, binary encodes each pixel in the image"
+    return dataset.map(lambda img: tf.cast(img == 1.0, tf.uint8))
+
+
+y_test, y_train, y_validation = map(
+    ground_truth_postprocess, [y_test, y_train, y_validation]
 )
