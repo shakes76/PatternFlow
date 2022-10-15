b01dcc344dfd26db0ab05ad0b9fb6b56ecfc729d
Kai Zhen <k.zhen@uq.net.au>
2022-10-16 02:25:30 +1000

    updated config.

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ TRES = 256                                            # target resolution
 BSIZE = (16, 16, 16, 16, 16, 8, 8)                    # batch size of each resolution
 FILTERS = (256, 256, 256, 256, 128, 64, 32)           # number of filters of each resolution
 EPOCHS = {
-    0: 50,                                            # ephocs of base model
-    1: (20, 10),                                      # ephocs of 8x8 fade in and stabilize
-    2: (20, 20),                                      # ephocs of 16x16 fade in and stabilize
-    3: (20, 20),                                      # ephocs of 32x32 fade in and stabilize
+    0: 30,                                            # ephocs of base model
+    1: (20, 20),                                      # ephocs of 8x8 fade in and stabilize
+    2: (25, 20),                                      # ephocs of 16x16 fade in and stabilize
+    3: (25, 20),                                      # ephocs of 32x32 fade in and stabilize
     4: (30, 20),                                      # ephocs of 64x64 fade in and stabilize
     5: (30, 30),                                      # ephocs of 128x128 fade in and stabilize
     6: (30, 30)                                       # ephocs of 256x256 fade in and stabilize
-    }                                                 
+    }                                                   
 
 NSAMPLES = 25                                      # number of output images must be a number with int sqrt
 TRAINING_IMAGE_DIR = 'your training Images directory' # training image folder
