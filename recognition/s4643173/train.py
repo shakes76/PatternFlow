255c4e8b008b75f36c885961802a7ed2153eb4fe
Hrishikesh Bhatt <bhatthrishikesh123@gmail.com>
2022-10-15 11:32:58 +1000

    Created a fit function in train.py that loads the data, creates the model and uses the train function to train the model for a set number of epochs.

--- recognition/s4643173/train.py
+++ recognition/s4643173/train.py
@@ -1,9 +1,20 @@
 import torch
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+from modules import *
+from dataset import *
 
 torch.manual_seed(3710)
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+NUM_EPOCHS = 20
+BATCH_SIZE_TRAIN = 32
+BATCH_SIZE_TEST = 16
+LEARNING_RATE = 3e-4
+NUM_CHANNELS = 3
+D = 256
+K = 512
 
 def train(dataloader, model, optimiser):
     for real_images, _ in dataloader:
@@ -16,3 +27,27 @@ def train(dataloader, model, optimiser):
         loss = recon_error + vq_loss
         loss.backward()
         optimiser.step()
+
+def fit():
+    train_loader, test_loader = get_data(BATCH_SIZE_TRAIN, BATCH_SIZE_TEST)
+
+    model = VQVA(NUM_CHANNELS, D, K).to(DEVICE)
+    optimiser = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    
+    fixed_images, _ = next(iter(test_loader))
+    fixed_grid = make_grid(fixed_images, nrow=8, normalize=True)
+    plt.figure(figsize=(12, 12))
+    plt.axis('off')
+    plt.imshow(np.transpose(fixed_grid, (1, 2, 0)))
+    plt.show()
+
+    for epoch in range(NUM_EPOCHS):
+        train(train_loader, model, optimiser)
+
+        reconstruction = generate_samples(fixed_images, model).cpu()
+        print('SSIM:', ssim(fixed_images, reconstruction))
+        grid = make_grid(reconstruction, nrow=8, normalize=True)
+        plt.figure(figsize=(12, 12))
+        plt.axis('off')
+        plt.imshow(np.transpose(grid, (1, 2, 0)))
+        plt.show()
