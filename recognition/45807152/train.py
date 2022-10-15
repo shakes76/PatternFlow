bef488e8ec413ea299a20a9b6fa7ac81e97b7cbd
George Laugher <george_laugher@outlook.com>
2022-10-15 17:38:28 +1000

    Added essential imports required.

---
+++ recognition/45807152/train.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from PIL import Image
+
+training_path = "../../ISIC-2017_Training_Data/ISIC_0000000"
+
+# img = tf.io.decode_png(training_path)
+
+print(training_path)
