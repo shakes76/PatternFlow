d514bed935e3a12f3ed003963e21b5fefa5de252
James Seymour <james.seymour@cubiko.com.au>
2022-10-15 16:17:15 +1000

    dataset: include ground truth data, cleaner parsing

--- recognition/46417585_IUnet/dataset.py
+++ recognition/46417585_IUnet/dataset.py
@@ -1,18 +1,28 @@
 import tensorflow as tf
 
-# fmt: off
-test = tf.data.Dataset.list_files("isic_dataset/ISIC-2017_Test_v2_Data/ISIC_*.jpg")
-train = tf.data.Dataset.list_files("isic_dataset/ISIC-2017_Training_Data/ISIC_*.jpg")
-validation = tf.data.Dataset.list_files("isic_dataset/ISIC-2017_Validation_Data/ISIC_*.jpg")
-# fmt: on
 
-
-def parse_dataset(dataset: tf.data.Dataset):
+def parsed_dataset(pathname):
+    "With a glob string containing image filepaths, read and decode images, resize them to a 256x256, and normalise colour"
+    from glob import glob
     from operator import truediv as divide
 
     return (
-        dataset.map(tf.io.read_file)
+        tf.data.Dataset.from_tensor_slices(sorted(glob(pathname)))
+        .map(tf.io.read_file)
         .map(tf.image.decode_jpeg)
         .map(lambda img: tf.image.resize(img, (256, 256)))
         .map(lambda img: divide(img, 255))
     )
+
+
+x_test, y_test, x_train, y_train, x_validation, y_validation = map(
+    parsed_dataset,
+    [
+        "isic_dataset/ISIC-2017_Test_v2_Data/ISIC_*.jpg",
+        "isic_dataset/ISIC-2017_Test_v2_Data/ISIC_*.png",
+        "isic_dataset/ISIC-2017_Training_Data/ISIC_*.jpg",
+        "isic_dataset/ISIC-2017_Training_Data/ISIC_*.png",
+        "isic_dataset/ISIC-2017_Validation_Data/ISIC_*.jpg",
+        "isic_dataset/ISIC-2017_Validation_Data/ISIC_*.png",
+    ],
+)
