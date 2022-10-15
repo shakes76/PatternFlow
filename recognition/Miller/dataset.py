13144c6100173e3479054a2c1748c0a160da666a
dapmiller <dapmiller244@gmail.com>
2022-10-15 14:42:42 +1000

    Add comments to function for explanation and understanding in dataset.py file for normalising and preprocessing training data

--- recognition/Miller/dataset.py
+++ recognition/Miller/dataset.py
@@ -1,47 +1,52 @@
 """
 dataset.py" containing the data loader for loading and preprocessing your data
 """
+
 import tensorflow as tf
-import pathlib
 import glob
 import numpy as np
 from matplotlib import pyplot
 from matplotlib import image
 
-# Load Data and Process it
-# Download the Oasis Data
+# Download the Oasis Data as zip file. Will need to extract it manually afterwards
 def download_oasis ():
     
     dataset_url = "https://cloudstor.aarnet.edu.au/plus/s/n5aZ4XX1WBKp6HZ/download"
-    data_dir = tf.keras.utils.get_file(origin=dataset_url,fname='oa-sis' ,untar=True)
-    data_dir = pathlib.Path(data_dir)
-    data_dir = data_dir
+    # Download file from URL Path, origin=path, fname=file name, untar=compress file
+    tf.keras.utils.get_file(origin=dataset_url,fname='oa-sis' ,untar=True)
+  
 
-    # unzip data to current directory 
-    #! unzip /root/.keras/datasets/oa-sis.tar.gz
-
-# Loads the training images (non segmented) in the path and store in numpy array
+# Loads the training images (non segmented) from given path and returns an numpy array of arrays
 def load_training (path):
     image_list = []
 
     # Iterate through all paths and convert to 'png'
     for filename in glob.glob(path + '/*.png'): 
-        im=image.imread (filename)
+        # Read an image from the given filename into an array
+        im = image.imread (filename)
+        # Append array to list
         image_list.append(im)
 
-    print('train_X shape:',np.array(image_list).shape)
+    print('train_X shape:', np.array(image_list).shape)
+
+    # Create array of arrays
     train_set = np.array(image_list, dtype=np.float32)
     return train_set
-"""
+
 # Normalizes training images and adds 4th dimention 
 def process_training (data_set):
-    train_set = data_set
-    train_set = (train_set - np.mean(train_set))/ np.std(train_set)
-    train_set= (train_set- np.amin(train_set))/ np.amax(train_set- np.amin(train_set))
-    train_set = train_set [:,:,:,np.newaxis]
-    
-    return train_set
 
+    # Calculate the residuals of the data - each residual is dist from each distribution mean which is now zero
+    data_set = (data_set - np.mean(data_set)) / np.std(data_set)
+    # Rescale Data - ratio of dist of each value from min value in each dataset to range of values in each dataset -> value between (0,1) now
+    # Forces dataset to be same scale, and perseves shape of distribution -> "Squeezed and shifted to fit between 0 and 1"
+    data_set= (data_set - np.amin(data_set)) / np.amax(data_set - np.amin(data_set))
+    # Add 4th dimension
+    data_set = data_set [:,:,:,np.newaxis]
+    
+    return data_set
+
+"""
 # loads labels images and map pixel values to class indices and convert image data type to unit8 
 def load_labels (path):
     image_list =[]
