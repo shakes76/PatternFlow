9b4da7a502b313bf3bf1ba4a483a9c3c4399b24a
Sanchit Jain <sanchitjain1632@gmail.com>
2022-10-15 16:27:33 +1000

    Model compiled and summarised

--- recognition/s4746168_report_assessment/training.py
+++ recognition/s4746168_report_assessment/training.py
@@ -1,1 +1,9 @@
+from modules import *
 
+unet_model = build_unet_model()
+
+unet_model.compile(optimizer=tf.keras.optimizers.Adam(),
+                   loss="binary_crossentropy",
+                   metrics="accuracy")
+
+unet_model.summary()
