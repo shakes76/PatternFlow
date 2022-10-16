7080e5e618206bea8a643b3494d810f5fe0a7176
unknown <syx123123123@hotmail.com>
2022-10-17 04:14:29 +1000

    create modules.py, add Generator

---
+++ recognition/45643172_stylegan/modules.py
@@ -0,0 +1,126 @@
+import os
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+from enum import Enum
+from glob import glob
+from functools import partial
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow_addons.layers import InstanceNormalization
+
+import gdown
+from zipfile import ZipFile
+
+class Generator:
+    def __init__(self, start_res_log2, target_res_log2):
+        self.start_res_log2 = start_res_log2
+        self.target_res_log2 = target_res_log2
+        self.num_stages = target_res_log2 - start_res_log2 + 1
+        # list of generator blocks at increasing resolution
+        self.g_blocks = []
+        # list of layers to convert g_block activation to RGB
+        self.to_rgb = []
+        # list of noise input of different resolutions into g_blocks
+        self.noise_inputs = []
+        # filter size to use at each stage, keys are log2(resolution)
+        self.filter_nums = {
+            0: 512,
+            1: 512,
+            2: 512,  # 4x4
+            3: 512,  # 8x8
+            4: 512,  # 16x16
+            5: 512,  # 32x32
+            6: 256,  # 64x64
+            7: 128,  # 128x128
+            8: 64,  # 256x256
+            9: 32,  # 512x512
+            10: 16,
+        }  # 1024x1024
+
+        start_res = 2 ** start_res_log2
+        self.input_shape = (start_res, start_res, self.filter_nums[start_res_log2])
+        self.g_input = layers.Input(self.input_shape, name="generator_input")
+
+        for i in range(start_res_log2, target_res_log2 + 1):
+            filter_num = self.filter_nums[i]
+            res = 2 ** i
+            self.noise_inputs.append(
+                layers.Input(shape=(res, res, 1), name=f"noise_{res}x{res}")
+            )
+            to_rgb = Sequential(
+                [
+                    layers.InputLayer(input_shape=(res, res, filter_num)),
+                    EqualizedConv(3, 1, gain=1),
+                ],
+                name=f"to_rgb_{res}x{res}",
+            )
+            self.to_rgb.append(to_rgb)
+            is_base = i == self.start_res_log2
+            if is_base:
+                input_shape = (res, res, self.filter_nums[i - 1])
+            else:
+                input_shape = (2 ** (i - 1), 2 ** (i - 1), self.filter_nums[i - 1])
+            g_block = self.build_block(
+                filter_num, res=res, input_shape=input_shape, is_base=is_base
+            )
+            self.g_blocks.append(g_block)
+
+    def build_block(self, filter_num, res, input_shape, is_base):
+        input_tensor = layers.Input(shape=input_shape, name=f"g_{res}")
+        noise = layers.Input(shape=(res, res, 1), name=f"noise_{res}")
+        w = layers.Input(shape=512)
+        x = input_tensor
+
+        if not is_base:
+            x = layers.UpSampling2D((2, 2))(x)
+            x = EqualizedConv(filter_num, 3)(x)
+
+        x = AddNoise()([x, noise])
+        x = layers.LeakyReLU(0.2)(x)
+        x = InstanceNormalization()(x)
+        x = AdaIN()([x, w])
+
+        x = EqualizedConv(filter_num, 3)(x)
+        x = AddNoise()([x, noise])
+        x = layers.LeakyReLU(0.2)(x)
+        x = InstanceNormalization()(x)
+        x = AdaIN()([x, w])
+        return keras.Model([input_tensor, w, noise], x, name=f"genblock_{res}x{res}")
+
+    def grow(self, res_log2):
+        res = 2 ** res_log2
+
+        num_stages = res_log2 - self.start_res_log2 + 1
+        w = layers.Input(shape=(self.num_stages, 512), name="w")
+
+        alpha = layers.Input(shape=(1), name="g_alpha")
+        x = self.g_blocks[0]([self.g_input, w[:, 0], self.noise_inputs[0]])
+
+        if num_stages == 1:
+            rgb = self.to_rgb[0](x)
+        else:
+            for i in range(1, num_stages - 1):
+
+                x = self.g_blocks[i]([x, w[:, i], self.noise_inputs[i]])
+
+            old_rgb = self.to_rgb[num_stages - 2](x)
+            old_rgb = layers.UpSampling2D((2, 2))(old_rgb)
+
+            i = num_stages - 1
+            x = self.g_blocks[i]([x, w[:, i], self.noise_inputs[i]])
+
+            new_rgb = self.to_rgb[i](x)
+
+            rgb = fade_in(alpha[0], new_rgb, old_rgb)
+
+        return keras.Model(
+            [self.g_input, w, self.noise_inputs, alpha],
+            rgb,
+            name=f"generator_{res}_x_{res}",
+        )
