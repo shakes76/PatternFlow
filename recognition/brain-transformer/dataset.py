52abf5b6ce64f9ca56ee7e34fa52bc7f4994a72e
Dan <dong.lee2@uqconnect.edu.au>
2022-10-16 23:12:27 +1000

    minor bug changes

--- recognition/brain-transformer/dataset.py
+++ recognition/brain-transformer/dataset.py
@@ -11,8 +11,8 @@ def load_test():
 
 
 def load_train_data():
-    train_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(240, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='training')
-    valid_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(240, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='validation')
+    train_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(256, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='training')
+    valid_data = tf.keras.utils.image_dataset_from_directory(train_directory,image_size=(256, 256),color_mode='grayscale',seed = 12, validation_split=0.25, subset='validation')
     norm = tf.keras.layers.Rescaling(1./255)
     train_data = train_data.map(lambda x,y:(norm(x),y))
     valid_data = valid_data.map(lambda x,y:(norm(x),y))
