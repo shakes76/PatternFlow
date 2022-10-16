19f745a2214123e58bf0a01004e836be435895b4
Quantum-Kitten <Baynesdylan@gmail.com>
2022-10-16 14:06:33 +1000

    Bug fixes
    - Fixed issues with creating the model
    - Started work on getting the model to fit data

--- recognition/s4582280-UNET/modules.py
+++ recognition/s4582280-UNET/modules.py
@@ -3,32 +3,34 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_datasets as tfds
+from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
 import numpy as np
 
 # A block which creates a double convolutional layer 
 def ConvDouble(x, filters):
-    x = layers.Conv2D(filters, 3, activation="relu",
+    x = layers.Conv2D(filters, 3, padding="same", activation="relu",
                       kernel_initializer="he_normal")(x)
-    return layers.Conv2D(filters, 3, activation="relu",
+    
+    x = layers.Conv2D(filters, 3, padding="same", activation="relu",
                       kernel_initializer="he_normal")(x)
+    return x
         
 # Downsampling block for feature extraction
 def Downsample(x, filters):
     block = ConvDouble(x, filters)
     pooling = layers.MaxPool2D(2)(block)
-    pooling = layers.Dropout(0.25)(pooling)
+    pooling = layers.Dropout(0.3)(pooling)
     return block, pooling
 
 # Upsampling block for decoding/expanding
 def Upsample(x, features, filters):
     # Upsample
-    x = layers.Conv2DTranspose(filters, 3, 3, padding="same")(x)
+    x = layers.Conv2DTranspose(filters, 3, 2, padding="same")(x)
     # Concatenation
     x = layers.concatenate([x, features])
     # dropout layer
-    x = layers.Dropout(0.25)(x)
+    x = layers.Dropout(0.3)(x)
     # Double convolutional layers
     return ConvDouble(x, filters)
 
@@ -42,4 +44,19 @@ def BuildUNET():
     f2, p2 = Downsample(p1, 128)
     f3, p3 = Downsample(p2, 256)
     f4, p4 = Downsample(p3, 512)
+
+    # Bottleneck layer
+    p5 = ConvDouble(p4, 1024)
+
+    # Decoding
+    p6 = Upsample(p5, f4, 512)
+    p7 = Upsample(p6, f3, 256)
+    p8 = Upsample(p7, f2, 128)
+    p9 = Upsample(p8, f1, 64)
+
+    # Outputs
+    outputs = layers.Conv2D(3, 1, padding="same", activation="softmax")(p9)
+    return tf.keras.Model(inputs, outputs, name="U-NET")
+
+    
     
--- recognition/s4582280-UNET/train.py
+++ recognition/s4582280-UNET/train.py
@@ -1,12 +1,14 @@
 #train.py
 # All the code for training and testing the model
 from dataset import load_isic
-
+from modules import BuildUNET
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
+
+EPOCHS = 18
 
 # Load the data from file
 t, e, tr, et = load_isic(size=0.05)
@@ -20,5 +22,22 @@ def display(display_list):
         plt.imshow(display_list[i])
         plt.axis("off")
     plt.show()
+
+def DiceLoss(targets, inputs, smooth=1e-6):
     
+    #flatten label and prediction tensors
+    inputs = tf.keras.backend.flatten(inputs)
+    targets = tf.keras.backend.flatten(targets)
+    
+    intersection = tf.keras.backend.sum(tf.keras.backend.dot(targets, inputs))
+    dice = (2*intersection + smooth) / (tf.keras.backend.sum(targets) + tf.keras.backend.sum(inputs) + smooth)
+    return 1 - dice
+
 display([t[0], tr[0]])
+
+model = BuildUNET()
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=[DiceLoss],
+                  metrics="accuracy")
+
+history = model.fit(t, tr, epochs=EPOCHS)
