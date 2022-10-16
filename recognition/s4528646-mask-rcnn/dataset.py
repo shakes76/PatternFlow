583c3c3c2adbd9225444df2d94eb04dd0de8701b
Joshua Morris <joshuajohnmorris@gmail.com>
2022-10-16 12:20:10 +1000

    Compute bounding box and write main training loop

--- recognition/s4528646-mask-rcnn/dataset.py
+++ recognition/s4528646-mask-rcnn/dataset.py
@@ -8,6 +8,9 @@ import numpy as np
 import glob
 import os
 from PIL import Image
+import cv2
+
+device = torch.device("cpu")
 
 class ISICDataset(Dataset):
     """
@@ -17,10 +20,11 @@ class ISICDataset(Dataset):
         image_folder_path (str): path to a directory containing skin lesion images
         mask_folder_path (str): path to a directory containing mask images
     """
-    def __init__(self, image_folder_path, mask_folder_path, diagnoses_path):
+    def __init__(self, image_folder_path, mask_folder_path, diagnoses_path, device):
         self.diagonoses_df = pd.read_csv(diagnoses_path)
         self.mask_folder_path = mask_folder_path
         self.image_folder_path = image_folder_path
+        self.device = device
         
     def get_image_path(self, image_id):
         return os.path.join(self.image_folder_path, image_id + ".jpg")
@@ -38,6 +42,7 @@ class ISICDataset(Dataset):
             image: Tensor[C, H, W] normalised to 0-1
             label: diagnoses label, 0 if no diagnosis, 1 if melanoma, 2 if seborrheic keratosis
             mask: UInt8Tensor[N, H, W]
+            bounding box: Tensor[N, 4] bounding box generated from mask
         """
         image_id = self.diagonoses_df.iloc[idx]["image_id"]
         melanoma = self.diagonoses_df.iloc[idx]["melanoma"]
@@ -49,11 +54,32 @@ class ISICDataset(Dataset):
         else:
             label = 0
             
+        """Load image"""
         image_path = self.get_image_path(image_id)
-        mask_path = self.get_mask_path(image_id)
-        image = np.array(Image.open(image_path)) / 255.0
+        image = cv2.imread(image_path)
         image = np.swapaxes(image, 0, 2)
-        mask = np.array(Image.open(mask_path))
-        return torch.from_numpy(image), label, torch.from_numpy(mask)
         
+        """Load ground truth segmentation"""
+        mask_path = self.get_mask_path(image_id)
+        mask = cv2.imread(mask_path)
+        mask = (mask > 0).astype(np.uint8)[..., 0]
+        mask = np.expand_dims(mask, axis=0)
+        
+        """Compute bounding box"""
+        x, y, w, h = cv2.boundingRect(mask[0, ...])
+        
+        targets = {
+            "labels": torch.tensor([label], dtype=torch.int64, device=self.device), 
+            "masks": torch.from_numpy(mask).to(self.device), 
+            "boxes": torch.tensor([[x, y, x + w, y + h]], dtype=torch.float32, device=self.device)
+            }
+        return torch.from_numpy(image).to(self.device).double(), targets
+        
+    
+train_data = ISICDataset(
+    image_folder_path="./data/ISIC-2017_Training_Data", 
+    mask_folder_path="./data/ISIC-2017_Training_Part1_GroundTruth", 
+    diagnoses_path="./data/ISIC-2017_Training_Part3_GroundTruth.csv",
+    device=device,
+    )
     
--- recognition/s4528646-mask-rcnn/train.py
+++ recognition/s4528646-mask-rcnn/train.py
@@ -2,19 +2,42 @@ from dataset import *
 from modules import model
 import matplotlib.pyplot as plt
 import torch
+from tqdm import tqdm
 
-NUM_EPOCHS = 10
-print("GPU Available:", torch.cuda.is_available())
+MODE = "debug"
 
-train_data = ISICDataset("./data/ISIC-2017_Training_Data", "./data/ISIC-2017_Training_Part1_GroundTruth", "./data/ISIC-2017_Training_Part3_GroundTruth.csv")
-# train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=4, shuffle=True)
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# device = torch.device("cpu")
+model = model.to(device)
+if MODE == "debug":
+    print(device)
 
-model.eval()
-model.float()
-for i, batch in enumerate(train_data):
-    image, label, mask = batch
-    # plt.imshow(prediction[0]["masks"].detach().numpy().reshape(767, 1022, 3))
-    print(mask.shape)
-    if i > 10:
-        break
+train_data = ISICDataset(
+    image_folder_path="./data/ISIC-2017_Training_Data", 
+    mask_folder_path="./data/ISIC-2017_Training_Part1_GroundTruth", 
+    diagnoses_path="./data/ISIC-2017_Training_Part3_GroundTruth.csv",
+    device=device,
+    )
+train_dataloader = torch.utils.data.DataLoader(
+    train_data, 
+    batch_size=2, 
+    shuffle=True, 
+    collate_fn=lambda x:list(zip(*x))
+    )
 
+def train_model(model, dataloader, n_epochs):
+    model.to(device)
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
+    model.train()
+    model.double()
+    for epoch in tqdm(range(n_epochs)):
+        for images, targets in tqdm(dataloader):
+            optimizer.zero_grad()
+            losses = model(images, targets)
+            losses = sum(loss for loss in losses.values())
+            losses.backward()
+            optimizer.step()
+        
+            
+train_model(model, train_dataloader, 10)
