b1712365f1b37c56de476df4c37efe8a727e51c3
Dennis Khoo <dennis@s4580814.vpn.uq.edu.au>
2022-10-16 12:02:07 +1000

    Commented dataset

--- recognition/45808140_VQVAE/dataset.py
+++ recognition/45808140_VQVAE/dataset.py
@@ -3,15 +3,20 @@ from tensorflow import keras
 import numpy as np
 
 def load_data(root_path, batch_size=32):
+    """Function to load image data in from the root folder of where images are stored"""
 
-    train_data = keras.utils.image_dataset_from_directory('%s/train/' % root_path, labels=None, color_mode='grayscale', batch_size=batch_size)
-    test_data = keras.utils.image_dataset_from_directory('%s/test/' % root_path, labels=None, color_mode='grayscale', batch_size=batch_size)
+    #loading batched images using keras Dataset format
+    train_data = keras.utils.image_dataset_from_directory('%s/train/' % root_path, labels=None, 
+        color_mode='grayscale', batch_size=batch_size)
+    test_data = keras.utils.image_dataset_from_directory('%s/test/' % root_path, labels=None, 
+        color_mode='grayscale', batch_size=batch_size)
 
-    #normalise training data
+    #normalise train and test data
     normalization_layer = tf.keras.layers.Rescaling(1./255.0)
     train_data = train_data.map(lambda x: normalization_layer(x))
     test_data = test_data.map(lambda x: normalization_layer(x))
 
+    #calculate train data variance
     train_variance = np.var(np.concatenate(list(train_data)))
 
     return (train_data, test_data, train_variance)
