25d562d76f3e6dd18ab1382ad6f808194ffbebbb
George Laugher <george_laugher@outlook.com>
2022-10-16 23:21:31 +1000

    Added driver.ipynb for implementation testing

--- recognition/45807152/train.py
+++ recognition/45807152/train.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-import timeit
-
 from tensorflow.keras import datasets, layers, models
 
 data_path = "../../../ISIC_Data/ISIC-2017_Training_Data/*.jpg"
@@ -22,7 +20,7 @@ def prepareData(filenames):
     raw_image = tf.io.decode_jpeg(new_img, channels=3)
 
     # Resize
-    raw_image = tf.image.resize(raw_image, [100, 100])
+    raw_image = tf.image.resize_with_pad(raw_image, 480, 480)
 
     # Normalise
     raw_image = raw_image / 255.0
@@ -35,7 +33,7 @@ def prepareMasks(filenames):
     raw_image = tf.io.decode_png(new_img, channels=3)
 
     # Resize
-    raw_image = tf.image.resize(raw_image, [100, 100])
+    raw_image = tf.image.resize_with_pad(raw_image, 480, 480)
 
     # Normalise
     raw_image = raw_image / 255.0
@@ -52,13 +50,19 @@ preData = ISIC_Data.map(prepareData)
 Mask_Data = tf.data.Dataset.list_files(mask_path, shuffle=False)
 preMasks = Mask_Data.map(prepareMasks)
 
-for i in preData.take(1):
-    plt.imshow(i.numpy())
-    plt.show()
+# for i in preData.take(1):
+#     plt.imshow(i.numpy())
+#     plt.show()
 
-for j in preMasks.take(1):
-    plt.imshow(j.numpy())
-    plt.show()
+# for j in preMasks.take(1):
+#     plt.imshow(j.numpy())
+#     plt.show()
+
+im = tf.io.read_file("../../../ISIC_Data/ISIC-2017_Training_Data/ISIC_0000000.jpg")
+box = np.array([0.1, 0.2, 0.5, 0.9])
+boxes = box.reshape([1, 1, 4])
+colours = np.array([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+bbox = tf.image.draw_bounding_boxes(im, boxes, colours)
 
 # plt.figure(figsize=(1022, 767))
 # plt.imshow(images[i].numpy().astype("uint8"))
