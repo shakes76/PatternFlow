4993f823e88c9694fc5f4f32242f8f4ebece5f50
kamran healy-sheeraz <kamranhealysheeraz@hotmail.com>
2022-10-16 20:10:39 +1000

    Testing code for DataSet implementation in train.py now works. Still to implement appropriate transform compositions in dataset.py

--- recognition/Improved UNet Segmentation s4484695/train.py
+++ recognition/Improved UNet Segmentation s4484695/train.py
@@ -9,25 +9,32 @@ import torchvision
 import torchvision.transforms as transforms
 import time
 
+# Hyper-parameters
+num_epochs = 35
+learning_rate = 0.1
+batchSize = 4
+
 validDataSet = dataset.ISIC2017DataSet(r"C:\Users\kamra\OneDrive\Desktop\Uni Stuff\2022\COMP3710\Report\Small Data\Validation\Images", r"C:\Users\kamra\OneDrive\Desktop\Uni Stuff\2022\COMP3710\Report\Small Data\Validation\Labels", dataset.ISIC_Transform_Valid())
 
-train_dataloader = DataLoader(validDataSet, batch_size=4, shuffle=False)
+train_dataloader = DataLoader(validDataSet, batch_size=batchSize, shuffle=False)
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
 
-# Hyper-parameters
-num_epochs = 35
-learning_rate = 0.1
-
 # Display image and label.
 train_features, train_labels = next(iter(train_dataloader))
 print(f"Feature batch shape: {train_features.size()}")
 print(f"Labels batch shape: {train_labels.size()}")
-img = train_features[0].squeeze()
-label = train_labels[0]
-plt.imshow(img, cmap="gray")
+
+_, axs = plt.subplots(batchSize, 2)
+for row in range(batchSize):
+    img = train_features[row]
+    img = img.permute(1,2,0).numpy()
+    label = train_labels[row]
+    label = label.permute(1,2,0).numpy()
+    axs[row][0].imshow(img)
+    axs[row][1].imshow(label, cmap="gray")
+
 plt.show()
-print(f"Label: {label}")
