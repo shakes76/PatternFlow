58efcf1ae80b204d1cbdf57b00522284768b58a6
Rayve <rgreenhillbrown@gmail.com>
2022-10-16 15:44:24 +1000

    cleaned up dataset.py, renamed processing class and added function to actually process data. several miscellaneous functions added for testing purposes

--- recognition/44343309-Facebook-GCN/dataset.py
+++ recognition/44343309-Facebook-GCN/dataset.py
@@ -3,6 +3,11 @@ import scipy.sparse as sp
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelBinarizer, normalize
 
+import numpy as np 
+import scipy.sparse as sp
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelBinarizer, normalize
+
 def loadData(data):
     dataset = np.load(data)
     edges = dataset["edges"]
@@ -11,47 +16,66 @@ def loadData(data):
 
     return edges, features, target
 
+data = loadData("facebook.npz")
+
 def normaliseAdjacency(adjacency):
     adjacency += sp.eye(adjacency.shape[0])
     degree = np.array(adjacency.sum(1))
     d_hat = sp.diags(np.power(degree, -0.5).flatten())
     return d_hat.dot(adjacency).dot(d_hat).tocoo()
-class dataLoadAndProcess:
+
+class DataProcess:
   def __init__(self):
-    self.data = loadData()
-    self.edges
-    self.features
-    self.target
-    self.numNodes
-    self.numFeatures
-
-    self.dataset
-
-  def allocateData(self):
-    self.edges = self.data["edges"]
-    self.features = self.data["features"]
-    self.target = self.data["target"]
-
+    self.edges, self.features, self.target = loadData("facebook.npz")
     self.numNodes = self.features.shape[0]
     self.numFeatures = self.features.shape[1]
+
+    self.dataset = self.processing()
     
   def splitData(self):
     """
     split is a cool 80:10:10
     """
-    numTrain = len(self.numNodes)*0.8
-    numTestValid = len(self.numNodes)*0.1
+    numTrain = int(self.numNodes*0.8)
+    numTestValid = int(self.numNodes*0.1)
     trainLabels = self.target[:numTrain]
     testLabels = self.target[numTrain:numTrain + numTestValid]
     validaLabels = self.target[numTrain + numTestValid:numTrain + 2 * numTestValid]
-    print(len(trainLabels))
+    
+    trainMask = np.zeros(self.numNodes, dtype=np.bool)
+    testMask = np.zeros(self.numNodes, dtype=np.bool)
+    validaMask = np.zeros(self.numNodes, dtype=np.bool)
 
+    trainLabels[:numTrain] = True
+    testLabels[numTrain:numTrain + numTestValid] = True
+    validaLabels[numTrain + numTestValid:numTrain + 2 * numTestValid] = True
 
-  
+    return trainLabels, testLabels, validaLabels, trainMask, testMask, validaMask
+
+  def processing(self):
+    one_hot_labels = np_utils.to_categorical(self.target)
+    self.features = normalize(self.features)
+
+    adjMat = sp.coo_matrix(
+            (np.ones(self.edges.shape[0]), (self.edges[:, 0], self.edges[:, 1])),
+            shape=(self.target.shape[0], self.target.shape[0]),
+            dtype=np.float64
+        )
+
+    normalAdj = normaliseAdjacency(adjMat)
+
+    trainLabels, testLabels, validaLabels, trainMask, testMask, validaMask = self.splitData()
+    trainLabels = LabelBinarizer().fit_transform(trainLabels)
+    testLabels = LabelBinarizer().fit_transform(testLabels)
+    validaLabels = LabelBinarizer().fit_transform(validaLabels)
+    
+    return self.features, one_hot_labels, normalAdj, trainMask, validaMask, testMask, trainLabels, validaLabels, testLabels
+
 
   def getData(self):
     return self.dataset
 
+#misc functions for testing
 def showFiles(data):
   print(data.files)
   return data.files
@@ -62,13 +86,10 @@ def organiseData(data):
   target = data['target']
   return edges, features, target
 
+#def main():
+ # facebook = np.load("facebook.npz")
+ #dataset = DataLoadAndProcess()
+ # datatat = dataset.getData()
 
-
-def main():
-    data = loadData()
-    showFiles(data)
-    edges, features, target = organiseData(data)
-
-
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+    #main()
