1c976c6aad7f723e20b2e4d125dfc8e68ab59079
unknown <wangjoshua8@gmail.com>
2022-10-16 17:10:17 +1000

    Add function for building model and fix errors in other layers.

--- recognition/46965611-ADNI-Brain-Visual-Transformer/modules.py
+++ recognition/46965611-ADNI-Brain-Visual-Transformer/modules.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 from tensorflow.keras.layers import Layer
 from tensorflow.keras import layers
 
+
 class PatchLayer(Layer):
 	"""
 	Layer for transforming input images into patches.
@@ -31,34 +32,86 @@ class PatchLayer(Layer):
 		patches = tf.reshape(patches, [batch_size, -1, patch_dims])
 		return patches
 
+
 class FlattenAndEmbedPatch(Layer):
 	"""
 	Layer for projecting patches into a vector. Also adds a learnable
 	position embedding to the projected vector.
 	"""
 	def __init__(self, num_patches, projection_dim):
+		super(FlattenAndEmbedPatch, self).__init__()
 		self.num_patches = num_patches
 		self.projection_dim = projection_dim
 		self.projection = layers.Dense(units=projection_dim)
 		self.position_embedding = layers.Embedding(
-			input_dim=self.num_patches, output_dim=projection_dim
+			input_dim=self.num_patches, output_dim=self.projection_dim
 		)
 	
 	def call(self, patch):
-		positions = tf.range(0, self.num_patches)
+		positions = tf.range(0, self.num_patches, delta=1)
 		return self.projection(patch) + self.position_embedding(positions)
 
-class MLP(Layer):
-	"""
-	Multi-layer perceptron to be used in the transformer encoder.
-	"""
-	def __init__(self, previous_layer, hidden_units, dropout_rate):
-		self.x = previous_layer
-		self.hidden_units = hidden_units
-		self.dropout = dropout_rate
 
-	def call(self):
-		for units in self.hidden_units:
-			x = layers.Dense(units, activation=tf.nn.gelu)(x)
-			x = layers.Dropout(self.dropout)(x)
-		return x
+def build_vision_transformer(input_shape, patch_size, num_patches,
+		attention_heads, projection_dim, hidden_units, dropout_rate,
+		transformer_layers):
+	"""
+	Builds the vision transformer model.
+	"""
+	# Input layer
+	inputs = layers.Input(shape=input_shape)
+
+	# Convert image data into patches
+	patches = PatchLayer(patch_size)(inputs)
+	
+	# Encode patches
+	encoded_patches = FlattenAndEmbedPatch(
+		num_patches, projection_dim
+	)(patches)
+
+	# Create transformer layers
+	for _ in range(transformer_layers):
+		# First layer normalisation
+		layer_norm_1 = layers.LayerNormalization(
+			epsilon=1e-6
+		)(encoded_patches)
+
+		# Multi-head attention layer
+		attention_output = layers.MultiHeadAttention(
+			num_heads=attention_heads, key_dim=projection_dim,
+			dropout=dropout_rate
+		)(layer_norm_1, layer_norm_1)
+
+		# First skip connection
+		skip_1 = layers.Add()([attention_output, encoded_patches])
+
+		# Second layer normalisation
+		layer_norm_2 = layers.LayerNormalization(epsilon=1e-6)(skip_1)
+
+		# Multi-Layer Perceptron
+		mlp_layer = layer_norm_2
+		for units in hidden_units:
+			mlp_layer = layers.Dense(units, activation=tf.nn.gelu)(mlp_layer)
+			mlp_layer = layers.Dropout(dropout_rate)(mlp_layer)
+		
+		# Second skip connection
+		encoded_patches = layers.Add()([mlp_layer, skip_1])
+
+	# Create a [batch_size, projection_dim] tensor
+	representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+	representation = layers.Flatten()(representation)
+	representation = layers.Dropout(0.5)(representation)
+
+	# MLP layer for learning features
+	features = representation
+	for units in [2048, 1024, 512]:
+		features = layers.Dense(units, activation=tf.nn.gelu)(features)
+		features = layers.Dropout(0.5)(features)
+
+	# Classify outputs
+	logits = layers.Dense(2)(features)
+
+	# Create Keras model
+	model = tf.keras.Model(inputs=inputs, outputs=logits)
+
+	return model
