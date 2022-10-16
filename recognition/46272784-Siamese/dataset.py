91ea2bd7f1b8305074abb4d67231aa1fdbc9814f
Haolong Du <haolong.du@uqconnect.edu.au>
2022-10-17 00:46:08 +1000

    Changing back to the original method since the new method took too much memory

--- recognition/46272784-Siamese/dataset.py
+++ recognition/46272784-Siamese/dataset.py
@@ -1,76 +1,61 @@
 # This file contains the data loader
-from cgitb import grey
 import numpy as np
 import os
-import random
-from PIL import Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow import image
+from tensorflow.keras import utils
 import matplotlib.pyplot as plt
+
+def transform_images(img, label):
+    # transform to [0,1]
+    img = image.rgb_to_grayscale(img)
+    img = img / 255
+    return img, label
 
 def loadFile(dir):
     print('>> Begin data loading')
-    path = {'train_ad': os.path.join(dir, 'train/AD'),
-            'train_nc': os.path.join(dir, 'train/NC'),
-            'test_ad': os.path.join(dir, 'test/AD'),
-            'test_nc': os.path.join(dir, 'test/NC')}
+    train_dir = os.path.join(dir, 'train')
+    print('-Directory of the Training files is: {}'.format(train_dir))
+    print('\n> 1/2 Loading Training Data...')
+    train_ds = utils.image_dataset_from_directory(train_dir, 
+                                                     labels = 'inferred', 
+                                                     class_names =['AD', 'NC'],
+                                                     validation_split=0.3,
+                                                     subset="training",
+                                                     seed=1,
+                                                     image_size=(256, 240),
+                                                     shuffle=True,
+                                                     batch_size=8)
+    print('\n> 2/2 Loading Validation Data...')
+    valid_ds = utils.image_dataset_from_directory(train_dir, 
+                                                     labels = 'inferred', 
+                                                     class_names =['AD', 'NC'],
+                                                     validation_split=0.3,
+                                                     subset="validation",
+                                                     seed=1,
+                                                     image_size=(256, 240),
+                                                     shuffle=True,
+                                                     batch_size=8)
+    print('\n> Mapping datasets to greyscale...')
+    train_ds = train_ds.map(transform_images)
+    valid_ds = valid_ds.map(transform_images)
     
-    print('-Directory of the Training files of class AD is: {}'.format(path['train_ad']))
-    print('-Directory of the Training files of class NC is: {}'.format(path['train_nc']))
-    print('-Directory of the Testing files of class AD is: {}'.format(path['test_ad']))
-    print('-Directory of the Testing files of class NC is: {}'.format(path['test_nc']))
-    # print('\n> 1/6 Loading Training Data of class AD...')
-    
-    ds = {'train_ad': [],
-          'train_nc': [],
-          'test_ad': [],
-          'test_nc': [],
-          'valid_ad': [],
-          'valid_nc': []}
-    # load images in train and test folders
-    for which in path:
-        print('> Loading image in {}'.format(which))
-        for file in os.listdir(path[which]):
-            filePath = os.path.join(path[which], file)
-            # load and convert image to greyscale
-            image = np.asarray(Image.open(filePath).convert('L'))
-            if image is not None:
-                ds[which].append(image)
-    # Split training data to obtain validation data (30%)
-    print('> Generate validation set')
-    print('>> Shuffling ...')
-    random.shuffle(ds['train_ad'])
-    random.shuffle(ds['train_nc'])
-    random.shuffle(ds['test_ad'])
-    random.shuffle(ds['test_nc'])
-    train_ad = ds['train_ad']
-    train_nc = ds['train_nc']
-    print('>> Extracting image to form validation set ...')
-    valid_ad = train_ad[:round(len(train_ad)*0.3)]
-    valid_nc = train_nc[:round(len(train_nc)*0.3)]
-    train_ad = train_ad[round(len(train_ad)*0.3):]
-    train_nc = train_nc[round(len(train_nc)*0.3):]
-    ds['train_ad'] = train_ad
-    ds['train_nc'] = train_nc
-    ds['valid_ad'] = valid_ad
-    ds['valid_nc'] = valid_nc
-    print('> Completed')
-            
-    return ds['train_ad'], ds['train_nc'], ds['valid_ad'], ds['valid_nc'], ds['test_ad'], ds['test_nc']
+    print('\n>> Data loading complete')
+    return train_ds, valid_ds
     
 def plotExample(ds):
-
-    for x in ds:
-        print(x.shape)
-        print(x[100])
-        # print(len(x))
+    batch = ds.take(1)
+    for img, label in batch:
         plt.axis("off")
-        plt.imshow((x), cmap='gray', vmin=0, vmax=255)
+        plt.imshow((img.numpy()*255).astype("int32")[0], cmap='gray', vmin=0, vmax=255)
         plt.show()
         break
 
 def main():
     # Code for testing the functions
-    ta, tn, va, vn, ta, tn = loadFile('F:/AI/COMP3710/data/AD_NC/')
-    plotExample(ta)
+    t, v = loadFile('F:/AI/COMP3710/data/AD_NC/')
+    plotExample(t)
 
 if __name__ == "__main__":
     main()
