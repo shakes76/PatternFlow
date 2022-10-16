f2021abb4acaa547fd19259afaf051514d504a72
MattPChoy <matthewpchoy@gmail.com>
2022-10-16 22:07:47 +1000

    Modified dataloader to downscale images

---
+++ recognition/MattChoy-VQVAE/constants.py
@@ -0,0 +1,5 @@
+batch_size = 8
+image_size = (64, 64)
+raw_image_size = (256, 240) # Raw image size from ADNI dataset
+dataset_seed = 13
+n_channels = 3
--- recognition/MattChoy-VQVAE/dataset.py
+++ recognition/MattChoy-VQVAE/dataset.py
@@ -7,28 +7,21 @@ Setup:
 """
 
 import tensorflow as tf
-from constants import batch_size
 
 import os
 import math
 import numpy as np
 
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import array_to_img
-from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-from IPython.display import display
-from constants import image_size, batch_size, dataset_seed, n_channels
+from constants import image_size, raw_image_size, batch_size, dataset_seed, n_channels
 import matplotlib.pyplot as plt
-dataset_fp = os.path.join(os.getcwd(), "data", "ADNI")
+dataset_fp = os.path.join(os.getcwd(), "data")
 
 train_ds = image_dataset_from_directory(
     os.path.join(dataset_fp, "train"),
     batch_size=batch_size,
-    image_size=image_size,
+    image_size=raw_image_size,
     seed=dataset_seed,
     label_mode = None
 )
@@ -36,7 +29,7 @@ train_ds = image_dataset_from_directory(
 test_ds = image_dataset_from_directory(
     os.path.join(dataset_fp, "test"),
     batch_size=batch_size,
-    image_size=image_size,
+    image_size=raw_image_size,
     seed=dataset_seed,
     label_mode = None
 )
@@ -44,20 +37,27 @@ test_ds = image_dataset_from_directory(
 def scaling(input_image):
     input_image = input_image / 255.0
     input_image = tf.image.rgb_to_grayscale(input_image)
+    input_image = tf.image.resize(input_image, image_size)
     return input_image
 
 train_ds = train_ds.map(scaling)
 test_ds = test_ds.map(scaling)
 
-for batch in train_ds:
-    for im in batch:
-        print(im.shape)
-        plt.imshow(im)
-        plt.show()
-        break
-    break
+train_scaled_ds = image_dataset_from_directory(
+    os.path.join(dataset_fp, "train"),
+    batch_size=batch_size,
+    image_size=image_size,
+    seed=dataset_seed,
+    label_mode = None
+)
 
-
+def downscale(input_image):
+    input_image = input_image / 255.0
+    input_image = tf.image.rgb_to_grayscale(input_image)
+    input_image = tf.image.resize(input_image, image_size)
+    input_image -= 0.5
+    return input_image
+train_scaled_ds = train_scaled_ds.map(downscale)
 #
 # n_batches = len(train_ds)
 # n_samples = n_batches * batch_size
