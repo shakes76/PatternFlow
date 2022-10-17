bb5f0e43e432691b287e6e5130ab389bfcdb0ffc
JacobDyson01 <112358789+JacobDyson01@users.noreply.github.com>
2022-10-17 13:54:59 +1000

    Updated the data loader to include normalisation and the data and labels required for the model.

--- recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/dataset.py
+++ recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/dataset.py
@@ -2,46 +2,65 @@ import tensorflow as tf
 import glob
 import pathlib
 
-# input data folders
-training_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Data"
-test_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_Part1_GroundTruth"
-validation_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Part1_GroundTruth"
 
-size = 256
-batch_size = 16
-# training_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Data"
+def normalise_data(data):
+    data = tf.cast(data, tf.float32) / 255.0
+    return data
 
-# training_input_dir = tf.keras.utils.get_file(origin=training_input_url)
-training_labels = tf.keras.utils.image_dataset_from_directory(training_labels_dir,
+
+def normalise_label(data):
+    data = tf.cast(data, tf.float32) / 255.0
+    data = tf.math.round(data)
+    return data
+
+
+def data_loader(size, batch_size):
+    # input data folders
+    training_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Part1_GroundTruth"
+    test_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Part1_GroundTruth"
+    validation_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Part1_GroundTruth"
+    training_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Data"
+    validation_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Data"
+    test_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Data"
+
+    # size = 128
+    # batch_size = 16
+
+    training_labels = tf.keras.utils.image_dataset_from_directory(training_labels_dir,
+                                                                  labels=None,
+                                                                  color_mode='grayscale',
+                                                                  batch_size=batch_size,
+                                                                  image_size=(size, size))
+    test_labels = tf.keras.utils.image_dataset_from_directory(test_labels_dir,
                                                               labels=None,
                                                               color_mode='grayscale',
                                                               batch_size=batch_size,
                                                               image_size=(size, size))
-test_labels = tf.keras.utils.image_dataset_from_directory(test_labels_dir,
-                                                          labels=None,
-                                                          color_mode='grayscale',
-                                                          batch_size=batch_size,
-                                                          image_size=(size, size))
-validation_labels = tf.keras.utils.image_dataset_from_directory(validation_labels_dir,
+    validation_labels = tf.keras.utils.image_dataset_from_directory(validation_labels_dir,
+                                                                    labels=None,
+                                                                    color_mode='grayscale',
+                                                                    batch_size=batch_size,
+                                                                    image_size=(size, size))
+    training_data = tf.keras.utils.image_dataset_from_directory(training_input_dir,
                                                                 labels=None,
-                                                                color_mode='grayscale',
                                                                 batch_size=batch_size,
-                                                                image_size=(size, size))
-training_data = tf.keras.utils.image_dataset_from_directory(training_input_dir,
+                                                                image_size=(size, size),
+                                                                )
+    validation_data = tf.keras.utils.image_dataset_from_directory(validation_input_dir,
+                                                                  labels=None,
+                                                                  batch_size=batch_size,
+                                                                  image_size=(size, size)
+                                                                  )
+    test_data = tf.keras.utils.image_dataset_from_directory(test_input_dir,
                                                             labels=None,
                                                             batch_size=batch_size,
-                                                            image_size=(size, size),
-                                                            validation_split=0.3,
-                                                            subset="training")
-validation_data = tf.keras.utils.image_dataset_from_directory(training_input_dir,
-                                                              labels=None,
-                                                              batch_size=batch_size,
-                                                              image_size=(size, size),
-                                                              validation_split=0.15,
-                                                              subset="validation")
-validation_data = tf.keras.utils.image_dataset_from_directory(training_input_dir,
-                                                              labels=None,
-                                                              batch_size=batch_size,
-                                                              image_size=(size, size),
-                                                              validation_split=0.15,
-                                                              subset="validation")
+                                                            image_size=(size, size)
+                                                            )
+    training_data = normalise_data(training_data)
+    test_data = normalise_data(test_data)
+    validation_data = normalise_data(validation_data)
+    training_labels = normalise_label(training_labels)
+    test_labels = normalise_label(test_labels)
+    validation_labels = normalise_label(validation_labels)
+    return training_data, training_labels, test_data, test_labels, validation_data, validation_labels
+
