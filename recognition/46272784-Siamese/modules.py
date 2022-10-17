f7b168cd83da9f4a6c000b2f6b1ca029261ce613
Haolong Du <haolong.du@uqconnect.edu.au>
2022-10-17 15:31:22 +1000

    Implimented the Siamese network

--- recognition/46272784-Siamese/modules.py
+++ recognition/46272784-Siamese/modules.py
@@ -48,19 +48,40 @@ def makeCNN():
     return Model(inputs=inputs, outputs=out, name='CNN')
 
 def makeSiamese(cnn):
+    EPS = 1e-8
+    
     input_1 = layers.Input((256, 240, 1))
     input_2 = layers.Input((256, 240, 1))
     
     tower_1 = cnn(input_1)
     tower_2 = cnn(input_2)
-    # TODO Finish Siamese
+    # Merging the two networks outputs (EPS is used to avoied 0 distance)
+    distance = lambda v: tf.math.sqrt(tf.math.maximum(tf.math.reduce_sum(tf.math.square(v[0] - v[1]), axis=1, keepdims=True), EPS))
+    merge_layer = layers.Lambda(distance)([tower_1, tower_2])
+    normal_layer = layers.BatchNormalization()(merge_layer)
+    # 1 if same class, 0 if not
+    output_layer = layers.Dense(1, activation="sigmoid")(normal_layer)
+    
+    return Model(inputs=[input_1, input_2], outputs=output_layer, name='Siamese')
 
+
+def loss(margin=1):
+    def contrastive_loss(y_true, y_pred):
+        return tf.math.reduce_mean(
+            (1 - y_true) * tf.math.square(y_pred) + y_true * tf.math.square(tf.math.maximum(margin - (y_pred), 0))
+        )
+    return contrastive_loss
 
 def main():
     # Code for testing the functions
     t, v = loadFile('F:/AI/COMP3710/data/AD_NC/')
     d = generatePairs(t)
     print(len(d)*8)
+    
+    cnn = makeCNN()
+    print(cnn.summary())
+    siamese = makeSiamese(cnn)
+    print(siamese.summary())
 
 if __name__ == "__main__":
     main()
