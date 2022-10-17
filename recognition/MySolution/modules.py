4507c784eedfed72c294ac9943fe70227ef97750
SongYuan Wang <s4580279@student.uq.edu.au>
2022-10-17 20:26:13 +1000

    Added pre-processed model training in train.py trained model is saved in pre-trained_model folder history log is saved as history_lod.csv history log can be displayed by calling display_graph()

--- recognition/MySolution/modules.py
+++ recognition/MySolution/modules.py
@@ -4,7 +4,8 @@ import stellargraph as sg
 from scipy import sparse as sp
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
-from stellargraph.mapper import FullBatchNodeGenerator
+from stellargraph.layer import DeepGraphInfomax
+from stellargraph.mapper import CorruptedGenerator, FullBatchNodeGenerator
 from tensorflow import keras
 from keras import layers, Input, Model, optimizers, losses
 from keras.layers import Dropout, Dense
@@ -31,44 +32,43 @@ def get_node_indices(graph, ids):
     return node_indices
 
 
-def build_model(data, split_data):
-    train_data, val_data, test_data, train_targets, val_targets, test_targets = \
-        split_data
-    generator = FullBatchNodeGenerator(data.get_graph(), method="gcn")
-    train_gen = generator.flow(train_data.index, train_targets)
-    gcn_model = GCN(
-        layer_sizes=[16, 16],
-        activations=["relu", "relu"],
-        generator=generator,
-        dropout=0.5
-    )
-    x_inp, x_out = gcn_model.in_out_tensors()
-    predictions = layers.Dense(
-        units=train_targets.shape[1],
-        activation="softmax"
-    )(x_out)
-    model = Model(inputs=x_inp, outputs=predictions)
-    model.compile(
-        optimizer=optimizers.Adam(lr=0.01),
-        loss=losses.categorical_crossentropy,
-        metrics=["acc"],
-    )
-    val_gen = generator.flow(val_data.index, val_targets)
-    return model, val_gen, train_gen
-
-
 class Modules:
     def __init__(self, data):
+        self.model = None
+        self.train_gen = None
         data_group = get_training_data(data.get_target())
-        self.model, self.val_gen, self.train_gen = build_model(data, data_group)
+        self.build_model(data, data_group)
+        self.data_group = data_group
 
+    def build_model(self, data, split_data):
+        train_data, val_data, test_data, train_targets, val_targets, test_targets = \
+            split_data
+        generator = FullBatchNodeGenerator(data.get_graph())
+        corrupted_generator = CorruptedGenerator(generator)
+        train_gen = corrupted_generator.flow(data.get_graph().nodes())
+        gcn_model = GCN(
+            layer_sizes=[16, 16],
+            activations=["relu", "relu"],
+            generator=generator,
+            dropout=0.5
+        )
+        deep_graph = DeepGraphInfomax(gcn_model, corrupted_generator)
+        x_in, x_out = deep_graph.in_out_tensors()
+        model = Model(inputs=x_in, outputs=x_out)
+        model.compile(
+            loss=tf.nn.sigmoid_cross_entropy_with_logits,
+            optimizer=optimizers.Adam(lr=1e-3)
+        )
+        self.model = model
+        self.train_gen = train_gen
+    
     def get_model(self):
         return self.model
 
-    def get_val_gen(self):
-        return self.val_gen
-
     def get_train_gen(self):
         return self.train_gen
+
+    def get_data_group(self):
+        return self.data_group
 
 
--- recognition/MySolution/train.py
+++ recognition/MySolution/train.py
@@ -1,24 +1,39 @@
 import stellargraph as sg
+from tensorflow import keras
 from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+import pandas as pd
+import keras.losses
+import tensorflow as tf
 
 
-def handle_training(module):
-    model = module.get_model()
+def plot_loss_epoch(history):
+    plt.plot(history['loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train'], loc='upper right')
+    plt.show()
+
+
+def handle_training(module, new_model=False):
     train_gen = module.get_train_gen()
-    val_gen = module.get_val_gen()
-    es_callback = EarlyStopping(
-        monitor="val_acc",
-        patience=50,
-        restore_best_weights=True
+    train_data, val_data, test_data, train_targets, val_targets, test_targets =\
+        module.get_data_group()
+    model = module.get_model()
+    history_log = keras.callbacks.CSVLogger(
+        "history_log.csv",
+        separator=",",
+        append=True
     )
     history = model.fit(
         train_gen,
-        epochs=200,
-        validation_data=val_gen,
+        epochs=5,
         verbose=2,
-        shuffle=False,
-        callbacks=[es_callback],
+        callbacks=[history_log]
     )
-    sg.utils.plot_history(history)
+    model.save("pre-trained_model")
+    plot_loss_epoch(history.history)
+
 
 
