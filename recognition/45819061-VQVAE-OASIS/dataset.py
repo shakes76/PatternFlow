02dafd4aca078417537f99dbb681ab8e043e971a
jbart <jbart178190@gmail.com>
2022-10-17 11:41:43 +1000

    made dataloading function and littl preprocessing

--- recognition/45819061-VQVAE-OASIS/dataset.py
+++ recognition/45819061-VQVAE-OASIS/dataset.py
@@ -1,31 +1,41 @@
+import os
 import tensorflow as tf
 import numpy as np
+from tqdm import tqdm
 
-DATA_DIR = './data/keras_png_slices_data'
-TRAIN_DATA = DATA_DIR + '/keras_png_slices_train/'
-TEST_DATA = DATA_DIR + '/keras_png_slices_test/'
-VALIDATE_DATA = DATA_DIR + '/keras_png_slices_validate/'
+BATCH_SIZE = 128
+DATA_DIR = 'data/keras_png_slices_data'
+TRAIN_DATA = DATA_DIR + '/keras_png_slices_train'
+TEST_DATA = DATA_DIR + '/keras_png_slices_test'
+VALIDATE_DATA = DATA_DIR + '/keras_png_slices_validate'
 
-x_train = tf.keras.utils.image_dataset_from_directory(TRAIN_DATA, labels=None)
-x_validate = tf.keras.utils.image_dataset_from_directory(VALIDATE_DATA, labels=None)
-x_test = tf.keras.utils.image_dataset_from_directory(TEST_DATA, labels=None)
+def reader(f):
+    return tf.io.decode_png(tf.io.read_file(f), channels=1)
 
-# get mean, variance of training set
-data_info = x_train.reduce((0, 0, 0), 
-        lambda x, y: x + (
-            1,                                  # n
-            y,                                  # running mean
-            (x[0]*y - x[1])**2/(x[0]*(x[0]+1))  # running variance
-        )
-    )
+def load(files):
+    lst = map(reader, tqdm(files))
+    imgs = np.asarray(list(lst), dtype='float32')
+    return imgs
 
-data_mean = data_info[1]/data_info[0]
-data_var = data_info[2]/data_info[0]
 
-# Basic scaling and preprocessing
-def scaling(data):
-    return (data/255.0) - data/255.0
+def get_data():
+    files_train = [os.path.join(TRAIN_DATA, f) for f in os.listdir(TRAIN_DATA) if os.path.isfile(os.path.join(TRAIN_DATA, f))]
+    files_test = [os.path.join(TEST_DATA, f) for f in os.listdir(TEST_DATA) if os.path.isfile(os.path.join(TEST_DATA, f))]
+    files_validate = [os.path.join(VALIDATE_DATA, f) for f in os.listdir(VALIDATE_DATA) if os.path.isfile(os.path.join(VALIDATE_DATA, f))]
 
-x_train = x_train.apply(scaling)
-x_test = x_test.apply(scaling)
-x_validate = x_validate.apply(scaling)
+    print("Loading data")
+    x_train = load(files_train)
+    x_test = load(files_test)
+    x_validate = load(files_validate)
+
+    mean = np.mean(x_train)
+    var = np.mean(x_train)
+
+    x_train -= 127.5
+    x_train /= 127.5
+    x_test -= 127.5
+    x_test /= 127.5
+    x_validate -= 127.5
+    x_validate /= 127.5
+
+    return x_train, x_test, x_validate, mean, var
