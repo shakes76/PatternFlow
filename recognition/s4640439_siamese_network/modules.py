e0769e3f551daf7ee52e5ae4ab154b914464633c
Matt McDonnell <85379302+bushlemon@users.noreply.github.com>
2022-10-18 09:30:43 +1000

    Defined model generator functions
    - Created the functions necessary for building the NNs that will be used for training and classification.
    - Siamese model needs to be tweaked for performance and output size
    - Binary classifier needs to be built as it only has the barebones

--- recognition/s4640439_siamese_network/modules.py
+++ recognition/s4640439_siamese_network/modules.py
@@ -1,3 +1,47 @@
-# Generate Siamese model
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, LeakyReLU, Flatten, MaxPool2d
 
-# Generate binary classifier
+IMAGE_SIZE = (240,256,1)
+ALPHA = 0.2
+
+def build_siamese():
+    """
+    Generate Siamese model
+    This model needs to be a CNN that reduces an image to a vector
+    """ 
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=3, input_shape=IMAGE_SIZE))
+    model.add(LeakyReLU(alpha=ALPHA))
+
+    model.add(MaxPool2d(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(64, kernel_size=3))
+    model.add(LeakyReLU(alpha=ALPHA))
+
+    model.add(MaxPool2d(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Conv2D(128, kernel_size=3))
+    model.add(LeakyReLU(alpha=ALPHA))
+
+    model.add(MaxPool2d(pool_size=(2,2), strides=(1, 1)))
+
+    model.add(Flatten())
+
+    return model
+
+
+
+def build_binary():
+    """
+    Generate binary classifier
+    This model needs to be a binary classifier that takes an output vector from 
+    siamese model and converts it into a single value in the range [0,1]
+    """
+
+    # TODO: define layers of model
+
+    model = Sequential()
+
+    return model
