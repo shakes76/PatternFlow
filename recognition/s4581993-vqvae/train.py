4d3cbf099e08ff1cd93de6d22b644a2ada40657f
yeinydd <yeinydd1@outlook.com>
2022-10-17 21:26:09 +1000

    💾 code to save vqvae model weights

--- recognition/s4581993-vqvae/train.py
+++ recognition/s4581993-vqvae/train.py
@@ -1,12 +1,11 @@
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras import layers
-import tensorflow_probability as tfp
 import tensorflow as tf
 
 from dataset import get_train_dataset
 import modules
 from modules import VQVAETrainer
+from utils import models_directory, vqvae_weights_filename
 
 # Get the datasets
 train_ds = get_train_dataset()
@@ -16,3 +15,6 @@ data_variance = np.var(train_ds)
 vqvae_trainer = VQVAETrainer(data_variance, latent_dim=16, num_embeddings=128)
 vqvae_trainer.compile(optimizer=keras.optimizers.Adam())
 vqvae_trainer.fit(train_ds, epochs=30, batch_size=128)
+
+# Save the model
+vqvae_trainer.save_weights(models_directory + vqvae_weights_filename)
---
+++ recognition/s4581993-vqvae/utils.py
@@ -0,0 +1,4 @@
+models_directory = "trained_models/"
+vqvae_weights_filename = "vqvae"
+pixelcnn_weights_filename = "pixelcnn"
+
