aa80b85349be2dca95f46c137f0e617baeb33dd9
Donghao Yang <donghao.yang@uqconnect.edu.au>
2022-10-17 20:09:34 +1000

    Add a function to plot the TSNE embeddings plot in the train.py

--- recognition/DonghaoYang_45930032_GCN/train.py
+++ recognition/DonghaoYang_45930032_GCN/train.py
@@ -4,6 +4,8 @@ imported from "modules.py" and the data loader is imported from "dataset.py".
 """
 
 # All needed library for training and testing the GCN model.
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 from sklearn.metrics import classification_report
 from dataset import *
 from modules import *
@@ -39,6 +41,26 @@ Test the Trained GCN model with test data set
 test_result = model.predict([features_matrix, adjacency_matrix], batch_size=features_matrix.shape[0])
 print("Test Result with test data set: ")
 print(classification_report(np.argmax(test_target, axis=1), np.argmax(test_result[test_data_mask], axis=1)))
+
+
+def TSNE_embedding_plot(result, target):
+    """
+    This function returns a TSNE embeddings plot with ground truth in colors
+    """
+    # decrease the dimension of data to 2D
+    tsne = TSNE(n_components=2, perplexity=40, init='pca', n_iter=3000).fit_transform(result)
+    node_color = np.argmax(target, axis=1)  # Returns the indices of the maximum values along an axis.
+    pyplot.figure()
+    for class_i in range(4):  # there are only four classes
+        class_i_nodes_positions = (np.where(node_color == class_i))[0]
+        pyplot.scatter(tsne[class_i_nodes_positions, 0], tsne[class_i_nodes_positions, 1], label=class_i)
+    pyplot.show()
+
+
+"""
+Plot the TSNE embeddings plot with ground truth in colors
+"""
+TSNE_embedding_plot(test_result, targets)
 
 """
 Plot the losses and accuracy of the training and validation
