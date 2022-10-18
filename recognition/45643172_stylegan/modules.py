57404ca67149d776c414836ec3890e015c716c5d
unknown <syx123123123@hotmail.com>
2022-10-19 03:08:01 +1000

    Completed the pre-helper function

--- recognition/45643172_stylegan/modules.py
+++ recognition/45643172_stylegan/modules.py
@@ -16,7 +16,127 @@ from tensorflow_addons.layers import InstanceNormalization
 
 import gdown
 from zipfile import ZipFile
+def fade_in(alpha, a, b):
+    return alpha * a + (1.0 - alpha) * b
 
+
+def wasserstein_loss(y_true, y_pred):
+    return -tf.reduce_mean(y_true * y_pred)
+
+
+def pixel_norm(x, epsilon=1e-8):
+    return x / tf.math.sqrt(tf.reduce_mean(x ** 2, axis=-1, keepdims=True) + epsilon)
+
+
+def minibatch_std(input_tensor, epsilon=1e-8):
+    n, h, w, c = tf.shape(input_tensor)
+    group_size = tf.minimum(4, n)
+    x = tf.reshape(input_tensor, [group_size, -1, h, w, c])
+    group_mean, group_var = tf.nn.moments(x, axes=(0), keepdims=False)
+    group_std = tf.sqrt(group_var + epsilon)
+    avg_std = tf.reduce_mean(group_std, axis=[1, 2, 3], keepdims=True)
+    x = tf.tile(avg_std, [group_size, h, w, 1])
+    return tf.concat([input_tensor, x], axis=-1)
+
+
+class EqualizedConv(layers.Layer):
+    def __init__(self, out_channels, kernel=3, gain=2, **kwargs):
+        super(EqualizedConv, self).__init__(**kwargs)
+        self.kernel = kernel
+        self.out_channels = out_channels
+        self.gain = gain
+        self.pad = kernel != 1
+
+    def build(self, input_shape):
+        self.in_channels = input_shape[-1]
+        initializer = keras.initializers.RandomNormal(mean=0.0, stddev=1.0)
+        self.w = self.add_weight(
+            shape=[self.kernel, self.kernel, self.in_channels, self.out_channels],
+            initializer=initializer,
+            trainable=True,
+            name="kernel",
+        )
+        self.b = self.add_weight(
+            shape=(self.out_channels,), initializer="zeros", trainable=True, name="bias"
+        )
+        fan_in = self.kernel * self.kernel * self.in_channels
+        self.scale = tf.sqrt(self.gain / fan_in)
+
+    def call(self, inputs):
+        if self.pad:
+            x = tf.pad(inputs, [[0, 0], [1, 1], [1, 1], [0, 0]], mode="REFLECT")
+        else:
+            x = inputs
+        output = (
+            tf.nn.conv2d(x, self.scale * self.w, strides=1, padding="VALID") + self.b
+        )
+        return output
+
+
+class EqualizedDense(layers.Layer):
+    def __init__(self, units, gain=2, learning_rate_multiplier=1, **kwargs):
+        super(EqualizedDense, self).__init__(**kwargs)
+        self.units = units
+        self.gain = gain
+        self.learning_rate_multiplier = learning_rate_multiplier
+
+    def build(self, input_shape):
+        self.in_channels = input_shape[-1]
+        initializer = keras.initializers.RandomNormal(
+            mean=0.0, stddev=1.0 / self.learning_rate_multiplier
+        )
+        self.w = self.add_weight(
+            shape=[self.in_channels, self.units],
+            initializer=initializer,
+            trainable=True,
+            name="kernel",
+        )
+        self.b = self.add_weight(
+            shape=(self.units,), initializer="zeros", trainable=True, name="bias"
+        )
+        fan_in = self.in_channels
+        self.scale = tf.sqrt(self.gain / fan_in)
+
+    def call(self, inputs):
+        output = tf.add(tf.matmul(inputs, self.scale * self.w), self.b)
+        return output * self.learning_rate_multiplier
+
+
+class AddNoise(layers.Layer):
+    def build(self, input_shape):
+        n, h, w, c = input_shape[0]
+        initializer = keras.initializers.RandomNormal(mean=0.0, stddev=1.0)
+        self.b = self.add_weight(
+            shape=[1, 1, 1, c], initializer=initializer, trainable=True, name="kernel"
+        )
+
+    def call(self, inputs):
+        x, noise = inputs
+        output = x + self.b * noise
+        return output
+
+
+class AdaIN(layers.Layer):
+    def __init__(self, gain=1, **kwargs):
+        super(AdaIN, self).__init__(**kwargs)
+        self.gain = gain
+
+    def build(self, input_shapes):
+        x_shape = input_shapes[0]
+        w_shape = input_shapes[1]
+
+        self.w_channels = w_shape[-1]
+        self.x_channels = x_shape[-1]
+
+        self.dense_1 = EqualizedDense(self.x_channels, gain=1)
+        self.dense_2 = EqualizedDense(self.x_channels, gain=1)
+
+    def call(self, inputs):
+        x, w = inputs
+        ys = tf.reshape(self.dense_1(w), (-1, 1, 1, self.x_channels))
+        yb = tf.reshape(self.dense_2(w), (-1, 1, 1, self.x_channels))
+        return ys * x + yb
+        
 class Generator:
     def __init__(self, start_res_log2, target_res_log2):
         self.start_res_log2 = start_res_log2
