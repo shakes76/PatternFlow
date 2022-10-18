56bc10a2a1e2835856f579aa2cf174b9cdedee44
yeinydd <yeinydd1@outlook.com>
2022-10-18 22:59:44 +1000

    📈 show histogram of ssim values

--- recognition/s4581993-vqvae/train.py
+++ recognition/s4581993-vqvae/train.py
@@ -49,7 +49,7 @@ plt.tight_layout()
 plt.show()
 
 # Find the SSIM between the original and decoded images
-print("Calculating average SSIM for test set...")
+print("Calculating SSIM across test set...")
 trained_vqvae_model = vqvae_trainer.vqvae
 reconstructions_test = trained_vqvae_model.predict(test_ds)
 
@@ -58,3 +58,11 @@ for i in range(len(test_ds)):
     ssim_values[i] = tf.image.ssim(test_ds[i], reconstructions_test[i], 1)
 avg_ssim = np.mean(ssim_values)
 print("SSIM index:", avg_ssim)
+
+# Show a histogram of the SSIM values
+plt.hist(ssim_values)
+plt.ylabel('Number of occurrences')
+plt.xlabel('SSIM')
+plt.title('Structural similarity index measure')
+plt.annotate(f'Mean: %f' % avg_ssim, xy=(0.05, 0.95), xycoords='axes fraction')
+plt.show()
