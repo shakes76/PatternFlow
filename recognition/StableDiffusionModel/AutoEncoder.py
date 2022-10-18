4230bbdf83a969c4a64c3b50429db33579d03157
Wonge <daniel.jwong@outlook.com>
2022-10-18 12:46:16 +1000

    Added toggling group norm layers on/off in AutoEncoder

--- recognition/StableDiffusionModel/AutoEncoder.py
+++ recognition/StableDiffusionModel/AutoEncoder.py
@@ -14,58 +14,58 @@ from keras import layers
 from CustomLayers import *
 
 class Encoder(kr.Sequential) :
-    def __init__(self, inputSize, activation = kr.activations.swish, downSampleLayer = ConvDownsample) :
+    def __init__(self, inputSize, activation = kr.activations.swish, downSampleLayer = ConvDownsample, normLayers = True) :
         super().__init__(
             [
             # Block downsampling by factor of 2
-                ResidualNetBlock(1, 64, 3, activation = activation),
-                ResidualNetBlock(64, 64, 3, activation = activation),
-                ResidualNetBlock(64, 64, 3, activation = activation), 
+                ResidualNetBlock(1, 64, 3, activation = activation, normLayers = normLayers),
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers), 
                 downSampleLayer(64),
             
             # Block downsampling by factor of 2
-                ResidualNetBlock(64, 64, 3, activation = activation),  
-                ResidualNetBlock(64, 64, 3, activation = activation), 
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),  
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers), 
                 downSampleLayer(64),
             
             # Block downsampling by factor of 2
-                ResidualNetBlock(64, 64, 3, activation = activation),   
-                ResidualNetBlock(64, 64, 3, activation = activation),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
                 downSampleLayer(64),
             
             # Reducing filter channel back to 1
-                ResidualNetBlock(64, 64, 3, activation = activation),   
-                ResidualNetBlock(64, 64, 3, activation = activation),  
-                ResidualNetBlock(64, 1, 3, activation = activation)
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),  
+                ResidualNetBlock(64, 1, 3, activation = activation, normLayers = normLayers)
             ])
         
         
 ################################################ CONSIDER DECONVOLUTION  ################################################
 
 class Decoder(kr.Sequential) :
-    def __init__(self, latentSpaceSize, outputSize, activation = kr.activations.swish, upSampleLayer = kr.layers.UpSampling2D) :
+    def __init__(self, latentSpaceSize, outputSize, activation = kr.activations.swish, upSampleLayer = kr.layers.UpSampling2D, normLayers = True) :
         super().__init__(
             [    
             # Block upsampling by factor of 2
-                ResidualNetBlock(1, 64, 3, activation = activation),
-                ResidualNetBlock(64, 64, 3, activation = activation),
-                ResidualNetBlock(64, 64, 3, activation = activation),   
+                ResidualNetBlock(1, 64, 3, activation = activation, normLayers = normLayers),
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
                 upSampleLayer(),
             
             # Block upsampling by factor of 2
-                ResidualNetBlock(64, 64, 3, activation = activation),   
-                ResidualNetBlock(64, 64, 3, activation = activation),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
                 upSampleLayer(),
             
             # Block upsampling by factor of 2
-                ResidualNetBlock(64, 64, 3, activation = activation),   
-                ResidualNetBlock(64, 64, 3, activation = activation),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
                 upSampleLayer(),
             
             # Reducing filter channel back to 1
-                ResidualNetBlock(64, 64, 3, activation = activation),   
-                ResidualNetBlock(64, 64, 3, activation = activation),  
-                ResidualNetBlock(64, 1, 3, activation = activation)
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),   
+                ResidualNetBlock(64, 64, 3, activation = activation, normLayers = normLayers),  
+                ResidualNetBlock(64, 1, 3, activation = activation, normLayers = normLayers)
             ])
                                  
 class AutoEncoder(kr.Model) :
@@ -74,25 +74,25 @@ class AutoEncoder(kr.Model) :
     
     """
     
-    def __init__(self, inputSize, latentSpaceSize, activation = kr.activations.swish) :
+    def __init__(self, inputSize, latentSpaceSize, activation = kr.activations.swish, normLayers = True) :
         super().__init__()
         
         self.inputSize = inputSize
         self.latentSpaceSize = latentSpaceSize
         
-        self.encoder = self.__buildEncoderLayers(inputSize, activation=activation)
-        self.decoder = self.__buildDecoderLayers(latentSpaceSize, inputSize, activation=activation)
+        self.encoder = self.__buildEncoderLayers(inputSize, activation=activation, normLayers = normLayers)
+        self.decoder = self.__buildDecoderLayers(latentSpaceSize, inputSize, activation=activation, normLayers = normLayers)
         
     def call(self, inputs) :
         x = self.encoder(inputs)
         return self.decoder(x)
         
-    def __buildEncoderLayers(self, inputSize, activation=kr.activations.swish) : 
-        return Encoder(inputSize, activation=activation)
+    def __buildEncoderLayers(self, inputSize, activation=kr.activations.swish, normLayers = True) : 
+        return Encoder(inputSize, activation=activation, normLayers = normLayers)
         
         
-    def __buildDecoderLayers(self, latentSpaceSize, outputSize, activation=kr.activations.swish) :
-        return Decoder(latentSpaceSize, outputSize, activation=activation)
+    def __buildDecoderLayers(self, latentSpaceSize, outputSize, activation=kr.activations.swish, normLayers = True) :
+        return Decoder(latentSpaceSize, outputSize, activation=activation, normLayers = normLayers)
         
 
     def buildEncoder(self) :
@@ -102,4 +102,3 @@ class AutoEncoder(kr.Model) :
     def buildDecoder(self) :
         newLatent = kr.Input(self.latentSpaceSize)
         return kr.models.Model(newLatent, self.decoder(newLatent)) ####################################################
-
