f08ec6e3e1b33aa3cdf98b00c2661e7a43d67893
unknown <syx123123123@hotmail.com>
2022-10-19 04:38:16 +1000

    first edition of predict.py

---
+++ recognition/45643172_stylegan/predict.py
@@ -0,0 +1,30 @@
+import os
+
+from modules import *
+from train import *
+
+
+
+def predict(url, name):
+    weights_path = keras.utils.get_file(
+        name,
+        url,
+        extract=True,
+        cache_dir=os.path.abspath("."),
+        cache_subdir="pretrained",
+    )
+
+    style_gan.grow_model(128)
+    style_gan.load_weights(os.path.join("pretrained/stylegan"))
+
+    tf.random.set_seed(196)
+    batch_size = 2
+    z = tf.random.normal((batch_size, style_gan.z_dim))
+    w = style_gan.mapping(z)
+    noise = style_gan.generate_noise(batch_size=batch_size)
+    images = style_gan({"style_code": w, "noise": noise, "alpha": 1.0})
+    plot_images(images, 5)
+
+url = ""
+name = ""
+predict(url, name)
