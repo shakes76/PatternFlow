384c05eb1f60dc03ae98889c88ef0f44b794104f
JacobDyson01 <112358789+JacobDyson01@users.noreply.github.com>
2022-10-18 13:35:16 +1000

    Fixed bugs with importing of data and cleaned up variable names in modules.py for better readability

--- recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/dataset.py
+++ recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/dataset.py
@@ -16,12 +16,12 @@ def normalise_label(data):
 
 def data_loader(size=128, batch_size=16):
     # input data folders
-    training_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Part1_GroundTruth/*.png"
-    test_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Part1_GroundTruth/*.png"
-    validation_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Part1_GroundTruth/*.png"
-    training_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Data/*.jpg"
-    validation_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Data/*.jpg"
-    test_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Data/*.jpg"
+    training_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Part1_GroundTruth"
+    test_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Part1_GroundTruth"
+    validation_labels_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Part1_GroundTruth"
+    training_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Training_Data"
+    validation_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Validation_Data"
+    test_input_dir = "C:/Users/Jacob/Downloads/ISIC-2017_Test_v2_Data"
 
     # size = 128
     # batch_size = 16
@@ -56,15 +56,18 @@ def data_loader(size=128, batch_size=16):
                                                             batch_size=batch_size,
                                                             image_size=(size, size)
                                                             )
-    training_data = normalise_data(training_data)
-    test_data = normalise_data(test_data)
-    validation_data = normalise_data(validation_data)
-    training_labels = normalise_label(training_labels)
-    test_labels = normalise_label(test_labels)
-    validation_labels = normalise_label(validation_labels)
+    training_data = training_data.map(normalise_data)
+    test_data = test_data.map(normalise_data)
+    validation_data = validation_data.map(normalise_data)
+    training_labels = training_labels.map(normalise_label)
+    test_labels = test_labels.map(normalise_label)
+    validation_labels = validation_labels.map(normalise_label)
 
-    train_ds = tf.data.Dataset.from_tensor_slices((training_data, training_labels))
-    test_ds = tf.data.Dataset.from_tensor_slices((test_data, test_labels))
-    validation_ds = tf.data.Dataset.from_tensor_slices((validation_data, validation_labels))
+    # train_ds = tf.data.Dataset.from_tensor_slices((training_data, training_labels))
+    # test_ds = tf.data.Dataset.from_tensor_slices((test_data, test_labels))
+    # validation_ds = tf.data.Dataset.from_tensor_slices((validation_data, validation_labels))
+    train_ds = tf.data.Dataset.zip((training_data, training_labels))
+    test_ds = tf.data.Dataset.zip((test_data, test_labels))
+    validation_ds = tf.data.Dataset.zip((validation_data, validation_labels))
     return train_ds, test_ds, validation_ds
 
--- recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/modules.py
+++ recognition/s4580306 Segmentation of the ISIC data set using Improved UNet/modules.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
-from tensorflow.keras import layers
+from tensorflow import keras
+
+
+# from tensorflow.keras import layers
 
 
 class ImprovedUNET(tf.keras.Model):
@@ -14,94 +17,93 @@ class ImprovedUNET(tf.keras.Model):
 
     def context_module(self, input, output_filters):
         convolution1 = tfa.layers.InstanceNormalization()(input)
-        convolution1 = layers.Conv2D(output_filters, (3, 3), padding=self.padding,
-                                     activation=layers.leakyReLU(alpha=self.leakyAlpha))(convolution1)
-        dropout = layers.Dropout(self.contextDropoutRate)(convolution1)
+        convolution1 = tf.keras.layers.Conv2D(output_filters, kernel_size=(3, 3), padding=self.padding,
+                                              activation='relu')(convolution1)
+        dropout = tf.keras.layers.Dropout(self.contextDropoutRate)(convolution1)
         convolution2 = tfa.layers.InstanceNormalization()(dropout)
-        convolution2 = layers.Conv2D(output_filters, (3, 3), padding=self.padding,
-                                     activation=layers.leakyReLU(alpha=self.leakyAlpha))(convolution2)
+        convolution2 = tf.keras.layers.Conv2D(output_filters, kernel_size=(3, 3), padding=self.padding,
+                                              activation='relu')(convolution2)
         return convolution2
 
     def perform_upsampling(self, input, output_filters):
-        upsample = layers.UpSampling2D((2, 2))(input)
-        upsample = layers.Conv2D(output_filters, (3, 3), padding=self.padding,
-                                 activation=layers.LeakyReLU(alpha=self.leakyAlpha))(upsample)
+        upsample = tf.keras.layers.UpSampling2D(size=(2, 2))(input)
+        upsample = tf.keras.layers.Conv2D(output_filters, kernel_size=(3, 3), padding=self.padding,
+                                          activation='relu')(upsample)
         upsample = tfa.layers.InstanceNormalization()(upsample)
         return upsample
 
     def localization_module(self, input, output_filters):
         convolution1 = tfa.layers.InstanceNormalization()(input)
-        convolution1 = layers.Conv2D(output_filters, (3, 3), padding=self.padding,
-                                     activation=layers.LeakyReLU(alpha=self.leakyAlpha))(convolution1)
+        convolution1 = tf.keras.layers.Conv2D(output_filters, kernel_size=(3, 3), padding=self.padding,
+                                              activation='relu')(convolution1)
         convolution2 = tfa.layers.InstanceNormalization()(convolution1)
-        convolution2 = layers.Conv2D(output_filters, (3, 3), padding=self.padding,
-                                     activation=layers.LeakyReLU(alpha=self.leakyAlpha))(convolution2)
+        convolution2 = tf.keras.layers.Conv2D(output_filters, kernel_size=(3, 3), padding=self.padding,
+                                              activation='relu')(convolution2)
         return convolution2
 
     def data_pipe_line(self):
-
-        input_layer = layers.Input(shape=(256, 256, 3))
+        input_layer = tf.keras.layers.Input(shape=(256, 256, 3))
 
         # Encoder
 
-        convolution1 = layers.Conv2D(self.initial_output, (3, 3), padding=self.padding)(input_layer)
+        convolution1 = tf.keras.layers.Conv2D(self.initial_output, kernel_size=(3, 3), padding=self.padding,
+                                              activation='relu')(input_layer)
         convolution_module1 = self.context_module(convolution1, self.initial_output)
-        sum1 = layers.Add()([convolution1, convolution_module1])
+        sum1 = tf.keras.layers.Add()([convolution1, convolution_module1])
         first_skip = sum1
 
-        convolution2 = layers.Conv2D(self.initial_output * 2, (3, 3), padding=self.padding)(input_layer)
-        convolution_module2 = self.context_module(convolution1, self.initial_output * 2)
-        sum2 = layers.Add()([convolution2, convolution_module2])
+        convolution2 = tf.keras.layers.Conv2D(self.initial_output * 2, kernel_size=(3, 3), strides=(2, 2),
+                                              padding=self.padding, activation='relu')(sum1)
+        convolution_module2 = self.context_module(convolution2, self.initial_output * 2)
+        sum2 = tf.keras.layers.Add()([convolution2, convolution_module2])
         second_skip = sum2
 
-        convolution3 = layers.Conv2D(self.initial_output * 4, (3, 3), padding=self.padding)(input_layer)
-        convolution_module3 = self.context_module(convolution1, self.initial_output * 4)
-        sum3 = layers.Add()([convolution3, convolution_module3])
+        convolution3 = tf.keras.layers.Conv2D(self.initial_output * 4, kernel_size=(3, 3), strides=(2, 2),
+                                              padding=self.padding, activation='relu')(sum2)
+        convolution_module3 = self.context_module(convolution3, self.initial_output * 4)
+        sum3 = tf.keras.layers.Add()([convolution3, convolution_module3])
         third_skip = sum3
 
-        convolution4 = layers.Conv2D(self.initial_output * 8, (3, 3), padding=self.padding)(input_layer)
-        convolution_module4 = self.context_module(convolution1, self.initial_output * 8)
-        sum4 = layers.Add()([convolution4, convolution_module4])
+        convolution4 = tf.keras.layers.Conv2D(self.initial_output * 8, kernel_size=(3, 3), strides=(2, 2),
+                                              padding=self.padding, activation='relu')(sum3)
+        convolution_module4 = self.context_module(convolution4, self.initial_output * 8)
+        sum4 = tf.keras.layers.Add()([convolution4, convolution_module4])
         fourth_skip = sum4
 
-        convolution5 = layers.Conv2D(self.initial_output * 16, (3, 3), padding=self.padding)(input_layer)
-        convolution_module5 = self.context_module(convolution1, self.initial_output * 16)
-        sum5 = layers.Add()([convolution5, convolution_module5])
+        convolution5 = tf.keras.layers.Conv2D(self.initial_output * 16, kernel_size=(3, 3), strides=(2, 2),
+                                              padding=self.padding, activation='relu')(sum4)
+        convolution_module5 = self.context_module(convolution5, self.initial_output * 16)
+        sum5 = tf.keras.layers.Add()([convolution5, convolution_module5])
 
         # Decoder
         upsample_module1 = self.perform_upsampling(sum5, self.initial_output * 8)
-        concatenation_module1 = layers.concatenate([upsample_module1, fourth_skip])
+        concatenation_module1 = tf.keras.layers.concatenate([upsample_module1, fourth_skip])
         localisation_output1 = self.localization_module(concatenation_module1, self.initial_output * 8)
 
         upsample_module2 = self.perform_upsampling(localisation_output1, self.initial_output * 4)
-        concatenation_module2 = layers.concatenate([upsample_module2, third_skip])
+        concatenation_module2 = tf.keras.layers.concatenate([upsample_module2, third_skip])
         localisation_output2 = self.localization_module(concatenation_module2, self.initial_output * 4)
 
-        segmentation_one = layers.Conv2D(1, (1, 1), padding=self.padding)(localisation_output2)
-        segmentation_one = layers.UpSampling2D(size=(2, 2))(segmentation_one)
+        lower_segmented = tf.keras.layers.Conv2D(1, (1, 1), padding=self.padding)(localisation_output2)
+        upscaled_lower_segment = tf.keras.layers.UpSampling2D(size=(2, 2))(lower_segmented)
 
         upsample_module3 = self.perform_upsampling(localisation_output2, self.initial_output * 2)
-        concatenation_module3 = layers.concatenate([upsample_module3, fourth_skip])
+        concatenation_module3 = tf.keras.layers.concatenate([upsample_module3, second_skip])
         localisation_output3 = self.localization_module(concatenation_module3, self.initial_output * 2)
 
-        segmentation_two = layers.Conv2D(1, (1, 1), padding=self.padding)(localisation_output3)
-        sum6 = layers.Add()([segmentation_one, segmentation_two])
-        sum6 = layers.UpSampling2D(size=(2, 2))(sum6)
+        middle_segmented = tf.keras.layers.Conv2D(1, (1, 1), padding=self.padding)(localisation_output3)
+        first_skip_sum = tf.keras.layers.Add()([upscaled_lower_segment, middle_segmented])
+        upscaled_middle_segment = tf.keras.layers.UpSampling2D(size=(2, 2))(first_skip_sum)
 
         upsample_module4 = self.perform_upsampling(localisation_output3, self.initial_output * 1)
-        concatenation_module4 = layers.concatenate([upsample_module4, fourth_skip])
+        concatenation_module4 = tf.keras.layers.concatenate([upsample_module4, first_skip])
 
-        convolution_module6 = layers.Conv2D(self.initial_output * 2, (3, 3), padding=self.padding)(concatenation_module4)
-        segmentation_three = layers.Conv2D(1, (1, 1), padding=self.padding)(convolution_module6)
-        sum7 = layers.Add()([sum, segmentation_three])
+        convolution_module6 = tf.keras.layers.Conv2D(self.initial_output * 2, kernel_size=(3, 3), padding=self.padding)(
+            concatenation_module4)
+        upper_segmented = tf.keras.layers.Conv2D(1, kernel_size=(1, 1), padding=self.padding)(convolution_module6)
+        final_node = tf.keras.layers.Add()([upscaled_middle_segment, upper_segmented])
 
-        outputs = layers.Conv2D(1, (1, 1), activation="sigmoid")(sum7)
+        outputs = tf.keras.layers.Activation("sigmoid")(final_node)
         model = tf.keras.Model(inputs=input, outputs=outputs)
 
         return model
-
-
-
-
-
-
