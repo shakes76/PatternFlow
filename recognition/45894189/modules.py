0bd0baa16166268da55019c3d92757c25e8f635f
Sam Smeaton <samsmeaton24@gmail.com>
2022-10-18 11:42:28 +1000

    lowered learning rate hyperparameters. reordered generator and discriminator training. built mapping network into generator. turned noise and AdaIN classes into functions that return keras models. Increased latent dimensions and initial generator filter number to 512. changed generator activation to sigmoid. Made tweaks to generator architecture especially with activation blocks.

--- recognition/45894189/modules.py
+++ recognition/45894189/modules.py
@@ -1,43 +1,42 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras import layers, Sequential
+from keras import layers
 
-class Noise(layers.Layer):
+def add_noise(n_filters, image_size):
     """
     Noise input layer [B]
     """
-    def build(self, input_shape):
-        initializer = keras.initializers.RandomNormal(mean=0.0, stddev=1.0)
-        self.b = self.add_weight(shape=[1, 1, 1, 1], initializer=initializer, trainable=True)
+    input = layers.Input((image_size, image_size, n_filters))
+    noise = layers.Input((image_size, image_size, 1))
+    x = input
 
-    def call(self, inputs):
-        x, noise = inputs
-        output = x + self.b * noise
-        return output
+    scaled_noise = layers.Dense(n_filters)(noise)
+    x = x + scaled_noise
+    return tf.keras.Model([input, noise], x)
 
-class AdaIN(layers.Layer):
+def AdaIN(n_filters, image_size, epsilon=1e-8):
     """
     Combined AdaIN and [A] layer. Affine transform of W is used to perform
     Instance Normalisation on X values 
     """
-    def __init__(self, epsilon=1e-8):
-        super(AdaIN, self).__init__()
-        self.epsilon = epsilon
+    input = layers.Input((image_size, image_size, n_filters))
+    w = layers.Input(512) # = latent_dim
+    x = input
 
-    def build(self, input_shape):
-        self.dense_ys = layers.Dense(1)
-        self.dense_yb = layers.Dense(1)
+    ys = layers.Dense(n_filters)(w)
+    ys = layers.Reshape([1, 1, n_filters])(ys)
 
-    def call(self, inputs):
-        x, w = inputs
-        ys = tf.reshape(self.dense_ys(w), (-1, 1, 1, 1))
-        yb = tf.reshape(self.dense_yb(w), (-1, 1, 1, 1))
-        axes = list(range(1, len(x.shape) - 1))
-        mean = tf.math.reduce_mean(x, axes, keepdims=True)
-        stdev = tf.math.reduce_std(x, axes, keepdims=True) 
-        return ys * ((x - mean)/(stdev + self.epsilon)) + yb
+    yb = layers.Dense(n_filters)(w)
+    yb = layers.Reshape([1, 1, n_filters])(yb)
 
-def WNetwork(latent_dim=256):
+    axes = [1, 2]   # axes image pixel values, without batch or filters axes
+    mean = tf.math.reduce_mean(x, axes, keepdims=True)
+    stddev = tf.math.reduce_std(x, axes, keepdims=True) + epsilon
+
+    x = ys*((x-mean)/(stddev + epsilon))
+    return tf.keras.Model([input, w], x)
+
+def WNetwork(latent_dim=512):
     """
     Mapping Network z -> w mapping latent noise to style code with set of
     fully connected layers
@@ -53,83 +52,92 @@ def WNetwork(latent_dim=256):
 class Generator():
     def __init__(self):
         self.init_size = 4
-        self.init_filters = 256
-        self.noise_inputs = [tf.random.normal([32, res, res, 1]) for res in [4, 8, 16, 32, 64, 128, 256]]
+        self.init_filters = 512
 
     def generator_block(self, n_filters, image_size):
         input_tensor = layers.Input(shape=(image_size, image_size, n_filters))
         noise = layers.Input(shape=(2*image_size, 2*image_size, 1))
-        w = layers.Input(shape=256)
+        w = layers.Input(shape=512)
 
         x = input_tensor
         n_filters = n_filters // 2
 
-        x = layers.UpSampling2D((2,2))(x)
-        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
-        x = Noise()([x, noise])
-        x = layers.LeakyReLU(alpha=0.2)(x)
-        x = AdaIN()([x, w])
+        x = layers.UpSampling2D(size=(2,2), interpolation="bilinear")(x)
 
         x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
-        x = Noise()([x, noise])
+        x = add_noise(n_filters, image_size*2)([x, noise])
+        x = AdaIN(n_filters, image_size*2)([x, w])
         x = layers.LeakyReLU(alpha=0.2)(x)
-        x = AdaIN()([x, w])
+        
+        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
+        x = add_noise(n_filters, image_size*2)([x, noise])
+        x = AdaIN(n_filters, image_size*2)([x, w])
+        x = layers.LeakyReLU(alpha=0.2)(x)
 
         return tf.keras.Model([input_tensor, w, noise], x)
 
     def generator(self):
-        current_size = self.init_size
-        n_filters = self.init_filters
+        current_size = self.init_size # 4
+        n_filters = self.init_filters # 512
         input = layers.Input(shape=(current_size, current_size, n_filters))
-
-        noise_inputs = []
-        curr_size = self.init_size
-        while curr_size <= 256:
-            noise_inputs.append(layers.Input(shape=[curr_size, curr_size, 1]))
-            curr_size *= 2
-
-        w = layers.Input(shape=n_filters)
         x = input
         i = 0
 
-        x = Noise()([x, self.noise_inputs[i]])
-        x = layers.LeakyReLU(alpha=0.2)(x)
-        x = AdaIN()([x, w])
-        x = layers.Conv2D(256, kernel_size=3, padding="same")(x)
+        noise_inputs, z_inputs = [], []
+        curr_size = self.init_size
+        while curr_size <= 256:
+            noise_inputs.append(layers.Input(shape=[curr_size, curr_size, 1]))
+            z_inputs.append(layers.Input(shape=[512]))
+            curr_size *= 2
 
-        x = Noise()([x, self.noise_inputs[i]])
+        z = layers.Input(shape=[512])
+        w = z
+        for _ in range(8):
+            w = layers.Dense(n_filters)(w)
+            w = layers.LeakyReLU(0.2)(w)
+        mapping = tf.keras.Model(z, w)
+
+        x = layers.Activation("linear")(x)    
+        x = add_noise(n_filters, current_size)([x, noise_inputs[i]])
+        x = AdaIN(n_filters, current_size)([x, mapping(z_inputs[i])])
         x = layers.LeakyReLU(alpha=0.2)(x)
-        x = AdaIN()([x, w])
+        
+        x = layers.Conv2D(512, kernel_size=3, padding="same")(x)
+        x = add_noise(n_filters, current_size)([x, noise_inputs[i]])
+        x = AdaIN(n_filters, current_size)([x, mapping(z_inputs[i])])
+        x = layers.LeakyReLU(alpha=0.2)(x)
 
         while current_size < 256:
             i += 1
-            x = self.generator_block(n_filters, current_size)([x, w, noise_inputs[i]])
+            x = self.generator_block(n_filters, current_size)([x, mapping(z_inputs[i]), noise_inputs[i]])
             current_size = 2 * current_size
             n_filters = n_filters // 2
 
-        x = layers.Conv2D(1, kernel_size=4, padding="same", activation="tanh")(x)
-        return tf.keras.Model([input, w, noise_inputs], x)
+        x = layers.Conv2D(1, kernel_size=3, padding="same", activation="sigmoid")(x)
+        return tf.keras.Model([input, z_inputs, noise_inputs], x)
 
 class Discriminator():
 
     def __init__(self):
         self.init_size = 256
-        self.init_filters = 16
+        self.init_filters = 8
 
     def discriminator_block(self, n_filters, image_size):
         """
         Main block for discriminator containing two convolutional layers with LeakyReLU activation. the second layer doubles the number
         of filters. The block is ended with a downsampling of the image that halves it's size
         """
-        input_tensor = layers.Input(shape=(image_size, image_size, n_filters))
-        x = layers.Conv2D(n_filters, kernel_size=3, padding="same", input_shape=(image_size, image_size, n_filters))(input_tensor)
-        x = layers.LeakyReLU(alpha=0.2)(x)
 
-        x = layers.Conv2D(2*n_filters, kernel_size=3, padding="same", input_shape=(image_size, image_size, n_filters))(x)
-        x = layers.LeakyReLU(alpha=0.2)(x)
+        if image_size == self.init_size:
+            input_tensor = layers.Input(shape=(image_size, image_size, 1))
+        else:
+            input_tensor = layers.Input(shape=(image_size, image_size, n_filters // 2))
 
-        x = layers.AveragePooling2D((2,2))(x)   # Downsizing
-
+        x = input_tensor
+        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
+        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
+        x = layers.AveragePooling2D((2,2))(x)   # Downsizing 
+        x = layers.LeakyReLU(0.2)(x)
         return tf.keras.Model(input_tensor, x)
 
     def discriminator(self):
@@ -143,19 +151,16 @@ class Discriminator():
         input_tensor = layers.Input(shape=[current_size, current_size, 1])
         x = input_tensor
 
-        x = layers.Conv2D(n_filters, kernel_size=1, padding="same", input_shape=(current_size, current_size, 1))(x)
-
         while current_size > 4:
             x = self.discriminator_block(n_filters, current_size)(x)
             current_size = current_size // 2
             n_filters = 2 * n_filters
 
-        x = layers.Conv2D(n_filters, kernel_size=3, padding="same", input_shape=(current_size, current_size, n_filters))(x)
-        x = layers.LeakyReLU(alpha=0.2)(x)
-        x = layers.Conv2D(n_filters, kernel_size=4, padding="same", input_shape=(current_size, current_size, n_filters))(x)
-        x = layers.LeakyReLU(alpha=0.2)(x)
+        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
+        x = layers.Conv2D(n_filters, kernel_size=3, padding="same")(x)
+        x = layers.LeakyReLU(0.2)(x)
 
         x = layers.Flatten()(x)
-        x = layers.Dense(1, activation="linear")(x)
+        x = layers.Dense(1, activation="sigmoid")(x)
 
         return tf.keras.Model(input_tensor, x)
--- recognition/45894189/train.py
+++ recognition/45894189/train.py
@@ -9,12 +9,12 @@ class StyleGAN(keras.Model):
         super(StyleGAN, self).__init__()
         self.discriminator = Discriminator().discriminator()
         self.generator = Generator().generator()
-        self.mapping = WNetwork()
+        # self.mapping = WNetwork()
 
     def compile(self):
         super(StyleGAN, self).compile()
-        self.d_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001)
-        self.g_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001)
+        self.d_optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5)
+        self.g_optimizer=tf.keras.optimizers.Adam(learning_rate=1.25e-5)
         self.loss_fn=tf.keras.losses.BinaryCrossentropy()
         self.discriminator_loss_metric = keras.metrics.Mean(name="discriminator_loss")
         self.generator_loss_metric = keras.metrics.Mean(name="generator_loss")
@@ -26,11 +26,11 @@ class StyleGAN(keras.Model):
     @tf.function
     def train_step(self, real_images):
         batch_size = tf.shape(real_images)[0]
-        g_loss = self.train_generator(batch_size)
         d_loss = self.train_discriminator(real_images, batch_size)
+        g_loss = self.train_generator(batch_size)
 
-        self.generator_loss_metric.update_state(g_loss)
         self.discriminator_loss_metric.update_state(d_loss)
+        self.generator_loss_metric.update_state(g_loss)
 
         return {
             "discriminator_loss": self.discriminator_loss_metric.result(),
@@ -38,43 +38,41 @@ class StyleGAN(keras.Model):
         }
 
     def train_generator(self, batch_size):
-        z = tf.random.normal((batch_size, 256)) #TODO: Pass in latent_dim and stop magic numbers
-        noise = [tf.random.normal([batch_size, res, res, 1]) for res in [4, 8, 16, 32, 64, 128, 256]] #TODO: Global variable these upscaling values somewhere
-        input = tf.ones([32, 4, 4, 256])
+        z = [tf.random.normal((batch_size, 512)) for i in range(7)]
+        noise = [tf.random.uniform([batch_size, res, res, 1]) for res in [4, 8, 16, 32, 64, 128, 256]] #TODO: Global variable these upscaling values somewhere
+        input = tf.ones([32, 4, 4, 512])
         with tf.GradientTape() as g_tape:
-            w = self.mapping(z)
-            fake_images = self.generator([input, w, noise])
+            fake_images = self.generator([input, z, noise])
             predictions = self.discriminator(fake_images)
 
             goal_labels = tf.zeros([batch_size, 1])
             g_loss = self.loss_fn(goal_labels, predictions)
 
-            trainable_weights = (self.mapping.trainable_weights + self.generator.trainable_weights)
-        gradients = g_tape.gradient(g_loss, trainable_weights)
-        self.g_optimizer.apply_gradients(zip(gradients, trainable_weights))
+            # trainable_variables = (self.mapping.trainable_variables + self.generator.trainable_variables)
+            trainable_variables = self.generator.trainable_variables
+            gradients = g_tape.gradient(g_loss, trainable_variables)
+            self.g_optimizer.apply_gradients(zip(gradients, trainable_variables))
 
         return g_loss
 
     def train_discriminator(self, real_images, batch_size):
-        z = tf.random.normal([batch_size, 256]) #TODO: Pass in latent_dim and stop magic numbers
-        noise = [tf.random.normal([batch_size, res, res, 1]) for res in [4, 8, 16, 32, 64, 128, 256]] #TODO: Global variable these upscaling values somewhere
-        input = tf.ones([32, 4, 4, 256])
-        w = self.mapping(z)
-        generated_images = self.generator([input, w, noise])
+        z = [tf.random.normal((batch_size, 512)) for i in range(7)]
+        noise = [tf.random.uniform([batch_size, res, res, 1]) for res in [4, 8, 16, 32, 64, 128, 256]] #TODO: Global variable these upscaling values somewhere
+        input = tf.ones([32, 4, 4, 512])
+        generated_images = self.generator([input, z, noise])
 
         # Combine real and fake, add labels with random noise
         images = tf.concat([generated_images, real_images], axis=0)
         labels = tf.concat(
             [tf.ones([batch_size, 1]), tf.zeros([batch_size, 1])], axis=0
         )
-        labels += 0.05 * tf.random.uniform(tf.shape(labels))
 
         # Train discriminator model
         with tf.GradientTape() as d_tape:
             predictions = self.discriminator(images)
             d_loss = self.loss_fn(labels, predictions)
-        gradients = d_tape.gradient(d_loss, self.discriminator.trainable_weights)
-        self.d_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_weights))
+            gradients = d_tape.gradient(d_loss, self.discriminator.trainable_variables)
+            self.d_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
 
         return d_loss
 
