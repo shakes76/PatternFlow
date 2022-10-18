b7e84901efb3e1b09c66c840b61cff2c2dc731f3
sadrabt <sbayattork@gmail.com>
2022-10-18 10:51:38 +1000

    Resize the input data by factor of 4

--- recognition/46425487_ADNI_MRI_super-resolution/dataset.py
+++ recognition/46425487_ADNI_MRI_super-resolution/dataset.py
@@ -37,4 +37,17 @@ def scale(image):
 # Scale to (0, 1)
 train_ds = train_ds.map(scale)
 valid_ds = valid_ds.map(scale)
+
+def resize_input(input):
+    return tf.image.resize(input, [256//4, 240//4], method="area")
+
+train_ds = train_ds.map(
+    lambda x: (resize_input(x), x)
+)
+train_ds = train_ds.prefetch(buffer_size=32)
+
+valid_ds = valid_ds.map(
+    lambda x: (resize_input(x), x)
+)
+valid_ds = valid_ds.prefetch(buffer_size=32)
         
