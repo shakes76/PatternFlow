5a5431b6adaa0218f77bddb4a4edf7b987690a4e
MikStap <111872333+MikStap@users.noreply.github.com>
2022-10-19 08:29:01 +1000

    Functional DataLoader

--- recognition/AA_VQVAE_Mine/dataset.py
+++ recognition/AA_VQVAE_Mine/dataset.py
@@ -20,8 +20,8 @@ from pathlib import Path
 from random import sample, choice
 import shutil
 
-img_w = 4288
-img_h = 2848
+img_h = 4288
+img_w = 2848
 b_size = 32
 partition = {}
 labels = {}
@@ -52,7 +52,7 @@ test_labels = list((im_root / "ISIC-2017_Test_Truth").glob("*.png"))
 val_imgs = list((im_root / "ISIC-2017_Validation_Data").glob("*.jpg"))
 val_labels = list((im_root / "ISIC-2017_Validation_Truth").glob("*.png"))
 
-(len(train_imgs),len(train_labels)), (len(val_imgs),len(val_labels)) , (len(test_imgs),len(test_labels))
+(len(train_imgs),len(train_labels)), (len(test_imgs),len(test_labels)) , (len(val_imgs),len(val_labels))
 
 def make_pair(img,label,dataset):
     pairs = []
@@ -62,8 +62,8 @@ def make_pair(img,label,dataset):
     return pairs
 
 train_pair = make_pair(train_imgs, "ISIC-2017_Training_Truth", im_root)
-test_pair = make_pair(val_imgs, "ISIC-2017_Test_Truth", im_root)
-val_pair = make_pair(test_imgs, "ISIC-2017_Validation_Truth", im_root)
+test_pair = make_pair(test_imgs, "ISIC-2017_Test_Truth", im_root)
+val_pair = make_pair(val_imgs, "ISIC-2017_Validation_Truth", im_root)
 
 temp = choice(train_pair)
 img = img_to_array(load_img(temp[0], target_size=(img_w,img_h)))
@@ -96,107 +96,76 @@ def form_2D_label(mask,class_map):
 lab = form_2D_label(mask,class_map)
 np.unique(lab,return_counts=True)
 
+class DataGenerator(Sequence):
+    'Generates data for Keras'
+    
+    def __init__(self, pair, class_map, batch_size=16, dim=(224,224,3), shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.pair = pair
+        self.class_map = class_map
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.on_epoch_end()
 
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.pair) / self.batch_size))
 
-'''
-seed = 909 # (IMPORTANT) to transform image and corresponding mask with same augmentation parameter.
-image_datagen = ImageDataGenerator(width_shift_range=0.1,
-                 height_shift_range=0.1,
-                 preprocessing_function = image_preprocessing) # custom fuction for each image you can use resnet one too.
-mask_datagen = ImageDataGenerator(width_shift_range=0.1,
-                 height_shift_range=0.1,
-                 preprocessing_function = mask_preprocessing)  # to make mask as feedable formate (256,256,1)
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
-image_generator =image_datagen.flow_from_directory(os.path.join(im_root, "ISIC-2017_Training_Data"),
-                                                    class_mode=None, seed=seed)
+        # Find list of IDs
+        list_IDs_temp = [k for k in indexes]
 
-mask_generator = mask_datagen.flow_from_directory(os.path.join(im_root, "ISIC-2017_Training_Truth"),
-                                                   class_mode=None, seed=seed)
-'''
+        # Generate data
+        X, Y = self.__data_generation(list_IDs_temp)
+
+        return X, Y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.pair))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        batch_imgs = list()
+        batch_labels = list()
+
+        # Generate data
+        for i in list_IDs_temp:
+            # Store sample
+            img = load_img(self.pair[i][0] ,target_size=self.dim)
+            img = img_to_array(img)/255.
+            batch_imgs.append(img)
+
+            label = load_img(self.pair[i][1],target_size=self.dim)
+            label = img_to_array(label)
+            label = form_2D_label(label,self.class_map)
+            label = to_categorical(label , num_classes = 2)
+            batch_labels.append(label)
+            
+        return np.array(batch_imgs) ,np.array(batch_labels)
+
+train_generator = DataGenerator(train_pair+test_pair,class_map,b_size, dim=(img_w,img_h,3) ,shuffle=True)
+train_steps = train_generator.__len__()
+
+X,Y = train_generator.__getitem__(1)
+print(Y.shape)
+
+val_generator = DataGenerator(val_pair, class_map, batch_size=4, dim=(img_w,img_h,3) ,shuffle=True)
+val_steps = val_generator.__len__()
+
 
 print("no errors")
 
 
 '''
-im_root = path = os.path.join(os.getcwd(), "recognition\AA_VQVAE_Mine\DataSets\AD_NC")
-
-
-training_set = tf.keras.utils.image_dataset_from_directory(
-                        os.path.join(im_root,"train"),
-                        labels='inferred',
-                        label_mode='int',
-                        color_mode='grayscale',
-                        image_size=(image_width, image_height),
-                        batch_size = None,
-                        shuffle=True,
-                        seed=46,
-                        validation_split=0.3,
-                        subset='training',
-                        interpolation='bilinear',
-                        crop_to_aspect_ratio=True
-                    )
-
-validation_set = tf.keras.utils.image_dataset_from_directory(
-                        os.path.join(im_root,"train"),
-                        labels='inferred',
-                        label_mode='int',
-                        color_mode='grayscale',
-                        image_size=(image_width, image_height),
-                        batch_size = None,
-                        shuffle=True,
-                        seed=46,
-                        validation_split=0.3,
-                        subset='validation',
-                        interpolation='bilinear',
-                        crop_to_aspect_ratio=True
-                    )
-
-test_set = tf.keras.utils.image_dataset_from_directory(
-                    os.path.join(im_root,"test"),
-                    labels='inferred',
-                    label_mode='int',
-                    color_mode='grayscale',
-                    image_size=(image_width, image_height),
-                    batch_size = None,
-                    shuffle=True,
-                    seed=46,
-                    interpolation='bilinear',
-                    crop_to_aspect_ratio=True
-                )
-
-class_names = training_set.class_names
-#print(class_names)
-
-
-"""Convert images to floating point with the range [0.5, 0.5]"""
-(x_train, y_train) = zip(*training_set)
-#x_train = np.expand_dims(x_train, -1)
-x_train = np.asarray(x_train)
-x_train_scaled = (x_train / 255.0) - 0.5
-(x_val,y_val) = zip(*validation_set)
-#x_val = np.expand_dims(x_val, -1)
-x_val = np.asarray(x_val)
-x_val_scaled = (x_val / 255.0) - 0.5
-(x_test,y_test) = zip(*test_set)
-#x_test = np.expand_dims(x_test, -1)
-x_test = np.asarray(x_test)
-x_test_scaled = (x_test / 255.0) - 0.5
-
-
-data_variance = np.var(x_train / 255.0)
-
-
-
-#And plot images
-plt.figure(figsize=(10, 10))
-for images, labels in training_set.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"),cmap='gray')
-        plt.title(class_names[labels[i]])
-        plt.axis("off")
-
-plt.show()
 
 import pickle
 
@@ -212,4 +181,5 @@ with open(filename, "wb") as file:
 filename = "resultc.pickle"
 with open(filename, "wb") as file:
     pickle.dump(x_test_scaled, file)
-    '''
+
+'''
