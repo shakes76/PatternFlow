b3c4c3085081ab73f099a57957581f6a99a7a300
Zhao Wang <68866053+wangzhaomxy@users.noreply.github.com>
2022-10-18 10:54:10 +1000

    Update modules.py

--- recognition/s46704847_diffusion_OASIS/modules.py
+++ recognition/s46704847_diffusion_OASIS/modules.py
@@ -110,7 +110,8 @@ def show_forward_noise(train_images):
 
 
 """Constructing U-Net model"""
-#Helper functions
+
+"""Helper functions"""
 
 def exists(x):
     return x is not None
@@ -195,3 +196,218 @@ class GELU(Layer):
 
     def call(self, x, training=True):
         return gelu(x, self.approximate)
+
+
+""" Building blocks of the U-Net model"""
+
+# building block modules
+class Block(Layer):
+    def __init__(self, dim, groups=8):
+        super(Block, self).__init__()
+        self.proj = nn.Conv2D(dim, kernel_size=3, strides=1, padding='SAME')
+        self.norm = tfa.layers.GroupNormalization(groups, epsilon=1e-05)
+        self.act = SiLU()
+
+
+    def call(self, x, gamma_beta=None, training=True):
+        x = self.proj(x)
+        x = self.norm(x, training=training)
+
+        if exists(gamma_beta):
+            gamma, beta = gamma_beta
+            x = x * (gamma + 1) + beta
+
+        x = self.act(x)
+        return x
+
+class ResnetBlock(Layer):
+    def __init__(self, dim, dim_out, time_emb_dim=None, groups=8):
+        super(ResnetBlock, self).__init__()
+
+        self.mlp = Sequential([
+            SiLU(),
+            nn.Dense(units=dim_out * 2)
+        ]) if exists(time_emb_dim) else None
+
+        self.block1 = Block(dim_out, groups=groups)
+        self.block2 = Block(dim_out, groups=groups)
+        self.res_conv = nn.Conv2D(filters=dim_out, kernel_size=1, 
+                                  strides=1) if dim != dim_out else Identity()
+
+    def call(self, x, time_emb=None, training=True):
+        gamma_beta = None
+        if exists(self.mlp) and exists(time_emb):
+            time_emb = self.mlp(time_emb)
+            time_emb = rearrange(time_emb, 'b c -> b 1 1 c')
+            gamma_beta = tf.split(time_emb, num_or_size_splits=2, axis=-1)
+
+        h = self.block1(x, gamma_beta=gamma_beta, training=training)
+        h = self.block2(h, training=training)
+
+        return h + self.res_conv(x)
+
+class LinearAttention(Layer):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super(LinearAttention, self).__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        self.hidden_dim = dim_head * heads
+
+        self.attend = nn.Softmax()
+        self.to_qkv = nn.Conv2D(filters=self.hidden_dim * 3, kernel_size=1, strides=1, use_bias=False)
+
+        self.to_out = Sequential([
+            nn.Conv2D(filters=dim, kernel_size=1, strides=1),
+            LayerNormalization()
+        ])
+
+    def call(self, x, training=True):
+        b, h, w, c = x.shape
+        qkv = self.to_qkv(x)
+        qkv = tf.split(qkv, num_or_size_splits=3, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b x y (h c) -> b h c (x y)', h=self.heads), qkv)
+
+        q = tf.nn.softmax(q, axis=-2)
+        k = tf.nn.softmax(k, axis=-1)
+
+        q = q * self.scale
+        context = einsum('b h d n, b h e n -> b h d e', k, v)
+
+        out = einsum('b h d e, b h d n -> b h e n', context, q)
+        out = rearrange(out, 'b h c (x y) -> b x y (h c)', h=self.heads, x=h, y=w)
+        out = self.to_out(out, training=training)
+
+        return out
+
+class Attention(Layer):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super(Attention, self).__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        self.hidden_dim = dim_head * heads
+
+        self.to_qkv = nn.Conv2D(filters=self.hidden_dim * 3, kernel_size=1, strides=1, use_bias=False)
+        self.to_out = nn.Conv2D(filters=dim, kernel_size=1, strides=1)
+
+    def call(self, x, training=True):
+        b, h, w, c = x.shape
+        qkv = self.to_qkv(x)
+        qkv = tf.split(qkv, num_or_size_splits=3, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b x y (h c) -> b h c (x y)', h=self.heads), qkv)
+        q = q * self.scale
+
+        sim = einsum('b h d i, b h d j -> b h i j', q, k)
+        sim_max = tf.stop_gradient(tf.expand_dims(tf.argmax(sim, axis=-1), axis=-1))
+        sim_max = tf.cast(sim_max, tf.float32)
+        sim = sim - sim_max
+        attn = tf.nn.softmax(sim, axis=-1)
+
+        out = einsum('b h i j, b h d j -> b h i d', attn, v)
+        out = rearrange(out, 'b h (x y) d -> b x y (h d)', x = h, y = w)
+        out = self.to_out(out, training=training)
+
+        return out
+
+"""## Building the U-Net model"""
+
+class Unet(Model):
+    def __init__(self,
+                 dim=64,
+                 init_dim=None,
+                 out_dim=None,
+                 dim_mults=(1, 2, 4, 8),
+                 channels=3,
+                 resnet_block_groups=8,
+                 learned_variance=False,
+                 sinusoidal_cond_mlp=True
+                 ):
+        super(Unet, self).__init__()
+        
+        # determine dimensions
+        self.channels = channels
+        
+        init_dim = default(init_dim, dim // 3 * 2)
+        self.init_conv = nn.Conv2D(filters=init_dim, kernel_size=7, strides=1, 
+                                                                padding='SAME')
+        
+        dims = [init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+        
+        block_klass = partial(ResnetBlock, groups = resnet_block_groups)
+        
+        # time embeddings
+        time_dim = dim * 4
+        self.sinusoidal_cond_mlp = sinusoidal_cond_mlp
+        
+        self.time_mlp = Sequential([
+            SinusoidalPosEmb(dim),
+            nn.Dense(units=time_dim),
+            GELU(),
+            nn.Dense(units=time_dim)
+        ], name="time embeddings")
+        
+        # layers
+        self.downs = []
+        self.ups = []
+        num_resolutions = len(in_out)
+        
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append([
+                block_klass(dim_in, dim_out, time_emb_dim=time_dim),
+                block_klass(dim_out, dim_out, time_emb_dim=time_dim),
+                Residual(PreNorm(dim_out, LinearAttention(dim_out))),
+                Downsample(dim_out) if not is_last else Identity()
+            ])
+  
+        mid_dim = dims[-1]
+        self.mid_block1 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        self.mid_attn = Residual(PreNorm(mid_dim, Attention(mid_dim)))
+        self.mid_block2 = block_klass(mid_dim, mid_dim, time_emb_dim=time_dim)
+        
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.ups.append([
+                block_klass(dim_out * 2, dim_in, time_emb_dim=time_dim),
+                block_klass(dim_in, dim_in, time_emb_dim=time_dim),
+                Residual(PreNorm(dim_in, LinearAttention(dim_in))),
+                Upsample(dim_in) if not is_last else Identity()
+            ])
+        
+        default_out_dim = channels * (1 if not learned_variance else 2)
+        self.out_dim = default(out_dim, default_out_dim)
+        
+        self.final_conv = Sequential([
+            block_klass(dim * 2, dim),
+            nn.Conv2D(filters=self.out_dim, kernel_size=1, strides=1)
+        ], name="output")
+        
+    def call(self, x, time=None, training=True, **kwargs):
+        x = self.init_conv(x)
+        t = self.time_mlp(time)
+        
+        h = []
+
+        for block1, block2, attn, downsample in self.downs:
+            x = block1(x, t)
+            x = block2(x, t)
+            x = attn(x)
+            h.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_attn(x)
+        x = self.mid_block2(x, t)
+
+        for block1, block2, attn, upsample in self.ups:
+            x = tf.concat([x, h.pop()], axis=-1)
+            x = block1(x, t)
+            x = block2(x, t)
+            x = attn(x)
+            x = upsample(x)
+
+        x = tf.concat([x, h.pop()], axis=-1)
+        x = self.final_conv(x)
+        return x
