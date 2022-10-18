39ce39b61c64adb838bb8759c8184929ecaad6e6
SmArT4899 <56865527+SmArT4899@users.noreply.github.com>
2022-10-18 14:25:41 +1000

    Add files via upload
    Changes to dataset file

--- recognition/s46852430/dataset.py
+++ recognition/s46852430/dataset.py
@@ -3,77 +3,113 @@
 Created on Mon Oct  3 17:57:39 2022
 
 @author: eudre
-"""
 
-"""
-Created on Mon Oct  3 17:29:06 2022
-
-@author: eudre
 """
 #importing the libraries
 import os 
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import glob
+import cv2
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
-train_path ='C:\\Users\\eudre\\test\\AD_NC\\train'
-test_path ='C:\\Users\\eudre\\test\\AD_NC\\test'
+train_path ='C:\\Users\\eudre\\test\\ISIC-2017_Training_Data'
+mask_path ='C:\\Users\\eudre\\test\\ISIC-2017_Training_Part1_GroundTruth'
 
-class_names = ['AD', 'NC']
+H = 256
+W = 256
 
-# apply glob module to retrieve files/pathnames  
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        
+# Use to delete superpixel image
+def delete_super(path):
+    os.chdir(train_path)
+    for fname in os.listdir(train_path):
+        if fname.endswith('superpixels.png') & fname.endswith('.csv'):
+            os.remove(fname)
 
-x_train=[]
-x_test=[]
-for folder in os.listdir(train_path):
-    sub_path=train_path+"/"+folder
-
-    for img in os.listdir(sub_path):
-
-        image_path=sub_path+"/"+img
-
-        img_arr=cv2.imread(image_path)
-
-        img_arr=cv2.resize(img_arr,(224,224))
-
-        x_train.append(img_arr)
+def load_data(train_path, mask_path):
+    images = sorted(glob(os.path.join(train_path, "*.jpg")))
+    masks = sorted(glob(os.path.join(mask_path, "*.jpg")))
     
-for folder in os.listdir(test_path):
-    sub_path=test_path+"/"+folder
-
-    for img in os.listdir(sub_path):
-
-        image_path=sub_path+"/"+img
-
-        img_arr=cv2.imread(image_path)
-
-        img_arr=cv2.resize(img_arr,(224,224))
-
-        x_test.append(img_arr)
+    test_size = int(len(images) * 0.2)
     
-train_x=np.array(x_train)
-test_x=np.array(x_test)
+    train_x, valid_x = train_test_split(images, test_size=test_size, random_state=42)
+    train_y, valid_y = train_test_split(masks, test_size=test_size, random_state=42)
 
-train_x=train_x/255.0
-test_x=test_x/255.0
+    train_x, test_x = train_test_split(train_x, test_size=test_size, random_state=42)
+    train_y, test_y = train_test_split(train_y, test_size=test_size, random_state=42)
+   
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
 
-train_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory(train_path,
-                                                 target_size = (224, 224),
-                                                 batch_size = 32,
-                                                 class_mode = 'sparse')
+def read_image(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_COLOR)  ## (H, W, 3)
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)
+    return x                                ## (256, 256, 3)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory(test_path,
-                                            target_size = (224, 224),
-                                            batch_size = 32,
-                                            class_mode = 'sparse')
+def read_mask(path):
+    path = path.decode()
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  ## (H, W)
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)                    ## (256, 256)
+    x = np.expand_dims(x, axis=-1)              ## (256, 256, 1)
+    return x
 
+def tf_parse(x, y):
+    def _parse(x, y):
+        x = read_image(x)
+        y = read_mask(y)
+        return x, y
 
-train_y=training_set.classes
-test_y=test_set.classes
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float32, tf.float32])
+    x.set_shape([H, W, 3])
+    y.set_shape([H, W, 1])
+    return x, y
 
+def tf_dataset(X, Y, batch):
+    dataset = tf.data.Dataset.from_tensor_slices((X, Y))
+    dataset = dataset.map(tf_parse)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(10)
+    return dataset
 
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
 
+    """ Folder for saving data """
+    create_dir("files")
 
+    """ Hyperparameters """
+    batch_size = 4
+    lr = 1e-4 ## (0.0001)
+    num_epoch = 5
+    model_path = "files/model.h5"
+    csv_path = "files/data.csv"
+
+    """ Dataset : 60/20/20 """
+    dataset_path = "/media/nikhil/ML/ml_dataset/isic-challenge-2018/"
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(train_path, mask_path)
+
+    print(f"Train: {len(train_x)} - {len(train_y)}")
+    print(f"Valid: {len(valid_x)} - {len(valid_y)}")
+    print(f"Test: {len(test_x)} - {len(test_y)}")
+
+    train_dataset = tf_dataset(train_x, train_y, batch_size)
+    valid_dataset = tf_dataset(valid_x, valid_y, batch_size)
+
+    train_steps = len(train_x)//batch_size
+    valid_steps = len(valid_x)//batch_size
+
+    if len(train_x) % batch_size != 0:
+        train_steps += 1
+
+    if len(valid_x) % batch_size != 0:
+        valid_steps += 1
