Calculated training variance and fixed data-related constants (e.g. file path, etc.)

recognition/45811935_Project/dataset.py
@@ -6,9 +6,11 @@
 """
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
-FILE_PATH = "./ADNI_AD_NC_2D/"
+""" Main data-related constants """
+FILE_PATH = "./ADNI_AD_NC_2D/AD_NC/"
 IMG_DIMENSION = 256
 BATCH_SIZE = 32
+SHIFT = 0.5
 
 
 def load_preprocess_image_data(path, img_dim, batch_size, shift):
@@ -24,10 +26,10 @@ def load_preprocess_image_data(path, img_dim, batch_size, shift):
             A tf.data.Dataset of the image files.
     """
     img_data = image_dataset_from_directory(path,
-                                        label_mode=None,
-                                        image_size=(img_dim, img_dim),
-                                        color_mode="rgb",
-                                        batch_size=batch_size,
-                                        shuffle=True)
+                                            label_mode=None,
+                                            image_size=(img_dim, img_dim),
+                                            color_mode="rgb",
+                                            batch_size=batch_size,
+                                            shuffle=True)
 
     return img_data.map(lambda x: (x / 255.0) - shift)

recognition/45811935_Project/train.py
@@ -5,6 +5,8 @@
     Author: Adrian Rahul Kamal Rajkamal
     Student Number: 45811935
 """
+import numpy as np
+
 import tensorflow as tf
 import tensorflow_probability as tfp
 import matplotlib.pyplot as plt
@@ -14,8 +16,20 @@ from modules import *
 """ APPROACH """
 
 # Load data (which also pre-processes it)
+train_data = load_preprocess_image_data(FILE_PATH + "train", IMG_DIMENSION, BATCH_SIZE, SHIFT)
 
-# Calculate training variance --> maybe goes into dataset.py or make a utils.py?
+# Calculate (unbiased) training variance (across all pixels of all images)
+full_train_data = train_data.unbatch()
+num_train_images = full_train_data.reduce(np.int32(0), lambda x, _: x + 1).numpy()
+num_train_pixels = num_train_images * IMG_DIMENSION ** 2
+
+image_sum = full_train_data.reduce(np.float32(0), lambda x, y: x + y).numpy().flatten()
+total_pixel_sum = image_sum.sum()
+mean_pixel_train = total_pixel_sum / num_train_pixels
+
+image_sse = full_train_data.reduce(np.float32(0), lambda x, y: x + (y - mean_pixel_train) ** 2)\
+                           .numpy().flatten()
+var_pixel_train = image_sse.sum() / (num_train_pixels - 1)
 
 # Create VQ-VAE model
 
@@ -27,11 +41,11 @@ from modules import *
 
 # Test/generate --> maybe load saved model above and put this in predict.py
 
-# Get VQ-VAE codebooks
+# Get trained VQ-VAE codebooks
 
 # Create PixelCNN model
 
-# Compile & Fit (with VQ-VAE codebooks)
+# Compile & Fit (with trained VQ-VAE codebooks)
 
 # Save
 