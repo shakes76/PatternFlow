81afff7b599c7ff14fee074474b1b7419432fddf
Dan <dong.lee2@uqconnect.edu.au>
2022-10-19 22:57:38 +1000

    significant refactoring of code from TF to Pytorch

--- recognition/brain-transformer/dataset.py
+++ recognition/brain-transformer/dataset.py
@@ -17,3 +17,34 @@ def load_train_data():
     train_data = train_data.map(lambda x,y:(norm(x),y))
     valid_data = valid_data.map(lambda x,y:(norm(x),y))
     return train_data, valid_data
+
+
+
+
+
+
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+
+def torch_train(batch_size=64,validation_split=0.25):
+    transform = transforms.Compose([transforms.Resize((256, 256)),transforms.ToTensor()])
+    dataset = datasets.ImageFolder('train',transform=transform)
+    size = len(dataset)
+    valid_size = int(np.floor(validation_split*size))
+    indicies = list(range(size))
+    np.random.shuffle(indicies)
+    train_indices, val_indices = indicies[valid_size:], indicies[:valid_size]
+    tsampler = SubsetRandomSampler(train_indices)
+    vsampler = SubsetRandomSampler(val_indices)
+    train_loader = DataLoader(dataset, batch_size=batch_size,sampler=tsampler)
+    validation_loader = DataLoader(dataset, batch_size=batch_size,sampler=vsampler)
+    return train_loader, validation_loader
+
+def torch_test(batch_size=64):
+    transform = transforms.Compose([transforms.Resize((256, 256)),transforms.ToTensor()])
+    dataset = datasets.ImageFolder('test',transform=transform)
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+    return dataloader
