0fd6e261e02549804505ab6eac7fad096ee53a6d
Luke Harrison <luke2000harrison@gmail.com>
2022-10-19 14:49:47 +1000

    Updated Modules. Now includes Context, Upsampling, Stride 2 and Localisation Modules.
    All modules in working order except for Localisation modules.
    
    Majority of UNet Path implemented, still needs Segmentation Layers and
    Softmax Output.

--- recognition/s4587520-UNet/modules.py
+++ recognition/s4587520-UNet/modules.py
@@ -6,14 +6,13 @@ from torch import nn
 class UNet(nn.Module):
   def __init__(self):
     super().__init__()
-    self.input = nn.Sequential(
-        nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3),
-    )
+    self.conv16 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3)
+    self.conv32 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3)
     self.context16 = Context_Module(16)
     self.context32 = Context_Module(32)
     self.context64 = Context_Module(64)
     self.context128 = Context_Module(128)
-    self.contect256 = Context_Module(256)
+    self.context256 = Context_Module(256)
     self.stride32 = Stride2_Conv(32)
     self.stride64 = Stride2_Conv(64)
     self.stride128 = Stride2_Conv(128)
@@ -22,23 +21,36 @@ class UNet(nn.Module):
     self.upsample32 = Upsampling_Module(32)
     self.upsample64 = Upsampling_Module(64)
     self.upsample128 = Upsampling_Module(128)
-    
+    self.local16 = Localisation_Module(16)
+    self.local32 = Localisation_Module(32)
+    self.local64 = Localisation_Module(64)
+    self.local128 = Localisation_Module(128)
     
 
   def forward(self, x):
-    x = self.input(x)
+    x = self.conv16(x)
     x1 = self.context16(x)
-    x2 = self.stride32(x1)
-    x2 = self.context32(x2)
-    x3 = self.stride64(x2)
-    x3 = self.context64(x3)
-    x4 = self.stride128(x3)
-    x4 = self.context128(x4)
-    x5 = self.stride256(x4)
-    x5 = self.context256(x5)
+    x = self.stride32(x1)
+    x2 = self.context32(x)
+    x = self.stride64(x2)
+    x3 = self.context64(x)
+    x = self.stride128(x3)
+    x4 = self.context128(x)
+    x = self.stride256(x4)
+    x = self.context256(x)
+    print(x.size())
+    print(x4.size())
+    x = self.upsample128(x, x4)
+    x = self.local128(x)
+    x = self.upsample64(x, x3)
+    x5 = self.local64(x)
+    x = self.upsample32(x5, x2)
+    x6 = self.local32(x)
+    x = self.upsample16(x6, x1)
+    x = self.conv32(x)
     
-    prediction = self.softmax(x)
-    return prediction
+    #prediction = self.softmax(x)
+    return x
 
 
 #Context Module: 2 3x3 convs connected by 0.3 dropout, adds input after module
@@ -46,14 +58,14 @@ class Context_Module(nn.Module):
   def __init__(self, output_filters):
     super().__init__()
     self.stack = nn.Sequential(
-      nn.Conv2d(output_filters, output_filters, kernel_size=3),
+      nn.Conv2d(output_filters, output_filters, kernel_size=3, padding=1),
       nn.Dropout(p=0.3),
-      nn.Conv2d(output_filters, output_filters, kernel_size=3)
+      nn.Conv2d(output_filters, output_filters, kernel_size=3, padding=1)
     )
 
   def forward(self, x):
     #Return result of module elemnt-wise summed with input
-    return torch.sum(x, self.stack(x))
+    return torch.add(x, self.stack(x))
 
 
 #Repeat each feature voxel twice in each dimension, then 3x3 conv
@@ -61,12 +73,12 @@ class Upsampling_Module(nn.Module):
   def __init__(self, output_filters):
     super().__init__()
     self.stack = nn.Sequential(
-      nn.ReplicationPad3d(padding=1), #Padding value is applied in both directions on each dimensions
-      nn.Conv2d(output_filters, output_filters, kernel_size=3)
+      nn.UpsamplingNearest2d(scale_factor=2), #Padding value is applied in both directions on each dimensions
+      nn.Conv2d((2*output_filters), output_filters, kernel_size=3, padding=2)
     )
 
   def forward(self, main, cat):
-    #main is the signal to be processed and cat is the singal to be concatenated
+    #main is the signal to be processed and cat is the signal to be concatenated
     return torch.cat((self.stack(main), cat))
 
 #Performs a 3x3 conv with stride 2 at set output filters
@@ -74,21 +86,20 @@ class Stride2_Conv(nn.Module):
   def __init__(self, output_filters):
     super().__init__()
     self.stack = nn.Sequential(
-      nn.Conv2d(2*output_filters, output_filters, kernel_size=3, stride=2)
+      nn.Conv2d(int(output_filters/2), output_filters, kernel_size=3, stride=2)
     )
 
   def forward(self, x):
-    #main is the signal to be processed and cat is the singal to be concatenated
     return self.stack(x)
   
-  #Localisation module
-  #3x3 conv followed by 1x1 conv that halves features
-  class Localisation_Module(nn.Module):
-    def __init__(self, output_filters):
-      super().__init__()
-      self.stack = nn.Sequential(
-          nn.Conv2d(2*output_filters, 2*output_filters, kernel_size=3),
-          nn.Conv2d(2*output_filters, output_filters, kernel_size=1)
-      )
-    def forward(self, x):
-      return self.stack(x)
+#Localisation module
+#3x3 conv followed by 1x1 conv that halves features
+class Localisation_Module(nn.Module):
+  def __init__(self, output_filters):
+    super().__init__()
+    self.stack = nn.Sequential(
+        nn.Conv2d(2*output_filters, 2*output_filters, kernel_size=3),
+        nn.Conv2d(2*output_filters, output_filters, kernel_size=1)
+    )
+  def forward(self, x):
+    return self.stack(x)
