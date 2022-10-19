218563b2e693320ac35debe03b82555b72781c24
Niklas <nikmatha@gmail.com>
2022-10-19 16:39:00 +1000

    Imported functions from other files so that train.py can run seamlessly

--- recognition/46418108_YOLO_ISIC/train.py
+++ recognition/46418108_YOLO_ISIC/train.py
@@ -1,3 +1,18 @@
 import os
+import subprocess
+from modules import *
 
-os.system("pip install -r yolov5/requirements.txt")
+def install_requirements():
+    os.system("pip install -r yolov5/requirements.txt")
+
+def train(batch_size : int, epochs : int, workers : int, weights: str, name : str):
+    os.system("cd yolov5")
+    command = "python train.py --img 640 --batch " + str(batch_size) + " --epochs " + str(epochs)  + " --data data.yaml --weights " + weights +  " --workers " + str(workers) + " --name " + name
+    print(command)
+    #os.system(command)
+
+
+if __name__ == '__main__':
+    #get_yolo()
+    #install_requirements()
+    train(16, 300, 24, "yolov5l.pt", "yolo_lesion_det")
