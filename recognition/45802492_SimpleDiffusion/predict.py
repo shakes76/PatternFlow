66d48d39a10538c70ce04b758a90bfe9dd4197ec
michaelholmes <michaelholmes@hcloudh.com>
2022-10-19 11:54:53 +1000

    Fixed predict not saving image

--- recognition/45802492_SimpleDiffusion/predict.py
+++ recognition/45802492_SimpleDiffusion/predict.py
@@ -3,12 +3,13 @@ from modules import Unet, Trainer
 from torch.utils.data import DataLoader
 from dataset import Dataset
 from torch.optim import Adam
+import os
 
 #Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('model', help="Path to model", type=str)
-parser.add_argument('--output', '-o', help="Output path to save images to", type=str)
-parser.add_argument('--num_images', '-i', help="Number of images to create", type=int)
+parser.add_argument('--output', '-o', help="Output path to save images to default: ./", type=str)
+parser.add_argument('--num_images', '-i', help="Number of images to create, deafult: 1", type=int)
 parser.add_argument('--name', '-n', help="Name prefix for images", type=str)
 args = parser.parse_args()
 
@@ -17,7 +18,7 @@ model = args.model
 if args.num_images:
     num_images = args.num_images
 else:
-    num_images = 0
+    num_images = 1
 if args.output:
     output = args.output
 else:
@@ -33,6 +34,11 @@ trainer = Trainer(img_size=64, timesteps=1000, start=0.0001, end=0.02)
 #load model
 trainer.load_model(model)
 
-#create images
+#Create directory if none exist
+if output != './':
+    exists = os.path.exists(output)
+    if not exists:
+        os.makedirs(output)
+
 for i in range(num_images):
     trainer.generate_image(output + name + "{}.jpeg".format(i))
