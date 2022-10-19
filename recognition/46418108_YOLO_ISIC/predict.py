954ab142c90a97e6706fbfdd112f322279b57472
Niklas <nikmatha@gmail.com>
2022-10-19 16:40:01 +1000

    Finished creating all the functions for inference.

--- recognition/46418108_YOLO_ISIC/predict.py
+++ recognition/46418108_YOLO_ISIC/predict.py
@@ -2,15 +2,14 @@ import torch
 import cv2
 from PIL import Image
 
-def load_model():
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
+def load_model(path='best.pt'):
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=path, force_reload=True)
     return model
 
-
-def predict(model, image_path='ISIC_0010251.jpg'):
+def predict(model, image_path):
     im = Image.open(image_path)
     results = model(im)
-    results.show()
+    results.save()
 
 model = load_model()
-predict(model)
+predict(model, "path_to_image")
