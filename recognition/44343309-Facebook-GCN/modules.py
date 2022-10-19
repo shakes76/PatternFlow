b3c0766a54ebb162eb4e18fe0fd1b99d72f7033f
Rayve <rgreenhillbrown@gmail.com>
2022-10-19 19:00:53 +1000

    updated how GCN model actually runs

--- recognition/44343309-Facebook-GCN/modules.py
+++ recognition/44343309-Facebook-GCN/modules.py
@@ -1,25 +1,29 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import datasets, layers, models, regularizers, preprocessing, Model
-from tensorflow.keras.layers import Layer
+from tensorflow.keras import datasets, layers, models, regularizers, preprocessing, Model, regularizers, activations, initializers, constraints, backend
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.layers import Layer, Input, Dropout
 import matplotlib.pyplot as plt
 import numpy as np
 
-class GCN(Model):
-  def __init__(self):
-    super(GCN, self).__init__()
 
-  def call(self, inputs, training=False):
-    edges = inputs[1]
-    outModel = models.Sequential([
-        GCNLayer(input_dim=128, output_dim=64, activation="relu", kernel_regularizer=regularizers.L2(0.01))(inputs), 
-        layers.Dropout(0.3),
-        GCNLayer(input_dim=64, output_dim=16, activation="relu")(edges),
-        layers.Dropout(0.3),
-        GCNLayer(input_dim=16, output_dim=4, activation="softmax")(edges)
-        ])
-    return outModel
+def GCN(numNodes, numFeatures, numClasses, channelA=32, channelB=8, dropout=0.1):
+  features = Input(shape=(numFeatures))
+  nodes = Input((numNodes), sparse=True)
 
+  dp1 = Dropout(dropout)(features)
+  layer1 = GCNLayer(channelA, activation='relu')([dp1, nodes])
+
+  dp2 = Dropout(dropout)(layer1)
+  layer2 = GCNLayer(channelB, activation='relu')([dp2, nodes])
+
+  dp3 = Dropout(dropout)(layer2)
+  layer3 = GCNLayer(numClasses, activation='softmax')([dp3, nodes])
+
+  model = Model(inputs=[features, nodes], outputs=layer3)
+    
+  return model
 class GCNLayer(Layer):
   def __init__(self, inDim, outDim, activation=None, bias=False):
     super(GCNLayer, self).__init__()
