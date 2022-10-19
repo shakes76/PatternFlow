be903321c5b1a4085ddce5535812c1d8117c2ae7
Haolong Du <haolong.du@uqconnect.edu.au>
2022-10-19 20:04:00 +1000

    rescaled image to 244*244

--- recognition/46272784-Siamese/dataset.py
+++ recognition/46272784-Siamese/dataset.py
@@ -1,30 +1,7 @@
 # This file contains the data loader
-import numpy as np
 import os
-import tensorflow as tf
-import tensorflow_datasets as tfds
 from tensorflow import image
 from tensorflow.keras import utils
-import matplotlib.pyplot as plt
-from pathlib import Path
-
-# def preprocess_image(filename):
-#     """
-#     Load the specified file as a JPEG image, preprocess it and
-#     resize it to the target shape.
-#     """
-
-#     image_string = tf.io.read_file(filename)
-#     image = tf.image.decode_jpeg(image_string, channels=3)
-#     image = tf.image.convert_image_dtype(image, tf.float32)
-#     return image
-
-# def loadImage(dir):
-#     AD_dir = os.path.join(dir, 'train/AD')
-#     NC_dir = os.path.join(dir, 'train/NC')
-#     ad = sorted([os.path.join(AD_dir, f) for f in os.listdir(AD_dir)])
-#     nc = sorted([os.path.join(NC_dir, f) for f in os.listdir(NC_dir)])
-
 
 def transform_images(img):
     # transform to grayscale and standardize to [0,1]
@@ -46,7 +23,7 @@ def loadFile(dir, batch=8):
                                                      validation_split=0.3,
                                                      subset="training",
                                                      seed=1,
-                                                     image_size=(256, 240),
+                                                     image_size=(224, 224),
                                                      shuffle=True,
                                                      batch_size=batch)
     
@@ -56,7 +33,7 @@ def loadFile(dir, batch=8):
                                                      validation_split=0.3,
                                                      subset="training",
                                                      seed=1,
-                                                     image_size=(256, 240),
+                                                     image_size=(224, 224),
                                                      shuffle=True,
                                                      batch_size=batch)
     print('\n> 2/2 Loading Validation Data...')
@@ -66,7 +43,7 @@ def loadFile(dir, batch=8):
                                                      validation_split=0.3,
                                                      subset="validation",
                                                      seed=1,
-                                                     image_size=(256, 240),
+                                                     image_size=(224, 224),
                                                      shuffle=True,
                                                      batch_size=batch)
     valid_nc_ds = utils.image_dataset_from_directory(train_nc_dir, 
@@ -75,7 +52,7 @@ def loadFile(dir, batch=8):
                                                      validation_split=0.3,
                                                      subset="validation",
                                                      seed=1,
-                                                     image_size=(256, 240),
+                                                     image_size=(224, 224),
                                                      shuffle=True,
                                                      batch_size=batch)
     print('\n> Mapping datasets to greyscale...')
@@ -86,22 +63,10 @@ def loadFile(dir, batch=8):
     
     print('\n>> Data loading complete')
     return train_ad_ds, train_nc_ds, valid_ad_ds, valid_nc_ds
-    
-def plotExample(ds):
-    batch = ds.take(1)
-    for img, label in batch:
-        plt.axis("off")
-        plt.imshow((img.numpy()*255).astype("int32")[0], cmap='gray', vmin=0, vmax=255)
-        plt.show()
-        break
 
 def main():
     # Code for testing the functions
     t_a, t_n, v_a, v_n = loadFile('F:/AI/COMP3710/data/AD_NC/')
-    # plotExample(t)
-    batch = t.take(1)
-    for img, label in batch:
-        print(img, label)
 
 if __name__ == "__main__":
     main()
