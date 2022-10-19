1dc60b69dec470541f9505e165a7540bf2a0355c
GallagherN <gallagher_8186@hotmail.com>
2022-10-19 14:01:36 +1000

    put the training model into a function

--- recognition/46340496_super_resolution_network/train.py
+++ recognition/46340496_super_resolution_network/train.py
@@ -14,6 +14,7 @@ class ESPCNCallback(tf.keras.callbacks.Callback):
                 test_img = img
                 break
         self.test_img = tf.image.resize(test_img, (64, 60))
+        self.model = get_model(upscale_factor=UPSCALE_FACTOR, channels=1)
 
     # Store PSNR value in each epoch
     def on_epoch_begin(self, epoch, logs=None):
@@ -21,39 +22,39 @@ class ESPCNCallback(tf.keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         print("Mean PSNR for epoch: %.2f" %  (np.mean(self.psnr)))
-        # model, callbacks, loss_fn, optimizer = model_checkpoint()
         if True:
-            result = model(self.test_img[tf.newaxis, ...])[0]
+            result = self.model(self.test_img[tf.newaxis, ...])[0]
             plt.imshow(result)
             plt.show()
 
     def on_test_batch_end(self, batch, logs=None):
         self.psnr.append(10 * math.log10(1 / logs["loss"]))
 
-# def model_checkpoint():
+def model_checkpoint():
 
-model = get_model(upscale_factor=UPSCALE_FACTOR, channels=1)
-model.summary()
 
-callbacks = [ESPCNCallback()]
-loss_fn = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model = get_model(upscale_factor=UPSCALE_FACTOR, channels=1)
+    model.summary()
 
-    # return model, callbacks, loss_fn, optimizer
+    callbacks = [ESPCNCallback()]
+    loss_fn = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-# def training():
+    return model, callbacks, loss_fn, optimizer
 
-    # model, callbacks, loss_fn, optimizer = model_checkpoint()
+def training():
 
-epochs = 5
-train_ds, valid_ds = mapping_target()
-        
-model.compile(
-    optimizer=optimizer, loss=loss_fn,
-)
+    model, callbacks, loss_fn, optimizer = model_checkpoint()
 
-model.fit(
-    train_ds, epochs=epochs, callbacks=callbacks, validation_data=valid_ds, verbose=1
-)
+    epochs = 5
+    train_ds, valid_ds = mapping_target()
+            
+    model.compile(
+        optimizer=optimizer, loss=loss_fn,
+    )
 
-# sup = training()
+    model.fit(
+        train_ds, epochs=epochs, callbacks=callbacks, validation_data=valid_ds, verbose=1
+    )
+
+sup = training()
