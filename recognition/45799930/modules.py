a251fc01a8021a9ddfb9e7e8d08b6cda49026fd2
jessica <jessica@sullivans.id.au>
2022-10-19 10:41:25 +1000

    Added and Edited Documentation
    Added documentation for the new functions created and changed the spacing of the encoder part of create_module so it corresponds with the layers on the improved UNet. structure.

--- recognition/45799930/modules.py
+++ recognition/45799930/modules.py
@@ -26,77 +26,86 @@ def context_module(layer, num_filters):
 
 def upsample_module(layer, num_filters):
     """
+    Creates the up-sample module based off the improved UNet structure. First  you up-sample
+    followed by a 3x3 conv.
 
-    :param layer:
-    :param num_filters:
-    :return:
+    :param layer: the input layer within this module.
+    :param num_filters: the number of filters to pass into Conv2D.
+    :return: the final layer of this up-sample module.
     """
     up = UpSampling2D(2)(layer)
     return Conv2D(num_filters, 3, activation='relu', padding='same')(up)
 
 
+def localisation_module(layer, num_filters):
+    """
+    creates the localisation module based on the improved UNet structure. This is two convolutions,
+    with the last one being a kernel size of 1x1 and the first one being a kernel size of 3x3.
+
+    :param layer: the input layer within this module.
+    :param num_filters: the number of filters to pass into Conv2D.
+    :return: the final layer of this localisation module.
+    """
+    decoder = Conv2D(64, 3, activation='relu', padding='same')(layer)
+    return Conv2D(64, 1, activation='relu', padding='same')(decoder)
+
+
 def create_model(input_shape=INPUT_SHAPE):
     """
-    Create the improved unet model. This will include all the convolutions, max pooling,
-    and upsampling.
+    Create the improved unet model.
 
     :param input_shape: The shape of  the images that we will be passing into the model.
                         As we are using the one type of image we set the default to (256, 256, 1)
                         but added this feature so that it could be easily adapted to another model
                         if required in the future.
-    :return:
+    :return: the module that has been created.
     """
 
     # Create the input for the model.
     input = Input(shape=input_shape)
 
+    # initial 3x3  conv with filter of 16
+    setup = Conv2D(16, 3, activation='relu', padding='same')(input)
+
     # Create the encoder:
     # First layer : 2 x 2D Convolutions, filter size 16,  with a 3x3 kernel size and a stride size of (2,2).
-    encoder_layer1 = context_module(input, 16)
-
+    encoder_layer1 = context_module(setup, 16)
     # element wise sum of pre context module and post context module.
     sum1 = Add()([input, encoder_layer1])
 
     # Do conv with stride of 2 to reduce the size of the model.
     pool1 = Conv2D(32, 3, 2, activation='relu', padding='same')(sum1)
-
     # Second layer : 2 x 2D Convolutions, filter size 32,  with a 3x3 kernel size and a stride size of (2,2).
     encoder_layer2 = context_module(pool1, 32)
-
     # element wise sum of pre context module and post context module.
     sum2 = Add()([pool1, encoder_layer2])
 
     # Do conv with stride of 2 to reduce the size of the model.
     pool2 = Conv2D(64, 3, 2, activation='relu', padding='same')(sum2)
-
     # Third layer : 2 x 2D Convolutions, filter size 64,  with a 3x3 kernel size and a stride size of (2,2).
     encoder_layer3 = context_module(pool2, 64)
-
     # element wise sum of pre context module and post context module.
     sum3 = Add()([pool2, encoder_layer3])
 
     # Do conv with stride of 2 to reduce the size of the model.
     pool3 = Conv2D(128, 3, 2, activation='relu', padding='same')(sum3)
-
     # Forth layer : 2 x 2D Convolutions, filter size 128,  with a 3x3 kernel size and a stride size of (2,2).
     encoder_layer4 = context_module(pool3, 128)
-
     # element wise sum of pre context module and post context module.
     sum4 = Add()([pool3, encoder_layer4])
 
     # Do conv with stride of 2 to reduce the size of the model.
     pool4 = Conv2D(256, 3, 2, activation='relu', padding='same')(sum4)
-
     # Fifth layer : 2 x 2D Convolutions, filter size 256,  with a 3x3 kernel size and a stride size of (2,2).
     encoder_layer5 = context_module(pool4, 256)
-
     # element wise sum of pre context module and post context module.
     sum5 = Add()([pool4, encoder_layer5])
 
     # Create the decoder:
 
     # up sample layer 5 to get to layer 4
-    up4 = upsample_module(encoder_layer5, 128)
+    up4 = upsample_module(sum5, 128)
+
     # concatenate the output of encoder_layer4 with  the up sample
     con4 = concatenate([up4, encoder_layer4])
     # Forth layer : 2 x 2D Convolutions, filter size 128,  with a 3x3 kernel size for the first one and 1x1 for the
