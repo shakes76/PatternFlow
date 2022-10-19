bd9c48dbce94ad3a674bd8ffd178a424059fdb21
Amit Levin <levinamit6@gmail.com>
2022-10-19 20:39:14 +1000

    Added plots to show loss and dsc

--- recognition/45812473_pattern_recognition/train.py
+++ recognition/45812473_pattern_recognition/train.py
@@ -2,6 +2,7 @@ from dataset import process_data, train_val_test_split
 from modules import ImprovedUnetModel
 from utils import dice_similarity
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -16,12 +17,32 @@ def main():
     xtrain, xval, xtest = train_val_test_split(data, 0.8, 0.1)
     ytrain, yval, ytest = train_val_test_split(masks, 0.8, 0.1)
 
+    # Build compile and fit the model
     unet = ImprovedUnetModel()
     unet.build_model()
     unet.model.compile(optimizer = tf.keras.optimizers.Adam(), loss = "binary_crossentropy", metrics = [dice_similarity])
     model = unet.model
     model.summary()
-    history = model.fit(xtrain, ytrain, epochs = 25, validation_data = (xval, yval), batch_size = 64)
+    metrics = model.fit(xtrain, ytrain, epochs = 30, validation_data = (xval, yval), batch_size = 64)
+
+    # Plot the loss and the dice similarity coefficient of the model
+    fig, (loss, dsc) = plt.subplots(2, sharex = True)
+
+    loss.set_title("Loss vs Epoch")
+    loss.plot(range(len(metrics.history['loss'])), metrics.history['loss'], label = 'Training Loss')
+    loss.plot(range(len(metrics.history['val_loss'])), metrics.history['val_loss'], label = 'Validation Loss')
+    loss.legend()
+
+    dsc.set_title("Dice Similarity Coefficient vs Epoch")
+    dsc.plot(range(len(metrics.history['dice_similarity'])), metrics.history['dice_similarity'], label = "Training Dice Similarity Coefficient")
+    dsc.plot(range(len(metrics.history['val_dice_similarity'])), metrics.history['val_dice_similarity'], label = "Validation Dice Similarity Coefficient")
+    dsc.legend()
+
+    fig.show()
+    fig.savefig("Loss_DSC")
+
+
+
 
     return
 
