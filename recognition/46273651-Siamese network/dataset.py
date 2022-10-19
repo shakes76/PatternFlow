cbb2706b4d3edc667b2ca7fae560c778ffbbe98e
JasonZhao1 <zzc010220@gmail.com>
2022-10-19 12:28:45 +1000

    Reconstruct the methods of loading data and data preprocessing

--- recognition/46273651-Siamese network/dataset.py
+++ recognition/46273651-Siamese network/dataset.py
@@ -1,120 +1,99 @@
+import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-import tensorflow as tf
 
-class data_loader():
-    def __init__(self, path, label):
-        self.path = path
-        self.label = label
 
-        self.data = np.array([])
-        self.labels = np.array([])
-        self.load_data()
-        self.load_labels()
 
-    def load_data(self):
-        directory = os.listdir(self.path)
-        self.data = np.array([plt.imread(self.path + '/' + i) for i in directory])
-        self.data = np.expand_dims(self.data, axis=3)
+def load_data(path, img_size):
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        path, label_mode=None, color_mode = 'grayscale', image_size=img_size, batch_size=16
+    )
 
-    def load_labels(self):
-        if self.label == 'AD':
-            self.labels = np.ones(self.data.shape[0])
-        elif self.label == 'NC':
-            self.labels = np.zeros(self.data.shape[0])
+    # normalize the data
+    dataset = dataset.map(lambda x: x / 255.0)
 
-    def get_data(self):
-        return self.data
+    # unbatch the data
+    dataset = dataset.unbatch()
 
-    def get_labels(self):
-        return self.labels
+    return dataset
 
-class data_preprocess():
-    def __init__(self, AD, NC):
-        self.AD = AD
-        self.NC = NC
 
-        self.data = np.concatenate((self.AD.get_data(), self.NC.get_data()), axis=0)
-        self.labels = np.concatenate((self.AD.get_labels(), self.NC.get_labels()), axis=0)
-        self.tf_ds = tf.data.Dataset.from_tensor_slices((self.data, self.labels))
+def make_pair(datset1, dataset2):
+    # zip the data and labels if their labels are the same (positive pairs), otherwise zip them with different labels (negative pairs)
+    pos_pair1 = tf.data.Dataset.zip((datset1, datset1))
+    pos_pair2 = tf.data.Dataset.zip((dataset2, dataset2))
+    neg_pair1 = tf.data.Dataset.zip((datset1, dataset2))
+    neg_pair2 = tf.data.Dataset.zip((dataset2, datset1))
 
-        self.tf_ds = self.resize(self.tf_ds)
-        self.tf_ds = self.normalize(self.tf_ds)
-        self.tf_ds = self.shuffle(self.tf_ds)
-        self.tf_ds = self.batch(self.tf_ds, 8)
+    # label the data and labels
+    pos_pair1 = pos_pair1.map(lambda x, y: (x, y, 0.0))
+    pos_pair2 = pos_pair2.map(lambda x, y: (x, y, 0.0))
+    neg_pair1 = neg_pair1.map(lambda x, y: (x, y, 1.0))
+    neg_pair2 = neg_pair2.map(lambda x, y: (x, y, 1.0))
 
-    def resize(self, data):
-        return data.map(lambda x, y: (tf.image.resize(x, (224, 224)), y))
+    return pos_pair1, pos_pair2, neg_pair1, neg_pair2
 
-    def normalize(self, data):
-        return data.map(lambda x, y: (x / 255.0, y))
 
-    def shuffle(self, data):
-        return data.shuffle(len(self.data))
-    
-    def batch(self, data, batch_size):
-        return data.batch(batch_size)
+def shuffle(pos_pair1, pos_pair2, neg_pair1, neg_pair2):
+    choice_dataset = tf.data.experimental.sample_from_datasets([pos_pair1, pos_pair2, neg_pair1, neg_pair2])
 
-    def get_data(self):
-        return self.tf_ds
+    return choice_dataset
 
-    def get_triplet_data(self):
-        anchor = tf.data.Dataset.from_tensor_slices(self.AD.get_data())
-        positive = tf.data.Dataset.from_tensor_slices(self.AD.get_data())
-        negative = tf.data.Dataset.from_tensor_slices(self.NC.get_data())
 
-        anchor = anchor.map(lambda x: tf.image.resize(x, (224, 224)))
-        positive = positive.map(lambda x: tf.image.resize(x, (224, 224)))
-        negative = negative.map(lambda x: tf.image.resize(x, (224, 224)))
+def split_dataset(dataset, batch_size, train_size):
+    # batch the data
+    dataset = dataset.batch(batch_size)
 
-        anchor = anchor.map(lambda x: x / 255.0)
-        positive = positive.map(lambda x: x / 255.0)
-        negative = negative.map(lambda x: x / 255.0)
+    # split the data into train and test
+    train_dataset = dataset.take(train_size)
+    validation_dataset = dataset.skip(train_size)
 
-        anchor = anchor.shuffle(len(self.AD.get_data()))
-        positive = positive.shuffle(len(self.AD.get_data()))
-        negative = negative.shuffle(len(self.NC.get_data()))
+    return train_dataset, validation_dataset
 
-        self.shuffle(anchor)
-        self.shuffle(positive)
-        self.shuffle(negative)
 
-        dataset = tf.data.Dataset.zip((anchor, positive, negative))
+def visualize(img1, img2, labels, to_show=6, num_col=3, predictions=None, test=False):
 
-        # split the data into train and validation 
-        train_dataset = dataset.take(round(self.AD.get_data().shape[0] * 0.8))
-        val_dataset = dataset.skip(round(self.AD.get_data().shape[0] * 0.8))
 
-        # batch the data
-        train_dataset = train_dataset.batch(8)
-        train_dataset = train_dataset.prefetch(8)
+    num_row = to_show // num_col if to_show // num_col != 0 else 1
 
-        val_dataset = val_dataset.batch(8)
-        val_dataset = val_dataset.prefetch(8)
+    to_show = num_row * num_col
 
-        return train_dataset, val_dataset
+    # Plot the images
+    fig, axes = plt.subplots(num_row, num_col, figsize=(10, 10))
+    for i in range(to_show):
 
-    def split_data(self, data):
-        train_size = int(0.8 * len(data))
+        # If the number of rows is 1, the axes array is one-dimensional
+        if num_row == 1:
+            ax = axes[i % num_col]
+        else:
+            ax = axes[i // num_col, i % num_col]
 
-        train_data = data.take(train_size)
-        val_data = data.skip(train_size)
+        ax.imshow((tf.concat([img1[i], img2[i]], axis=1).numpy()*255.0).astype("uint8"))
+        ax.set_axis_off()
+        if test:
+            ax.set_title("True: {} | Pred: {:.5f}".format(labels[i], predictions[i][0]))
+        else:
+            ax.set_title("Label: {}".format(labels[i]))
+    if test:
+        plt.tight_layout(rect=(0, 0, 1.9, 1.9), w_pad=0.0)
+    else:
+        plt.tight_layout(rect=(0, 0, 1.5, 1.5))
+    plt.show()
 
-        self.batch(train_data, 8)
-        self.batch(val_data, 8)
 
-        return train_data, val_data
+def main():
+    AD_dataset = load_data('./AD_NC/train/AD', (224, 224))
+    NC_dataset = load_data('./AD_NC/train/NC', (224, 224))
 
+    pos_pair1, pos_pair2, neg_pair1, neg_pair2 = make_pair(AD_dataset, NC_dataset)
+
+    choice_dataset = shuffle(pos_pair1, pos_pair2, neg_pair1, neg_pair2)
+
+    train_dataset, validation_dataset = split_dataset(choice_dataset, 16, 100)
+
+    for img1, img2, label in train_dataset.take(1):
+        visualize(img1, img2, label)
+
+        
 if __name__ == '__main__':
-    AD = data_loader('AD_NC/train/AD', 'AD')
-    NC = data_loader('AD_NC/train/NC', 'NC')
-    data = data_preprocess(AD, NC)
-    train_data, val_data = data.split_data(data.get_data())
-
-    print(train_data)
-    print(val_data)
-
-    training_data, validation_data = data.get_triplet_data()
-    print(training_data)
-    print(validation_data)
+    main()
