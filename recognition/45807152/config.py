baddb5e2b3b816a6ace807874459e0c20f5678f7
George Laugher <george_laugher@outlook.com>
2022-10-19 23:40:09 +1000

    Added image, model and training parameters

---
+++ recognition/45807152/config.py
@@ -0,0 +1,11 @@
+# Image size configuration
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 256
+
+# Establish YOLOv1 Output Shape
+S = 7
+B = 1   # Single Box
+C = 1   # Melanoma or Benign
+
+# Training Parameters
+BATCH_SIZE = 16
