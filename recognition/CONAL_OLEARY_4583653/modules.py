caa97b3a90bf37115af2ba0aae74da5331b6ae82
Conal O'Leary <52145084+scooobs@users.noreply.github.com>
2022-10-19 19:23:53 +1000

    PixelCNN Implementation

--- recognition/CONAL_OLEARY_4583653/modules.py
+++ recognition/CONAL_OLEARY_4583653/modules.py
@@ -221,3 +221,93 @@ class VQVAE(keras.models.Model):
             plt.title("Code")
             plt.axis("off")
             plt.show()
+
+
+class PixelConvLayer(layers.Layer):
+    def __init__(self, mask_type, **kwargs):
+        super(PixelConvLayer, self).__init__()
+        self.mask_type = mask_type
+        self.conv = layers.Conv2D(**kwargs)
+
+    def build(self, input_shape):
+        # Build the conv2d layer to initialize kernel variables
+        self.conv.build(input_shape)
+        # Use the initialized kernel to create the mask
+        kernel_shape = self.conv.kernel.get_shape()
+        self.mask = np.zeros(shape=kernel_shape)
+        self.mask[: kernel_shape[0] // 2, ...] = 1.0
+        self.mask[kernel_shape[0] // 2, : kernel_shape[1] // 2, ...] = 1.0
+        if self.mask_type == "B":
+            self.mask[kernel_shape[0] // 2, kernel_shape[1] // 2, ...] = 1.0
+
+    def call(self, inputs):
+        self.conv.kernel.assign(self.conv.kernel * self.mask)
+        return self.conv(inputs)
+
+
+class ResidualBlock(keras.layers.Layer):
+    def __init__(self, filters, **kwargs):
+
+        super(ResidualBlock, self).__init__(**kwargs)
+        self.conv1 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu")
+        self.norm1 = keras.layers.BatchNormalization()
+        self.pixel_conv = PixelConvLayer(
+            mask_type="B",
+            filters=filters // 2,
+            kernel_size=3,
+            activation="relu",
+            padding="same",
+        )
+        self.norm2 = keras.layers.BatchNormalization()
+        self.conv2 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu")
+        self.norm3 = keras.layers.BatchNormalization()
+
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.norm1(x)
+        x = self.pixel_conv(x)
+        x = self.norm2(x)
+        x = self.conv2(x)
+        x = self.norm3(x)
+        return keras.layers.add([inputs, x])
+
+
+class PixelCNN(keras.models.Model):
+    def __init__(self, num_blocks, num_layers, num_embeddings, **kwargs):
+        super().__init__(**kwargs)
+        self.num_blocks = num_blocks
+        self.num_layers = num_layers
+        self.first_layer = PixelConvLayer(
+            mask_type="A", filters=128, kernel_size=7, activation="relu", strides=1, padding="same")
+        self.norm1 = keras.layers.BatchNormalization()
+
+        self.block_layers = []
+
+        for _ in range(num_blocks):
+            self.block_layers.append(ResidualBlock(filters=128))
+
+        self.pixel_layers = []
+        self.norms = []
+        for _ in range(num_layers):
+            self.pixel_layers.append(PixelConvLayer(
+                mask_type="B", filters=128, kernel_size=1, strides=1, activation="relu", padding="valid"))
+            self.norms.append(keras.layers.BatchNormalization())
+
+        self.final_layer = keras.layers.Conv2D(
+            filters=num_embeddings, kernel_size=1, strides=1, padding="valid")
+
+    def call(self, input):
+        input = self.first_layer(input)
+        input = self.norm1(input)
+
+        for i in range(self.num_blocks):
+            input = self.block_layers[i](input)
+
+        for i in range(self.num_layers):
+            input = self.pixel_layers[i](input)
+            input = self.norms[i](input)
+
+        output = self.final_layer(input)
+        return output
