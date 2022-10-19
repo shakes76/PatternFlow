b0069efda7847b5c6a68259cedb949e1041583ae
unknown <wangjoshua8@gmail.com>
2022-10-19 20:41:23 +1000

    Add function for loading and predicting on trained model.

--- recognition/46965611-ADNI-Brain-Visual-Transformer/predict.py
+++ recognition/46965611-ADNI-Brain-Visual-Transformer/predict.py
@@ -6,3 +6,59 @@ Script for showing example usage of trained model.
 Author: Joshua Wang (Student No. 46965611)
 Date Created: 11 Oct 2022
 """
+import tensorflow as tf
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+from dataset import DataLoader
+from modules import EmbedPatch, MultiHeadAttentionLSA, PatchLayer
+from parameters import DATA_LOAD_PATH, LEARNING_RATE, MODEL_SAVE_PATH, IMAGE_SIZE, BATCH_SIZE, WEIGHT_DECAY
+
+
+def predict(load_path, test_data):
+    optimizer = tfa.optimizers.AdamW(
+        learning_rate=LEARNING_RATE,
+        weight_decay=WEIGHT_DECAY
+    )
+
+    model = tf.keras.models.load_model(
+        load_path,
+        custom_objects={
+            'PatchLayer': PatchLayer,
+            'EmbedPatch': EmbedPatch,
+            'MultiHeadAttentionLSA': MultiHeadAttentionLSA,
+            'AdamW': optimizer
+        }
+    )
+    
+    model.evaluate(test_data)
+
+    # Plot confusion matrix
+    y_true = []
+    y_pred = []
+
+    for image_batch, label_batch in test_data:
+        y_true.append(label_batch)
+        y_pred.append((model.predict(image_batch, verbose=0) > 0.5).astype('int32'))
+
+    labels_true = tf.concat([tf.cast(item[0], tf.int32) for item in y_true], axis=0)
+    labels_pred = tf.concat([item[0] for item in y_pred], axis=0)
+
+    matrix = tf.math.confusion_matrix(labels_true, labels_pred, 2).numpy()
+
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.matshow(matrix, cmap=plt.cm.Blues, alpha=0.3)
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            ax.text(x=j, y=i, s=matrix[i, j], va='center', ha='center', size='xx-large')
+    
+    plt.xlabel('Predictions', fontsize=18)
+    plt.ylabel('Actual Label', fontsize=18)
+    plt.suptitle('Confusion Matrix', fontsize=18)
+    plt.savefig('confusion_matrix')
+    plt.clf()
+
+if __name__ == '__main__':
+    loader = DataLoader(DATA_LOAD_PATH, IMAGE_SIZE, BATCH_SIZE)
+    train, val, test = loader.load_data()
+
+    predict(MODEL_SAVE_PATH, test)
