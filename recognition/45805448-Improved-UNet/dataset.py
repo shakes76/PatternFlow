7b90042846d4f0e514b401f49fab680747ad0878
Jantoom <75770311+Jantoom@users.noreply.github.com>
2022-10-19 22:06:46 +1000

    Slight edits to dataset.py. Made sure preprocessed data is recoverable and outputting as expected when plotting so issues don't arise when plotting learned segmentations.

--- recognition/45805448-Improved-UNet/dataset.py
+++ recognition/45805448-Improved-UNet/dataset.py
@@ -8,7 +8,7 @@ def merge_image_mask_datasets(image_dataset, mask_dataset):
     Returns:
         merged dataset
     """
-    return tf.data.Dataset.zip(image_dataset, mask_dataset)
+    return tf.data.Dataset.zip((image_dataset, mask_dataset))
 
 def save_dataset(dataset: tf.data.Dataset, filepath):
     """
@@ -25,7 +25,7 @@ def load_dataset(filepath):
     """
     return tf.data.Dataset.load(filepath).cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-def load_image_dataset_from_directories(directories, batch_size=8, image_size=(64, 64), seed=69):
+def load_image_dataset_from_directories(directories, image_size=(64, 64)):
     """
     Loads images from several directories and concatenates them into one cached dataset. 
 
@@ -34,7 +34,7 @@ def load_image_dataset_from_directories(directories, batch_size=8, image_size=(6
     """
     datasets = []
     for directory in directories:
-        datasets.append(load_image_dataset_from_directory(directory, batch_size, image_size, seed))
+        datasets.append(load_image_dataset_from_directory(directory, image_size))
 
     image_dataset = datasets[0]
     for dataset in datasets[1:]:
@@ -42,42 +42,44 @@ def load_image_dataset_from_directories(directories, batch_size=8, image_size=(6
 
     return image_dataset.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-def load_image_dataset_from_directory(directory, batch_size=8, image_size=(64, 64), seed=69):
+def load_image_dataset_from_directory(directory, image_size=(64, 64)):
     """
     Loads a set of images from a directory.
 
     Returns:
         dataset of images
     """
-    return tf.keras.utils.image_dataset_from_directory(directory, labels=None, color_mode='grayscale', 
-                                    batch_size=batch_size, image_size=image_size, shuffle=True, seed=seed)
+    return tf.keras.utils.image_dataset_from_directory(directory, labels=None, color_mode='rgb', 
+                                    batch_size=3, image_size=image_size, shuffle=True)
 
-def preprocess_dataset(dataset):
+def preprocess_dataset(dataset, seed=420):
     """
     Preprocesses an entire dataset.
 
     Returns:
         the preprocessed dataset
     """
+    tf.random.set_seed(seed)
     return dataset.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE, deterministic=False)
 
-def preprocess(images, masks):
+def preprocess(images):
     """
-    Preprocess the images and their respective masks by applying normalization, random horizontal & 
+    Preprocess the images by applying normalization, random horizontal & 
     vertical flips, and random cropping.
 
     Returns:
         similarly augmented images and masks
     """
-    seed = tf.random.uniform(shape=[], minval=0, maxval=69, dtype=tf.int32)
+    # Normalization
+    normalize = Rescaling(1./255)(images)
+
+    # Random flipping
+    horizontal_flip = RandomFlip(mode='horizontal')(normalize)
+    vertical_flip = RandomFlip(mode='vertical')(horizontal_flip)
+
+    # Random cropping
     _, height, width, _ = images.shape
+    pad = ZeroPadding2D(padding=4)(vertical_flip)
+    crop = RandomCrop(height=height, width=width)(pad)
 
-    augmentations = tf.keras.Sequential([
-        Rescaling(1./255),
-        RandomFlip(mode='horizontal', seed=seed),
-        RandomFlip(mode='vertical', seed=seed),
-        ZeroPadding2D(padding=(4, 4)),
-        RandomCrop(height=height, width=width, seed=seed),
-    ])
-
-    return augmentations(images), augmentations(masks)
+    return crop
