3edc7fea8d10689b441c429c603bfdfb2def1876
Yoxiourn <matthewshearer2@gmail.com>
2022-10-20 19:10:32 +1100

    Function (borrowed from a keras tutorial, in code referencing is given) has been implemented that creates training, testing and validation pairs of images to feed to the Siamese network.

--- recognition/s4642283-ADNI-SIAMESE/dataset.py
+++ recognition/s4642283-ADNI-SIAMESE/dataset.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import os
 from PIL import Image
+import random
+from sklearn.model_selection import train_test_split
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -44,4 +46,66 @@ for fname in os.listdir(os.path.join(__location__, "ADNI_AD_NC_2D/AD_NC/test/NC"
 # Convert to nparray
 x_test = np.array(x_test)
 y_test = np.array(y_test)
-print(max(x_test[0][120]))
+
+
+
+
+# Make validation set
+x_train, x_val, y_train, y_val = train_test_split(X_training, X_train_labels, test_size=0.2, random_state=8, shuffle=True)
+
+# This function borrows logic from https://keras.io/examples/vision/siamese_contrastive/
+def make_pairs(x, y):
+    """
+    Creates a tuple containing pairs of images, where images belonging to
+    the same class are labelled 0 while images that belong to different classes
+    are labelled 1.
+    """
+
+    num_classes = max(y) + 1
+    digit_indices = [np.where(y == i)[0] for i in range(num_classes)]
+
+    pairs = []
+    labels = []
+
+    for idx1 in range(len(x)):
+        # add a matching example
+        x1 = x[idx1]
+        label1 = y[idx1]
+        idx2 = random.choice(digit_indices[label1])
+        x2 = x[idx2]
+
+        pairs += [[x1, x2]]
+        labels += [0]
+
+        # add a non-matching example
+        label2 = random.randint(0, num_classes - 1)
+        while label2 == label1:
+            label2 = random.randint(0, num_classes - 1)
+
+        idx2 = random.choice(digit_indices[label2])
+        x2 = x[idx2]
+
+        pairs += [[x1, x2]]
+        labels += [1]
+
+    return np.array(pairs), np.array(labels).astype("float32")
+
+
+# make train pairs
+pairs_train, labels_train = make_pairs(x_train, y_train)
+
+# make validation pairs
+pairs_val, labels_val = make_pairs(x_val, y_val)
+
+# make test pairs
+pairs_test, labels_test = make_pairs(x_test, y_test)
+
+# Create pairs to feed Left and Right Siamese
+x_train_1 = pairs_train[:, 0]
+x_train_2 = pairs_train[:, 1]
+
+x_val_1 = pairs_val[:, 0]
+x_val_2 = pairs_val[:, 1]
+
+x_test_1 = pairs_test[:, 0]
+x_test_2 = pairs_test[:, 1]
