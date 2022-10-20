745182574c7565be0828564c0b8813b1d2d6aae2
binotation <68807919+binotation@users.noreply.github.com>
2022-10-20 19:33:27 +1000

    Change dataset split

--- recognition/45840056-jingwei-perceiver-adni/predict.py
+++ recognition/45840056-jingwei-perceiver-adni/predict.py
@@ -1,16 +1,22 @@
+import numpy as np
 import torch
-from numpy import random
-from dataset import ADNIDataset
+from torch.utils.data import random_split, DataLoader
+from dataset import ADNI
+from train import print_accuracy
 
-def predict():
-    device = torch.device("cpu")
-    ds = ADNIDataset("/home/jingweini/Documents/uni/COMP3710/report/data/AD_NC", device)
-    img_seq, label = ds[random.randint(0, ADNIDataset.NUM_SCAN_GROUPS)]
-    # print(img_seq.shape, label.shape)
-    print(label.item())
-    model = torch.load("model_0.1.pkl").to(device)
-    out = model(img_seq.unsqueeze(0))
-    print(out.item())
+def predict(model, ds, batch_size):
+    loader = DataLoader(ds, shuffle=True, batch_size=batch_size)
+    batch, labels = next(iter(loader))
+    print(f"true labels:\n{labels}")
+    pred = model(batch)
+    print(f"predictions:\n{torch.round(pred)}")
 
 if __name__ == "__main__":
-    predict()
+    batch_size = 8
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.load("model.pkl").to(device)
+    ds = ADNI(device, "/home/jingweini/Documents/uni/COMP3710/report/data/AD_NC")
+    other_size  = int(np.round(ADNI.NUM_SEQUENCES * 0.8))
+    ts_size  = int(np.round(ADNI.NUM_SEQUENCES * 0.2))
+    ds, ts_ds = random_split(ds, (other_size, ts_size), torch.Generator().manual_seed(42))
+    predict(model, ts_ds, batch_size)
