65a76a46ca27b2907b65c81c8d8aa74e1818c246
4lon <alon.nusem@gmail.com>
2022-10-21 04:57:32 +1000

    Redesigned VQVAE modules for tensorflow. This is a simpler design overall however time is limited. If I have time I intend to work back to this and improve the model

---
+++ recognition/44801582_OASIS_VAE/modules.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class VectorQuantizer(keras.layers.Layer):
+    def __init__(self, num_embeddings, embedding_dim, beta=0.25, **kwargs):
+        super().__init__(**kwargs)
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+
+        self.beta = beta
+
+        w_init = tf.random_uniform_initializer()
+        self.embeddings = tf.Variable(
+            initial_value=w_init(shape=(self.embedding_dim, self.num_embeddings), dtype="float32"),
+            trainable=True,
+            name="embeddings_vqvae")
+
+    def call(self, x):
+        encoding_indices = self.get_code_indices(tf.reshape(x, [-1, self.embedding_dim]))
+        encodings = tf.one_hot(encoding_indices, self.num_embeddings)
+        quantized = tf.reshape(tf.matmul(encodings, self.embeddings, transpose_b=True), tf.shape(x))
+
+        self.add_loss(self.beta * tf.reduce_mean((tf.stop_gradient(quantized) - x) ** 2)
+                      + tf.reduce_mean((quantized - tf.stop_gradient(x)) ** 2))
+
+        quantized = x + tf.stop_gradient(quantized - x)
+
+        return quantized
+
+    def get_code_indices(self, flattened_inputs):
+        similarity = tf.matmul(flattened_inputs, self.embeddings)
+        encoding_indices = tf.argmin((tf.reduce_sum(flattened_inputs ** 2, axis=1, keepdims=True)
+                     + tf.reduce_sum(self.embeddings ** 2, axis=0) - 2 * similarity), axis=1)
+
+        return encoding_indices
+
+
+def def_encoder(latent_dim):
+    encoder_inputs = keras.Input(shape=(256, 256, 1))
+    x = keras.layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(
+        encoder_inputs
+    )
+    x = keras.layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    encoder_outputs = keras.layers.Conv2D(latent_dim, 1, padding="same")(x)
+    return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
+
+
+def def_decoder(latent_dim):
+    latent_inputs = keras.Input(shape=def_encoder(latent_dim).output.shape[1:])
+    x = keras.layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
+        latent_inputs
+    )
+    x = keras.layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+    decoder_outputs = keras.layers.Conv2DTranspose(1, 3, padding="same")(x)
+    return keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+
+def get_vqvae(latent_dim=16, num_embeddings=64):
+    vq_layer = VectorQuantizer(num_embeddings, latent_dim, name="vector_quantizer")
+    encoder = def_encoder(latent_dim)
+    decoder = def_decoder(latent_dim)
+    inputs = keras.Input(shape=(256, 256, 1))
+    encoded = encoder(inputs)
+    quantized = vq_layer(encoded)
+    reconstructions = decoder(quantized)
+    return keras.Model(inputs, reconstructions, name="vq_vae")
