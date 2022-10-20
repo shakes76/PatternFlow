1113063936440e02bb71a2eb93bc54e7e9ee09f4
mishiznic <63307108+mishiznic@users.noreply.github.com>
2022-10-20 23:17:44 +1000

    Dice coeff function added. Still need to train the model...

---
+++ recognition/45838464-improvedUNET-ISIC/utils.py
@@ -0,0 +1,16 @@
+from tensorflow.keras.backend import flatten, sum
+def dice_coefficient(a, b):
+    """
+    Dice Coefficient function from : https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient
+    Used to determine how closely two set overlap each other. In this case we use it to see how close the
+    predicted mask matches the ground truth mask.
+    """
+   
+    
+    a = flatten(a)
+    b = flatten(b)
+    a_union_b = sum(a * b)
+    mag_a = sum(a)
+    mag_b = sum(b)
+    
+    return (2.0 * a_union_b) / (mag_a + mag_b)
