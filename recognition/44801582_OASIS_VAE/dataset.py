eb289e9b3884e67212e4c1e6494096a65602fb77
4lon <alon.nusem@gmail.com>
2022-10-20 15:59:55 +1000

    Updated to make dataloaders in dataset.py

--- recognition/44801582_OASIS_VAE/dataset.py
+++ recognition/44801582_OASIS_VAE/dataset.py
@@ -2,22 +2,12 @@
 Data loader for loading and preprocessing your data
 """
 import os
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 
 
 class OASISDataset(Dataset):
     def __init__(self, data_dir):
-        # root = "keras_png_slices_data/keras_png_slices_"
-        # if dataset == "train":
-        #     self.data = os.listdir(root + "train")
-        # elif dataset == "test":
-        #     self.data = os.listdir(root + "test")
-        # elif dataset == "validate":
-        #     self.data = os.listdir(root + "validate")
-        # else:
-        #     print("Invalid type used, defaulting to train")
-        #     self.data = os.listdir(root + "train")
         self.data = os.listdir(data_dir)
 
         self.transform = torchvision.transforms.Compose([
@@ -31,3 +21,18 @@ class OASISDataset(Dataset):
         image = torchvision.io.read_image(self.data[index])
         image = self.transform(image)
         return image
+
+
+def get_loaders():
+    batch_size = 4
+    num_workers = 1
+
+    train_loader = DataLoader(OASISDataset("keras_png_slices_data/keras_png_slices_train"),
+                              batch_size=4, num_workers=num_workers, pin_memory=True)
+    test_loader = DataLoader(OASISDataset("keras_png_slices_data/keras_png_slices_test"),
+                             batch_size=batch_size, shuffle=True)
+    validation_loader = DataLoader(OASISDataset("keras_png_slices_data/keras_png_slices_validate"),
+                              batch_size=batch_size, drop_last=True,
+                              num_workers=num_workers, pin_memory=True)
+
+    return train_loader, test_loader, validation_loader
