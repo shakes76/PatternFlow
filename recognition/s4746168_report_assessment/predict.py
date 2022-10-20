cc2333fa0e58cfa8763f8d0821bf05bda5a1b6eb
Sanchit Jain <sanchitjain1632@gmail.com>
2022-10-21 01:26:09 +1000

    Updated predict.py with comments and a bit of code

--- recognition/s4746168_report_assessment/predict.py
+++ recognition/s4746168_report_assessment/predict.py
@@ -1,6 +1,19 @@
 import keras
 from dataset import *
+import matplotlib.pyplot as pyplot
 
+# Loads the saved model
 Unet_model = keras.models.load_model("UNet_model")
 
+# Evaluates the model trained before
 Unet_model.evaluate(X_test, Y_test)
+
+"""
+    # This assigns the predicted value of images
+    # Then the array is rounded into decimal values
+    # The maximum axis value is assigned
+"""
+out = Unet_model.predict(X_test)
+out_r = np.round(out)
+out_argmax = np.argmax(out, -1)
+gt_test_Y = np.argmax(Y_test, -1)
