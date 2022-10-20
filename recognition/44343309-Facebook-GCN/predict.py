e7cafeda4c8f2f4690c86fba2673d42de177804d
Rayve <rgreenhillbrown@gmail.com>
2022-10-20 12:04:02 +1000

    added ability to plot tnse in predict.py

--- recognition/44343309-Facebook-GCN/predict.py
+++ recognition/44343309-Facebook-GCN/predict.py
@@ -19,3 +19,20 @@ model.load_weights("training/cp.ckpt")
 predictions = model.predict([features, adjacency], batch_size=numNodes)
 
 report = classification_report(testMask, np.argmax(predictions, axis=1))
+
+# build a tnse plot
+gcnOutputs = [layer.output for layer in model.layers]
+activationModel = tf.keras.Model(inputs=model.input, outputs=gcnOutputs)
+activations = activationModel.predict([features, adjacency], batch_size=numNodes)
+tnseGraph = TSNE(n_components=2).fit_transform(activations[3])
+
+# plot the tnse plot
+colours = np.argmax(target, axis=1)
+plt.figure(figsize=(15, 15))
+for klass in range(classes):
+    indices = np.where(colours == klass)
+    indices = indices[0]
+    plt.scatter(tnseGraph[indices,0], tnseGraph[indices, 1], label=klass)
+plt.legend()
+plt.grid()
+plt.show()
