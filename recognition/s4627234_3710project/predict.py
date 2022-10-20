f7c2f31187daf9cdf0f34ae85ecccb90b3e78dd0
DaveD123321 <davedai@DavedeMacBook-Pro.local>
2022-10-20 15:56:58 +1000

    finish the project

--- recognition/s4627234_3710project/predict.py
+++ recognition/s4627234_3710project/predict.py
@@ -1,89 +1,61 @@
 
 import os 
-import SimpleITK as sitk
-import matplotlib
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import train
 import dataset
 import modules
 
-def image_show(file_path, images_paths, image_num):
-    ''' Show the image_num'th image from file_path with printing out its shape'''
-    image_array=sitk.ReadImage(os.path.join(file_path,images_paths[image_num]))
-    print(sitk.GetArrayFromImage(image_array).shape)
-    plt.imshow(sitk.GetArrayFromImage(image_array))
-    plt.show() 
 
-def image_save():
-    pass
+def plot_data(history, type):
+    plt.figure(figsiz = (10, 5))
 
-val_x, val_y = dataset.load_val()
-model = train.training(data_reshape = False)
+    if type == 'acc': add = 'DSC'
+    else: add = 'loss'
 
-pred = model.predict(val_x)
-gt = tf.convert_to_tensor(val_y,dtype=tf.float32)
-print (modules.DSC(gt,pred))
-
-r = [1, 23, 45, 67, 78, 12, 5, 28]
-for n in r:
-    fig = plt.figure()
-
-    fig.subplots_adjust(hspace = 0.4, wspace = 0.4)
-    ax = fig.add_subplot(1, 3, 1)
-    ax.imshow(val_x[r])
-    ax.title.set_text("Testing Image")
-    ax.axis('off')
-
-    result=pred[r]>0.5
-    ax = fig.add_subplot(1, 3, 2)
-    ax.imshow(result*255, cmap="gray")
-    ax.title.set_text("Predict Image")
-    ax.axis('off')
-
-    ax = fig.add_subplot(1, 3, 3)
-    ax.imshow(val_y[r])
-    ax.title.set_text("Ground Truth")
-    ax.axis('off')
-
-    plt.savefig('./images/output'+ str(n) +'.png')
+    plt.plot(history.history['' + add], label='Training ' + add)
+    plt.plot(history.history['val_' + add], label='Validation ' + add)
+    plt.title('Test vs Validation ' + add)
+    plt.legend(loc='lower right')
+    plt.xlabel('Epochs')
+    plt.ylabel('' + add)
+    plt.savefig('./images/'+ add +'.png')
+    plt.show()
 
 
+def main():
+    val_x, val_y = dataset.load_val()
+    model, history = train.training(data_reshape = False)
+    plot_data(history, 'acc')
+    plot_data(history, 'loss')
 
-# # analyse history of training the model
-# def analyse_training_history(history):
-#     """Plots the acuraccy and validation accuracy of the model as it trains."""
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(history.history['accuracy'], label='accuracy')
-#     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-#     plt.xlabel('Epoch')
-#     plt.ylabel('Accuracy')
-#     plt.ylim([0, 1])
-#     plt.legend(loc='lower right')
-#     plt.show()
-# analyse_training_history(history)
+    pred = model.predict(val_x)
+    gt = tf.convert_to_tensor(val_y, dtype=tf.float32)
+    print(modules.DSC(gt, pred))
 
-# # plot some predictions
-# def display_predictions(model, ds, n=1):
-#     """Makes n predictions using the model and the given dataset and displays
-#     these predictions."""
-#     for image, mask in ds.take(n):
-#         pred_mask = model.predict(image[tf.newaxis, ...])
-#         pred_mask = tf.math.round(pred_mask)
-#         display([tf.squeeze(image), tf.squeeze(mask), tf.squeeze(pred_mask)])
+    number_list = range(50) # image number (any)
+    for each in number_list:
+        fig = plt.figure()
 
-# display_predictions(model, test_ds, n=3)
+        fig.subplots_adjust(hspace = 0.4, wspace = 0.4)
+        ax = fig.add_subplot(1, 3, 1)
+        ax.imshow(val_x[each])
+        ax.title.set_text("Testing Image")
+        ax.axis('off')
 
-# # compute dice similarity coefficients predictions
-# def compute_dice_coefficients(model, ds):
-#     """Computes the average dice similarity coefficient for all predictions
-#     made using the provided dataset."""
-#     DCEs = []
-#     for image, mask in ds:
-#         pred_mask = model.predict(image[tf.newaxis, ...])
-#         pred_mask = tf.math.round(pred_mask)
-#         DCE = dice_coefficient(mask, pred_mask)
-#         DCEs.append(DCE)
-#     print("Average Dice Coefficient = ", sum(DCEs)/len(DCEs))
+        result=pred[each]>0.5
+        ax = fig.add_subplot(1, 3, 2)
+        ax.imshow(result*255, cmap="gray")
+        ax.title.set_text("Predict Image")
+        ax.axis('off')
 
-# compute_dice_coefficients(model, test_ds)
+        ax = fig.add_subplot(1, 3, 3)
+        ax.imshow(val_y[each])
+        ax.title.set_text("Ground Truth")
+        ax.axis('off')
+
+        plt.savefig('./images/output'+ str(each) +'.png')
+
+
+if __name__ == "__main__":
+    main()
--- recognition/s4627234_3710project/train.py
+++ recognition/s4627234_3710project/train.py
@@ -8,12 +8,10 @@ def training(data_reshape = False):
     train_x, train_y, test_x, test_y = dataset.load_dataset(data_reshape)
     # print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
 
-    #normalize!!!
-
     model = modules.UNet_imp()
 
     # fit the model with normal learning rate
-    model.compile(optimizer=Adam(0.0005), loss = modules.DSC_loss, metrics=['accuracy',modules.DSC])
+    model.compile(optimizer = Adam(0.0005), loss = modules.DSC_loss, metrics=['accuracy', modules.DSC])
 
     history = model.fit(train_x, train_y,  validation_data= (test_x, test_y),
                             batch_size=8,shuffle='True',epochs=10)
@@ -23,4 +21,4 @@ def training(data_reshape = False):
     # history2 = model.fit(train_x, train_y,  validation_data= (test_x, test_y),
     #                         batch_size=8,shuffle='True',epochs=10)
 
-    return model
+    return model, history
