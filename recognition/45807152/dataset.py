b6a3e6157316809242829a2f3ecaeb6c69bc2bd7
George Laugher <george_laugher@outlook.com>
2022-10-21 00:26:30 +1000

    Changed to tf fn mapping for data load and preprocess

--- recognition/45807152/dataset.py
+++ recognition/45807152/dataset.py
@@ -26,26 +26,18 @@ class ISIC_Dataset():
         self.image_width = image_width
 
     def get_data_splits(self, train_split, val_split, test_split):
-        data = []
-        masks = []
+        # Convert path lists to tensors.
+        data_tensor = tf.convert_to_tensor(self.data_files)
+        data_tensor = tf.map_fn(self.retrieve_processed_image, data_tensor,
+                                dtype=tf.float32)
 
-        for data_file in self.data_files:
-            data_image = self.retrieve_processed_image(data_file)
-            # Append data to list of data.
-            data.append(data_image)
-
-        for mask_file in self.mask_files:
-            mask_detail = self.retrieve_processed_mask(mask_file)
-            # Append mask to list of masks.
-            masks.append(mask_detail)
-
-        # Convert lists to numpy arrays.
-        data = np.array(data)
-        masks = np.array(masks)
+        masks_tensor = tf.convert_to_tensor(self.mask_files)
+        masks_tensor = tf.map_fn(self.retrieve_processed_mask,
+                                 masks_tensor, dtype=tf.float32)
         # One-hot encode mask details with 2 clases
         # Class 1 -> no lesion
         # Class 2 -> lesion
-        masks = to_categorical(masks, num_classes=2)
+        masks_tensor = to_categorical(masks_tensor, num_classes=2)
 
         # Get training, validation and test split number of items
         train_size = round(train_split * self.total_data_len)
@@ -53,13 +45,13 @@ class ISIC_Dataset():
         test_size = round(test_split * self.total_data_len)
 
         # Split image and mask arrays into specified groups.
-        self.train_x = data[:train_size]
-        self.val_x = data[train_size:(train_size + val_size)]
-        self.test_x = data[(train_size + val_size):]
+        self.train_x = data_tensor[:train_size]
+        self.val_x = data_tensor[train_size:(train_size + val_size)]
+        self.test_x = data_tensor[(train_size + val_size):]
 
-        self.train_y = data[:train_size]
-        self.val_y = data[train_size:(train_size + val_size)]
-        self.test_y = data[(train_size + val_size):]
+        self.train_y = masks_tensor[:train_size]
+        self.val_y = masks_tensor[train_size:(train_size + val_size)]
+        self.test_y = masks_tensor[(train_size + val_size):]
 
         # Return splits
         return ((self.train_x, self.train_y), (self.val_x, self.val_y),
