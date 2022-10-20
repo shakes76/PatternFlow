5cb7a4043ea888b26377b8b92c20d937fb727777
4lon <alon.nusem@gmail.com>
2022-10-20 14:14:07 +1000

    Updated to use pytorch embeddings and fleshed out vector quantizer.

--- recognition/44801582_OASIS_VAE/modules.py
+++ recognition/44801582_OASIS_VAE/modules.py
@@ -4,6 +4,7 @@ Source code of the components of your model. Each component must be implemented 
 
 import torch
 import torch.nn as nn
+from functions import vector_quantizer, vector_quantizer_straight_through
 
 
 # class VQVAE(nn.Module):
@@ -35,7 +36,7 @@ def get_pad(output, stride):
 #
 #         return z
 def get_encoder(latent_dim=16):
-    model = nn.Sequential(
+    enc_model = nn.Sequential(
         nn.LazyConv2d(32, 3, stride=2, padding=1),
         nn.ReLU(),
         nn.LazyConv2d(64, 3, stride=2, padding=1),
@@ -43,46 +44,29 @@ def get_encoder(latent_dim=16):
         nn.LazyConv2d(latent_dim, 1, padding=1)
     )
 
-    return model
+    return enc_model
 
 
 class VQ(nn.Module):
     def __init__(self, num_embeddings, embedding_dim, beta=0.25):
         super().__init__()
-        self.embedding_dim = embedding_dim
-        self.num_embeddings = num_embeddings
-        self.beta = beta
-
-        self.embeddings = torch.rand(size=(embedding_dim, num_embeddings), dtype=torch.float32)
+        self.embed = nn.Embedding(num_embeddings, embedding_dim)
 
     def forward(self, x):
-        input_shape = torch.Size(x)
-        flattened = torch.reshape(x, [-1, self.embedding_dim])
+        x = torch.permute(x, (0, 2, 3, 1)).contiguous()
+        latent = vector_quantizer(x, self.embed.weight)
 
-        encoding_indices = self.get_code_indices(flattened)
-        encodings = nn.functional.one_hot(encoding_indices, self.num_embeddings)
-        quantized = torch.matmul(encodings, torch.transpose(self.embeddings))
+        return latent
 
-        quantized = torch.reshape(quantized, input_shape)
+    def straight_through(self, x):
+        x = x.permute(0, 2, 3, 1).contiguous()
+        x_q, ind = vector_quantizer_straight_through(x, self.embed.weight.detach())
+        x_q = x_q.permute(0, 3, 1, 2).contiguous()
 
-        commitment_loss = torch.mean((quantized.detach() - x) ** 2)
-        codebook_loss = torch.mean((quantized.detach() - x.detach()) ** 2)  # quantized may need another detach here
-        self.loss += (self.beta * commitment_loss + codebook_loss)
+        flat = torch.index_select(self.embedding.weight, dim=0, index=ind).view_as(x).permute(0, 3, 1, 2).contiguous()
 
-        quantized = x + (quantized - x).detach()
-        return quantized
+        return x_q, flat
 
-    def get_code_indices(self, flattened_inputs):
-        similarity = torch.matmul(flattened_inputs, self.embeddings)
-        distances = (
-            torch.sum(flattened_inputs ** 2, dim=1, keepdim=True)
-            + torch.sum(self.embeddings ** 2, dim=0)
-            - 2 * similarity
-        )
-
-        # Derive the indices for minimum distances.
-        encoding_indices = torch.argmin(distances, dim=1)
-        return encoding_indices
 
 
 # class Decoder(nn.Module):
@@ -98,7 +82,7 @@ class VQ(nn.Module):
 #         return x
 
 def get_decoder(latent_dim=16):
-    model = nn.Sequential(
+    dec_model = nn.Sequential(
         nn.LazyConvTranspose2d(64, 3, stride=2, padding="same"),
         nn.ReLU(),
         nn.LazyConvTranspose2d(32, 3, stride=2, padding="same"),
@@ -106,17 +90,17 @@ def get_decoder(latent_dim=16):
         nn.LazyConvTranspose2d(1, 3, stride=2, padding="same")
     )
 
-    return model
+    return dec_model
 
 
 def get_vqvae(latent_dim=16, num_embeddings=64):
-    model = nn.Sequential(
+    vqvae_model = nn.Sequential(
         get_encoder(latent_dim),
         VQ(num_embeddings, latent_dim),
         get_decoder(latent_dim)
     )
 
-    return model
+    return vqvae_model
 
 
 
