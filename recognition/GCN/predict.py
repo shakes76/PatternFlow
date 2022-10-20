9223ba9557c8e0bf12b9947748ed227686202082
maryam-m-k <maryamkkhan62@gmail.com>
2022-10-21 05:48:18 +1000

    Added code to load and predict on samples

--- recognition/GCN/predict.py
+++ recognition/GCN/predict.py
@@ -1,5 +1,3 @@
-'''example usage of trained model: print any results and/or provide visualisations where applicable'''
-
 import modules as m
 import train as t
 # import networkx as nx
@@ -9,7 +7,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import scipy
-import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.module import Module
 from torch.nn.parameter import Parameter
+from sklearn.manifold import TSNE
+import torch
+from modules import Net, GNNLayer
+from train import test_gcn
+from dataset import preprocess
+
+PATH = "/Users/maryamkhan/Documents/UNI/2022/SEM2/COMP3710/PatternFlow/recognition/GCN/best_model.pt"
+N_EPOCHS = 5
+NHID = 4
+NOUTPUT = 4
+
+adj_mtrx, node_features, labels, train_ids, val_ids, test_ids = preprocess()
+sample_adj = adj_mtrx[0]
+sample_features = node_features[0]
+print(sample_features)
+
+# device_model = torch.device("cuda")
+model = m.Net(node_features.shape[1], NHID, NOUTPUT)
+model.load_state_dict(torch.load(PATH))
+# model.to(device_model)
+
+# print("saved", model)
+model.eval()
+gcn = model(torch.FloatTensor(np.asmatrix(sample_features)), torch.from_numpy(np.asmatrix(sample_adj)))
+test_gcn(gcn)
