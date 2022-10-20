4f347730780596aba1ba5de6a0d96d781fc99289
Daniel Sayer <daniel.sayer01@gmail.com>
2022-10-20 22:31:01 +1000

    Removed all name == main

--- recognition/s4581059 - ISIC Segmentation/predict.py
+++ recognition/s4581059 - ISIC Segmentation/predict.py
@@ -1,2 +1,10 @@
 #Shows example of trained model
 #Prints out results and provides visualisations 
+
+import train
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def run():
+    train.train_model()
