052eecfd30b28e2de47809d8bf273fff7c69119e
JasonZhao1 <zzc010220@gmail.com>
2022-10-20 14:10:00 +1000

    Add some features and fix some bugs

--- recognition/46273651-Siamese network/train.py
+++ recognition/46273651-Siamese network/train.py
@@ -1,82 +1,106 @@
+import dataset
+import modules
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-import dataset
-import modules
+TRAIN_AD_PATH = './AD_NC/train/AD'
+TRAIN_NC_PATH = './AD_NC/train/NC'
+TEST_AD_PATH = './AD_NC/test/AD'
+TEST_NC_PATH = './AD_NC/test/NC'
+
+INPUT_SHAPE = (224, 224)
+COLOR_MODE = 'grayscale'
+BATCH_SIZE = 16
+TRAINING_SIZE = 1800
+
+TRAINING_MODE = True
+VISUALIZE = False
 
 
 def main():
-    AD_dataset = dataset.load_data('./AD_NC/train/AD', (224, 224))
-    NC_dataset = dataset.load_data('./AD_NC/train/NC', (224, 224))
-
+    # load the Train and Validation data
+    AD_dataset = dataset.load_data(TRAIN_AD_PATH, INPUT_SHAPE, COLOR_MODE)
+    NC_dataset = dataset.load_data(TRAIN_NC_PATH, INPUT_SHAPE, COLOR_MODE)
     pos_pair1, pos_pair2, neg_pair1, neg_pair2 = dataset.make_pair(AD_dataset, NC_dataset)
-
     choice_dataset = dataset.shuffle(pos_pair1, pos_pair2, neg_pair1, neg_pair2)
+    train_dataset, validation_dataset = dataset.split_dataset(choice_dataset, BATCH_SIZE, TRAINING_SIZE)
 
-    train_dataset, validation_dataset = dataset.split_dataset(choice_dataset, 16, 100)
+    # load the Test data
+    AD_test_dataset = dataset.load_data(TEST_AD_PATH, INPUT_SHAPE, COLOR_MODE)
+    NC_test_dataset = dataset.load_data(TEST_NC_PATH, INPUT_SHAPE, COLOR_MODE)
+    pos_pair1, pos_pair2, neg_pair1, neg_pair2 = dataset.make_pair_test(AD_dataset, NC_dataset, AD_test_dataset, NC_test_dataset)
+    test_dataset = dataset.shuffle(pos_pair1, pos_pair2, neg_pair1, neg_pair2)
+    test_dataset = test_dataset.batch(16)
 
-    # for img1, img2, label in train_dataset.take(1):
-    #     dataset.visualize(img1, img2, label)
-
-    # create the model
-    siamese = modules.siamese
-
-
-    if training:
-        optimizer = tf.keras.optimizers.Adam(0.00006)
-        loss_tracker = tf.keras.metrics.Mean(name="loss")
-        acc_tracker = tf.keras.metrics.Mean(name="accuracy")
-
-        # save the accuracy and loss for each epoch
-        train_loss_results = []
-        train_accuracy_results = []
+    # Visualize the training data
+    if VISUALIZE:
+        for img1, img2, labels in train_dataset.take(1):
+            dataset.visualize(img1, img2, labels)
 
 
-        for epoch in range(1, 11):
-            print("Start of epoch %d" % (epoch,))
-            # Iterate over the batches of the dataset.
-            for step, (x1, x2, y) in enumerate(train_dataset):
-                with tf.GradientTape() as tape:
-                    # Compute the output of the model
-                    logits = siamese([x1, x2], training=True)
+    # set up the callbacks to save the weights 
+    checkpoint_path = "training2/cp-{epoch:04d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path, 
+        verbose=1, 
+        save_weights_only=True,
+        save_freq='epoch')
 
-                    # expand the labels to match the shape of logits
-                    y = tf.expand_dims(y, 1)
+    # Start the training if TRAINING_MODE is True
+    if TRAINING_MODE:
+        # create the model
+        siamese = modules.SiameseModel()
 
-                    # Compute the loss value
-                    loss = tf.keras.losses.binary_crossentropy(y, logits)
-                    # Compute the accuracy 
-                    acc = tf.keras.metrics.binary_accuracy(y, logits)
+        # record the training history
+        csv_logger = tf.keras.callbacks.CSVLogger('training.log',append=False)
 
-                    # Compute the gradient of the loss with respect to the parameters of the model
-                    grads = tape.gradient(loss, siamese.trainable_weights)
-                    # Update the weights of the model
-                    optimizer.apply_gradients(zip(grads, siamese.trainable_weights))
+        # compile the model 
+        siamese.compile(optimizer=tf.keras.optimizers.Adam(0.00006))
 
-                    # update loss and accuracy metrics
-                    loss_tracker.update_state(loss)
-                    acc_tracker.update_state(acc)
+        # train the model
+        history = siamese.fit(train_dataset, epochs=10, validation_data=validation_dataset, callbacks=[cp_callback, csv_logger])
 
-                if step % 100 == 0:
-                    print("loss: ", loss_tracker.result().numpy(), "accuracy: ", acc_tracker.result().numpy())
+        if VISUALIZE:
+            # plot the training loss and accuracy
+            N = 10
+            plt.style.use("ggplot")
+            plt.figure()
+            plt.plot(np.arange(0, N), history.history["loss"], label="train_loss")
+            plt.plot(np.arange(0, N), history.history["val_loss"], label="val_loss")
+            plt.plot(np.arange(0, N), history.history["accuracy"], label="train_acc")
+            plt.plot(np.arange(0, N), history.history["val_accuracy"], label="val_acc")
+            plt.title("Training Loss and Accuracy on Siamese Network")
+            plt.xlabel("Epoch #")
+            plt.ylabel("Loss/Accuracy")
+            plt.legend(loc="lower left")
+            plt.savefig("plot.png")
 
-
-            # save the loss and accuracy for each epoch
-            train_loss_results.append(loss_tracker.result().numpy())
-            train_accuracy_results.append(acc_tracker.result().numpy())
-
-            # save the weights of the model after every epoch as checkpoints
-            checkpoint_path = "checkpoints/cp-{epoch:04d}.ckpt"
-            siamese.save_weights(checkpoint_path.format(epoch=epoch))
     else:
         # load the weights of the model
         siamese.load_weights(tf.train.latest_checkpoint("checkpoints"))
 
-    for img1, img2, label in train_dataset.take(1):
-        dataset.visualize(img1, img2, label, to_show=4, num_col=4, predictions=siamese([img1, img2], training=False).numpy(), test=True)
+    # evaluate the model on the validation data
+    loss, accuracy = siamese.evaluate(test_dataset)
+
+    # visualize the the prediction of the model on the validation data
+    if VISUALIZE:
+        for img1, img2, label in validation_dataset.take(1):
+            dataset.visualize(img1, img2, label, to_show=4, num_col=4, predictions=siamese([img1, img2], training=False).numpy(), test=True)
+
+    # evaluate the model on the test data
+    loss, accuracy = siamese.evaluate(test_dataset)
+
+    # visualize the the prediction of the model on the test data
+    if VISUALIZE:
+        for img1, img2, label in test_dataset.take(1):
+            dataset.visualize(img1, img2, label, to_show=4, num_col=4, predictions=siamese([img1, img2], training=False).numpy(), test=True)
+
 
 if __name__ == '__main__':
     main()
