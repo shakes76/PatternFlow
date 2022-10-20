49da87e4e41b3793f9cdfb346e8c1b462c8b6497
JasonZhao1 <zzc010220@gmail.com>
2022-10-20 14:09:08 +1000

    Adjust the hyperparameter of the modules

--- recognition/46273651-Siamese network/modules.py
+++ recognition/46273651-Siamese network/modules.py
@@ -7,20 +7,23 @@ import numpy as np
 embedding_model = tf.keras.Sequential(
     [
         keras.Input(shape=(224, 224, 1)),
-        layers.Conv2D(64, (10,10), activation="relu"),
+        layers.Conv2D(64, (10,10), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
         layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(128, (7,7), activation="relu"),
+        layers.Conv2D(128, (7,7), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
         layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(128, (4,4), activation="relu"),
+
+
+        layers.Conv2D(128, (4,4), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
         layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(256, (4,4), activation="relu"),
+        layers.Conv2D(256, (4,4), activation="relu", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
 
         layers.Flatten(),
-        layers.Dense(512, activation="sigmoid", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
+        layers.Dense(256, activation="sigmoid", kernel_regularizer=tf.keras.regularizers.l2(1e-3)),
     ],
     name="embedding"
 )
 
+embedding_model.summary()
 
 # Provided two tensors t1 and t2
 # Euclidean distance = sqrt(sum(square(t1-t2)))
@@ -50,9 +53,86 @@ tower_1 = embedding_model(input_1)
 tower_2 = embedding_model(input_2)
 
 merge_layer = layers.Lambda(euclidean_distance)([tower_1, tower_2])
-normal_layer = tf.keras.layers.BatchNormalization()(merge_layer)
-output_layer = layers.Dense(1, activation="sigmoid")(normal_layer)
+output_layer = layers.Dense(1, activation="sigmoid", kernel_regularizer=tf.keras.regularizers.l2(1e-3))(merge_layer)
 siamese = keras.Model(inputs=[input_1, input_2], outputs=output_layer)
+siamese.summary()
+
+
+class SiameseModel(keras.Model):
+
+    def __init__(self, siamese_network, margin=1.0):
+        super(SiameseModel, self).__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.acc_tracker = keras.metrics.Mean(name="acc")
+
+    def call(self, inputs):
+        return self.siamese_network([inputs[0], inputs[1]])
+
+    def train_step(self, data):
+        # GradientTape is a context manager that records every operation that
+        # you do inside. We are using it here to compute the loss so we can get
+        # the gradients and apply them using the optimizer specified in
+        # `compile()`.
+        with tf.GradientTape() as tape:
+            img1, img2, y_true = data
+            y_true = tf.expand_dims(y_true, 1)            
+            logits = self.siamese_network([img1, img2], training=True)
+
+            loss = keras.losses.binary_crossentropy(y_true, logits)
+            acc = keras.metrics.binary_accuracy(y_true, logits)
+
+        # Storing the gradients of the loss function with respect to the
+        # weights/parameters.
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        # Applying the gradients on the model using the specified optimizer
+        self.optimizer.apply_gradients(
+            zip(gradients, self.siamese_network.trainable_weights)
+        )
+
+        # Let's update and return the training loss metric.
+        self.loss_tracker.update_state(loss)
+
+        # Update the accuracy metric.
+        self.acc_tracker.update_state(acc)
+
+        return {"loss": self.loss_tracker.result(), "accuracy": self.acc_tracker.result()}
+
+    def test_step(self, data):
+        img1, img2, y_true = data
+        y_true = tf.expand_dims(y_true, 1)
+
+        loss = keras.losses.binary_crossentropy(y_true, self.siamese_network([img1, img2]))
+        acc = keras.metrics.binary_accuracy(y_true, self.siamese_network([img1, img2]))
+
+        # Let's update and return the loss metric.
+        self.loss_tracker.update_state(loss)
+
+        # Update the accuracy metric.
+        self.acc_tracker.update_state(acc)
+
+        return {"loss": self.loss_tracker.result(), "accuracy": self.acc_tracker.result()}
+
+    @property
+    def metrics(self):
+        # We need to list our metrics here so the `reset_states()` can be
+        # called automatically.
+        return [self.loss_tracker, self.acc_tracker]
+
+
+def classifier():
+    embedding_model.trainable = False
+
+    model =  tf.keras.Sequential([
+                tf.keras.layers.InputLayer(input_shape=(224, 224, 1)),
+                embedding_model,
+
+                tf.keras.layers.Dense(2, activation='softmax')
+            ])
+    
+    return model
 
 
 def main():
