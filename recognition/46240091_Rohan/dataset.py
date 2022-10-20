f4b8fbd046dede4455adb108ef681d0651e036dd
rohanjivrajani <88996099+rohanjivrajani@users.noreply.github.com>
2022-10-20 23:53:50 +1000

    Changed to using glob as its simpler to interpret

--- recognition/46240091_Rohan/dataset.py
+++ recognition/46240091_Rohan/dataset.py
@@ -1,19 +1,23 @@
-import os
 from tensorflow import keras
 import numpy as np
+import glob
 
 SHAPE = (64,64)
 
 def data_loader(dir_path, scale_flag):
-    all_files = os.listdir(dir_path)
-    image_list = []
-    for i in all_files:
-        image = keras.preprocessing.image.load_img(dir_path + "/" + i, grayscale = True, 
-        target_size=SHAPE)
-        if scale_flag:
-          image_list.append(
-                keras.preprocessing.image.img_to_array(image) / 255)
-        else:
-          image_list.append(
-                keras.preprocessing.image.img_to_array(image))
-    return np.array(image_list)
+  """
+  Loading and preprocessing data using keras image loader: 
+  load_image and img_to_array and converting it to grayscale.
+  """
+  all_files = glob.glob(dir_path + "/*.png")
+  image_list = []
+  for i in all_files:
+      image = keras.preprocessing.image.load_img(i, grayscale = True, 
+      target_size=SHAPE)
+      if scale_flag:
+        image_list.append(
+              keras.preprocessing.image.img_to_array(image) / 255)
+      else:
+        image_list.append(
+              keras.preprocessing.image.img_to_array(image))
+  return np.array(image_list)
