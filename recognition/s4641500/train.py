dd5965a1907cc435672d3987615179c3eba87490
thomas2001u <thomas.barton@uqconnect.edu.au>
2022-10-20 10:24:54 -0700

    Update train.py
    Implement pixel cnn. Basic functionality works.

--- recognition/s4641500/train.py
+++ recognition/s4641500/train.py
@@ -1,44 +1,77 @@
-class VQVAETrainer(keras.models.Model):
-    def __init__(self, train_variance, dim=32, embed_n=128, **kwargs):
-        super(VQVAETrainer, self).__init__(**kwargs)
-        self.train_variance = train_variance
-        self.dim = dim
-        self.embed_n = embed_n
-        self.vqvae = get_vqvae(self.dim, self.embed_n)
-        self.total_loss = keras.metrics.Mean(name="total_loss")
-        self.reconstruction_loss = keras.metrics.Mean(
-            name="reconstruction_loss"
-        )
-        self.vq_loss = keras.metrics.Mean(name="vq_loss")
+from dataset import *
+from modules import *
 
-    @property
-    def metrics(self):
-        return [
-            self.total_loss,
-            self.reconstruction_loss,
-            self.vq_loss,
-        ]
+EPOCHS = 20
 
-    def train_step(self, x):
-        with tf.GradientTape() as tape:
-            reconstructions = self.vqvae(x)
+# initialise and train
+vqvae_trainer = Train_VQVAE(data_variance, dim=16, embed_n=128)
+vqvae_trainer.compile(optimizer=keras.optimizers.Adam())
+vqvae_trainer.fit(x_train_scaled, epochs=EPOCHS, batch_size=128)
 
-            # calculate loss
-            reconstruction_loss = (
-                tf.reduce_mean((x - reconstructions) ** 2) / self.train_variance
-            )
-            total_loss = reconstruction_loss + sum(self.vqvae.losses)
+# test trained model
+encoder = vqvae_trainer.vqvae.get_layer("encoder")
+quantizer = vqvae_trainer.vqvae.get_layer("vector_quantizer")
+trained_vqvae_model = vqvae_trainer.vqvae
+idx = np.random.choice(len(x_test_scaled), 10)
+test_images = x_test_scaled[idx]
+reconstructions_test = trained_vqvae_model.predict(test_images)
+encoded_outputs = encoder.predict(test_images)
+flat_enc_outputs = encoded_outputs.reshape(-1, encoded_outputs.shape[-1])
+codebook_indices = quantizer.get_code_indices(flat_enc_outputs)
+codebook_indices = codebook_indices.numpy().reshape(encoded_outputs.shape[:-1])
 
-        # backpropagate
-        grads = tape.gradient(total_loss, self.vqvae.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.vqvae.trainable_variables))
+num_residual_blocks = 2
+num_pixelcnn_layers = 2
+pixelcnn_input_shape = encoded_outputs.shape[1:-1]
+print(f"Input shape of the PixelCNN: {pixelcnn_input_shape}")
 
-        # track loss
-        self.total_loss.update_state(total_loss)
-        self.reconstruction_loss.update_state(reconstruction_loss)
-        self.vq_loss.update_state(sum(self.vqvae.losses))
-        return {
-            "loss": self.total_loss.result(),
-            "reconstruction_loss": self.reconstruction_loss.result(),
-            "vqvae_loss": self.vq_loss.result(),
-        }
+pixelcnn_inputs = keras.Input(shape=pixelcnn_input_shape, dtype=tf.int32)
+ohe = tf.one_hot(pixelcnn_inputs, vqvae_trainer.embed_n)
+x = PixelConvLayer(
+    mask_type="A", filters=128, kernel_size=7, activation="relu", padding="same"
+)(ohe)
+
+for _ in range(num_residual_blocks):
+    x = ResBlock(filters=128)(x)
+
+for _ in range(num_pixelcnn_layers):
+    x = PixelConvLayer(
+        mask_type="B",
+        filters=128,
+        kernel_size=1,
+        strides=1,
+        activation="relu",
+        padding="valid",
+    )(x)
+
+out = keras.layers.Conv2D(
+    filters=vqvae_trainer.embed_n, kernel_size=1, strides=1, padding="valid"
+)(x)
+
+pcnn = keras.Model(pixelcnn_inputs, out, name="pixel_cnn")
+pcnn.summary()
+
+# generate the codebook indices.
+encoded_outputs = encoder.predict(x_train_scaled)
+# reduce indices because my VRAM is insufficient
+encoded_outputs = encoded_outputs[:len(encoded_outputs) // 2]
+flat_enc_outputs = encoded_outputs.reshape(-1, encoded_outputs.shape[-1])
+codebook_indices = quantizer.get_code_indices(flat_enc_outputs)
+codebook_indices = codebook_indices.numpy().reshape(encoded_outputs.shape[:-1])
+print(f"Shape of the training data for PixelCNN: {codebook_indices.shape}")
+
+# compile and train pcnn
+pcnn.compile(
+    optimizer=keras.optimizers.Adam(3e-4),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+pcnn.fit(
+    x=codebook_indices,
+    y=codebook_indices,
+    batch_size=128,
+    epochs=EPOCHS,
+    validation_split=0.2,
+)
+
+pcnn.save("saved model/model")
