b37a39796e7c750d67baa8a8def88e5afe814d0c
Yoxiourn <matthewshearer2@gmail.com>
2022-10-21 03:08:11 +1100

    Started writing to README file, implemented the train code (although training currently gives very poor results since the module components haven't been tweaked yet).

---
+++ recognition/s4642283-ADNI-SIAMESE/train.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import os
+
+from dataset import data_loader
+from modules import siamese_model
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Load Data
+x_train, labels_train, x_val, labels_val, x_test, labels_test = data_loader()
+
+# Save model checkpoints
+checkpoint_path = os.path.join(__location__, "training/cp.ckpt")
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+epochs = 10
+batch_size = 16
+
+# Create siamese model
+siamese = siamese_model((240, 256, 1))
+
+# Compile and show model
+siamese.compile(loss="binary_crossentropy", optimizer="Adam", metrics=["accuracy"])
+siamese.summary()
+
+# Train model
+history = siamese.fit(
+    [x_train[0], x_train[1]],
+    labels_train,
+    validation_data=([x_val[0], x_val[1]], labels_val),
+    batch_size=batch_size,
+    epochs=epochs,
+    callbacks=[cp_callback]
+)
+
+# Save model once training completes
+siamese.save(os.path.join(__location__, "SiameseModel"))
