d13bc3b2c4cedd195d4283dc3d006764ba575f4b
yeinydd <yeinydd1@outlook.com>
2022-10-20 14:48:29 +1000

    🧠 changed strides on vqvae conv2ds

--- recognition/s4581993-vqvae/modules.py
+++ recognition/s4581993-vqvae/modules.py
@@ -76,9 +76,9 @@ def get_encoder(latent_dim=16):
             input_shape=image_size)(
         encoder_inputs
     )
-    x = layers.Conv2D(32, 3, activation="relu", padding="same")(x)
-    x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
-    x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
     encoder_outputs = layers.Conv2D(latent_dim, 1, padding="same")(x)
     return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
 
@@ -88,9 +88,9 @@ def get_decoder(latent_dim=16):
     x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
         latent_inputs
     )
-    x = layers.Conv2DTranspose(64, 3, activation="relu", padding="same")(x)
-    x = layers.Conv2DTranspose(32, 3, activation="relu", padding="same")(x)
-    x = layers.Conv2DTranspose(32, 3, activation="relu", padding="same")(x)
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
     decoder_outputs = layers.Conv2DTranspose(1, 3, padding="same")(x)
     return keras.Model(latent_inputs, decoder_outputs, name="decoder")
 
