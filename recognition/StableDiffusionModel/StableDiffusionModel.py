d280e199206532f7f4df26aa02dd88dcfd98c41a
Wonge <daniel.jwong@outlook.com>
2022-10-20 15:59:22 +1000

    Added new keras layer for block in UNET model. Unhardcoded UNET model

--- recognition/StableDiffusionModel/StableDiffusionModel.py
+++ recognition/StableDiffusionModel/StableDiffusionModel.py
@@ -11,104 +11,73 @@ import matplotlib as plt
 import numpy as np
 
 from keras import layers
+from CustomLayers import *
 
 
+## Todo: move this to customlayers
+class UNetBlock(kr.layers.Layer) :
+    def __init__(self, outputSize, upBlock = False, upSampler = ConvUpsample, downSampler = ConvDownsample) :
+        super().__init__()
+        
+        self.conv1 = kr.layers.Conv2D(outputSize, 3, padding = "same", activation = kr.layers.ReLu)
+        self.bNor1 = keras.Layers.BatchNormalization()
+        self.conv2 = kr.layers.Conv2D(outputSize, 3, padding = "same", activation = kr.layers.ReLu)
+        self.bNorm2 = keras.Layers.BatchNormalization()
+        
+        # If upsampling occurs in block
+        if (upBlock) :
+            self.transformer = upSampler(outputSize)
+        else :
+            self.transformer = downSampler(outputSize)
+    
+    def call(self, inputs) :
+        imageInput = inputs[0]
+        timeEmbedding = inputs[1]
+        
+        
+        x = self.conv1(imageInput) 
+        x = self.bNorm1(x) + timeEmedding
+        x = self.conv2(x)
+        x = self.bNorm2(x)
+        
+        transformed = self.transformer(x)
+        return (x, transformed)
 
-#TODO: If time permits, unhardcode this later
+        
 class UNet(kr.layers.Layer):
-    def __init__(self) :
-    # Downsampling Process
-        self.convA1 = kr.layers.Conv2D(64, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convA2 = kr.layers.Conv2D(64, 3, padding = "same", activation = kr.activations.ReLu)
-        self.downSampleA1 = kr.layers.MaxPooling2D((2,2))
+    def __init__(self, blocks = [64, 128, 256, 512], bottleNeck = 1024) :
+        super().__init__()
         
-        self.convB1 = kr.layers.Conv2D(128, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convB2 = kr.layers.Conv2D(128, 3, padding = "same", activation = kr.activations.ReLu)
-        self.downSampleB1 = kr.layers.MaxPooling2D((2,2))
-        
-        self.convC1 = kr.layers.Conv2D(256, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convC2 = kr.layers.Conv2D(256, 3, padding = "same", activation = kr.activations.ReLu)
-        self.downSampleC1 = kr.layers.MaxPooling2D((2,2))
-        
-        self.convD1 = kr.layers.Conv2D(512, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convD2 = kr.layers.Conv2D(512, 3, padding = "same", activation = kr.activations.ReLu)
-        self.downSampleD1 = kr.layers.MaxPooling2D((2,2))
-        
-    # Lowest layer / start of upsampling process
-        self.convE1 = kr.layers.Conv2D(1024, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convE2 = kr.layers.Conv2D(1024, 3, padding = "same", activation = kr.activations.ReLu)
-        self.upSampleE1 = kr.layers.Conv2DTranspose(512, 3, 2, padding = "same")
+        self.layerCount = len(blocks)
+        self.finalLayer = kr.layers.Conv2D(filters = 1, kernel_size = 1, strides = 1, activation = kr.activations.relu)
+        self.bottleNeck = UNetBlock(bottleNeck, upBlock = True)
         
         
-        self.convF1 = kr.layers.Conv2D(512, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convF2 = kr.layers.Conv2D(512, 3, padding = "same", activation = kr.activations.ReLu)
-        self.upSampleF1 = kr.layers.Conv2DTranspose(256, 3, 2, padding = "same")
+        self.downBlocks = []
+        self.upBlocks = []
         
-        self.convG1 = kr.layers.Conv2D(256, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convG2 = kr.layers.Conv2D(256, 3, padding = "same", activation = kr.activations.ReLu)
-        self.upSampleG1 = kr.layers.Conv2DTranspose(128, 3, 2, padding = "same")
+        for b in blocks :
+            self.downBlocks.append(UNetBlock(b, upBlock = False))
+            self.upBlocks.insert(0, UNetBlock(b, upBlock = True))
+            
+    def call(self, image, timeEmbedding) :
+        lastBlock = image
+        skipConnections = []
         
-        self.convH1 = kr.layers.Conv2D(128, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convH2 = kr.layers.Conv2D(128, 3, padding = "same", activation = kr.activations.ReLu)
-        self.upSampleH1 = kr.layers.Conv2DTranspose(64, 3, 2, padding = "same")
+        for i in range(self.layerCount) :
+            skip, lastBlock = self.downBlocks[i]((lastBlock, timeEmbdedding))
+            skipConnections.insert(0, skip)
+            
+        _, lastBlock = self.bottleNeck(lastBlock, timeEmbedding)
         
-        self.convI1 = kr.layers.Conv2D(64, 3, padding = "same", activation = kr.activations.ReLu)
-        self.convI2 = kr.layers.Conv2D(64, 3, padding = "same", activation = kr.activations.ReLu)
-        self.finalConv = kr.layers.Conv2D(2, 1, padding = "same", activation = kr.activations.ReLu)
-        
-    def call(self, inputs):
-        
-        
-        a = self.convA1(inputs)
-        a = self.convA2(a)
-        
-        b = self.downSampleA1(a)
-        b = self.convB1(b)
-        b = self.convB2(b)
-        
-        c = self.downSampleB1(b)
-        c = self.convC1(c)
-        c = self.convC2(c)
-        
-        
-        d = self.downSampleC1(c)
-        d = self.convD1(d)
-        d = self.convD2(d)
-        
-        e = self.downSampleD1(d)
-        e = self.convE1(e)
-        e = self.convE2(e)
-        
-        f = self.upSampleE1(e)
-        f = kr.layers.concatenate((d,f))
-        f = self.convF1(f)
-        f = self.convF2(f)
-        
-        g = self.upSampleF1(f)
-        g = kr.layers.concatenate((c,g))
-        g = self.convG1(g)
-        g = self.convG2(g)
-        
-        h = self.upSampleG1(g)
-        h = kr.layers.concatenate((b,h))
-        h = self.convG1(h)
-        h = self.convG2(h)
-        
-        i = self.upSampleH1(h)
-        i = kr.layers.concatenate((a,i))
-        i = self.convG1(h)
-        i = self.convG2(h)
-        
-        return self.finalConv(i)
-        
-        
-        
-        
-    
-    
+        for i in range(self.layerCount) :
+            lastBlock = keras.concatenate((skipConnections[i], lastBlock))
+            _, lastBlock = self.upBlocks[i]((image, timeEmbdedding))
+            
+        return self.finalLayer(lastBlock)
+  
 
-
-class StableDiffusionModel(kr.Model) :
+#class StableDiffusionModel(kr.Model) :
     """
     Implementation of a Stable Diffusion Model. 
     
@@ -116,6 +85,37 @@ class StableDiffusionModel(kr.Model) :
     Uses the DiffusionModel class to generate new data from noised latent space.
     
     """
+    
+    """
+    
+############################### SIMPLY ADD TIME EMBEDDING AT EACH STEP!!!! ####
+    def __init__(self):
+        super().__init__()
+        
+        # Input Image
+        self.input =
+        self.timeEmbedding = kr.layers.Input()
+        
+    def diffusionSchedule() :
+        
+        # linear diffusion schedule:
+        return tf.linspace(0.0001, 0.9999)
+        
+    def denoise(self) :
+        
+    
+        
+    def __addNoise(self, images) :
+        
+    def train_step(self, images) :
+        
+        # Channel first format:
+        noise = tf.random.normal(shape=(batch_size, 3, image_size, image_size))
+        
+        
+        with tf.GradientTape as tape:
+            
+"""
     
     
     
