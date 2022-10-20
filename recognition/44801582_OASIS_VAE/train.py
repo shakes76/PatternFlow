43cc8753d15610a4611e18a37f91f756c651aa49
4lon <alon.nusem@gmail.com>
2022-10-20 16:43:15 +1000

    Built out saving model and modularised functions

--- recognition/44801582_OASIS_VAE/train.py
+++ recognition/44801582_OASIS_VAE/train.py
@@ -4,18 +4,39 @@ should be imported from “modules.py” and the data loader should be imported 
 sure to plot the losses and metrics during training.
 """
 from dataset import *
+from modules import VQ_VAE
 
 import torch
 from tensorboardX import SummaryWriter
 from datetime import datetime
 
-def main(device):
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+
+def train(model, data, optimizer, writer):
+    pass
+
+
+def test(model, data, writer):
+    pass
+
+
+def run_epoch(model, train_data, test_data, optimizer, writer):
+    train(model, train_data, optimizer, writer)
+    test(model, test_data, writer)
+
+
+def main():
     writer = SummaryWriter(f"./logs/{current_time}")
-    save_filename = f"./models/{current_time}"
 
     train_loader, test_loader, validation_loader = get_loaders()
+
+    model = VQ_VAE().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+
+    for epoch in range(3):
+        run_epoch(model, train_loader, test_loader, optimizer, writer)
+
+        with open(f"{save_filename}/model_{epoch + 1}.pt", 'wb') as f:
+            torch.save(model.state_dict(), f)
 
 
 if __name__ == "__main__":
@@ -25,7 +46,10 @@ if __name__ == "__main__":
     if not os.path.exists('./models'):
         os.makedirs('./models')
 
-    # Device
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else 'cpu')
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    save_filename = f"./models/{current_time}"
+    os.makedirs(save_filenameBu)
 
-    main(device)
+    main()
