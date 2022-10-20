fdb01a7fe9302f32c2730f62a306f1ceec36a2f9
Wonge <daniel.jwong@outlook.com>
2022-10-20 23:47:15 +1000

    Re-hardcoded UNet, as found could not save model midway through training when layers were inside of lists. Implemented first version of StableDiffusionModel class.

---
+++ recognition/StableDiffusionModel/StableDiffusionHelperFunctions.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 13:48:39 2022
+
+@author: Danie
+"""
+import tensorflow.keras as kr
+import tensorflow as tf
+import matplotlib as plt
+import numpy as np
+
+from keras import layers
+
+def sinusoidalEmbedding(input) :
+    
