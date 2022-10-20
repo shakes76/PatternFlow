4a63ca74a970437c7a9a5e6d6a39786b0863d473
jessica <jessica@sullivans.id.au>
2022-10-20 15:23:34 +1000

    Dice Similarity Added
    A function was created in train.py, which calculates the dice similarity coefficient of two images that will be passed in as tensors. This is based on the link in the task sheet.

--- recognition/45799930/train.py
+++ recognition/45799930/train.py
@@ -1,5 +1,18 @@
 from modules import create_model
 from dataset import DataSet
+from tensorflow import reduce_sum
+
+
+def dice_sim_co(x, y):
+    """
+    This implements the dice similarity coefficients as described on the link in the task sheet.
+    To get the coefficient the equation is
+    (2 * |X ∩ Y|)/(|X| + |Y|)
+    :param x : An image in the form of a tensor
+    :param y : An image to compare to in the form of a tensor.
+    :return:
+    """
+    return (2 * reduce_sum(x * y)) / (reduce_sum(x) + reduce_sum(y))
 
 
 class TrainedModel:
