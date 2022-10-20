cb76564644f41d061c21ad46825046b97a520d4e
George Laugher <george_laugher@outlook.com>
2022-10-21 00:42:20 +1000

    Added CNN new layer functions

--- recognition/45807152/modules.py
+++ recognition/45807152/modules.py
@@ -1,13 +1,19 @@
 # Model of Improved UNet
 import tensorflow as tf
 from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv2D, LeakyReLU, Dropout, UpSampling2D
 
 
 class ImprovedUNet():
     """
-    An improved UNet UNet model based on the research paper.
+    Improved UNet model for lesion segmentation.
 
-    Ability to build CNN model.
+    An improved UNet model based on the "Brain Tumor Segmentation and
+    Radiomics Survival Prediction: Contribution to the BRATS 2017 Challenge"
+    research paper.
+
+    Provides the ability to generate a UNet model for lesion segmentation
+    with specified input image dimensions.
     """
 
     def __init__(self, image_height, image_width, image_channels, filters,
@@ -23,3 +29,44 @@ class ImprovedUNet():
         self.padding = padding
         self.dropout = dropout
         self.upsample = upsample
+
+    def new_conv_layer(self, input, filters, kernel_size, strides):
+        """Convolution layer for model."""
+        conv_layer = Conv2D(filters=filters,
+                            kernel_size=kernel_size,
+                            strides=strides,
+                            padding=self.padding,
+                            activation=LeakyReLU(alpha=self.alpha))(input)
+
+        return conv_layer
+
+    def new_context_module(self, input, filters):
+        """Context module for model."""
+        first_conv_layer = self.new_conv_layer(input, filters,
+                                               self.kernel_size, 1)
+        second_conv_layer = self.new_conv_layer(first_conv_layer, filters,
+                                                self.kernel_size, 1)
+        dropout = Dropout(self.dropout)(second_conv_layer)
+
+        return dropout
+
+    def new_upsample_module(self, input, filters):
+        """Upsample module for model."""
+        upsample_layer = UpSampling2D(self.upsample)(input)
+        conv_layer = self.new_conv_layer(upsample_layer, filters,
+                                         self.kernel_size, 1)
+        return conv_layer
+
+    def new_localisation_module(self, input, filters):
+        """Localisation module for model."""
+        first_conv_layer = self.new_conv_layer(input, filters,
+                                               self.kernel_size, 1)
+        second_conv_layer = self.new_conv_layer(first_conv_layer, filters,
+                                                self.kernel_size, 1)
+        return second_conv_layer
+
+    def new_segmentation_layer(self, input, filters):
+        """Segmentation layer for model."""
+        conv_layer = self.new_conv_layer(input, filters, (1, 1), 1)
+        
+        return conv_layer
