7df85e386374197798d6080f31c067f641ea7c17
Yoxiourn <matthewshearer2@gmail.com>
2022-10-21 01:32:04 +1100

    Added very simple Siamese network component to modules.py, which creates a CNN (similar to the one seen in the given paper), creates two models with this and feeds an input to each model, and then computes the L1 distance between the two vectors produced from the models, which is then fed into a sigmoid layer for classification.

---
+++ recognition/s4642283-ADNI-SIAMESE/modules.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow import keras
+import keras.backend as K
+from keras import layers
+from keras.layers import Lambda
+
+def siamese_model(input_shape):
+    """
+    Siamese model with distance computation and final sigmoid layer.
+    """
+
+    # CNN and Pooling layers
+    input = layers.Input(input_shape)
+    x = layers.Conv2D(4, (10, 10), activation="relu")(input)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(8, (7, 7), activation="relu")(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(8, (4, 4), activation="relu")(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(16, (4, 4), activation="relu")(x)
+    x = layers.Flatten()(x)
+
+    # Produce 10 dimensional vector
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = layers.Dense(10, activation="relu")(x)
+    embedding_network = keras.Model(input, x)
+
+    # Inputs for Siamese Networks
+    input_1 = layers.Input(input_shape)
+    input_2 = layers.Input(input_shape)
+
+    # Both Siamese Networks have the same embedding network
+    network_1 = embedding_network(input_1)
+    network_2 = embedding_network(input_2)
+
+    # Compute L1 Distance between two embeddings
+    L1_distance_layer = Lambda(lambda tensors: K.abs(tensors[0] - tensors[1]))
+    distance_computation_layer = L1_distance_layer([network_1, network_2])
+    normal_layer = tf.keras.layers.BatchNormalization()(distance_computation_layer)
+
+    # Classify whether the images belong to the same class or different classes
+    prediction = layers.Dense(1, activation="sigmoid")(normal_layer)
+    siamese = keras.Model(inputs=[input_1, input_2], outputs=prediction)
+
+    return siamese
