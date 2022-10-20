747670106c724ce96af21a14f9d099ccd2b32cee
Daniel Sayer <daniel.sayer01@gmail.com>
2022-10-20 19:12:11 +1000

    Data split as separate method

--- recognition/s4581059 - ISIC Segmentation/dataset.py
+++ recognition/s4581059 - ISIC Segmentation/dataset.py
@@ -1,41 +1,82 @@
 from sklearn.model_selection import train_test_split
+import os
 from glob import glob
+import cv2
+import numpy as np
 import tensorflow as tf
 #Contains the data loader for loading and preprocessing data
 
 
-img_height = 64
-img_width = 64
-batch_size = 32
-
 def load_data(path):
     """
-    Loads a ISIC data set - where each image is scaled to 64x64 to assist with 
-    performance/load, dataset is loaded in batches of 32. With a validation split
-    of 0.2
-
-    Returns: Raw datasets in the form: training_dataset, validation_dataset
+    Loads a ISIC data set
+    Param: path - should reference a folder which contains 2 subfolders one with images, and one with masks
+    Param: split - the ratio of valid and testing data: default is 0.2 i.e 0.6/0.2/0.2 train/test/valid
+    Returns: Raw data in the form: (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
     """
 
-    training_dataset = tf.keras.utils.image_dataset_from_directory(
-        path,
-        validation_split=0.2,
-        subset="training",
-        seed=42,
-        image_size=(img_height, img_width),
-        batch_size=batch_size
-    )
+    #Instantiate empty arrays of the processed images
+    processed_images = []
+    processed_masks = []
+    
+    #Adds only the jpg files to the list i.e. NOT THE SUPERPIXEL images
+    images = sorted(glob(os.path.join(path, "ISIC-2017_Training_Data", "*.jpg")))
+    #Adds only the png files to the list: in this case it is all files, but filters for files that shouldn't be there
+    masks = sorted(glob(os.path.join(path, "ISIC-2017_Training_Part1_GroundTruth", "*.png")))
 
-    validation_dataset = tf.keras.utils.image_dataset_from_directory(
-        path,
-        validation_split=0.2,
-        subset="validation",
-        seed=42,
-        image_size=(img_height, img_width),
-        batch_size=batch_size
-    )
+    #Process Data
+    for image, mask in zip(images, masks):
+        processed_image = process_image(image, 128, 128)
+        processed_mask = process_image(mask, 128, 128)
 
-    return training_dataset, validation_dataset
+        #Add to list of completed processed images
+        processed_images.append(processed_image)
+        processed_masks.append(processed_mask)
+    print("Images processed")
+    
+    #Conver to numpy arrays for convenience and to save memory
+    processed_images = np.array(processed_images)
+    processed_masks = np.array(processed_masks)
+
+    #One-hot encode the masks
+    processed_masks = tf.keras.utils.to_categorical(processed_masks)
+    
+    return processed_images, processed_masks
+
+def process_image(image, width, height):
+    """
+    Processes an image and resizes to a set size
+    Param: image - the path of the image which is to be processed
+    Param: (tuple) size - the size of the image
+    Returns: a processed black and white image that is normalized and resized
+    """
+    processed_image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+    processed_image = cv2.resize(processed_image, (width, height))
+    #Normalize image
+    processed_image = processed_image / 255.0   # type: ignore
+    return processed_image
+
+def train_test_valid(images, masks, split=0.2):
+    """
+    Splits two given image sets (of equal length) into training, validation and
+    testing data for each of the sets
+    Param: images - the images set to be split
+    Param: masks - the masks set to be split
+    Param: split - A split ratio, to indicate how much data shall be used for testing
+           0 < split < 1 - default split = 0.2 i.e. 80% of the data will be used for 
+           training and 20% for testing and validation
+    Returns: Split data in the form: (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+    """
+    test_size = int(len(images) * split)
+
+    train_x, valid_x = train_test_split(images, test_size=test_size, random_state=42)
+    train_y, valid_y = train_test_split(masks, test_size=test_size, random_state=42)
+
+    train_x, test_x = train_test_split(train_x, test_size=test_size, random_state=42)
+    train_y, test_y = train_test_split(train_y, test_size=test_size, random_state=42)
+
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+    
 
 if __name__ == "__main__":
     print(load_data("C:/Users/danie/Downloads/ISIC DATA/"))
