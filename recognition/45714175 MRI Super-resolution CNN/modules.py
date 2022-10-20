af5782289a383dcb4028d39d1c42ee022efaa4c4
matthew griffiths <matthewg056@gmail.com>
2022-10-20 19:51:33 +1000

    Started implementation of callback function.

--- recognition/45714175 MRI Super-resolution CNN/modules.py
+++ recognition/45714175 MRI Super-resolution CNN/modules.py
@@ -4,8 +4,8 @@ Contains function to return the model of the super-resolution CNN
 """
 
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Input
-from tensorflow.keras import Model
+import numpy as np
+import math
 
 def get_model():
     """Returns super resolution CNN model"""
@@ -24,3 +24,22 @@ def get_model():
     outputs = tf.nn.depth_to_space(x, upscale_factor)
 
     return tf.keras.Model(input, outputs)
+
+class ESPCNCallback(tf.keras.callbacks.Callback):
+    def __init__(self, testImage):
+        super(ESPCNCallback, self).__init__()
+        self.testImage = testImage.resize((testImage[0] // 4, testImage[1] // 4), method="guassian")
+
+    def on_epoch_begin(self, epoch, logs=None):
+        """Initialise PSNR array"""
+        self.psnr = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        """Compute and display PSNR metric"""
+        psnrMean = np.mean(self.psnr)
+        print("Mean PSNR for epoch: %.2f" % (psnrMean))
+        # TO DO: predictions
+
+    def on_test_batch_end(self, batch, logs=None):
+        """Append psnr metrics to array"""
+        self.psnr.append(10 * math.log10(1 / logs["loss"]))
