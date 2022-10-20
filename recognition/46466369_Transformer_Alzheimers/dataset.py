a8739ff7da916abdbda38c41b3f64e11bd28576a
erkshen <esrhiecn02@gmail.com>
2022-10-20 20:47:24 +1000

    Replaced load_dataset with version from colab implementation

--- recognition/46466369_Transformer_Alzheimers/dataset.py
+++ recognition/46466369_Transformer_Alzheimers/dataset.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import numpy as np
 from PIL import Image
+import zipfile
 
 """
 dataset.py
@@ -9,24 +10,24 @@ Contains the data loader for loading and preprocessing the data
 """
 
 # Path to dataset directory
-path = "C:/Users/Eric/Documents/uni/2022_sem2/comp3710/report/AD_NC"
+path = 'C:/Users/Eric/Documents/uni/2022_sem2/comp3710/report/'
+#path = '/content/gdrive/MyDrive/ADNI/train_AD.zip'
 
 # Load all the training images
-def load_dataset():
-    train_images = []
-    for f in os.listdir(Path(path + "/train/AD").absolute()):
-        img = np.array(Image.open(Path(path + "/train/AD/" + f).absolute()))
-        train_images.append(img)
-    for f in os.listdir(Path(path + "/train/NC").absolute()):
-        img = np.array(Image.open(Path(path + "/train/NC/" + f).absolute()))
-        train_images.append(img)
-    test_images = []
-    for f in os.listdir(Path(path + "/test/AD").absolute()):
-        img = np.array(Image.open(Path(path + "/test/AD/" + f).absolute()))
-        test_images.append(img)
-    for f in os.listdir(Path(path + "/test/NC").absolute()):
-        img = np.array(Image.open(Path(path + "/test/NC/" + f).absolute()))
-        test_images.append(img)
+def load_dataset(size):
 
+    zipped_images = zipfile.ZipFile(f'{path}train_AD.zip')
+    images_train_AD = np.array([np.array(Image.open(zipped_images.open(image)).resize((size, size))) for image in zipped_images.infolist()])
+    zipped_images = zipfile.ZipFile(f'{path}train_NC.zip')
+    images_train_NC = np.array([np.array(Image.open(zipped_images.open(image)).resize((size, size))) for image in zipped_images.infolist()])
+    zip = zipfile.ZipFile(f'{path}test_AD.zip')
+    images_test_AD = np.array([np.array(Image.open(zip.open(image)).resize((size, size))) for image in zip.infolist()])
+    zip = zipfile.ZipFile(f'{path}test_NC.zip')
+    images_test_NC = np.array([np.array(Image.open(zip.open(image)).resize((size, size))) for image in zip.infolist()])
+    
+    images_test_labels = np.concatenate((np.ones(images_test_AD.shape[0]), np.zeros(images_test_NC.shape[0])))
+    images_test = np.concatenate((images_test_AD, images_test_NC), axis = 0)
+    images_train_labels = np.concatenate((np.ones(images_train_AD.shape[0]), np.zeros(images_train_NC.shape[0])))
+    images_train = np.concatenate((images_train_AD, images_train_NC), axis = 0)
     print("Data successfully loaded")
-    return (np.array(train_images), np.array(test_images))
+    return (images_train, images_train_labels, images_test, images_test_labels)
