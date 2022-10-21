a04d1ccf82b2865b7cc94ace1fe4f1a1606c053d
George Laugher <george_laugher@outlook.com>
2022-10-21 16:08:44 +1000

    Added additional export comments

--- recognition/45807152/predict.py
+++ recognition/45807152/predict.py
@@ -20,26 +20,33 @@ def make_prediction(model, test_x, test_y, slice=(0, 1)):
     for i in range(first_test, last_test):
         plt.figure(figure_counter)
         figure_counter += 1
-
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
+
         # Original Image Plot
         ax1.set_title("Original Image")
         ax1.axis("off")
         ax1.imshow(test_x[i])
+
         # Ground Truth Mask Plot
         ax2.set_title("Ground Truth Mask")
         ax2.axis("off")
-        ax2.imshow(tf.argmax(test_y[i], axis=2), cmap='gray')
+        # Plot lesion regions (axis 2)
+        ax2.imshow(tf.argmax(test_y[i], axis=2), cmap='gray')  
+
         # Get metrics
         pred_dice = np.around(dice_coefficient(
             test_y[i], predictions[i]).numpy(), 3)
         pred_iou = np.around(IoU(test_y[i], predictions[i]).numpy(), 3)
-        
+
+        # Prediction Mask Plot
         ax3.set_title(f"Prediction")
         ax3.axis("off")
+        # Add metric subtext
         ax3.text(0, 150, f"DSC={str(pred_dice)}, IoU={str(pred_iou)}")
+        # Plot lesion predicted regions (axis 2)
         ax3.imshow(tf.argmax(predictions[i], axis=2), cmap='gray')
-
-        plt.savefig(f"./figures/testComparison{i}.png")
+        
+        # Save figure locally
+        plt.savefig(f"./figures/testComparison{i}.png", bbox_inches='tight')
 
     return
