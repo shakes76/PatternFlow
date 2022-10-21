9ee5ebe7d7aa99ce827316121118168e0e9758ca
Georgia Spanevello <s4581312@student.uq.edu.au>
2022-10-21 18:58:20 +1000

    Added comments to dataset.py. Functionality remained the same

--- recognition/45813126_VQVAE/dataset.py
+++ recognition/45813126_VQVAE/dataset.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 class Dataset:
-    # Initialise where the data is located (previously downloaded and extracted)
     def __init__(self):
+        """ Gather data. REQUIREMENT - Change path variable to direct the program to downloaded and extracted data """
         path = "C:/Users/Georgia Spanevello/.keras/datasets/oasis/keras_png_slices_data"
         self.train_data_path = path + "/keras_png_slices_train"
         self.train_labels_path = path + "/keras_png_slices_seg_train"
@@ -21,43 +21,40 @@ class Dataset:
         self.valid_data = self.load_process_images(self.valid_data_path)
         self.valid_labels = self.load_process_labels(self.valid_labels_path)
 
-    # Load and process the data at the given path
     def load_process_images(self, path):
+        """ Load and process the data at the given path """
         images = []
 
-        # Read all png images
+        # Read then store all images
         for file in glob.glob(path + "/*.png"):
             images.append(image.imread(file))
-        
-        # Numpy array to hold all the now numpy array converted images
         data = np.array(images, dtype = np.float32)
 
-        # Residual extraction
+        # Residual extraction then normalise
         data = (data - np.mean(data)) / np.std(data)
-        # Normalise
         data = (data - np.amin(data)) / np.amax(data - np.amin(data))
 
         return data[:, :, :, np.newaxis]
     
-    # Load and process (one-hot encoding) the labels at the given path
     def load_process_labels(self, path):
+        """ Load and process (one-hot encoding) the labels at the given path """
         images = []
 
+        # Read then store all labels
         for file in glob.glob(path + "/*.png"):
             curr_image = image.imread(file)
             one_hot_enc = np.zeros(curr_image.shape)
             for i, value in enumerate(np.unique(curr_image)):
                 one_hot_enc[:, :][curr_image == value] = i
             images.append(one_hot_enc)
-
         labels = np.array(images, dtype = np.uint8)
 
+        # Process labels using one-hot encoding
         one_hot_encs = []
         num_classes = 4
         for i in range(labels.shape[0]):
             curr_image = labels[i]
             one_hot_enc = np.zeros((curr_image.shape[0], curr_image.shape[1], num_classes), dtype = np.uint8)
-        
             for i, value in enumerate(np.unique(curr_image)):
                 one_hot_enc[:, :, i][curr_image == value] = 1
                 one_hot_encs.append(one_hot_enc)
