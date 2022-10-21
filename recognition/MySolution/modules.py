a6f777872d93b1415e712eaa45701badf07eed0f
pranaypremdas <pranaypremdas@yahoo.com.au>
2022-10-21 23:16:00 +1000

    Added comments to modules.py

--- recognition/MySolution/modules.py
+++ recognition/MySolution/modules.py
@@ -1,114 +1,142 @@
-import re
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras import layers
-
 from train import *
 from dataset import *
 
-def main():
-    #LoadData() # Loads all the given data
+"""
+Vision transformer class that contains all the model data
+"""
+class VIT:
+    """
+    Constructor used to call main
+    """
+    def __init__(self):
+        self.main()
 
-    x_train = np.load('X_train.npy')
+    def main(self):
+        LoadData() # Loads all the given data
 
-    num_classes = 1 # AD or NC
-    input_shape = (240, 256, 1)
-    image_size = 250
-    patch_size = 32
-    num_patches = (image_size // patch_size) ** 2
-    projection_dim = 64 # increase
-    num_heads = 8 # increase
-    transformer_units = [
-        projection_dim * 3, # increase
-        projection_dim,
-    ] # size of the transformer layers
-    transformer_layers = 9
-    mlp_head_units = [2048, 256] # Size of the dense layers of the final classifier
+        x_train = np.load('X_train.npy') # Loads x_train data
 
-    plt.figure(figsize=(4, 4))
-    image = x_train[np.random.choice(range(x_train.shape[0]))]
-    plt.imshow(image.astype("uint8"), cmap="gray")
-    plt.axis("off")
+        # All the parameters for the Vision Transformer
+        num_classes = 1 # AD or NC
+        input_shape = (240, 256, 1)
+        image_size = 250
+        patch_size = 32
+        num_patches = (image_size // patch_size) ** 2
+        projection_dim = 64 
+        num_heads = 8 
+        transformer_units = [
+            projection_dim * 3,
+            projection_dim,
+        ] 
+        # size of the transformer layers
+        transformer_layers = 9
+        # Size of the dense layers of the final classifier
+        mlp_head_units = [2048, 256]
 
-    resized_image = tf.image.resize(
-        tf.convert_to_tensor([image]), size=(image_size, image_size)
-    )
-    plt.savefig("OG.jpg") 
-
-    patches = Patches(patch_size)(resized_image)    
-    print(f"Image size: {image_size} X {image_size}", flush=True)
-    print(f"Patch size: {patch_size} X {patch_size}", flush=True)
-    print(f"Patches per image: {patches.shape[1]}", flush=True)
-    print(f"Elements per patch: {patches.shape[-1]}", flush=True)
-
-    n = int(np.sqrt(patches.shape[1]))
-    plt.figure(figsize=(4, 4))
-
-    for i, patch in enumerate(patches[0]):
-        ax = plt.subplot(n, n, i + 1)
-        patch_img = tf.reshape(patch, (patch_size, patch_size, 1))
-        plt.imshow(patch_img.numpy().astype("uint8"), cmap="gray")
+        #Plots all the images
+        plt.figure(figsize=(4, 4))
+        image = x_train[np.random.choice(range(x_train.shape[0]))]
+        plt.imshow(image.astype("uint8"), cmap="gray")
         plt.axis("off")
 
-    plt.savefig("Test.jpg")
+        resized_image = tf.image.resize(
+            tf.convert_to_tensor([image]), size=(image_size, image_size)
+        )
+        plt.savefig("OG.jpg") 
+
+        # Prints out all the data for the images
+        patches = Patches(patch_size)(resized_image)    
+        print(f"Image size: {image_size} X {image_size}", flush=True)
+        print(f"Patch size: {patch_size} X {patch_size}", flush=True)
+        print(f"Patches per image: {patches.shape[1]}", flush=True)
+        print(f"Elements per patch: {patches.shape[-1]}", flush=True)
+
+        n = int(np.sqrt(patches.shape[1]))
+        plt.figure(figsize=(4, 4))
+
+        for i, patch in enumerate(patches[0]):
+            ax = plt.subplot(n, n, i + 1)
+            patch_img = tf.reshape(patch, (patch_size, patch_size, 1))
+            plt.imshow(patch_img.numpy().astype("uint8"), cmap="gray")
+            plt.axis("off")
+
+        plt.savefig("Test.jpg")
+
+        # Data Augmentation used to improove model
+        data_augmentation = keras.Sequential(
+            [
+                layers.experimental.preprocessing.Normalization(),
+                layers.experimental.preprocessing.Resizing(image_size, image_size),
+                layers.experimental.preprocessing.RandomFlip("horizontal"),
+                layers.experimental.preprocessing.RandomRotation(factor=0.2),
+                layers.experimental.preprocessing.RandomZoom(
+                    height_factor=0.2, width_factor=0.2
+                ),
+            ],
+            name="data_augmentation"
+        )   
+        
+        data_augmentation.layers[0].adapt(x_train)
+        inputs = layers.Input(shape=input_shape)
+        augmented = data_augmentation(inputs)
+        patches = Patches(patch_size)(augmented) # Creates patches
+        encoded_patches = PatchEncoder(num_patches, projection_dim)(patches) # Applies Patch Encoder
+
+        # Loops through all transformer layers
+        for _ in range(transformer_layers):
+            x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            attention_output = layers.MultiHeadAttention(
+                num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+            )(x1, x1)
+            x2 = layers.Add()([attention_output, encoded_patches])
+            x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+            x3 = self.mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+            encoded_patches = layers.Add()([x3, x2])
+
+        # Adds additional layers
+        representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        representation = layers.Flatten()(representation)
+        representation = layers.Dropout(0.5)(representation)
+        
+        # Calls the Multi layer Perceptron
+        features = self.mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+        # Final layer of the model. Uses Sigmoid activation as it works the best (based on trial and error)
+        logits = layers.Dense(num_classes, activation="sigmoid")(features)
+
+        model = keras.Model(inputs=inputs, outputs=logits)
+        model.summary() # Outputs a model Summary
+        Train(model=model) # Trains the model
+
+    """
+    Function for the multi layer perceptron.
+    """
+    def mlp(self, x, hidden_units, dropout_rate):
+        for units in hidden_units:
+            x = layers.Dense(units, activation=tf.nn.gelu)(x)
+            x = layers.Dropout(dropout_rate)(x)
+        return x
 
 
-    data_augmentation = keras.Sequential(
-        [
-            layers.experimental.preprocessing.Normalization(),
-            layers.experimental.preprocessing.Resizing(image_size, image_size),
-            layers.experimental.preprocessing.RandomFlip("horizontal"),
-            layers.experimental.preprocessing.RandomRotation(factor=0.2),
-            layers.experimental.preprocessing.RandomZoom(
-                height_factor=0.2, width_factor=0.2
-            ),
-        ],
-        name="data_augmentation"
-    )   
-    
-    data_augmentation.layers[0].adapt(x_train)
-
-    inputs = layers.Input(shape=input_shape)
-    augmented = data_augmentation(inputs)
-    patches = Patches(patch_size)(augmented)
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
-
-    for _ in range(transformer_layers):
-        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
-        )(x1, x1)
-        x2 = layers.Add()([attention_output, encoded_patches])
-        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-        encoded_patches = layers.Add()([x3, x2])
-
-    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
-    representation = layers.Flatten()(representation)
-    representation = layers.Dropout(0.5)(representation)
-
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
-
-    logits = layers.Dense(num_classes, activation="sigmoid")(features)
-
-    model = keras.Model(inputs=inputs, outputs=logits)
-    # model.summary()
-    Train(model=model)
-
-def mlp(x, hidden_units, dropout_rate):
-    for units in hidden_units:
-        x = layers.Dense(units, activation='relu')(x)
-        x = layers.Dropout(dropout_rate)(x)
-    return x
-
+"""
+Patches class used for creating patches
+"""
 class Patches(layers.Layer):
 
+    """
+    Constructor for the Patch class
+    """
     def __init__(self, patch_size):
         super(Patches, self).__init__()
         self.patch_size = patch_size
-    
+
+    """
+    Creates the patches and returns it
+    """
     def call(self, images):
         batch_size = tf.shape(images)[0]
         patches = tf.image.extract_patches(
@@ -122,7 +150,15 @@ class Patches(layers.Layer):
         patches = tf.reshape(patches, [batch_size, -1, patch_dims])
         return patches
 
+
+"""
+Patch Encoding class used to encode Patches
+"""
 class PatchEncoder(layers.Layer):
+
+    """
+    Constructor for the Patch encoder class
+    """
     def __init__(self, num_patches, projection_dim):
         super(PatchEncoder, self).__init__()
         self.num_patches = num_patches
@@ -131,11 +167,12 @@ class PatchEncoder(layers.Layer):
             input_dim=num_patches, output_dim=projection_dim
         )
 
+    """
+    Returns encoded Patch
+    """
     def call(self, patch):
         positions = tf.range(start=0, limit=self.num_patches, delta=1)
         encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
 
-
-if __name__ == "__main__":
-    main()
+VIT() # Calls the Vision Transformer class to begin running the model
