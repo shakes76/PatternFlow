c2ac08fccdfa3cb82db2c7c660a125a2c57c6858
Wonge <daniel.jwong@outlook.com>
2022-10-21 21:03:03 +1000

    migrated more training documents over from google Colab, organised modules a bit

--- recognition/StableDiffusionModel/Train.py
+++ recognition/StableDiffusionModel/Train.py
@@ -17,34 +17,26 @@ from StableDiffusionModel import *
 from CustomLayers import *
 from AutoEncoder import *
 
-
-##
-##  TODO: MAKE BETTER DOCSTRINGS
-##
+from modules import *
 
 
 def downloadOASIS( destinationFolder = "./DataSets" ):
     """
-        Downloads the OASIS Brain MRI dataset.
+    Downloads the OASIS Brain MRI dataset.
     """
     dataURL = "https://cloudstor.aarnet.edu.au/plus/s/tByzSZzvvVh0hZA/download"
     dataDirectory = tf.keras.utils.get_file(origin=dataURL,fname='oa-sis' ,untar=True)
     dataDirectory = pathlib.Path(dataDirectory)
-    #file = tarfile.open("C:\Users\Danie\.keras\datasets")
-    #file.extractall('./DataSets')
-    
-    
+
 
 def loadTrainingData (path, dataType = np.float32):
     """
-        Downloads the OASIS Brain MRI dataset.
+    Loads the OASIS Brain MRI dataset
     """
     imageList = []
     for filename in gb.glob(path+'/*.png'): 
         img=plt.image.imread (filename)
         imageList.append(img)
-
-    print('Raw training data shape:',np.array(imageList).shape)
     trainSet = np.array(imageList, dtype=dataType)
     return trainSet
 
@@ -80,7 +72,7 @@ def autoEncoderBuildCompile(inputSize = 128,
                             normLayers = True, 
                             activation = kr.activations.relu, 
                             loadWeights = False, 
-                            loadWeightsPath = ""):
+                            loadWeightsPath = "./Weights/FinalModel"):
     """
     Builds and compiles the AutoEncoder defined in AutoEncoder.py, 
     and readies it for training.
@@ -117,30 +109,21 @@ def trainAutoEncoder(dataSet, batch_size = 32, epochs = 600, saveModel = True, s
                               callbacks=callbacks)
         return history
 
-
-
-
 def plotAutoEncoderExamples(autoEncoder, dataSet, inputDim=128, imagesShown = 3, fontSize = 10):
     """
     Plots images against their latent space and reconstruction from the autoencoder.   
 
     Parameters
-    ----------
     autoEncoder : AutoEncoder
         AutoEncoder model to plot results from
-        
     dataSet : Tensor
         Image data set autoencoder was trained on
-        
-    inputDim : TYPE, optional
+    inputDim : int, optional
         Size of the image, e.g. 64 for a 64x64 image
-        
-    imagesShown : TYPE, optional
+    imagesShown : int, optional
         Number of Images plotted
-        
-    fontSize : TYPE, optional
+    fontSize : int, optional
         font size of titles in plots
-    ----------
 
     """
     
@@ -167,16 +150,98 @@ def plotAutoEncoderExamples(autoEncoder, dataSet, inputDim=128, imagesShown = 3,
         axs[row, col].get_yaxis().set_visible(False)
     fig.tight_layout()
 
+def lossFunction(real, generated):
+    """
+    Implementation of mean squared error for use in the training function
+    """
+    loss = tf.math.reduce_mean((real - generated) ** 2)
+    return loss
+
+def trainOnBatch(batch):
+    """
+    Trains the stable diffusion model on the input batch
+    """
+    rng, tsrng = np.random.randint(0, 100000, size=(2,))
+    timestep_values = createTimeStamp(tsrng, batch.shape[0])
+
+    noised_image, noise = addNoise(rng, batch, timestep_values)
+    with tf.GradientTape() as tape:
+        prediction = unet(noised_image, timestep_values)
+        
+        lossValue = lossFunction(noise, prediction)
+    
+    gradients = tape.gradient(lossValue, unet.trainable_variables)
+    opt.apply_gradients(zip(gradients, unet.trainable_variables))
+
+    return lossValue
+
+def plotHistory(history):
+    """ plots the loss associated with the input training history """
+    
+    plt.pyplot.plot(history.history["loss"])
+
+def checkPointManager(model, path):
+  """
+  Creates and returns a checkpoint manager for the input model. The checkpoint
+  manager will write to the specified path
+  """
+  check = tf.train.Checkpoint(unet=model)
+  checkManager = tf.train.CheckpointManager(check, path, max_to_keep=2)
+  return check, checkManager
+
+
+
+def trainLoop(epochs, encoder):
+    for e in range(1, epochs+1):
+        bar = tf.keras.utils.Progbar(len(trainingData)-1)
+        for i, batch in enumerate(iter(trainingData)):
+            # Reducing the image to its latent representation
+            latent = encoder(batch)
+    
+            loss = train_step(latent)
+            losses.append(loss)
+            bar.update(i, values=[("loss", loss)])
+    
+        avg = np.mean(losses)
+        print(f"Mean loss {e}/{epochs}: {avg}")
+        ckpt_manager.save(checkpoint_number=e)
+
+
 
 if __name__ == "__main__":
     downloadOASIS()
-    trainDataRaw = loadTrainingData('./DataSets/keras_png_slices_train')
+    trainDataRaw = loadTrainingData('/content/keras_png_slices_data/keras_png_slices_train')
     trainData = processTrainingData(trainDataRaw)
+
+    autoEnc = autoEncoderBuildCompile(loadWeights = True, loadWeightsPath = "/content/FinalModel")
     
-    autoEnc = autoEncoderBuildCompile(loadWeights = True, loadWeightsPath = "./CheckPoints/FinalModel")
+    # Training the AutoEncoder
     history = trainAutoEncoder(trainData)
+    plotHistory(history)
+
+    # Plotting examples of autoEncoder reducing images to latent space
+    plotAutoEncoderExamples(autoEnc, trainData, imagesShown = 3, fontSize = 8)
+
+    # Initalising unet for diffusion model
+    unet = Unet(channels=1, dim=64)
     
-    plotAutoEncoderExamples(autoEnc, trainData, imagesShown = 5, fontSize = 8)
+    # creating checkpoint manager
+    ckpt, ckpt_manager= checkPointManager(model, path)
+    
+    # load from a previous checkpoint if it exists, else initialize the model from scratch
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        start_interation = int(ckpt_manager.latest_checkpoint.split("-")[-1])
+        print("Restored from checkpoint {}".format(ckpt_manager.latest_checkpoint))
+    else:
+        print("Creating new checkpoint sequence")
+    
+    # Optimizer used in training
+    opt = kr.optimizers.Adam(learning_rate=1e-4)
+    
+    # Batching and shuffling training data
+    trainingData = tf.data.Dataset.from_tensor_slices(trainData).shuffle(1000).batch(64)
+    
     
     
     
