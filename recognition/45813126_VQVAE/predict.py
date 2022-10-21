2209459bcde2b7c06ac85181eed164a4d3cf6784
Georgia Spanevello <s4581312@student.uq.edu.au>
2022-10-21 12:16:48 +1000

    Added ssim calculations in predict.py. SSIM calculated at approx 0.76 after 5 epochs and looking at 100 test images

--- recognition/45813126_VQVAE/predict.py
+++ recognition/45813126_VQVAE/predict.py
@@ -1,6 +1,7 @@
 from train import data, trained_model
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def show_subplot(original, reconstructed):
     plt.subplot(1, 2, 1)
@@ -16,9 +17,16 @@ def show_subplot(original, reconstructed):
     plt.show()
 
 
-idx = np.random.choice(len(data.test_data), 10)
+num_test_images = 100
+idx = np.random.choice(len(data.test_data), num_test_images)
 test_images = data.test_data[idx]
 reconstructions = trained_model.predict(test_images)
 
+total_ssim_val = 0
 for test_image, reconstructed_image in zip(test_images, reconstructions):
-    show_subplot(test_image, reconstructed_image)
+    ssim = tf.image.ssim(test_image, reconstructed_image, max_val=1.0)
+    total_ssim_val += ssim.numpy()
+    #show_subplot(test_image, reconstructed_image)
+
+average_ssim = total_ssim_val / num_test_images
+print(average_ssim)
