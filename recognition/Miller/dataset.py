Fixed Bug in model.fit that caused loss. It was due to variance being calculated incorrectly.

recognition/Miller/dataset.py
@@ -8,6 +8,9 @@ import tensorflow as tf
 import glob
 import numpy as np
 from matplotlib import image
+import os
+from PIL import Image
+
 
 # Download the Oasis Data as zip file. Will need to extract it manually afterwards
 def download_oasis ():
@@ -19,8 +22,8 @@ def download_oasis ():
   
 # Loads the training images (non segmented) from given path and returns an numpy array of arrays
 def load_training (path):
+    
     image_list = []
-
     # Iterate through all paths and convert to 'png'
     for filename in glob.glob(path + '/*.png'): 
         # Read an image from the given filename into an array
@@ -32,11 +35,13 @@ def load_training (path):
 
     # Create an numpy array to hold all the array turned images
     train_set = np.array(image_list, dtype=np.float32)
+       
+  
     return train_set
 
 # Normalizes training images and adds 4th dimention 
 def process_training (data_set):
-
+    
     """ Residual Extraction -> Useful for comparing distributions with different means but similar shapes"""
     # Calculate the residuals of the data - each residual is dist from each distribution mean which is now zero
     data_set = (data_set - np.mean(data_set)) / np.std(data_set)

recognition/Miller/predict.py
@@ -1,57 +1,113 @@
 """
 “predict.py" showing example usage of your trained model. Print out any results and / or provide visualisations where applicable
 """
-import tensorflow as tf 
-import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import image
-import glob
 import modules as mod
-import train as t
+
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 # Show how well program performs 
 
 
 """ MODEL AND TRAIN VQ-VAE """
-# Create a instance of the VQ-VAE model
-latent_dimensions = 16
-embeddings_number = 64
-image_size = 256
-# beta = [0.25, 2]
-beta = 0.25
-model = mod.vqvae_model(image_size, latent_dimensions, embeddings_number, beta)
 
-model.summary()
-
-
-model.compile (optimizer="Adam", loss= tf.keras.losses.CategoricalCrossentropy())
-
-# record history of training to display loss over ephocs 
-history = model.fit(t.train_X, t.train_Y,  validation_data= (t.validate_X, t.validate_Y) ,batch_size=32,shuffle=True,epochs=5)
-
-# evaluate against testing data 
-model.evaluate(t.test_X,t.test_Y)
-
-def show_subplot(original, reconstructed):
+""" RECONSTRUCTION RESULTS"""
+# Plots the original image against the reconstructed one
+def plot_comparision_original_to_reconstructed(original, reconstructed):
+    plt.figure(figsize = (10,12))
     plt.subplot(1, 2, 1)
-    plt.imshow(original.squeeze() + 0.5)
+    plt.imshow(original.squeeze() + 0.5, cmap = 'gray')
     plt.title("Original")
     plt.axis("off")
 
     plt.subplot(1, 2, 2)
-    plt.imshow(reconstructed.squeeze() + 0.5)
+    plt.imshow(reconstructed.squeeze() + 0.5, cmap = 'gray')
     plt.title("Reconstructed")
     plt.axis("off")
 
     plt.show()
 
-
-
-idx = np.random.choice(len(t.test_X), 10)
-test_images = t.test_X[idx]
-reconstructions_test = model.predict(test_images)
+trained_model = mod.model.model
+idx = np.random.choice(len(test_X), 10)
+test_images = test_X[idx]
+reconstructions_test = trained_model.predict(test_images)
 
 for test_image, reconstructed_image in zip(test_images, reconstructions_test):
-    show_subplot(test_image, reconstructed_image)
+    plot_comparision_original_to_reconstructed(test_image, reconstructed_image)
+
+# Return the average pixel value for the image and the reconstruction
+def calculate_mean(image, reconstructed_image):
+    image_pixel = 0
+    reconstructed_pixel = 0
+
+    for row in range(256):
+        for col in range(256):
+            image_pixel += image[row][col]
+            reconstructed_pixel += reconstructed_image[row][col]
+
+    image_pixel = image_pixel / (256**2)
+    reconstructed_pixel = reconstructed_pixel / (256**2)
+
+    return image_pixel, reconstructed_image
+
+# Returns std dev for the pixel value of each image
+def calculate_stddev(image, reconstructed_image, image_mean, reconstructed_image_mean):
+
+    image_variance = 0
+    reconstructed_image_variance = 0
+
+    for row in range(256):
+        for col in range(256):
+            image_variance += np.square(image[row][col] - image_mean)
+            reconstructed_image_variance += np.square(reconstructed_image[row][col] - reconstructed_image_mean)
+    
+    image_variance = np.sqrt(image_variance/256**2 - 1)
+    reconstructed_image_variance = np.sqrt(reconstructed_image_variance/256**2 - 1)
+    return image_variance, reconstructed_image_variance
+
+# Returns the covariance for both images
+def calculate_covariance(image, reconstructed_image, image_mean, predicted_mean):
+    covariance_value = 0
+  
+    for row in range(256):
+        for col in range(256): 
+            covariance_value += (image[row][col] - image_mean)*(reconstructed_image[row][col] - predicted_mean)
+    
+    return covariance_value/256**256-1
+
+
+# Return the structural similarity between two images; measures the window x and y of common size.
+# https://en.wikipedia.org/wiki/Structural_similarity
+def structural_similarity(mean_X, predicted_mean, stddev_X, predicted_stddev, covariance):
+    K1 = 0.01 # default value
+    K2 = 0.03 # default value
+    L = 255 # dynamic range of pixel value (2^bits per pixel -1)
+    C1 = (K1 * L)**2
+    C2 = (K2 * L)**2
+    C3 = C2 / 2
+    
+    luminance_x_y = (2*mean_X*predicted_mean + C1)/(mean_X**2+predicted_mean**2+C1)
+    contrast_x_y = (2*stddev_X*predicted_stddev + C2)/(stddev_X**2+np. predicted_stddev**2+C2)
+    structure_x_y = (covariance+C3)/(stddev_X*predicted_stddev+C3)
+    return luminance_x_y * contrast_x_y * structure_x_y
+
+# Returns the structured similarity for the entire data set
+def structural_similarity_mean(test_X, model):
+    structured_similarity_coef = 0
+
+    for i, data in enumerate(test_X):
+        # get reconstructed image
+        image_reconstruction = model.predict(data)
+        data = data[0,:,:,0]
+        image_reconstruction = image_reconstruction[0,:,:,0]
+
+        # Calculate structured similarity and add to total
+        mean_X, predicted_mean = calculate_mean(data, image_reconstruction)
+        stddev_X, predicted_stddev = calculate_stddev(data, image_reconstruction, mean_X, predicted_mean)
+        covariance = calculate_covariance(data, image_reconstruction, mean_X, predicted_mean)
+        structured_similarity_coef += structural_similarity(mean_X, predicted_mean, stddev_X, predicted_stddev, covariance)
+
+    return structured_similarity_coef / len(test_X)
+
+print(structural_similarity_mean(test_X, trained_model))