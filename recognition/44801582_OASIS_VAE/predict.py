3fb1dfcd8e87c8b42bfd2e893b2beb057470f9a1
4lon <alon.nusem@gmail.com>
2022-10-21 23:53:00 +1000

    Fixed up incorrect function call from refactor and add SSIM evaulation and recontruction to predict.py

--- recognition/44801582_OASIS_VAE/predict.py
+++ recognition/44801582_OASIS_VAE/predict.py
@@ -5,6 +5,7 @@ import numpy as np
 import dataset
 import modules
 import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity
 
 
 def create_new_brains():
@@ -41,16 +42,65 @@ def create_new_brains():
 
     for i in range(batch):
         plt.subplot(1, 2, 1)
-        plt.imshow(priors[i])
+        plt.imshow(priors[i], cmap='gray')
         plt.title("Code")
         plt.axis("off")
 
         plt.subplot(1, 2, 2)
-        plt.imshow(generated_samples[i].squeeze() + 0.5)
+        plt.imshow(generated_samples[i].squeeze() + 0.5, cmap='gray')
         plt.title("Generated Sample")
         plt.axis("off")
         plt.show()
 
 
+def get_structural_similarity():
+    vqvae = modules.VQVAE(16, 128)
+    vqvae.load_weights("samples/vqvae_model_weights.h5")
+    _, _, test_data, _ = dataset.oasis_dataset(500)
+
+    similarity_scores = []
+    reconstructions_test = vqvae.predict(test_data)
+
+    for i in range(reconstructions_test.shape[0]):
+        original = test_data[i, :, :, 0]
+        reconstructed = reconstructions_test[i, :, :, 0]
+
+        similarity_scores.append(structural_similarity(original, reconstructed,
+                                                       data_range=original.max() - original.min()))
+
+    average_similarity = np.average(similarity_scores)
+
+    print(average_similarity)
+
+
+def plot_reconstructions():
+    vqvae = modules.VQVAE(16, 128)
+    vqvae.load_weights("samples/vqvae_model_weights.h5")
+    _, _, test_data, _ = dataset.oasis_dataset(500)
+    num_tests = 8
+    test_images = test_data[np.random.choice(len(test_data), 8)]
+    reconstructions = vqvae.predict(test_images)
+
+    i = 0
+    plt.figure(figsize=(4, num_tests * 2), dpi=512)
+    for test_image, reconstructed_image in zip(test_images, reconstructions):
+        test_image = test_image.squeeze()
+        reconstructed_image = reconstructed_image[:, :, 0]
+        plt.subplot(num_tests, 2, 2 * i + 1, )
+        plt.imshow(test_image, cmap='gray')
+        plt.title("Original")
+        plt.axis("off")
+
+        plt.subplot(num_tests, 2, 2 * i + 2)
+        plt.imshow(reconstructed_image, cmap='gray')
+        plt.title(f"Reconstructed (SSIM:{structural_similarity(test_image, reconstructed_image, data_range=test_image.max() - test_image.min()):.2f})")
+
+        plt.axis("off")
+
+        i += 1
+
+    plt.show()
+
+
 if __name__ == "__main__":
-    create_new_brains()
+    plot_reconstructions()
--- recognition/44801582_OASIS_VAE/train.py
+++ recognition/44801582_OASIS_VAE/train.py
@@ -66,54 +66,8 @@ def plot_losses(history, time, name):
     plt.close()
 
 
-def plot_reconstructions(trained_vqvae_model, dataset, time):
-    num_tests = 8
-    test_images = dataset[np.random.choice(len(dataset), 8)]
-    reconstructions = trained_vqvae_model.predict(test_images)
-
-    i = 0
-    plt.figure(figsize=(4, num_tests * 2), dpi=512)
-    for test_image, reconstructed_image in zip(test_images, reconstructions):
-        test_image = test_image.squeeze()
-        reconstructed_image = reconstructed_image[:, :, 0]
-        plt.subplot(num_tests, 2, 2 * i + 1, )
-        plt.imshow(test_image, cmap='gray')
-        plt.title("Original")
-        plt.axis("off")
-
-        plt.subplot(num_tests, 2, 2 * i + 2)
-        plt.imshow(reconstructed_image, cmap='gray')
-        plt.title(f"Reconstructed (SSIM:{structural_similarity(test_image, reconstructed_image, data_range=test_image.max() - test_image.min()):.2f})")
-
-        plt.axis("off")
-
-        i += 1
-
-    plt.savefig(f"out/{time}/reconstruction.png")
-    plt.close()
-
-
-def get_structural_similarity(model, dataset):
-    sample_size = 10
-    similarity_scores = []
-    idx = np.random.choice(len(dataset), sample_size)
-    test_images = dataset[idx]
-    reconstructions_test = model.predict(test_images)
-
-    for i in range(reconstructions_test.shape[0]):
-        original = test_images[i, :, :, 0]
-        reconstructed = reconstructions_test[i, :, :, 0]
-
-        similarity_scores.append(
-            structural_similarity(original, reconstructed, data_range=original.max() - original.min()))
-
-    average_similarity = np.average(similarity_scores)
-
-    return average_similarity
-
-
 def train_vq(time, num_embeddings, latent_dim, batch_size):
-    (train_data, validate_data, test_data, data_variance) = dataset.oasis_dataset(200)
+    (train_data, validate_data, test_data, data_variance) = dataset.oasis_dataset(10)
 
     vqvae_trainer = Trainer(data_variance, latent_dim=latent_dim, num_embeddings=num_embeddings)
     vqvae_trainer.compile(optimizer=tf.keras.optimizers.Adam())
@@ -123,14 +77,11 @@ def train_vq(time, num_embeddings, latent_dim, batch_size):
     vqvae_trainer.vqvae.save(f"out/{time}/vqvae_model")
     vqvae_trainer.vqvae.save_weights(f"out/{time}/vqvae_model_weights.h5")
 
-    print(f"#################\n\rSSIM:{get_structural_similarity(vqvae_trainer.vqvae, test_data)}\n\r#################")
-
-    plot_reconstructions(vqvae_trainer.vqvae, test_data, time)
     plot_losses(history, time, "vq_vae")
 
 
 def train_pixel(time, num_embeddings, latent_dim, batch_size, vqvae_train_path):
-    (train_data, validate_data, test_data, data_variance) = dataset.oasis_dataset(200)
+    (train_data, validate_data, test_data, data_variance) = dataset.oasis_dataset(10)
     vqvae = tf.keras.models.load_model(vqvae_train_path)
     embeddings = vqvae.get_layer("vector_quantizer").embeddings
 
@@ -141,12 +92,12 @@ def train_pixel(time, num_embeddings, latent_dim, batch_size, vqvae_train_path):
                                   + tf.reduce_sum(embeddings ** 2, axis=0) - 2
                                   * tf.matmul(flat_enc_outputs, embeddings)), axis=1).numpy().reshape(encoded_outputs.shape[:-1])
 
-    pixel_cnn = modules.PixelCNN(latent_dim, (64, 64), num_embeddings, 2, 2)
+    pixel_cnn = modules.PixelCNN(latent_dim, num_embeddings,  2, 2)
     pixel_cnn.compile(optimizer=tf.keras.optimizers.Adam(3e-4),
                       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                       metrics=["accuracy"])
     history = pixel_cnn.fit(x=codebook_indices, y=codebook_indices, batch_size=batch_size,
-                            epochs=30, validation_split=0.2)
+                            epochs=10, validation_split=0.2)
 
     plot_losses(history, time, "pixelcnn")
     pixel_cnn.save(f"out/{time}/pixelcnn_model")
