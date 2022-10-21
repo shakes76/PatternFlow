e77a82573414394a1d754339adde34ab9a296de4
Zach Thomas <zthomas1310@gmail.com>
2022-10-21 14:52:30 +1000

    Fixed some issues where there were too many linear layers

--- recognition/45287325_perciever_AD/modules.py
+++ recognition/45287325_perciever_AD/modules.py
@@ -1,11 +1,9 @@
-from inspect import Parameter
-from turtle import forward
 import torch
 import torch.nn as nn
 
 def Conv1D(out_dim):
     return nn.Linear(out_dim, out_dim)
- 
+
 class CrossAttention(nn.Module):
     """
     The inputs are a latent array and the actual data. The latent array is the query and the data forms the key and value.
@@ -16,22 +14,11 @@ class CrossAttention(nn.Module):
     """
     def __init__(self, d_latents):
         super(CrossAttention, self).__init__()
-        self.q_ln = nn.LayerNorm(d_latents)
-        self.kv_ln = nn.LayerNorm(1)
-        self.q_l = nn.Linear(d_latents, d_latents)
-        self.k_l = nn.Linear(1, d_latents)
-        self.v_l = nn.Linear(1, d_latents)
-        self.attn = nn.MultiheadAttention(d_latents, 1, batch_first=True)           
+        self.attn = nn.MultiheadAttention(d_latents, 4, batch_first=True)           
         self.o_l = nn.Linear(d_latents, d_latents)
 
-    def forward(self, latent, data):
-        q = self.q_ln(latent)
-        q = self.q_l(q)
-        k = self.kv_ln(data)
-        k = self.k_l(k)
-        v = self.kv_ln(data)
-        v = self.v_l(v)
-        attn = self.attn(q, k, v)[0]
+    def forward(self, latent, kv):
+        attn = self.attn(latent, kv, kv)[0]
         output = self.o_l(attn)
         return output
 
@@ -45,23 +32,11 @@ class SelfAttention(nn.Module):
     """
     def __init__(self, d_latents):
         super(SelfAttention, self).__init__()
-        self.q_ln = nn.LayerNorm(d_latents)
-        self.k_ln = nn.LayerNorm(d_latents)
-        self.v_ln = nn.LayerNorm(d_latents)
-        self.q_l = nn.Linear(d_latents, d_latents)
-        self.k_l = nn.Linear(d_latents, d_latents)
-        self.v_l = nn.Linear(d_latents, d_latents)
         self.attn = nn.MultiheadAttention(d_latents, 1, batch_first=True)           
         self.o_l = nn.Linear(d_latents, d_latents)
 
     def forward(self, latent):
-        q = self.q_ln(latent)
-        q = self.q_l(q)
-        k = self.k_ln(latent)
-        k = self.k_l(k)
-        v = self.v_ln(latent)
-        v = self.v_l(v)
-        attn = self.attn(q, k, v)[0]
+        attn = self.attn(latent, latent, latent)[0]
         output = self.o_l(attn)
         return output
 
@@ -75,12 +50,14 @@ class LatentTransformer(nn.Module):
         self.ff = nn.ModuleList([MLP(d_latents) for _ in range(depth)])
         self.sa = nn.ModuleList([SelfAttention(d_latents) for _ in range(depth)])
         self.depth = depth
+        self.ln1 = nn.LayerNorm(d_latents)
+        self.ln2 = nn.LayerNorm(d_latents)
 
     def forward(self, x):
         latent = x
         for i in range(self.depth):
-            latent = self.sa[i](latent) + latent
-            latent = self.ff[i](latent)
+            latent = self.sa[i](self.ln1(latent)) + latent
+            latent = self.ff[i](self.ln2(latent)) + latent
         return latent
 
 class MLP(nn.Module):
@@ -103,27 +80,30 @@ class MLP(nn.Module):
 
 class Block(nn.Module):
     """
-    A Cross attend block followed by a Latent Transformer block.
+    A Cross attend block followed.
     """
     def __init__(self, d_latents) -> None:
         super(Block, self).__init__()
         self.ca = CrossAttention(d_latents)
         self.ff = MLP(d_latents)
+        self.ln1 = nn.LayerNorm(d_latents)
+        self.ln2 = nn.LayerNorm(d_latents)
 
     def forward(self, x, data):
-        attn = self.ca(x, data)
+        attn = self.ca(self.ln1(x), data)
         x = attn + x
-        x = self.ff(x)
+        x = self.ff(self.ln2(x) + x)
         return x
         
 
 class Output(nn.Module):
-    def __init__(self, d_latents, n_classes=2) -> None:
+    # FIX PARAMTERER HERE
+    def __init__(self, n_latents, n_classes=2) -> None:
         super(Output, self).__init__()
-        self.project = nn.Linear(d_latents, n_classes)
+        self.project = nn.Linear(n_latents, n_classes)
     
     def forward(self, x):
-        average = torch.mean(x, dim=1)
+        average = torch.mean(x, dim=2)
         logits = self.project(average)
         return logits
 
@@ -140,14 +120,25 @@ class Perciever(nn.Module):
 
         self.ca = nn.ModuleList([Block(d_latents) for _ in range(n_cross_attends)])
         self.lt = nn.ModuleList([LatentTransformer(d_latents, transformer_depth) for _ in range(n_cross_attends)])
-        self.op = Output(d_latents)
+        self.op = Output(n_latents)
+
+        self.image_project = nn.Linear(1, d_latents)
+
+        self.pe = nn.Parameter(torch.empty(1, 240*240, d_latents))
+        nn.init.normal_(self.pe)
 
 
     def forward(self, data):
         b, _, _, _ = data.size()
-        x = torch.stack([self.latent for _ in range(b)])
+        flat_img = torch.flatten(data, start_dim=1)[:, :, None]
+        proj_img = self.image_project(flat_img)
+
+        proj_img = proj_img + self.pe.repeat(b, 1, 1)
+
+        x = self.latent.repeat(b, 1, 1)
+
         for i in range(self.depth):
-            x = self.ca[i](x, torch.flatten(data, start_dim=1)[:, :, None])
+            x = self.ca[i](x, proj_img)
             x = self.lt[i](x)
         output = self.op(x)
         return output
