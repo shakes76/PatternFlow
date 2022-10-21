718e59d67096e8904253ca7208e89c0be01a4b68
Yoxiourn <matthewshearer2@gmail.com>
2022-10-22 00:00:49 +1100

    Fixed bug in predict.py such that it now receives a path on the command line correctly and tests the model. Finalized README file.

--- recognition/s4642283-ADNI-SIAMESE/predict.py
+++ recognition/s4642283-ADNI-SIAMESE/predict.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from PIL import Image
 import sys
 
 # Takes a path to data as a command line argument and makes predictions.
@@ -12,7 +13,10 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 # Load Model
 classifier_model = keras.models.load_model(os.path.join(__location__, "Classifier_Model"))
 
+X_data = []
+X_data_labels = []
 # Load Data (Expects two directory at location, an AD (Alzheimer's) and a NC directory)
+print(os.path.join(__location__, data_path, "AD"))
 for fname in os.listdir(os.path.join(__location__, data_path, "AD")):
     fpath = os.path.join(__location__, data_path, "AD", fname)
     im = Image.open(fpath)
