6c39adabd0cb81682a0692ff44e8192b3df1960b
SkyJemmy28 <jemmyjiang15@hotmail.com>
2022-10-21 22:02:25 +1000

    Fixed bugs inside the get_IOU() function in predict.py

--- recognition/ISIC_YOLO_45828447/predict.py
+++ recognition/ISIC_YOLO_45828447/predict.py
@@ -1,17 +1,23 @@
+# The prediction functionality is done through YOLOv5's detect.py, which is used inside the jupyter notebook, the following are just calculations
+# for determining some of the metrics used to determine the accuracy of the prediction
 import torch
 import numpy as np
 import os
 from PIL import Image
 
 def get_IOU(box_a, box_b):
-    x_left = max(box_a[0], box_b[0])
-    y_top = max(box_a[1], box_b[1])
-    x_right = min(box_a[0] + box_a[2], box_b[0] + box_b[2])
-    y_bottom = min(box_a[1] + box_a[3], box_b[1] + box_b[3])
-
+    if (box_a[0] - box_a[2]/2 > box_b[0] - box_b[2]/2):
+        temp = box_a
+        box_a = box_b
+        box_b = temp
+    x_left = max(box_a[0] - box_a[2]/2, box_b[0] - box_b[2]/2)
+    y_top = max(box_a[1] - box_a[3]/2, box_b[1] - box_b[3]/2)
+    x_right = min(box_a[0] + box_a[2]/2, box_b[0] + box_b[2]/2)
+    y_bottom = min(box_a[1] + box_a[3]/2, box_b[1] + box_b[3]/2)
+    
     if x_right < x_left or y_bottom < y_top:
         return 0.0
-
+    
     intersect_area = (x_right - x_left) * (y_bottom - y_top)
 
     box_a_area = (box_a[2] * box_a[3])
@@ -23,7 +29,7 @@ def get_IOU(box_a, box_b):
 
 def calculate_Image_IOU(test_image_path, label_path):
     # Import model
-    model = torch.hub.load('yolov5', 'custom', 'runs/train/exp2/weights/best.pt')
+    model = torch.hub.load('ultralytics/yolov5', 'custom', '/kaggle/working/yolov5/runs/train/exp5/weights/best.pt')
     iou_list = []
     for dirname, _, filenames in os.walk(test_image_path):
         for filename in filenames:
@@ -31,23 +37,23 @@ def calculate_Image_IOU(test_image_path, label_path):
             file_path = os.path.join(dirname, filename)
             img = Image.open(file_path)
             prediction = model(img)
-            prediction = prediction.xywh[0]
+            prediction = prediction.pandas().xywh[0]
 
             # If no bounding boxes
             if not prediction.empty:
-                min_x = prediction.loc[0]["xcenter"]/img.shape[0]
-                max_y = prediction.loc[0]["ycenter"]/img.shape[1]
-                width = prediction.loc[0]["width"]/img.shape[0]
-                height = prediction.loc[0]["height"]/img.shape[1]
-                
+                center_x = prediction.loc[0]["xcenter"]/np.array(img).shape[0]
+                center_y = prediction.loc[0]["ycenter"]/np.array(img).shape[1]
+                width = prediction.loc[0]["width"]/np.array(img).shape[0]
+                height = prediction.loc[0]["height"]/np.array(img).shape[1]
+                #print(center_x, center_y, width, height)
                 # Open label .txt
                 label_val = open(label_path + label_filename).read().split()
 
                 # Convert center coords to left most and top most
-                label_min_x = float(label_val[1]) - float(label_val[3])/2
-                label_min_y = float(label_val[2]) - float(label_val[4])/2
-                
-                iou = get_IOU([min_x, max_y, width, height], [label_min_x, label_min_y, float(label_val[3]), float(label_val[4])])
+                #label_min_x = float(label_val[1]) - float(label_val[3])/2
+                #label_min_y = float(label_val[2]) - float(label_val[4])/2
+
+                iou = get_IOU([center_x, center_y, width, height], [float(label_val[1]), float(label_val[2]), float(label_val[3]), float(label_val[4])])
                 iou_list.append(iou)
 
     return min(iou_list), np.average(iou_list)
