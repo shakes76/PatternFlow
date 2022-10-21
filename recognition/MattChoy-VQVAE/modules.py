9d78ea8c70500cb5bb3c1f4a09dd6444ead41b9e
MattPChoy <matthewpchoy@gmail.com>
2022-10-21 11:08:48 +1000

    Doubled kernels in VQVAE

--- recognition/MattChoy-VQVAE/modules.py
+++ recognition/MattChoy-VQVAE/modules.py
@@ -16,11 +16,11 @@ class Encoder(tf.keras.Model):
         super().__init__(**kwargs)
         self.encoder = Sequential(name="encoder")
         # filters, kernel_size
-        self.encoder.add(Conv2D(16, 3, activation="relu", strides=2, padding="same",\
+        self.encoder.add(Conv2D(32, 3, activation="relu", strides=2, padding="same",\
                         input_shape=(image_shape[0], image_shape[0], n_channels)))
-        self.encoder.add(Conv2D(32, 3, activation="relu", strides=2, padding="same"))
         self.encoder.add(Conv2D(64, 3, activation="relu", strides=2, padding="same"))
         self.encoder.add(Conv2D(128, 3, activation="relu", strides=2, padding="same"))
+        self.encoder.add(Conv2D(256, 3, activation="relu", strides=2, padding="same"))
         self.encoder.add(Conv2D(latent_dimensions, 1, padding="same"))
 
         self.out_shape = (1, 16, 16, 16)
@@ -32,10 +32,10 @@ class Decoder(tf.keras.Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.decoder = Sequential(name="decoder")
+        self.decoder.add(Conv2DTranspose(256, 3, activation="relu", strides=2, padding="same"))
         self.decoder.add(Conv2DTranspose(128, 3, activation="relu", strides=2, padding="same"))
         self.decoder.add(Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same"))
         self.decoder.add(Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same"))
-        self.decoder.add(Conv2DTranspose(16, 3, activation="relu", strides=2, padding="same"))
         self.decoder.add(Conv2DTranspose(1, 3, padding="same"))
 
     def call(self, x):
