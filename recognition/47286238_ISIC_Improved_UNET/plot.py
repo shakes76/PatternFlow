548a2fca1b9232ba3989a3dbda817f6160235d8a
Avatar Azka <avatar.azka@gmail.com>
2022-10-21 10:47:20 +1000

    docs: add comments in plot.py

--- recognition/47286238_ISIC_Improved_UNET/plot.py
+++ recognition/47286238_ISIC_Improved_UNET/plot.py
@@ -16,11 +16,15 @@ if __name__ == '__main__':
     with open('validation_loss.pkl', 'rb') as f:
         validation_loss = pickle.load(f)
     
+    # move tensors to cpu
     train_dsc = [x.cpu() for x in train_dsc]
     train_loss = [x.item() for x in train_loss]
     validation_dsc = [x.cpu() for x in validation_dsc]
     validation_loss = [x.item() for x in validation_loss]
+
     fig = plt.figure()
+
+    # plot DSC change over epochs
     plt.title('Change in Dice Similarity Coefficient over Epochs')
     plt.plot(range(len(train_dsc)), train_dsc, label='Training DSC')
     plt.plot(range(len(train_dsc)), validation_dsc, label='Validation DSC')
@@ -30,6 +34,7 @@ if __name__ == '__main__':
     plt.savefig('static/dsc_history.png')
 
     plt.clf()
+    # plot Loss change pover opeepochs
     plt.title('Change in DSC Loss over Epochs')
     plt.plot(range(len(train_loss)), train_loss, label='Training Loss')
     plt.plot(range(len(validation_loss)), validation_loss, label='Validation Loss')
