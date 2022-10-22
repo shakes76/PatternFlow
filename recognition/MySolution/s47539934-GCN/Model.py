9d5c1a20e09df1a697666808c3c5d59a836f89c2
mr-popo123 <116279628+mr-popo123@users.noreply.github.com>
2022-10-22 19:12:35 +1000

    added header block and comments in model.py

--- recognition/MySolution/s47539934-GCN/Model.py
+++ recognition/MySolution/s47539934-GCN/Model.py
@@ -1,3 +1,7 @@
+"""
+Author: Arsh Upadhyaya, 47539934
+Code for 2 layer GCN model
+"""
 import math
 import torch
 import torch.nn as nn
@@ -7,16 +11,19 @@ from torch.nn.parameter import Parameter
 
 
 class GraphConvolution(nn.Module):
- 
-
+    '''
+    Starting of graph convolutional layer.
+    Parameters:
+    input_features: dimensions of input layer
+    output_features: dimenstions of output layer
+    use_bias: optional but good practice
+    '''
     def __init__(self, input_features, output_features, use_bias=True):
         super(GraphConvolution, self).__init__()
         self.input_features = input_features
         self.output_features = output_features
-        # weight in the layer
         self.use_bias=use_bias
         self.weight = nn.Parameter(torch.FloatTensor(input_features, output_features))
-        # bias in the layer
         if self.use_bias:
             self.bias = nn.Parameter(torch.FloatTensor(output_features))
         else:
@@ -29,9 +36,13 @@ class GraphConvolution(nn.Module):
         if self.use_bias:
             init.zeros_(self.bias)
 
-
+'''
+parameters:
+in_feature: an n-dimenstional vector
+adj_matrix: an adjacency matrix in tensor format
+'''
     def forward(self, adj_matrix, in_feature):
-        # input * weight
+     
         support = torch.mm(in_feature, self.weight) 
         output = torch.sparse.mm(adj_matrix, support)
         if self.bias is not None:
@@ -44,14 +55,16 @@ class GraphConvolution(nn.Module):
                + str(self.input_features) + ' -> ' \
                + str(self.output_features) + ')'
 
-# 2 layer GCN
 class GCN(nn.Module):
+ '''
+ A model that contains 2 layers of GCN , by creating 2 instances from GraphConvolution function
+ '''
     def __init__(self, in_feature=128):
         super(GCN, self).__init__()
 
-        # first GraphConvolution layer
+        # first GraphConvolution layer taking input of 128 as that is format in facebook.npz
         self.layer1 = GraphConvolution(in_feature,32) 
-        # second GraphConvolution layer
+        # second GraphConvolution layer having 32 as input, which is output from previous layer
         self.layer2 = GraphConvolution(32, 8)  
         
 
