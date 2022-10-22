9c683b13bfd5934c552de2df7ea04e5959670d3b
Danielle Soilleux <d.soilleux@uqconnect.edu.au>
2022-10-22 21:42:32 +1000

    Updated preprocess_data(), cleaned docstrings/imports

--- recognition/45853757-VQVAE/dataset.py
+++ recognition/45853757-VQVAE/dataset.py
@@ -1,29 +1,19 @@
-import numpy as np
-#from PIL import Image
-#import tensorflow as tf
-from tensorflow.keras.utils import load_img, img_to_array
 import os
-import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.utils import load_img, img_to_array
+
 
 def preprocess_data(training_data, validation_data, testing_data):
-    """
-    Normalise each data set and find the variance of training data
+    " Normalises each data set and finds the variance of training data "
 
-    Params: 
-
-    Returns: 
-    """
     training_data = np.array(training_data)
     training_data = training_data.astype('float16') / 255.
-    training_data = training_data.reshape((len(training_data), np.prod(training_data.shape[1:])))
 
     validation_data = np.array(validation_data)
     validation_data = validation_data.astype('float16') / 255.
-    validation_data = validation_data.reshape((len(validation_data), np.prod(validation_data.shape[1:])))
-
+   
     testing_data = np.array(testing_data)
     testing_data = testing_data.astype('float16') / 255.
-    testing_data = testing_data.reshape((len(testing_data), np.prod(testing_data.shape[1:])))
 
     variance = np.var(training_data / 255.)
 
@@ -43,17 +33,16 @@ def load_data():
     validation_data = []
     testing_data = []
 
-    # Create tuple pairs of the directories for the files with the images, and
+    # Create list pairs of the directories for the files with the images, and
     # which list they should be sorted into
     location_and_data_category = [["D:/keras_png_slices_data/keras_png_slices_train", \
             training_data], ["D:/keras_png_slices_data/keras_png_slices_vaildate", \
             validation_data], ["D:/keras_png_slices_data/keras_png_slices_test", \
             testing_data]]
 
-    # Find and store the individual directories for each image in each file
+    # Find and store each image in each file into the correct list
     for dataset in location_and_data_category:
         for file_name in os.listdir(dataset[0]):
             dataset[1].append(img_to_array(load_img(os.path.join(dataset[0], file_name), color_mode="grayscale")))
-        #dataset[1] = np.array(dataset[1]).squeeze()
     
     return preprocess_data(training_data, validation_data, testing_data)
