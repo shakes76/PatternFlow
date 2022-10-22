c808c20fe3a6058c7889a6242ca86c61215ed34b
mr-popo123 <116279628+mr-popo123@users.noreply.github.com>
2022-10-23 02:49:40 +1000

    Change structure of data.py, to better fit test and train set
    The previous datatypes for adjacency matrix, and especially features were creating problems during testing phase.

--- recognition/MySolution/s47539934-GCN/Data.py
+++ recognition/MySolution/s47539934-GCN/Data.py
@@ -14,34 +14,41 @@ import torch.optim as optim
 
 
 def load_data(file_path):
-  data=np.load(file_path)
-  edges= data['edges']
-  features=data['features']
-  target=data['target']
+    '''
+    parameters: 
+    takes file path
+    returns:
+    Adjacency matrix: normalized coo matrix
+    features and labels(targets) as tensors
+    '''
 
-def normalize_adj(matrix):
-  #add identity matrix
-  matrix += sp.eye(matrix.shape[0])
-  degree = np.array(matrix.sum(1))
-  #calculate L=D^-0.5 * (A+I) * D^-0.5 
-  d_hat = sp.diags(np.power(degree, -0.5).flatten())
-  return d_hat.dot(matrix).dot(d_hat).tocoo()
+    data = np.load("/content/drive/MyDrive/facebook.npz")
+    edges = data['edges']
+    features = data['features']
+    labels = data['target']
 
-file_dir="/content/drive/MyDrive/facebook.npz"#path in google colab
-load_data(file_dir)
-features=preprocessing.normalize(features)#normalize features
-# Adjacency matrix A-- n*n
-adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-                        shape=(features.shape[0], features.shape[0]),
-                        dtype=np.float32)
+    features = sp.csr_matrix(features)
 
-adj=normalize_adj(adj)#normalize adjacency matrix
-features = torch.FloatTensor(features)#transform normalized data to tensor
-labels = torch.LongTensor(target)
-num_nodes = features.shape[0]
-num_features = features.shape[1]
-num_edges = edges.shape[0]
-#split the data into train, validation and test in 20:20:60 ratio
-train_set = torch.LongTensor(range(int(num_nodes*0.2)))
-val_set = torch.LongTensor(range(int(num_nodes*0.2),int(num_nodes*0.4)))
-test_set = torch.LongTensor(range(int(num_nodes*0.4),num_nodes))
+    adj= sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),shape=(labels.shape[0], labels.shape[0]))
+
+    #normalize
+    colsum = np.array(adj.sum(0))
+    D = np.power(colsum, -1)[0]
+    D[np.isinf(D)] = 0
+    D_inv = sp.diags(D)
+    adj_trans = D_inv.dot(adj)
+
+    #transform data type
+    indices = torch.LongTensor(np.vstack((adj_trans.tocoo().row, adj_trans.tocoo().col)))
+    values = torch.FloatTensor(adj_trans.data)
+    shape = adj_trans.shape
+
+    adj_trans = torch.sparse_coo_tensor(indices, values, shape)
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(labels)
+
+    return adj_trans, features, labels
+    #split the data into train, validation and test in 20:20:60 ratio
+    train_set = torch.LongTensor(range(int(num_nodes*0.2)))
+    val_set = torch.LongTensor(range(int(num_nodes*0.2),int(num_nodes*0.4)))
+    test_set = torch.LongTensor(range(int(num_nodes*0.4),num_nodes))
