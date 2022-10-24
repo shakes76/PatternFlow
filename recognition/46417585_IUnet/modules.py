2c0c418a7a116f72337ce0833539c82252285d2b
James Seymour <james.seymour@cubiko.com.au>
2022-10-25 00:17:58 +1000

    modules: add comments to all layers and remove unused Decoder

--- recognition/46417585_IUnet/modules.py
+++ recognition/46417585_IUnet/modules.py
@@ -14,6 +14,10 @@ from keras.models import Model
 
 # fmt: off
 def ConvBlock(filters: int):
+    """ 
+    A convolution block consisting of two 2D convolutional layers with instance normalised inputs that are joined by a dropout to prevent overfitting
+    Emulates the keras functional API
+    """
     def _create(inputs):
         normalized = InstanceNormalization()(inputs)
         conv_layer_1 = Conv2D(filters, (3, 3), padding="same", activation=LeakyReLU(0.01))(normalized)
@@ -29,6 +33,10 @@ def ConvBlock(filters: int):
 
 
 def Encoder(filters: int, strides=(2,2)):
+    """ 
+    The main encoder block which returns the sum of a regular 2D convolutional layer and the above `ConvBlock`
+    Emulates the keras functional API
+    """
     def _create(inputs):
         conv = Conv2D(filters, (3, 3), padding="same", strides=strides)(inputs)
         encoded = ConvBlock(filters)(conv)
@@ -39,6 +47,10 @@ def Encoder(filters: int, strides=(2,2)):
 
 
 def UpSample(filters: int):
+    """
+    A wrapper for the `UpSampling2D` layer which also includes a convolutional layer to extract any upsampled features
+    Emulates the keras functional API
+    """
     def _create(inputs):
         upsampled = UpSampling2D(size=(2, 2))(inputs)
         conv = Conv2D(filters, (3, 3), padding="same", activation=LeakyReLU(0.01))(upsampled)
@@ -47,12 +59,20 @@ def UpSample(filters: int):
     return _create
 
 def Segmentation(filters: int):
+    """
+    A wrapper for a regular `Conv2D` layer that creates a one-to-one projection which pools feature maps
+    Emulates the keras functional API
+    """
     def _create(inputs):
         return Conv2D(filters, (1, 1), padding="same", activation=LeakyReLU(0.01))(inputs)
 
     return _create
 
 def Localisation(filters: int):
+    """
+    A localisation module which recombines the upsampled features with the corresponding contextual features
+    Emulates the keras functional API
+    """
     def _create(inputs):
         conv = Conv2D(filters, (3, 3), padding="same", activation=LeakyReLU(0.01))(inputs)
         conv = Conv2D(filters, (1, 1), padding="same", activation=LeakyReLU(0.01))(conv)
@@ -60,37 +80,26 @@ def Localisation(filters: int):
 
     return _create
 
-
-def Decoder(filters: int):
-    def _create(inputs, skip):
-        # Upsample input
-        upsampled = UpSampling2D(size=(2, 2))(inputs)
-        conv = Conv2D(filters, (3, 3), padding="same", activation=LeakyReLU(0.01))(upsampled)
-
-        # Join skip
-        concatenated = Concatenate()([conv, skip])
-
-        # Localisation (halves features)
-        localisation = Localisation(filters)(concatenated)
-        
-        return localisation
-
-    return _create
 # fmt: on
 
 from constants import IMG_DIM
 
 
 def UNet():
+    """
+    Builds and returns a version of an improved UNet which can be used to segment the ISIC challenge dataset
+    """
     base_filters = 16
     inputs = Input(shape=(IMG_DIM, IMG_DIM, 3))
 
+    # Feature extraction and aggregation of high level characteristics
     encoded_256 = Encoder(base_filters, strides=(1, 1))(inputs)
     encoded_128 = Encoder(base_filters * 2)(encoded_256)
     encoded_64 = Encoder(base_filters * 4)(encoded_128)
     encoded_32 = Encoder(base_filters * 8)(encoded_64)
     encoded_16 = Encoder(base_filters * 16)(encoded_32)
 
+    # Upsampling of features and recombination with aggregated features
     upsample_32 = UpSample(base_filters * 8)(encoded_16)
     concat_32 = Concatenate()([upsample_32, encoded_32])
     localisation_32 = Localisation(base_filters * 8)(concat_32)
@@ -99,6 +108,7 @@ def UNet():
     concat_64 = Concatenate()([upsample_64, encoded_64])
     localisation_64 = Localisation(base_filters * 4)(concat_64)
 
+    # Balance model performance / training time with segmentation layerse
     segmentation_64 = Segmentation(base_filters)(localisation_64)
     segmentation_64 = UpSampling2D()(segmentation_64)
 
