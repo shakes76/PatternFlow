7e57863802f7d815e468665db922783889c4d721
James Seymour <james.seymour@cubiko.com.au>
2022-10-24 22:31:07 +1000

    train: decrease learning_rate to 0.0003

--- recognition/46417585_IUnet/train.py
+++ recognition/46417585_IUnet/train.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from keras.optimizers import Adam
 
 from constants import EPOCHS
@@ -9,7 +10,7 @@ from utils import DSC, DSC_loss
 def train():
     model = UNet()
 
-    model.compile(optimizer=Adam(learning_rate=0.001), loss=DSC_loss, metrics=[DSC])
+    model.compile(optimizer=Adam(learning_rate=0.0003), loss=DSC_loss, metrics=[DSC])
 
     history = model.fit(
         train_data,
@@ -19,4 +20,8 @@ def train():
 
     model.save("UNet.h5")
 
-    return model, history
+    return model
+
+
+if __name__ == "__main__":
+    train()
