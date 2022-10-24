aa8df4fe065fd4df5c1b0013704a7a7b2f965519
Joshua McAllan <joshuamcallan@gmail.com>
2022-10-25 03:19:53 +1000

    Added a simple decoder network for the VQVAE.

--- recognition/s46144603-VQVAE/modules.py
+++ recognition/s46144603-VQVAE/modules.py
@@ -13,3 +13,12 @@ def encoder_net(latent_dim=32):
   return tf.keras.Model(inputs=input, outputs=enc_out, name='encoder')
 
 
+# Build decoder
+def decoder_net(latent_dim=32):
+  decoder_in = tf.keras.Input(shape=encoder_net(latent_dim).output.shape[1:])
+
+  net = tf.keras.layers.Conv2DTranspose(depth*2, 3, padding='same', strides=2, activation='relu')(decoder_in)
+  net = tf.keras.layers.Conv2DTranspose(depth, 3, padding='same', strides=2, activation='relu')(net)
+  network = tf.keras.layers.Conv2DTranspose(1, 3, padding='same')(net)
+
+  return tf.keras.Model(inputs=decoder_in, outputs=network, name='decoder')
