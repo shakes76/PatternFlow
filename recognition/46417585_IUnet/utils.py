ccf7a231bee41caddb92780d6629f770c4b78cab
James Seymour <james.seymour@cubiko.com.au>
2022-10-24 21:41:00 +1000

    utils: clean up DSC calculation and calculate DSC_loss

--- recognition/46417585_IUnet/utils.py
+++ recognition/46417585_IUnet/utils.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 
 
-def DSC(tensor_1: tf.Tensor, tensor_2: tf.Tensor):
+def DSC(tensor_1, tensor_2):
     "Calculates the Dice similarity coefficient for two given tensors"
-    matched_elements = tf.math.reduce_sum(tf.cast(tensor_1 == tensor_2, tf.uint8))
-    total_elements = tf.size(tensor_1) + tf.size(tensor_2)
-    return 2 * matched_elements.numpy() / total_elements.numpy()
+    intersection = tf.reduce_sum((tensor_1 * tensor_2))
+    union = tf.reduce_sum(tensor_1) + tf.reduce_sum(tensor_2)
+    return 2 * intersection / union
+
+
+DSC_loss = lambda t1, t2: 1 - DSC(t1, t2)
