81f131e644daded0164f314034b2af9a364b14cc
Justin Ye <s4647738@student.uq.edu.au>
2022-10-25 15:42:37 +1000

    Added LeakyReLu with negative slope of 10^-2 for all Conv2D layers as specified in the paper

--- recognition/s4647738-UNet/modules.py
+++ recognition/s4647738-UNet/modules.py
@@ -17,11 +17,13 @@ class ContextModule(Layer):
     super(ContextModule, self).__init__()
     self.convA = Conv2D(filters = filters,
                         kernel_size = (3,3),
-                        padding='same')
+                        padding='same',
+                        activation = LeakyReLU(0.01))
     self.dropout = Dropout(0.3)
     self.convB = Conv2D(filters = filters,
                         kernel_size = (3,3),
-                        padding='same')
+                        padding = 'same',
+                        activation = LeakyReLU(0.01))
   def call(self, inputs):
     x = self.convA(inputs)
     x = self.convB(x)
@@ -35,10 +37,12 @@ class LocalizationModule(Layer):
     super(LocalizationModule, self).__init__()
     self.convA = Conv2D(filters = filters,
                         kernel_size = (3,3),
-                        padding='same')
+                        padding='same',
+                        activation = LeakyReLU(0.01))
     self.convB = Conv2D(filters = filters,
                         kernel_size = (1,1),
-                        padding='same')
+                        padding='same',
+                        activation = LeakyReLU(0.01))
   def call(self, inputs):
     x = self.convA(inputs)
     return self.convB(x)
@@ -51,8 +55,8 @@ class SegmentationModule(Layer):
     super(SegmentationModule, self).__init__()
     self.conv = Conv2D(filters = filters, 
                        kernel_size = (3, 3), 
-                       padding='same')
-    self.relu = LeakyReLU(alpha = 0.01)
+                       padding='same',
+                       activation = LeakyReLU(0.01))
     self.up = UpSampling2D(size = (2, 2))
     self.upscale = upscale
   def call(self, inputs):
@@ -70,27 +74,27 @@ def improved_unet():
   ### ENCODER ###
   inputs = Input(shape=(256, 256, 1))
   # layer 1
-  conv1 = Conv2D(filters = 16, kernel_size = (3,3), padding='same')(inputs)
+  conv1 = Conv2D(filters = 16, kernel_size = (3,3), padding='same', activation = LeakyReLU(0.01))(inputs)
   context1 = ContextModule(16)(conv1) 
   add1 = Add()([conv1, context1])
 
   # layer 2
-  conv2 = Conv2D(filters = 32, kernel_size = (3,3), padding='same', strides = 2)(add1)
+  conv2 = Conv2D(filters = 32, kernel_size = (3,3), padding='same', strides = 2, activation = LeakyReLU(0.01))(add1)
   context2 = ContextModule(32)(conv2)
   add2 = Add()([conv2, context2])
 
   # layer 3
-  conv3 = Conv2D(filters = 64, kernel_size = (3,3), padding='same', strides = 2)(add2)
+  conv3 = Conv2D(filters = 64, kernel_size = (3,3), padding='same', strides = 2, activation = LeakyReLU(0.01))(add2)
   context3 = ContextModule(64)(conv3)
   add3 = Add()([conv3, context3])
 
   # layer 4
-  conv4 = Conv2D(filters = 128, kernel_size = (3,3), padding='same', strides = 2)(add3)
+  conv4 = Conv2D(filters = 128, kernel_size = (3,3), padding='same', strides = 2, activation = LeakyReLU(0.01))(add3)
   context4 = ContextModule(128)(conv4)
   add4 = Add()([conv4, context4])
 
   # layer 5
-  conv5 = Conv2D(filters = 256, kernel_size = (3,3), padding='same', strides = 2)(add4)
+  conv5 = Conv2D(filters = 256, kernel_size = (3,3), padding='same', strides = 2, activation = LeakyReLU(0.01))(add4)
   context5 = ContextModule(256)(conv5)
   add5 = Add()([conv5, context5])
   ### DECODER ###
@@ -117,7 +121,7 @@ def improved_unet():
 
   # layer 1
   concat4 = Concatenate()([up4, add1])
-  conv2 = Conv2D(filters = 32, kernel_size = (3,3), padding='same')(concat4)
+  conv2 = Conv2D(filters = 32, kernel_size = (3,3), padding='same', activation = LeakyReLU(0.01))(concat4)
   seg3 = SegmentationModule(2, upscale=False)(conv2)
   add7 = Add()([seg3, add6])
   outputs = Conv2D(filters = 2, kernel_size = (3, 3), padding='same', activation='softmax')(add7)
