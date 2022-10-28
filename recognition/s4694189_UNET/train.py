8b3a33c7b7941e46454206eba37ca22743d5b8f4
Ojas Chaudhari <o.chaudhari@uqconnect.edu.au>
2022-10-28 22:57:54 +1000

    Final train python file

--- recognition/s4694189_UNET/train.py
+++ recognition/s4694189_UNET/train.py
@@ -1,60 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
 from dataset import *
 from module import *
 from keras import backend as K
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 # Unet model is being called from the file modules
 #model = unet_model()
-
-
-# In[17]:
-
-
 model = unet_model()
 
-
-# In[15]:
-
-
-smooth = 1e-15
-def dice_coef(y_true, y_pred):
+def dice_coef(y_true, y_pred, smooth = 1e-15):
+    y_true = tf.convert_to_tensor(y_true, dtype='float32')
+    y_pred = tf.convert_to_tensor(y_pred, dtype='float32')
     y_true = tf.keras.layers.Flatten()(y_true)
     y_pred = tf.keras.layers.Flatten()(y_pred)
     intersection = tf.reduce_sum(y_true * y_pred)
     return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
 
-
-# In[ ]:
-
-
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics = [dice_coef])
 model.summary()
-
-
-# In[ ]:
-
 
 model.fit(X_train, masks_train_images, batch_size=8, epochs=10,
                validation_data=(X_validate, masks_valid_images))
 
 
-# In[ ]:
-
-
 model.save("improved_UNET.h5")
 
-
-# In[ ]:
-
-
 # Plotting our loss charts
-import matplotlib.pyplot as plt
 
 # Use the History object we created to get our saved performance results
 history_dict = model.history.history
@@ -78,9 +49,6 @@ plt.legend()
 plt.show()
 
 
-# In[ ]:
-
-
 # Plotting the graph for dice coefficient
 plt.plot(model.history.history['dice_coef'])
 plt.plot(model.history.history['val_dice_coef'])
@@ -90,7 +58,6 @@ plt.xlabel('epoch')
 plt.show()
 
 
-# In[ ]:
 
 
 
