5a0a00e456d66037751260f830412c27ae1b9edd
Ojas Chaudhari <o.chaudhari@uqconnect.edu.au>
2022-10-28 23:03:46 +1000

    Final predict file

--- recognition/s4694189_UNET/predict.py
+++ recognition/s4694189_UNET/predict.py
@@ -1,33 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
+# ## Load the required libraries and functions from different files
 
 # We need to import our load_model function 
 from tensorflow.keras.models import load_model
 import keras
 from dataset import *
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 model = load_model('improved_UNET.h5')
 
-
-# In[ ]:
-
-
+#Evaluate model on test dataset
 model.evaluate(np.round(model.predict(x_test,batch_size=8)))
 
-
-# In[ ]:
-
-
+#Predict the images
 pred_image = np.round(model.predict(x_test,batch_size=8))
 
 
-# In[ ]:
-
-
+#Visualize the images
 def imshow(title, image = None, size = 20):
     if image.any():
         w, h = image.shape[0], image.shape[1]
@@ -38,9 +26,6 @@ def imshow(title, image = None, size = 20):
         plt.show()
     else:
         print("Image not found")
-
-
-# In[ ]:
 
 
 for i in range(0,6):
